767278a83014f1d4438b6333154a1b1de5ca762b
Peter Pistorius <peter.pistorius@gmail.com>
2019-06-23 09:54:22 +0200

    Parse argvs.

--- packages/hammer-cli/src/index.js
+++ packages/hammer-cli/src/index.js
@@ -2,36 +2,59 @@ import path from 'path';
 import React from 'react';
 import { render, Color, Text, Box } from 'ink';
 import requireDir from 'require-dir';
+import argv from 'yargs-parser';
 
 import Header from 'src/components/Header';
 
-// TODO: Show this menu if an unknown command was passed.
-// TODO: Route to the executed command.
-const MainMenu = () => {
-  const commands = requireDir('./commands', {
+export const getCommands = (cmdPath = './commands') => {
+  return requireDir('./commands', {
     recurse: false,
     extensions: ['.js'],
   });
+};
 
+export const parseArgs = () => {
+  return argv(process.argv.slice(2));
+};
+
+const CommandList = ({ commands }) => {
   return (
     <Box flexDirection="column">
-      <Header />
-      <Box flexDirection="column">
-        <Box paddingY={1} bold>
+      <Box marginBottom={1}>
+        <Text bold underline>
           Commands
-        </Box>
-        {Object.keys(commands).map(command => {
-          return (
-            <Text key={command}>
-              <Text bold> {command}</Text>
-              {'    '}
-              {commands[command].description}
-            </Text>
-          );
-        })}
+        </Text>
       </Box>
+      {Object.keys(commands).map(command => {
+        const {
+          commandProps: { description },
+        } = commands[command];
+
+        return (
+          <Text key={command}>
+            <Text bold> {command}</Text>
+            {'    '}
+            {description}
+          </Text>
+        );
+      })}
     </Box>
   );
 };
 
-render(<MainMenu />);
+const MainMenu = ({ commands, args }) => {
+  const command = args._[0];
+
+  return (
+    <Box flexDirection="column">
+      <Header marginBottom={1} />
+      {Object.keys(commands).includes(command) ? (
+        commands[command].default({ args })
+      ) : (
+        <CommandList commands={commands} />
+      )}
+    </Box>
+  );
+};
+
+render(<MainMenu commands={getCommands()} args={parseArgs()} />);
