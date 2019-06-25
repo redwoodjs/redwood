47a78ce1feb8d209ffebbec7f025c326acddbf4d
Peter Pistorius <peter.pistorius@gmail.com>
2019-06-25 09:22:30 +0200

    A small reorganization.

--- packages/hammer-cli/src/index.js
+++ packages/hammer-cli/src/index.js
@@ -1,55 +1,21 @@
 import path from 'path';
 import React from 'react';
 import { render, Color, Text, Box } from 'ink';
-import requireDir from 'require-dir';
-import argv from 'yargs-parser';
 
-import Header from 'src/components/Header';
-
-export const getCommands = (cmdPath = './commands') => {
-  return requireDir('./commands', {
-    recurse: false,
-    extensions: ['.js'],
-  });
-};
-
-export const parseArgs = () => {
-  return argv(process.argv.slice(2));
-};
-
-const CommandList = ({ commands }) => {
-  return (
-    <Box flexDirection="column">
-      <Box marginBottom={1}>
-        <Text bold underline>
-          Commands
-        </Text>
-      </Box>
-      {Object.keys(commands).map(command => {
-        const {
-          commandProps: { description },
-        } = commands[command];
-
-        return (
-          <Text key={command}>
-            <Text bold> {command}</Text>
-            {'    '}
-            {description}
-          </Text>
-        );
-      })}
-    </Box>
-  );
-};
+import { getCommands, parseArgs } from 'src/lib';
+import { Header, CommandList } from 'src/components';
 
 const MainMenu = ({ commands, args }) => {
-  const command = args._[0];
+  const commandToRun = args._[0];
+  const command = commands.find(({ commandProps: { name, alias } }) =>
+    [name, alias].includes(commandToRun)
+  );
 
   return (
     <Box flexDirection="column">
       <Header marginBottom={1} />
-      {Object.keys(commands).includes(command) ? (
-        commands[command].default({ args })
+      {command ? (
+        command.default({ args })
       ) : (
         <CommandList commands={commands} />
       )}
@@ -57,4 +23,6 @@ const MainMenu = ({ commands, args }) => {
   );
 };
 
-render(<MainMenu commands={getCommands()} args={parseArgs()} />);
+if (process.env.NODE_ENV !== 'test') {
+  render(<MainMenu commands={getCommands()} args={parseArgs()} />);
+}
