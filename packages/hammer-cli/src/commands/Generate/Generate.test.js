91dc50452c6aa1d55e969b195849678e4256db31
Peter Pistorius <peter.pistorius@gmail.com>
2019-06-30 09:49:48 +0200

    Moving some things around.

--- packages/hammer-cli/src/commands/Generate/Generate.test.js
+++ packages/hammer-cli/src/commands/Generate/Generate.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'ink-testing-library';
 
-import Generate from './generate';
+import Generate from './Generate';
 
 describe('Command: Generate', () => {
   const generators = {
