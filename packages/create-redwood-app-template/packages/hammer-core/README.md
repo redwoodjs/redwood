d06d1fea099f8465f148549e8cdf987b3ce6bfce
Peter Pistorius <peter.pistorius@gmail.com>
2019-09-14 11:49:39 +0200

    Add hammer core.

---
+++ packages/create-redwood-app-template/packages/hammer-core/README.md
@@ -0,0 +1,5 @@
+# The Hammer Core
+
+This package searches for and parses the `hammer.toml` configuration
+file. The `hammer.toml` file is used to designate the base
+directory of a hammer app.
---
+++ packages/create-redwood-app-template/packages/hammer-core/src/main.ts
@@ -0,0 +1,63 @@
+import fs from 'fs'
+import path from 'path'
+import findUp from 'findup-sync'
+import toml from 'toml'
+
+const HAMMER_CONFIG_FILE = 'hammer.toml'
+
+export const getHammerConfigPath = (): string => {
+  const configPath = findUp(HAMMER_CONFIG_FILE)
+  if (!configPath) {
+    throw new Error(
+      `Could not find a "${HAMMER_CONFIG_FILE}" file, are you in a hammer project?`
+    )
+  }
+  return configPath
+}
+
+export const getHammerBaseDir = (
+  configPath: string = getHammerConfigPath()
+): string => {
+  return path.dirname(configPath)
+}
+
+export interface HammerConfig {
+  baseDir: string
+  web: {
+    port: number
+    apiProxyPath: string
+  }
+  api: {
+    port: number
+    paths: {
+      functions: string
+      graphql: string
+      generated: string
+    }
+  }
+}
+
+export const getHammerConfig = (): HammerConfig => {
+  const configPath = getHammerConfigPath()
+  const baseDir = getHammerBaseDir(configPath)
+  try {
+    const config = toml.parse(
+      fs.readFileSync(configPath, 'utf8')
+    ) as HammerConfig
+
+    return {
+      ...config,
+      baseDir,
+      api: {
+        ...config.api,
+        paths: {
+          functions: path.join(baseDir, config.api.paths.functions),
+          graphql: path.join(baseDir, config.api.paths.graphql),
+          generated: path.join(baseDir, config.api.paths.generated),
+        },
+      },
+    }
+  } catch (e) {
+    throw new Error(`Could not parse "${configPath}": ${e}`)
+  }
+}
