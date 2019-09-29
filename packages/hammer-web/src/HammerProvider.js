47bf78b7ee37371711ec28768e3c40a4f62f628f
Peter Pistorius <peter.pistorius@gmail.com>
2019-09-29 18:48:51 +0200

    Add default value.

--- packages/hammer-web/src/HammerProvider.js
+++ packages/hammer-web/src/HammerProvider.js
@@ -9,7 +9,7 @@ export const useAuth = () => {
   return USE_AUTH()
 }
 
-const HammerProvider = ({ auth, theme = {}, children }) => {
+const HammerProvider = ({ auth = {}, theme = {}, children }) => {
   const {
     AuthProvider = React.Fragment,
     GraphQLProvider = RealGraphQLProvider,
