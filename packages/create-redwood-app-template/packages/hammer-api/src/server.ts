fa309feedc280643a8096be3a205400de3a52362
Peter Pistorius <peter.pistorius@gmail.com>
2019-11-16 15:52:28 +0100

    Wrap apollo server.

--- packages/create-redwood-app-template/packages/hammer-api/src/server.ts
+++ packages/create-redwood-app-template/packages/hammer-api/src/server.ts
@@ -1,32 +1,15 @@
-import { queryType, makeSchema } from 'nexus'
-import { ApolloServer } from 'apollo-server-lambda'
+import { ApolloServer, Config } from 'apollo-server-lambda'
 
-export interface Config {
-  context?: object
-  types: object
-}
-
-export const server = (
-  { context, types }: Config = { types: {} }
-): ApolloServer => {
-
-  const BaseQueryType = queryType({
-    definition(t) {
-      t.string('help', {
-        resolve() {
-          return `Learn more about adding nexus definitions over here: "https://nexus.js.org/docs/api-extendtype"`
-        },
-      })
-    },
-  })
-
-  const schema = makeSchema({
-    types: [BaseQueryType, ...Object.values(types)],
-    outputs: false,
-  })
-
-  return new ApolloServer({
-    schema,
-    context,
-  })
+/**
+ * An ApolloServer wrapper
+ *
+ * Example
+ *
+ * import { server } from '@hammerframework/api'
+ *
+ * export const handler = server({ schema, context }).createHandler()
+ *
+ */
+export const server = (options: Config): ApolloServer => {
+  return new ApolloServer(options)
 }
