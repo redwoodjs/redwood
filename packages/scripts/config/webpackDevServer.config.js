7725e9cdd2c4a32ef88bea3fea02634c6b1769d9
Peter Pistorius <peter.pistorius@gmail.com>
2019-11-25 08:33:43 +0100

    Fix webpack scripts.

--- packages/scripts/config/webpackDevServer.config.js
+++ packages/scripts/config/webpackDevServer.config.js
@@ -1,15 +1,15 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const path = require('path')
-
 const merge = require('webpack-merge')
-const { getHammerConfig } = require('@hammerframework/hammer-core')
 const escapeRegExp = require('lodash.escaperegexp')
+const { getHammerConfig } = require('@hammerframework/hammer-core')
 
 const webpackConfig = require('./webpack.config.js')
+
 const hammerConfig = getHammerConfig()
 
 module.exports = merge(webpackConfig('development'), {
   devServer: {
+    // https://webpack.js.org/configuration/dev-server/
     hot: true,
     writeToDisk: false,
     compress: true,
