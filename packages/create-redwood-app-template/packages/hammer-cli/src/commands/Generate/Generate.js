e1b50a2f67fbf2f7d5641eeba9c0194e0385a9ec
Rob Cameron <rob.cameron@fastmail.com>
2019-12-18 12:55:55 -0800

    Updates generator syntax to allow setting name and description
    Loop through defined generators to show help text

--- packages/create-redwood-app-template/packages/hammer-cli/src/commands/Generate/Generate.js
+++ packages/create-redwood-app-template/packages/hammer-cli/src/commands/Generate/Generate.js
@@ -7,23 +7,23 @@ import { getHammerBaseDir } from '@hammerframework/hammer-core'
 import { writeFile, bytes } from 'src/lib'
 
 import component from './generators/component'
+import page from './generators/page'
 
 /**
  * A generator is a function that takes a name and returns a list of filenames
  * and contents that should be written to the disk.
  */
-const DEFAULT_GENERATORS = {
-  component,
-}
+const DEFAULT_GENERATORS = [component, page]
 
-const DEFAULT_COMPONENT_DIR = () =>
-  path.join(getHammerBaseDir(), './web/src/components/')
+const DEFAULT_SRC_DIR = () =>
+  path.join(getHammerBaseDir(), './web/src/')
 
 const Generate = ({
   args,
   generators = DEFAULT_GENERATORS,
   fileWriter = writeFile,
 }) => {
+
   if (!getHammerBaseDir()) {
     return (
       <Color red>
@@ -36,12 +36,22 @@ const Generate = ({
     _commandName,
     generatorName,
     name,
-    targetDir = DEFAULT_COMPONENT_DIR(),
+    targetDir = DEFAULT_SRC_DIR(),
   ] = args
 
-  const generator = generators[generatorName]
+  const generator = generators.find(generator => generator.name === generatorName)
 
   if (!generator || !name) {
+    const generatorText = generators.map((generator, i) => {
+      return (
+        <Box key={i} marginLeft={1}>
+          <Box width={12}>
+            <Color yellow>{generator.name}</Color></Box>
+          <Box>{generator.description}</Box>
+        </Box>
+      )
+    })
+
     return (
       <>
         <Box flexDirection="column" marginBottom={1}>
@@ -53,37 +63,46 @@ const Generate = ({
             <Color blue>{generatorName || 'generator'} name [path]</Color>
           </Text>
         </Box>
-        <Box flexDirection="column" marginBottom={1}>
+        <Box marginBottom={1}>
           <Text bold underline>
             Available generators:
           </Text>
-          <Box marginX={2} flexDirection="column">
-            <Text> component</Text>
-            <Text> Generate a React component</Text>
-          </Box>
         </Box>
+        { generatorText }
       </>
     )
   }
 
-  const files = generator(name)
-  const results = Object.keys(files).map((filename) => {
-    const contents = files[filename]
-    try {
-      fileWriter(path.join(targetDir, filename), contents)
-      return (
-        <Text key={`wrote-${filename}`}>
-          Wrote {filename} {bytes(contents)} bytes
-        </Text>
-      )
-    } catch (e) {
-      return (
-        <Text key={`error-${filename}`}>
-          <Color red>{e}</Color>
-        </Text>
-      )
-    }
-  })
+  let results = []
+
+  // Generator found, do we need to create any files?
+
+  if ('files' in generator) {
+    const files = generator.files(name)
+    results = results.concat(Object.keys(files).map((filename) => {
+      const contents = files[filename]
+      try {
+        fileWriter(path.join(targetDir, filename), contents)
+        return (
+          <Text key={`wrote-${filename}`}>
+            Wrote {filename} {bytes(contents)} bytes
+          </Text>
+        )
+      } catch (e) {
+        return (
+          <Text key={`error-${filename}`}>
+            <Color red>{e}</Color>
+          </Text>
+        )
+      }
+    }))
+  }
+
+  // Do we need to append any routes?
+
+  if ('routes' in generator) {
+
+  }
 
   return results
 }
