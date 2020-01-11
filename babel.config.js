29d93da69d87823e65dc462cc0f92b20114db88c
Peter Pistorius <peter.pistorius@gmail.com>
2020-01-11 10:03:40 +0100

    Add packages to support tests.

--- babel.config.js
+++ babel.config.js
@@ -4,5 +4,6 @@ module.exports = {
     ['@babel/plugin-proposal-class-properties', { loose: true }],
     ['@babel/plugin-proposal-export-default-from'],
     ['@babel/plugin-proposal-object-rest-spread'],
+    ['babel-plugin-require-context-hook'],
   ],
 }
---
+++ packages/web/jest.setup.js
@@ -0,0 +1,4 @@
+// `require.context` is only available in webpack
+require('babel-plugin-require-context-hook/register')()
+
+import '@testing-library/jest-dom/extend-expect'
