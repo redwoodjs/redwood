d20304ce1dd14100bae7af2bf4f647e813e512d6
Peter Pistorius <peter.pistorius@gmail.com>
2020-01-11 10:14:06 +0100

    Export the helper.

--- packages/web/src/index.js
+++ packages/web/src/index.js
@@ -2,7 +2,5 @@ export { __REDWOOD__ } from './config'
 export { default as RedwoodProvider, useAuth } from './RedwoodProvider'
 export { default as gql } from 'graphql-tag'
 export * from './graphql'
-export { useQuery, useMutation } from '@apollo/react-hooks'
-
-
 export * from './router/autoImportPages'
+export { useQuery, useMutation } from '@apollo/react-hooks'
