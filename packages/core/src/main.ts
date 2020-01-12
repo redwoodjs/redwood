6d2b29b545130c288e80d94f350f5345e700d4b9
Peter Pistorius <peter.pistorius@gmail.com>
2020-01-12 16:17:29 +0100

    Add paths for web router, pages and components.

--- packages/core/src/main.ts
+++ packages/core/src/main.ts
@@ -5,6 +5,11 @@ import findUp from 'findup-sync'
 import toml from 'toml'
 
 const CONFIG_FILE_NAME = 'redwood.toml'
+const PATH_API_DIR_FUNCTIONS = 'api/src/functions'
+const PATH_API_DIR_GRAPHQL = 'api/src/graphql'
+const PATH_WEB_ROUTER = 'web/src/Router.js'
+const PATH_WEB_DIR_PAGES = 'web/src/pages/'
+const PATH_WEB_DIR_COMPONENTS = 'web/src/components'
 
 export const getConfigPath = (): string => {
   const configPath = findUp(CONFIG_FILE_NAME)
@@ -25,34 +30,56 @@ export interface ConfigInterface {
   web: {
     port: number
     apiProxyPath: string
+    paths: {
+      router: string
+      pages: string
+      components: string
+    }
   }
   api: {
     port: number
     paths: {
       functions: string
       graphql: string
-      generated: string
     }
   }
 }
 
 export const getConfig = (): ConfigInterface => {
   const configPath = getConfigPath()
-  const baseDir = getBaseDir(configPath)
+  const BASE_DIR = getBaseDir(configPath)
   try {
     const config = toml.parse(
       fs.readFileSync(configPath, 'utf8')
     ) as ConfigInterface
 
+    const {
+      functions = PATH_API_DIR_FUNCTIONS,
+      graphql = PATH_API_DIR_GRAPHQL,
+    } = config.api.paths
+
+    const {
+      router = PATH_WEB_ROUTER,
+      pages = PATH_WEB_DIR_PAGES,
+      components = PATH_WEB_DIR_COMPONENTS,
+    } = config.web.paths
+
     return {
       ...config,
-      baseDir,
+      baseDir: BASE_DIR,
       api: {
         ...config.api,
         paths: {
-          functions: path.join(baseDir, config.api.paths.functions),
-          graphql: path.join(baseDir, config.api.paths.graphql),
-          generated: path.join(baseDir, config.api.paths.generated),
+          functions: path.join(BASE_DIR, functions),
+          graphql: path.join(BASE_DIR, graphql),
+        },
+      },
+      web: {
+        ...config.web,
+        paths: {
+          router: path.join(BASE_DIR, router),
+          pages: path.join(BASE_DIR, pages),
+          components: path.join(BASE_DIR, components),
         },
       },
     }
