8e87cc684099d1aea16573a6989915487b79f41a
Tom Preston-Werner <tom@mojombo.com>
2020-01-14 22:37:27 -0800

    Make sure pages subdirs are considered.

--- packages/eslint-plugin-redwood/src/no-unavailable-pages.js
+++ packages/eslint-plugin-redwood/src/no-unavailable-pages.js
@@ -33,11 +33,11 @@ function processDir(dir, prefix = []) {
         // onto the deps array.
         const basename = path.posix.basename(entry.name, '.js')
         const importName = prefix.join() + basename
-        const importFile = path.join('src', 'pages', basename)
+        const importFile = path.join('src', 'pages', ...prefix, basename)
         deps.push({
-          const: entry.name,
+          const: importName,
           path: path.join(dir, entry.name),
-          importStatement: `import ${importName} from '${importFile}'`,
+          importStatement: `const ${importName} = () => import(/* webpackChunkName: "${entry.name}" */ '${importFile}')`,
         })
       } else {
         // If the Page doesn't exist then we are in a directory of Page
