3afa67250614d012f198b7b3e6501bde0d15f527
Peter Pistorius <peter.pistorius@gmail.com>
2020-01-27 08:30:15 +0100

    Optional chaining is Stage 4 and added by preset-env.

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,6 @@ module.exports = {
     ['@babel/plugin-proposal-class-properties', { loose: true }],
     ['@babel/plugin-proposal-export-default-from'],
     ['@babel/plugin-proposal-object-rest-spread'],
-    ['@babel/plugin-proposal-optional-chaining'],
   ],
   ignore: ['**/*.test.js', '**/__tests__', '**/__mocks__'],
 }
