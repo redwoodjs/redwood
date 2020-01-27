d963058c215ad5215b625e0cccb94d58363c4096
Peter Pistorius <peter.pistorius@gmail.com>
2020-01-27 09:27:33 +0100

    Add tests.

--- packages/create-redwood-app-template/packages/core/src/__tests__/paths.test.ts
+++ packages/create-redwood-app-template/packages/core/src/__tests__/paths.test.ts
@@ -1,12 +1,12 @@
 import path from 'path'
 
-import { processPagesDir } from '../src/main'
+import { processPagesDir } from '../paths'
 
 describe('paths', () => {
   describe('processPagesDir', () => {
     it('it accurately finds the pages', () => {
       const deps = processPagesDir(
-        path.resolve(__dirname, '../__mocks__/pages/')
+        path.resolve(__dirname, './fixtures/web/pages/')
       )
       expect(deps[0].const).toEqual('AdminMargleTheWorld')
       expect(deps[1].const).toEqual('HelloWorld')
