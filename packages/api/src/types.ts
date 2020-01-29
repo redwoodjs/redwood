7e53b803ee7ba360d9783c585205ed3a5dc1a5aa
Peter Pistorius <peter.pistorius@gmail.com>
2020-01-29 18:25:06 +0100

    Remve unused types.

--- packages/api/src/types.ts
+++ packages/api/src/types.ts
@@ -1,52 +1,11 @@
 import { IResolvers } from 'graphql-tools'
 import { DocumentNode, GraphQLSchema } from 'graphql'
 
-export type Schema = {
-  schema: DocumentNode,
-  resolvers?: IResolvers
-}
-export type ImportedSchemas = {
-  [name: string]: Schema
-}
-
 export type Services = { [funcName: string]: any }
 export type ImportedServices = {
   [serviceName: string]: Services
 }
-
 export interface MakeServicesInterface {
   services: ImportedServices
 }
 export type MakeServices = (args: MakeServicesInterface) => Services
-
-export type MakeMergedSchema = (
-  schemas: ImportedSchemas,
-  services: ImportedServices,
-  debug: boolean
-) => GraphQLSchema
-
-export type SchemasWithServices = {
-  [name: string]: {
-    schema: GraphQLSchema
-    resolvers?: IResolvers
-  }
-}
-export interface MapServicesToSchemaInterface {
-  schemas: ImportedSchemas
-  services: Services
-}
-export type MapServicesToSchema = (
-  args: MapServicesToSchemaInterface
-) => SchemasWithServices
-
-export interface MapSchemaTypeFieldsToServiceInterface {
-  fields: { [key: string]: any }
-  resolvers?: any
-  service: Services
-}
-export type MapSchemaTypeFieldsToService = (
-  args: MapSchemaTypeFieldsToServiceInterface
-) => {
-  schema: GraphQLSchema
-  resolvers: IResolvers
-}
