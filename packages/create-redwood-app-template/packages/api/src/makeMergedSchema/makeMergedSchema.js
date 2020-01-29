fdc5ff129e47ee794ca9d5e85a2b7276c8bcc09b
Peter Pistorius <peter.pistorius@gmail.com>
2020-01-29 18:08:34 +0100

    Lint files.

--- packages/create-redwood-app-template/packages/api/src/makeMergedSchema/makeMergedSchema.js
+++ packages/create-redwood-app-template/packages/api/src/makeMergedSchema/makeMergedSchema.js
@@ -1,39 +1,37 @@
-
-import { mergeSchemas, makeExecutableSchema, addResolveFunctionsToSchema } from 'apollo-server-lambda'
+import { mergeSchemas, addResolveFunctionsToSchema } from 'apollo-server-lambda'
 import merge from 'lodash.merge'
 import omitBy from 'lodash.omitby'
 
 import * as rootSchema from './rootSchema'
-
 
 class SchemaFieldResolverNotFoundError extends Error { }
 const mapFieldsToService = ({
   fields = {},
   resolvers: unmappedResolvers,
   services,
-}) => Object.keys(fields).reduce((resolvers, name) => {
-  // Does the resolver already exist in the schema definition?
-  if (resolvers?.[name]) {
-    return resolvers
-  }
-
-  // Does a function exist in the service?
-  if (services?.[name]) {
-    return {
-      ...resolvers,
-      // Map the resolver type to the service type.
-      [name]: (root, args, context) =>
-        services[name](args, { root, context }),
+}) =>
+  Object.keys(fields).reduce((resolvers, name) => {
+    // Does the resolver already exist in the schema definition?
+    if (resolvers?.[name]) {
+      return resolvers
     }
-  }
 
-  // The function does not exist in resolvers or service.Let the developer know that
-  // they've got a potential bug.
-  throw new SchemaFieldResolverNotFoundError(
-    `Could not find resolver or service for "${name}".`
-  )
-}, unmappedResolvers)
+    // Does a function exist in the service?
+    if (services?.[name]) {
+      return {
+        ...resolvers,
+        // Map the resolver type to the service type.
+        [name]: (root, args, context) =>
+          services[name](args, { root, context }),
+      }
+    }
 
+    // The function does not exist in resolvers or service.Let the developer know that
+    // they've got a potential bug.
+    throw new SchemaFieldResolverNotFoundError(
+      `Could not find resolver or service for "${name}".`
+    )
+  }, unmappedResolvers)
 
 /**
  * This iterates over all the schemas definitions and figures out which resolvers
@@ -42,32 +40,36 @@ const mapFieldsToService = ({
  * A warning is displayed if they cannot be found.
  */
 const mergeResolversWithServices = ({ schema, resolvers, services }) => {
-  const mergedServices = merge({}, ...Object.keys(services).map(name => services[name]))
+  const mergedServices = merge(
+    {},
+    ...Object.keys(services).map((name) => services[name])
+  )
   return {
     ...resolvers,
     Query: mapFieldsToService({
       fields: schema.getType('Query')?.getFields(),
       resolvers: resolvers?.Query,
-      services: mergedServices
+      services: mergedServices,
     }),
     Mutation: mapFieldsToService({
       fields: schema.getType('Mutation')?.getFields(),
       resolvers: resolvers?.Mutation,
-      services: mergedServices
-    })
+      services: mergedServices,
+    }),
   }
 }
 
-const mergeResolvers = (schemas) => omitBy(
-  merge(
-    {},
-    ...[
-      rootSchema.resolvers,
-      ...Object.values(schemas).map(({ resolvers }) => resolvers),
-    ]
-  ),
-  (v) => typeof v === 'undefined'
-)
+const mergeResolvers = (schemas) =>
+  omitBy(
+    merge(
+      {},
+      ...[
+        rootSchema.resolvers,
+        ...Object.values(schemas).map(({ resolvers }) => resolvers),
+      ]
+    ),
+    (v) => typeof v === 'undefined'
+  )
 
 /**
  * Merge GraphQL schemas and resolvers into a single schema.
@@ -84,15 +86,18 @@ const mergeResolvers = (schemas) => omitBy(
  * ```
  */
 export const makeMergedSchema = ({ schemas, services }) => {
-  // Combine all the schemas together.
   const schema = mergeSchemas({
     schemas: [
       rootSchema.schema,
       ...Object.values(schemas).map(({ schema }) => schema),
-    ]
+    ],
   })
 
-  const resolvers = mergeResolversWithServices({ schema, resolvers: mergeResolvers(schemas), services })
+  const resolvers = mergeResolversWithServices({
+    schema,
+    resolvers: mergeResolvers(schemas),
+    services,
+  })
   addResolveFunctionsToSchema({ schema, resolvers })
 
   return schema
