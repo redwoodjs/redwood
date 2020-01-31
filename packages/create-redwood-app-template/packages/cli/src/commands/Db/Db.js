69a368348a9704e5304bb477d32d89edb1d25c6a
Peter Pistorius <peter.pistorius@gmail.com>
2020-01-31 10:22:17 +0100

    Add database migration commands.

---
+++ packages/create-redwood-app-template/packages/cli/src/commands/Db/Db.js
@@ -0,0 +1,74 @@
+// The `redwood db` commands run in the api folder and allows the user to apply
+// the migrations to the database, to generate a client, and to save migrations.
+// $ redwood db up
+// $ redwood db save
+
+import React from 'react'
+import { Box, Text, Color } from 'ink'
+import concurrently from 'concurrently'
+import { getPaths } from '@redwoodjs/core'
+
+const up = () => {
+  const { base } = getPaths()
+
+  concurrently(
+    [
+      {
+        command: `cd ${base}/api && yarn prisma2 migrate up --experimental`,
+        name: 'migrate',
+        prefixColor: 'yellow',
+      },
+    ],
+    {
+      prefix: 'name',
+    }
+  )
+  return null
+}
+
+const save = () => {
+  const { base } = getPaths()
+
+  concurrently(
+    [
+      {
+        command: `cd ${base}/api && yarn prisma2 migrate save --experimental`,
+        name: 'migrate',
+        prefixColor: 'yellow',
+      },
+    ],
+    {
+      prefix: 'name',
+    }
+  )
+  return null
+}
+
+export default ({ args }) => {
+  const commands = {
+    up,
+    save,
+  }
+
+  const subcommandToRun = args?.[0]?.[1]
+  if (!commands[subcommandToRun]) {
+    // TODO: Improve this with actual usage.
+    return (
+      <Box>
+        <Text>
+          yarn rw database "{subcommandToRun}" is a not a valid argument.
+        </Text>
+        <Text>Usage: </Text>
+        <Text>$ yarn rw db {Object.keys(commands).join(', ')}</Text>
+      </Box>
+    )
+  }
+
+  return commands[subcommandToRun]({ args })
+}
+
+export const commandProps = {
+  name: 'database',
+  alias: 'db',
+  description: 'Migrations, generators, all the datase things.',
+}
