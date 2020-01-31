4a64175d49a27e0257269f30f725b9074000d246
Peter Pistorius <peter.pistorius@gmail.com>
2020-01-31 10:53:00 +0100

    Allow user to specify what to run in dev command.

--- packages/create-redwood-app-template/packages/cli/src/commands/Dev/Dev.js
+++ packages/create-redwood-app-template/packages/cli/src/commands/Dev/Dev.js
@@ -5,31 +5,34 @@
 import concurrently from 'concurrently'
 import { getPaths } from '@redwoodjs/core'
 
-const Dev = () => {
-  const redwoodPaths = getPaths()
+const Dev = ({ args }) => {
+  const { base } = getPaths()
+  const availableWatchers = {
+    api: `cd ${base}/api && yarn dev-server`,
+    web: `cd ${base}/web && yarn webpack-dev-server --config ./config/webpack.dev.js`,
+    db: `cd ${base}/api && yarn prisma2 generate --watch`,
+  }
+
+  // The user can do something like `$ yarn rw dev api,web` or just `yarn rw dev`
+  const subcommandToRun = args?.[0]?.[1]
+
+  let runCommands = Object.keys(availableWatchers)
+  if (subcommandToRun) {
+    // TODO: Split by ' ' char?
+    const watchers = subcommandToRun.split(',')
+    runCommands = watchers.filter((n) => runCommands.includes(n))
+  }
 
   concurrently(
-    [
-      {
-        command: `cd ${redwoodPaths.base}/web && yarn webpack-dev-server --config ./config/webpack.dev.js`,
-        name: 'web',
-        prefixColor: 'yellow',
-      },
-      {
-        command: `cd ${redwoodPaths.base}/api && yarn prisma2 generate --watch`,
-        name: 'database',
-        prefixColor: 'yellow',
-      },
-      {
-        command: `cd ${redwoodPaths.base}/api && yarn dev-server`,
-        name: 'api',
-        prefixColor: 'green',
-      },
-    ],
+    runCommands.map((name) => ({
+      command: availableWatchers[name],
+      name,
+    })),
     {
       prefix: 'name',
     }
   )
+
   return null
 }
 
