7cb4d2770b6854c0b34e83be81b0bb0ea13489b6
Peter Pistorius <peter.pistorius@gmail.com>
2020-02-07 19:40:35 +0100

    Use execa for running commands.

--- packages/create-redwood-app-template/packages/cli/src/commands/build.js
+++ packages/create-redwood-app-template/packages/cli/src/commands/build.js
@@ -1,15 +1,14 @@
 import ora from 'ora'
+import execa from 'execa'
 
-import { asyncExec, getPaths } from 'src/lib'
+import { getPaths } from 'src/lib'
 import c from 'src/lib/colors'
 
 export const command = 'build [app..]'
 export const desc = 'Build the [app] for deployment.'
 export const builder = { app: { default: ['api', 'web'] } }
 export const handler = ({ app }) => {
-
   const { base: BASE_DIR } = getPaths()
-
   const execCommandsForApps = {
     api: `cd ${BASE_DIR}/api && NODE_ENV=production yarn babel src --out-dir dist`,
     web: `cd ${BASE_DIR}/web && yarn webpack --config ../node_modules/@redwoodjs/scripts/config/webpack.production.js`,
@@ -18,9 +17,7 @@ export const handler = ({ app }) => {
   app.forEach(async (appName) => {
     const spinner = ora(`Building "${appName}..."`).start()
     try {
-      // Could we stream the output here?
-      await asyncExec(execCommandsForApps[appName])
-
+      await execa(execCommandsForApps[appName], undefined, { shell: true })
       spinner.succeed()
     } catch (e) {
       spinner.fail()
