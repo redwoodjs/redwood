aca25a1139638a583f06447b8a500b18ed0065b1
Peter Pistorius <peter.pistorius@gmail.com>
2020-02-07 19:22:35 +0100

    Convert entry point into yargs.

--- packages/cli/src/index.js
+++ packages/cli/src/index.js
@@ -1,31 +1,6 @@
 #!/usr/bin/env node
 
-import React from 'react'
-import { render, Box } from 'ink'
+import yargs from 'yargs'
 
-import { getCommands, parseArgs } from 'src/lib'
-import { Header, CommandList } from 'src/components'
-
-const Router = ({ commands, args = [[], {}] }) => {
-  const commandToRun = args[0][0]
-  const command = commands.find(({ commandProps: { name, alias } }) =>
-    [name, alias].includes(commandToRun)
-  )
-
-  if (!command) {
-    return (
-      <Box flexDirection="column">
-        <Header marginBottom={1} />
-        <CommandList commands={commands} />
-      </Box>
-    )
-  }
-
-  return command.default({ args })
-}
-
-if (process.env.NODE_ENV !== 'test') {
-  render(<Router commands={getCommands()} args={parseArgs()} />)
-}
-
-export default Router
+// TODO: Fix the way that the commands are found.
+yargs.commandDir('./commands').demandCommand().help().argv
