08bfa57ddd0f6284b8fd5156a5cd0d568894ebd3
Peter Pistorius <peter.pistorius@gmail.com>
2020-02-13 10:44:33 +0100

    Strict doesn't work as expected.

--- packages/create-redwood-app-template/packages/cli/src/index.js
+++ packages/create-redwood-app-template/packages/cli/src/index.js
@@ -3,7 +3,4 @@
 import yargs from 'yargs'
 
 // eslint-disable-next-line no-unused-expressions
-yargs
-  .commandDir('./commands')
-  .demandCommand()
-  .strict().argv
+yargs.commandDir('./commands').demandCommand().argv
