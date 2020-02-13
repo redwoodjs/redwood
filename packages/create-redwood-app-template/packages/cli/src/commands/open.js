c408793715ec17605f270fa2c262314126b543b5
Peter Pistorius <peter.pistorius@gmail.com>
2020-02-13 09:54:30 +0100

    Fix open command.

--- packages/create-redwood-app-template/packages/cli/src/commands/open.js
+++ packages/create-redwood-app-template/packages/cli/src/commands/open.js
@@ -1,9 +1,8 @@
 import execa from 'execa'
 import { getConfig } from '@redwoodjs/core'
 
-export const command = 'open'
+export const command = 'open [port]'
 export const desc = 'Open your project in your browser.'
-
 export const handler = () => {
-  execa(`open http://localhost:${getConfig().web.port}`)
+  execa(`open http://localhost:${getConfig().web.port}`, { shell: true })
 }
