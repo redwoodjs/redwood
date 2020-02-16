d523b87ec01dc84e0eab8e4b054f63a340fdf3be
Peter Pistorius <peter.pistorius@gmail.com>
2020-02-16 11:32:54 +0100

    Output html file instead of dev-server for stats.

--- packages/core/config/webpack.stats.js
+++ packages/core/config/webpack.stats.js
@@ -3,10 +3,12 @@ const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 const webpackProduction = require('./webpack.production')
 
+// https://github.com/webpack-contrib/webpack-bundle-analyzer
 module.exports = merge(webpackProduction, {
   plugins: [
     new BundleAnalyzerPlugin({
       generateStatsFile: true,
+      analyzerMode: 'static',
     }),
   ],
 })
