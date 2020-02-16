4ff8a40875f24da51cd2cb1a36d1814c17881dc4
Peter Pistorius <peter.pistorius@gmail.com>
2020-02-16 11:32:34 +0100

    Add stats and verbose flags to build command.

--- packages/cli/src/commands/build.js
+++ packages/cli/src/commands/build.js
@@ -1,31 +1,62 @@
 import execa from 'execa'
 import Listr from 'listr'
+import VerboseRenderer from 'listr-verbose-renderer'
 
 import { getPaths } from 'src/lib'
+import c from 'src/lib/colors'
 
 export const command = 'build [app..]'
 export const desc = 'Build for production.'
 export const builder = {
   app: { choices: ['api', 'web'], default: ['api', 'web'] },
+  verbose: { type: 'boolean', default: false, alias: ['v'] },
+  stats: { type: 'boolean', default: false },
 }
 
-// TODO: Add verbose flag.
-export const handler = ({ app }) => {
+export const handler = async ({ app, verbose, stats }) => {
   const { base: BASE_DIR } = getPaths()
   const execCommandsForApps = {
-    api: `cd ${BASE_DIR}/api && NODE_ENV=production yarn babel src --out-dir dist`,
-    web: `cd ${BASE_DIR}/web && yarn webpack --config ../node_modules/@redwoodjs/core/config/webpack.production.js`,
+    api: {
+      cwd: `${BASE_DIR}/api`,
+      cmd: 'NODE_ENV=production babel src --out-dir dist',
+    },
+    web: {
+      cwd: `${BASE_DIR}/web`,
+      cmd: `webpack --config ../node_modules/@redwoodjs/core/config/webpack.${
+        stats ? 'stats' : 'production'
+      }.js`,
+    },
+  }
+
+  if (stats) {
+    app = ['web']
+    console.log(
+      ' ⏩ Skipping `api` build because stats only works for Webpack at the moment.'
+    )
   }
 
   const tasks = new Listr(
-    app.map((appName) => ({
-      title: `Building "${appName}..."`,
-      task: () => {
-        return execa(execCommandsForApps[appName], undefined, {
-          shell: true,
-        })
-      },
-    }))
+    app.map((appName) => {
+      const { cwd, cmd } = execCommandsForApps[appName]
+      return {
+        title: `Building "${appName}"${(stats && ' for stats') || ''}...`,
+        task: () => {
+          return execa(cmd, undefined, {
+            stdio: verbose ? 'inherit' : 'pipe',
+            shell: true,
+            cwd,
+          })
+        },
+      }
+    }),
+    {
+      renderer: verbose && VerboseRenderer,
+    }
   )
-  tasks.run()
+
+  try {
+    await tasks.run()
+  } catch (e) {
+    console.log(c.error(e.message))
+  }
 }
