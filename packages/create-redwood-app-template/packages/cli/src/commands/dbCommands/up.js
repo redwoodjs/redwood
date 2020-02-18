9ab4ff0e20554413681dbb028e31ada14c4249fc
Peter Pistorius <peter.pistorius@gmail.com>
2020-02-18 22:08:40 +0100

    Rename "rw db up" command.

---
+++ packages/create-redwood-app-template/packages/cli/src/commands/dbCommands/up.js
@@ -0,0 +1,50 @@
+import execa from 'execa'
+import Listr from 'listr'
+import VerboseRenderer from 'listr-verbose-renderer'
+
+import { getPaths } from 'src/lib'
+import c from 'src/lib/colors'
+
+export const command = 'up'
+export const desc = 'Generate the Prisma client and apply migrations.'
+export const builder = {
+  verbose: { type: 'boolean', default: false, alias: ['v'] },
+}
+
+export const handler = async ({ verbose }) => {
+  const execaOpts = {
+    shell: true,
+    cwd: `${getPaths().base}/api`,
+    stdio: verbose ? 'inherit' : 'pipe',
+  }
+
+  const tasks = new Listr(
+    [
+      {
+        title: 'Applying migrations...',
+        task: async () => {
+          return execa(
+            'yarn prisma2',
+            ['migrate up', '--create-db', '--experimental'],
+            execaOpts
+          )
+        },
+      },
+      {
+        title: 'Generating Prisma2 client...',
+        task: async () => {
+          return execa('yarn prisma2', ['generate'], execaOpts)
+        },
+      },
+    ],
+    {
+      renderer: verbose && VerboseRenderer,
+    }
+  )
+
+  try {
+    await tasks.run()
+  } catch (e) {
+    console.log(c.error(e.message))
+  }
+}
