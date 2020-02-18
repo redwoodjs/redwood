71037295b98102b3cebd01a9c6e6c0c04c6e24a1
Peter Pistorius <peter.pistorius@gmail.com>
2020-02-18 22:30:59 +0100

    Standardize "rw db generate" command.

--- packages/cli/src/commands/dbCommands/generate.js
+++ packages/cli/src/commands/dbCommands/generate.js
@@ -1,23 +1,40 @@
 import execa from 'execa'
 import Listr from 'listr'
+import VerboseRenderer from 'listr-verbose-renderer'
 
 import { getPaths } from 'src/lib'
 import c from 'src/lib/colors'
 
 export const command = 'generate'
-export const desc = 'Generate the prisma2 client.'
+export const desc = 'Generate the Prisma client.'
+export const builder = {
+  verbose: { type: 'boolean', default: false, alias: ['v'] },
+}
 
-export const handler = async () => {
-  const tasks = new Listr([
-    {
-      title: 'Generating database client...',
-      task: async () => {
-        return execa('yarn prisma2', ['generate'], {
-          shell: true,
-          cwd: `${getPaths().base}/api`,
-        })
+export const handler = async ({ verbose }) => {
+  const execaOpts = {
+    shell: true,
+    cwd: `${getPaths().base}/api`,
+    stdio: verbose ? 'inherit' : 'pipe',
+  }
+
+  const tasks = new Listr(
+    [
+      {
+        title: 'Generating Prisma2 client...',
+        task: async () => {
+          return execa('yarn prisma2', ['generate'], execaOpts)
+        },
       },
-    },
-  ])
-  tasks.run()
+    ],
+    {
+      renderer: verbose && VerboseRenderer,
+    }
+  )
+
+  try {
+    await tasks.run()
+  } catch (e) {
+    console.log(c.error(e.message))
+  }
 }
