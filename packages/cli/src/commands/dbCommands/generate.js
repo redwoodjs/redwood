81852c31c94a79a0fc73641d3947b9553eaa8ba9
Peter Pistorius <peter.pistorius@gmail.com>
2020-02-20 07:03:09 +0100

    Create runCommandTask.

--- packages/cli/src/commands/dbCommands/generate.js
+++ packages/cli/src/commands/dbCommands/generate.js
@@ -1,40 +1,21 @@
-import execa from 'execa'
-import Listr from 'listr'
-import VerboseRenderer from 'listr-verbose-renderer'
-
-import { getPaths } from 'src/lib'
-import c from 'src/lib/colors'
+import { runCommandTask } from 'src/lib'
 
 export const command = 'generate'
 export const desc = 'Generate the Prisma client.'
 export const builder = {
   verbose: { type: 'boolean', default: false, alias: ['v'] },
 }
-
 export const handler = async ({ verbose }) => {
-  const execaOpts = {
-    shell: true,
-    cwd: `${getPaths().base}/api`,
-    stdio: verbose ? 'inherit' : 'pipe',
-  }
-
-  const tasks = new Listr(
+  await runCommandTask(
     [
       {
-        title: 'Generating Prisma2 client...',
-        task: async () => {
-          return execa('yarn prisma2', ['generate'], execaOpts)
-        },
+        title: 'Generating the Prisma client...',
+        cmd: 'yarn prisma2',
+        args: ['generate'],
       },
     ],
     {
-      renderer: verbose && VerboseRenderer,
+      verbose,
     }
   )
-
-  try {
-    await tasks.run()
-  } catch (e) {
-    console.log(c.error(e.message))
-  }
 }
--- packages/cli/src/commands/dbCommands/save.js
+++ packages/cli/src/commands/dbCommands/save.js
@@ -1,41 +1,21 @@
-import execa from 'execa'
-import Listr from 'listr'
-import VerboseRenderer from 'listr-verbose-renderer'
-
-import { getPaths } from 'src/lib'
-import c from 'src/lib/colors'
+import { runCommandTask } from 'src/lib'
 
 export const command = 'save [name..]'
 export const desc = 'Create a new migration.'
-
-export const handler = async ({ name }) => {
-  const execaOpts = {
-    shell: true,
-    cwd: `${getPaths().base}/api`,
-    stdio: 'inherit',
-  }
-
-  const tasks = new Listr(
+export const builder = {
+  verbose: { type: 'boolean', default: false, alias: ['v'] },
+}
+export const handler = async ({ name, verbose }) => {
+  await runCommandTask(
     [
       {
-        title: 'Migrating your database up...',
-        task: async () => {
-          return execa(
-            'yarn prisma2',
-            ['migrate save', name && `--name ${name}`, '--experimental'],
-            execaOpts
-          )
-        },
+        title: 'Migrating database up...',
+        cmd: 'yarn prisma2',
+        args: ['migrate save', name && `--name ${name}`, '--experimental'],
       },
     ],
     {
-      renderer: VerboseRenderer,
+      verbose,
     }
   )
-
-  try {
-    await tasks.run()
-  } catch (e) {
-    console.log(c.error(e.message))
-  }
 }
