13ab68a51962ac61406e47579e994446bb50a7f5
David S Price <thedavid@thedavidprice.com>
2020-02-21 16:21:59 -0800

    add min max required commands for generate and db

--- packages/cli/src/commands/db.js
+++ packages/cli/src/commands/db.js
@@ -3,4 +3,6 @@ export const aliases = ['database']
 export const desc = 'Database tools.'
 
 export const builder = (yargs) =>
-  yargs.commandDir('./dbCommands').demandCommand().argv
+  yargs
+    .commandDir('./dbCommands')
+    .demandCommand(1, 1, 'You need at least one command when using `db`').argv
--- packages/cli/src/commands/generate.js
+++ packages/cli/src/commands/generate.js
@@ -3,4 +3,11 @@ export const aliases = ['g']
 export const desc = 'Save time by generating boilerplate code.'
 
 export const builder = (yargs) =>
-  yargs.commandDir('./generate/commands').demandCommand().argv
+  yargs
+    .commandDir('./generate/commands')
+    .demandCommand(
+      2,
+      2,
+      'You need at least two commands to use `generate`',
+      'You entered too many commands. Maybe you used a space in your directory name?'
+    ).argv
