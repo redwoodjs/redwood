7531d2fc29f468b691803ddc9445ee54388d9f66
Peter Pistorius <peter.pistorius@gmail.com>
2020-02-22 22:01:40 +0100

    Add tests for handling the context.

--- packages/api/src/graphQLServer.ts
+++ packages/api/src/graphQLServer.ts
@@ -2,38 +2,42 @@ import { ApolloServer, Config } from 'apollo-server-lambda'
 
 import { setContext } from './globalContext'
 
+export const handleContext = (options: Config) => {
+  // Returns a function that deals with the context on request.
+  return ({ context = {}, events } = {}) => {
+    // Prevent the Lambda function from waiting for all resources,
+    // such as database connections, to be released before returning
+    // a reponse.
+    context.callbackWaitsForEmptyEventLoop = false
+
+    // The user can a context object or function when they
+    // initialize the handler.
+    let userContext = options?.context || {}
+    if (typeof userContext === 'function') {
+      userContext = userContext({ context, events })
+    }
+
+    // The context object returned from this function is passed to
+    // the resolvers.
+    // Redwood also introduces a **global** context object.
+    return setContext({
+      ...context,
+      ...userContext,
+    })
+  }
+}
+
 /**
- * An Apollo serverless GraphqL server wrapper
+ * Creates an Apollo GraphQL Server.
  *
  * ```js
- * export const handler = server({ schema, context }).createHandler()
+ * export const handler = createGraphQLHandler({ schema, context })
  * ```
  */
 export const createGraphQLHandler = (options: Config = {}) => {
   const handler = new ApolloServer({
     ...options,
-    context: ({ context, events }) => {
-      // Prevent the Lambda function from waiting for all resources,
-      // such as database connections, to be released before returning
-      // a reponse.
-      context.callbackWaitsForEmptyEventLoop = false
-
-      // The user can a context object or function when they
-      // initialize the handler.
-      let userContext = options.context
-      if (typeof userContext === 'function') {
-        userContext = userContext({ context, events })
-      }
-
-      // The context object returned in this function is passed to
-      // the resolvers. Redwood also introduces a context object.
-      // We use some babel-auto-import magic to make it appear that
-      // it's available as a global.
-      return setContext({
-        ...context,
-        ...userContext,
-      })
-    },
+    context: handleContext(options),
   }).createHandler()
 
   return handler
