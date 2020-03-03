324341187f406df69730b999092fc886db73a178
Rob Cameron <rob.cameron@fastmail.com>
2020-03-03 12:23:19 -0800

    Fallback to default schema if "redwood" provider isn't found

--- packages/cli/src/lib/generateTempSchema.js
+++ packages/cli/src/lib/generateTempSchema.js
@@ -30,29 +30,43 @@ const getProvider = (host) => {
   return protocol ? PROTOCOL_TO_PROVIDER_MAP[protocol] : null
 }
 
+const hasRedwoodProvider = (schema) => {
+  return !!schema.match(/provider *= *['"]redwood['"]/)
+}
+
 // reads api/prisma/schema.prisma and dynamically sets the provider based on what you have set in
 // your ENV, then writes out the schema to a tmp file, returning the path to said file
 export const generateTempSchema = () => {
-  const tempSchemaPath = tmp.tmpNameSync({
-    prefix: 'schema',
-    postfix: '.prisma',
-  })
   let schema = fs.readFileSync(getPaths().api.dbSchema).toString()
+
+  // no need to continue if the schema doesn't have the "redwood" provider,
+  // just return the path to the default schema in the app
+  if (!hasRedwoodProvider(schema)) {
+    return getPaths().api.dbSchema
+  }
+
   const host = getConnectionString(schema)
 
   if (host) {
     const provider = getProvider(host)
 
     if (provider) {
-      schema = schema.replace('redwood', provider)
+      const tempSchemaPath = tmp.tmpNameSync({
+        prefix: 'schema',
+        postfix: '.prisma',
+      })
+
+      fs.writeFileSync(
+        tempSchemaPath,
+        schema.replace(/(['"])redwood['"]/, `$1${provider}$1`)
+      )
       console.info(`Using ${provider} provider as set in DB_HOST`)
+
+      return tempSchemaPath
     } else {
       throw new Error(
-        `Unable to determine provider from host "${host}".\nMake sure your db host protocol (the part before ://) is one of: [file, postgres, mysql]`
+        `Unable to determine provider from host "${host}".\nMake sure your db host protocol (the part before :) is one of: [file, postgres, mysql]`
       )
     }
   }
-  fs.writeFileSync(tempSchemaPath, schema)
-
-  return tempSchemaPath
 }
