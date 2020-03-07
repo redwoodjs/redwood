fe7d8c3f45b8f542ca04578d8e3f63e0abfe7a6b
Peter Pistorius <peter.pistorius@gmail.com>
2020-03-07 22:29:28 +0100

    Allow 'rw db up' to conditionally skip prisma client gen.

--- packages/cli/src/commands/dbCommands/up.js
+++ packages/cli/src/commands/dbCommands/up.js
@@ -1,23 +1,24 @@
 import { runCommandTask } from 'src/lib'
+import { handler as generatePrismaClient } from 'src/commands/dbCommands/generate'
 
 export const command = 'up'
 export const desc = 'Generate the Prisma client and apply migrations.'
 export const builder = {
   verbose: { type: 'boolean', default: true, alias: ['v'] },
+  dbClient: { type: 'boolean', default: true },
 }
 
-export const handler = async ({ verbose }) => {
+export const handler = async ({ verbose, dbClient }) => {
+  if (dbClient) {
+    await generatePrismaClient({ force: true, verbose: true })
+  }
+
   await runCommandTask(
     [
       {
         title: 'Migrate database up...',
         cmd: 'prisma2',
         args: ['migrate up', '--experimental', '--create-db'],
-      },
-      {
-        title: 'Generating the Prisma client...',
-        cmd: 'prisma2',
-        args: ['generate'],
       },
     ],
     { verbose }
