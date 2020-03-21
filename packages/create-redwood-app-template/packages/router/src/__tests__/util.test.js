760a4c4cb7681a36c047a358cae34e58099e58fc
Peter Pistorius <peter.pistorius@gmail.com>
2020-03-21 20:16:57 +0100

    param types is optional now.

--- packages/create-redwood-app-template/packages/router/src/__tests__/util.test.js
+++ packages/create-redwood-app-template/packages/router/src/__tests__/util.test.js
@@ -1,32 +1,21 @@
 import { matchPath } from '../util'
 
-const coreParamTypes = {
-  Int: {
-    constraint: /\d+/,
-    transform: Number,
-  },
-}
-
 describe('matchPath', () => {
   it('matches paths correctly', () => {
-    expect(matchPath('/post/{id:Int}', '/post/7', coreParamTypes)).toEqual({
+    expect(matchPath('/post/{id:Int}', '/post/7')).toEqual({
       match: true,
       params: { id: 7 },
     })
 
     expect(
-      matchPath(
-        '/blog/{year}/{month}/{day}',
-        '/blog/2019/12/07',
-        coreParamTypes
-      )
+      matchPath('/blog/{year}/{month}/{day}', '/blog/2019/12/07')
     ).toEqual({ match: true, params: { day: '07', month: '12', year: '2019' } })
 
-    expect(matchPath('/about', '/', coreParamTypes)).toEqual({ match: false })
+    expect(matchPath('/about', '/')).toEqual({ match: false })
   })
 
   it('transforms a param based on the specified transform', () => {
-    expect(matchPath('/post/{id:Int}', '/post/1337', coreParamTypes)).toEqual({
+    expect(matchPath('/post/{id:Int}', '/post/1337')).toEqual({
       match: true,
       params: { id: 1337 },
     })
