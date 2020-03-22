bc45b5b7975e8b2138c2da9578b240ede305efda
Kasper Mikiewicz <IderedPL@gmail.com>
2020-03-22 01:45:08 +0100

    chore: move mergeUserWebpackConfig to webpack.common

--- packages/core/config/webpack.production.js
+++ packages/core/config/webpack.production.js
@@ -1,6 +1,5 @@
 const webpackConfig = require('./webpack.common')
-const { mergeUserWebpackConfig } = require('./utils')
-
+const { mergeUserWebpackConfig } = webpackConfig
 const baseConfig = webpackConfig('production')
 
 module.exports = mergeUserWebpackConfig('production', baseConfig)
