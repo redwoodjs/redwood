2370f6c235c10e8e6d65fb6d1cd91749d27391d7
David S Price <thedavid@thedavidprice.com>
2020-03-27 16:12:41 -0700

    update prettier settings no longer needed

--- packages/create-redwood-app-template/prettier.config.js
+++ packages/create-redwood-app-template/prettier.config.js
@@ -1,9 +1,8 @@
 // https://prettier.io/docs/en/options.html
 module.exports = {
-  trailingComma: 'es5',
   bracketSpacing: true,
   tabWidth: 2,
   semi: false,
   singleQuote: true,
-  arrowParens: 'always',
+  endOfLine: 'auto'
 }
