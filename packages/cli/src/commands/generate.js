016b4837fa1369bc36f662d2ec8123970589cb6a
Peter Pistorius <peter.pistorius@gmail.com>
2020-04-05 11:10:53 +0200

    Use strict in generators.

--- packages/cli/src/commands/generate.js
+++ packages/cli/src/commands/generate.js
@@ -3,11 +3,4 @@ export const aliases = ['g']
 export const desc = 'Save time by generating boilerplate code.'
 
 export const builder = (yargs) =>
-  yargs
-    .commandDir('./generate/commands')
-    .demandCommand(
-      2,
-      2,
-      'You need at least two commands to use `generate`',
-      'You entered too many commands. Maybe you used a space in your directory name?'
-    ).argv
+  yargs.commandDir('./generate/commands').demandCommand().strict().argv
