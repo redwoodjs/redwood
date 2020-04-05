11b4f7a0b628f953a17439b32c2f6da1a52003f7
Peter Pistorius <peter.pistorius@gmail.com>
2020-04-05 11:09:31 +0200

    Again with the strict.

--- packages/cli/src/commands/db.js
+++ packages/cli/src/commands/db.js
@@ -3,6 +3,4 @@ export const aliases = ['database']
 export const desc = 'Database tools.'
 
 export const builder = (yargs) =>
-  yargs
-    .commandDir('./dbCommands')
-    .demandCommand(1, 1, 'You need at least one command when using `db`').argv
+  yargs.commandDir('./dbCommands').demandCommand().strict().argv
