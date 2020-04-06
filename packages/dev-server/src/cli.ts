c26f13c4f46c0d0d8b12e3dc230411762b56d60c
Peter Pistorius <peter.pistorius@gmail.com>
2020-04-06 21:22:46 +0200

    Make CLI tie http and watch functions together.

--- packages/dev-server/src/cli.ts
+++ packages/dev-server/src/cli.ts
@@ -5,24 +5,39 @@ import {
   NodeTargetPaths,
 } from '@redwoodjs/internal'
 
+import { server } from './http'
+import { watchFunctions } from './watchFunctions'
+
 export const getArgsForSide = (
   side: string
-): { port: number; host: string; functionsPath: string; watchPath: string } => {
+): { port: number; host: string; paths: NodeTargetPaths } => {
   const { port, host } = getSideConfig(side)
   const paths = getSidePaths(side) as NodeTargetPaths
 
   return {
     port,
     host,
-    functionsPath: paths.functions,
-    watchPath: paths.base,
+    paths,
   }
 }
 
 const { side } = yargs.option('side', { default: 'api' }).argv
 
 try {
-  const { host, port, functionsPath, watchPath } = getArgsForSide(side)
+  const { host, port, paths } = getArgsForSide(side)
+
+  console.log()
+  server().listen(8910, () => {
+    console.log(`Running at 'http://${host}:${port}'`)
+    console.log(`Watching files in '${paths.functions}'`)
+
+    watchFunctions({
+      paths,
+      onImport: (functions) => {
+        console.log(Object.keys(functions))
+      },
+    })
+  })
 } catch (e) {
   console.error(e.message)
   process.exit(1)
