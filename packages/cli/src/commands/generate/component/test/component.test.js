cda228cd8ee92aed522bed98f5227254f721a9fd
Rob Cameron <rob.cameron@fastmail.com>
2020-04-08 10:23:46 -0700

    Adds multi-word component tests

--- packages/cli/src/commands/generate/component/test/component.test.js
+++ packages/cli/src/commands/generate/component/test/component.test.js
@@ -3,20 +3,37 @@ import { loadGeneratorFixture } from 'src/lib/test'
 
 import * as component from '../component'
 
-let files
+let singleWordFiles, multiWordFiles
 
 beforeAll(() => {
-  files = component.files({ name: 'User' })
+  singleWordFiles = component.files({ name: 'User' })
+  multiWordFiles = component.files({ name: 'UserProfile' })
 })
 
-test('creates a component', () => {
-  expect(files['/path/to/project/web/src/components/User/User.js']).toEqual(
-    loadGeneratorFixture('component', 'component.js')
-  )
-})
-
-test('creates a component test', () => {
+test('creates a single word component', () => {
   expect(
-    files['/path/to/project/web/src/components/User/User.test.js']
-  ).toEqual(loadGeneratorFixture('component', 'component.test.js'))
+    singleWordFiles['/path/to/project/web/src/components/User/User.js']
+  ).toEqual(loadGeneratorFixture('component', 'singleWordComponent.js'))
+})
+
+test('creates a single word component test', () => {
+  expect(
+    singleWordFiles['/path/to/project/web/src/components/User/User.test.js']
+  ).toEqual(loadGeneratorFixture('component', 'singleWordComponent.test.js'))
+})
+
+test('creates a multi word component', () => {
+  expect(
+    multiWordFiles[
+      '/path/to/project/web/src/components/UserProfile/UserProfile.js'
+    ]
+  ).toEqual()
+})
+
+test('creates a multi word component test', () => {
+  expect(
+    multiWordFiles[
+      '/path/to/project/web/src/components/UserProfile/UserProfile.test.js'
+    ]
+  ).toEqual()
 })
---
+++ packages/cli/src/commands/generate/component/test/fixtures/multiWordComponent.js.fixture
@@ -0,0 +1,10 @@
+const UserProfile = () => {
+  return (
+    <div>
+      <h2>{'UserProfile'}</h2>
+      <p>{'Find me in ./web/src/components/UserProfile/UserProfile.js'}</p>
+    </div>
+  )
+}
+
+export default UserProfile
