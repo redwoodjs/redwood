a5d92d1c38fb8645d522c653d38d228445ad4307
Rob Cameron <rob.cameron@fastmail.com>
2020-04-08 10:54:55 -0700

    Adds multi word tests for service

--- packages/cli/src/commands/generate/service/test/service.test.js
+++ packages/cli/src/commands/generate/service/test/service.test.js
@@ -3,33 +3,66 @@ import { loadGeneratorFixture } from 'src/lib/test'
 
 import * as service from '../service'
 
-let files, crudFiles
+test('creates a single word service file', async () => {
+  const files = await service.files({ name: 'User', crud: false })
 
-beforeAll(async () => {
-  files = await service.files({ name: 'User', crud: false })
-  crudFiles = await service.files({ name: 'Post', crud: true })
-})
-
-test('creates a service file', () => {
   expect(files['/path/to/project/api/src/services/users/users.js']).toEqual(
-    loadGeneratorFixture('service', 'service.js')
+    loadGeneratorFixture('service', 'singleWordService.js')
   )
 })
 
-test('creates a service test file', () => {
+test('creates a single word service test file', async () => {
+  const files = await service.files({ name: 'User', crud: false })
+
   expect(
     files['/path/to/project/api/src/services/users/users.test.js']
-  ).toEqual(loadGeneratorFixture('service', 'service.test.js'))
+  ).toEqual(loadGeneratorFixture('service', 'singleWordService.test.js'))
 })
 
-test('creates a service file with CRUD actions', () => {
-  expect(crudFiles['/path/to/project/api/src/services/posts/posts.js']).toEqual(
-    loadGeneratorFixture('service', 'serviceCrud.js')
+test('creates a multi word service file', async () => {
+  const files = await service.files({ name: 'UserProfile', crud: false })
+
+  expect(
+    files['/path/to/project/api/src/services/userProfiles/userProfiles.js']
+  ).toEqual(loadGeneratorFixture('service', 'multiWordService.js'))
+})
+
+test('creates a multi word service test file', async () => {
+  const files = await service.files({ name: 'UserProfile', crud: false })
+
+  expect(
+    files['/path/to/project/api/src/services/userProfiles/userProfiles.test.js']
+  ).toEqual(loadGeneratorFixture('service', 'multiWordService.test.js'))
+})
+
+test('creates a single word service file with CRUD actions', async () => {
+  const files = await service.files({ name: 'Post', crud: true })
+
+  expect(files['/path/to/project/api/src/services/posts/posts.js']).toEqual(
+    loadGeneratorFixture('service', 'singleWordServiceCrud.js')
   )
 })
 
-test('creates a service test file with CRUD actions', () => {
+test('creates a service test file with CRUD actions', async () => {
+  const files = await service.files({ name: 'Post', crud: true })
+
   expect(
-    crudFiles['/path/to/project/api/src/services/posts/posts.test.js']
-  ).toEqual(loadGeneratorFixture('service', 'serviceCrud.test.js'))
+    files['/path/to/project/api/src/services/posts/posts.test.js']
+  ).toEqual(loadGeneratorFixture('service', 'singleWordServiceCrud.test.js'))
+})
+
+test('creates a multi word service file with CRUD actions', async () => {
+  const files = await service.files({ name: 'UserProfile', crud: true })
+
+  expect(
+    files['/path/to/project/api/src/services/userProfiles/userProfiles.js']
+  ).toEqual(loadGeneratorFixture('service', 'multiWordServiceCrud.js'))
+})
+
+test('creates a multi word service test file with CRUD actions', async () => {
+  const files = await service.files({ name: 'UserProfile', crud: true })
+
+  expect(
+    files['/path/to/project/api/src/services/userProfiles/userProfiles.test.js']
+  ).toEqual(loadGeneratorFixture('service', 'multiWordServiceCrud.test.js'))
 })
