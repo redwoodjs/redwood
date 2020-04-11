a9619cd6bf72ac9de18e773386431ef4e6e261e2
Olivier Lance <olivier@techtrails.io>
2020-04-11 17:19:33 +0200

    Refactor existing tests using `it.each` to clean things up

--- packages/router/src/__tests__/util.test.js
+++ packages/router/src/__tests__/util.test.js
@@ -1,53 +1,31 @@
 import {matchPath, parseSearch, validatePath} from '../util'
 
 describe('matchPath', () => {
-  it('matches paths correctly', () => {
+  it.each([
+    ['/post/{id:Int}', '/post/notAnInt'],
+    ['/post/{id:Int}', '/post/2.0'],
+    ['/post/{id:Int}', '/post/.1'],
+    ['/post/{id:Int}', '/post/0.1'],
+    ['/post/{id:Int}', '/post/123abcd'],
+    ['/post/{id:Int}', '/post/abcd123'],
+    ['/blog/{year}/{month:Int}/{day}', '/blog/2019/december/07'],
+    ['/blog/{year}/{month}/{day}', '/blog/2019/07'],
+    ['/posts/{id}/edit', '/posts//edit'],
+    ['/about', '/']
+  ])('does not match route "%s" with path "%s"',
+    (route, pathname) => {
+    expect(matchPath(route, pathname)).toEqual({ match: false })
+  })
+
+  it('matches valid paths and extracts params correctly', () => {
     expect(matchPath('/post/{id:Int}', '/post/7')).toEqual({
       match: true,
       params: { id: 7 },
     })
 
-    expect(matchPath('/post/{id:Int}', '/post/notAnInt')).toEqual({
-      match: false,
-    })
-
-    expect(matchPath('/post/{id:Int}', '/post/2.0')).toEqual({
-      match: false,
-    })
-
-    expect(matchPath('/post/{id:Int}', '/post/.1')).toEqual({
-      match: false,
-    })
-
-    expect(matchPath('/post/{id:Int}', '/post/0.1')).toEqual({
-      match: false,
-    })
-
-    expect(matchPath('/post/{id:Int}', '/post/123abcd')).toEqual({
-      match: false,
-    })
-
-    expect(matchPath('/post/{id:Int}', '/post/abcd123')).toEqual({
-      match: false,
-    })
-
     expect(
       matchPath('/blog/{year}/{month}/{day}', '/blog/2019/12/07')
     ).toEqual({ match: true, params: { day: '07', month: '12', year: '2019' } })
-
-    expect(
-      matchPath('/blog/{year}/{month:Int}/{day}', '/blog/2019/december/07')
-    ).toEqual({ match: false })
-
-    expect(matchPath('/blog/{year}/{month}/{day}', '/blog/2019/07')).toEqual({
-      match: false,
-    })
-
-    expect(matchPath('/posts/{id}/edit', '/posts//edit')).toEqual({
-      match: false,
-    })
-
-    expect(matchPath('/about', '/')).toEqual({ match: false })
   })
 
   it('transforms a param based on the specified transform', () => {
