d26cc45a12cd9874cbab7ccb4c47e6f9260cd48e
Peter Pistorius <peter.pistorius@gmail.com>
2020-05-02 23:59:42 +0200

    Make RedwoodProvider auth aware.

---
+++ packages/web/src/components/RedwoodProvider.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react'
+
+import { GraphQLProvider, createGraphQLClient } from 'src/graphql'
+
+const useAuthStub = () => ({ loading: false, authenticated: false })
+
+/**
+ * Redwood's Provider ties together our authentication and Apollo's GraphQL Provider
+ * when you pass the `useAuth` hook from `@redwoodjs/auth`
+ * The authentication token is added to the headers of the GraphQL client.
+ */
+const RedwoodProvider = ({ useAuth = useAuthStub, children }) => {
+  const { loading, authenticated, getToken, type } = useAuth()
+  const [authToken, setAuthToken] = useState()
+
+  useEffect(() => {
+    if (authenticated) {
+      getToken().then((token) => setAuthToken(token))
+    }
+  }, [authenticated, getToken])
+
+  // This really sucks because rendering is completely blocked whilst we're
+  // restoring authentication.
+  if (loading) {
+    return null
+  }
+
+  const client = authToken
+    ? createGraphQLClient({
+        headers: {
+          'X-Redwood-Auth-Type': type,
+          Authorization: `Bearer ${authToken}`,
+        },
+      })
+    : undefined
+
+  return <GraphQLProvider client={client}>{children}</GraphQLProvider>
+}
+
+export default RedwoodProvider
