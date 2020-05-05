de38968729c9c428e9179fa4d237d06647581b80
Rob Cameron <rob.cameron@fastmail.com>
2020-05-05 16:57:31 -0700

    Adds function generator
    Closes #516

---
+++ packages/cli/src/commands/generate/function/function.js
@@ -0,0 +1,48 @@
+import path from 'path'
+
+import camelcase from 'camelcase'
+
+import { getPaths } from 'src/lib'
+
+import {
+  templateForComponentFile,
+  createYargsForComponentGeneration,
+} from '../helpers'
+
+export const files = async ({ name, ...rest }) => {
+  const functionName = camelcase(name)
+  const file = templateForComponentFile({
+    name: functionName,
+    componentName: functionName,
+    apiPathSection: 'functions',
+    generator: 'function',
+    templatePath: 'function.js.template',
+    templateVars: { ...rest },
+    outputPath: path.join(getPaths().api.functions, `${functionName}.js`),
+  })
+
+  return { [file[0]]: file[1] }
+
+  // Returns
+  // {
+  //    "path/to/fileA": "<<<template>>>",
+  //    "path/to/fileB": "<<<template>>>",
+  // }
+  return [file].reduce((acc, [outputPath, content]) => {
+    return {
+      [outputPath]: content,
+      ...acc,
+    }
+  }, {})
+}
+
+export const desc = 'Generate a function'
+
+export const builder = {
+  force: { type: 'boolean', default: false },
+}
+
+export const { command, handler } = createYargsForComponentGeneration({
+  componentName: 'function',
+  filesFn: files,
+})
