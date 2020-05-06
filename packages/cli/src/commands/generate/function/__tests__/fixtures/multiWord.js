39912acbc16f9469f3eb3b78657c37b00fbd1f35
Rob Cameron <rob.cameron@fastmail.com>
2020-05-06 14:57:06 -0700

    Fix handler export in generated function

--- packages/cli/src/commands/generate/function/__tests__/fixtures/multiWord.js
+++ packages/cli/src/commands/generate/function/__tests__/fixtures/multiWord.js
@@ -1,6 +1,6 @@
-exports handler = async (event, context) => {
+export const handler = async (event, context) => {
   return {
     statusCode: 200,
-    body: `sendMail function`
+    body: `sendMail function`,
   }
 }
--- packages/cli/src/commands/generate/function/__tests__/fixtures/singleWord.js
+++ packages/cli/src/commands/generate/function/__tests__/fixtures/singleWord.js
@@ -1,6 +1,6 @@
-exports handler = async (event, context) => {
+export const handler = async (event, context) => {
   return {
     statusCode: 200,
-    body: `foo function`
+    body: `foo function`,
   }
 }
