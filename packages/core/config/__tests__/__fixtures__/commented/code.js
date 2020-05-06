3e25de39622acdcaf3446831221d054cf710883e
Robert Broersm <robertbroersma@gmail.com>
2020-05-06 16:49:12 +0200

    test(core): add tests for babel cell plugin

---
+++ packages/core/config/__tests__/__fixtures__/commented/code.js
@@ -0,0 +1,28 @@
+export const QUERY = gql`
+  query {
+    posts {
+      id
+      title
+      body
+    }
+  }
+`
+
+// export const beforeQuery = () => ({})
+export const afterQuery = () => ({})
+
+export function Loading() {
+  return 'Loading'
+}
+
+// export function Empty() {
+//   return 'Empty'
+// }
+
+export function Failure({ error }) {
+  return error.message
+}
+
+export const Success = ({ posts }) => {
+  return JSON.stringify(posts, null, 2)
+}
