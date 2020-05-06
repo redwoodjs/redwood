c79f341c63f4ec0d4d74629cd2187bf6037774cb
Robert <robertbroersma@gmail.com>
2020-05-06 14:10:54 +0200

    Code readability tweaks

--- packages/core/config/babel-plugin-redwood-cell.js
+++ packages/core/config/babel-plugin-redwood-cell.js
@@ -3,11 +3,12 @@
 // The end result of this plugin is something like:
 // ```js
 // import { withCell } from '@redwoodjs/web'
+// 
 // export default withCell({ QUERY, Loading, Succes, Failure, Empty, beforeQuery, afterQuery })
 // ```
 module.exports = function ({ types: t }) {
   // These are the expected named exports from a Cell file.
-  const EXPORTS_FROM_CELL = [
+  const EXPECTED_EXPORTS_FROM_CELL = [
     'QUERY',
     'Loading',
     'Success',
@@ -17,10 +18,30 @@ module.exports = function ({ types: t }) {
     'afterQuery',
   ]
 
+  // This array will
+  // - collect exports from the Cell file during ExportNamedDeclaration
+  // - collected exports will then be passed to `withCell`
+  // - be cleared after Program exit to prepare for the next file
   let exportNames = []
 
   return {
     visitor: {
+      ExportNamedDeclaration(path) {
+        const { declaration } = path.node
+
+        let name
+        if (declaration.type === 'VariableDeclaration') {
+          name = declaration.declarations[0].id.name
+        }
+
+        if (declaration.type === 'FunctionDeclaration') {
+          name = declaration.id.name
+        }
+
+        if (EXPECTED_EXPORTS_FROM_CELL.includes(name)) {
+          exportNames.push(name)
+        }
+      },
       Program: {
         exit(path) {
           // import { withCell } from '@redwoodjs/web'
@@ -56,22 +77,6 @@ module.exports = function ({ types: t }) {
 
           exportNames = []
         },
-      },
-      ExportNamedDeclaration(path) {
-        const { declaration } = path.node
-
-        let name
-        if (declaration.type === 'VariableDeclaration') {
-          name = declaration.declarations[0].id.name
-        }
-
-        if (declaration.type === 'FunctionDeclaration') {
-          name = declaration.id.name
-        }
-
-        if (EXPORTS_FROM_CELL.includes(name)) {
-          exportNames.push(name)
-        }
       },
     },
   }
