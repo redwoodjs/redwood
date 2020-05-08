a800bd20a95f18abdd11f080c3ff21e7a3541572
Robert Broersm <robertbroersma@gmail.com>
2020-05-08 09:52:46 +0200

    fix(core): configure jest to work nicely with testing package

--- packages/core/config/jest.config.web.js
+++ packages/core/config/jest.config.web.js
@@ -2,6 +2,10 @@
 
 const path = require('path')
 
+const { getPaths } = require('@redwoodjs/internal')
+
+const redwoodPaths = getPaths()
+
 module.exports = {
   resolver: 'jest-directory-named-resolver',
   rootDir: process.cwd(),
@@ -9,4 +13,18 @@ module.exports = {
     __REDWOOD__API_PROXY_PATH: '/',
   },
   setupFilesAfterEnv: [path.resolve(__dirname, './jest.setup.web.js')],
+  moduleNameMapper: {
+    '^react$': path.resolve(redwoodPaths.base, 'node_modules', 'react'),
+    '^react-dom$': path.resolve(redwoodPaths.base, 'node_modules', 'react-dom'),
+    '^@apollo/react-common': path.resolve(
+      redwoodPaths.base,
+      'node_modules',
+      '@apollo/react-common'
+    ),
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|css)$': path.resolve(
+      __dirname,
+      'fileMock.js'
+    ),
+    '~/web/Routes': '<rootDir>/src/Routes',
+  },
 }
