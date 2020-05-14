6c17586da55c3078425d66878043c3590ce5aaba
Justin Reidy <justin.reidy@airbnb.com>
2020-05-14 12:39:24 -0400

    update tests for sdl

---
+++ packages/cli/src/commands/generate/sdl/__tests__/fixtures/multiWordSdlCrud.ts
@@ -0,0 +1,31 @@
+import gql from 'graphql-tag'
+
+export const schema = gql`
+  type UserProfile {
+    id: Int!
+    username: String!
+    userId: Int!
+    user: User!
+  }
+
+  type Query {
+    userProfiles: [UserProfile!]!
+    userProfile(id: Int!): UserProfile!
+  }
+
+  input CreateUserProfileInput {
+    username: String!
+    userId: Int!
+  }
+
+  input UpdateUserProfileInput {
+    username: String
+    userId: Int
+  }
+
+  type Mutation {
+    createUserProfile(input: CreateUserProfileInput!): UserProfile!
+    updateUserProfile(id: Int!, input: UpdateUserProfileInput!): UserProfile!
+    deleteUserProfile(id: Int!): UserProfile!
+  }
+`
