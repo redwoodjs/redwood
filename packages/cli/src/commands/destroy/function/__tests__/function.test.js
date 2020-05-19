49b8568599c7e161f2941dcb1742c6ae2828a313
Anton Moiseev <a@antonmoiseev.com>
2020-05-19 10:34:54 +0400

    Add test for destroy function command

--- packages/cli/src/commands/destroy/function/__tests__/function.test.js
+++ packages/cli/src/commands/destroy/function/__tests__/function.test.js
@@ -11,11 +11,11 @@ import fs from 'fs'
 
 import 'src/lib/test'
 
-import { files } from '../../../generate/service/service'
-import { tasks } from '../service'
+import { files } from '../../../generate/function/function'
+import { tasks } from '../function'
 
 beforeEach(async () => {
-  fs.__setMockFiles(await files({ name: 'User' }))
+  fs.__setMockFiles(await files({ name: 'sendMail' }))
 })
 
 afterEach(() => {
@@ -25,11 +25,15 @@ afterEach(() => {
 
 test('destroys service files', async () => {
   const unlinkSpy = jest.spyOn(fs, 'unlinkSync')
-  const t = tasks({ componentName: 'service', filesFn: files, name: 'User' })
+  const t = tasks({
+    componentName: 'service',
+    filesFn: files,
+    name: 'sendMail',
+  })
   t.setRenderer('silent')
 
   return t.run().then(async () => {
-    const generatedFiles = Object.keys(await files({ name: 'User' }))
+    const generatedFiles = Object.keys(await files({ name: 'sendMail' }))
     expect(generatedFiles.length).toEqual(unlinkSpy.mock.calls.length)
     generatedFiles.forEach((f) => expect(unlinkSpy).toHaveBeenCalledWith(f))
   })
