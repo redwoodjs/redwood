a25e05ab4f6de87db24a4fffdf1817b3b63b47e6
Peter Pistorius <peter.pistorius@gmail.com>
2020-05-28 11:02:51 +0200

    Remove Apollo Mock export.

--- packages/testing/src/index.js
+++ packages/testing/src/index.js
@@ -1,7 +1,5 @@
-import * as routerMock from './routerMock'
-export { routerMock }
+export { MockRouter } from './MockRouter'
 
 // https://testing-library.com/docs/react-testing-library/setup#custom-render
 export * from '@testing-library/react'
-export { MockedProvider } from '@apollo/react-testing'
 export { customRender as render } from './render'
