df188ce2922ef8245e478ac910c9e3d7436c936f
Peter Pistorius <peter.pistorius@gmail.com>
2020-05-30 10:17:00 +0200

    Fix babel-test for routes path.

--- packages/core/config/babel-preset.js
+++ packages/core/config/babel-preset.js
@@ -132,14 +132,15 @@ module.exports = () => ({
         ],
       ],
     },
+    // ** Files ending in `Cell` **
     {
       test: /.+Cell.(js|tsx)$/,
       plugins: [require('../dist/babel-plugin-redwood-cell')],
     },
-    // Automatically import files in `src/pages/*` in to
-    // the `src/Routes.[ts|jsx]` file.
+    // Automatically import files in `./web/src/pages/*` in to
+    // the `./web/src/Routes.[ts|jsx]` file.
     {
-      test: redwoodPaths.web.routes,
+      test: /\/web\/src\/Routes.(js|tsx)$/,
       plugins: [require('../dist/babel-plugin-redwood-routes-auto-loader')],
     },
   ],
