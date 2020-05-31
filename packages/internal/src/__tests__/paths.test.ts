8e121985b96f1262a69d061590814803af98aea6
Peter Pistorius <peter.pistorius@gmail.com>
2020-05-31 16:50:19 +0200

    Adjust tests to not test absolute paths.

--- packages/internal/src/__tests__/paths.test.ts
+++ packages/internal/src/__tests__/paths.test.ts
@@ -5,27 +5,11 @@ import { processPagesDir, resolveFile } from '../paths'
 describe('paths', () => {
   describe('processPagesDir', () => {
     it('it accurately finds the pages', () => {
-      const deps = processPagesDir(
+      const pages = processPagesDir(
         path.resolve(__dirname, './fixtures/web/pages/')
       )
-      expect(deps).toMatchInlineSnapshot(`
-        Array [
-          Object {
-            "const": "AdminMargleTheWorld",
-            "importName": "AdminMargleTheWorld",
-            "importPath": "src/pages/Admin/MargleTheWorld",
-            "importStatement": "const AdminMargleTheWorld = { name: 'AdminMargleTheWorld', loader: () => import('src/pages/Admin/MargleTheWorld') }",
-            "path": "/Users/peterp/x/redwoodjs/redwood/packages/internal/src/__tests__/fixtures/web/pages/Admin/MargleTheWorld",
-          },
-          Object {
-            "const": "HelloWorld",
-            "importName": "HelloWorld",
-            "importPath": "src/pages/HelloWorld",
-            "importStatement": "const HelloWorld = { name: 'HelloWorld', loader: () => import('src/pages/HelloWorld') }",
-            "path": "/Users/peterp/x/redwoodjs/redwood/packages/internal/src/__tests__/fixtures/web/pages/HelloWorld",
-          },
-        ]
-      `)
+      expect(pages[0].importPath).toEqual('src/pages/Admin/MargleTheWorld')
+      expect(pages[1].importPath).toEqual('src/pages/HelloWorld')
     })
   })
 
