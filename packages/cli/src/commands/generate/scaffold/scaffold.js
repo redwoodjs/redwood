bea2212bb6a73cce8cd82c6c7ef8fa9b5dd1a558
Lachlan Campbell <lachlan@hackclub.com>
2020-06-03 11:21:26 -0400

    Fix component name

--- packages/cli/src/commands/generate/scaffold/scaffold.js
+++ packages/cli/src/commands/generate/scaffold/scaffold.js
@@ -198,7 +198,7 @@ const componentFiles = async (name, scaffoldPath = '') => {
   const intForeignKeys = intForeignKeysForModel(model)
   let fileList = {}
   const fieldComponents = {
-    Boolean: 'CheckBox',
+    Boolean: 'CheckboxField',
     String: 'TextField',
   }
   const editableColumns = columns
