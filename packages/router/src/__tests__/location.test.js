2a38b805aada7eb3fe0ddc5618419c0736b696e5
Terris Kremer <terris.kremer@gmail.com>
2020-06-03 15:32:38 -0400

    add expect tests for each location value

--- packages/router/src/__tests__/location.test.js
+++ packages/router/src/__tests__/location.test.js
@@ -3,28 +3,38 @@ import { render } from '@testing-library/react'
 import { LocationProvider, useLocation } from '../location'
 
 describe('useLocation', () => {
-  const MyLocation = () => {
+  const TestComponent = () => {
     const location = useLocation()
-    return <div>{JSON.stringify(location)}</div>
+    return (
+      <div>
+        <p>{JSON.stringify(location)}</p>
+        <input data-testid="pathname" defaultValue={location.pathname} />
+        <input data-testid="search" defaultValue={location.search} />
+        <input data-testid="hash" defaultValue={location.hash} />
+      </div>
+    )
   }
 
-  it('returns the correct pathname search and hash values', () => {
+  it('returns the correct pathname, search, and hash values', () => {
     const mockLocation = {
       pathname: '/dunder-mifflin',
       search: '?facts=bears',
-      hash: '#beats',
+      hash: '#woof',
     }
 
-    const { getByText } = render(
+    const { getByText, getByTestId } = render(
       <LocationProvider location={mockLocation}>
-        <MyLocation />
+        <TestComponent />
       </LocationProvider>
     )
 
     expect(
       getByText(
-        '{"pathname":"/dunder-mifflin","search":"?facts=bears","hash":"#beats"}'
+        '{"pathname":"/dunder-mifflin","search":"?facts=bears","hash":"#woof"}'
       )
     ).toBeTruthy()
+    expect(getByTestId('pathname').value).toEqual('/dunder-mifflin')
+    expect(getByTestId('search').value).toEqual('?facts=bears')
+    expect(getByTestId('hash').value).toEqual('#woof')
   })
 })
