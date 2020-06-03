010c7c496fdf5018c74aa8669401e6b50dd16240
Peter Pistorius <peter.pistorius@gmail.com>
2020-06-03 17:17:46 +0200

    Remove tsignore, we have types!

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,6 +1,5 @@
 export * from 'apollo-server-lambda'
 export * from './makeServices'
-// @ts-ignore
 export * from './makeMergedSchema/makeMergedSchema'
 export * from './functions/graphql'
 export * from './globalContext'
