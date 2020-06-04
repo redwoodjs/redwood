20f6acff95cecddfe86ae1ae9191cd5fbebe229d
Peter Pistorius <peter.pistorius@gmail.com>
2020-06-04 23:51:59 +0200

    Make adding new auth clients slightly easier.

--- packages/auth/src/authClient/index.ts
+++ packages/auth/src/authClient/index.ts
@@ -3,14 +3,12 @@ import type { Auth0, Auth0User } from './auth0'
 import type { GoTrue, GoTrueUser } from './gotrue'
 import type { MagicLink, MagicUser } from './magicLink'
 import type { FirebaseClient } from './firebase'
-//
 import { mapAuthClientNetlify } from './netlify'
 import { mapAuthClientAuth0 } from './auth0'
 import { mapAuthClientGoTrue } from './gotrue'
 import { mapAuthClientMagicLink } from './magicLink'
 import { mapAuthClientFirebase } from './firebase'
 
-// TODO: Make this an enum
 export type SupportedAuthClients =
   | Auth0
   | GoTrue
@@ -18,7 +16,6 @@ export type SupportedAuthClients =
   | MagicLink
   | FirebaseClient
 
-// TODO: Make these enums
 export type SupportedAuthTypes =
   | 'auth0'
   | 'gotrue'
@@ -26,35 +23,31 @@ export type SupportedAuthTypes =
   | 'magic.link'
   | 'firebase'
 
+export type { Auth0User }
+export type { GoTrueUser }
+export type { MagicUser }
+export type SupportedAuthUsers = null | Auth0User | GoTrueUser | MagicUser
+
 export interface AuthClient {
   restoreAuthState?(): void | Promise<any>
   login(options?: any): Promise<any>
   logout(): void | Promise<void>
   getToken(): Promise<null | string>
-  currentUser(): Promise<null | Auth0User | GoTrueUser | MagicUser>
+  currentUser(): Promise<SupportedAuthUsers>
   client: SupportedAuthClients
   type: SupportedAuthTypes
 }
 
-// TODO: Make this an object,
 export const createAuthClient = (
   client: SupportedAuthClients,
   type: SupportedAuthTypes
 ): AuthClient => {
-  switch (type) {
-    case 'auth0':
-      return mapAuthClientAuth0(client as Auth0)
-    case 'gotrue':
-      return mapAuthClientGoTrue(client as GoTrue)
-    case 'netlify':
-      return mapAuthClientNetlify(client as NetlifyIdentity)
-    case 'magic.link':
-      return mapAuthClientMagicLink(client as MagicLink)
-    case 'firebase':
-      return mapAuthClientFirebase(client as FirebaseClient)
-    default:
-      throw new Error(
-        `The ${type} auth client is not currently supported, please consider adding it.`
-      )
+  const clients: Record<SupportedAuthClients, () => SupportedAuthClients> = {
+    auth0: () => mapAuthClientAuth0(client),
+    gotrue: () => mapAuthClientGoTrue(client),
+    netlify: () => mapAuthClientNetlify(client),
+    'magic.link': () => mapAuthClientMagicLink(client),
+    firebase: () => mapAuthClientFirebase(client),
   }
+  return clients[type]()
 }
