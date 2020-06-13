fff93f99e7df8b19f010081ee9e8156fadb3832b
Robert Broersma <robertbroersma@gmail.com>
2020-06-13 13:01:54 +0200

    Wrap MSW rest in server instance

--- packages/testing/src/index.ts
+++ packages/testing/src/index.ts
@@ -1,4 +1,4 @@
-import { graphql as originalGraphql } from 'msw'
+import { graphql as originalGraphql, rest as originalRest } from 'msw'
 
 import server from './mockServer'
 
@@ -15,4 +15,21 @@ export interface GraphQLMock {
 export const graphql: GraphQLMock = {
   query: (...args) => server.use(originalGraphql.query(...args)),
   mutation: (...args) => server.use(originalGraphql.mutation(...args)),
+}
+
+export interface RestMock {
+  get(...args: Parameters<typeof originalRest['get']>): void
+  post(...args: Parameters<typeof originalRest['post']>): void
+  delete(...args: Parameters<typeof originalRest['delete']>): void
+  put(...args: Parameters<typeof originalRest['put']>): void
+  patch(...args: Parameters<typeof originalRest['patch']>): void
+  options(...args: Parameters<typeof originalRest['options']>): void
+}
+export const rest: RestMock = {
+  get: (...args) => server.use(originalRest.get(...args)),
+  post: (...args) => server.use(originalRest.post(...args)),
+  delete: (...args) => server.use(originalRest.delete(...args)),
+  put: (...args) => server.use(originalRest.put(...args)),
+  patch: (...args) => server.use(originalRest.patch(...args)),
+  options: (...args) => server.use(originalRest.options(...args)),
 }
