af1d2d49135c973992595dcb315f77f20b316af1
Peter Pistorius <peter.pistorius@gmail.com>
2020-06-27 08:50:12 +0200

    Make the page template link to itself.

--- packages/cli/src/commands/generate/page/__tests__/fixtures/singleWordPage.js
+++ packages/cli/src/commands/generate/page/__tests__/fixtures/singleWordPage.js
@@ -1,9 +1,15 @@
+import { Link } from '@redwoodjs/router'
+
 const HomePage = () => {
   return (
-    <div>
+    <>
       <h1>HomePage</h1>
-      <p>Find me in ./web/src/pages/HomePage/HomePage.js</p>
-    </div>
+      <p>Find me in "./web/src/pages/HomePage/HomePage.js"</p>
+      <p>
+        My default route is named "home", link to me with `
+        <Link to="home">routes.home()</Link>`
+      </p>
+    </>
   )
 }
 
