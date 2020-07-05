09fb85eeb3f339d2954dc6e9240f7558d041199d
Dominic <dominicsaadi@protonmail.com>
2020-07-05 01:00:40 -0700

    Rename to studio

--- packages/cli/src/commands/dbCommands/studio.js
+++ packages/cli/src/commands/dbCommands/studio.js
@@ -6,27 +6,27 @@ import terminalLink from 'terminal-link'
 import { runCommandTask, getPaths } from 'src/lib'
 import c from 'src/lib/colors'
 
-export const command = 'ide'
+export const command = 'studio'
 export const description = 'Start Prisma Studio'
 
 export const builder = (yargs) => {
   yargs.epilogue(
     `Also see the ${terminalLink(
       'Redwood CLI Reference',
-      'https://redwoodjs.com/reference/command-line-interface#db-ide'
+      'https://redwoodjs.com/reference/command-line-interface#db-studio'
     )}`
   )
 }
 
 export const handler = async () => {
-  // No database, no migrations, no IDE.
+  // No database, no migrations, no studio.
   const FILE_DB = path.join(getPaths().api.db, 'dev.db')
   const DIR_MIGRATIONS = path.join(getPaths().api.db, 'migrations')
 
   if (!fs.existsSync(FILE_DB) && !fs.existsSync(DIR_MIGRATIONS)) {
     console.log(
       // eslint-disable-next-line
-      `${c.warning('[warning]')} your app doesn't have a database (${c.info('api/prisma/dev.db')}) and/or migrations (${c.info('api/prisma/migrations')}). ${c.green('Save and up')} before starting the IDE.`
+      `${c.warning('[warning]')} your app doesn't have a Database (${c.info('api/prisma/dev.db')}) and/or Migrations (${c.info('api/prisma/migrations')}). ${c.green('Save and up')} before starting Studio.`
     )
     return
   }
