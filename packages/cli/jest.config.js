bb1d067e10085784d7a947964d7341ffab447707
Peter Pistorius <peter.pistorius@gmail.com>
2020-07-08 14:24:18 +0200

    Upgrade jest.

--- packages/cli/jest.config.js
+++ packages/cli/jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/*.test.[jt]s?(x)'],
   testPathIgnorePatterns: ['fixtures'],
+  moduleNameMapper: {
+    'src/(.*)': '<rootDir>/src/$1',
+  },
 }
