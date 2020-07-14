ee5a5e70440ca435a64f8ae337bcddbc28cd4f79
Peter Pistorius <peter.pistorius@gmail.com>
2020-07-14 18:55:09 +0200

    Re-export rest msw functionality.

--- packages/testing/src/msw.ts
+++ packages/testing/src/msw.ts
@@ -1,12 +1,10 @@
-// MSW is shared by Jest (NodeJS) and Storybook (Webpack).
+// MSW is shared by Jest (NodeJS) and Storybook (Webpack)
 import * as msw from 'msw'
 
 export const start = async () => {
   if (SERVER) {
-    console.log('it returns the server')
     return SERVER
   }
-
   if (typeof global.process === 'undefined') {
     const { setupWorker } = require('msw')
     SERVER = setupWorker()
@@ -38,19 +36,19 @@ export const graphql: GraphQLMock = {
   mutation: (...args) => getServer().use(msw.graphql.mutation(...args)),
 }
 
-// export interface RestMock {
-//   get(...args: Parameters<typeof originalRest['get']>): void
-//   post(...args: Parameters<typeof originalRest['post']>): void
-//   delete(...args: Parameters<typeof originalRest['delete']>): void
-//   put(...args: Parameters<typeof originalRest['put']>): void
-//   patch(...args: Parameters<typeof originalRest['patch']>): void
-//   options(...args: Parameters<typeof originalRest['options']>): void
-// }
-// export const rest: RestMock = {
-//   get: (...args) => server.use(originalRest.get(...args)),
-//   post: (...args) => server.use(originalRest.post(...args)),
-//   delete: (...args) => server.use(originalRest.delete(...args)),
-//   put: (...args) => server.use(originalRest.put(...args)),
-//   patch: (...args) => server.use(originalRest.patch(...args)),
-//   options: (...args) => server.use(originalRest.options(...args)),
-// }
+export interface RestMock {
+  get(...args: Parameters<typeof msw.rest.get>): void
+  post(...args: Parameters<typeof msw.rest.post>): void
+  delete(...args: Parameters<typeof msw.rest.delete>): void
+  put(...args: Parameters<typeof msw.rest.put>): void
+  patch(...args: Parameters<typeof msw.rest.patch>): void
+  options(...args: Parameters<typeof msw.rest.options>): void
+}
+export const rest: RestMock = {
+  get: (...args) => getServer().use(msw.rest.get(...args)),
+  post: (...args) => getServer().use(msw.rest.post(...args)),
+  delete: (...args) => getServer().use(msw.rest.delete(...args)),
+  put: (...args) => getServer().use(msw.rest.put(...args)),
+  patch: (...args) => getServer().use(msw.rest.patch(...args)),
+  options: (...args) => getServer().use(msw.rest.options(...args)),
+}
