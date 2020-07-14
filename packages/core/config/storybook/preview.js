7bbcc100f01ca2e904216c924967602449c783ae
Peter Pistorius <peter.pistorius@gmail.com>
2020-07-14 15:49:30 +0200

    Clean up StorybookLoader.

--- packages/core/config/storybook/preview.js
+++ packages/core/config/storybook/preview.js
@@ -1,11 +1,13 @@
 const React = require('react')
 const { addDecorator } = require('@storybook/react')
 
+// The StorybookLoader is responsible for importing all the mock files and booting
+// up the mock service workers.
+const { StorybookLoader } = require('@redwoodjs/core/dist/storybook')
+
 // Import the user's default CSS file
 require('~__REDWOOD__USER_WEB_DEFAULT_CSS')
 
-// The StorybookLoader is responsible for importing the mock files and booting
-// up the mock service workers.
-const { default: StorybookLoader } = require('@redwoodjs/core/dist/StorybookLoader')
-
-addDecorator((storyFn) => React.createElement(StorybookLoader, { storyFn }))
+addDecorator(
+  (storyFn) => React.createElement(StorybookLoader, { storyFn })
+)
---
+++ packages/core/src/storybook/index.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import { MockProviders, mockData, getMockData, msw } from '@redwoodjs/testing'
+
+global.mockData = mockData
+global.__RW__mockData = mockData
+global.getMockData = getMockData
+global.__RW__getMockData = getMockData
+
+export const StorybookLoader = ({ storyFn }) => {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const init = async () => {
+      await msw.startServer()
+      // Import all the `*.mock.*` files.
+      const reqs = require.context(
+        '~__REDWOOD__USER_WEB_SRC',
+        true,
+        /.+(mock).(js|ts)$/
+      )
+      reqs.keys().forEach((r) => {
+        reqs(r)
+      })
+
+      setLoading(false)
+    }
+    init()
+  }, [])
+
+  if (loading) {
+    return null
+  }
+
+  return <MockProviders>{storyFn()}</MockProviders>
+}
