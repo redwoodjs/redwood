9c5604573f2490a073e20440be66fa307cdd7b88
Rob Cameron <rob.cameron@fastmail.com>
2020-07-16 16:14:31 -0700

    Step 2: adds generate command for creating a new migration

---
+++ packages/cli/src/commands/generate/dataMigration/__tests__/fixtures/dataMigration.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-console */
+const { PrismaClient } = require('@prisma/client')
+const dotenv = require('dotenv')
+
+dotenv.config()
+const db = new PrismaClient()
+
+async function main() {
+  // Your migration here...
+}
+
+main().finally(async () => {
+  await db.disconnect()
+})
