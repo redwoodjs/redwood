8028d11e5afaba36519df26a7b5c26f70a7385b9
Peter Pistorius <peter.pistorius@gmail.com>
2020-07-17 10:58:40 +0200

    Use new mockRequests implementation.

--- packages/core/src/storybook/index.tsx
+++ packages/core/src/storybook/index.tsx
@@ -1,16 +1,9 @@
 import { useEffect, useState } from 'react'
 import {
   MockProviders,
-  mockData,
-  getMockData,
-  MOCK_DATA,
-  msw,
+  startMSW,
+  setupRequestHandlers,
 } from '@redwoodjs/testing'
-
-global.mockData = mockData
-global.__RW__mockData = mockData
-global.getMockData = getMockData
-global.__RW__getMockData = getMockData
 
 export const StorybookLoader: React.FunctionComponent<{
   storyFn: Function
@@ -26,32 +19,12 @@ export const StorybookLoader: React.FunctionComponent<{
         true,
         /.+(mock).(js|ts)$/
       )
-      console.groupCollapsed(`[RW] Found ${reqs.length} mock file(s)`)
       reqs.keys().forEach((r) => {
-        console.log(r)
         reqs(r)
       })
-      console.groupEnd()
 
-      await msw.start()
-      // Reset the msw handlers since a user can overwrite them.
-      msw.getServer().resetHandlers()
-      // Set GraphQL query and mutuation handlers for each MOCK_DATA value.
-      // Skip the generated handlers.
-      console.groupCollapsed('[RW] Mocking GraphQL resolvers')
-      for (const name of Object.keys(MOCK_DATA).filter(
-        (n) => !n.includes(':')
-      )) {
-        console.groupCollapsed(name)
-        const data = MOCK_DATA[name]
-        console.log(JSON.stringify(data, null, 2))
-        const mockHandler = (_req: any, res: any, ctx: any) =>
-          res(ctx.data(data))
-        msw.graphql.query(name, mockHandler)
-        msw.graphql.mutation(name, mockHandler)
-        console.groupEnd()
-      }
-      console.groupEnd()
+      await startMSW()
+      setupRequestHandlers()
       setLoading(false)
     }
     init()
