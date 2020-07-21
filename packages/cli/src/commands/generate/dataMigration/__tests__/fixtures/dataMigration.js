5df438b0e9b5c6e48792ba4a33f1c8d3adb69d0d
Rob Cameron <rob.cameron@fastmail.com>
2020-07-21 13:41:27 -0700

    Test updates

--- packages/cli/src/commands/generate/dataMigration/__tests__/fixtures/dataMigration.js
+++ packages/cli/src/commands/generate/dataMigration/__tests__/fixtures/dataMigration.js
@@ -1,14 +1,3 @@
-/* eslint-disable no-console */
-const { PrismaClient } = require('@prisma/client')
-const dotenv = require('dotenv')
-
-dotenv.config()
-const db = new PrismaClient()
-
-async function main() {
-  // Your migration here...
+export default async ({ db }) => {
+  // Migration here...
 }
-
-main().finally(async () => {
-  await db.disconnect()
-})
