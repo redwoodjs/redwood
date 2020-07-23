ad42dcf267d947565b33acf3bebce26d2352f2ab
David S Price <thedavid@thedavidprice.com>
2020-07-23 16:52:02 -0700

    remove `.all-contributorsrc` during installation

--- packages/create-redwood-app/src/create-redwood-app.js
+++ packages/create-redwood-app/src/create-redwood-app.js
@@ -113,6 +113,8 @@ const createProjectTasks = ({ newAppDir }) => {
             path.join(newAppDir, '.gitignore.app'),
             path.join(newAppDir, '.gitignore')
           )
+
+          fs.unlinkSync(path.join(newAppDir, '.all-contributorsrc'))
         } catch (e) {
           throw new Error('Could not move project files')
         }
