0a585e2d664de97e8a447d63cfd4748977a40b0b
Peter Pistorius <peter.pistorius@gmail.com>
2020-07-29 23:30:13 +0200

    Adopt new way of generate pages.

--- packages/cli/src/commands/generate/page/__tests__/page.test.js
+++ packages/cli/src/commands/generate/page/__tests__/page.test.js
@@ -1,108 +1,35 @@
-global.__dirname = __dirname
 import path from 'path'
 
-import { loadGeneratorFixture } from 'src/lib/test'
+import { getProject } from '@redwoodjs/structure'
 
-import * as page from '../page'
+import { templates } from '../page'
 
-let singleWordFiles, multiWordFiles, pluralWordFiles
+describe('page generator', () => {
+  const fx = path.resolve(
+    __dirname,
+    '../../../../../../../__fixtures__/example-todo-main'
+  )
+  const project = getProject(fx)
 
-beforeAll(() => {
-  singleWordFiles = page.files({ name: 'Home' })
-  multiWordFiles = page.files({ name: 'ContactUs' })
-  pluralWordFiles = page.files({ name: 'Cats' })
-})
-
-test('returns exactly 3 files', () => {
-  expect(Object.keys(singleWordFiles).length).toEqual(3)
-})
-
-test('creates a page component', () => {
-  expect(
-    singleWordFiles[
-      path.normalize('/path/to/project/web/src/pages/HomePage/HomePage.js')
+  it('creates the correct templates', () => {
+    const name = 'Home'
+    const path = '/home'
+    const route = project.router.createRouteString(name, path)
+    const newTemplates = templates({ name, path, routes: [route] }, { project })
+    // Test the path output.
+    expect(newTemplates.map((t) => t.path.replace(fx, '')))
+      .toMatchInlineSnapshot(`
+    Array [
+      "/web/src/pages/HomePage/HomePage.stories.js",
+      "/web/src/pages/HomePage/HomePage.test.js",
+      "/web/src/pages/HomePage/HomePage.js",
+      "/web/src/Routes.js",
     ]
-  ).toEqual(loadGeneratorFixture('page', 'singleWordPage.js'))
-})
+  `)
 
-test('creates a page test', () => {
-  expect(
-    singleWordFiles[
-      path.normalize('/path/to/project/web/src/pages/HomePage/HomePage.test.js')
-    ]
-  ).toEqual(loadGeneratorFixture('page', 'singleWordPage.test.js'))
-})
-
-test('creates a page test', () => {
-  expect(
-    singleWordFiles[
-      path.normalize(
-        '/path/to/project/web/src/pages/HomePage/HomePage.stories.js'
-      )
-    ]
-  ).toEqual(loadGeneratorFixture('page', 'singleWordPage.stories.js'))
-})
-
-test('creates a page component', () => {
-  expect(
-    multiWordFiles[
-      path.normalize(
-        '/path/to/project/web/src/pages/ContactUsPage/ContactUsPage.js'
-      )
-    ]
-  ).toEqual(loadGeneratorFixture('page', 'multiWordPage.js'))
-})
-
-test('creates a page test', () => {
-  expect(
-    multiWordFiles[
-      path.normalize(
-        '/path/to/project/web/src/pages/ContactUsPage/ContactUsPage.test.js'
-      )
-    ]
-  ).toEqual(loadGeneratorFixture('page', 'multiWordPage.test.js'))
-})
-
-test('creates a page story', () => {
-  expect(
-    multiWordFiles[
-      path.normalize(
-        '/path/to/project/web/src/pages/ContactUsPage/ContactUsPage.stories.js'
-      )
-    ]
-  ).toEqual(loadGeneratorFixture('page', 'multiWordPage.stories.js'))
-})
-
-test('creates a page component with a plural word for name', () => {
-  expect(
-    pluralWordFiles[
-      path.normalize('/path/to/project/web/src/pages/CatsPage/CatsPage.js')
-    ]
-  ).toEqual(loadGeneratorFixture('page', 'pluralWordPage.js'))
-})
-
-test('creates a single-word route name', () => {
-  const names = ['Home', 'home']
-
-  names.forEach((name) => {
-    expect(page.routes({ name: name, path: '/' })).toEqual([
-      '<Route path="/" page={HomePage} name="home" />',
-    ])
+    // write a snapshot for each generated template.
+    for (const t of newTemplates) {
+      expect(t.contents).toMatchSnapshot(t.path.replace(fx, ''))
+    }
   })
-})
-
-test('creates a camelCase route name for multiple word names', () => {
-  const names = ['FooBar', 'foo_bar', 'foo-bar', 'fooBar']
-
-  names.forEach((name) => {
-    expect(page.routes({ name: name, path: 'foo-bar' })).toEqual([
-      '<Route path="foo-bar" page={FooBarPage} name="fooBar" />',
-    ])
-  })
-})
-
-test('creates a path equal to passed path', () => {
-  expect(page.routes({ name: 'FooBar', path: 'fooBar-baz' })).toEqual([
-    '<Route path="fooBar-baz" page={FooBarPage} name="fooBar" />',
-  ])
 })
--- packages/cli/src/commands/generate/page/page.js
+++ packages/cli/src/commands/generate/page/page.js
@@ -1,67 +1,77 @@
 import { execSync } from 'child_process'
 
 import Listr from 'listr'
-import camelcase from 'camelcase'
-import pascalcase from 'pascalcase'
 import terminalLink from 'terminal-link'
+import { getProject } from '@redwoodjs/structure'
+import paramCase from 'param-case'
+import { writeFilesTask } from 'src/x/listr/writeFilesTask'
 
-import { writeFilesTask, addRoutesToRouterTask } from 'src/lib'
 import c from 'src/lib/colors'
 
-import { templateForComponentFile, pathName } from '../helpers'
+import { templateForComponentFile } from '../helpers'
 
 const COMPONENT_SUFFIX = 'Page'
 const REDWOOD_WEB_PATH_NAME = 'pages'
 
-export const files = ({ name, ...rest }) => {
-  const pageFile = templateForComponentFile({
-    name,
-    suffix: COMPONENT_SUFFIX,
-    webPathSection: REDWOOD_WEB_PATH_NAME,
-    generator: 'page',
-    templatePath: 'page.js.template',
-    templateVars: rest,
-  })
-  const testFile = templateForComponentFile({
-    name,
-    suffix: COMPONENT_SUFFIX,
-    extension: '.test.js',
-    webPathSection: REDWOOD_WEB_PATH_NAME,
-    generator: 'page',
-    templatePath: 'test.js.template',
-    templateVars: rest,
-  })
-  const storiesFile = templateForComponentFile({
-    name,
-    suffix: COMPONENT_SUFFIX,
-    extension: '.stories.js',
-    webPathSection: REDWOOD_WEB_PATH_NAME,
-    generator: 'page',
-    templatePath: 'stories.js.template',
-    templateVars: rest,
-  })
-
-  // Returns
-  // {
-  //    "path/to/fileA": "<<<template>>>",
-  //    "path/to/fileB": "<<<template>>>",
-  // }
-  return [pageFile, testFile, storiesFile].reduce(
-    (acc, [outputPath, content]) => {
-      return {
-        [outputPath]: content,
-        ...acc,
-      }
+export const templates = (
+  { name, path, routes },
+  { project = getProject() } = {}
+) => {
+  const page = templateForComponentFile(
+    {
+      name,
+      suffix: COMPONENT_SUFFIX,
+      webPathSection: REDWOOD_WEB_PATH_NAME,
+      generator: 'page',
+      templatePath: 'page.js.template',
+      templateVars: { path },
     },
-    {}
+    project.pathHelper
   )
-}
+  const test = templateForComponentFile(
+    {
+      name,
+      suffix: COMPONENT_SUFFIX,
+      extension: '.test.js',
+      webPathSection: REDWOOD_WEB_PATH_NAME,
+      generator: 'page',
+      templatePath: 'test.js.template',
+      templateVars: { path },
+    },
+    project.pathHelper
+  )
+  const stories = templateForComponentFile(
+    {
+      name,
+      suffix: COMPONENT_SUFFIX,
+      extension: '.stories.js',
+      webPathSection: REDWOOD_WEB_PATH_NAME,
+      generator: 'page',
+      templatePath: 'stories.js.template',
+      templateVars: { path },
+    },
+    project.pathHelper
+  )
 
-export const routes = ({ name, path }) => {
   return [
-    `<Route path="${path}" page={${pascalcase(name)}Page} name="${camelcase(
-      name
-    )}" />`,
+    {
+      path: stories[0],
+      contents: stories[1],
+    },
+    {
+      path: test[0],
+      contents: test[1],
+    },
+    {
+      path: page[0],
+      contents: page[1],
+    },
+    {
+      path: project.pathHelper.web.routes,
+      contents: project.router.createRouterString(routes),
+      overwrite: true, // file exists
+      doNotDelete: true, // do not delete
+    },
   ]
 }
 
@@ -83,6 +93,11 @@ export const builder = (yargs) => {
       description: 'Overwrite existing files',
       type: 'boolean',
     })
+    .option('json', {
+      default: false,
+      description: 'Output as JSON',
+      type: 'boolean',
+    })
     .epilogue(
       `Also see the ${terminalLink(
         'Redwood CLI Reference',
@@ -91,14 +106,13 @@ export const builder = (yargs) => {
     )
 }
 
-export const handler = async ({ name, path, force }) => {
-  if (process.platform === 'win32') {
+const cleanPathArgForWindows = (path) => {
+  if (path && process.platform === 'win32') {
     // running `yarn rw g page home /` on Windows using GitBash
     // POSIX-to-Windows path conversion will kick in.
     // See https://github.com/git-for-windows/build-extra/blob/d715c9e/ReleaseNotes.md
     // As a workaround we try to detect when this has happened, and reverse
     // the action
-
     try {
       // `cygpath -m /` will return something like 'C:/Program Files/Git/\n'
       const slashPath = execSync('cygpath -m /', {
@@ -112,35 +126,41 @@ export const handler = async ({ name, path, force }) => {
       // `yarn rw g page about /about` =>
       //   page === 'C:/Program Files/Git/about'
       // Sometimes there is a / after 'Git' to match, sometimes there isn't
-      path = path.replace(new RegExp(`^${slashPath}?`), '/')
+      return path.replace(new RegExp(`^${slashPath}?`), '/')
     } catch {
       // probably using PowerShell or cmd, in which case no special handling
       // is needed
     }
   }
+}
 
-  const tasks = new Listr(
-    [
-      {
-        title: 'Generating page files...',
-        task: async () => {
-          const f = await files({ name, path: pathName(path, name) })
-          return writeFilesTask(f, { overwriteExisting: force })
-        },
-      },
-      {
-        title: 'Updating routes file...',
-        task: async () => {
-          addRoutesToRouterTask(routes({ name, path: pathName(path, name) }))
-        },
-      },
-    ].filter(Boolean),
-    { collapse: false }
-  )
+export const handler = async ({ name, path, force, json }) => {
+  path = cleanPathArgForWindows(path)
+  // Generate a path if the user didn't supply one
+  path = path ?? `/${paramCase(name)}`
 
-  try {
-    await tasks.run()
-  } catch (e) {
-    console.log(c.error(e.message))
+  const newRoute = getProject().router.createRouteString(name, path)
+  const files = await templates({ name, path, routes: [newRoute] })
+
+  if (json) {
+    console.log(JSON.stringify(files, 2))
+  } else {
+    const tasks = new Listr(
+      [
+        {
+          title: 'Generating page files...',
+          task: async () => {
+            return writeFilesTask(files, { overwrite: force })
+          },
+        },
+      ].filter(Boolean),
+      { collapse: false }
+    )
+
+    try {
+      await tasks.run()
+    } catch (e) {
+      console.log(c.error(e.message))
+    }
   }
 }
