63f9a56ec0df620b69326790d89871ff421f2e86
Christopher Burns <christopher@tapstudio.co.uk>
2020-08-01 07:20:41 +0100

    added globals back in

--- packages/web/src/index.d.ts
+++ packages/web/src/index.d.ts
@@ -1,4 +1,3 @@
-// import type React from 'react'
 import type _React from 'react'
 import type _gql from 'graphql-tag'
 import type _PropTypes from 'prop-types'
@@ -14,13 +13,17 @@ import type {
   useApolloClient as _useApolloClient,
 } from '@apollo/react-hooks'
 
-declare module '@redwoodjs/web' {
+declare global {
+  // We reduce the number of imports that a user has to do by making them
+  // globals via `Webpack.ProvidePlugin`
   const React: typeof _React
   const gql: typeof _gql
   const PropTypes: typeof _PropTypes
   const mockGraphQLQuery: typeof _mockGraphQLQuery
   const mockGraphQLMutation: typeof _mockGraphQLMutation
+}
 
+declare module '@redwoodjs/web' {
   const useQuery: typeof _useQuery
   const useMutation: typeof _useMutation
   const useLazyQuery: typeof _useLazyQuery
