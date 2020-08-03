50f5e03f5afc8164fe98e49f01cc5d496a88233a
Peter Pistorius <peter.pistorius@gmail.com>
2020-08-03 12:26:27 +0200

    Remove babel-plugin-macros.

--- packages/core/config/babel-preset.js
+++ packages/core/config/babel-preset.js
@@ -15,7 +15,6 @@ module.exports = () => {
     presets: ['@babel/preset-react', '@babel/preset-typescript'],
     plugins: [
       ['@babel/plugin-proposal-class-properties', { loose: true }],
-      'babel-plugin-macros',
       [
         '@babel/plugin-transform-runtime',
         {
