8c1974fb1589c37b2edc6d9fdf3717361b512508
Peter Pistorius <peter.pistorius@gmail.com>
2020-08-05 13:48:04 +0200

    Ignore these errors. Shrug.

--- packages/web/src/global.web-auto-imports.ts
+++ packages/web/src/global.web-auto-imports.ts
@@ -1,8 +1,9 @@
-/* eslint-disable no-redeclare,  no-undef */
+/* eslint-disable no-redeclare */
+/* eslint-disable no-undef */
 import type _React from 'react'
 import type _gql from 'graphql-tag'
 import type _PropTypes from 'prop-types'
-import {
+import type {
   mockGraphQLMutation as _mockGraphQLMutation,
   mockGraphQLQuery as _mockGraphQLQuery,
 } from '@redwoodjs/testing'
@@ -10,9 +11,14 @@ import {
 declare global {
   // We reduce the number of imports that a user has to do by making them
   // globals via `Webpack.ProvidePlugin`
+  // @ts-ignore
   const React: typeof _React
+  // @ts-ignore
   const gql: typeof _gql
+  // @ts-ignore
   const PropTypes: typeof _PropTypes
+  // @ts-ignore
   const mockGraphQLQuery: typeof _mockGraphQLQuery
+  // @ts-ignore
   const mockGraphQLMutation: typeof _mockGraphQLMutation
 }
