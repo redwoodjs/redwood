9ea3e9154874fd1b6fed819cdab88082ad9207f7
Peter Pistorius <peter.pistorius@gmail.com>
2020-08-12 20:09:55 +0200

    Fix import dir test.

--- packages/core/src/__tests__/babel-plugin-redwood-import-dir.test.ts
+++ packages/core/src/__tests__/babel-plugin-redwood-import-dir.test.ts
@@ -4,21 +4,24 @@ import pluginTester from 'babel-plugin-tester'
 
 import plugin from '../babel-plugin-redwood-import-dir'
 
-describe('babel plugin redwood import dir', () => {
+describe.only('babel plugin redwood import dir', () => {
   pluginTester({
     plugin,
     pluginOptions: {
-      generateTypesPath: '__fixtures__/import-dir',
+      generateTypesPath: 'node_modules/@types/redwoodjs',
       host: {
         readFileSync: () => '',
         appendFileSync: (p, contents) => {
-          expect(p).toEqual('__fixtures__/import-dir/index.d.ts')
+          expect(p).toEqual('node_modules/@types/redwoodjs/index.d.ts')
           expect(contents).toEqual(
-            `<reference path="import-dir-services.d.ts" />`
+            '/// <reference path="import-dir-services.d.ts" />\n'
           )
         },
+        existsSync: () => true,
         writeFileSync: (p, contents) => {
-          expect(p).toEqual('__fixtures__/import-dir/import-dir-services.d.ts')
+          expect(p).toEqual(
+            'node_modules/@types/redwoodjs/import-dir-services.d.ts'
+          )
           expect(contents.replace(/\s/g, '')).toMatch(
             `// @ts-expect-error
             declare module 'src/__fixtures__/**/*.{js,ts}';
