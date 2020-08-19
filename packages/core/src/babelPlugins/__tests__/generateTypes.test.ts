e1c271b053a4470e3a1f0bb46b4bac3e0ab9a1dc
Peter Pistorius <peter.pistorius@gmail.com>
2020-08-19 18:07:19 +0200

    Use mocks for GenerateTypes tests.

--- packages/core/src/babelPlugins/__tests__/generateTypes.test.ts
+++ packages/core/src/babelPlugins/__tests__/generateTypes.test.ts
@@ -1,24 +1,31 @@
-import { generateTypeDefIndex } from '../generateTypes'
-import { Host } from '@redwoodjs/structure'
+import { generateTypeDefIndex, generateTypeDef } from '../generateTypes'
 
-test('that it reads the generated types and writes the index file correctly', () => {
-  const fakeHost = {
-    readdirSync: () => ['import-dir-services.d.ts', 'import-dir-graphql.d.ts'],
-    writeFileSync: jest.fn(),
+const mockReaddirSync = jest.fn(() => [
+  'import-dir-service.d.ts',
+  'import-dir-graphql.d.ts',
+])
+const mockWriteFileSync = jest.fn()
+
+jest.mock('@redwoodjs/structure', () => {
+  return {
+    DefaultHost: jest.fn().mockImplementation(() => ({
+      readdirSync: mockReaddirSync,
+      writeFileSync: mockWriteFileSync,
+      paths: {
+        types: '/fake/project/node_modules/@types/@redwoodjs/generated',
+      },
+    })),
   }
+})
 
-  generateTypeDefIndex({
-    // @ts-expect-error
-    host: fakeHost,
-    genTypesPath: '/fake/project/node_modules/@types/@redwoodjs/generated',
-  })
+test('generates index.d.ts file correctly', () => {
+  generateTypeDefIndex()
 
-  expect(fakeHost.writeFileSync).toBeCalledTimes(1)
-  expect(fakeHost.writeFileSync.mock.calls[0][0]).toMatchInlineSnapshot(
+  expect(mockWriteFileSync.mock.calls[0][0]).toMatchInlineSnapshot(
     `"/fake/project/node_modules/@types/@redwoodjs/index.d.ts"`
   )
-  expect(fakeHost.writeFileSync.mock.calls[0][1]).toMatchInlineSnapshot(`
-    "/// <reference path=\\"./generated/import-dir-services.d.ts\\" />
+  expect(mockWriteFileSync.mock.calls[0][1]).toMatchInlineSnapshot(`
+    "/// <reference path=\\"./generated/import-dir-service.d.ts\\" />
     /// <reference path=\\"./generated/import-dir-graphql.d.ts\\" />"
   `)
 })
