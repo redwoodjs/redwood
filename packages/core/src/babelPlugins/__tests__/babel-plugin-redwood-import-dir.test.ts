726f6a94c4925481bdbcf062e106c7319a2aa1fb
Peter Pistorius <peter.pistorius@gmail.com>
2020-08-19 18:51:01 +0200

    Test import-dir type def generators.

--- packages/core/src/babelPlugins/__tests__/babel-plugin-redwood-import-dir.test.ts
+++ packages/core/src/babelPlugins/__tests__/babel-plugin-redwood-import-dir.test.ts
@@ -1,36 +1,48 @@
 import path from 'path'
-
 import pluginTester from 'babel-plugin-tester'
-
 import plugin from '../babel-plugin-redwood-import-dir'
 
-describe.only('babel plugin redwood import dir', () => {
+const mockReaddirSync = jest.fn(() => [
+  'import-dir-service.d.ts',
+  'import-dir-graphql.d.ts',
+])
+const mockWriteFileSync = jest.fn()
+
+jest.mock('@redwoodjs/structure', () => {
+  return {
+    DefaultHost: jest.fn().mockImplementation(() => ({
+      readdirSync: mockReaddirSync,
+      writeFileSync: mockWriteFileSync,
+      paths: {
+        types: '/fake/project/node_modules/@types/@redwoodjs/generated',
+      },
+    })),
+  }
+})
+
+describe('babel plugin redwood import dir', () => {
   pluginTester({
     plugin,
-    pluginOptions: {
-      generateTypesPath: 'node_modules/@types/redwoodjs',
-      host: {
-        readFileSync: () => '',
-        appendFileSync: (p, contents) => {
-          expect(p).toEqual('node_modules/@types/redwoodjs/index.d.ts')
-          expect(contents).toEqual(
-            '/// <reference path="import-dir-services.d.ts" />\n'
-          )
-        },
-        existsSync: () => true,
-        writeFileSync: (p, contents) => {
-          expect(p).toEqual(
-            'node_modules/@types/redwoodjs/import-dir-services.d.ts'
-          )
-          expect(contents.replace(/\s/g, '')).toMatch(
-            `// @ts-expect-error
-            declare module 'src/__fixtures__/**/*.{js,ts}';
-            `.replace(/\s/g, '')
-          )
-        },
-      },
-    },
     pluginName: 'babel-plugin-redwood-import-dir',
     fixtures: path.join(__dirname, '__fixtures__/import-dir'),
+  })
+
+  afterAll(() => {
+    expect(mockWriteFileSync.mock.calls).toMatchInlineSnapshot(`
+      Array [
+        Array [
+          /fake/project/node_modules/@types/@redwoodjs/generated/import-dir-services.d.ts,
+          
+                // @ts-expect-error
+                declare module 'src/__fixtures__/**/*.{js,ts}';
+              ,
+        ],
+        Array [
+          /fake/project/node_modules/@types/@redwoodjs/index.d.ts,
+          /// <reference path="./generated/import-dir-service.d.ts" />
+      /// <reference path="./generated/import-dir-graphql.d.ts" />,
+        ],
+      ]
+    `)
   })
 })
