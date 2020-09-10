689ed10686f6551570c5014fc61169c484979db8
Peter Pistorius <peter.pistorius@gmail.com>
2020-09-10 16:29:22 +0200

    Selectively export Apollo things.

--- packages/web/src/index.js
+++ packages/web/src/index.js
@@ -1,6 +1,7 @@
 import './config'
 
-export * from '@apollo/client'
+export { useQuery } from '@apollo/client/react/hooks/useQuery'
+export { useMutation } from '@apollo/client/react/hooks/useMutation'
 
 export { default as FatalErrorBoundary } from 'src/components/FatalErrorBoundary'
 export { default as RedwoodProvider } from 'src/components/RedwoodProvider'
