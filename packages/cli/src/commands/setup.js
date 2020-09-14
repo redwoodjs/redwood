3eb79733e689373d50f1cff1a0eb3e6d904969ee
Thomas Seillan <smokinglaslo@gmail.com>
2020-09-14 09:14:10 +0200

    pr/setup - adding entry point command.

---
+++ packages/cli/src/commands/setup.js
@@ -0,0 +1,36 @@
+export const command = 'setup <type>'
+export const aliases = ['g']
+export const description = 'Execute some setup logic'
+import terminalLink from 'terminal-link'
+
+export const builder = (yargs) =>
+  yargs
+    .commandDir('./setup', { recurse: true })
+    .demandCommand()
+    .epilogue(
+      `Also see the ${terminalLink(
+        'Redwood CLI Reference',
+        'https://redwoodjs.com/reference/command-line-interface#generate-alias-g'
+      )}`
+    )
+
+export const yargsDefaults = {
+  force: {
+    alias: 'f',
+    default: false,
+    description: 'Overwrite existing files',
+    type: 'boolean',
+  },
+  javascript: {
+    alias: 'js',
+    default: true,
+    description: 'Generate JavaScript files',
+    type: 'boolean',
+  },
+  typescript: {
+    alias: 'ts',
+    default: false,
+    description: 'Generate TypeScript files',
+    type: 'boolean',
+  },
+}
