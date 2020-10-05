5250c25ca3fcf9169bb438c97b15214607222b09
shzmr <shzmrdev@gmail.com>
2020-10-05 12:56:04 +0000

    Removed @ts-expect-error from web/src/index.ts

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -7,7 +7,6 @@ export { useQuery } from '@apollo/client/react/hooks/useQuery'
 export { useMutation } from '@apollo/client/react/hooks/useMutation'
 export { useApolloClient } from '@apollo/client/react/hooks/useApolloClient'
 
-// @ts-expect-error - no defs
 export { default as FatalErrorBoundary } from 'src/components/FatalErrorBoundary'
 // @ts-expect-error - no defs
 export { default as RedwoodProvider } from 'src/components/RedwoodProvider'
