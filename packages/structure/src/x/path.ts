1579fa0546cc9083fce81bcf006e912063560411
Aldo Bucchi <aldo.bucchi@gmail.com>
2020-10-09 14:35:51 -0700

    structure: improve windows+path+fileURI handling

--- packages/structure/src/x/path.ts
+++ packages/structure/src/x/path.ts
@@ -1,27 +1,32 @@
-import { basename } from 'path'
+import { basename, sep, normalize } from 'path'
 
 export function directoryNameResolver(dirName: string): string {
-  const parts = dirName.split('/')
+  dirName = normalize(dirName)
+  const parts = dirName.split(sep)
   const pp = parts[parts.length - 1]
   parts.push(pp)
-  return parts.join('/') + '.js'
+  return parts.join(sep) + '.js'
 }
 
 export function followsDirNameConvention(filePath: string): boolean {
-  const ending = basenameNoExt(filePath) + '/' + basename(filePath)
+  filePath = normalize(filePath)
+  const ending = basenameNoExt(filePath) + sep + basename(filePath)
   return filePath.endsWith(ending)
 }
 
 export function basenameNoExt(path: string): string {
+  path = normalize(path)
   const parts = basename(path).split('.')
   if (parts.length > 1) parts.pop()
   return parts.join('.')
 }
 
 export function isLayoutFileName(f: string): boolean {
+  f = normalize(f)
   return basenameNoExt(f).endsWith('Layout')
 }
 
 export function isCellFileName(f: string): boolean {
+  f = normalize(f)
   return basenameNoExt(f).endsWith('Cell')
 }
