690a7b1ce827920f830bca825ba22662c680fbf7
Rob Cameron <cannikin@fastmail.com>
2020-10-26 11:39:56 -0700

    Proposal: Update cell tests to just test that nothing is thrown (#1397)
    * Update cell tests to just test that nothing is thrown
    
    * Adds comment about starting to write your own tests
    
    * Update tests for generated test file
    
    * More test updates
    
    Co-authored-by: Rob Cameron <rob.cameron@fastmail.com>

--- packages/cli/src/commands/generate/cell/__tests__/fixtures/camelCaseWordCell.test.js
+++ packages/cli/src/commands/generate/cell/__tests__/fixtures/camelCaseWordCell.test.js
@@ -4,23 +4,31 @@ import { standard } from './UserProfileCell.mock'
 
 describe('UserProfileCell', () => {
   test('Loading renders successfully', () => {
-    render(<Loading />)
-    // Use screen.debug() to see output
-    expect(screen.getByText('Loading...')).toBeInTheDocument()
+    expect(() => {
+      render(<Loading />)
+    }).not.toThrow()
   })
 
   test('Empty renders successfully', async () => {
-    render(<Empty />)
-    expect(screen.getByText('Empty')).toBeInTheDocument()
+    expect(() => {
+      render(<Empty />)
+    }).not.toThrow()
   })
 
   test('Failure renders successfully', async () => {
-    render(<Failure error={new Error('Oh no')} />)
-    expect(screen.getByText(/Oh no/i)).toBeInTheDocument()
+    expect(() => {
+      render(<Failure error={new Error('Oh no')} />)
+    }).not.toThrow()
   })
 
+  // When you're ready to test the actual output of your component render
+  // you could test that, for example, certain text is present:
+  //
+  //   expect(screen.getByText('Hello, world')).toBeInTheDocument()
+
   test('Success renders successfully', async () => {
-    render(<Success userProfile={standard().userProfile} />)
-    expect(screen.getByText(/42/i)).toBeInTheDocument()
+    expect(() => {
+      render(<Success userProfile={standard().userProfile} />)
+    }).not.toThrow()
   })
 })
