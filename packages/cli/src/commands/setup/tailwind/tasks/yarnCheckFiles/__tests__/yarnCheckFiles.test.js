1ff4e083ce0bb6dfd50f7450f908593970e37376
Olivier Lance <olivier@techtrails.io>
2020-11-07 19:09:32 +0100

    Tests for the yarnCheckFiles task

---
+++ packages/cli/src/commands/setup/tailwind/tasks/yarnCheckFiles/__tests__/yarnCheckFiles.test.js
@@ -0,0 +1,17 @@
+import execa from 'execa'
+
+import 'src/lib/test'
+import yarnCheckFiles from '..'
+
+jest.mock('execa', () => {
+  return jest.fn()
+})
+
+describe('rw setup tailwind - yarnCheckFiles task', () => {
+  test('it calls yarn install --check-files', () => {
+    const task = yarnCheckFiles()
+    task()
+
+    expect(execa).toHaveBeenCalledWith('yarn', ['install', '--check-files'])
+  })
+})
