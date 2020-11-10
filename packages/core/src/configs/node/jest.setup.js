8fe457992ede464108bb44d11f3d129953d9bf34
Rob Cameron <rob.cameron@fastmail.com>
2020-11-10 12:37:41 -0800

    Adds testWithFixtures global function

--- packages/core/src/configs/node/jest.setup.js
+++ packages/core/src/configs/node/jest.setup.js
@@ -1,22 +1,45 @@
 const path = require('path')
 
-const camelCase = require('lodash/camelCase')
 const { getPaths } = require('@redwoodjs/internal')
 
 const redwoodPaths = getPaths()
 
 const { db } = require(path.join(redwoodPaths.api.src, 'lib', 'db'))
 
-/**
- * Loop through all db models and clear the tables before every test
- */
-beforeEach(async () => {
-  for (let model of db._dmmf.datamodel.models) {
-    if (model.name === 'RW_DataMigration') continue
+window.testWithFixtures = (...args) => {
+  return global.test(args[0], async (done) => {
+    const path = require('path')
+    const testFilePath = path.parse(window.jasmine.testPath)
 
-    await db[camelCase(model.name)].deleteMany()
-  }
-})
+    const allFixtures = require(`${testFilePath.dir}/${
+      testFilePath.name.split('.')[0]
+    }.fixtures`)
+
+    // create fixtures
+    const fixtures = {}
+    for (const [model, dataArray] of Object.entries(allFixtures)) {
+      const transFixtures = []
+
+      for (const data of dataArray) {
+        transFixtures.push(db[model].create({ data }))
+      }
+
+      fixtures[model] = await db.$transaction(transFixtures)
+    }
+
+    // run actual test
+    await args[1](fixtures)
+
+    // delete fixtures
+    for (const [model] of Object.entries(fixtures)) {
+      await db.$queryRaw(`DELETE FROM ${model}`)
+    }
+
+    done()
+  })
+}
+
+window.itWithFixtures = window.testWithFixtures
 
 afterAll(async () => {
   await db.$disconnect()
