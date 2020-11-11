41238d6758653e78cf10aca8ced19c9d5117f194
Peter Pistorius <peter.pistorius@gmail.com>
2020-11-11 23:26:21 +0100

    Use original prisma path.

--- packages/internal/src/config.ts
+++ packages/internal/src/config.ts
@@ -57,7 +57,7 @@ const DEFAULT_CONFIG: Config = {
     port: 8911,
     path: './api',
     target: TargetEnum.NODE,
-    schemaPath: './api/db/schema.prisma',
+    schemaPath: './api/prisma/schema.prisma',
   },
   browser: {
     open: false,
