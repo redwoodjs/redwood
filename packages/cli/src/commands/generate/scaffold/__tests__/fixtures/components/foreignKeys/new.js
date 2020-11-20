d08736b3b0088cc89263995423ce161cd8672ac6
Rob Cameron <cannikin@fastmail.com>
2020-11-19 16:45:57 -0800

    Remove unnecessary refetchQuery calls (#1497)
    Co-authored-by: Rob Cameron <rob.cameron@fastmail.com>

--- packages/cli/src/commands/generate/scaffold/__tests__/fixtures/components/foreignKeys/new.js
+++ packages/cli/src/commands/generate/scaffold/__tests__/fixtures/components/foreignKeys/new.js
@@ -21,11 +21,6 @@ const NewUserProfile = () => {
         navigate(routes.userProfiles())
         addMessage('UserProfile created.', { classes: 'rw-flash-success' })
       },
-      // This refetches the query on the list page. Read more about other ways to
-      // update the cache over here:
-      // https://www.apollographql.com/docs/react/data/mutations/#making-all-other-cache-updates
-      refetchQueries: [{ query: QUERY }],
-      awaitRefetchQueries: true,
     }
   )
 
