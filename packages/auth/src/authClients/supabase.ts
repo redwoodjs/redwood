093d55ab569bc6bec077a6c809c33de49483c074
Duncan Healy <duncan.healy@gmail.com>
2020-11-26 12:21:06 +0000

    format types for supabase #1426

--- packages/auth/src/authClients/supabase.ts
+++ packages/auth/src/authClients/supabase.ts
@@ -1,29 +1,31 @@
-import type {
-  GoTrueClient
-} from '@supabase/gotrue-js'
-// import type { default as GoTrue, User } from 'gotrue-js'
-
+import type { GoTrueClient } from '@supabase/gotrue-js'
+import type { default as Supabase } from '@supabase/supabase-js'
 import type { AuthClient } from './index'
 export type SupabaseUser = GoTrueClient
-
+export type Supabase = typeof Supabase
 export interface SupabaseClient extends GoTrueClient {
   auth: {
     user: () => any
-    signIn: (options: {email: string, password: string, remember?: boolean}) => any
+    signIn: (options: {
+      email: string
+      password: string
+      remember?: boolean
+    }) => any
     signOut: () => any
-    signUp: (options: {email: string, password: string, remember?: boolean}) => any
+    signUp: (options: {
+      email: string
+      password: string
+      remember?: boolean
+    }) => any
   }
 }
 export interface AuthClientSupabase extends AuthClient {
-  login(options: {
-    email: string
-    password: string
-  }): Promise<SupabaseUser>
+  login(options: { email: string; password: string }): Promise<GoTrueClient>
   signup(options: {
     email: string
     password: string
     remember?: boolean
-  }): Promise<SupabaseUser>
+  }): Promise<GoTrueClient>
   client: GoTrueClient
 }
 
@@ -31,11 +33,11 @@ export const supabase = (client: SupabaseClient): AuthClientSupabase => {
   return {
     type: 'supabase',
     client,
-    login: ({ email, password }) => client.auth.signIn({email, password}),
+    login: ({ email, password }) => client.auth.signIn({ email, password }),
     logout: () => {
       client.auth.signOut()
     },
-    signup: ({ email, password }) => client.auth.signUp({email, password}),
+    signup: ({ email, password }) => client.auth.signUp({ email, password }),
     getToken: async () => {
       const supabaseJson = localStorage.getItem('supabase.auth.token')
       const supabaseData = supabaseJson ? JSON.parse(supabaseJson) : null
