d593b5dc755f8ee8ab8b5741f947afc8e2283c68
dom <dominic.saadi@hey.com>
2020-12-07 23:54:43 -0800

    feat: use the project's babel config

--- packages/cli/src/commands/console.js
+++ packages/cli/src/commands/console.js
@@ -1,34 +1,33 @@
 import path from 'path'
 import repl from 'repl'
 
+import babelRequireHook from '@babel/register'
+
 import { getPaths } from 'src/lib'
 
-/**
- * Console is less technical and more Railsy
- */
 export const command = 'console'
-/**
- * @todo
- * Appropriate alias?
- */
 export const aliases = ['c']
+export const description = 'Launch an interactive Redwood shell (experimental)'
 
-/**
- * @todo
- * Should we make this more console related?
- * I.e. does using the word REPL here contradict our efforts to be less technical?
- */
-export const description = 'Start the Redwood REPL'
+const paths = getPaths().api
 
 const mapDBToContext = (ctx) => {
-  const { db } = require(path.join(getPaths().api.lib, 'db'))
+  const { db } = require(path.join(paths.lib, 'db'))
   ctx.db = db
 }
 
 export const handler = () => {
   // Transpile on the fly
-  require('@babel/register')
+  babelRequireHook({
+    extends: path.join(paths.base, '.babelrc.js'),
+    extensions: ['.js', '.ts'],
+    only: [paths.base],
+    ignore: ['node_modules'],
+    cache: false,
+  })
+
   const r = repl.start()
-  // Just make the user's db (i.e. Prisma Client) available
+
+  // Make the project's db (i.e. Prisma Client) available
   mapDBToContext(r.context)
 }
