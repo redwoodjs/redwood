006a8d12d0c56ede6a702d76ca898d89c2c6e74f
Olivier Lance <olivier@techtrails.io>
2020-12-27 16:26:56 +0100

    test: only run .test.* files, event when they're in `__tests__` directories

--- packages/cli/jest.config.js
+++ packages/cli/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/*.test.[jt]s?(x)'],
+  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)', '**/*.test.[jt]s?(x)'],
   testPathIgnorePatterns: ['fixtures'],
   moduleNameMapper: {
     'src/(.*)': '<rootDir>/src/$1',
