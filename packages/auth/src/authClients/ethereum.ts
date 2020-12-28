e1af54c23ac2b113021a941071eb0eb861e6dbc5
Patrick Gallagher <Blockchainbuddha@gmail.com>
2020-12-28 18:28:05 -0500

    add ethereumAuthPackage

--- packages/auth/src/authClients/ethereum.ts
+++ packages/auth/src/authClients/ethereum.ts
@@ -1,17 +1,23 @@
 // import type { Ethereum, EthereumUser } from 'ethereumAuthClient'
-type Ethereum = any
-type EthereumUser = any
+export type Ethereum = any
+export type EthereumUser = any
 
-export type Ethereum = Ethereum
-export type EthereumUser = EthereumUser
+export interface EthereumUser {
+  address: string | null
+}
 
 import type { AuthClient } from './'
 
-export const ethereumAuth = (client: Ethereum): AuthClient => {
+export const ethereum = (client: Ethereum): AuthClient => {
   return {
     type: 'ethereum',
     client,
     login: async () => await client.login(),
+    signup: () => {
+      throw new Error(
+        `Ethereum auth does not support "signup". Please use "login" instead.`
+      )
+    },
     logout: async () => await client.logout(),
     getToken: async () => await client.getToken(),
     getUserMetadata: async () => await client.getUserMetadata(),
