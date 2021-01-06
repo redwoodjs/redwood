75ef59fbddcecc9c86473b7c6aa59731e6bb6f05
Rob Cameron <rob.cameron@fastmail.com>
2021-01-06 10:30:08 -0800

    Revert "Explicitly call window on default mockCurrentUser()"
    This reverts commit 424ca60eaa8cba5d93338a75bbfe39c8ef2001c3.

--- packages/core/src/storybook/StorybookProvider.tsx
+++ packages/core/src/storybook/StorybookProvider.tsx
@@ -36,7 +36,7 @@ export const StorybookProvider: React.FunctionComponent<{
   }
 
   // default to a non-existent user at the beginning of each story
-  window.mockCurrentUser(null)
+  mockCurrentUser(null)
 
   return <MockProviders>{storyFn()}</MockProviders>
 }
