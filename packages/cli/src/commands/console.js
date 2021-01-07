fcd64f1476547e2b3f70a0709fc56d0c589380f0
dom <dominic.saadi@hey.com>
2021-01-06 21:08:53 -0800

    refactor: reorganize console command files

--- packages/cli/src/commands/console.js
+++ packages/cli/src/commands/console.js
@@ -5,7 +5,8 @@ export const aliases = ['c']
 export const description = 'Launch an interactive Redwood shell (experimental)'
 
 export const handler = () => {
-  execa.commandSync(`node --experimental-repl-await ${__dirname}/asyncRepl.js`, {
-    stdio: 'inherit',
-  })
+  execa.commandSync(
+    `node --experimental-repl-await ${__dirname}/console/asyncRepl.js`,
+    { stdio: 'inherit' }
+  )
 }
