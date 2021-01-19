200a01faa5c40a588fb6f702ff766775be8290db
patrick <blockchainbuddha@gmail.com>
2021-01-18 20:02:11 -0500

    Update decoder "env check" to match the supabase one

--- packages/api/src/auth/decoders/ethereum.ts
+++ packages/api/src/auth/decoders/ethereum.ts
@@ -1,20 +1,15 @@
 import jwt from 'jsonwebtoken'
 
 export const ethereum = (token: string) => {
-  if (process.env.NODE_ENV === 'production') {
-    if (!process.env.ETHEREUM_JWT_SECRET) {
-      throw new Error('`JWT_SECRET` env var is not set.')
-    }
+  if (!process.env.ETHEREUM_JWT_SECRET) {
+    console.error('ETHEREUM_JWT_SECRET env var is not set.')
+    throw new Error('`ETHEREUM_JWT_SECRET` env var is not set.')
+  }
 
-    try {
-      const secret = process.env.ETHEREUM_JWT_SECRET as string
-      return Promise.resolve(
-        jwt.verify(token, secret) as Record<string, unknown>
-      )
-    } catch (error) {
-      return Promise.reject(error)
-    }
-  } else {
-    return Promise.resolve(jwt.decode(token))
+  try {
+    const secret = process.env.ETHEREUM_JWT_SECRET as string
+    return Promise.resolve(jwt.verify(token, secret) as Record<string, unknown>)
+  } catch (error) {
+    return Promise.reject(error)
   }
 }
