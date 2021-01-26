6cd5d5ee4d6e92e24318c96d24674a55be1ba7e0
A. David Thyresson <dthyresson@gmail.com>
2021-01-26 04:37:38 -0500

    Sypport Supabase OAuth and Magic Link auth

--- packages/auth/src/authClients/supabase.ts
+++ packages/auth/src/authClients/supabase.ts
@@ -6,9 +6,17 @@ export type Supabase = SupabaseClient
 export type SupabaseUser = User
 
 export interface AuthClientSupabase extends AuthClient {
+  /**
+   * Log in an existing user, or login via a third-party provider.
+   * @param option The user login details.
+   * @param option.email The user's email address.
+   * @param option.password The user's password.
+   * @param option.provider One of the providers supported by GoTrue.
+   */
   login(options: {
-    email: string
-    password: string
+    email?: string | undefined
+    password?: string | undefined
+    provider?: Provider
   }): Promise<{
     data: Session | null
     user: User | null
@@ -17,6 +25,12 @@ export interface AuthClientSupabase extends AuthClient {
     error: Error | null
   }>
   logout(): Promise<{ error: Error | null }>
+  /**
+   * Creates a new user.
+   * @param options The user login details.
+   * @param options.email The user's email address.
+   * @param options.password The user's password.
+   */
   signup(options: {
     email: string
     password: string
@@ -32,13 +46,27 @@ export const supabase = (client: Supabase): AuthClientSupabase => {
   return {
     type: 'supabase',
     client,
-    login: ({ email, password }) => client.auth.signIn({ email, password }),
-    logout: () => client.auth.signOut(),
-    signup: ({ email, password }) => client.auth.signUp({ email, password }),
+    login: async ({ email, password, provider }) => {
+      if (email && !password) {
+        // magic link
+        return await client.auth.signIn({ email })
+      }
+      // email and password
+      if (email && password)
+        return await client.auth.signIn({ email, password })
+      // oauth, such as github, gitlab, etc.
+      if (provider) return await client.auth.signIn({ provider })
+      throw new Error(
+        `You must provide either an email or a third-party provider.`
+      )
+    },
+    logout: async () => await client.auth.signOut(),
+    signup: async ({ email, password }) =>
+      await client.auth.signUp({ email, password }),
     getToken: async () => {
       const currentSession = client.auth.session()
       return currentSession?.access_token || null
     },
-    getUserMetadata: async () => client.auth.user(),
+    getUserMetadata: async () => await client.auth.user(),
   }
 }
