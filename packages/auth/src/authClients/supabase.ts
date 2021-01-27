b92d59d5794d41b09ce29ec0fd73afc51511e3eb
A. David Thyresson <dthyresson@gmail.com>
2021-01-27 12:30:48 -0500

    Addresses request PR review changes.

--- packages/auth/src/authClients/supabase.ts
+++ packages/auth/src/authClients/supabase.ts
@@ -8,10 +8,10 @@ export type SupabaseUser = User
 export interface AuthClientSupabase extends AuthClient {
   /**
    * Log in an existing user, or login via a third-party provider.
-   * @param option The user login details.
-   * @param option.email The user's email address.
-   * @param option.password The user's password.
-   * @param option.provider One of the providers supported by GoTrue.
+   * @param options The user login details.
+   * @param options.email The user's email address.
+   * @param options.password The user's password.
+   * @param {'bitbucket' | 'github' | 'gitlab' | 'google' | 'azure'} options.provider One of the providers supported by GoTrue.
    */
   login(options: {
     email?: string | undefined
@@ -47,27 +47,38 @@ export const supabase = (client: Supabase): AuthClientSupabase => {
     type: 'supabase',
     client,
     login: async ({ email, password, provider }) => {
+      // magic link
       if (email && !password) {
-        // magic link
         return await client.auth.signIn({ email })
       }
       // email and password
-      if (email && password)
+      if (email && password) {
         return await client.auth.signIn({ email, password })
-      // oauth, such as github, gitlab, etc.
+      }
+      // oauth, such as github, gitlab, bitbucket, google, azure etc.
       if (provider) return await client.auth.signIn({ provider })
       throw new Error(
         `You must provide either an email or a third-party provider.`
       )
     },
-    logout: async () => await client.auth.signOut(),
-    signup: async ({ email, password }) =>
-      await client.auth.signUp({ email, password }),
+    logout: async () => {
+      return await client.auth.signOut()
+    },
+    signup: async ({ email, password }) => {
+      return await client.auth.signUp({ email, password })
+    },
+
     getToken: async () => {
       const currentSession = client.auth.session()
       return currentSession?.access_token || null
     },
-    getUserMetadata: async () => await client.auth.user(),
-    restoreAuthState: async () => await client.auth.refreshSession(),
+    getUserMetadata: async () => {
+      return await client.auth.user()
+    },
+    // restore authentication when an OAuth or magiclink callback
+    // redirects back to site with access token
+    restoreAuthState: async () => {
+      return await client.auth.refreshSession()
+    },
   }
 }
