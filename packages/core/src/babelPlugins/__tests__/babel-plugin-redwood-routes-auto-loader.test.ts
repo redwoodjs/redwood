ed3417df6bdc8854acde4b2a9a60abe5d0e00956
Daniel Choudhury <dannychoudhury@gmail.com>
2021-02-03 15:08:59 +0000

    Add tests for routes-auto-loader static mode

--- packages/core/src/babelPlugins/__tests__/babel-plugin-redwood-routes-auto-loader.test.ts
+++ packages/core/src/babelPlugins/__tests__/babel-plugin-redwood-routes-auto-loader.test.ts
@@ -54,7 +54,35 @@ describe('routes auto loader', () => {
     pluginOptions: {
       project,
     },
-    fixtures: path.join(__dirname, '__fixtures__/routes-auto-loader'),
+    fixtures: path.join(__dirname, '__fixtures__/routes-auto-loader/dynamic'),
+  })
+
+  afterAll(() => {
+    expect(mockWriteFileSync.mock.calls[0][0]).toContain(`routes.d.ts`)
+    expect(mockWriteFileSync.mock.calls[0][1]).toContain(`home: () => "/"`)
+    expect(mockWriteFileSync.mock.calls[1][0]).toContain(`index.d.ts`)
+    expect(mockWriteFileSync.mock.calls[1][1]).toContain(
+      `/// <reference path="./generated/routes.d.ts" />`
+    )
+    jest.clearAllMocks()
+  })
+})
+
+describe('routes auto loader useStaticImports', () => {
+  const exampleTodoPath = path.resolve(
+    __dirname,
+    '../../../../../__fixtures__/example-todo-main'
+  )
+  const project = getProject(exampleTodoPath)
+
+  pluginTester({
+    plugin,
+    pluginName: 'babel-plugin-redwood-routes-auto-loader',
+    pluginOptions: {
+      project,
+      useStaticImports: true,
+    },
+    fixtures: path.join(__dirname, '__fixtures__/routes-auto-loader/static'),
   })
 
   afterAll(() => {
