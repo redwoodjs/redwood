769c9af4f417e4e3fa9f01a4f19f6e73e579786f
Peter Pistorius <peter.pistorius@gmail.com>
2021-02-04 20:17:22 +0200

    Remove old db commands and replace them with a deprecation notice.

--- packages/cli/src/commands/db.js
+++ packages/cli/src/commands/db.js
@@ -1,15 +1,38 @@
-export const command = 'db <command>'
-export const aliases = ['database']
-export const description = 'Database tools'
 import terminalLink from 'terminal-link'
 
-export const builder = (yargs) =>
-  yargs
-    .commandDir('./dbCommands')
-    .demandCommand()
-    .epilogue(
-      `Also see the ${terminalLink(
-        'Redwood CLI Reference',
-        'https://redwoodjs.com/reference/command-line-interface#db'
-      )}`
-    )
+import c from 'src/lib/colors'
+
+export const command = 'db [...commands]'
+
+export const builder = () => {
+  const argv = process.argv.slice(2)
+
+  console.log(c.warning('Deprecation notice:'))
+  console.log(
+    `The ${c.bold(
+      'rw ' + argv.join(' ')
+    )} command is now deprecated, please use ${c.bold(
+      `rw prisma ` + (argv[1] || '\b')
+    )} instead.`
+  )
+
+  switch (argv[1]) {
+    case 'up':
+    case 'down':
+    case 'save':
+      console.log()
+      console.log(
+        `Migrations are now available via the ${c.bold(
+          'rw prisma migrate'
+        )} command.`
+      )
+      console.log()
+      console.log(
+        `View the ${terminalLink(
+          'migration docs',
+          'https://www.prisma.io/docs/reference/api-reference/command-reference#prisma-migrate-preview'
+        )}`
+      )
+      break
+  }
+}
