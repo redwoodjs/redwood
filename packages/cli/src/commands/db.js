a80d797f5d2926c247f51df438e23be90b25111c
David S Price <thedavid@thedavidprice.com>
2021-02-04 21:17:18 -0800

    db command deprecation help text

--- packages/cli/src/commands/db.js
+++ packages/cli/src/commands/db.js
@@ -4,35 +4,70 @@ import c from 'src/lib/colors'
 
 export const command = 'db [...commands]'
 
+// TO DO: Description
+
 export const builder = () => {
+  // yargs.help(false)
   const argv = process.argv.slice(2)
 
-  console.log(c.warning('Deprecation notice:'))
-  console.log(
-    `The ${c.bold(
-      'rw ' + argv.join(' ')
-    )} command is now deprecated, please use ${c.bold(
-      `rw prisma ` + (argv[1] || '\b')
-    )} instead.`
-  )
+  const deprecationMessage = (newCommand, fullLine = false) => {
+    try {
+      console.log(c.warning('\n' + 'WARNING: deprecated command'))
+      if (fullLine) {
+        console.log(newCommand)
+      } else {
+        console.log(
+          'Please use the new command: ' +
+            c.green(`yarn rw prisma ${newCommand} \n`)
+        )
+      }
+
+      console.log(
+        `See the ${terminalLink(
+          'Redwood CLI Reference',
+          'https://redwoodjs.com/docs/cli-commands#prisma'
+        )} \n`
+      )
+    } catch (e) {
+      console.log(c.error(e.message))
+    }
+  }
 
   switch (argv[1]) {
+    case 'help':
+      deprecationMessage(
+        "'yarn rw db' commands are deprecated \n" +
+          "See 'yarn rw prisma --help' for new commands  \n",
+        true
+      )
+      break
     case 'up':
+      deprecationMessage('migrate dev')
+      break
     case 'down':
+      deprecationMessage(
+        `Prisma Migrate no longer supports down migrations \n \n` +
+          `See possible alternative ${terminalLink(
+            'Migration Flows',
+            'https://www.prisma.io/docs/concepts/components/prisma-migrate/prisma-migrate-flows'
+          )} \n`,
+        true
+      )
+      break
+    case 'generate':
+      deprecationMessage('generate')
+      break
+    case 'introspect':
+      deprecationMessage('introspect')
+      break
+    case 'seed':
+      deprecationMessage('db seed')
+      break
+    case 'studio':
+      deprecationMessage('studio')
+      break
     case 'save':
-      console.log()
-      console.log(
-        `Migrations are now available via the ${c.bold(
-          'rw prisma migrate'
-        )} command.`
-      )
-      console.log()
-      console.log(
-        `View the ${terminalLink(
-          'migration docs',
-          'https://www.prisma.io/docs/reference/api-reference/command-reference#prisma-migrate-preview'
-        )}`
-      )
+      deprecationMessage('migrate dev')
       break
   }
 }
