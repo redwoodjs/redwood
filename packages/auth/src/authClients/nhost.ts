a9ca0b19cd081fae14b3bce65748e7f0b62d5696
Nuno Pato <nunopato@gmail.com>
2021-02-11 17:02:37 -0100

    Add AuthClientNhost that implements AuthClient

--- packages/auth/src/authClients/nhost.ts
+++ packages/auth/src/authClients/nhost.ts
@@ -1,9 +1,42 @@
-import type { NhostClient, User } from 'nhost-js-sdk'
+import type { Session, NhostClient, User } from 'nhost-js-sdk'
 
 export type Nhost = NhostClient
 export type NhostUser = User
 
 import { AuthClient } from './'
+export interface AuthClientNhost extends AuthClient {
+  login(options: {
+    email?: string
+    password?: string
+    provider?: string
+  }): Promise<{
+    session: Session | null
+    user: NhostUser | null
+    mfa?: {
+      ticket: string
+    }
+  }>
+  logout(): Promise<void>
+  signup(options: {
+    email: string
+    password: string
+    registrationOptions?: {
+      userData?: any
+      defaultRole?: string
+      allowedRoles?: string[]
+    }
+  }): Promise<{
+    session: Session | null
+    user: User | null
+  }>
+  getToken(): Promise<string | null>
+  getUserMetadata(): Promise<NhostUser | null>
+  restoreAuthState(): Promise<{
+    session: Session | null
+    user: NhostUser | null
+  }>
+  client: Nhost
+}
 
 export const nhost = (client: Nhost): AuthClient => {
   return {
@@ -26,8 +59,10 @@ export const nhost = (client: Nhost): AuthClient => {
       return await client.auth.logout()
     },
     signup: async ({ email, password }) => {
-      return await client.auth.register(email, password, {
-        userData: { display_name: email },
+      return await client.auth.register({
+        email,
+        password,
+        registrationOptions: { userData: { display_name: email } },
       })
     },
     getToken: async () => {
