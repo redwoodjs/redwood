8561d2b3af2d8a4b8a90e7599c96f9dc969107d6
Daniel Choudhury <dannychoudhury@gmail.com>
2021-02-24 18:15:23 +0000

    WIP: handle 404

--- packages/prerender/src/router.ts
+++ packages/prerender/src/router.ts
@@ -6,14 +6,14 @@ export const detectPrerenderRoutes = () => {
   const routes = rwProject.getRouter().routes
 
   const prerenderRoutes = routes
-    .filter((route) => !route.isNotFound) // ignore notFound page
     .filter((route) => !route.hasParameters) // ignore routes that take params
     .filter((route) => route.prerender) // only select routes with prerender prop
     .map((route) => ({
-      name: route.name,
-      path: route.path,
+      name: route.isNotFound ? '404' : route.name,
+      path: route.isNotFound ? '/404' : route.path,
       hasParams: route.hasParameters,
       id: route.id,
+      isNotFound: route.isNotFound,
       filePath: route.page?.filePath,
     }))
 
