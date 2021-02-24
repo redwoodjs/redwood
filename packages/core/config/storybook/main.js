66c2c4fbcaff56b9fcad3a08c60751a063dfd975
jtoar <dominic.saadi@hey.com>
2021-02-23 21:08:07 -0800

    feat: add toml toggle

--- packages/core/config/storybook/main.js
+++ packages/core/config/storybook/main.js
@@ -1,12 +1,14 @@
 const fs = require('fs')
 const path = require('path')
 const { merge } = require('webpack-merge')
-const { getPaths } = require('@redwoodjs/internal')
+const { getConfig, getPaths } = require('@redwoodjs/internal')
 const { getSharedPlugins } = require('../webpack.common')
+
+const config = getConfig()
 
 const baseConfig = {
   stories: ['../../../../web/src/**/*.stories.{tsx,jsx,js}'],
-  addons: ['@storybook/addon-a11y'],
+  addons: config.web.a11y ? ['@storybook/addon-a11y'] : undefined,
   webpackFinal: (sbConfig, { configType }) => {
     // configType is 'PRODUCTION' or 'DEVELOPMENT', why shout?
     const isEnvProduction = configType && configType.toLowerCase() === 'production'
