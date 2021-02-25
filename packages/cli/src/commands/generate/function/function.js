2ecd835d7446e242194c3dafa1cd1c43cca1221f
Ryan <jesselockard99@gmail.com>
2021-02-24 23:13:04 -0500

    refactored generate-function command to provide TS support

--- packages/cli/src/commands/generate/function/function.js
+++ packages/cli/src/commands/generate/function/function.js
@@ -3,41 +3,47 @@ import path from 'path'
 import camelcase from 'camelcase'
 import terminalLink from 'terminal-link'
 
-import { getPaths } from 'src/lib'
+import { getPaths, transformTSToJS } from 'src/lib'
 
+import { yargsDefaults } from '../../generate'
 import {
   templateForComponentFile,
   createYargsForComponentGeneration,
 } from '../helpers'
 
-export const files = async ({ name, ...rest }) => {
+export const files = ({ name, ...rest }) => {
+  // Taken from ../component; should be updated to take from the project's configuration
+  const isJavascript = rest.javascript && !rest.typescript
+
   const functionName = camelcase(name)
   const file = templateForComponentFile({
     name: functionName,
     componentName: functionName,
+    extension: isJavascript ? '.js' : '.ts',
     apiPathSection: 'functions',
     generator: 'function',
-    templatePath: 'function.js.template',
+    templatePath: 'function.ts.template',
     templateVars: { ...rest },
     outputPath: path.join(getPaths().api.functions, `${functionName}.js`),
   })
 
-  return { [file[0]]: file[1] }
+  // If --javascript=true convert TS->JS before returning
+  const template = isJavascript ? transformTSToJS(file[0], file[1]) : file[1]
+
+  return { [file[0]]: template }
 }
 
 export const description = 'Generate a Function'
 
+// This could be built using createYargsForComponentGeneration;
+// however, functions wouldn't have a `stories` option. createYargs...
+// should be reversed to provide `yargsDefaults` as the default configuration
+// and accept a configuration such as its CURRENT default to append onto a command.
 export const builder = (yargs) => {
   yargs
     .positional('name', {
       description: 'Name of the Function',
       type: 'string',
-    })
-    .option('force', {
-      alias: 'f',
-      default: false,
-      description: 'Overwrite existing files',
-      type: 'boolean',
     })
     .epilogue(
       `Also see the ${terminalLink(
@@ -45,6 +51,10 @@ export const builder = (yargs) => {
         'https://redwoodjs.com/reference/command-line-interface#generate-function'
       )}`
     )
+  // Add default options, includes '--typescript', '--javascript', '--force', ...
+  Object.entries(yargsDefaults).forEach(([option, config]) => {
+    yargs.option(option, config)
+  })
 }
 
 export const { command, handler } = createYargsForComponentGeneration({
