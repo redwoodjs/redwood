a58ff86a8b3e04e26b5239cb70a5ff4435f273ff
Peter Pistorius <peter.pistorius@gmail.com>
2021-03-06 15:58:33 +0200

    App.js to App.tsx.

---
+++ packages/create-redwood-app/template/web/src/App.tsx
@@ -0,0 +1,17 @@
+import { FatalErrorBoundary } from '@redwoodjs/web'
+import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
+
+import FatalErrorPage from 'src/pages/FatalErrorPage'
+import Routes from 'src/Routes'
+
+import './index.css'
+
+const App = () => (
+  <FatalErrorBoundary page={FatalErrorPage}>
+    <RedwoodApolloProvider>
+      <Routes />
+    </RedwoodApolloProvider>
+  </FatalErrorBoundary>
+)
+
+export default App
