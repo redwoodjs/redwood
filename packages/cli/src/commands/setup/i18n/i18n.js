0ace08deee2502aeb0290d62e338d964c766f01b
Simon Gagnon <simon.gagnonr@gmail.com>
2021-03-08 22:20:58 -0500

    fix "Module not found: Error: Can't resolve ..."

--- packages/cli/src/commands/setup/i18n/i18n.js
+++ packages/cli/src/commands/setup/i18n/i18n.js
@@ -66,6 +66,16 @@ export const handler = async ({ force }) => {
               ])
             },
           },
+          {
+            title: 'Sync yarn.lock and node_modules',
+            task: async () => {
+              /**
+               * Sync yarn.lock file and node_modules folder.
+               * Refer https://github.com/redwoodjs/redwood/issues/1301 for more details.
+               */
+              await execa('yarn', ['install', '--check-files'])
+            },
+          },
         ])
       },
     },
