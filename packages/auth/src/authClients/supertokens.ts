2a04b5aa76fdfb41a9937a7e6a1365e7414921ad
Rishabh Poddar <rishabh.poddar@gmail.com>
2021-03-10 13:24:57 +0530

    inital base impl

---
+++ packages/auth/src/authClients/supertokens.ts
@@ -0,0 +1,43 @@
+import sessions from "supertokens-auth-react/recipe/session";
+
+// TODO: remove use of emailpassword like this
+import emailpassowrd from "supertokens-auth-react/recipe/emailpassword";
+
+import { AuthClient } from './index';
+
+export interface SuperTokensUser {
+  userId: string,
+  jwtPayload: any
+}
+
+
+export const supertokens = (): AuthClient => {
+  return {
+    type: 'supertokens',
+    client: undefined,
+    restoreAuthState: async () => {
+      // TODO: anythng here?
+    },
+
+    // TODO: use lib's login with redirect function (need to create one)
+    login: async () => window.location.href = "/auth",
+
+    logout: () => emailpassowrd.signOut(),
+
+    // TODO: specifically go to sign up
+    signup: async () => window.location.href = "/auth",
+
+    getToken: async () => sessions.doesSessionExist() ? "" : null,
+
+    getUserMetadata: async (): Promise<SuperTokensUser | null> => {
+      if (sessions.doesSessionExist()) {
+        return {
+          userId: sessions.getUserId(),
+          jwtPayload: await sessions.getJWTPayloadSecurely()
+        }
+      } else {
+        return null;
+      }
+    },
+  }
+}
