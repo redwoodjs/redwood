31a9d9367c82082d1868224d650454213d7e40c1
Johan Eliasson <johan@eliasson.us>
2021-03-16 11:22:55 +0100

    Forcing change to see if linting kicks in

--- packages/api/src/auth/decoders/azureActiveDirectory.ts
+++ packages/api/src/auth/decoders/azureActiveDirectory.ts
@@ -10,7 +10,9 @@ const verifyAzureActiveDirectoryToken = (
       throw new Error('`AZURE_ACTIVE_DIRECTORY_AUTHORITY` env var is not set.')
     }
 
-    /** @docs: https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-protocols-oidc#sample-response */
+    /**
+     * @docs: https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-protocols-oidc#sample-response
+     */
     const client = jwksClient({
       jwksUri: `${AZURE_ACTIVE_DIRECTORY_AUTHORITY}/discovery/v2.0/keys`,
     })
