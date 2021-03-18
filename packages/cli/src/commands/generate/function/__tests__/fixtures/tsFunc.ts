508e29ed610e2039e72535066b83b5f6081ca4d5
Ryan Lockard <25166787+realStandal@users.noreply.github.com>
2021-03-18 12:00:21 -0400

    updated tsFunc fixture to include gen. comment

--- packages/cli/src/commands/generate/function/__tests__/fixtures/tsFunc.ts
+++ packages/cli/src/commands/generate/function/__tests__/fixtures/tsFunc.ts
@@ -1,5 +1,13 @@
 import type { APIGatewayEvent, Context } from 'aws-lambda'
 
+/**
+ * The handler function is your code that processes http request events.
+ * You can use return and throw to send a response or error, respectively.
+ * 
+ * @param { import('aws-lambda').APIGatewayEvent } event - an object which contains information from the invoker.
+ * @param { import('aws-lambda').Context } context - contains information about the invocation,
+ * function, and execution environment.
+ */
 export const handler = async (event: APIGatewayEvent, context: Context) => {
   return {
     statusCode: 200,
