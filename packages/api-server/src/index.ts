79d10d87996cd7ee93a413a0dec36d7cb2f1427e
Peter Pistorius <peter.pistorius@gmail.com>
2021-03-18 11:52:58 +0200

    Add deprecation message for "api-server."

--- packages/api-server/src/index.ts
+++ packages/api-server/src/index.ts
@@ -3,6 +3,14 @@ import yargs from 'yargs'
 
 import { http } from './http'
 
+if (process.argv0 === 'api-server') {
+  console.log()
+  console.warn(
+    '"api-server" is deprecated, please use "rw-api-server" instead.'
+  )
+  console.log()
+}
+
 const { port, socket } = yargs
   .option('port', { default: 8911, type: 'number', alias: 'p' })
   .option('socket', { type: 'string' }).argv
