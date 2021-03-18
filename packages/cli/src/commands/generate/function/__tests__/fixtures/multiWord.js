e9ffcbaa2548bb1b1b8a8bc894df89cc54d1bd1d
Ryan Lockard <jesselockard99@gmail.com>
2021-03-18 13:23:21 -0400

    there were spaces, now there are not

--- packages/cli/src/commands/generate/function/__tests__/fixtures/multiWord.js
+++ packages/cli/src/commands/generate/function/__tests__/fixtures/multiWord.js
@@ -1,7 +1,7 @@
 /**
  * The handler function is your code that processes http request events.
  * You can use return and throw to send a response or error, respectively.
- * 
+ *
  * @param { import('aws-lambda').APIGatewayEvent } event - an object which contains information from the invoker.
  * @param { import('aws-lambda').Context } context - contains information about the invocation,
  * function, and execution environment.
--- packages/cli/src/commands/generate/function/__tests__/fixtures/tsFunc.ts
+++ packages/cli/src/commands/generate/function/__tests__/fixtures/tsFunc.ts
@@ -3,7 +3,7 @@ import type { APIGatewayEvent, Context } from 'aws-lambda'
 /**
  * The handler function is your code that processes http request events.
  * You can use return and throw to send a response or error, respectively.
- * 
+ *
  * @param { import('aws-lambda').APIGatewayEvent } event - an object which contains information from the invoker.
  * @param { import('aws-lambda').Context } context - contains information about the invocation,
  * function, and execution environment.
