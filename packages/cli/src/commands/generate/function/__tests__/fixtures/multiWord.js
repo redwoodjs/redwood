fff838d5710c9a09ed98fbaf8155a87bb5677ddb
Ryan <jesselockard99@gmail.com>
2021-03-21 18:20:26 -0400

    updated function-gen fixtures to match template-comment

--- packages/cli/src/commands/generate/function/__tests__/fixtures/multiWord.js
+++ packages/cli/src/commands/generate/function/__tests__/fixtures/multiWord.js
@@ -2,8 +2,10 @@
  * The handler function is your code that processes http request events.
  * You can use return and throw to send a response or error, respectively.
  *
- * @param { import('aws-lambda').APIGatewayEvent } event - an object which contains information from the invoker.
- * @param { import('aws-lambda').Context } context - contains information about the invocation,
+ * @typedef { import('aws-lambda').APIGatewayEvent } APIGatewayEvent
+ * @typedef { import('aws-lambda').Context } Context
+ * @param { APIGatewayEvent } event - an object which contains information from the invoker.
+ * @param { Context } context - contains information about the invocation,
  * function, and execution environment.
  */
 export const handler = async (event, context) => {
--- packages/cli/src/commands/generate/function/__tests__/fixtures/tsFunc.ts
+++ packages/cli/src/commands/generate/function/__tests__/fixtures/tsFunc.ts
@@ -4,8 +4,10 @@ import type { APIGatewayEvent, Context } from 'aws-lambda'
  * The handler function is your code that processes http request events.
  * You can use return and throw to send a response or error, respectively.
  *
- * @param { import('aws-lambda').APIGatewayEvent } event - an object which contains information from the invoker.
- * @param { import('aws-lambda').Context } context - contains information about the invocation,
+ * @typedef { import('aws-lambda').APIGatewayEvent } APIGatewayEvent
+ * @typedef { import('aws-lambda').Context } Context
+ * @param { APIGatewayEvent } event - an object which contains information from the invoker.
+ * @param { Context } context - contains information about the invocation,
  * function, and execution environment.
  */
 export const handler = async (event: APIGatewayEvent, context: Context) => {
