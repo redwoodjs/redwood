ffe7cfdbb3b75b80a6f62fb5b6c575be73220709
Daniel Choudhury <dannychoudhury@gmail.com>
2021-03-22 16:59:38 +0000

    Fix router lint issue | Bump and pin @types/react to 17.0.3

--- packages/router/src/params.tsx
+++ packages/router/src/params.tsx
@@ -9,7 +9,9 @@ export interface ParamsContextProps {
 
 export const ParamsContext = createNamedContext<ParamsContextProps>('Params')
 
-export const ParamsProvider: React.FC<{}> = ({ children }) => {
+export const ParamsProvider: React.FC<Record<string, unknown>> = ({
+  children,
+}) => {
   const [params, setParams] = useState<Record<string, string> | undefined>(
     undefined
   )
