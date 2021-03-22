8a838985ab252e6f463fb880c87903881e1f1d0c
Ryan Lockard <25166787+realStandal@users.noreply.github.com>
2021-03-22 19:37:32 -0400

    TypeScript support for CLI function-generator (#1854)
    * refactored generate-helpers.test to use function.ts.template
    
    * converted function.js.template to TypeScript
    
    * added JS and TS options test-fixtures
    
    * converted function.test.js to TS, added tests for new options
    
    * refactored generate-function command to provide TS support
    
    * added lanauge-specific extension to file-outputPath
    
    * resolved --ts option test path misconfiguration
    
    * removed redundant comment, adjusted repetitive logic
    
    * updated tsFunc.ts test-fixture to match template
    
    * resolved ESLint warning
    
    * Added type-only import to template
    
    Co-authored-by: Tobbe Lundberg <tobbe@tlundberg.com>
    
    * Update packages/cli/src/commands/generate/function/templates/function.ts.template
    
    @param provided JS typings, @typedef did not; using VSCode on Ubuntu.
    
    Co-authored-by: Peter Pistorius <peter.pistorius@gmail.com>
    
    * updated tsFunc fixture to include gen. comment
    
    * updated jsFunc fixture to include gen. comment
    
    * comments for function-fixture multiWord.js
    
    * can't forget a comment for function-fixture multiWord.js
    
    I did, 4 times
    
    * there were spaces, now there are not
    
    * Update packages/cli/src/commands/generate/function/templates/function.ts.template
    
    Co-authored-by: Peter Pistorius <peter.pistorius@gmail.com>
    
    * updated formatting of function-generator template-comment
    
    * updated function-gen fixtures to match template-comment
    
    Co-authored-by: Tobbe Lundberg <tobbe@tlundberg.com>
    Co-authored-by: Peter Pistorius <peter.pistorius@gmail.com>

--- packages/cli/src/commands/generate/function/__tests__/fixtures/multiWord.js
+++ packages/cli/src/commands/generate/function/__tests__/fixtures/multiWord.js
@@ -1,3 +1,13 @@
+/**
+ * The handler function is your code that processes http request events.
+ * You can use return and throw to send a response or error, respectively.
+ *
+ * @typedef { import('aws-lambda').APIGatewayEvent } APIGatewayEvent
+ * @typedef { import('aws-lambda').Context } Context
+ * @param { APIGatewayEvent } event - an object which contains information from the invoker.
+ * @param { Context } context - contains information about the invocation,
+ * function, and execution environment.
+ */
 export const handler = async (event, context) => {
   return {
     statusCode: 200,
