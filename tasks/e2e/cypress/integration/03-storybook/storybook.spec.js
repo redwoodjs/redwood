a43d194142945c04bca7bce0c3c5f9aa7c1b98e5
David S Price <thedavid@thedavidprice.com>
2021-03-23 18:14:09 -0700

    success with two components!

--- tasks/e2e/cypress/integration/03-storybook/storybook.spec.js
+++ tasks/e2e/cypress/integration/03-storybook/storybook.spec.js
@@ -11,7 +11,7 @@ describe('Redwood Storybook Integration', () => {
       .its('code')
       .should('eq', 0)
   })
-
+  // TO DO fix Storybook build command to output to public/
   it('0.2 Serve Storybook Static Files with Dev Server', () => {
     cy.exec(
       `cd ${BASE_DIR}; mv web/storybook-static web/public/storybook-static`
@@ -20,7 +20,7 @@ describe('Redwood Storybook Integration', () => {
       .should('eq', 0)
   })
 
-  it('1. BlogPost Component', { baseUrl: null }, () => {
+  it('1. BlogPost Component', () => {
     cy.visit(
       `http://localhost:8910/storybook-static/iframe.html?id=components-blogpost--generated&viewMode=story`
     )
@@ -28,23 +28,23 @@ describe('Redwood Storybook Integration', () => {
     cy.get('#error-message').should('contain.text', 'Cannot read property')
   })
 
-  // it('2. BlogPostCell', () => {
-  //   cy.visit(
-  //     `${BASE_DIR}/web/storybook-static/iframe.html?id=cells-blogpostcell--loading&viewMode=story`
-  //   )
-  //   cy.get('#root div').should('contain.text', 'Loading')
-  //   cy.visit(
-  //     `${BASE_DIR}/web/storybook-static/iframe.html?id=cells-blogpostcell--empty&viewMode=story`
-  //   )
-  //   cy.get('#root div').should('contain.text', 'Empty')
-  //   cy.visit(
-  //     `${BASE_DIR}/web/storybook-static/iframe.html?id=cells-blogpostcell--failure&viewMode=story`
-  //   )
-  //   cy.get('#root div').should('contain.text', 'Error')
-  //   cy.visit(
-  //     `${BASE_DIR}/web/storybook-static/iframe.html?id=cells-blogpostcell--success&viewMode=story`
-  //   )
-  //   // TO DO: code mode to update mock
-  //   cy.get('#error-message').should('contain.text', 'Cannot read property')
-  // })
+  it('2. BlogPostCell', () => {
+    cy.visit(
+      `http://localhost:8910/storybook-static/iframe.html?id=cells-blogpostcell--loading&viewMode=story`
+    )
+    cy.get('#root div').should('contain.text', 'Loading')
+    cy.visit(
+      `http://localhost:8910/storybook-static/iframe.html?id=cells-blogpostcell--empty&viewMode=story`
+    )
+    cy.get('#root div').should('contain.text', 'Empty')
+    cy.visit(
+      `http://localhost:8910/storybook-static/iframe.html?id=cells-blogpostcell--failure&viewMode=story`
+    )
+    cy.get('#root div').should('contain.text', 'Error')
+    cy.visit(
+      `http://localhost:8910/storybook-static/iframe.html?id=cells-blogpostcell--success&viewMode=story`
+    )
+    // TO DO: code mode to update mock
+    cy.get('#error-message').should('contain.text', 'Cannot read property')
+  })
 })
