953625752ba0e4c9f4d669a7f4a9249c0e312fea
Peter Pistorius <peter.pistorius@gmail.com>
2021-03-29 22:10:01 +0200

    Make Set understand auth.

--- packages/router/src/Set.tsx
+++ packages/router/src/Set.tsx
@@ -1,6 +1,8 @@
 import React, { ReactElement, ReactNode, FunctionComponentElement } from 'react'
 
+import { Redirect } from './links'
 import { useLocation } from './location'
+import { usePrivate } from './private-context'
 import { isRoute } from './router'
 import { useRouterState } from './router-context'
 import { flattenAll, matchPath } from './util'
@@ -20,33 +22,48 @@ interface Props {
 export const Set: React.FC<Props> = ({ children, wrap }) => {
   const routerState = useRouterState()
   const location = useLocation()
-  const wrappers = Array.isArray(wrap) ? wrap : [wrap]
+  const { loading } = routerState.useAuth()
+  const { isPrivate, unauthorized, unauthenticated } = usePrivate()
 
+  const wrappers = Array.isArray(wrap) ? wrap : [wrap] // slap the wrappers in an array
   const flatChildArray = flattenAll(children)
+  const routes = flatChildArray
+    .filter(isRoute)
+    .filter((r) => typeof r.props.path !== 'undefined')
 
-  const matchingChildRoute = flatChildArray.some((child) => {
-    if (isRoute(child)) {
-      const { path } = child.props
+  for (const route of routes) {
+    const path = route.props.path as string
 
-      if (path) {
-        const { match } = matchPath(
-          path,
-          location.pathname,
-          routerState.paramTypes
-        )
+    const { match } = matchPath(path, location.pathname, routerState.paramTypes)
+    if (match) {
+      if (isPrivate && unauthorized()) {
+        if (loading) {
+          return route.props?.whileLoading?.() || null
+        } else {
+          const currentLocation =
+            global.location.pathname +
+            encodeURIComponent(global.location.search)
 
-        if (match) {
-          return true
+          const unauthenticatedRoute = routerState.routes.filter(
+            ({ name }) => unauthenticated === name
+          )[0]
+
+          return (
+            <Redirect
+              to={`${unauthenticatedRoute.path}?redirectTo=${currentLocation}`}
+            />
+          )
         }
       }
+
+      // Expand and nest the wrapped elements.
+      return (
+        wrappers.reduceRight<ReduceType>((acc, wrapper) => {
+          return React.createElement(wrapper, undefined, acc ? acc : children)
+        }, undefined) || null
+      )
     }
-
-    return false
-  })
-
-  return matchingChildRoute
-    ? wrappers.reduceRight<ReduceType>((acc, wrapper) => {
-        return React.createElement(wrapper, undefined, acc ? acc : children)
-      }, undefined) || null
-    : null
+  }
+  // No match, no render.
+  return null
 }
