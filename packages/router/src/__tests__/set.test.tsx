bd647d54fb2b857b7aa3d4e9f29fddb29ef9f7e0
Tobbe Lundberg <tobbe@tlundberg.com>
2021-04-01 15:57:07 +0200

    Set: Forward props to wrapper components (#2125)
    * Set: Forward props to wrapper components
    
    * Set: no `unknown` types

--- packages/router/src/__tests__/set.test.tsx
+++ packages/router/src/__tests__/set.test.tsx
@@ -90,3 +90,66 @@ test('wraps components in other components', async () => {
     </div>
   `)
 })
+
+test('passes props to wrappers', async () => {
+  interface Props {
+    propOne: string
+    propTwo: string
+  }
+
+  const PropWrapper: React.FC<Props> = ({ children, propOne, propTwo }) => (
+    <div>
+      <h1>Prop Wrapper</h1>
+      <p>1:{propOne}</p>
+      <p>2:{propTwo}</p>
+      {children}
+    </div>
+  )
+  const TestSet = () => (
+    <Router useAuth={window.__REDWOOD__USE_AUTH}>
+      <Set wrap={[PropWrapper, GlobalLayout]} propOne="une" propTwo="deux">
+        <Route path="/" page={ChildA} name="childa" />
+      </Set>
+    </Router>
+  )
+
+  const screen = render(<TestSet />)
+
+  await waitFor(() => screen.getByText('ChildA'))
+
+  expect(screen.container).toMatchInlineSnapshot(`
+    <div>
+      <div>
+        <h1>
+          Prop Wrapper
+        </h1>
+        <p>
+          1:
+          une
+        </p>
+        <p>
+          2:
+          deux
+        </p>
+        <div>
+          <h1>
+            Global Layout
+          </h1>
+          <h1>
+            ChildA
+          </h1>
+          <div
+            aria-atomic="true"
+            aria-live="assertive"
+            id="redwood-announcer"
+            role="alert"
+            style="position: absolute; top: 0px; width: 1px; height: 1px; padding: 0px; overflow: hidden; clip: rect(0px, 0px, 0px, 0px); white-space: nowrap; border: 0px;"
+          />
+          <footer>
+            This is a footer
+          </footer>
+        </div>
+      </div>
+    </div>
+  `)
+})
