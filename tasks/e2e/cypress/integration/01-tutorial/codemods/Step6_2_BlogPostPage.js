66352b45917328df9d26a11b5447e1f02b6536ca
David Price <thedavid@thedavidprice.com>
2021-04-06 00:34:18 -0700

    Update E2E Tutorial test: add Toast and <Set /> per Tutorial (#2164)
    * add Toast and <Set /> per Tutorial
    
    * end tutorial test on home page
    
    * add scaffold css checks; add toast message checks

--- tasks/e2e/cypress/integration/01-tutorial/codemods/Step6_2_BlogPostPage.js
+++ tasks/e2e/cypress/integration/01-tutorial/codemods/Step6_2_BlogPostPage.js
@@ -1,14 +1,11 @@
 export default `
 // web/src/pages/BlogPostPage/BlogPostPage.js
 
-import BlogLayout from 'src/layouts/BlogLayout'
 import BlogPostCell from 'src/components/BlogPostCell'
 
 const BlogPostPage = ({ id }) => {
   return (
-    <BlogLayout>
       <BlogPostCell id={id} />
-    </BlogLayout>
   )
 }
 
