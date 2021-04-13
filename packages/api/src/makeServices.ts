f06f5e5fbf81a09a6036e110d4f8ec14f812330e
Rob Cameron <rob.cameron@fastmail.com>
2021-04-13 11:34:07 -0700

    Builds out makeServices to use BeforeSpec

--- packages/api/src/makeServices.ts
+++ packages/api/src/makeServices.ts
@@ -1,9 +1,48 @@
+import { getConfig } from '../../internal/src/config'
+
+import BeforeSpec from './BeforeSpec'
 import { MakeServices } from './types'
 
-/**
- * This function is a stub for when we fully introduce the concept of
- * services.
- */
 export const makeServices: MakeServices = ({ services }) => {
-  return services
+  if (!getConfig().api.experimentalSecureServices) {
+    return services
+  }
+
+  const exportServices = {}
+
+  for (const [name, resolvers] of Object.entries(services)) {
+    if (!resolvers?.beforeResolver) {
+      throw new Error(
+        `Must define a 'beforeResolver()' in ${name.replaceAll(
+          '_',
+          '/'
+        )} service`
+      )
+    }
+
+    const spec = new BeforeSpec(Object.keys(resolvers))
+    resolvers.beforeResolver(spec)
+
+    const exportResolvers = {}
+
+    for (const [resolverName, resolverFunc] of Object.entries(resolvers)) {
+      if (resolverName === 'beforeResolver') {
+        continue
+      }
+
+      if (typeof resolverFunc === 'function') {
+        // wrap resolver function in spec verification
+        exportResolvers[resolverName] = (...args) => {
+          spec.verify(resolverName)
+          return resolverFunc(...args)
+        }
+      } else {
+        // resolver is just a sub-type
+        exportResolvers[resolverName] = resolverFunc
+      }
+    }
+    exportServices[name] = exportResolvers
+  }
+
+  return exportServices
 }
