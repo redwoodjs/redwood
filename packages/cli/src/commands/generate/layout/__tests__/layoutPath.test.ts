21cbfc5a0a7ee7c15ef8595b7b5e66750dfbe7a1
Francois Veauvy <veauvy.fr@gmail.com>
2021-04-16 23:24:24 +0200

    add tests for path and nested path layouts

---
+++ packages/cli/src/commands/generate/layout/__tests__/layoutPath.test.ts
@@ -0,0 +1,171 @@
+global.__dirname = __dirname
+import path from 'path'
+
+import { loadGeneratorFixture } from 'src/lib/test'
+
+import * as layout from '../layout'
+
+let singleWordDefaultFiles,
+  multiWordDefaultFiles,
+  javascriptFiles,
+  typescriptFiles,
+  withoutTestFiles,
+  withoutStoryFiles,
+  withSkipLinkFiles
+
+beforeAll(() => {
+  singleWordDefaultFiles = layout.files({ name: 'admin/app' })
+  multiWordDefaultFiles = layout.files({ name: 'admin/singlePage' })
+  javascriptFiles = layout.files({
+    name: 'admin/JavascriptPage',
+    javascript: true,
+  })
+  typescriptFiles = layout.files({
+    name: 'admin/TypescriptPage',
+    typescript: true,
+  })
+  withoutTestFiles = layout.files({
+    name: 'admin/withoutTests',
+    javascript: true,
+    tests: false,
+  })
+  withoutStoryFiles = layout.files({
+    name: 'admin/withoutStories',
+    javascript: true,
+    stories: false,
+  })
+  withSkipLinkFiles = layout.files({
+    name: 'admin/A11y',
+    skipLink: true,
+  })
+})
+
+test('returns exactly 3 files', () => {
+  expect(Object.keys(singleWordDefaultFiles).length).toEqual(3)
+})
+
+test('creates a single word layout component', () => {
+  expect(
+    singleWordDefaultFiles[
+      path.normalize(
+        '/path/to/project/web/src/layouts/admin/AppLayout/AppLayout.tsx'
+      )
+    ]
+  ).toEqual(loadGeneratorFixture('layout', 'singleWordLayout.tsx'))
+})
+
+test('creates a single word layout test', () => {
+  expect(
+    singleWordDefaultFiles[
+      path.normalize(
+        '/path/to/project/web/src/layouts/admin/AppLayout/AppLayout.test.tsx'
+      )
+    ]
+  ).toEqual(loadGeneratorFixture('layout', 'singleWordLayout.test.tsx'))
+})
+
+test('creates a single word layout stories', () => {
+  expect(
+    singleWordDefaultFiles[
+      path.normalize(
+        '/path/to/project/web/src/layouts/admin/AppLayout/AppLayout.stories.tsx'
+      )
+    ]
+  ).toEqual(loadGeneratorFixture('layout', 'singleWordLayout.stories.tsx'))
+})
+
+test('creates a multi word layout component', () => {
+  expect(
+    multiWordDefaultFiles[
+      path.normalize(
+        '/path/to/project/web/src/layouts/admin/SinglePageLayout/SinglePageLayout.tsx'
+      )
+    ]
+  ).toEqual(loadGeneratorFixture('layout', 'multiWordLayout.tsx'))
+})
+
+test('creates a multi word layout test', () => {
+  expect(
+    multiWordDefaultFiles[
+      path.normalize(
+        '/path/to/project/web/src/layouts/admin/SinglePageLayout/SinglePageLayout.test.tsx'
+      )
+    ]
+  ).toEqual(loadGeneratorFixture('layout', 'multiWordLayout.test.tsx'))
+})
+
+test('creates a multi word layout test', () => {
+  expect(
+    multiWordDefaultFiles[
+      path.normalize(
+        '/path/to/project/web/src/layouts/admin/SinglePageLayout/SinglePageLayout.stories.tsx'
+      )
+    ]
+  ).toEqual(loadGeneratorFixture('layout', 'multiWordLayout.stories.tsx'))
+})
+
+test('creates JS layout components if javacript = true', () => {
+  expect(
+    javascriptFiles[
+      path.normalize(
+        '/path/to/project/web/src/layouts/admin/JavascriptPageLayout/JavascriptPageLayout.js'
+      )
+    ]
+  ).not.toBeUndefined()
+  expect(
+    javascriptFiles[
+      path.normalize(
+        '/path/to/project/web/src/layouts/admin/JavascriptPageLayout/JavascriptPageLayout.test.js'
+      )
+    ]
+  ).not.toBeUndefined()
+})
+
+test('creates TS layout components if typescript = true', () => {
+  expect(
+    typescriptFiles[
+      path.normalize(
+        '/path/to/project/web/src/layouts/admin/TypescriptPageLayout/TypescriptPageLayout.tsx'
+      )
+    ]
+  ).not.toBeUndefined()
+  expect(
+    typescriptFiles[
+      path.normalize(
+        '/path/to/project/web/src/layouts/admin/TypescriptPageLayout/TypescriptPageLayout.test.tsx'
+      )
+    ]
+  ).not.toBeUndefined()
+})
+
+test("doesn't include storybook file when --stories is set to false", () => {
+  expect(Object.keys(withoutStoryFiles)).toEqual([
+    path.normalize(
+      '/path/to/project/web/src/layouts/admin/WithoutStoriesLayout/WithoutStoriesLayout.test.js'
+    ),
+    path.normalize(
+      '/path/to/project/web/src/layouts/admin/WithoutStoriesLayout/WithoutStoriesLayout.js'
+    ),
+  ])
+})
+
+test("doesn't include test file when --tests is set to false", () => {
+  expect(Object.keys(withoutTestFiles)).toEqual([
+    path.normalize(
+      '/path/to/project/web/src/layouts/admin/WithoutTestsLayout/WithoutTestsLayout.stories.js'
+    ),
+    path.normalize(
+      '/path/to/project/web/src/layouts/admin/WithoutTestsLayout/WithoutTestsLayout.js'
+    ),
+  ])
+})
+
+test.only('includes skip link when --skipLink is set to true', () => {
+  expect(
+    withSkipLinkFiles[
+      path.normalize(
+        '/path/to/project/web/src/layouts/admin/A11yLayout/A11yLayout.tsx'
+      )
+    ]
+  ).toEqual(loadGeneratorFixture('layout', 'withSkipLinkLayout.tsx'))
+})
