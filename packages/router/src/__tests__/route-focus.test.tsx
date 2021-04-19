d09d56ffa5fd0918b0be83a3519669f59161f21b
jtoar <dominic.saadi@hey.com>
2021-04-19 15:25:33 -0700

    feat: add some tests for getFocus

---
+++ packages/router/src/__tests__/route-focus.test.tsx
@@ -0,0 +1,198 @@
+import { render, waitFor } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
+import { Router, Route, routes, getFocus } from '../internal'
+import RouteFocus from '../route-focus'
+
+// SETUP
+const RouteFocusPage = () => (
+  <>
+    <RouteFocus>
+      <a>a link is a focusable element</a>
+    </RouteFocus>
+    <h1>Route Focus Page</h1>
+    <p></p>
+  </>
+)
+
+const NoRouteFocusPage = () => <h1>No Route Focus Page</h1>
+
+const RouteFocusNoChildren = () => (
+  <>
+    <RouteFocus></RouteFocus>
+    <h1>Route Focus No Children Page</h1>
+    <p></p>
+  </>
+)
+
+const RouteFocusTextNodePage = () => (
+  <>
+    <RouteFocus>some text</RouteFocus>
+    <h1>Route Focus Text Node Page </h1>
+    <p></p>
+  </>
+)
+
+const RouteFocusNegativeTabIndexPage = () => (
+  <>
+    <RouteFocus>
+      <p>my tabindex is -1</p>
+    </RouteFocus>
+    <h1>Route Focus Negative Tab Index Page </h1>
+    <p></p>
+  </>
+)
+
+beforeEach(() => {
+  window.history.pushState({}, null, '/')
+  Object.keys(routes).forEach((key) => delete routes[key])
+})
+
+// test('getFocus gets a focusable elements and ignores', async () => {
+//   const TestRouter = () => (
+//     <Router>
+//       <Route path="/" page={RouteFocusPage} name="routeFocus" />
+//       <Route path="/noRouteFocus" page={NoRouteFocusPage} name="noRouteFocus" />
+//       <Route
+//         path="/routeFocusNoChild"
+//         page={RouteFocusNoChildren}
+//         name="routeFocusNoChild"
+//       />
+//       <Route
+//         path="/routeFocusTextNode"
+//         page={RouteFocusTextNodePage}
+//         name="routeFocusTextNode"
+//       />
+//       <Route
+//         path="/routeFocusNegativeTabIndex"
+//         page={RouteFocusNegativeTabIndexPage}
+//         name="routeFocusNegativeTabIndex"
+//       />
+//     </Router>
+//   )
+
+//   const screen = render(<TestRouter />)
+
+//   // should return the <a>
+//   await waitFor(() => {
+//     screen.getByText(/Route Focus Page/i)
+//     const routeFocus = getFocus()
+//     expect(routeFocus).toHaveTextContent('a link is a focusable element')
+//   })
+
+//   // should return null
+//   act(() => navigate(routes.noRouteFocus()))
+//   await waitFor(() => {
+//     screen.getByText(/No Route Focus Page/i)
+//     const routeFocus = getFocus()
+//     expect(routeFocus).toBeNull()
+//   })
+
+//   // should return null
+//   act(() => navigate(routes.routeFocusNoChild()))
+//   await waitFor(() => {
+//     screen.getByText(/Route Focus No Child Page/i)
+//     const routeFocus = getFocus()
+//     expect(routeFocus).toBeNull()
+//   })
+
+//   // should return null
+//   act(() => navigate(routes.routeFocusTextNode()))
+//   await waitFor(() => {
+//     screen.getByText(/Route Focus Text Node Page/i)
+//     const routeFocus = getFocus()
+//     expect(routeFocus).toBeNull()
+//   })
+
+//   // should return null
+//   act(() => navigate(routes.routeFocusNegativeTabIndex()))
+//   await waitFor(() => {
+//     screen.getByText(/Route Focus Negative Tab Index Page/i)
+//     const routeFocus = getFocus()
+//     expect(routeFocus).toBeNull()
+//   })
+// })
+
+test('getFocus returns a focusable element if RouteFocus has one', async () => {
+  const TestRouter = () => (
+    <Router>
+      <Route path="/" page={RouteFocusPage} name="routeFocus" />
+    </Router>
+  )
+
+  const screen = render(<TestRouter />)
+
+  await waitFor(() => {
+    screen.getByText(/Route Focus Page/i)
+    const routeFocus = getFocus()
+    expect(routeFocus).toHaveTextContent('a link is a focusable element')
+  })
+})
+
+test("getFocus returns null if there's no RouteFocus", async () => {
+  const TestRouter = () => (
+    <Router>
+      <Route path="/" page={NoRouteFocusPage} name="noRouteFocus" />
+    </Router>
+  )
+
+  const screen = render(<TestRouter />)
+
+  await waitFor(() => {
+    screen.getByText(/No Route Focus Page/i)
+    const routeFocus = getFocus()
+    expect(routeFocus).toBeNull()
+  })
+})
+
+test("getFocus returns null if RouteFocus doesn't have any children", async () => {
+  const TestRouter = () => (
+    <Router>
+      <Route path="/" page={RouteFocusNoChildren} name="routeFocusNoChildren" />
+    </Router>
+  )
+
+  const screen = render(<TestRouter />)
+
+  await waitFor(() => {
+    screen.getByText(/Route Focus No Children Page/i)
+    const routeFocus = getFocus()
+    expect(routeFocus).toBeNull()
+  })
+})
+
+test('getFocus returns null if RouteFocus has just a Text node', async () => {
+  const TestRouter = () => (
+    <Router>
+      <Route path="/" page={RouteFocusTextNodePage} name="routeFocusTextNode" />
+    </Router>
+  )
+
+  const screen = render(<TestRouter />)
+
+  await waitFor(() => {
+    screen.getByText(/Route Focus Text Node Page/i)
+    const routeFocus = getFocus()
+    expect(routeFocus).toBeNull()
+  })
+})
+
+test("getFocus returns null if RouteFocus's child isn't focusable", async () => {
+  const TestRouter = () => (
+    <Router>
+      <Route
+        path="/"
+        page={RouteFocusNegativeTabIndexPage}
+        name="routeFocusNegativeTabIndex"
+      />
+    </Router>
+  )
+
+  const screen = render(<TestRouter />)
+
+  await waitFor(() => {
+    screen.getByText(/Route Focus Negative Tab Index Page/i)
+    const routeFocus = getFocus()
+    expect(routeFocus).toBeNull()
+  })
+})
