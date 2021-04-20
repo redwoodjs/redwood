d8626ea4401c6ec63b2cc96d5a59037dee4676fb
Rob Cameron <rob.cameron@fastmail.com>
2021-04-20 14:03:57 -0700

    Updates service generator to add beforeResolver()

--- packages/cli/src/commands/generate/service/__tests__/fixtures/multiWord_crud.ts
+++ packages/cli/src/commands/generate/service/__tests__/fixtures/multiWord_crud.ts
@@ -1,5 +1,10 @@
 import { Prisma } from '@prisma/client'
 import { db } from 'src/lib/db'
+import { requireAuth } from 'src/lib/auth'
+
+export const beforeResolver = (rules) => {
+  rules.apply(requireAuth)
+}
 
 export const userProfiles = () => {
   return db.userProfile.findMany()
--- packages/cli/src/commands/generate/service/__tests__/fixtures/singleWord_crud.js
+++ packages/cli/src/commands/generate/service/__tests__/fixtures/singleWord_crud.js
@@ -1,4 +1,9 @@
 import { db } from 'src/lib/db'
+import { requireAuth } from 'src/lib/auth'
+
+export const beforeResolver = (rules) => {
+  rules.apply(requireAuth)
+}
 
 export const posts = () => {
   return db.post.findMany()
