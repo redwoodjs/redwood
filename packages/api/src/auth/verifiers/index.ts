ca4887562f2c244567547dc067ed5f87885dfd24
David Thyresson <dthyresson@gmail.com>
2021-04-22 11:49:23 -0400

    Verify signatures for secure webhooks and functions (#1843)
    * secureHandler verifies webhook signatures
    
    * Do not run verify as cannot acces env
    
    * Tests verify event. PR review updates.
    
    * WebhookVerificationError message is a string
    
    * Fixes Expected { after 'if' condition  curly
    
    * Refactor secureHandler for a verifier
    
    * Get to green on ecureHandler.test
    
    * WIP verifier refactoring
    
    * Refactor into verifiers with types
    
    * Tests and implement verifyWebhook
    
    * Refactors signature header into options
    
    * Adds tests for none, secretKey, sha256
    
    * Correct jsdocs
    
    * Adds ability to override event body payload
    
    * Refactor to use receiveAndVerify. Add tests. Support object payload for sha.
    
    * Add JWT verifier/Renames *Verifier & None to Skip
    
    * jwtVerifier wip
    
    * WIP
    
    * Add webhooks to api
    
    * Reorganize and add sha1 verifier
    
    * Adds more webhooks tests per verifier
    
    * Refactor createVerifier
    
    * Uses jwt.verify to check the issuer
    
    * Refactor circular dependency w separate types file
    
    * redwoodjs/api needs webhooks
    
    * Made VerifyOptions optional
    
    * Refactor WebhookVerifier interface w/ Danny
    
    * Cleanup jsdocs
    
    * Explains WEBHOOK_SECRET

---
+++ packages/api/src/auth/verifiers/index.ts
@@ -0,0 +1,23 @@
+import { verifierLookup } from './common'
+import type {
+  SupportedVerifierTypes,
+  VerifyOptions,
+  WebhookVerifier,
+} from './common'
+
+/**
+ * @param {SupportedVerifierTypes} type - What verification type methods used to sign and verify signatures
+ * @param {VerifyOptions} options - Options used to verify the signature based on verifiers requirements
+ */
+export const createVerifier = (
+  type: SupportedVerifierTypes,
+  options?: VerifyOptions | undefined
+): WebhookVerifier => {
+  if (options) {
+    return verifierLookup[type](options)
+  } else {
+    return verifierLookup[type]()
+  }
+}
+
+export * from './common'
---
+++ packages/api/src/auth/verifiers/secretKeyVerifier.ts
@@ -0,0 +1,43 @@
+import { WebhookVerificationError, DEFAULT_WEBHOOK_SECRET } from './common'
+import type { WebhookVerifier, VerifyOptions } from './common'
+
+export interface SecretKeyVerifier extends WebhookVerifier {
+  type: 'secretKeyVerifier'
+}
+
+/**
+ *
+ * Secret Key Verfifier
+ *
+ * Use when the payload is not signed, but rather authorized via a known secret key
+ *
+ */
+export const secretKeyVerifier = (
+  options?: VerifyOptions | undefined
+): SecretKeyVerifier => {
+  if (options) {
+    console.warn(
+      `With the SecretKeyVerifier verifier, VerifyOptions are ignored`
+    )
+  }
+  return {
+    sign: ({ secret }) => {
+      console.warn(
+        `With the SecretKeyVerifier verifier, your body isn't signed with a secret`
+      )
+      return secret
+    },
+    verify: ({ signature, secret = DEFAULT_WEBHOOK_SECRET }) => {
+      const verified = signature === secret
+
+      if (!verified) {
+        throw new WebhookVerificationError()
+      }
+
+      return verified
+    },
+    type: 'secretKeyVerifier',
+  }
+}
+
+export default secretKeyVerifier
