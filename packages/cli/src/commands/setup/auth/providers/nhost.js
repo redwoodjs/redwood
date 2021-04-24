9d6ce5ea07ced381d0cbe092a5d8619c2b72d9ed
A. David Thyresson <dthyresson@gmail.com>
2021-04-24 18:41:30 -0400

    Added required NHOST_JWT_SECRET to Nhost setup

--- packages/cli/src/commands/setup/auth/providers/nhost.js
+++ packages/cli/src/commands/setup/auth/providers/nhost.js
@@ -18,5 +18,5 @@ export const apiPackages = []
 
 // any notes to print out when the job is done
 export const notes = [
-  "You will need to add your project's backend URL (NHOST_BACKEND_URL) to your .env file.",
+  "You will need to add your project's backend URL (NHOST_BACKEND_URL) and JWT Key Secret (NHOST_JWT_SECRET) to your .env file.",
 ]
