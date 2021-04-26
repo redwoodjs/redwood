14c9dc89130c2f2919a8b07c8202fc29a715c4dc
A. David Thyresson <dthyresson@gmail.com>
2021-04-26 10:45:13 -0400

    Removes sub and iss from Nhost decoder

--- packages/api/src/auth/decoders/nhost.ts
+++ packages/api/src/auth/decoders/nhost.ts
@@ -3,7 +3,6 @@ import jwt from 'jsonwebtoken'
 export const nhost = async (token: string) => {
   const CLAIMS_NAMESPACE =
     process.env.NHOST_CLAIMS_NAMESPACE || 'https://hasura.io/jwt/claims'
-  const SUB_CLAIM = process.env.NHOST_SUB_CLAIM || 'x-hasura-user-id'
   const ROLES_CLAIM = process.env.NHOST_ROLES_CLAIM || 'x-hasura-allowed-roles'
 
   if (!process.env.NHOST_JWT_SECRET) {
@@ -16,10 +15,9 @@ export const nhost = async (token: string) => {
     const decoded = (await jwt.verify(token, secret)) as Record<string, unknown>
 
     const claims = decoded[CLAIMS_NAMESPACE] as Record<string, unknown>
-    const sub = claims[SUB_CLAIM]
     const roles = claims[ROLES_CLAIM]
 
-    return { ...decoded, sub, iss: 'nhost', roles }
+    return { ...decoded, roles }
   } catch (error) {
     throw new Error(error)
   }
