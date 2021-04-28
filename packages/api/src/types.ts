30aec820a31283e54f4bfce9cfc215facc75fc34
Rob Cameron <cannikin@fastmail.com>
2021-04-28 11:03:12 -0700

    Pass service args through to beforeResolver verification functions (#2363)
    * Pass service args through to beforeResolver verification functions
    
    * Fix types
    
    * Improve js docs
    
    Co-authored-by: Rob Cameron <rob.cameron@fastmail.com>
    Co-authored-by: Daniel Choudhury <dannychoudhury@gmail.com>

--- packages/api/src/types.ts
+++ packages/api/src/types.ts
@@ -21,7 +21,7 @@ export type MakeServices = (args: MakeServicesInterface) => ServicesCollection
 
 export type GraphQLTypeWithFields = GraphQLObjectType | GraphQLInterfaceType
 
-export type RuleValidator = (name: string) => any
+export type RuleValidator = (name: string, ...inputs: Array<unknown>) => void
 export type ValidatorCollection = {
   validators: Array<RuleValidator>
   skippable: boolean
@@ -43,9 +43,26 @@ export type RuleOptions =
     }
 
 export interface BeforeResolverSpecType {
+  /**
+   * @param  {RuleValidator|Array<RuleValidator>} functions - Function or Array of Functions that validates whether service function is allowed to run. Should Throw if not.
+   * @param {RuleOptions} [options]  - Optionally pass to selectively apply rule to specific service functions
+   */
   add: (
     functions: RuleValidator | Array<RuleValidator>,
     options?: RuleOptions
   ) => void
+  /**
+   *
+   * @param {RuleValidator|Array<RuleValidator>} functions - Function or Array of Functions that validates whether service function is allowed to run. Should Throw if not.
+   * @param {RuleOptions} [options]  - Optionally pass to selectively skip rule for specific service functions
+   *
+   * @example <caption>Skip all rules</caption>
+   * rules.skip()
+   * @example <caption>Skip single rule</caption>
+   * rules.skip(requireAuth)
+   * @example <caption>Skip single rule, for a single service function</caption>
+   * rules.skip(requireAuth, {only: 'posts'})
+   *
+   * */
   skip: (...args: SkipArgs) => void
 }
