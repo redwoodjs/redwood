6a04d5baf68b66ba6c1e53a8e00b5b92d4ca31de
Peter Pistorius <peter.pistorius@gmail.com>
2021-05-21 10:37:10 +0200

    Fix test.

--- packages/structure/src/model/__tests__/model.test.ts
+++ packages/structure/src/model/__tests__/model.test.ts
@@ -76,7 +76,7 @@ describe('Cells', () => {
   it('Can get the operation name of the QUERY', () => {
     const projectRoot = getFixtureDir('example-todo-main')
     const project = new RWProject({ projectRoot, host: new DefaultHost() })
-    const cell = project.cells.find((x) => x.uri.endsWith('TodoListCell.js'))
+    const cell = project.cells.find((x) => x.uri.endsWith('TodoListCell.tsx'))
     expect(cell.queryOperationName).toMatch('TodoListCell_GetTodos')
   })
 
