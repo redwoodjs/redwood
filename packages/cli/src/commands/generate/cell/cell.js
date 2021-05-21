90759a266170d91f15932a96e9c0b7b3c4689e80
A. David Thyresson <dthyresson@gmail.com>
2021-05-20 20:27:45 -0400

    Adds list option to cell generation

--- packages/cli/src/commands/generate/cell/cell.js
+++ packages/cli/src/commands/generate/cell/cell.js
@@ -1,5 +1,9 @@
 import pascalcase from 'pascalcase'
+import pluralize from 'pluralize'
 
+import { getSchema } from 'src/lib'
+
+import { yargsDefaults } from '../../generate'
 import {
   templateForComponentFile,
   createYargsForComponentGeneration,
@@ -31,48 +35,69 @@ const uniqueOperationName = async (name, index = 1) => {
   return uniqueOperationName(name, index + 1)
 }
 
+const getIdType = (model) => {
+  return model.fields.find((field) => field.isId)?.type
+}
+
 export const files = async ({
   name,
   typescript: generateTypescript,
   ...options
 }) => {
+  const model = await getSchema(pascalcase(pluralize.singular(name)))
+
+  let cellName = name
+  let templateNameSuffix = ''
+
+  if (options.list) {
+    console.log(`Making list cell ${options.list}`)
+    console.log(options)
+    console.log(options.list)
+    cellName = options.list
+    templateNameSuffix = 'List'
+  }
+
   // Create a unique operation name.
-  const operationName = await uniqueOperationName(name)
+  const operationName = await uniqueOperationName(cellName)
 
   const cellFile = templateForComponentFile({
-    name,
+    name: cellName,
     suffix: COMPONENT_SUFFIX,
     extension: generateTypescript ? '.tsx' : '.js',
     webPathSection: REDWOOD_WEB_PATH_NAME,
     generator: 'cell',
-    templatePath: 'cell.js.template',
+    templatePath: `cell${templateNameSuffix}.js.template`,
     templateVars: {
       operationName,
+      idType: getIdType(model),
     },
   })
+
   const testFile = templateForComponentFile({
-    name,
+    name: cellName,
     suffix: COMPONENT_SUFFIX,
     extension: generateTypescript ? '.test.tsx' : '.test.js',
     webPathSection: REDWOOD_WEB_PATH_NAME,
     generator: 'cell',
     templatePath: 'test.js.template',
   })
+
   const storiesFile = templateForComponentFile({
-    name,
+    name: cellName,
     suffix: COMPONENT_SUFFIX,
     extension: generateTypescript ? '.stories.tsx' : '.stories.js',
     webPathSection: REDWOOD_WEB_PATH_NAME,
     generator: 'cell',
     templatePath: 'stories.js.template',
   })
+
   const mockFile = templateForComponentFile({
-    name,
+    name: cellName,
     suffix: COMPONENT_SUFFIX,
     extension: generateTypescript ? '.mock.ts' : '.mock.js',
     webPathSection: REDWOOD_WEB_PATH_NAME,
     generator: 'cell',
-    templatePath: 'mock.js.template',
+    templatePath: `mock${templateNameSuffix}.js.template`,
   })
 
   const files = [cellFile]
@@ -106,4 +131,14 @@ export const { command, description, builder, handler } =
   createYargsForComponentGeneration({
     componentName: 'cell',
     filesFn: files,
+    optionsObj: {
+      ...yargsDefaults,
+      list: {
+        alias: 'l',
+        default: null,
+        description:
+          'Use when you want to generate a cell for a list of the model name.',
+        type: 'string',
+      },
+    },
   })
