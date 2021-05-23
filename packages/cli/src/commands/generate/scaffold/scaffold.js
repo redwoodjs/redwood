bf8fba65ba8f3d9e202402e14f4d5fa5b551a8db
Aditya Pandey <aditya@autospace.co>
2021-05-23 15:08:46 +0530

    Auto import Set in Routes

--- packages/cli/src/commands/generate/scaffold/scaffold.js
+++ packages/cli/src/commands/generate/scaffold/scaffold.js
@@ -49,6 +49,7 @@ const COMPONENTS = fs.readdirSync(
 const SCAFFOLD_STYLE_PATH = './scaffold.css'
 // Any assets that should not trigger an overwrite error and require a --force
 const SKIPPABLE_ASSETS = ['scaffold.css']
+const PACKAGE_SET = 'Set'
 
 const getIdType = (model) => {
   return model.fields.find((field) => field.isId)?.type
@@ -409,6 +410,31 @@ const addLayoutImport = ({ model: name, path: scaffoldPath = '' }) => {
   return 'Added layout import to Routes.{js,tsx}'
 }
 
+const addSetImport = () => {
+  const routesPath = getPaths().web.routes
+  const routesContent = readFile(routesPath).toString()
+  const [redwoodRouterImport, importStart, spacing, importContent, importEnd] =
+    routesContent.match(/(import {)(\s*)([^]*)(} from '@redwoodjs\/router')/)
+  const routerImports = importContent.replace(/\s/g, '').split(',')
+  if (routerImports.includes(PACKAGE_SET)) {
+    return 'Skipping Set import'
+  }
+  const newRoutesContent = routesContent.replace(
+    redwoodRouterImport,
+    importStart +
+      spacing +
+      PACKAGE_SET +
+      `,` +
+      spacing +
+      importContent +
+      importEnd
+  )
+
+  writeFile(routesPath, newRoutesContent, { overwriteExisting: true })
+
+  return 'Added Set import to Routes.{js,tsx}'
+}
+
 const addScaffoldImport = () => {
   const appJsPath = getPaths().web.app
   let appJsContents = readFile(appJsPath).toString()
@@ -464,6 +490,10 @@ const tasks = ({ model, path, force, tests, typescript, javascript }) => {
       {
         title: 'Adding layout import...',
         task: async () => addLayoutImport({ model, path }),
+      },
+      {
+        title: 'Adding set import...',
+        task: async () => addSetImport({ model, path }),
       },
       {
         title: 'Adding scaffold routes...',
