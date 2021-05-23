528d536ba3849d1c6a2cb9ab37584bcaf5da76b2
Peter Pistorius <peter.pistorius@gmail.com>
2021-05-23 23:02:31 +0200

    Export file finders and type-def generators.

--- packages/internal/src/index.ts
+++ packages/internal/src/index.ts
@@ -3,4 +3,5 @@ export * from './config'
 export * from './ts2js'
 export * from './dev'
 
-export { generateTypeDefs } from './generate/typeDefinitions'
+export * from './files'
+export * from './generate/typeDefinitions'
