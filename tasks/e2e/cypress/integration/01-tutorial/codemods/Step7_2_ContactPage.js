e1d59127bb8b23d7883b1d933ddfa943fb5e1c78
David Price <thedavid@thedavidprice.com>
2021-06-02 18:49:10 -0700

    E2E Cypress improvements — fix Cells and Forms, upgrade Cypress (#2707)
    * upgrade Cypress v7
    
    * improve Cells and Forms sections; clean exit

--- tasks/e2e/cypress/integration/01-tutorial/codemods/Step7_2_ContactPage.js
+++ tasks/e2e/cypress/integration/01-tutorial/codemods/Step7_2_ContactPage.js
@@ -8,87 +8,54 @@ import {
   Submit,
   FieldError,
   Label,
-  FormError,
 } from '@redwoodjs/forms'
-import { useMutation } from '@redwoodjs/web'
-import { toast, Toaster } from '@redwoodjs/web/toast'
-import { useForm } from 'react-hook-form'
-
-const CREATE_CONTACT = gql\`
-  mutation CreateContactMutation($input: CreateContactInput!) {
-    createContact(input: $input) {
-      id
-    }
-  }
-\`
 
 const ContactPage = () => {
-  const formMethods = useForm()
-
-  const [create, { loading, error }] = useMutation(CREATE_CONTACT, {
-    onCompleted: () => {
-      toast.success('Thank you for your submission!')
-      formMethods.reset()
-    },
-  })
-
   const onSubmit = (data) => {
-    create({ variables: { input: data } })
     console.log(data)
   }
 
   return (
-    <>
-      <Toaster />
-      <Form
-        onSubmit={onSubmit}
-        validation={{ mode: 'onBlur' }}
-        error={error}
-        formMethods={formMethods}
-      >
-        <FormError
-          error={error}
-          wrapperStyle={{ color: 'red', backgroundColor: 'lavenderblush' }}
-        />
-        <Label name="name" errorClassName="error">
-          Name
-        </Label>
-        <TextField
-          name="name"
-          validation={{ required: true }}
-          errorClassName="error"
-        />
-        <FieldError name="name" className="error" />
-
-        <Label name="email" errorClassName="error">
-          Email
-        </Label>
-        <TextField
-          name="email"
-          validation={{
-            required: true,
-            pattern: {
-              value: /[^@]+@[^.]+\..+/,
-              message: 'Please enter a valid email address',
-            },
-          }}
-          errorClassName="error"
-        />
-        <FieldError name="email" className="error" />
-
-        <Label name="message" errorClassName="error">
-          Message
-        </Label>
-        <TextAreaField
-          name="message"
-          validation={{ required: true }}
-          errorClassName="error"
-        />
-        <FieldError name="message" className="error" />
-
-        <Submit disabled={loading}>Save</Submit>
-      </Form>
-    </>
+    <Form
+      id="tutorial-form"
+      onSubmit={onSubmit}
+      validation={{ mode: 'onBlur' }}
+    >
+      <Label name="name" errorClassName="error">
+        Name
+      </Label>
+      <TextField
+        name="name"
+        validation={{ required: true }}
+        errorClassName="error"
+      />
+      <FieldError name="name" className="error" />
+      <Label name="email" errorClassName="error">
+        Email
+      </Label>
+      <TextField
+        name="email"
+        validation={{
+          required: true,
+          pattern: {
+            value: /[^@]+@[^.]+\..+/,
+            message: 'Please enter a valid email address',
+          },
+        }}
+        errorClassName="error"
+      />
+      <FieldError name="email" className="error" />
+      <Label name="message" errorClassName="error">
+        Message
+      </Label>
+      <TextAreaField
+        name="message"
+        validation={{ required: true }}
+        errorClassName="error"
+      />
+      <FieldError name="message" className="error" />
+      <Submit>Save</Submit>
+    </Form>
   )
 }
 
