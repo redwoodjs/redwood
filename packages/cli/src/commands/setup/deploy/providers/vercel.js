eb4ade030b2eb521042a4b2d74ed87ca7a744e6a
Daniel Choudhury <dannychoudhury@gmail.com>
2021-06-07 17:37:03 +0100

    Add vercel routes config for `setup deploy vercel`

--- packages/cli/src/commands/setup/deploy/providers/vercel.js
+++ packages/cli/src/commands/setup/deploy/providers/vercel.js
@@ -1,4 +1,23 @@
+import path from 'path'
+
+import { getPaths } from 'src/lib'
+
 export const apiProxyPath = '/api'
+
+const VERCEL_CONFIG = {
+  // spa-redirect, if you prerender, vercel knows to use filesystem first
+  rewrites: [{ source: '/(.*)', destination: '/index.html' }],
+  cleanUrls: true,
+  trailingSlash: false,
+}
+
+// any files to create
+export const files = [
+  {
+    path: path.join(getPaths().base, 'vercel.json'),
+    content: JSON.stringify(VERCEL_CONFIG, undefined, 2), // format it neatly
+  },
+]
 
 // any notes to print out when the job is done
 export const notes = [
