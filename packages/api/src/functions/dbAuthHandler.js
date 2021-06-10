8c088e2eb4b446b68bd4583ae0f520197fef7936
Rob Cameron <rob.cameron@fastmail.com>
2021-06-10 11:46:51 -0700

    Converting dbAuthHandler to class, first pass complete

--- packages/api/src/functions/dbAuthHandler.js
+++ packages/api/src/functions/dbAuthHandler.js
@@ -1,35 +1,79 @@
 import CryptoJS from 'crypto-js'
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebt_en'
 import { v4 as uuidv4 } from 'uuid'
 
 import * as DbAuthError from './dbAuthErrors'
 
-// maps the auth functions to their required HTTP verb for access
-const METHOD_VERBS = {
-  login: 'POST',
-  logout: 'POST',
-  signup: 'POST',
-  currentUser: 'GET',
-  getToken: 'GET',
-}
-
 export const DbAuthHandler = class {
+  // class constant: maps the auth functions to their required HTTP verb for access
+  static get VERBS() {
+    return {
+      login: 'POST',
+      logout: 'POST',
+      signup: 'POST',
+      currentUser: 'GET',
+      getToken: 'GET',
+    }
+  }
+
+  // class constant: all the attributes of the cookie other than the value itself
+  static get COOKIE_META() {
+    return [
+      `Path=/`,
+      `Domain=${process.env.SELF_HOST.split('//')[1].split(':')[0]}`,
+      'HttpOnly',
+      'SameSite=Strict',
+      'Secure',
+    ]
+  }
+
+  // default to epoch when we want to expire
+  static get PAST_EXPIRES_DATE() {
+    return new Date(1970, 0, 1).toUTCString()
+  }
+
+  // generate a new token (standard UUID)
+  static get CSRF_TOKEN() {
+    return uuidv4()
+  }
+
+  // convert to the UTC datetime string that's required for cookies
+  get _futureExpiresDate() {
+    let futureDate = new Date()
+    futureDate.setSeconds(futureDate.getSeconds() + this.options.loginExpires)
+    return futureDate.toUTCString()
+  }
+
+  // returns the Set-Cookie header to mark the cookie as expired ("deletes" the session)
+  get _deleteSessionHeader() {
+    return {
+      'Set-Cookie': [
+        'session=',
+        ...this._cookieAttributes({ expires: 'now' }),
+      ].join(';'),
+    }
+  }
+
   constructor(event, context, options = {}) {
     try {
       this.event = event
       this.context = context
       this.options = options
       this.db = this.options.db
+      this.dbAccessor = this.db[this.options.authModelAccessor]
       this.headerCsrfToken = this.event.headers['x-csrf-token']
       this.hasInvalidSession = false
 
-      const [session, csrfToken] = this.decryptSession()
+      const [session, csrfToken] = this._decryptSession()
       this.session = session
       this.sessionCsrfToken = csrfToken
     } catch (e) {
-      // if session can't be decrypted, log out user immediately
+      // if session can't be decrypted, keep track so we can log them out when
+      // the auth method is called
       if (e instanceof DbAuthError.SessionDecryptionError) {
         this.hasInvalidSession = true
+      } else {
+        throw e
       }
     }
   }
@@ -40,116 +84,79 @@ export const DbAuthHandler = class {
     // if there was a problem decryption the session, just return the logout
     // response immediately
     if (this.hasInvalidSession) {
-      return this.ok(...this.logoutResponse())
+      return this._ok(...this._logoutResponse())
     }
 
     try {
       // figure out which auth function is trying to be called
-      const method = this.getAuthMethod(this.event)
+      const method = this._getAuthMethod(this.event)
 
       // return a 404 if the auth method doesn't exist or the request didn't
       // use the required HTTP verb
-      if (!this[method] || this.event.httpMethod !== METHOD_VERBS[method]) {
-        return this.notFound()
+      if (
+        !this[method] ||
+        this.event.httpMethod !== DbAuthHandler.VERBS[method]
+      ) {
+        return this._notFound()
       }
 
       // call whatever auth method was requested and return the body and headers
       const [body, headers, { statusCode = 200 }] = await this[method]()
 
-      return this.ok(body, headers, { statusCode })
+      return this._ok(body, headers, { statusCode })
     } catch (e) {
       if (e instanceof DbAuthError.WrongVerbError) {
-        return this.notFound()
+        return this._notFound()
       } else {
-        return this.badRequest(e.message)
+        return this._badRequest(e.message)
       }
     }
   }
 
-  // all the attributes of the cookie other than the value itself
-  cookieMeta() {
-    return [
-      `Path=/`,
-      `Domain=${process.env.SELF_HOST.split('//')[1].split(':')[0]}`,
-      'HttpOnly',
-      'SameSite=Strict',
-      'Secure',
-    ]
-  }
-
-  // convert to the UTC datetime string that's required for cookies
-  futureExpiresDate() {
-    let futureDate = new Date()
-    futureDate.setSeconds(futureDate.getSeconds() + global.options.loginExpires)
-
-    return futureDate.toUTCString()
-  }
-
-  // default to epoch when we want to expire
-  pastExpiresDate() {
-    return new Date(1970, 0, 1).toUTCString()
-  }
-
-  // returns the proper prisma db accessor based on the model name, like `db.user`
-  dbAccessor() {
-    return global.db[global.options.authModelAccessor]
-  }
-
   // returns the Set-Cookie header to be returned in the request (effectively creates the session)
-  createSessionHeader(data, csrfToken) {
+  _createSessionHeader(data, csrfToken) {
     const session = JSON.stringify(data) + ';' + csrfToken
     const encrypted = CryptoJS.AES.encrypt(session, process.env.SESSION_SECRET)
     const cookie = [
       `session=${encrypted.toString()}`,
-      ...cookieAttributes({ expires: 'future' }),
+      ...this._cookieAttributes({ expires: 'future' }),
     ].join(';')
 
     return { 'Set-Cookie': cookie }
-  }
-
-  // returns the Set-Cookie header to mark the cookie as expired ("deletes" the session)
-  deleteSessionHeader() {
-    return {
-      'Set-Cookie': ['session=', ...cookieAttributes({ expires: 'now' })].join(
-        ';'
-      ),
-    }
   }
 
   // returns all the cookie attributes in an array with the proper expiration date
   //
   // pass the argument `expires` set to "now" to get the attributes needed to expire
   // the session, any other string will (or even null) will return the attributes to
-  // expire the session at the `futureExpiresDate`
-  cookieAttributes(options = { expires: 'future' }) {
-    const meta = JSON.parse(JSON.stringify(cookieMeta()))
+  // expire the session at the `FUTURE_EXPIRES_DATE`
+  _cookieAttributes(options = { expires: 'future' }) {
+    const meta = JSON.parse(JSON.stringify(DbAuthHandler.COOKIE_META))
     const date =
-      options.expires === 'now' ? pastExpiresDate() : futureExpiresDate()
+      options.expires === 'now'
+        ? DbAuthHandler.PAST_EXPIRES_DATE
+        : this._futureExpiresDate
     meta.push(`Expires=${date}`)
 
     return meta
   }
 
-  generateCsrfToken() {
-    return uuidv4()
-  }
-
   // checks the CSRF token in the header against the CSRF token in the session and
   // throw an error if they are not the same (not used yet)
-  validateCsrf() {
-    if (global.sessionCsrfToken !== global.headerCsrfToken) {
+  _validateCsrf() {
+    if (this.sessionCsrfToken !== this.headerCsrfToken) {
       throw new DbAuthError.CsrfTokenMismatchError()
     }
     return true
   }
 
   // returns the actual value of the session cookie
-  getSession() {
-    if (typeof global.event.headers.cookie === 'undefined') {
+  _getSession() {
+    if (typeof this.event.headers.cookie === 'undefined') {
       return null
     }
 
-    const cookies = global.event.headers.cookie.split(';')
+    const cookies = this.event.headers.cookie.split(';')
     const sessionCookie = cookies.find((cook) => {
       return cook.split('=')[0].trim() === 'session'
     })
@@ -162,8 +169,8 @@ export const DbAuthHandler = class {
   }
 
   // decrypts the session cookie and returns an array: [data, csrf]
-  decryptSession() {
-    const session = getSession()
+  _decryptSession() {
+    const session = this._getSession()
     if (!session) {
       return []
     }
@@ -183,7 +190,7 @@ export const DbAuthHandler = class {
   }
 
   // verifies that a username and password are correct, and returns the user if so
-  async verifyUser(username, password) {
+  async _verifyUser(username, password) {
     // do we have all the query params we need to check the user?
     if (
       !username ||
@@ -195,8 +202,8 @@ export const DbAuthHandler = class {
     }
 
     // does user exist?
-    const user = await dbAccessor().findUnique({
-      where: { [global.options.authFields.username]: username },
+    const user = await this.dbAccessor.findUnique({
+      where: { [this.options.authFields.username]: username },
     })
 
     if (!user) {
@@ -204,36 +211,23 @@ export const DbAuthHandler = class {
     }
 
     // is password correct?
-    const [hashedPassword, _salt] = hashPassword(
+    const [hashedPassword, _salt] = this._hashPassword(
       password,
-      user[global.options.authFields.salt]
+      user[this.options.authFields.salt]
     )
-    if (hashedPassword === user[global.options.authFields.hashedPassword]) {
+    if (hashedPassword === user[this.options.authFields.hashedPassword]) {
       return user
     } else {
       throw new DbAuthError.IncorrectPasswordError()
     }
   }
 
-  // gets the user from the database and returns it as an object with
-  // `excludeUserFields` fields stripped out
-  async getCurrentUser() {
-    if (!global.session?.id) {
-      throw new DbAuthError.NotLoggedInError()
-    }
-
-    const user = await dbAccessor().findUnique({
-      where: { [global.options.authFields.id]: global.session?.id },
-    })
-
-    if (!user) {
-      throw new DbAuthError.UserNotFoundError()
-    }
-
+  // removes sensative properties from a given user object
+  _sanitizeUser(user) {
     const exclude = [
-      global.options.authFields.hashedPassword,
-      global.options.authFields.salt,
-      ...global.options.excludeUserFields,
+      this.options.authFields.hashedPassword,
+      this.options.authFields.salt,
+      ...this.options.excludeUserFields,
     ]
 
     const userArray = Object.entries(user)
@@ -243,17 +237,35 @@ export const DbAuthHandler = class {
     return Object.fromEntries(filteredUser)
   }
 
+  // gets the user from the database and returns it as an object with
+  // `excludeUserFields` fields stripped out
+  async _getCurrentUser() {
+    if (!this.session?.id) {
+      throw new DbAuthError.NotLoggedInError()
+    }
+
+    const user = await this.dbAccessor.findUnique({
+      where: { [this.options.authFields.id]: this.session?.id },
+    })
+
+    if (!user) {
+      throw new DbAuthError.UserNotFoundError()
+    }
+
+    return this._sanitizeUser(user)
+  }
+
   // creates and returns a user, first checking that the username/password
   // values pass validation
-  async createUser() {
+  async _createUser() {
     const { username, password, ...userAttributes } = JSON.parse(
-      global.event.body
+      this.event.body
     )
-    validateField('username', username)
-    validateField('password', password)
+    this._validateField('username', username)
+    this._validateField('password', password)
 
-    let user = await dbAccessor().findUnique({
-      where: { [global.options.authFields.username]: username },
+    let user = await this.dbAccessor.findUnique({
+      where: { [this.options.authFields.username]: username },
     })
     if (user) {
       throw new DbAuthError.DuplicateUsernameError(username)
@@ -261,8 +273,8 @@ export const DbAuthHandler = class {
 
     // if we get here everything is good, call the app's signup handler and let
     // them worry about scrubbing data and saving to the DB
-    const [hashedPassword, salt] = hashPassword(password)
-    const newUser = await global.options.signupHandler({
+    const [hashedPassword, salt] = this._hashPassword(password)
+    const newUser = await this.options.signupHandler({
       username,
       hashedPassword,
       salt,
@@ -274,7 +286,7 @@ export const DbAuthHandler = class {
 
   // hashes a password using either the given `salt` argument, or creates a new
   // salt and hashes using that. Either way, returns an array with [hash, salt]
-  hashPassword(text, salt) {
+  _hashPassword(text, salt) {
     const useSalt = salt || CryptoJS.lib.WordArray.random(128 / 8).toString()
 
     return [
@@ -284,19 +296,19 @@ export const DbAuthHandler = class {
   }
 
   // figure out which auth method we're trying to call
-  getAuthMethod() {
+  _getAuthMethod() {
     // first, try getting the method name out of the URL in the form of /.redwood/functions/auth/[methodName]
-    let methodName = global.event.path.split('/').pop()
+    let methodName = this.event.path.split('/').pop()
 
-    if (!methods[methodName]) {
+    if (!this[methodName]) {
       // next, try getting it from the query string instead, /.redwood/functions/auth?method=[methodName]
-      methodName = global.event.queryStringParameters.method
+      methodName = this.event.queryStringParameters.method
     }
 
-    if (!methods[methodName]) {
+    if (!this[methodName]) {
       // finally, try getting it from the body in JSON: { method: [methodName] }
       try {
-        methodName = JSON.parse(global.event.body).method
+        methodName = JSON.parse(this.event.body).method
       } catch (e) {
         // there's no body, or it's not JSON, `handler` will return a 404
       }
@@ -307,82 +319,82 @@ export const DbAuthHandler = class {
 
   // checks that a single field meets validation requirements and
   // currently checks for presense only
-  validateField(name, value) {
+  _validateField(name, value) {
     // check for presense
-    if (!value || value === '' || value.trim() === '') {
+    if (!value || value.trim() === '') {
       throw new DbAuthError.FieldRequiredError(name)
     } else {
       return true
     }
   }
 
-  logoutResponse(message) {
+  _logoutResponse(message) {
     return [
       message ? JSON.stringify({ message }) : '',
       {
-        ...deleteSessionHeader(),
+        ...this._deleteSessionHeader,
       },
     ]
   }
 
   async login() {
-    const { username, password } = JSON.parse(global.event.body)
-    const user = await verifyUser(username, password)
-    const sessionData = { id: user[global.options.authFields.id] }
+    const { username, password } = JSON.parse(this.event.body)
+    const user = await this._verifyUser(username, password)
+    const sessionData = { id: user[this.options.authFields.id] }
 
     // this needs to go into graphql somewhere so that each request makes a new CSRF token
     // and sets it in both the encrypted session and the x-csrf-token header
-    const csrfToken = generateCsrfToken()
+    const csrfToken = DbAuthHandler.CSRF_TOKEN
 
     return [
       sessionData,
       {
         'X-CSRF-Token': csrfToken,
-        ...createSessionHeader(sessionData, csrfToken),
+        ...this._createSessionHeader(sessionData, csrfToken),
       },
     ]
   }
 
   logout() {
-    return logoutResponse()
+    return this._logoutResponse()
   }
 
   async signup() {
     try {
-      const user = await createUser()
-      const sessionData = { id: user[global.options.authFields.id] }
-      const csrfToken = generateCsrfToken()
+      const user = await this._createUser()
+      const sessionData = { id: user[this.options.authFields.id] }
+      const csrfToken = DbAuthHandler.CSRF_TOKEN
 
       return [
         sessionData,
         {
           'X-CSRF-Token': csrfToken,
-          ...createSessionHeader(sessionData, csrfToken),
+          ...this._createSessionHeader(sessionData, csrfToken),
         },
         { statusCode: 201 },
       ]
     } catch (e) {
-      return logoutResponse(e.message)
+      return this._logoutResponse(e.message)
     }
   }
 
   // converts the currentUser data to a JWT. returns `null` if session is not present
   async getToken() {
     try {
-      const user = await getCurrentUser()
+      const user = await this._getCurrentUser()
       const token = jwt.sign(JSON.stringify(user), process.env.SESSION_SECRET)
 
       return [token]
     } catch (e) {
       if (e instanceof DbAuthError.NotLoggedInError) {
-        return logoutResponse()
+        return this._logoutResponse()
       } else {
-        return logoutResponse(e.message)
+        return this._logoutResponse(e.message)
       }
     }
   }
 
-  goodStatus(statusCode, body, headers = {}) {
+  _ok(body, headers = {}, { statusCode = 200 }) {
     return {
       statusCode,
       body,
@@ -390,17 +402,13 @@ export const DbAuthHandler = class {
     }
   }
 
-  ok(body, headers = {}, { statusCode }) {
-    return goodStatus(statusCode, body, headers)
-  }
-
-  notFound() {
+  _notFound() {
     return {
       statusCode: 404,
     }
   }
 
-  badRequest(message) {
+  _badRequest(message) {
     return {
       statusCode: 400,
       body: { message },
