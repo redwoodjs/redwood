7d9c7895fb6b61f19f1ff7a576612af5241e4182
Rob Cameron <rob.cameron@fastmail.com>
2021-06-11 15:37:24 -0700

    Convert auth method call to either querystring or body variable
    Some deploy targets (like Netlify) can't handle anything in the URL after the function name, and others might be similar

--- packages/auth/src/authClients/dbAuth.js
+++ packages/auth/src/authClients/dbAuth.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-undef */
-export const dbAuth = (client) => {
+export const dbAuth = () => {
   const getToken = async () => {
     const response = await fetch(
-      `${global.__REDWOOD__API_PROXY_PATH}/auth/getToken`
+      `${global.__REDWOOD__API_PROXY_PATH}/auth?method=getToken`
     )
     const token = await response.text()
     return token
@@ -10,33 +10,28 @@ export const dbAuth = (client) => {
 
   const login = async (attributes) => {
     const { username, password } = attributes
-    const response = await fetch(
-      `${global.__REDWOOD__API_PROXY_PATH}/auth/login`,
-      {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username, password }),
-      }
-    )
+    const response = await fetch(`${global.__REDWOOD__API_PROXY_PATH}/auth`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username, password, method: 'login' }),
+    })
     return await response.json()
   }
 
   const logout = async () => {
-    await fetch(`${global.__REDWOOD__API_PROXY_PATH}/auth/logout`, {
+    await fetch(`${global.__REDWOOD__API_PROXY_PATH}/auth`, {
       method: 'POST',
+      body: JSON.stringify({ method: 'logout' }),
     })
     return true
   }
 
   const signup = async (attributes) => {
-    const response = await fetch(
-      `${global.__REDWOOD__API_PROXY_PATH}/auth/signup`,
-      {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(attributes),
-      }
-    )
+    const response = await fetch(`${global.__REDWOOD__API_PROXY_PATH}/auth`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ method: 'signup', ...attributes }),
+    })
     return await response.json()
   }
 
