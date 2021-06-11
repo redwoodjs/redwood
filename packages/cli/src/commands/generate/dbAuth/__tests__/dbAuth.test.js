2a23d618aa587bbcd66363c986dbc2452956d176
Rob Cameron <rob.cameron@fastmail.com>
2021-06-11 16:52:59 -0700

    Adds placeholder tests for dbAuth generator

---
+++ packages/cli/src/commands/generate/dbAuth/__tests__/dbAuth.test.js
@@ -0,0 +1,7 @@
+describe('dbAuth', () => {
+  it('creates a login page', () => {})
+  it('creates a signup page', () => {})
+  it('add routes', () => {})
+  it('creates a scaffold CSS file', () => {})
+  it('adds a scaffold import', () => {})
+})
