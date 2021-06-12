ee4598911229c7a3c2353423bf8442bef24b95cb
Peter Pistorius <peter.pistorius@gmail.com>
2021-06-12 18:30:46 +0200

    Move prisma wrapper from builder to handler.

--- packages/cli/src/index.js
+++ packages/cli/src/index.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
+import fs from 'fs'
 import path from 'path'
 
 import { config } from 'dotenv-defaults'
 import yargs from 'yargs'
 
-import { getPaths, getConfig, getConfigPath } from '@redwoodjs/internal'
+import { getPaths, getConfigPath } from '@redwoodjs/internal'
 
 /**
  * The current working directory can be set via:
@@ -17,39 +18,44 @@ import { getPaths, getConfig, getConfigPath } from '@redwoodjs/internal'
  */
 const getCwdMiddleware = (argv) => {
   let configPath
+
   try {
     let cwd
     if (argv.cwd) {
       cwd = argv.cwd
+      // We delete the argument because it's not actually referenced in CLI,
+      // we use the `RWJS_CWD` env-var,
+      // and it conflicts with "forwarding" commands such as test and prisma.
+      delete argv.cwd
     } else if (process.env.RWJS_CWD) {
       cwd = process.env.RWJS_CWD
     } else {
-      cwd = getConfigPath()
+      cwd = path.dirname(getConfigPath())
     }
 
     configPath = path.resolve(process.cwd(), cwd, 'redwood.toml')
-    getConfig(configPath)
-    argv.cwd = cwd
+    if (!fs.existsSync(configPath)) {
+      throw new Error('Could not find `redwood.toml` config file.')
+    }
+
     process.env.RWJS_CWD = cwd
   } catch (e) {
     console.error()
-    console.error(
-      'Error: Redwood CLI could not find, or parse, your configuration file.'
-    )
-    console.error(`We expected '${configPath}'`)
+    console.error('Error: Redwood CLI could not find your config file.')
+    console.error(`Expected '${configPath}'`)
     console.error()
     console.error(`Did you run Redwood CLI in a RedwoodJS project?`)
     console.error(`Or specify an incorrect '--cwd' option?`)
     console.error()
-    process.exit()
+    process.exit(1)
   }
 }
 
 const loadDotEnvDefaultsMiddleware = () => {
   config({
     path: path.join(getPaths().base, '.env'),
-    encoding: 'utf8',
     defaults: path.join(getPaths().base, '.env.defaults'),
+    encoding: 'utf8',
   })
 }
 
