e8afb836a8466a4e39ca531a263a0416493ee171
A. David Thyresson <dthyresson@gmail.com>
2021-06-14 19:46:13 -0400

    Fix lint issue in graphql template

--- packages/create-redwood-app/template/api/src/functions/graphql.ts
+++ packages/create-redwood-app/template/api/src/functions/graphql.ts
@@ -6,7 +6,6 @@ import {
 
 import schemas from 'src/graphql/**/*.{js,ts}'
 import { db } from 'src/lib/db'
-
 import services from 'src/services/**/*.{js,ts}'
 
 export const handler = createGraphQLHandler({
