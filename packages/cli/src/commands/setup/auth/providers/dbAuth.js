1790ccda0dad6ebe30accaae67600960f8921feb
Rob Cameron <rob.cameron@fastmail.com>
2021-06-14 09:54:20 -0700

    Reword help text, use getPaths() to get real function path

--- packages/cli/src/commands/setup/auth/providers/dbAuth.js
+++ packages/cli/src/commands/setup/auth/providers/dbAuth.js
@@ -1,3 +1,5 @@
+import { getPaths } from '@redwoodjs/internal'
+
 import c from 'src/lib/colors'
 
 // the lines that need to be added to App.{js,tsx}
@@ -11,30 +13,34 @@ export const config = {
 // required packages to install
 export const webPackages = []
 export const apiPackages = []
+export const functionsPath = getPaths().api.functions.replace(
+  getPaths().base,
+  ''
+)
 
 // any notes to print out when the job is done
 export const notes = [
   `${c.warning('Done! But you have a little more work to do:')}\n`,
   'You will need to add a couple of fields to your User table in order',
-  'to store their hashed password and salt values:',
+  'to store a hashed password and salt:',
   '',
   '  model User {',
-  '    id              Int @id  @default(autoincrement())',
-  '    email           String   @unique',
-  '    hashedPassword  String   // <--- add this line',
-  '    salt            String   // <--- and this one',
+  '    id             Int @id @default(autoincrement())',
+  '    email          String  @unique',
+  '    hashedPassword String   // <─┐',
+  '    salt           String   // <─┴─ add these lines',
   '  }',
   '',
   'If you already have existing user records you will need to provide',
   'a default value or Prisma complains, so change those to:',
   '',
-  '  hashedPassword  String   @default("")',
-  '  salt            String   @default("")',
+  '  hashedPassword String @default("")',
+  '  salt           String @default("")',
   '',
   "You'll need to let Redwood know what field you're using for your",
   "users' `id` and `username` fields. In this case we're using `id` and",
   '`email`, so update those in the `authFields` config in',
-  '`api/src/functions/auth.js` (this is also the place to tell Redwood if',
+  `\`${functionsPath}/auth.js\` (this is also the place to tell Redwood if`,
   'you used a different name for the `hashedPassword` or `salt` fields):',
   '',
   '  authFields: {',
