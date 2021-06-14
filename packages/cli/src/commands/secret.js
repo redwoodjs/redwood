c28205f93ffeccf05f3aefba0cb0f198d5a3cf28
Rob Cameron <rob.cameron@fastmail.com>
2021-06-14 15:01:42 -0700

    Adds `yarn rw secret` command

---
+++ packages/cli/src/commands/secret.js
@@ -0,0 +1,25 @@
+import Listr from 'listr'
+import password from 'secure-random-password'
+import terminalLink from 'terminal-link'
+
+export const command = 'secret'
+export const description =
+  'Generates a secret string using a cryptographically-secure source of entropy'
+
+export const builder = (yargs) => yargs
+
+export const handler = () => {
+  const secret = password.randomPassword({
+    length: 64,
+    characters: [password.lower, password.upper, password.digits],
+  })
+
+  console.info('')
+  console.info(`  ${secret}`)
+  console.info('')
+  console.info(
+    "If you're using this with dbAuth, set a SESSION_SECRET environment variable to this value."
+  )
+  console.info('')
+  console.info('Keep it secret, keep it safe!')
+}
