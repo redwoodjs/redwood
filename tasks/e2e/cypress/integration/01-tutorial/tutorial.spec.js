a269a69d232fa0cc468d68e8c9714c92c72b49a1
Daniel Choudhury <dannychoudhury@gmail.com>
2021-06-16 18:21:25 +0100

    Undo some of the changes

--- tasks/e2e/cypress/integration/01-tutorial/tutorial.spec.js
+++ tasks/e2e/cypress/integration/01-tutorial/tutorial.spec.js
@@ -35,175 +35,152 @@ describe('The Redwood Tutorial - Golden path edition', () => {
   // TODO: https://redwoodjs.com/tutorial/saving-data
   // TODO: https://redwoodjs.com/tutorial/administration
 
-  // it('0. Starting Development', () => {
-  //   // https://redwoodjs.com/tutorial/installation-starting-development
-  //   cy.writeFile(path.join(BASE_DIR, 'web/src/Routes.js'), Step1_1_Routes)
-  //   cy.visit('http://localhost:8910')
-  //   cy.get('h1 > span').contains('Welcome to RedwoodJS!')
+  it('0. Starting Development', () => {
+    // https://redwoodjs.com/tutorial/installation-starting-development
+    cy.writeFile(path.join(BASE_DIR, 'web/src/Routes.js'), Step1_1_Routes)
+    cy.visit('http://localhost:8910')
+    cy.get('h1 > span').contains('Welcome to RedwoodJS!')
+  })
 
-  //   // Check that the project is linked correctly
-  //   const versionQuery = `query Redwood {
-  //     redwood {
-  //       version
-  //     }
-  //   }`
-  //   const cliPackageJson = require('../../../../../packages/cli/package.json')
+  it('1. Our First Page', () => {
+    //redwoodjs.com/tutorial/our-first-page
+    cy.visit('http://localhost:8910')
+    cy.exec(`cd ${BASE_DIR}; yarn redwood generate page home / --force`)
+    cy.get('h1').should('contain', 'HomePage')
+  })
 
-  //   cy.request({
-  //     method: 'POST',
-  //     url: '.redwood/functions/graphql',
-  //     body: {
-  //       query: versionQuery,
-  //     },
-  //     timeout: 3000,
-  //   }).then((response) => {
-  //     expect(response.status).to.eq(200)
-  //     expect(response.body.data.redwood).to.have.property(
-  //       'version',
-  //       cliPackageJson.version
-  //     )
-  //   })
-  // })
+  it('2. A Second Page and a Link', () => {
+    // https://redwoodjs.com/tutorial/a-second-page-and-a-link
+    cy.exec(`cd ${BASE_DIR}; yarn redwood generate page about --force`)
+    cy.writeFile(
+      path.join(BASE_DIR, 'web/src/pages/HomePage/HomePage.js'),
+      Step2_1_PagesHome
+    )
+    cy.contains('About').click()
+    cy.get('h1').should('contain', 'AboutPage')
+    cy.writeFile(
+      path.join(BASE_DIR, 'web/src/pages/AboutPage/AboutPage.js'),
+      Step2_2_PagesAbout
+    )
+    cy.get('h1').should('contain', 'Redwood Blog')
+    cy.contains('Return home').click()
+  })
 
-  // it('1. Our First Page', () => {
-  //   //redwoodjs.com/tutorial/our-first-page
-  //   cy.visit('http://localhost:8910')
-  //   cy.exec(`cd ${BASE_DIR}; yarn redwood generate page home / --force`)
-  //   cy.get('h1').should('contain', 'HomePage')
-  // })
+  it('3. Layouts', () => {
+    cy.exec(`cd ${BASE_DIR}; yarn redwood generate layout blog --force`)
+    cy.writeFile(
+      path.join(BASE_DIR, 'web/src/layouts/BlogLayout/BlogLayout.js'),
+      Step3_1_LayoutsBlog
+    )
+    cy.writeFile(path.join(BASE_DIR, 'web/src/Routes.js'), Step3_2_Routes)
+    cy.writeFile(
+      path.join(BASE_DIR, 'web/src/pages/HomePage/HomePage.js'),
+      Step3_3_PagesHome
+    )
+    cy.contains('Redwood Blog').click()
+    cy.get('main').should('contain', 'Home')
 
-  // it('2. A Second Page and a Link', () => {
-  //   // https://redwoodjs.com/tutorial/a-second-page-and-a-link
-  //   cy.exec(`cd ${BASE_DIR}; yarn redwood generate page about --force`)
-  //   cy.writeFile(
-  //     path.join(BASE_DIR, 'web/src/pages/HomePage/HomePage.js'),
-  //     Step2_1_PagesHome
-  //   )
-  //   cy.contains('About').click()
-  //   cy.get('h1').should('contain', 'AboutPage')
-  //   cy.writeFile(
-  //     path.join(BASE_DIR, 'web/src/pages/AboutPage/AboutPage.js'),
-  //     Step2_2_PagesAbout
-  //   )
-  //   cy.get('h1').should('contain', 'Redwood Blog')
-  //   cy.contains('Return home').click()
-  // })
+    cy.writeFile(
+      path.join(BASE_DIR, 'web/src/pages/AboutPage/AboutPage.js'),
+      Step3_4_PagesAbout
+    )
+    cy.contains('About').click()
+    cy.get('p').should(
+      'contain',
+      'This site was created to demonstrate my mastery of Redwood: Look on my works, ye mighty, and despair!'
+    )
+  })
 
-  // it('3. Layouts', () => {
-  //   cy.exec(`cd ${BASE_DIR}; yarn redwood generate layout blog --force`)
-  //   cy.writeFile(
-  //     path.join(BASE_DIR, 'web/src/layouts/BlogLayout/BlogLayout.js'),
-  //     Step3_1_LayoutsBlog
-  //   )
-  //   cy.writeFile(path.join(BASE_DIR, 'web/src/Routes.js'), Step3_2_Routes)
-  //   cy.writeFile(
-  //     path.join(BASE_DIR, 'web/src/pages/HomePage/HomePage.js'),
-  //     Step3_3_PagesHome
-  //   )
-  //   cy.contains('Redwood Blog').click()
-  //   cy.get('main').should('contain', 'Home')
+  it('4. Getting Dynamic', () => {
+    // https://redwoodjs.com/tutorial/getting-dynamic
+    cy.writeFile(path.join(BASE_DIR, 'api/db/schema.prisma'), Step4_1_DbSchema)
+    cy.exec(`rm ${BASE_DIR}/api/db/dev.db`, { failOnNonZeroExit: false })
+    // need to also handle case where Prisma Client be out of sync
+    cy.exec(
+      `cd ${BASE_DIR}; yarn rimraf ./api/db/migrations && yarn rw prisma migrate reset --skip-seed --force`
+    )
+    cy.exec(`cd ${BASE_DIR}; yarn rw prisma migrate dev`)
 
-  //   cy.writeFile(
-  //     path.join(BASE_DIR, 'web/src/pages/AboutPage/AboutPage.js'),
-  //     Step3_4_PagesAbout
-  //   )
-  //   cy.contains('About').click()
-  //   cy.get('p').should(
-  //     'contain',
-  //     'This site was created to demonstrate my mastery of Redwood: Look on my works, ye mighty, and despair!'
-  //   )
-  // })
+    cy.exec(`cd ${BASE_DIR}; yarn rw g scaffold post --force`)
 
-  // it('4. Getting Dynamic', () => {
-  //   // https://redwoodjs.com/tutorial/getting-dynamic
-  //   cy.writeFile(path.join(BASE_DIR, 'api/db/schema.prisma'), Step4_1_DbSchema)
-  //   cy.exec(`rm ${BASE_DIR}/api/db/dev.db`, { failOnNonZeroExit: false })
-  //   // need to also handle case where Prisma Client be out of sync
-  //   cy.exec(
-  //     `cd ${BASE_DIR}; yarn rimraf ./api/db/migrations && yarn rw prisma migrate reset --skip-seed --force`
-  //   )
-  //   cy.exec(`cd ${BASE_DIR}; yarn rw prisma migrate dev`)
+    cy.visit('http://localhost:8910/posts')
 
-  //   cy.exec(`cd ${BASE_DIR}; yarn rw g scaffold post --force`)
+    cy.get('h1').should('contain', 'Posts')
+    cy.get('a.rw-button.rw-button-green').should(
+      'have.css',
+      'background-color',
+      'rgb(72, 187, 120)'
+    )
+    cy.contains(' New Post').click()
+    cy.get('h2').should('contain', 'New Post')
 
-  //   cy.visit('http://localhost:8910/posts')
+    // SAVE
+    cy.get('input#title').type('First post')
+    cy.get('input#body').type('Hello world!')
+    //check scaffold css
+    cy.get('button.rw-button.rw-button-blue').should(
+      'have.css',
+      'background-color',
+      'rgb(49, 130, 206)'
+    )
+    cy.get('button').contains('Save').click()
 
-  //   cy.get('h1').should('contain', 'Posts')
-  //   cy.get('a.rw-button.rw-button-green').should(
-  //     'have.css',
-  //     'background-color',
-  //     'rgb(72, 187, 120)'
-  //   )
-  //   cy.contains(' New Post').click()
-  //   cy.get('h2').should('contain', 'New Post')
+    cy.contains('Post created')
 
-  //   // SAVE
-  //   cy.get('input#title').type('First post')
-  //   cy.get('input#body').type('Hello world!')
-  //   //check scaffold css
-  //   cy.get('button.rw-button.rw-button-blue').should(
-  //     'have.css',
-  //     'background-color',
-  //     'rgb(49, 130, 206)'
-  //   )
-  //   cy.get('button').contains('Save').click()
+    cy.contains('Loading...').should('not.exist')
+    //checks Toast messages
+    cy.get('div[role="status"]').contains('Post created')
 
-  //   cy.contains('Post created')
+    // EDIT
+    cy.contains('Edit').click()
+    cy.contains('Loading...').should('not.exist')
+    cy.get('h2').contains('Edit Post 1')
+    cy.get('input#body').clear().type('No, Margle the World!')
+    cy.get('button').contains('Save').click()
+    cy.get('td').contains('No, Margle the World!')
+    cy.get('div[role="status"]').contains('Post updated')
 
-  //   cy.contains('Loading...').should('not.exist')
-  //   //checks Toast messages
-  //   cy.get('div[role="status"]').contains('Post created')
+    cy.contains('Post updated')
 
-  //   // EDIT
-  //   cy.contains('Edit').click()
-  //   cy.contains('Loading...').should('not.exist')
-  //   cy.get('h2').contains('Edit Post 1')
-  //   cy.get('input#body').clear().type('No, Margle the World!')
-  //   cy.get('button').contains('Save').click()
-  //   cy.get('td').contains('No, Margle the World!')
-  //   cy.get('div[role="status"]').contains('Post updated')
+    // DELETE
+    cy.contains('Delete').click()
 
-  //   cy.contains('Post updated')
+    // No more posts, so it should be in the empty state.
+    cy.contains('Post deleted')
+    cy.get('div[role="status"]').contains('Post deleted')
 
-  //   // DELETE
-  //   cy.contains('Delete').click()
+    cy.contains('Create one?').click()
+    cy.get('input#title').type('Second post')
+    cy.get('input#body').type('Hello world!')
+    cy.get('button').contains('Save').click()
+  })
 
-  //   // No more posts, so it should be in the empty state.
-  //   cy.contains('Post deleted')
-  //   cy.get('div[role="status"]').contains('Post deleted')
+  it('5. Cells', () => {
+    cy.exec(`cd ${BASE_DIR}; yarn rw g cell BlogPosts --force`)
+    cy.writeFile(
+      path.join(BASE_DIR, 'web/src/components/BlogPostsCell/BlogPostsCell.js'),
+      Step5_1_ComponentsCellBlogPost
+    )
+    cy.writeFile(
+      path.join(
+        BASE_DIR,
+        'web/src/components/BlogPostsCell/BlogPostsCell.test.js'
+      ),
+      Step5_2_ComponentsCellBlogPostTest
+    )
+    cy.writeFile(
+      path.join(BASE_DIR, 'web/src/pages/HomePage/HomePage.js'),
+      Step5_3_PagesHome
+    )
+    cy.visit('http://localhost:8910/posts/2') // adding step for pause
+    cy.visit('http://localhost:8910/')
 
-  //   cy.contains('Create one?').click()
-  //   cy.get('input#title').type('Second post')
-  //   cy.get('input#body').type('Hello world!')
-  //   cy.get('button').contains('Save').click()
-  // })
-
-  // it('5. Cells', () => {
-  //   cy.exec(`cd ${BASE_DIR}; yarn rw g cell BlogPosts --force`)
-  //   cy.writeFile(
-  //     path.join(BASE_DIR, 'web/src/components/BlogPostsCell/BlogPostsCell.js'),
-  //     Step5_1_ComponentsCellBlogPost
-  //   )
-  //   cy.writeFile(
-  //     path.join(
-  //       BASE_DIR,
-  //       'web/src/components/BlogPostsCell/BlogPostsCell.test.js'
-  //     ),
-  //     Step5_2_ComponentsCellBlogPostTest
-  //   )
-  //   cy.writeFile(
-  //     path.join(BASE_DIR, 'web/src/pages/HomePage/HomePage.js'),
-  //     Step5_3_PagesHome
-  //   )
-  //   cy.visit('http://localhost:8910/posts/2') // adding step for pause
-  //   cy.visit('http://localhost:8910/')
-
-  //   cy.get('main').should(
-  //     'contain',
-  //     // [{"title":"Second post","body":"Hello world!","__typename":"Post"}]
-  //     '"body":"Hello world!"'
-  //   )
-  // })
+    cy.get('main').should(
+      'contain',
+      // [{"title":"Second post","body":"Hello world!","__typename":"Post"}]
+      '"body":"Hello world!"'
+    )
+  })
 
   it('6. Routing Params', () => {
     // https://redwoodjs.com/tutorial/routing-params
@@ -268,35 +245,35 @@ describe('The Redwood Tutorial - Golden path edition', () => {
     cy.get('main').should('contain', 'foo bar')
   })
 
-  // it("7. Everyone's Favorite Thing to Build: Forms", () => {
-  //   // https://redwoodjs.com/tutorial/everyone-s-favorite-thing-to-build-forms
-  //   cy.exec(`cd ${BASE_DIR}; yarn rw g page contact --force`)
-  //   cy.writeFile(
-  //     path.join(BASE_DIR, 'web/src/layouts/BlogLayout/BlogLayout.js'),
-  //     Step7_1_BlogLayout
-  //   )
-  //   cy.writeFile(
-  //     path.join(BASE_DIR, 'web/src/pages/ContactPage/ContactPage.js'),
-  //     Step7_2_ContactPage
-  //   )
-  //   cy.writeFile(path.join(BASE_DIR, 'web/src/index.css'), Step7_3_Css)
-  //   cy.writeFile(path.join(BASE_DIR, 'web/src/Routes.js'), Step7_4_Routes)
+  it("7. Everyone's Favorite Thing to Build: Forms", () => {
+    // https://redwoodjs.com/tutorial/everyone-s-favorite-thing-to-build-forms
+    cy.exec(`cd ${BASE_DIR}; yarn rw g page contact --force`)
+    cy.writeFile(
+      path.join(BASE_DIR, 'web/src/layouts/BlogLayout/BlogLayout.js'),
+      Step7_1_BlogLayout
+    )
+    cy.writeFile(
+      path.join(BASE_DIR, 'web/src/pages/ContactPage/ContactPage.js'),
+      Step7_2_ContactPage
+    )
+    cy.writeFile(path.join(BASE_DIR, 'web/src/index.css'), Step7_3_Css)
+    cy.writeFile(path.join(BASE_DIR, 'web/src/Routes.js'), Step7_4_Routes)
 
-  //   cy.contains('Contact').click()
-  //   cy.contains('Save').click()
-  //   cy.get('main').should('contain', 'name is required')
-  //   cy.get('main').should('contain', 'email is required')
-  //   cy.get('main').should('contain', 'message is required')
+    cy.contains('Contact').click()
+    cy.contains('Save').click()
+    cy.get('main').should('contain', 'name is required')
+    cy.get('main').should('contain', 'email is required')
+    cy.get('main').should('contain', 'message is required')
 
-  //   cy.get('input#email').type('foo bar')
-  //   cy.contains('Save').click()
-  //   cy.get('main').should('contain', 'Please enter a valid email address')
+    cy.get('input#email').type('foo bar')
+    cy.contains('Save').click()
+    cy.get('main').should('contain', 'Please enter a valid email address')
 
-  //   cy.get('input#name').type('test name')
-  //   cy.get('input#email').type('foo@bar.com')
-  //   cy.get('textarea#message').type('test message')
-  //   cy.get('#tutorial-form').submit()
-  //   // console
-  //   // {name: "test name", email: "foo@bar.com", message: "test message"}
-  // })
+    cy.get('input#name').type('test name')
+    cy.get('input#email').type('foo@bar.com')
+    cy.get('textarea#message').type('test message')
+    cy.get('#tutorial-form').submit()
+    // console
+    // {name: "test name", email: "foo@bar.com", message: "test message"}
+  })
 })
