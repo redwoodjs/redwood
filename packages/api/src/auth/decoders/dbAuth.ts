32d0ef0d36ff433b4b54f7193cffb4cb1c9f9187
Rob Cameron <rob.cameron@fastmail.com>
2021-06-23 16:30:32 -0700

    Use proper APIGatewayProxyEvent type

--- packages/api/src/auth/decoders/dbAuth.ts
+++ packages/api/src/auth/decoders/dbAuth.ts
@@ -1,16 +1,13 @@
+import type { APIGatewayProxyEvent } from 'aws-lambda'
+
 import { dbAuthSession } from '../../functions/dbAuth/shared'
 
-export const dbAuth = (authHeaderValue: string, req: { event: any }) => {
+export const dbAuth = (
+  authHeaderValue: string,
+  req: { event: APIGatewayProxyEvent }
+) => {
   const session = dbAuthSession(req.event)
   const authHeaderUserId = authHeaderValue
-
-  // const encrypted = getSession(req.event.headers['cookie'])
-
-  // if (!encrypted) {
-  //   return null
-  // }
-
-  // const [session, _csrfToken] = decryptSession(encrypted)
 
   if (session.id.toString() !== authHeaderUserId) {
     throw new Error('Authorization header does not match decrypted user ID')
