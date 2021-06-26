ea6d8d9342c77425f590fd8491a03efba63dddbc
Tobbe Lundberg <tobbe@tlundberg.com>
2021-06-26 20:13:13 +0200

    Include HistoryProvider in template

--- packages/create-redwood-app/template/web/src/App.tsx
+++ packages/create-redwood-app/template/web/src/App.tsx
@@ -1,4 +1,4 @@
-import '@redwoodjs/history'
+import { HistoryProvider } from '@redwoodjs/history'
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
@@ -10,9 +10,11 @@ import './index.css'
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider>
-      <RedwoodApolloProvider>
-        <Routes />
-      </RedwoodApolloProvider>
+      <HistoryProvider>
+        <RedwoodApolloProvider>
+          <Routes />
+        </RedwoodApolloProvider>
+      </HistoryProvider>
     </RedwoodProvider>
   </FatalErrorBoundary>
 )
