8ae7563227ae276854a5202ed05cf7006f8bae57
David Price <thedavid@thedavidprice.com>
2021-06-28 13:26:42 -0700

    add TODO note

--- packages/core/config/webpack.common.js
+++ packages/core/config/webpack.common.js
@@ -123,6 +123,7 @@ const getStyleLoaders = (isEnvProduction) => {
 
 /** @returns {import('webpack').Plugin[]} Plugins shared with storybook, as well as the RW app */
 const getSharedPlugins = (isEnvProduction) => {
+  // TODO disable in Storybook config
   const shouldIncludeFastRefresh =
     redwoodConfig.web.fastRefresh !== false && !isEnvProduction
 
