11ab2b9c0d301004dddbeb1163dc578d4659f08c
Peter Pistorius <peter.pistorius@gmail.com>
2021-07-02 12:43:25 +0200

    Clean up watcher.

--- packages/api-server/src/watch.ts
+++ packages/api-server/src/watch.ts
@@ -4,13 +4,12 @@ import { fork } from 'child_process'
 import type { ChildProcess } from 'child_process'
 import path from 'path'
 
+import c from 'ansi-colors'
 import chokidar from 'chokidar'
 import dotenv from 'dotenv'
 import { debounce } from 'lodash'
 
-import { build as babelBuild } from '@redwoodjs/core/babel/apiBuild'
-import { build as esBuild } from '@redwoodjs/core/esbuild/apiBuild'
-import { getPaths } from '@redwoodjs/internal'
+import { getPaths, buildApi } from '@redwoodjs/internal'
 
 const rwjsPaths = getPaths()
 
@@ -18,115 +17,53 @@ dotenv.config({
   path: rwjsPaths.base,
 })
 
-let chokidarReady = false
-let httpServer: ChildProcess
-const tsInitialBuild = Date.now()
+// TODO:
+// 1. Move this file out of the HTTP server, and place it in the CLI?
 
-// So that we can use await
-const startBuildWatcher = async () => {
-  console.log('Building API...')
+let httpServerProcess: ChildProcess
 
-  if (process.env.ESBUILD === '1') {
-    const buildResult = await esBuild({ incremental: true })
+const rebuildApiServer = () => {
+  try {
+    const buildTs = Date.now()
+    process.stdout.write(c.dim(c.italic('Building... ')))
+    buildApi()
+    console.log(c.dim(c.italic('Took ' + (Date.now() - buildTs) + ' ms')))
 
-    process.on('SIGINT', () => {
-      console.log()
-      console.log('Shutting down... ')
-      httpServer.kill()
-      buildResult.stop?.()
-      console.log('Done.')
-      process.exit(0)
-    })
-    startApiSrcWatcher(() => buildResult?.rebuild?.() as Promise<any>)
-  } else {
-    // First  build
-    try {
-      await babelBuild()
-    } catch (e) {
-      process.exit(1)
-    }
-
-    process.on('SIGINT', () => {
-      console.log()
-      console.log('Shutting down... ')
-      httpServer.kill()
-      console.log('Done.')
-      process.exit(0)
-    })
-
-    startApiSrcWatcher(() =>
-      babelBuild({
-        watch: false,
-        rebuild: true,
-      })
-    )
+    // Restart the API server
+    httpServerProcess?.emit('exit')
+    httpServerProcess?.kill()
+    httpServerProcess = fork(path.join(__dirname, 'index.js'))
+  } catch (e) {
+    console.error(e)
   }
-
-  httpServer = fork(path.join(__dirname, 'index.js'))
 }
 
-/**
- *
- * @param onChange the rebuild function, based on whether esbuild or babel is being used
- */
-function startApiSrcWatcher(onChange: (filePath?: string) => Promise<any>) {
-  chokidar
-    .watch(rwjsPaths.api.base, {
-      persistent: true,
-      ignoreInitial: true,
-      ignored: (file: string) =>
-        file.includes('node_modules') ||
-        file.includes(rwjsPaths.api.dist) ||
-        file.includes(rwjsPaths.api.types) ||
-        file.includes(rwjsPaths.api.db) ||
-        [
-          '.db',
-          '.sqlite',
-          '-journal',
-          '.test.js',
-          '.test.ts',
-          '.scenarios.ts',
-          '.scenarios.js',
-          '.d.ts',
-        ].some((ext) => file.endsWith(ext)),
-    })
-    .on('ready', async () => {
-      chokidarReady = true
-      console.log('Built in', Date.now() - tsInitialBuild, 'ms')
-    })
-    .on('all', async (eventName, filePath) => {
-      if (!chokidarReady) {
-        return
-      }
+chokidar
+  .watch(rwjsPaths.api.base, {
+    persistent: true,
+    ignoreInitial: true,
+    ignored: (file: string) =>
+      file.includes('node_modules') ||
+      file.includes(rwjsPaths.api.dist) ||
+      file.includes(rwjsPaths.api.types) ||
+      file.includes(rwjsPaths.api.db) ||
+      [
+        '.db',
+        '.sqlite',
+        '-journal',
+        '.test.js',
+        '.test.ts',
+        '.scenarios.ts',
+        '.scenarios.js',
+        '.d.ts',
+      ].some((ext) => file.endsWith(ext)),
+  })
+  .on('ready', async () => {
+    rebuildApiServer()
+  })
+  .on('all', (eventName, filePath) => {
+    console.log(`[${eventName}]`, `${filePath.replace(rwjsPaths.api.base, '')}`)
 
-      console.log(
-        `[${eventName}]`,
-        `${filePath.replace(rwjsPaths.api.base, '')}`
-      )
-
-      const tsRebuild = Date.now()
-      console.log('Building API...')
-      await rebuildAndRestart(onChange)
-
-      try {
-        console.log('Built in', Date.now() - tsRebuild, 'ms')
-      } catch (e) {
-        console.error(e)
-      }
-    })
-}
-
-// debounce to prevent multipule rebuilds+restarts when using generators
-const rebuildAndRestart = debounce(
-  async (onChange: (filePath?: string) => Promise<any>) => {
-    await onChange?.()
-    // Restart HTTP...
-    httpServer.emit('exit')
-    httpServer.kill()
-    httpServer = fork(path.join(__dirname, 'index.js'))
-    return
-  },
-  100
-)
-
-startBuildWatcher()
+    //rebuildApiServer.cancel()
+    rebuildApiServer()
+  })
