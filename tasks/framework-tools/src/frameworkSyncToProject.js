3599ab432c9bff87aa708e4d84ee802b441f2857
jtoar <dominic.saadi@hey.com>
2021-07-07 17:06:57 -0400

    wip handle rebuilding packages

--- tasks/framework-tools/src/frameworkSyncToProject.js
+++ tasks/framework-tools/src/frameworkSyncToProject.js
@@ -7,15 +7,22 @@
  * - remove them when user cancels
  */
 
+const fs = require('fs')
+const path = require('path')
+
 const c = require('ansi-colors')
 const chokidar = require('chokidar')
 const execa = require('execa')
+const rimraf = require('rimraf')
+const terminalLink = require('terminal-link')
 
 const {
   gatherDeps,
   getPackageJson,
   REDWOOD_PACKAGES_PATH,
   redwoodPackages,
+  packagesFileList,
+  redwoodBins,
 } = require('./utils')
 
 const projectPath = process.argv?.[2] ?? process.env.RWJS_CWD
@@ -25,6 +32,8 @@ if (!projectPath) {
   console.log(`Usage: ${process.argv?.[1]} /path/to/rwjs/proect`)
   process.exit(1)
 }
+
+const REDWOOD_PROJECT_NODE_MODULES = path.join(projectPath, 'node_modules')
 
 const { packageJson, packageJsonLink, writePackageJson } =
   getPackageJson(projectPath)
@@ -83,6 +92,55 @@ chokidar
       console.error(e)
       process.exit(1)
     }
+
+    // copy over the files
+
+    rimraf.sync(path.join(REDWOOD_PROJECT_NODE_MODULES, '@redwoodjs'))
+
+    console.log('Copying...')
+    console.log()
+
+    const packages = packagesFileList()
+
+    for (const [packageName, files] of Object.entries(packages)) {
+      console.log(
+        terminalLink(
+          packageName,
+          'file://' + path.join(REDWOOD_PROJECT_NODE_MODULES, packageName)
+        ),
+        files.length,
+        'files'
+      )
+      for (const file of files) {
+        const src = path.join(
+          REDWOOD_PACKAGES_PATH,
+          packageName.replace('@redwoodjs', ''),
+          file
+        )
+        const dst = path.join(REDWOOD_PROJECT_NODE_MODULES, packageName, file)
+        fs.mkdirSync(path.dirname(dst), { recursive: true })
+        fs.copyFileSync(src, dst)
+      }
+    }
+    console.log()
+
+    const bins = redwoodBins()
+    for (let [binName, binPath] of Object.entries(bins)) {
+      // if the binPath doesn't exist, create it.
+      const binSymlink = path.join(
+        REDWOOD_PROJECT_NODE_MODULES,
+        '.bin',
+        binName
+      )
+      binPath = path.join(REDWOOD_PROJECT_NODE_MODULES, binPath)
+      if (!fs.existsSync(binSymlink)) {
+        fs.symlinkSync(binPath, binSymlink)
+      }
+
+      console.log('chmod +x', binName)
+      fs.chmodSync(binPath, '755')
+    }
+    console.log()
   })
   .on('change', (file) => {
     console.log(c.dim(`[${file}`))
@@ -107,9 +165,40 @@ chokidar
         }
       }
     } else {
-      console.log('rebuild files')
-      // - Copying the files: then if a change is detected in a particular package,
-      // run yarn build in that packages folder, and copy over the files of that package.
-      // (packagesFileList allows you to specify an array of packages that it should operate against.)
+      const packageDirs = redwoodPackages().map(path.dirname)
+      const packageToRebuild = packageDirs.find((dir) => file.startsWith(dir))
+      console.log(`Rebuilding ${packageToRebuild}`)
+      console.log()
+
+      execa.sync('yarn build', {
+        cwd: packageToRebuild,
+        shell: true,
+        stdio: 'inherit',
+      })
+
+      console.log()
+
+      // copy just this package's files
+      // console.log(
+      //   terminalLink(packageToRebuild, 'file://' + packageToRebuild),
+      //   files.length,
+      //   'files'
+      // )
+      //
+      // const files = packagesFileList()[packageToRebuild]
+      // for (const file of files) {
+      //   const src = path.join(
+      //     REDWOOD_PACKAGES_PATH,
+      //     packageToRebuild.replace('@redwoodjs', ''),
+      //     file
+      //   )
+      //   const dst = path.join(
+      //     REDWOOD_PROJECT_NODE_MODULES,
+      //     packageToRebuild,
+      //     file
+      //   )
+      //   fs.mkdirSync(path.dirname(dst), { recursive: true })
+      //   fs.copyFileSync(src, dst)
+      // }
     }
   })
