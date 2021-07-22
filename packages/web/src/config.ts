a7c8deaa1cf12911e29f8ef0df904d3b73877f2b
Simon Gagnon <simon.gagnonr@gmail.com>
2021-07-22 16:45:35 -0400

    remove check if undefined

--- packages/web/src/config.ts
+++ packages/web/src/config.ts
@@ -5,10 +5,5 @@ if (typeof __REDWOOD__API_PROXY_PATH !== 'undefined') {
   // @ts-expect-error-next-line
   global.__REDWOOD__API_PROXY_PATH = __REDWOOD__API_PROXY_PATH
 }
-/* eslint-disable no-undef */
-// The value is read from `redwood.toml`
-// @ts-expect-error This is replaced at build time by Webpack Define Plugin.
-if (typeof __REDWOOD__APP_TITLE !== 'undefined') {
-  // @ts-expect-error-next-line
-  global.__REDWOOD__APP_TITLE = __REDWOOD__APP_TITLE
-}
+// @ts-expect-error-next-line
+global.__REDWOOD__APP_TITLE = __REDWOOD__APP_TITLE
