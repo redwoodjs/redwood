bdbd9801f333a4b36422fc038487c2a8a22e9dc0
Peter Pistorius <peter.pistorius@gmail.com>
2021-07-24 17:10:18 +0200

    Finally.

--- packages/internal/src/__tests__/build_api.test.ts
+++ packages/internal/src/__tests__/build_api.test.ts
@@ -7,6 +7,11 @@ const FIXTURE_PATH = path.resolve(
   __dirname,
   '../../../../__fixtures__/example-todo-main'
 )
+import { ensurePosixPath } from '../paths'
+
+const cleanPaths = (p) => {
+  return ensurePosixPath(path.relative(FIXTURE_PATH, p))
+}
 
 beforeAll(() => {
   process.env.RWJS_CWD = FIXTURE_PATH
@@ -17,8 +22,8 @@ afterAll(() => {
 
 test('api files are prebuilt', () => {
   const builtFiles = prebuildApiFiles(findApiFiles())
-  console.warn(builtFiles)
+  const p = builtFiles.map(cleanPaths)
 
-  expect(builtFiles[0].endsWith('api/src/functions/graphql.js')).toBeTruthy()
-  expect(builtFiles[2].endsWith('api/src/graphql/todos.sdl.js')).toBeTruthy()
+  expect(p[0].endsWith('api/src/functions/graphql.js')).toBeTruthy()
+  expect(p[2].endsWith('api/src/graphql/todos.sdl.js')).toBeTruthy()
 })
