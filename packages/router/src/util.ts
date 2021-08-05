93499e520cbd78a2ac9409128cd803ac89d18854
Brian Liu <ralphbliu@gmail.com>
2021-08-06 09:54:33 +1000

    Update packages/router/src/util.ts
    Co-authored-by: Tobbe Lundberg <tobbe@tlundberg.com>

--- packages/router/src/util.ts
+++ packages/router/src/util.ts
@@ -256,7 +256,7 @@ function flattenAll(children: ReactNode): ReactNode[] {
  * @param {string} queryString
  * @returns {Array<string | Record<string, any>>} A flatten array of search params
  *
- * useMatch hook options searchParams requires flatten array
+ * useMatch hook options searchParams requires a flat array
  *
  * Examples:
  *
