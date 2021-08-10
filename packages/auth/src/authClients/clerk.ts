20b3fc4190ccd6f79909bebe7056454f3eaad548
Zack Sheppard <zack@zacksheppard.com>
2021-08-10 10:05:24 +0100

    Fix issue where Clerk client is sometimes unset
    Discovered this while using Clerk + Redwood in production.
    Because Clerk is set up during the render cycle by the provider component, in production on first load this value can be captured as `null` by the App.tsx closure. On the client side, this can be fixed by moving Auth Provider to a lower point in the render stack, but the auth setup generator doesn't have support for this. Instead, it can also be fixed by having the `@redwoodjs/auth` package fall back to `window.Clerk` when the client is not defined.

--- packages/auth/src/authClients/clerk.ts
+++ packages/auth/src/authClients/clerk.ts
@@ -9,18 +9,32 @@ export type { Clerk }
 
 export type ClerkUser = ClerkUserResource & { roles: string[] | null }
 
+// In production, there is an issue where the AuthProvider sometimes captures Clerk
+// as null (and then sends it over as () => null). This captures that case and
+// falls back to `window.Clerk` to access the client.
+function clerkClient(propsClient: Clerk | (() => null)): Clerk | null {
+  if (!propsClient || (typeof propsClient === 'function' && !propsClient())) {
+    return window.Clerk ?? null
+  } else {
+    return propsClient
+  }
+}
+
 export const clerk = (client: Clerk): AuthClientClerk => {
   return {
     type: 'clerk',
     client,
-    login: async (options?) => client.openSignIn(options || {}),
-    logout: async () => new Promise((res) => client.signOut(res)),
-    signup: async (options?) => client.openSignUp(options || {}),
+    login: async (options?) => clerkClient(client)?.openSignIn(options || {}),
+    logout: async () => new Promise((res) => clerkClient(client)?.signOut(res)),
+    signup: async (options?) => clerkClient(client)?.openSignUp(options || {}),
     // Clerk uses the session ID PLUS the __session cookie.
-    getToken: async () => client.session.id,
+    getToken: async () => clerkClient(client)?.session.id,
     getUserMetadata: async () => {
-      return client.user
-        ? { ...client.user, roles: client.user.publicMetadata?.['roles'] ?? [] }
+      return clerkClient(client)?.user
+        ? {
+            ...clerkClient(client)?.user,
+            roles: clerkClient(client)?.user.publicMetadata?.['roles'] ?? [],
+          }
         : null
     },
   }
