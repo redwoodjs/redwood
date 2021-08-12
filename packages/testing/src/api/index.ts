70ce2d31bc4caf24439611ee417814fce629a79a
Daniel Choudhury <dannychoudhury@gmail.com>
2021-08-12 14:38:13 +0100

    Address PP comments around exports

--- packages/testing/src/api/index.ts
+++ packages/testing/src/api/index.ts
@@ -1,1 +1,2 @@
 export * from './apiTestingMocks'
+export * from './scenario'
--- packages/testing/src/index.ts
+++
@@ -1,4 +0,0 @@
-// These are the common things that can be shared across
-// web and api tests
-
-export * from './scenario'
