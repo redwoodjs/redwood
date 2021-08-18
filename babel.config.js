d7c8f7b07d4468514befc9bbcdf05fcd1adbd46b
Daniel Choudhury <dannychoudhury@gmail.com>
2021-08-18 17:12:16 +0100

    fix(babel): Dont transform runtime for plugin

--- babel.config.js
+++ babel.config.js
@@ -53,18 +53,6 @@ module.exports = {
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
     ['@babel/plugin-proposal-private-methods', { loose: true }],
-    [
-      '@babel/plugin-transform-runtime',
-      {
-        // https://babeljs.io/docs/en/babel-plugin-transform-runtime/#core-js-aliasing
-        // Setting the version here also requires `@babel/runtime-corejs3`
-        corejs: { version: 3, proposals: true },
-        // https://babeljs.io/docs/en/babel-plugin-transform-runtime/#version
-        // Transform-runtime assumes that @babel/runtime@7.0.0 is installed.
-        // Specifying the version can result in a smaller bundle size.
-        version: packageJSON.devDependencies['@babel/runtime-corejs3'],
-      },
-    ],
   ],
   overrides: [
     // ** WEB PACKAGES **
