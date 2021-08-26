5f30696e431b985e504ba162c1ed11b224f5e937
Daniel Choudhury <dannychoudhury@gmail.com>
2021-08-26 14:44:18 +0100

    Fix safe context test

--- packages/graphql-server/src/plugins/__tests__/useRedwoodGlobalContextSetter.test.ts
+++ packages/graphql-server/src/plugins/__tests__/useRedwoodGlobalContextSetter.test.ts
@@ -1,6 +1,6 @@
 import { createTestkit } from '@envelop/testing'
 
-import { context } from '../../index'
+import { context, getPerRequestContext } from '../../index'
 import { testSchema, testQuery } from '../__fixtures__/common'
 import { useRedwoodGlobalContextSetter } from '../useRedwoodGlobalContextSetter'
 import { useRedwoodPopulateContext } from '../useRedwoodPopulateContext'
@@ -9,25 +9,24 @@ afterAll(() => {
   process.env.SAFE_GLOBAL_CONTEXT = '0' // set to default
 })
 
-// @TODO why do we need to set SAFE_GLOBAL_CONTEXT for these tests to pass?
-// !WARN: DO NOT MERGE THIS INTO MAIN
 test('Safe Context OFF: Updates global context with extended context', async () => {
   process.env.SAFE_GLOBAL_CONTEXT = '0' // default: use async local storage
-
   const teskit = createTestkit(
     [
-      useRedwoodGlobalContextSetter(),
       useRedwoodPopulateContext(() => ({ hello: 'world' })),
       useRedwoodPopulateContext({ foo: 'bar' }),
+      useRedwoodGlobalContextSetter(),
     ],
     testSchema
   )
 
-  await teskit.execute(testQuery, {}, {})
+  await getPerRequestContext().run(new Map(), async () => {
+    await teskit.execute(testQuery, {}, {})
 
-  console.log(context) //?
-  expect(context.hello).toBe('world')
-  expect(context.foo).toBe('bar')
+    expect(context.hello).toBe('world')
+    expect(context.foo).toBe('bar')
+    expect(context.bazinga).not.toBeDefined() // just an extra check here because this test is async
+  })
 })
 
 test('Safe Context ON: Updates global context with extended context', async () => {
