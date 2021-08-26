729af77d50fe58d3fd0e2b97c6df3460d2e95d62
A. David Thyresson <dthyresson@gmail.com>
2021-08-26 05:34:58 -0400

    Remove webhooks, logger from graphql-server

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,8 +1,7 @@
 import './global.api-auto-imports'
+
 export * from './global.api-auto-imports'
-
 export * from './auth'
-
 export * from './globalContext'
 export * from './functions/dbAuth/DbAuthHandler'
 export { dbAuthSession } from './functions/dbAuth/shared'
