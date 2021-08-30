a5137b810629bc9017e789c40d5ec72d0d55495e
Daniel Choudhury <dannychoudhury@gmail.com>
2021-08-30 11:58:22 +0100

    Try reconfiguring ts | Add reference to api from gql-server

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,8 +1,8 @@
-import packageJson from '../package.json'
-
 export * from './auth'
 export * from './functions/dbAuth/DbAuthHandler'
 export { dbAuthSession } from './functions/dbAuth/shared'
 
+// @NOTE: use require, to avoid messing around with tsconfig and nested output dirs
+const packageJson = require('../package.json')
 export const prismaVersion = packageJson?.dependencies['@prisma/client']
 export const redwoodVersion = packageJson?.version
