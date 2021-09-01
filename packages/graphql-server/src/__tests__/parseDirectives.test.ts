7fe1f46de5fe7d0c8c5f9b94989ba20de3e72a5b
Daniel Choudhury <dannychoudhury@gmail.com>
2021-09-01 12:48:14 +0100

    Add test for heavily nested directive

--- packages/graphql-server/src/__tests__/parseDirectives.test.ts
+++ packages/graphql-server/src/__tests__/parseDirectives.test.ts
@@ -17,15 +17,26 @@ test('Should map globs to defined structure correctly', async () => {
         directive @bazinga on FIELD_DEFINITION
       `,
     },
+    heavily_nested_bar: {
+      bar: async () => 'I am bar, async',
+      schema: gql`
+        directive @bar on FIELD_DEFINITION
+      `,
+    },
   }
 
-  const [fooDirective, bazingaDirective] = parseDirectives(directiveFiles)
+  const [fooDirective, bazingaDirective, barDirective] =
+    parseDirectives(directiveFiles)
 
   expect(fooDirective.name).toBe('foo')
   expect(fooDirective.onExecute()).toBe('I am foo')
-  expect(fooDirective.schema).toBeTruthy()
+  expect(fooDirective.schema.kind).toBe('Document')
 
   expect(bazingaDirective.name).toBe('bazinga')
   expect(await bazingaDirective.onExecute()).toBe('I am bazinga, async')
-  expect(bazingaDirective.schema).toBeTruthy()
+  expect(bazingaDirective.schema.kind).toBe('Document')
+
+  expect(barDirective.name).toBe('bar')
+  expect(await barDirective.onExecute()).toBe('I am bar, async')
+  expect(barDirective.schema.kind).toBe('Document')
 })
