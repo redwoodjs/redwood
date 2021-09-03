eae18dc6c80f61e1825907fed10b15806a148355
Daniel Choudhury <dannychoudhury@gmail.com>
2021-09-03 16:53:41 +0100

    Being silly, not required

--- packages/create-redwood-app/jest.config.js
+++ packages/create-redwood-app/jest.config.js
@@ -1,8 +1,7 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/*.test.[jt]s?(x)'],
-  passWithNoTests: true,
-  testPathIgnorePatterns: ['fixtures', 'template'],
+  testPathIgnorePatterns: ['fixtures', 'api/src', 'web/src'],
   moduleNameMapper: {
     'src/(.*)': '<rootDir>/src/$1',
   },
