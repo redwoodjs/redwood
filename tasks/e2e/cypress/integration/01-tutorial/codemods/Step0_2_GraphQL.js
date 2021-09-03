1c3bd98dec8333419943faea9775f64c588fc33f
Daniel Choudhury <dannychoudhury@gmail.com>
2021-09-03 15:57:39 +0100

    🙄 Whoopsie on codemod

--- tasks/e2e/cypress/integration/01-tutorial/codemods/Step0_2_GraphQL.js
+++ tasks/e2e/cypress/integration/01-tutorial/codemods/Step0_2_GraphQL.js
@@ -1,25 +1,21 @@
-import {
-  createGraphQLHandler,
-  makeMergedSchema,
-  makeServices,
-} from '@redwoodjs/graphql-server'
+export default `
+import { createGraphQLHandler } from '@redwoodjs/graphql-server'
 
-import directives from 'src/directives/**/*.{js,ts}'
-import schemas from 'src/graphql/**/*.{js,ts}'
+import directives from 'src/directives/**/*.directive.{js,ts}'
+import sdls from 'src/graphql/**/*.sdl.{js,ts}'
+import services from 'src/services/**/*.{js,ts}'
+
 import { db } from 'src/lib/db'
 import { logger } from 'src/lib/logger'
-import services from 'src/services/**/*.{js,ts}'
 
 export const handler = createGraphQLHandler({
   loggerConfig: { logger, options: {} },
-  schema: makeMergedSchema({
-    schemas,
-    services: makeServices({ services }),
-    directives,
-  }),
   directives,
+  sdls,
+  services,
   onException: () => {
     // Disconnect from your database with an unhandled exception.
     db.$disconnect()
   },
 })
+`
