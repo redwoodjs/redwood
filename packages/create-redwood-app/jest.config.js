0eccc4738c17fecc228d3f0d4dd9193ad0dc48b1
Daniel Choudhury <dannychoudhury@gmail.com>
2021-09-03 16:50:59 +0100

    Ignore tests in crwa template

--- packages/create-redwood-app/jest.config.js
+++ packages/create-redwood-app/jest.config.js
@@ -1,7 +1,8 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/*.test.[jt]s?(x)'],
-  testPathIgnorePatterns: ['fixtures', 'api/src', 'web/src'],
+  passWithNoTests: true,
+  testPathIgnorePatterns: ['fixtures', 'template'],
   moduleNameMapper: {
     'src/(.*)': '<rootDir>/src/$1',
   },
