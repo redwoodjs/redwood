31d283e1fa0a84e9d9d89b692e2b92d0c866a9b6
Rob Cameron <rob.cameron@fastmail.com>
2021-09-03 16:22:08 -0700

    Snapshot update

--- packages/internal/src/__tests__/config.test.ts
+++ packages/internal/src/__tests__/config.test.ts
@@ -8,43 +8,42 @@ describe('getConfig', () => {
       path.join(__dirname, './fixtures/redwood.empty.toml')
     )
     expect(config).toMatchInlineSnapshot(`
-      Object {
-        "api": Object {
-          "host": "localhost",
-          "path": "./api",
-          "port": 8911,
-          "schemaPath": "./api/db/schema.prisma",
-          "target": "node",
-          "title": "Redwood App",
-        },
-        "browser": Object {
-          "open": false,
-        },
-        "experimental": Object {
-          "esbuild": false,
-        },
-        "generate": Object {
-          "nestScaffoldByModel": true,
-          "stories": true,
-          "tests": true,
-        },
-        "telemetry": Object {
-          "apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNTg3NjYxMywiZXhwIjoxOTQxNDUyNjEzfQ.xDvtPNKfE1GLMIKO5kKJnk_NaQpCDvPpsHij6iz2UX8",
-          "url": "https://cuvvzwmeexzpnctfycwn.supabase.co/rest/v1",
-        },
-        "web": Object {
-          "a11y": true,
-          "apiProxyPath": "/.netlify/functions",
-          "apiProxyPort": 8911,
-          "fastRefresh": true,
-          "host": "localhost",
-          "path": "./web",
-          "port": 8910,
-          "target": "browser",
-          "title": "Redwood App",
-        },
-      }
-    `)
+Object {
+  "api": Object {
+    "host": "localhost",
+    "path": "./api",
+    "port": 8911,
+    "schemaPath": "./api/db/schema.prisma",
+    "target": "node",
+    "title": "Redwood App",
+  },
+  "browser": Object {
+    "open": false,
+  },
+  "experimental": Object {
+    "esbuild": false,
+  },
+  "generate": Object {
+    "nestScaffoldByModel": true,
+    "stories": true,
+    "tests": true,
+  },
+  "telemetry": Object {
+    "url": "https://telemetry.redwoodjs.com/.netlify/functions/telemetry",
+  },
+  "web": Object {
+    "a11y": true,
+    "apiProxyPath": "/.netlify/functions",
+    "apiProxyPort": 8911,
+    "fastRefresh": true,
+    "host": "localhost",
+    "path": "./web",
+    "port": 8910,
+    "target": "browser",
+    "title": "Redwood App",
+  },
+}
+`)
   })
 
   it('merges configs', () => {
