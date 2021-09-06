813a3a0c05d3464675259f1c9e8746e656ad3ecd
A. David Thyresson <dthyresson@gmail.com>
2021-09-06 15:18:01 -0400

    No roles in shipper requireAuth directive

--- packages/create-redwood-app/template/api/src/directives/requireAuth.directive.ts
+++ packages/create-redwood-app/template/api/src/directives/requireAuth.directive.ts
@@ -1,6 +1,5 @@
 import gql from 'graphql-tag'
 
-import { getDirectiveArgument } from '@redwoodjs/graphql-server'
 import type { RedwoodDirective } from '@redwoodjs/graphql-server'
 
 import { requireAuth as applicationRequireAuth } from 'src/lib/auth'
@@ -9,16 +8,6 @@ export const schema = gql`
   directive @requireAuth(roles: [String]) on FIELD_DEFINITION
 `
 
-export const requireAuth: RedwoodDirective = ({ directiveNode }) => {
-  // get the roles listed in your sdl
-  // e.g. @requireAuth(roles: ['ADMIN', 'BAZINGA'])
-  //                           ───┬───   ────┬──
-  //   requiredRoles ─────────────┘──────────┘
-  const requiredRoles = directiveNode
-    ? getDirectiveArgument(directiveNode, 'roles')
-    : []
-
-  applicationRequireAuth({
-    roles: requiredRoles,
-  })
+export const requireAuth: RedwoodDirective = () => {
+  applicationRequireAuth()
 }
