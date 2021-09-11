79ead40d65a1ccb84c40dca314315e72e3c2e77f
Omar El-Domeiri <omar@doesnotexist.com>
2021-09-11 01:44:35 -0400

    comment describing alternative JWT verification

--- packages/api/src/auth/decoders/firebase.ts
+++ packages/api/src/auth/decoders/firebase.ts
@@ -5,4 +5,6 @@ export const firebase = async (
   const admin = require('firebase-admin')
 
   return admin.auth().verifyIdToken(token)
+  // Alternative is to use process described for third-party JWT verification
+  // https://firebase.google.com/docs/auth/admin/verify-id-tokens#verify_id_tokens_using_a_third-party_jwt_library
 }
