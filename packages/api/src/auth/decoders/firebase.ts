b60c89dfc5e1f9ca12019f45b424152432002dc6
Omar El-Domeiri <omar@doesnotexist.com>
2021-09-18 13:23:17 -0400

    fix comments

--- packages/api/src/auth/decoders/firebase.ts
+++ packages/api/src/auth/decoders/firebase.ts
@@ -1,10 +1,10 @@
 export const firebase = async (
   token: string
 ): Promise<null | Record<string, unknown>> => {
-  // Use require here, to prevent needing clerk sdk in api deps
+  // Use require here to prevent dependency for non-firebase projects
   const admin = require('firebase-admin')
 
   return admin.auth().verifyIdToken(token)
-  // Alternative is to use process described for third-party JWT verification
+  // Alternative third-party JWT verification process described here:
   // https://firebase.google.com/docs/auth/admin/verify-id-tokens#verify_id_tokens_using_a_third-party_jwt_library
 }
