374c239bf54cb3b04e35b1fefb9a4e61dcdf26a1
Daniel Choudhury <dannychoudhury@gmail.com>
2021-09-21 19:09:14 +0100

    Update firebase template, marking optional env vars

--- packages/cli/src/commands/setup/auth/providers/firebase.js
+++ packages/cli/src/commands/setup/auth/providers/firebase.js
@@ -7,10 +7,13 @@ export const config = {
   init: `const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+
+  /** Optional config, may be needed, depending on how you use firebase
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID,
+  **/
 }
 
 const firebaseApp = ((config) => {
