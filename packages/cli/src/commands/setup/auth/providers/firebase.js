ef814653de811b75678c0d96c99f380f841fc552
Daniel Choudhury <dannychoudhury@gmail.com>
2021-09-21 15:39:42 +0100

    Update setup template changes

--- packages/cli/src/commands/setup/auth/providers/firebase.js
+++ packages/cli/src/commands/setup/auth/providers/firebase.js
@@ -7,10 +7,13 @@ export const config = {
   init: `const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+
+  /** Other Optional config **
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID,
+  */
 }
 
 const firebaseApp = ((config) => {
@@ -20,17 +23,13 @@ const firebaseApp = ((config) => {
   }
   return getApp()
 })(firebaseConfig)
-
-export const firebaseClient = {
-  firebaseAuth,
-  firebaseApp, // optional
-}`,
-  authProvider: { client: 'firebaseClient', type: 'firebase' },
+`,
+  authProvider: { client: 'firebaseApp', type: 'firebase' },
 }
 
 // required packages to install
-export const webPackages = ['firebase']
-export const apiPackages = ['firebase-admin']
+export const webPackages = ['firebase@9']
+export const apiPackages = ['firebase-admin@9']
 
 // any notes to print out when the job is done
 export const notes = [
