e21f3a461abc58afda06a1ff654277ad4e7712e3
Daniel Choudhury <dannychoudhury@gmail.com>
2021-09-21 17:04:51 +0100

    Revert "Update setup template changes"
    This reverts commit ef814653de811b75678c0d96c99f380f841fc552.

--- packages/cli/src/commands/setup/auth/providers/firebase.js
+++ packages/cli/src/commands/setup/auth/providers/firebase.js
@@ -7,13 +7,10 @@ export const config = {
   init: `const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-
-  /** Other Optional config **
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID,
-  */
 }
 
 const firebaseApp = ((config) => {
@@ -23,13 +20,17 @@ const firebaseApp = ((config) => {
   }
   return getApp()
 })(firebaseConfig)
-`,
-  authProvider: { client: 'firebaseApp', type: 'firebase' },
+
+export const firebaseClient = {
+  firebaseAuth,
+  firebaseApp, // optional
+}`,
+  authProvider: { client: 'firebaseClient', type: 'firebase' },
 }
 
 // required packages to install
-export const webPackages = ['firebase@9']
-export const apiPackages = ['firebase-admin@9']
+export const webPackages = ['firebase']
+export const apiPackages = ['firebase-admin']
 
 // any notes to print out when the job is done
 export const notes = [
