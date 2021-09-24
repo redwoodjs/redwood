eabfde3c9b7ad568dbfcd982c8da8159effd4684
Dominic Saadi <dominic.saadi@hey.com>
2021-09-24 13:34:58 -0700

    rename

--- packages/codemods/src/codemods/v0.37.x/updateGraphqlFunction/__tests__/updateGraphqlFunction.test.ts
+++ packages/codemods/src/codemods/v0.37.x/updateGraphqlFunction/__tests__/updateGraphqlFunction.test.ts
@@ -2,6 +2,6 @@ jest.autoMockOff()
 import { defineTest } from 'jscodeshift/dist/testUtils'
 
 // TS and JS are equivalent in this case
-defineTest(__dirname, 'graphqlFunctionImports', null, 'graphql', {
+defineTest(__dirname, 'updateGraphQLFunction', null, 'graphql', {
   parser: 'ts',
 })
