6e4f1626ff41196affbada6899a2214c4cc42981
Rob Cameron <rob.cameron@fastmail.com>
2021-09-24 15:37:46 -0700

    Adds responseType to request call

--- packages/auth/src/authClients/dbAuth.ts
+++ packages/auth/src/authClients/dbAuth.ts
@@ -12,6 +12,7 @@ export type DbAuth = () => null
 const request = async (
   verb: string,
   method: string,
+  responseType: string,
   attributes: Record<string, unknown> = {}
 ) => {
   const response = await fetch(`${global.__REDWOOD__API_PROXY_PATH}/auth`, {
@@ -20,16 +21,16 @@ const request = async (
     body: JSON.stringify({ ...attributes, method }),
   })
 
-  return await response.json()
+  if (responseType === 'json') {
+    return await response.json()
+  } else  {
+    return await response.text()
+  }
 }
 
 export const dbAuth = (): AuthClient => {
   const getToken = async () => {
-    // don't use request() here because we need the raw text, not JSON
-    const response = await fetch(
-      `${global.__REDWOOD__API_PROXY_PATH}/auth?method=getToken`
-    )
-    const token = await response.text()
+    const token = await request('GET', 'logout', 'text')
 
     if (token.length === 0) {
       return null
@@ -41,29 +42,29 @@ export const dbAuth = (): AuthClient => {
   const login = async (attributes: LoginAttributes) => {
     const { username, password } = attributes
 
-    return await request('POST', 'login', { username, password })
+    return await request('POST', 'login', 'json', { username, password })
   }
 
   const logout = async () => {
-    await request('POST', 'logout')
+    await request('POST', 'logout', 'text')
 
     return true
   }
 
   const signup = async (attributes: SignupAttributes) => {
-    return await request('POST', 'signup', attributes)
+    return await request('POST', 'signup', 'json', attributes)
   }
 
   const forgotPassword = async (username: string) => {
-    return await request('POST', 'forgotPassword', { username })
+    return await request('POST', 'forgotPassword', 'json', { username })
   }
 
   const resetPassword = async (password: string) => {
-    return await request('POST', 'resetPassword', { password })
+    return await request('POST', 'resetPassword', 'json', { password })
   }
 
   const validateResetToken = async (token: string | null) => {
-    return await request('POST', 'validateResetToken', { token })
+    return await request('POST', 'validateResetToken', 'json', { token })
   }
 
   return {
