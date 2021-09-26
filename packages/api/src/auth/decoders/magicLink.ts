08f438f9fffb20a4bf4af5f47af56ff22d7344fb
Omar El-Domeiri <doesnotexist@users.noreply.github.com>
2021-09-26 14:54:27 -0400

    MagicLink: api side token validation and decoding

--- packages/api/src/auth/decoders/magicLink.ts
+++ packages/api/src/auth/decoders/magicLink.ts
@@ -1,13 +1,30 @@
-export const magicLink = (token: string) => {
-  if (!process.env.MAGICLINK_PUBLIC) {
-    if (process.env.NODE_ENV === 'development') {
-      console.warn(
-        'MAGICLINK_PUBLIC env var is not set. Be certain to set this value in Production.'
-      )
-    } else {
-      console.error('Magic Auth configuration error.')
-      throw new Error('Magic Auth configuration error.')
-    }
+// Relevant type excerpt from Magiclink's magic-sdk/admin:
+// export interface Claim {
+//   iat: number; // Issued At Timestamp
+//   ext: number; // Expiration Timestamp
+//   iss: string; // Issuer of DID Token
+//   sub: string; // Subject
+//   aud: string; // Audience
+//   nbf: number; // Not Before Timestamp
+//   tid: string; // DID Token ID
+//   add: string; // Encrypted signature of arbitrary data
+// }
+// export type ParsedDIDToken = [string, Claim];
+//
+// This function validates the token, throw an error if validations fails
+// otherwise it will return the decoded token in the form of a ParsedDIDToken
+export const magicLink = async (token: string):  => {
+  const { MAGIC_SECRET_API_KEY } = process.env
+  if (!MAGIC_SECRET_API_KEY) {
+    throw new Error('`MAGIC_SECRET_API_KEY` environment variable not set.')
   }
-  return token
+  const { Magic } = require('@magic-sdk/admin')
+
+  const magicAdmin = new Magic(MAGIC_SECRET_API_KEY)
+
+  magicAdmin.token.validate(token)
+
+  return magicAdmin.token.decode(token)
+
 }
+
