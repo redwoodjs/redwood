88af2611c9c1d4b29ce0e9f213bf0fa666a5950a
Dominic Saadi <dominic.saadi@hey.com>
2021-09-27 23:18:34 -0700

    add updateGraphQLFunction to CLI

---
+++ packages/codemods/src/codemods/v0.37.x/updateGraphQLFunction/updateGraphQLFunction.yargs.ts
@@ -0,0 +1,22 @@
+import path from 'path'
+
+import fg from 'fast-glob'
+import task from 'tasuku'
+
+import { getPaths as getRWPaths } from '@redwoodjs/internal'
+
+import runTransform from '../../../lib/runTransform'
+
+export const command = 'update-graphql-function'
+export const description = 'Updates GraphQL functions'
+
+export const handler = async () => {
+  task('Updating GraphQL functions', async () => {
+    const rwPaths = getRWPaths()
+
+    runTransform({
+      transformPath: path.join(__dirname, 'updateGraphQLFunction.js'),
+      targetPaths: fg.sync(path.join(rwPaths.api.functions, 'graphql.{js,ts}')),
+    })
+  })
+}
