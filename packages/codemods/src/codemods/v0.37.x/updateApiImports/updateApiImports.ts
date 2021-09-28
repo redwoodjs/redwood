94990918f3e06af24091053b10d9eba87ddf5e90
Dominic Saadi <dominic.saadi@hey.com>
2021-09-27 23:42:39 -0700

    add wip updateApiImports

--- packages/codemods/src/codemods/v0.37.x/updateApiImports/updateApiImports.ts
+++ packages/codemods/src/codemods/v0.37.x/updateApiImports/updateApiImports.ts
@@ -14,11 +14,42 @@ const apiExports = [
 export default function transformer(file: FileInfo, api: API) {
   const j = api.jscodeshift
 
+  const ast = j(file.source)
+
+  const apiSpecifiers = new Set()
+  const graphqlServerSpecifiers = new Set()
+
   // Step 1: Find all named import statements from '@redwoodjs/api'
+  ast
+    .find(j.ImportDeclaration, { source: { value: '@redwoodjs/api' } })
+    .forEach((importDeclaration) => {
+      const { specifiers } = importDeclaration.node
+      // Step 2: For each import, check against list of api exports
+      specifiers?.forEach((specifier) => {
+        const { name } = specifier.imported
+        // if in list: leave the import
+        if (apiExports.includes(name)) {
+          apiSpecifiers.add(name)
+          // if not: push import to array, remove the import and add import from gql server
+        } else {
+          graphqlServerSpecifiers.add(name)
+        }
+      })
+    })
 
-  // Step 2: For each import, check against apiExports whitelist
-  // if in list: leave the import
-  // if not: push import to array, remove the import and add import from gql server
+  ast
+    .find(j.Program)
+    .get('body', 0)
+    .insertBefore(
+      `import { ${[...apiSpecifiers].join(', ')} } from '@redwoodjs/api'`
+    )
+
+  ast
+    .find(j.Program)
+    .get('body', 0)
+    .insertBefore(
+      `import { ${[...graphqlServerSpecifiers].join(
+        ', '
+      )} } from '@redwoodjs/graphql-server'`
+    )
 }
-
-// Edgecase: import context as baaaa from
