aaf9c1a8d5b68c0d5f4bb9c30001364db065f4d5
Dominic Saadi <dominic.saadi@hey.com>
2021-09-28 00:15:44 -0700

    add updateApiImports CLI

---
+++ packages/codemods/src/codemods/v0.37.x/updateApiImports/updateApiImports.yargs.ts
@@ -0,0 +1,34 @@
+import path from 'path'
+
+import task from 'tasuku'
+
+import { getPaths as getRWPaths } from '@redwoodjs/internal'
+
+import getFilesWithPattern from '../../../lib/getFilesWithPattern'
+import runTransform from '../../../lib/runTransform'
+
+export const command = 'update-api-imports'
+export const description =
+  'Updates @redwoodjs/api imports to @redwoodjs/graphql-server'
+
+export const handler = async () => {
+  task('Updating @redwoodjs/api imports', async ({ setWarning }) => {
+    const rwPaths = getRWPaths()
+
+    const files = getFilesWithPattern({
+      pattern: 'from @redwoodjs/api',
+      filesToSearch: [rwPaths.api.src],
+    })
+
+    console.log(files)
+
+    if (files.length === 0) {
+      setWarning('No files found')
+    } else {
+      runTransform({
+        transformPath: path.join(__dirname, 'updateApiImports.js'),
+        targetPaths: files,
+      })
+    }
+  })
+}
