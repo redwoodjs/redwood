8614663bb8fa791cbed4772e462f00d03a052e68
Dominic Saadi <dominic.saadi@hey.com>
2021-09-28 00:03:41 -0700

    polish updateApiImports codemod

--- packages/codemods/src/codemods/v0.37.x/updateApiImports/updateApiImports.ts
+++ packages/codemods/src/codemods/v0.37.x/updateApiImports/updateApiImports.ts
@@ -19,24 +19,27 @@ export default function transformer(file: FileInfo, api: API) {
   const apiSpecifiers = new Set()
   const graphqlServerSpecifiers = new Set()
 
-  // Step 1: Find all named import statements from '@redwoodjs/api'
+  // Find all named import statements from '@redwoodjs/api'.
+  // Seprate their specifiers.
   ast
     .find(j.ImportDeclaration, { source: { value: '@redwoodjs/api' } })
     .forEach((importDeclaration) => {
       const { specifiers } = importDeclaration.node
-      // Step 2: For each import, check against list of api exports
-      specifiers?.forEach((specifier) => {
+
+      specifiers.forEach((specifier) => {
         const { name } = specifier.imported
-        // if in list: leave the import
+
         if (apiExports.includes(name)) {
           apiSpecifiers.add(name)
-          // if not: push import to array, remove the import and add import from gql server
         } else {
           graphqlServerSpecifiers.add(name)
         }
       })
+
+      j(importDeclaration).remove()
     })
 
+  // Insert new import declarations at the top.
   ast
     .find(j.Program)
     .get('body', 0)
