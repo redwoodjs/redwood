6b0fa9a77cce2e24143cc1b1a9b7ce7565917ede
Daniel Choudhury <dannychoudhury@gmail.com>
2021-09-28 13:59:42 +0000

    Update jest config

--- packages/codemods/jest.config.js
+++ packages/codemods/jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/*.test.[jt]s?(x)'],
-  testPathIgnorePatterns: ['__fixtures__', '__tests__/utils/*'],
+  testPathIgnorePatterns: ['__fixtures__', '__tests__/utils/*', '.d.ts', 'dist'],
   setupFilesAfterEnv: ['./jest.setup.js'],
   testTimeout: 15000,
 }
