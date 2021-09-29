b172733577b7299a7930fc0d19e3013ae933bdcc
Dominic Saadi <dominic.saadi@hey.com>
2021-09-29 12:55:34 -0700

    only apply to codemods babel config

--- babel.config.js
+++ babel.config.js
@@ -52,7 +52,6 @@ module.exports = {
      **/
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
-    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
     ['@babel/plugin-proposal-private-methods', { loose: true }],
   ],
   overrides: [
