8f0b938c8a66731ef110e5c66f325f2f2ec788a8
Johan Eliasson <jeliasson@users.noreply.github.com>
2021-10-01 19:00:18 +0200

    Adding inital work on @azure/msal-browser

--- packages/auth/src/authClients/azureActiveDirectory.ts
+++ packages/auth/src/authClients/azureActiveDirectory.ts
@@ -1,4 +1,4 @@
-import type { UserAgentApplication as AzureActiveDirectory } from 'msal'
+import type { PublicClientApplication as AzureActiveDirectory } from '@azure/msal-browser'
 
 import type { AuthClient } from './'
 
@@ -13,28 +13,61 @@ export const azureActiveDirectory = (
   return {
     type: 'azureActiveDirectory',
     client,
-    login: async (options?) => await client.loginPopup(options),
-    logout: (options?) => client.logout(options),
-    signup: async (options?) => await client.loginPopup(options),
+    login: async (options?) => client.loginRedirect(options),
+    logout: (options?) => client.logoutRedirect(options),
+    signup: async (options?) => client.loginRedirect(options),
     getToken: async (options?: any) => {
-      const authRequest = options || {
-        scopes: ['openid', 'profile'],
-      }
+      const token = await client.acquireTokenSilent(options)
 
-      // The recommended call pattern is to first try to call AcquireTokenSilent,
-      // and if it fails with a MsalUiRequiredException, call AcquireTokenXYZ
-      // https://github.com/AzureAD/microsoft-authentication-library-for-dotnet/wiki/AcquireTokenSilentAsync-using-a-cached-token
-      // NOTE: We are not catching by the `MsalUiRequiredException`, perhaps we can branch off `error.name`
-      // if this strategy doesn't work properly.
-      try {
-        const response = await client.acquireTokenSilent(authRequest)
-        return response?.idToken?.rawIdToken || null
-      } catch (error) {
-        client.acquireTokenRedirect(authRequest)
-      }
+      return token.accessToken || null
 
-      return null
+      //return null
     },
-    getUserMetadata: async () => (await client.getAccount()) || null,
+    restoreAuthState: async (options?: any) => {
+      // As we are using the redirect flow, we need to call handleRedirectPromise
+      // to complete the flow. As this only should happen on a valid redirect, I think
+      // it makes sense to call this in the restoreAuthState.
+
+      if (window.location.href.includes('#code=')) {
+        const response = await client.handleRedirectPromise()
+
+        console.log(`[AAD:restoreAuthState:debug] Response`, response)
+
+        // Try get all accounts
+        const accounts = client.getAllAccounts()
+        console.log(`[AAD:restoreAuthState:debug] Accounts`, accounts)
+
+        if (accounts.length === 0) {
+          // Debug
+          console.log(
+            `[AAD:restoreAuthState:debug]: We got 0 account, which is odd since we got a #code= back, but hey; let's send a login request.`
+          )
+
+          // No accounts, so we need to login
+          client.loginRedirect(options)
+        } else if (accounts.length === 1) {
+          // Debug
+          console.log(`[AAD:restoreAuthState:debug]: We got 1 account`)
+
+          // We have one account, so we can set it as active
+          client.setActiveAccount(accounts[0])
+        } else {
+          // Debug
+          console.log(
+            `[AAD:restoreAuthState:debug]: We got multiple accounts. For now, create a new login request so the user to choose one`
+          )
+
+          // We have multiple accounts, so we need to ask the user which one to use
+          client.loginRedirect(options)
+        }
+      }
+    },
+    getUserMetadata: async () => {
+      console.log(`[AAD:debug] getUserMetadata`)
+
+      return {
+        status: 'Not implemented',
+      }
+    },
   }
 }
