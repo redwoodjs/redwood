f8f6e030b5a93f29e25a1fe605134f879700b8d4
Peter Colapietro <petercolapietro+spacehippie@gmail.com>
2021-10-02 20:19:39 -0400

    feat: add Redwood Setup command for Shadowenv
    Closes #3094
    
    fix: remove unused import

---
+++ packages/cli/src/commands/setup/shadowenv/shadowenv.js
@@ -0,0 +1,86 @@
+import fs from 'fs'
+import path from 'path'
+
+import boxen from 'boxen'
+import Listr from 'listr'
+
+import { getPaths, writeFile } from '../../../lib'
+import c from '../../../lib/colors'
+
+const shadowenvConfigPath = `${getPaths().base}/.shadowenv.d/rw.lisp`
+
+export const builder = (yargs) => {
+  yargs.option('force', {
+    alias: 'f',
+    default: false,
+    description: 'Overwrite existing configuration',
+    type: 'boolean',
+  })
+}
+
+export const handler = async ({ force }) => {
+  const tasks = new Listr([
+    {
+      title: 'Configuring Shadowenv...',
+      task: (ctx) => {
+        /**
+         * Check if Shadowenv config already exists.
+         * If it exists, throw an error.
+         */
+        if (!force && fs.existsSync(shadowenvConfigPath)) {
+          ctx.error = true
+          throw new Error(
+            'Shadowenv config already exists.\nUse --force to override existing config.'
+          )
+        } else {
+          return writeFile(
+            shadowenvConfigPath,
+            fs
+              .readFileSync(
+                path.resolve(__dirname, 'templates', 'rw.lisp.template')
+              )
+              .toString(),
+            { overwriteExisting: force }
+          )
+        }
+      },
+    },
+    {
+      title: 'Destination:',
+      enabled: (ctx) => !ctx.error,
+      task: (_ctx, task) => {
+        task.title = `  Wrote templates to ${shadowenvConfigPath.replace(
+          getPaths().base,
+          ''
+        )}
+        `
+      },
+    },
+    {
+      title: '',
+      enabled: (ctx) => !ctx.error,
+      task: (_ctx, task) => {
+        task.title = `One more thing...\n\n ${boxen(
+          [
+            c.green('Installation steps for Shadowenv'),
+            '1. On macOS, you can `brew install shadowenv`',
+            '2. In order to avoid an error about an “untrusted” shadowenv, run `shadowenv trust`',
+            '\nSee also: https://shopify.github.io/shadowenv/getting-started/',
+          ].join('\n   '),
+          {
+            padding: { top: 1, bottom: 1, right: 1, left: 1 },
+            margin: 1,
+            borderColour: 'gray',
+          }
+        )}  \n`
+      },
+    },
+  ])
+
+  try {
+    await tasks.run()
+  } catch (e) {
+    console.error(c.error(e.message))
+    process.exit(e?.exitCode || 1)
+  }
+}
