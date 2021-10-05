b2b31579bca2fa4946f1e6a7ed60b16dfd302e5c
A. David Thyresson <dthyresson@gmail.com>
2021-10-05 06:03:45 -0400

    Converts scripts from ts to js

--- packages/cli/src/commands/ts-to-js.js
+++ packages/cli/src/commands/ts-to-js.js
@@ -1,9 +1,13 @@
-import { getPaths, convertTsProjectToJs } from '@redwoodjs/internal'
+import {
+  getPaths,
+  convertTsProjectToJs,
+  convertTsScriptsToJs,
+} from '@redwoodjs/internal'
 
 export const command = 'ts-to-js'
 export const description = 'Convert a TypeScript project to JavaScript'
 
 export const handler = () => {
   convertTsProjectToJs(getPaths().base)
-  convertTsProjectToJs(getPaths().scripts)
+  convertTsScriptsToJs(getPaths().base)
 }
