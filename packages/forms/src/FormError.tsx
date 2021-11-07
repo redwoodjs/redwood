dc8dead244d5e8b4a656c9064e37bf86944e623b
David Thyresson <dthyresson@gmail.com>
2021-11-07 11:00:12 -0500

    Fixes UserInputError mesage display in FormError (#3704)
    * Fixes UserInputError mesage display in FormError
    
    * Just display property form error messages
    
    * rename extensions to properties (continued)
    
    Co-authored-by: Dominic Saadi <dominic.saadi@hey.com>

--- packages/forms/src/FormError.tsx
+++ packages/forms/src/FormError.tsx
@@ -56,10 +56,13 @@ const FormError = ({
     !!error.networkError && Object.keys(error.networkError).length > 0
 
   if (hasGraphQLError) {
-    const errors = error.graphQLErrors[0].extensions?.exception?.messages
     rootMessage = error.graphQLErrors[0].message ?? 'Something went wrong.'
-    for (const e in errors) {
-      errors[e].forEach((fieldError: any) => {
+
+    const propertyMessages =
+      error.graphQLErrors[0].extensions?.properties?.messages
+
+    for (const e in propertyMessages) {
+      propertyMessages[e].forEach((fieldError: any) => {
         messages.push(`${e} ${fieldError}`)
       })
     }
