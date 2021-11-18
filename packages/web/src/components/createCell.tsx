5b2f066318963158f45228feda62b588da5230dd
Aditya Pandey <aditya@autospace.co>
2021-11-19 03:32:16 +0530

    Feat: Allow to override `isEmpty` in Cells (#3694)
    * Allow `isEmpty` override in Cells.
    
    * rename default fn, pass data to Empty
    
    Co-authored-by: Dominic Saadi <dominic.saadi@hey.com>
    Co-authored-by: Dominic Saadi <32992335+jtoar@users.noreply.github.com>

--- packages/web/src/components/createCell.tsx
+++ packages/web/src/components/createCell.tsx
@@ -51,6 +51,12 @@ export type CellSuccessProps<TData = any> = Partial<
 export interface CreateCellProps<CellProps> {
   beforeQuery?: <TProps>(props: TProps) => { variables: TProps }
   QUERY: DocumentNode | ((variables: Record<string, unknown>) => DocumentNode)
+  isEmpty?: (
+    response: DataObject,
+    options: {
+      isDataEmpty: (data: DataObject) => boolean
+    }
+  ) => boolean
   afterQuery?: (data: DataObject) => DataObject
   Loading?: React.FC<CellLoadingProps & Partial<CellProps>>
   Failure?: React.FC<CellFailureProps & Partial<CellProps>>
@@ -107,7 +113,7 @@ const dataField = (data: DataObject) => {
   return data[Object.keys(data)[0]]
 }
 
-const isEmpty = (data: DataObject) => {
+const isDataEmpty = (data: DataObject) => {
   return isDataNull(data) || isDataEmptyArray(data)
 }
 
@@ -118,6 +124,7 @@ export function createCell<CellProps = any>({
     notifyOnNetworkStatusChange: true,
   }),
   QUERY,
+  isEmpty = isDataEmpty,
   afterQuery = (data) => ({ ...data }),
   Loading = () => <>Loading...</>,
   Failure,
@@ -154,9 +161,14 @@ export function createCell<CellProps = any>({
               throw error
             }
           } else if (data) {
-            if (typeof Empty !== 'undefined' && isEmpty(data)) {
+            if (
+              typeof Empty !== 'undefined' &&
+              isEmpty(data, { isDataEmpty })
+            ) {
               return (
-                <Empty {...{ updating: loading, ...queryRest, ...props }} />
+                <Empty
+                  {...{ ...data, updating: loading, ...queryRest, ...props }}
+                />
               )
             } else {
               return (
