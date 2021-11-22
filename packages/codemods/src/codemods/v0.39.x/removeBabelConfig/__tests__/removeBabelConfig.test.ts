14e299309211818ba5efb57bc432e6de03dd91d9
Daniel Choudhury <dannychoudhury@gmail.com>
2021-11-22 20:12:38 +0000

    tests failing, codemod needs some work!

--- packages/codemods/src/codemods/v0.39.x/removeBabelConfig/__tests__/removeBabelConfig.test.ts
+++ packages/codemods/src/codemods/v0.39.x/removeBabelConfig/__tests__/removeBabelConfig.test.ts
@@ -1,4 +1,8 @@
 import transform from '../removeBabelConfig'
-test('Checking testing mechanism', async () => {
+test('Should remove/rename configs correctly for the default setup', async () => {
   await matchFolderTransform(transform, 'defaultConfig')
+})
+
+test('Should preserve custom web config', async () => {
+  await matchFolderTransform(transform, 'webCustom')
 })
--- packages/codemods/testUtils/matchFolderTransform.ts
+++ packages/codemods/testUtils/matchFolderTransform.ts
@@ -2,8 +2,19 @@ import fs from 'fs'
 import path from 'path'
 
 import fg from 'fast-glob'
+import fse from 'fs-extra'
+import tempy from 'tempy'
 
-import { createProjectMock } from './index'
+export const createProjectMock = () => {
+  const tempDir = tempy.directory()
+
+  // // Override paths,
+  process.env.RWJS_CWD = tempDir
+  // // and add fake redwood.toml
+  fs.closeSync(fs.openSync(path.join(tempDir, 'redwood.toml'), 'w'))
+
+  return tempDir
+}
 
 export const matchFolderTransform = async (
   transformFunction: () => any,
@@ -20,9 +31,8 @@ export const matchFolderTransform = async (
 
   const GLOB_CONFIG = {
     absolute: false,
-    cwd: tempDir,
     dot: true,
-    ignore: ['redwood.toml', '.DS_Store'], // ignore the fake redwood.toml added for getRWPaths
+    ignore: ['redwood.toml', '**/*.DS_Store'], // ignore the fake redwood.toml added for getRWPaths
   }
 
   // Use require.resolve, so we can pass in ts/js/tsx without specifying
@@ -36,19 +46,19 @@ export const matchFolderTransform = async (
   const fixtureOutputDir = path.join(fixtureFolder, 'output')
 
   // Step 1: Copy files recursively from fixture folder to temp
-  fg.sync('**/*.*', GLOB_CONFIG).map((filePath) => {
-    fs.copyFileSync(
-      path.join(fixtureInputDir, filePath),
-      path.join(tempDir, filePath)
-    )
+  fse.copySync(fixtureInputDir, tempDir, {
+    overwrite: true,
   })
 
   // Step 2: Run transform against temp dir
-  await transformFunction()
+  // await transformFunction()
 
-  const transformedPaths = fg.sync('**/*.*', GLOB_CONFIG)
+  const transformedPaths = fg.sync('**/*', { ...GLOB_CONFIG, cwd: tempDir }) //?
 
-  const expectedPaths = fg.sync('**/*.*', GLOB_CONFIG)
+  const expectedPaths = fg.sync('**/*', {
+    ...GLOB_CONFIG,
+    cwd: fixtureOutputDir,
+  })
 
   // Step 3: Check output paths
   expect(transformedPaths).toEqual(expectedPaths)
