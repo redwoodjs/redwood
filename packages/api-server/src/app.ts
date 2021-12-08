15efa43794ee4eee2ea1e324c379537311aab26b
A. David Thyresson <dthyresson@gmail.com>
2021-12-08 13:13:44 -0500

    Simplify log message format for web

--- packages/api-server/src/app.ts
+++ packages/api-server/src/app.ts
@@ -1,14 +1,25 @@
+import crypto from 'crypto'
+
 import Fastify, { FastifyInstance } from 'fastify'
 
 export const createApp = (): FastifyInstance => {
   const app = Fastify({
+    genReqId: (req) => {
+      return (
+        crypto
+          .randomBytes(12)
+          .toString('hex')
+          .match(/.{1,8}/g)
+          ?.join('-') || req.ip
+      )
+    },
     logger: {
-      // These settings are identical to the default Redwood logger
       prettyPrint: process.env.NODE_ENV === 'development' && {
         colorize: true,
-        ignore: 'hostname,pid',
+        ignore: 'hostname,pid,responseTime,reqId,req,res',
         levelFirst: true,
-        messageFormat: false,
+        messageFormat:
+          '{reqId} {msg} {res.statusCode} {req.method} {responseTime}',
         translateTime: true,
       },
     },
