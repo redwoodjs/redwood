b65dec3419753ba4ea77d621649a9c1407cc281b
Orta Therox <git@orta.io>
2021-12-08 12:42:06 +0000

    Linter

--- packages/create-redwood-app/template/scripts/seed.ts
+++ packages/create-redwood-app/template/scripts/seed.ts
@@ -1,5 +1,4 @@
 import type { Prisma } from '@prisma/client'
-
 import { db } from 'api/src/lib/db'
 
 export default async () => {
@@ -10,7 +9,7 @@ export default async () => {
     //
     // Update "const data = []" to match your data model and seeding needs
     //
-    const data: Prisma.UserExampleCreateInput["data"][] = [
+    const data: Prisma.UserExampleCreateInput['data'][] = [
       // To try this example data with the UserExample model in schema.prisma,
       // uncomment the lines below and run 'yarn rw prisma migrate dev'
       //
@@ -29,7 +28,7 @@ export default async () => {
       //
       // Change to match your data model and seeding needs
       //
-      data.map(async (data: Prisma.UserExampleCreateInput["data"]) => {
+      data.map(async (data: Prisma.UserExampleCreateInput['data']) => {
         const record = await db.userExample.create({ data })
         console.log(record)
       })
