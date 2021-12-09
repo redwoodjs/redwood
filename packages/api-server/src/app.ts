2b8ae507d7553cc9734a981321366ac9e77149c7
A. David Thyresson <dthyresson@gmail.com>
2021-12-08 19:17:49 -0500

    Use randomUUID for requestId instead

--- packages/api-server/src/app.ts
+++ packages/api-server/src/app.ts
@@ -4,15 +4,7 @@ import Fastify, { FastifyInstance } from 'fastify'
 
 export const createApp = (): FastifyInstance => {
   const app = Fastify({
-    genReqId: (req) => {
-      return (
-        crypto
-          .randomBytes(12)
-          .toString('hex')
-          .match(/.{1,8}/g)
-          ?.join('-') || req.ip
-      )
-    },
+    genReqId: (req) => crypto.randomUUID() || req.ip,
     logger: {
       prettyPrint: process.env.NODE_ENV === 'development' && {
         colorize: true,
