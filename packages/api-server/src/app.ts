aa5a191e7d1345a5bd5236805a4886f4238983ae
A. David Thyresson <dthyresson@gmail.com>
2021-12-14 07:19:32 -0500

    Update fastify and remove req id

--- packages/api-server/src/app.ts
+++ packages/api-server/src/app.ts
@@ -1,12 +1,7 @@
-import crypto from 'crypto'
-
 import Fastify, { FastifyInstance } from 'fastify'
 
 export const createApp = (): FastifyInstance => {
   const app = Fastify({
-    // Note: genReqId will not be called if the header set in requestIdHeader is available
-    // (defaults to 'request-id').
-    genReqId: () => crypto.randomUUID(),
     logger: {
       prettyPrint: process.env.NODE_ENV === 'development' && {
         colorize: true,
