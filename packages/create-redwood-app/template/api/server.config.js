003b3691b1906b2ec8c06b2758ee9b551e06655c
Daniel Choudhury <dannychoudhury@gmail.com>
2021-12-16 17:16:38 +0000

    WIP: Need to fix things

---
+++ packages/create-redwood-app/template/api/server.config.js
@@ -0,0 +1,18 @@
+/**
+ * This config allows you to configure the fastify settings for the
+ * dev api server, but also if you are using `yarn rw serve`
+ *
+ *  See https://www.fastify.io/docs/latest/Reference/Server/#factory
+ *
+ * Note: They do not apply on serverless deploy
+ */
+
+/** @type {import('fastify').FastifyServerOptions} */
+const config = {
+  requestTimeout: 15000,
+  logger: {
+    level: process.env.NODE_ENV === 'development' ? 'debug' : 'warn',
+  },
+}
+
+export default config
