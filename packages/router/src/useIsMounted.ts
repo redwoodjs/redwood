180a75771d11c09f6755b1a6f195bd3c6b84e011
Tobbe Lundberg <tobbe@tlundberg.com>
2021-12-19 00:47:56 +0100

    useIsMounted: Make it easier to read

--- packages/router/src/useIsMounted.ts
+++ packages/router/src/useIsMounted.ts
@@ -3,7 +3,11 @@ import { useRef, useEffect, useCallback } from 'react'
 export const useIsMounted = () => {
   const isMounted = useRef(true)
 
-  useEffect(() => () => void (isMounted.current = false), [])
+  useEffect(() => {
+    return () => {
+      isMounted.current = false
+    }
+  }, [])
 
   return useCallback(() => isMounted.current, [])
 }
