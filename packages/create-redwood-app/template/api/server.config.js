9dd1d6123be260bf4b158f210cb9c4073086ca69
A. David Thyresson <dthyresson@gmail.com>
2021-12-20 15:22:33 -0500

    Server Config file wordsmithing

--- packages/create-redwood-app/template/api/server.config.js
+++ packages/create-redwood-app/template/api/server.config.js
@@ -1,15 +1,20 @@
 /**
- * This config allows you to configure the fastify settings for the
- * dev api server, but also if you are using `yarn rw serve`
+ * This file allows you to configure the Fastify Server settings
+ * used by the  RedwoodJS dev server.
  *
- *  See https://www.fastify.io/docs/latest/Reference/Server/#factory
+ * It also applies when running the api server with `yarn rw serve`.
  *
- * Note: They do not apply on serverless deploy
+ * For the Fastify server options that you can set, see:
+ * https://www.fastify.io/docs/latest/Reference/Server/#factory
+ *
+ * Examples include: logger settings, timeouts, maximum payload limits, and more.
+ *
+ * Note: This configuration does not apply in a serverless deploy.
  */
 
 /** @type {import('fastify').FastifyServerOptions} */
 const config = {
-  requestTimeout: 15000,
+  requestTimeout: 15_000,
   logger: {
     level: process.env.NODE_ENV === 'development' ? 'debug' : 'warn',
   },
