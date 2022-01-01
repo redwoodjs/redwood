b41c082c4d911679060dbb982a7c36a8f1cd6358
Aditya Pandey <aditya@autospace.co>
2022-01-02 00:23:00 +0530

    Remove --runInBand for web side

--- packages/cli/src/commands/test.js
+++ packages/cli/src/commands/test.js
@@ -103,7 +103,7 @@ export const handler = async ({
     collectCoverage ? '--collectCoverage' : null,
     '--passWithNoTests',
     ...jestFilterArgs,
-    '--runInBand', // @TODO always run in band, even for web as we get babel errors https://github.com/redwoodjs/redwood/issues/3646
+    sides.includes('api') && '--runInBand',
   ].filter((flagOrValue) => flagOrValue !== null) // Filter out nulls, not booleans because user may have passed a --something false flag
 
   // If the user wants to watch, set the proper watch flag based on what kind of repo this is
