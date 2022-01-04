1957c3f61e32afa1c68a2d7bb489ab9dd7c33df9
Simon Gagnon <hello@simoncrypta.dev>
2022-01-03 21:14:04 -0500

    remove exclusion for ui.js

--- packages/cli/src/commands/setup.js
+++ packages/cli/src/commands/setup.js
@@ -1,12 +1,12 @@
 export const command = 'setup <commmand>'
-export const description = 'Initialize project config and install packages'
+export const description = 'Initialize project config and install packages 2'
 import terminalLink from 'terminal-link'
 
 import detectRwVersion from '../middleware/detectProjectRwVersion'
 
 export const builder = (yargs) =>
   yargs
-    .commandDir('./setup', { recurse: true, exclude: /\/ui\/.*\.js$/ })
+    .commandDir('./setup', { recurse: true })
     .demandCommand()
     .middleware(detectRwVersion)
     .epilogue(
