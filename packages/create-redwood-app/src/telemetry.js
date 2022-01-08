85729632b4b8e28798ef0c51f4ff21e9716229dc
David Price <thedavid@thedavidprice.com>
2022-01-07 16:19:37 -0800

    add GitHub Action Telemetry Check

--- packages/create-redwood-app/src/telemetry.js
+++ packages/create-redwood-app/src/telemetry.js
@@ -1,6 +1,13 @@
-const { sendTelemetry } = require('@redwoodjs/telemetry/dist/sendTelemetry')
-
 // if this script is run directly by node then telemetry will be sent in immediately
 ;(async function () {
-  await sendTelemetry()
+  // Use local framework Telemetry code in case of CI context
+  if (process.env.REDWOOD_CI == 1) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore-next-line
+    const { sendTelemetry } = require('../../telemetry/dist/sendTelemetry')
+    await sendTelemetry()
+  } else {
+    const { sendTelemetry } = require('@redwoodjs/telemetry/dist/sendTelemetry')
+    await sendTelemetry()
+  }
 })()
