de32bd7f97a18f5a54f31aab2da2f48d7079b894
Dominic Saadi <dominic.saadi@hey.com>
2022-01-10 15:27:06 -0800

    rm console log

--- tasks/start-e2e.js
+++ tasks/start-e2e.js
@@ -9,8 +9,6 @@ const RW_APP_PATH = path.isAbsolute(process.argv[2])
   ? process.argv[2]
   : path.resolve(process.argv[2])
 
-console.log({ RW_APP_PATH })
-
 execa.sync('yarn', ['rw', 'dev', '--no-generate', '--fwd="--no-open"'], {
   cwd: RW_APP_PATH,
   shell: true,
