94c13c78dd8a6eb77337708cbceba49fbf82fb71
Aditya Pandey <aditya@autospace.co>
2022-01-15 12:18:22 +0530

    Fix: Run web tests in parallel (#4024)
    * Fix TextDecoder missing from jsdom env
    
    * Refer babel-jest config from a file as much as possible
    
    * Remove --runInBand for web side
    
    * Revert "add textdecoder as global (#3611)"
    
    This reverts commit 228821d5545d909d556be10ba1ee9e0da7f9452e.
    
    * Add details and credit for custom web env
    
    * Add jest detail web jest setup file
    
    * Always run web tests in parallel
    
    * Fix tests: Don't expect --runInBand
    
    * Make api and web configs consistent
    
    Co-authored-by: Daniel Choudhury <dannychoudhury@gmail.com>

--- packages/testing/config/jest/web/index.js
+++ packages/testing/config/jest/web/index.js
@@ -1,17 +1,13 @@
 const path = require('path')
-const { TextDecoder } = require('util')
 
-const {
-  getPaths,
-  getWebSideDefaultBabelConfig,
-} = require('@redwoodjs/internal')
+const { getPaths } = require('@redwoodjs/internal')
 
 const rwjsPaths = getPaths()
 const NODE_MODULES_PATH = path.join(rwjsPaths.base, 'node_modules')
 
 module.exports = {
   roots: ['<rootDir>/src/'],
-  testEnvironment: 'jest-environment-jsdom',
+  testEnvironment: path.join(__dirname, './RedwoodWebJestEnv.js'),
   displayName: {
     color: 'blueBright',
     name: 'web',
@@ -20,7 +16,6 @@ module.exports = {
     __REDWOOD_API_URL: '',
     __REDWOOD_API_GRAPHQL_SERVER_PATH: '/',
     __REDWOOD__APP_TITLE: 'Redwood App',
-    TextDecoder,
   },
   setupFilesAfterEnv: [path.resolve(__dirname, './jest.setup.js')],
   moduleNameMapper: {
@@ -57,7 +52,11 @@ module.exports = {
   transform: {
     '\\.[jt]sx?$': [
       'babel-jest',
-      getWebSideDefaultBabelConfig({ forJest: true }),
+      // When jest runs tests in parallel, it serializes the config before passing down options to babel
+      // that's why these must be serializable. Passing the reference to a config instead.
+      {
+        configFile: path.resolve(__dirname, './webBabelConfig.js'),
+      },
     ],
   },
 }
