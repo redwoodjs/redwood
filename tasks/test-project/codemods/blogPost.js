29c06b6c787af0012b1e0d480916261a7c088127
Daniel Choudhury <dannychoudhury@gmail.com>
2022-01-18 11:23:02 +0000

    Make the last test pass

--- tasks/test-project/codemods/blogPost.js
+++ tasks/test-project/codemods/blogPost.js
@@ -1,14 +1,29 @@
 const body = `
 <article>
-  <header className="mt-4">
-    <p className="text-sm">
-      {new Intl.DateTimeFormat('en-US', {  year: 'numeric', month: 'long', day: 'numeric' }).format(new Date(blogPost.createdAt))}
-    </p>
-    <h2 className="text-xl mt-2 font-semibold">
-      <Link className="hover:text-blue-600" to={routes.blogPost({ id: blogPost.id })}>{blogPost.title}</Link>
-    </h2>
-  </header>
-  <div className="mt-2 mb-4 text-gray-900 font-light">{blogPost.body}</div>
+{blogPost && (
+  <>
+    <header className="mt-4">
+      <p className="text-sm">
+        {new Intl.DateTimeFormat('en-US', {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+        }).format(new Date(blogPost.createdAt))}
+      </p>
+      <h2 className="text-xl mt-2 font-semibold">
+        <Link
+          className="hover:text-blue-600"
+          to={routes.blogPost({ id: blogPost.id })}
+        >
+          {blogPost.title}
+        </Link>
+      </h2>
+    </header>
+    <div className="mt-2 mb-4 text-gray-900 font-light">
+      {blogPost.body}
+    </div>
+  </>
+)}
 </article>
 `
 
