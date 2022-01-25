a8054da8e1763b86ea0600cd24518e8dc6add4a0
Daniel Choudhury <dannychoudhury@gmail.com>
2022-01-25 23:19:28 +0600

    Move sls deploy command to a sub yargs handler (to support first setup flag)

--- packages/cli/src/commands/deploy/aws-providers/serverless.js
+++ packages/cli/src/commands/deploy/aws-providers/serverless.js
@@ -1,8 +1,23 @@
 import path from 'path'
 
-import { getPaths } from '@redwoodjs/internal'
+import execa from 'execa'
+import Listr from 'listr'
 
+import { getPaths } from '../../../lib'
+import c from '../../../lib/colors'
 import ntfPack from '../packing/nft'
+
+export const command = 'serverless'
+export const description = 'Deploy to AWS via the serverless framework'
+
+export const builder = (yargs) => {
+  yargs.option('stage', {
+    describe:
+      'serverless stage pass through param: https://www.serverless.com/blog/stages-and-environments',
+    default: 'dev',
+    type: 'string',
+  })
+}
 
 export const preRequisites = () => [
   {
@@ -37,4 +52,56 @@ export const deployCommands = ({ stage, sides }) => {
       cwd: path.join(getPaths().base, side),
     }
   })
+}
+
+export const handler = async (yargs) => {
+  const tasks = new Listr(
+    [
+      ...preRequisites(yargs).map(mapCommandsToListr),
+      ...buildCommands(yargs).map(mapCommandsToListr),
+      ...deployCommands(yargs).map(mapCommandsToListr),
+    ],
+    {
+      exitOnError: true,
+    }
+  )
+  try {
+    await tasks.run()
+  } catch (e) {
+    console.error(c.error(e.message))
+    process.exit(e?.exitCode || 1)
+  }
+}
+
+const mapCommandsToListr = ({
+  title,
+  command,
+  task,
+  cwd,
+  errorMessage,
+  skip,
+  enabled,
+}) => {
+  return {
+    title,
+    task: task
+      ? task
+      : async () => {
+          try {
+            const executingCommand = execa(...command, {
+              cwd: cwd || getPaths().base,
+              shell: true,
+            })
+            executingCommand.stdout.pipe(process.stdout)
+            await executingCommand
+          } catch (error) {
+            if (errorMessage) {
+              error.message = error.message + '\n' + errorMessage.join(' ')
+            }
+            throw error
+          }
+        },
+    skip,
+    enabled,
+  }
 }
--- packages/cli/src/commands/deploy/aws.js
+++ packages/cli/src/commands/deploy/aws.js
@@ -1,30 +1,18 @@
-import fs from 'fs'
-import path from 'path'
-
-import execa from 'execa'
-import Listr from 'listr'
-import VerboseRenderer from 'listr-verbose-renderer'
 import terminalLink from 'terminal-link'
 
-import { errorTelemetry } from '@redwoodjs/telemetry'
-
-import { getPaths } from '../../lib'
-import c from '../../lib/colors'
-
-export const command = 'aws [provider]'
+export const command = 'aws <provider>'
 export const description = 'Deploy to AWS using the selected provider'
 export const builder = (yargs) => {
-  const SUPPORTED_PROVIDERS = fs
-    .readdirSync(path.resolve(__dirname, 'aws-providers'))
-    .map((file) => path.basename(file, '.js'))
-    .filter((file) => file !== 'README.md')
-
   yargs
-    .positional('provider', {
-      choices: SUPPORTED_PROVIDERS,
-      default: 'serverless',
-      description: 'AWS Deploy provider to configure',
-      type: 'string',
+    .commandDir('./aws-providers', {
+      recurse: true,
+      /*
+    @NOTE This regex will ignore all double nested commands
+    e.g. /setup/hi.js & setup/hi/hi.js are picked up,
+    but setup/hi/hello/bazinga.js will be ignored
+    The [\/\\] bit is for supporting both windows and unix style paths
+    */
+      exclude: /aws-providers[\/\\]+.*[\/\\]+.*[\/\\]/,
     })
     .option('sides', {
       describe: 'which Side(s) to deploy',
@@ -38,88 +26,10 @@ export const builder = (yargs) => {
       default: true,
       type: 'boolean',
     })
-    .option('stage', {
-      describe:
-        'serverless stage pass through param: https://www.serverless.com/blog/stages-and-environments',
-      default: 'dev',
-      type: 'string',
-    })
     .epilogue(
       `Also see the ${terminalLink(
         'Redwood CLI Reference',
         'https://redwoodjs.com/docs/cli-commands#deploy'
       )}\n`
     )
-}
-
-export const handler = async (yargs) => {
-  const { provider, verbose } = yargs
-  const BASE_DIR = getPaths().base
-  const providerData = await import(`./aws-providers/${provider}`)
-
-  const mapCommandsToListr = ({
-    title,
-    command,
-    task,
-    cwd,
-    errorMessage,
-    skip,
-    enabled,
-  }) => {
-    return {
-      title: title,
-      task: task
-        ? task
-        : async () => {
-            try {
-              const executingCommand = execa(...command, {
-                cwd: cwd || BASE_DIR,
-              })
-              executingCommand.stdout.pipe(process.stdout)
-              await executingCommand
-            } catch (error) {
-              if (errorMessage) {
-                error.message = error.message + '\n' + errorMessage.join(' ')
-              }
-              throw error
-            }
-          },
-      skip,
-      enabled,
-    }
-  }
-
-  const tasks = new Listr(
-    [
-      providerData.preRequisites &&
-        providerData.preRequisites.length > 0 && {
-          title: 'Checking pre-requisites',
-          task: () =>
-            new Listr(
-              providerData.preRequisites(yargs).map(mapCommandsToListr)
-            ),
-        },
-      {
-        title: 'Building and Packaging...',
-        task: () =>
-          new Listr(providerData.buildCommands(yargs).map(mapCommandsToListr), {
-            collapse: false,
-          }),
-      },
-      {
-        title: 'Deploying to AWS',
-        task: () =>
-          new Listr(providerData.deployCommands(yargs).map(mapCommandsToListr)),
-      },
-    ].filter(Boolean),
-    { collapse: false, renderer: verbose && VerboseRenderer }
-  )
-
-  try {
-    await tasks.run()
-  } catch (e) {
-    errorTelemetry(process.argv, e.message)
-    console.log(c.error(e.message))
-    process.exit(1)
-  }
 }
