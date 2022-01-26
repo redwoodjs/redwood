1370646abe59d7d193c2d66666a9f15f274171b0
Rob Cameron <rob.cameron@fastmail.com>
2022-01-26 11:27:59 -0800

    Adds -web and -api suffixes to service names

--- packages/cli/src/commands/setup/deploy/templates/serverless/api.js
+++ packages/cli/src/commands/setup/deploy/templates/serverless/api.js
@@ -6,7 +6,7 @@ import { getPaths } from '../../../../../lib'
 export const PROJECT_NAME = path.basename(getPaths().base)
 
 export const SERVERLESS_API_YML = `# See the full yml reference at https://www.serverless.com/framework/docs/providers/aws/guide/serverless.yml/
-service: ${PROJECT_NAME}
+service: ${PROJECT_NAME}-api
 
 # Uncomment org and app if you want to integrate your deployment with the
 # Serverless dashboard, or run \`serverless\` to be prompted to connect.
