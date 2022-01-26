d6939ae33d2bf16bda0c5ec3b51d3c275877b108
Rob Cameron <rob.cameron@fastmail.com>
2022-01-26 11:33:43 -0800

    Template comment updates

--- packages/cli/src/commands/setup/deploy/templates/serverless/web.js
+++ packages/cli/src/commands/setup/deploy/templates/serverless/web.js
@@ -7,8 +7,9 @@ export const PROJECT_NAME = path.basename(getPaths().base)
 export const SERVERLESS_WEB_YML = `# See the full yml reference at https://www.serverless.com/framework/docs/providers/aws/guide/serverless.yml/
 service: ${PROJECT_NAME}-web
 
-# Uncomment org and app if you want to integrate your deployment with the
-# Serverless dashboard, or run \`serverless\` to be prompted to connect.
+# Uncomment \`org\` and \`app\` and enter manually if you want to integrate your
+# deployment with the Serverless dashboard, or run \`yarn rw serverless\` to be
+# prompted to connect to an app and these will be filled in for you.
 # See https://www.serverless.com/framework/docs/dashboard/ for more details.
 # org: your-org
 # app: your-app
