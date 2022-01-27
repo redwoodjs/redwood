8b95fc76a926763540898f7fc9f74bf3f1feac80
Dominic Saadi <32992335+jtoar@users.noreply.github.com>
2022-01-27 14:38:43 -0800

    Add release tooling (#4274)
    * add release tooling
    
    * correct package json script
    
    * remove notes
    
    * remove comments
    
    * improve comments, disable some functionality
    
    * flesh out release patch
    
    * fix constraints
    
    * mv release-notes
    
    * rename release notes files
    
    * update yarn lock, many changes
    
    * updates
    
    * dedupe
    
    * update, add tests
    
    * tests
    
    * add msw, make more interactive
    
    * test updates
    
    * restore choices array
    
    * handle release notes
    
    * add confirm
    
    * improve prompts
    
    * prompts
    
    * copy
    
    * fix bug in prompt
    
    * prompts
    
    * updates
    
    * fix
    
    * updates
    
    Co-authored-by: David Price <thedavid@thedavidprice.com>

---
+++ tasks/release/cli.mjs
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+/* eslint-env node, es2021 */
+
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+
+import generateReleaseNotes from './generateReleaseNotes.mjs'
+import release from './release.mjs'
+import updateNextReleasePullRequestsMilestone from './updateNextReleasePullRequestsMilestone.mjs'
+
+yargs(hideBin(process.argv))
+  .scriptName('release')
+  .command('$0', 'Release RedwoodJS', {}, release)
+  .command(
+    'generate-release-notes [milestone]',
+    'Generates release notes for a given milestone',
+    (yargs) => {
+      yargs.positional('milestone', {
+        describe: 'The milestone to generate release notes for',
+        type: 'string',
+      })
+    },
+    (argv) => generateReleaseNotes(argv.milestone)
+  )
+  .command(
+    'update-next-release-prs-milestone <milestone>',
+    "Update next-release PRs' milestone. Note that this creates the milestone if it doesn't exist",
+    (yargs) => {
+      yargs.positional('milestone', {
+        describe: 'The milestone to update next-release PRs to',
+        type: 'string',
+      })
+    },
+    (argv) => updateNextReleasePullRequestsMilestone(argv.milestone)
+  )
+  .help()
+  .parse()
