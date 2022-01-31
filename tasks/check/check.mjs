90fa4a9f3f8fc866736c5e68f3bd53da333a0197
Dominic Saadi <dominic.saadi@hey.com>
2022-01-31 16:06:12 -0500

    prefer exec to getExecOutput

--- tasks/check/check.mjs
+++ tasks/check/check.mjs
@@ -1,13 +1,13 @@
 /* eslint-env node, es2021 */
 import core from '@actions/core'
-import { getExecOutput } from '@actions/exec'
+import { exec } from '@actions/exec'
 
 import { ok, fix } from '../release/prompts.mjs'
 
 console.log('-'.repeat(80))
 const constraintsCheck = 'yarn constraints'
 try {
-  await getExecOutput(constraintsCheck)
+  await exec(constraintsCheck)
   console.log(ok`${constraintsCheck} passed`)
 } catch (_e) {
   core.setFailed(`"${constraintsCheck}" failed`)
@@ -17,7 +17,7 @@ try {
 console.log('-'.repeat(80))
 const dedupeCheck = 'yarn dedupe --check'
 try {
-  await getExecOutput(dedupeCheck)
+  await exec(dedupeCheck)
   console.log(ok`${dedupeCheck} passed`)
 } catch (_e) {
   core.setFailed(`"${dedupeCheck}" failed`)
@@ -28,7 +28,8 @@ console.log('-'.repeat(80))
 const packageJSONsCheck =
   'yarn workspaces foreach --parallel dlx sort-package-json --check'
 try {
-  await getExecOutput(packageJSONsCheck, null, { silent: true })
+  // The output from this is just too much.
+  await exec(packageJSONsCheck, null, { silent: true })
   console.log(ok`${packageJSONsCheck} passed`)
 } catch (_e) {
   core.setFailed(`"${packageJSONsCheck}" failed`)
