af48e1ddcaf728c71d955df08671e2d78ad2da1e
Daniel Choudhury <dannychoudhury@gmail.com>
2022-02-03 01:36:44 +0600

    Update links to docs

--- packages/create-redwood-app/template/jest.config.js
+++ packages/create-redwood-app/template/jest.config.js
@@ -1,6 +1,6 @@
 // This the Redwood root jest config
 // Each side, e.g. ./web/ and ./api/ has specific config that references this root
-// More info at https://redwoodjs.com/docs/project-configuration
+// More info at https://redwoodjs.com/docs/project-configuration-dev-test-build
 
 module.exports = {
   rootDir: '.',
--- packages/create-redwood-app/template/web/jest.config.js
+++ packages/create-redwood-app/template/web/jest.config.js
@@ -1,4 +1,4 @@
-// More info at https://redwoodjs.com/docs/project-configuration
+// More info at https://redwoodjs.com/docs/project-configuration-dev-test-build
 
 const config = {
   rootDir: '../',
