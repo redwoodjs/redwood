d610a4e82a06293f5f8e4980ff7de8fa8114faff
Syeda Zainab <rimshazainab@hotmail.com>
2022-02-02 22:30:52 +0400

    added counts for warnings and errors separately

--- packages/structure/src/index.ts
+++ packages/structure/src/index.ts
@@ -22,22 +22,29 @@ export async function printDiagnostics(
   const project = getProject(projectRoot)
   const formatOpts = { cwd: projectRoot, ...opts }
   try {
-    let exceptions = 0
+    let warnings = 0
+    let errors = 0
     for (const d of await project.collectDiagnostics()) {
       const str = ExtendedDiagnostic_format(d, formatOpts)
       console.log(str)
       // counts number of warnings (2) and errors (1) encountered
-      if (d.diagnostic.severity === 2 || d.diagnostic.severity === 1) {
-        exceptions++
+      if (d.diagnostic.severity === 2) {
+        warnings++
+      }
+      if (d.diagnostic.severity === 1) {
+        errors++
       }
     }
 
-    if (exceptions === 0) {
+    if (warnings === 0 && errors === 0) {
       console.log('Success: no errors or warnings were detected')
     }
+    else if (errors > 0) {
+      throw new Error(`Failure: ${errors} errors and ${warnings} warnings detected`)
+    }
+
   } catch (e: any) {
-    console.log('runtime error: ' + e.message)
-    throw e
+    throw new Error(e.message)
   }
 }
 
