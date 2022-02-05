c733c73ff1b40d7806f3d66117ace14323c1bfff
Dominic Saadi <dominic.saadi@hey.com>
2022-02-05 15:56:04 -0500

    mock fetching file

--- packages/codemods/src/codemods/v0.44.x/updateJestConfig/__tests__/updateJestConfig.test.ts
+++ packages/codemods/src/codemods/v0.44.x/updateJestConfig/__tests__/updateJestConfig.test.ts
@@ -1,12 +1,46 @@
 import updateJestConfig from '../updateJestConfig'
 
+jest.mock('../../../../lib/fetchFileFromTemplate', () =>
+  jest.fn((_tag, file) => {
+    if (file === 'jest.config.js') {
+      return [
+        '// This the Redwood root jest config',
+        '// Each side, e.g. ./web/ and ./api/ has specific config that references this root',
+        '// More info at https://redwoodjs.com/docs/project-configuration-dev-test-build',
+        '',
+        'module.exports = {',
+        "  rootDir: '.',",
+        "  projects: ['<rootDir>/{*,!(node_modules)/**/}/jest.config.js'],",
+        '}',
+      ].join('\n')
+    }
+
+    return [
+      '// More info at https://redwoodjs.com/docs/project-configuration-dev-test-build',
+      '',
+      'const config = {',
+      "  rootDir: '../',",
+      `  preset: '@redwoodjs/testing/config/jest/${
+        file.match(/(?<side>api|web)/).groups.side
+      }',`,
+      '}',
+      '',
+      'module.exports = config',
+    ].join('\n')
+  })
+)
+
 describe('Update Jest Config', () => {
   it('Adds missing files', async () => {
-    await matchFolderTransform(updateJestConfig, 'missing')
+    await matchFolderTransform(updateJestConfig, 'missing', {
+      removeWhitespace: true,
+    })
   })
 
   it('Updates the v0.43.0 template', async () => {
-    await matchFolderTransform(updateJestConfig, 'default')
+    await matchFolderTransform(updateJestConfig, 'default', {
+      removeWhitespace: true,
+    })
   })
 
   it('Keeps custom jest config in api and web', async () => {
