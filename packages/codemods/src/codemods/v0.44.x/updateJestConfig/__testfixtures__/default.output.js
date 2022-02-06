9b3ae15b53dc942ac307f02c1af3230d01646a48
Dominic Saadi <32992335+jtoar@users.noreply.github.com>
2022-02-05 22:32:10 -0500

    Try fixing codemod test timeout (#4389)
    * mock fetching file
    
    * remove redundant tests

--- packages/codemods/src/codemods/v0.44.x/updateJestConfig/__testfixtures__/default.output.js
+++
@@ -1,8 +0,0 @@
-// More info at https://redwoodjs.com/docs/project-configuration-dev-test-build
-
-const config = {
-  rootDir: '../',
-  preset: '@redwoodjs/testing/config/jest/api',
-}
-
-module.exports = config
--- packages/codemods/src/codemods/v0.44.x/updateJestConfig/__testfixtures__/default/output/web/jest.config.js
+++
@@ -1,8 +0,0 @@
-// More info at https://redwoodjs.com/docs/project-configuration-dev-test-build
-
-const config = {
-  rootDir: '../',
-  preset: '@redwoodjs/testing/config/jest/web',
-}
-
-module.exports = config
