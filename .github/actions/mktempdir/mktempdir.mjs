fb886ced3ba018d8be36854cabfffd393cbb7437
Dominic Saadi <dominic.saadi@hey.com>
2022-02-16 19:31:51 +0900

    do it all in an action

--- .github/actions/mktempdir/mktempdir.mjs
+++
@@ -1,6 +0,0 @@
-import * as core from '@actions/core';
-import { getExecOutput } from '@actions/exec'
-
-const { stdout } = await getExecOutput('mktemp -d -t redwood.XXXXXX')
-
-core.setOutput('project_path', stdout)
