521290d0575b6142857a7bad69d136bb6f048388
Daniel Choudhury <dannychoudhury@gmail.com>
2022-02-17 17:30:01 +0700

    Move visual checks to rw serve, reduce threshold

--- tasks/smoke-test/tests/rwServe.spec.ts
+++ tasks/smoke-test/tests/rwServe.spec.ts
@@ -1,3 +1,5 @@
+import { expect } from '@playwright/test'
+
 import rwServeTest from '../playwright-fixtures/rwServe.fixture'
 
 import { smokeTest } from './common'
@@ -5,3 +7,14 @@ import { smokeTest } from './common'
 rwServeTest('Smoke test with rw serve', ({ port, page }) =>
   smokeTest({ webServerPort: port, page })
 )
+
+rwServeTest('Visual check', async ({ page, port }) => {
+  await page.goto(`http://localhost:${port}/`)
+
+  // Wait till cell has finished rendering
+  await page.textContent('text=Welcome to the blog!')
+
+  expect(await page.screenshot()).toMatchSnapshot('landing.png', {
+    threshold: 0.5, // reduce sensitivity of visual comparison
+  })
+})
