80869b9f830f7a4902cc61485708a9bacafbe889
Daniel Choudhury <dannychoudhury@gmail.com>
2022-02-17 15:25:32 +0700

    Remove copy from setup_test_project

--- .github/actions/setup_test_project/setup_test_project.mjs
+++ .github/actions/setup_test_project/setup_test_project.mjs
@@ -3,7 +3,6 @@ import path from 'node:path'
 
 import { exec } from '@actions/exec'
 import * as core from '@actions/core'
-import * as io from '@actions/io'
 
 const test_project_path = path.join(
   os.tmpdir(),
@@ -18,15 +17,5 @@ console.log({
 
 core.setOutput('test_project_path', test_project_path)
 
-// See https://github.com/actions/toolkit/tree/main/packages/io#cpmv.
-console.log('Copying test project fixture')
-await io.cp(
-  './__fixtures__/test-project',
-  test_project_path,
-  {
-    recursive: true,
-    copySourceDirectory: false
-  }
-)
 
 await exec(`yarn build:test-project --ts --link ${test_project_path}`)
