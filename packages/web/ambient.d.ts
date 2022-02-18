cda21124e1c76ac124802cef94f57127c74990b9
Dominic Saadi <dominic.saadi@hey.com>
2022-02-18 16:00:41 +0900

    rename helmet data to helmet server state

--- packages/web/ambient.d.ts
+++ packages/web/ambient.d.ts
@@ -1,11 +1,11 @@
 /* eslint-disable no-var */
-import type { HelmetData } from 'react-helmet-async'
+import type { HelmetServerState } from 'react-helmet-async'
 
 import type { AuthContextInterface } from '@redwoodjs/auth'
 
 declare global {
   var __REDWOOD__PRERENDERING: boolean
-  var __REDWOOD__HELMET_CONTEXT: { helmet?: HelmetData }
+  var __REDWOOD__HELMET_CONTEXT: { helmet?: HelmetServerState }
   var __REDWOOD__APP_TITLE: string
   var __REDWOOD__USE_AUTH: () => AuthContextInterface
 
@@ -22,7 +22,7 @@ declare global {
        * This global is set to true by the prerendering CLI command.
        */
       __REDWOOD__PRERENDERING: boolean
-      __REDWOOD__HELMET_CONTEXT: { helmet?: HelmetData }
+      __REDWOOD__HELMET_CONTEXT: { helmet?: HelmetServerState }
       __REDWOOD__APP_TITLE: string
       __REDWOOD__USE_AUTH: () => AuthContextInterface
 
