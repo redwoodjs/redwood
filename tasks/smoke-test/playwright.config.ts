55a322fc14defe3b0ff851dcae368f4fac248348
Daniel Choudhury <dannychoudhury@gmail.com>
2022-02-21 13:09:10 +0700

    Cleanup

--- tasks/smoke-test/playwright.config.ts
+++ tasks/smoke-test/playwright.config.ts
@@ -1,16 +1,8 @@
 import { PlaywrightTestConfig } from '@playwright/test'
+
+// See https://playwright.dev/docs/test-configuration#global-configuration
 const config: PlaywrightTestConfig = {
-  timeout: 60000,
-  use: {
-    // Browser options
-    // headless: false,
-    // slowMo: 1000,
-    // Context options
-    // viewport: { width: 1280, height: 720 },
-    // ignoreHTTPSErrors: true,
-    // Artifacts
-    // screenshot: 'only-on-failure',
-    // video: 'retry-with-video',
-  },
+  timeout: 60_000,
 }
+
 export default config
