c865f9e4220a1c8e4991137d38f28d76a7c2dfc0
Daniel Choudhury <dannychoudhury@gmail.com>
2022-02-23 11:02:37 +0000

    Update linux snapshot using codespaces

--- tasks/smoke-test/tests/rwServe.spec.ts
+++ tasks/smoke-test/tests/rwServe.spec.ts
@@ -1,3 +1,5 @@
+import { expect } from '@playwright/test'
+
 import rwServeTest from '../playwright-fixtures/rwServe.fixture'
 
 import { smokeTest } from './common'
@@ -6,16 +8,16 @@ rwServeTest('Smoke test with rw serve', ({ port, page }) =>
   smokeTest({ webServerPort: port, page })
 )
 
-// @TODO it might be useful to have this visual check
-// But gitpod screenshot doesnt seem to match up with github runners
-// Possibly due to fonts
-// rwServeTest('Visual check', async ({ page, port }) => {
-//   await page.goto(`http://localhost:${port}/`)
+// @NOTE: This may be a flaky test
+// Keeping in for now, but should this start failing, we should take it out
+// Linux snapshot created using github codespaces
+rwServeTest('Visual check', async ({ page, port }) => {
+  await page.goto(`http://localhost:${port}/`)
 
-//   // Wait till cell has finished rendering
-//   await page.textContent('text=Welcome to the blog!')
+  // Wait till cell has finished rendering
+  await page.textContent('text=Welcome to the blog!')
 
-//   expect(await page.screenshot()).toMatchSnapshot('landing.png', {
-//     threshold: 0.2, // reduce sensitivity of visual comparison
-//   })
-// })
+  expect(await page.screenshot()).toMatchSnapshot('landing.png', {
+    threshold: 0.2, // reduce sensitivity of visual comparison
+  })
+})
