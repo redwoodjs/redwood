1c6045ab56e13df916b4698418d6abf3f5d9186d
Dominic Saadi <dominic.saadi@hey.com>
2022-02-24 13:17:42 +0900

    get dev working

---
+++ packages/core/src/bins/cross-env.ts
@@ -0,0 +1,9 @@
+import { createRequire } from 'module'
+
+const requireFromCrossEnv = createRequire(
+  require.resolve('cross-env/package.json')
+)
+
+const bins = requireFromCrossEnv('./package.json')['bin']
+
+requireFromCrossEnv(`./${bins['cross-env']}`)
---
+++ packages/core/src/bins/rw-api-server-watch.ts
@@ -0,0 +1,9 @@
+import { createRequire } from 'module'
+
+const requireFromApiServer = createRequire(
+  require.resolve('@redwoodjs/api-server/package.json')
+)
+
+const bins = requireFromApiServer('./package.json')['bin']
+
+requireFromApiServer(bins['rw-api-server-watch'])
