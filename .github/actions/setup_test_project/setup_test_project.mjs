049662397f8b8e4a991ce293b23ef69a21eb0ff1
David Price <thedavid@thedavidprice.com>
2022-02-24 12:29:06 -0800

    add isTsProject check to setup; try test fail

--- .github/actions/setup_test_project/setup_test_project.mjs
+++ .github/actions/setup_test_project/setup_test_project.mjs
@@ -3,6 +3,7 @@ import path from 'node:path'
 
 import { exec } from '@actions/exec'
 import * as core from '@actions/core'
+import { getProject } from '@redwoodjs/structure'
 
 const test_project_path = path.join(
   os.tmpdir(),
@@ -17,5 +18,11 @@ console.log({
 
 core.setOutput('test_project_path', test_project_path)
 
-
 await exec(`yarn build:test-project --ts --link ${test_project_path}`)
+
+try {
+  if(!getProject().isTypeScriptProject) throw 'Error: Test-project is expected to be TypeScript'
+} catch(e) {
+  console.error(`\n${e}\nExiting test-project setup.\n`)
+  process.exit(1)
+}
