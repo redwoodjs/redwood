f9803f61a032d3488c0c2c25a2ca295b3d94e74f
Dominic Saadi <dominic.saadi@hey.com>
2022-02-26 00:30:57 +0900

    get test working

--- packages/core/src/bins/jest.ts
+++ packages/core/src/bins/jest.ts
@@ -3,6 +3,6 @@ import { createRequire } from 'module'
 
 const requireFromJest = createRequire(require.resolve('jest/package.json'))
 
-const bins = requireFromJest('./package.json')['bin']
+const bin = requireFromJest('./package.json')['bin']
 
-requireFromJest(bins['jest'])
+requireFromJest(bin)
