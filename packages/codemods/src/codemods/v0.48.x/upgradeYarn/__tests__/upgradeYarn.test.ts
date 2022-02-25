3abc9af463bf2893ee4070d2ac8e9d04abbe3842
Dominic Saadi <dominic.saadi@hey.com>
2022-02-25 16:39:49 +0900

    iterate on ci

--- packages/codemods/src/codemods/v0.48.x/upgradeYarn/__tests__/upgradeYarn.test.ts
+++
@@ -1,9 +0,0 @@
-// TODO. The fixture is simple, but this codemod uses the network a lot.
-//
-// import upgradeYarn from '../upgradeYarn'
-//
-// describe('upgradeYarn', () => {
-//   it('Changes the structure of a redwood project', async () => {
-//     await matchFolderTransform(upgradeYarn, 'default')
-//   })
-// })
