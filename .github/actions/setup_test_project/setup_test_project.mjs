74e62076d7aaec9ce0c6cac7dfda42dcbf2f7fd0
David Price <thedavid@thedavidprice.com>
2022-02-28 12:53:20 -0800

    fix dbAuth Auth template roles logic and types, run type-check on Smoke-test CI, and resolve TS errors (#4566)
    * run type-check on Smoke-test CI
    
    * Generate ts project, fix logic in rebuild fixture
    
    * Fix ts errors caused by dbauth
    
    * Regenerate fixture
    
    * add isTsProject check to setup; try test fail
    
    * add correct project path to TS check
    
    * give GH Actions a kick
    
    * use fs 'cause within Action
    
    * fail fast smoke test
    
    * Revert "Regenerate fixture"
    
    This reverts commit 7e833db52a1f5fe2acc8dcafaf469c74bdd0b59a.
    
    * improve error message
    
    * fixes TS project check logic
    
    * Update dbauth template with case when currentUser has array of roles, but supplied roles is string
    
    * Ensure latest auth packages get used in rebuild
    
    * remove packageManager from package.json
    
    * Update db auth models to include roles
    
    * Update tasks/test-project/test-project
    
    Co-authored-by: David Price <thedavid@thedavidprice.com>
    
    * Update get current user
    
    * Add roles to directive test too
    
    * Update require auth test in fixture
    
    * add sanity check when building w/out Fixture
    
    * updates Fixture with new Seed type #4560
    
    Co-authored-by: Daniel Choudhury <dannychoudhury@gmail.com>

--- .github/actions/setup_test_project/setup_test_project.mjs
+++ .github/actions/setup_test_project/setup_test_project.mjs
@@ -1,5 +1,6 @@
 import os from 'node:os'
 import path from 'node:path'
+import fs from 'fs-extra'
 
 import { exec } from '@actions/exec'
 import * as core from '@actions/core'
@@ -17,5 +18,18 @@ console.log({
 
 core.setOutput('test_project_path', test_project_path)
 
-
 await exec(`yarn build:test-project --ts --link ${test_project_path}`)
+
+try {
+  if (
+    !fs.existsSync(path.join(test_project_path, 'web/tsconfig.json')) ||
+    !fs.existsSync(path.join(test_project_path, 'api/tsconfig.json'))
+  ) {
+    throw ('Test-project is not TypeScript')
+  }
+} catch(e) {
+  console.log('********************************')
+  console.error('\nError: Test-project is expected to be TypeScript\nExiting test-project setup.\n')
+  console.log('********************************')
+  process.exit(1)
+}
