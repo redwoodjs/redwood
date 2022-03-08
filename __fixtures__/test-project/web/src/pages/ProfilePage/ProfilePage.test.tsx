428e56739a0348c40d371b72ff858b93c887986d
Daniel Choudhury <dannychoudhury@gmail.com>
2022-03-09 00:07:11 +0700

    Update test-project fixture

--- __fixtures__/test-project/web/src/pages/ProfilePage/ProfilePage.test.tsx
+++ __fixtures__/test-project/web/src/pages/ProfilePage/ProfilePage.test.tsx
@@ -1,14 +1,21 @@
-import { render } from '@redwoodjs/testing/web'
+import { render, waitFor, screen } from '@redwoodjs/testing/web'
 
 import ProfilePage from './ProfilePage'
 
-//   Improve this test with help from the Redwood Testing Doc:
-//   https://redwoodjs.com/docs/testing#testing-pages-layouts
-
 describe('ProfilePage', () => {
-  it('renders successfully', () => {
-    expect(() => {
-      render(<ProfilePage />)
-    }).not.toThrow()
+  it('renders successfully', async () => {
+    mockCurrentUser({
+      email: 'danny@bazinga.com',
+      id: 84849020,
+      roles: 'BAZINGA',
+    })
+
+    await waitFor(async () => {
+      expect(() => {
+        render(<ProfilePage />)
+      }).not.toThrow()
+    })
+
+    expect(await screen.findByText('danny@bazinga.com')).toBeInTheDocument()
   })
 })
