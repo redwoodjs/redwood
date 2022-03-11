338e95f7af37bb6ee3abdbec573f76e2d24c71a8
Daniel Choudhury <dannychoudhury@gmail.com>
2022-03-11 16:55:01 +0700

    Update fixture

--- __fixtures__/test-project/api/db/migrations/20220309090020_create_contact/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Contact" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "message" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
