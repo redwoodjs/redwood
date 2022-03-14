f4869621f30a6f5c898991488ef35d49666014fb
Rob Cameron <rob.cameron@fastmail.com>
2022-03-14 10:03:40 -0700

    Include entry for pm2 web process

--- packages/cli/src/commands/setup/deploy/templates/baremetal/ecosystem.config.js
+++ packages/cli/src/commands/setup/deploy/templates/baremetal/ecosystem.config.js
@@ -9,5 +9,14 @@ module.exports = {
       wait_ready: true,
       listen_timeout: 10000,
     },
+    {
+      name: 'web',
+      script: 'node_modules/.bin/rw',
+      args: 'serve web',
+      instances: 'max',
+      exec_mode: 'cluster',
+      wait_ready: true,
+      listen_timeout: 10000,
+    },
   ],
 }
