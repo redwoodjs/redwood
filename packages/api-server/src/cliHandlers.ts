9ef723e836fa8196e1d25204283d3605dd8ad834
Rob Cameron <rob.cameron@fastmail.com>
2022-03-13 20:15:43 -0700

    Abstract to function

--- packages/api-server/src/cliHandlers.ts
+++ packages/api-server/src/cliHandlers.ts
@@ -18,6 +18,10 @@ import type { HttpServerParams } from './server'
  * Also used in index.ts for the api server
  */
 
+const sendProcessReady = () => {
+  return process.send && process.send('ready')
+}
+
 export const commonOptions = {
   port: { default: getConfig().web?.port || 8910, type: 'number', alias: 'p' },
   socket: { type: 'string' },
@@ -74,9 +78,7 @@ export const apiServerHandler = async ({
     console.log(`API listening on ${on}`)
     const graphqlEnd = c.magenta(`${apiRootPath}graphql`)
     console.log(`GraphQL endpoint at ${graphqlEnd}`)
-    if (typeof process.send !== 'undefined') {
-      process.send('ready')
-    }
+    sendProcessReady()
   })
   process.on('exit', () => {
     http?.close()
@@ -113,9 +115,7 @@ export const bothServerHandler = async ({
     console.log(`API listening on ${on}`)
     const graphqlEnd = c.magenta(`${apiRootPath}graphql`)
     console.log(`GraphQL endpoint at ${graphqlEnd}`)
-    if (typeof process.send !== 'undefined') {
-      process.send('ready')
-    }
+    sendProcessReady()
   })
 }
 
@@ -157,9 +157,7 @@ export const webServerHandler = ({ port, socket, apiHost }: WebServerArgs) => {
     const webServer = c.green(`http://localhost:${port}`)
     console.log(`Web server started on ${webServer}`)
     console.log(`GraphQL endpoint is set to ` + c.magenta(`${graphqlEndpoint}`))
-    if (typeof process.send !== 'undefined') {
-      process.send('ready')
-    }
+    sendProcessReady()
   })
 }
 
