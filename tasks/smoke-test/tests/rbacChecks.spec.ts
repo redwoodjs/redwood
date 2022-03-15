fd819cb3fff86061971f3d1a9fc4d846ae0a02c5
Daniel Choudhury <dannychoudhury@gmail.com>
2022-03-15 11:59:48 +0700

    Ooops, forgot to commit RBAC checks

--- tasks/smoke-test/tests/rbacChecks.spec.ts
+++ tasks/smoke-test/tests/rbacChecks.spec.ts
@@ -17,26 +17,81 @@ import { loginAsTestUser, signUpTestUser } from './common'
 // Signup a user (admin@bazinga.com), because salt/secrets won't match, we need to do this
 // Then makes them admin with an rw exec script
 // Then checks if they have admin priviledges
-const email = 'admin@bazinga.com'
+const adminEmail = 'admin@bazinga.com'
 const password = 'test123'
 
-devServerTest.beforeAll(async ({ browser }: PlaywrightWorkerArgs) => {
-  const page = await browser.newPage()
+devServerTest.beforeEach(
+  async ({ webUrl, context }: PlaywrightTestArgs & DevServerFixtures) => {
+    const adminSignupPage = await context.newPage()
 
-  await signUpTestUser({
-    // @NOTE we can't access webUrl in beforeAll, so hardcoded
-    // But we can switch to beforeEach if required
-    webUrl: 'http://localhost:9000',
-    page,
-    email,
-    password,
-  })
+    await signUpTestUser({
+      // @NOTE we can't access webUrl in beforeAll, so hardcoded
+      // But we can switch to beforeEach if required
+      webUrl,
+      page: adminSignupPage,
+      email: adminEmail,
+      password,
+    })
 
-  await page.close()
-})
+    await adminSignupPage.close()
+
+    const regularUserSignupPage = await context.newPage()
+    // Signup non-admin user
+    await signUpTestUser({
+      webUrl,
+      page: regularUserSignupPage,
+    })
+
+    await regularUserSignupPage.close()
+
+    const incognitoPage = await context.newPage()
+
+    await fillOutContactFormAsAnonymousUser({
+      page: incognitoPage,
+      webUrl,
+    })
+
+    await incognitoPage.close()
+  }
+)
 
 devServerTest(
-  'RBAC checks',
+  'RBAC: Should not be able to delete contact as non-admin user',
+  async ({ webUrl, page }: DevServerFixtures & PlaywrightTestArgs) => {
+    // Login as non-admin user
+    await loginAsTestUser({
+      webUrl,
+      page,
+    })
+
+    // Go to http://localhost:8910/contacts
+    await page.goto(`${webUrl}/contacts`)
+
+    page.once('dialog', (dialog) => {
+      console.log(`Dialog message: ${dialog.message()}`)
+      dialog.accept().catch(() => {
+        console.error('Failed to accept dialog')
+      })
+    })
+
+    await page.locator('text=Delete').first().click()
+
+    await expect(
+      page
+        .locator('.rw-scaffold')
+        .locator("text=You don't have permission to do that")
+    ).toBeTruthy()
+
+    // @NOTE we do this because the scaffold content is actually on the page,
+    // This is the only way we validate if its actually showing visually
+    await expect(
+      page.locator('.rw-scaffold').locator('text=Contact deleted')
+    ).toBeHidden()
+  }
+)
+
+devServerTest(
+  'RBAC: Admin user should be able to delete contacts',
   async ({ webUrl, page }: DevServerFixtures & PlaywrightTestArgs) => {
     fs.writeFileSync(
       path.join(process.env.PROJECT_PATH, 'scripts/makeAdmin.ts'),
@@ -57,8 +112,8 @@ console.log(await db.user.findMany())
 }`
     )
 
-    console.log(`Giving ${email} ADMIN role....`)
-    await execa(`yarn rw exec makeAdmin --email ${email}`, {
+    console.log(`Giving ${adminEmail} ADMIN role....`)
+    await execa(`yarn rw exec makeAdmin --email ${adminEmail}`, {
       cwd: process.env.PROJECT_PATH,
       stdio: 'inherit',
       shell: true,
@@ -67,42 +122,54 @@ console.log(await db.user.findMany())
     await loginAsTestUser({
       webUrl,
       page,
-      email,
+      email: adminEmail,
       password,
     })
 
-    await page.goto(`${webUrl}/profile`)
+    // Go to http://localhost:8910/contacts
+    await page.goto(`${webUrl}/contacts`)
+    page.once('dialog', (dialog) => {
+      console.log(`Dialog message: ${dialog.message()}`)
+      dialog.accept().catch(() => {
+        console.error('Failed to accept dialog')
+      })
+    })
 
-    const usernameRow = await page.waitForSelector('*css=tr >> text=EMAIL')
-    await expect(await usernameRow.innerHTML()).toBe(
-      '<td>EMAIL</td><td>admin@bazinga.com</td>'
-    )
-
-    const isAdminRow = await page.waitForSelector('*css=tr >> text=Is Admin')
-    await expect(await isAdminRow.innerHTML()).toBe(
-      '<td>Is Admin</td><td>true</td>'
-    )
-
-    await page.goto(`${webUrl}/posts/new`)
-
-    // Click input[name="title"]
-    await page.locator('input[name="title"]').click()
-    // Fill input[name="title"]
-    await page
-      .locator('input[name="title"]')
-      .fill('This is a new post created by the admin')
-    // Press Tab
-    await page.locator('input[name="title"]').press('Tab')
-    // Fill input[name="body"]
-    await page.locator('input[name="body"]').fill('Bazinga, bazinga, bazinga')
-    // Click text=Save
-    await Promise.all([
-      page.waitForNavigation({ url: '**/' }),
-      page.click('text=SAVE'),
-    ])
+    await page.locator('text=Delete').first().click()
 
     await expect(
-      page.locator('text=This is a new post created by the admin')
-    ).toBeTruthy()
+      page.locator('.rw-scaffold').locator('text=Contact deleted')
+    ).toBeVisible()
   }
 )
+
+async function fillOutContactFormAsAnonymousUser({
+  page,
+  webUrl,
+}: {
+  page: PlaywrightTestArgs['page']
+  webUrl: string
+}) {
+  await page.goto(`${webUrl}/contact`)
+  // Click input[name="name"]
+  await page.locator('input[name="name"]').click()
+  // Fill input[name="name"]
+  await page.locator('input[name="name"]').fill('Charlie Chimichanga')
+  // Click input[name="email"]
+  await page.locator('input[name="email"]').click()
+  // Fill input[name="email"]
+  await page.locator('input[name="email"]').fill('charlie@chimichanga.com')
+  // Click textarea[name="message"]
+  await page.locator('textarea[name="message"]').click()
+  // Fill textarea[name="message"]
+  await page
+    .locator('textarea[name="message"]')
+    .fill('Hello, I love Mexican food. What about you? ')
+  // Click text=Save
+  const successMessage = page.locator(`text=Thank you for your submission!`)
+
+  await Promise.all([
+    successMessage.waitFor({ timeout: 5000 }),
+    page.locator('text=Save').click(),
+  ])
+}
