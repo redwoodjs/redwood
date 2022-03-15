0a2b3db28e35e0b63b94a123a956f9f0f31f1447
A. David Thyresson <dthyresson@gmail.com>
2022-03-15 18:46:25 -0400

    Adds back positive case when sdl is ok

---
+++ packages/internal/src/__tests__/validateSchema.test.ts
@@ -0,0 +1,19 @@
+import path from 'path'
+
+import { loadAndValidateSdls } from '../validateSchema'
+
+const FIXTURE_PATH = path.resolve(
+  __dirname,
+  '../../../../__fixtures__/example-todo-main'
+)
+
+beforeAll(() => {
+  process.env.RWJS_CWD = FIXTURE_PATH
+})
+afterAll(() => {
+  delete process.env.RWJS_CWD
+})
+
+test('Validates correctly on all platforms', async () => {
+  await expect(loadAndValidateSdls()).resolves.not.toThrowError()
+})
