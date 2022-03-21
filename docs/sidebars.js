ce490b02d6348c6989814965078ae55b16c32ea2
Rob Cameron <cannikin@fastmail.com>
2022-03-21 11:19:06 -0700

    Remove extra level of nesting on tutorial

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -52,65 +52,61 @@ module.exports = {
     },
     {
       type: 'category',
-      label: 'Tutorials',
+      label: 'Tutorial',
       className: 'tutorials-sidebar-category',
       items: [
+        'tutorial/forward',
         {
-          Tutorial: [
-            'tutorial/forward',
-            {
-              'Chapter 1': [
-                'tutorial/chapter1/prerequisites',
-                'tutorial/chapter1/installation',
-                'tutorial/chapter1/file-structure',
-                'tutorial/chapter1/first-page',
-                'tutorial/chapter1/second-page',
-                'tutorial/chapter1/layouts',
-              ],
-            },
-            {
-              'Chapter 2': [
-                'tutorial/chapter2/getting-dynamic',
-                'tutorial/chapter2/cells',
-                'tutorial/chapter2/side-quest',
-                'tutorial/chapter2/routing-params',
-              ],
-            },
-            {
-              'Chapter 3': [
-                'tutorial/chapter3/forms',
-                'tutorial/chapter3/saving-data',
-              ],
-            },
-            {
-              'Chapter 4': [
-                'tutorial/chapter4/authentication',
-                'tutorial/chapter4/deployment',
-              ],
-            },
-            'tutorial/intermission',
-            {
-              'Chapter 5': [
-                'tutorial/chapter5/storybook',
-                'tutorial/chapter5/first-story',
-                'tutorial/chapter5/testing',
-                'tutorial/chapter5/first-test',
-              ],
-            },
-            {
-              'Chapter 6': [
-                'tutorial/chapter6/the-redwood-way',
-                'tutorial/chapter6/multiple-comments',
-                'tutorial/chapter6/comments-schema',
-                'tutorial/chapter6/comment-form',
-              ],
-            },
-            {
-              'Chapter 7': ['tutorial/chapter7/rbac'],
-            },
-            'tutorial/afterword',
+          'Chapter 1': [
+            'tutorial/chapter1/prerequisites',
+            'tutorial/chapter1/installation',
+            'tutorial/chapter1/file-structure',
+            'tutorial/chapter1/first-page',
+            'tutorial/chapter1/second-page',
+            'tutorial/chapter1/layouts',
           ],
         },
+        {
+          'Chapter 2': [
+            'tutorial/chapter2/getting-dynamic',
+            'tutorial/chapter2/cells',
+            'tutorial/chapter2/side-quest',
+            'tutorial/chapter2/routing-params',
+          ],
+        },
+        {
+          'Chapter 3': [
+            'tutorial/chapter3/forms',
+            'tutorial/chapter3/saving-data',
+          ],
+        },
+        {
+          'Chapter 4': [
+            'tutorial/chapter4/authentication',
+            'tutorial/chapter4/deployment',
+          ],
+        },
+        'tutorial/intermission',
+        {
+          'Chapter 5': [
+            'tutorial/chapter5/storybook',
+            'tutorial/chapter5/first-story',
+            'tutorial/chapter5/testing',
+            'tutorial/chapter5/first-test',
+          ],
+        },
+        {
+          'Chapter 6': [
+            'tutorial/chapter6/the-redwood-way',
+            'tutorial/chapter6/multiple-comments',
+            'tutorial/chapter6/comments-schema',
+            'tutorial/chapter6/comment-form',
+          ],
+        },
+        {
+          'Chapter 7': ['tutorial/chapter7/rbac'],
+        },
+        'tutorial/afterword',
       ],
     },
     {
