fd8e3feaec997d7b4d8e92db83c0a772f215b77f
Tobbe Lundberg <tobbe@tlundberg.com>
2022-03-22 20:29:09 +0100

    Use Clerk's own errors

--- packages/api/src/auth/decoders/clerk.ts
+++ packages/api/src/auth/decoders/clerk.ts
@@ -6,26 +6,25 @@ export const clerk = async (token: string) => {
 
   const { users, base }: IClerk = new Clerk()
 
-  if (!process.env.CLERK_API_KEY) {
-    console.error('CLERK_API_KEY env var is not set.')
-    throw new Error('CLERK_API_KEY env var is not set.')
-  }
-
   if (!process.env.CLERK_JWT_KEY) {
     console.error('CLERK_JWT_KEY env var is not set.')
     throw new Error('CLERK_JWT_KEY env var is not set.')
   }
 
-  const jwtPayload = await base.verifySessionToken(token)
+  try {
+    const jwtPayload = await base.verifySessionToken(token)
 
-  if (!jwtPayload.sub) {
-    return Promise.reject(new Error('Session invalid'))
-  }
+    if (!jwtPayload.sub) {
+      return Promise.reject(new Error('Session invalid'))
+    }
 
-  const user = await users.getUser(jwtPayload.sub)
+    const user = await users.getUser(jwtPayload.sub)
 
-  return {
-    ...user,
-    roles: user.publicMetadata['roles'] ?? [],
+    return {
+      ...user,
+      roles: user.publicMetadata['roles'] ?? [],
+    }
+  } catch (error) {
+    return Promise.reject(error)
   }
 }
