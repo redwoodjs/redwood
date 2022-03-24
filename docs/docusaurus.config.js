788e92db7577270edcdb558e3f51f86a094cec8e
Dominic Saadi <32992335+jtoar@users.noreply.github.com>
2022-03-24 23:11:11 +0900

    try external url regex (#4909)

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -23,11 +23,12 @@ module.exports = {
   projectName: 'redwood', // Usually your repo name.,
   themeConfig: {
     algolia: {
-      appId: '9YZJ9GHCKX',
-      apiKey: 'b8585f09d7fec5154602cdf8788ccb1d',
-      indexName: 'docs',
+      appId: '37B3LHULK0',
+      apiKey: '1d7f2f299d9a38c157501c301425f090',
+      indexName: 'learn-redwood',
       contextualSearch: true,
       searchParameters: {},
+      externalUrlRegex: 'redwoodjs.com',
     },
     navbar: {
       title: 'RedwoodJS',
