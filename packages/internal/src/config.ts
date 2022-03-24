8bb73df824a00c516e465375cb8912b0437f3311
Daniel Choudhury <dannychoudhury@gmail.com>
2022-03-24 16:35:48 +0700

    Set debug port to 18911 by default

--- packages/internal/src/config.ts
+++ packages/internal/src/config.ts
@@ -93,6 +93,7 @@ const DEFAULT_CONFIG: Config = {
     target: TargetEnum.NODE,
     schemaPath: './api/db/schema.prisma',
     serverConfig: './api/server.config.js',
+    debugPort: 18911,
   },
   browser: {
     open: false,
