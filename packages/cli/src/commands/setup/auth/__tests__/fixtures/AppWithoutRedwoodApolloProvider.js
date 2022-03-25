6b5d4de161981806aa01a7210fb2c87dfd95030a
Ken Greeff <ken@kengreeff.com>
2022-03-26 10:24:41 +1100

    Update fixture to simulate alternate query provider

--- packages/cli/src/commands/setup/auth/__tests__/fixtures/AppWithoutRedwoodApolloProvider.js
+++ packages/cli/src/commands/setup/auth/__tests__/fixtures/AppWithoutRedwoodApolloProvider.js
@@ -1,15 +1,20 @@
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
-import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
 
 import './index.css'
 
+const queryClient = {}
+
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
-      <Routes />
+      <QueryClientProvider client={queryClient}>
+        <RedwoodReactQueryProvider>
+          <Routes />
+        </RedwoodReactQueryProvider>
+      </QueryClientProvider>
     </RedwoodProvider>
   </FatalErrorBoundary>
 )
