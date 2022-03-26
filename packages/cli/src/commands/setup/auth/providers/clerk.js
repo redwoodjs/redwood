7c8f230ef0bdac850f2053cd92778579f35b8c4e
Tobbe Lundberg <tobbe@tlundberg.com>
2022-03-26 18:50:37 +0100

    Tweak wording

--- packages/cli/src/commands/setup/auth/providers/clerk.js
+++ packages/cli/src/commands/setup/auth/providers/clerk.js
@@ -40,7 +40,7 @@ export const apiPackages = ['@clerk/clerk-sdk-node']
 
 // any notes to print out when the job is done
 export const notes = [
-  'You will need to add three environment variables with your Clerk URL, API and JWT keys.',
+  'You will need to add three environment variables with your Clerk URL, API key and JWT key.',
   'Check out web/src/App.{js,tsx} for the variables you need to add.',
   'See also: https://redwoodjs.com/docs/authentication#clerk',
 ]
