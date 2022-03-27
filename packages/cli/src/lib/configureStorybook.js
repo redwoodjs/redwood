1d2d4d57d5c89517ce4ad1830333d5895e906372
Simon Gagnon <git@simoncrypta.dev>
2022-03-27 19:35:20 -0400

    Merge both storybook config if file exist

--- packages/cli/src/lib/configureStorybook.js
+++ packages/cli/src/lib/configureStorybook.js
@@ -3,31 +3,67 @@ import fs from 'fs-extra'
 import { getPaths } from '.'
 
 /**
- * @param options {{ force?: boolean }}
- * @returns {"todo" | "done"}
+ * Configure Storybook for the given template by creating a custom preview config
  */
-export function checkStorybookStatus({ force }) {
-  const { storybookPreviewConfig } = getPaths().web // 'web/config/storybook.config.js'
-
+export default function configureStorybook({ force }, newStorybookPreview) {
+  const { storybookPreviewConfig } = getPaths().web
+  let finalNewStorybookPreview
+  /**
+   *  Check if storybookPreviewConfig already exists.
+   *  Merge both files if it does.
+   *  By removing import react and export decorator from new config
+   *  And put new config inside current config after last import
+   */
   if (fs.existsSync(storybookPreviewConfig)) {
     if (force) {
       fs.unlinkSync(storybookPreviewConfig)
+      finalNewStorybookPreview = newStorybookPreview
     } else {
-      console.log(
-        `Storybook preview config already exists at ${storybookPreviewConfig}\nUse --force to override existing config.`
+      const insideCurrentStorybookConfig = fs
+        .readFileSync(storybookPreviewConfig)
+        .toString()
+
+      const insideNewStorybookConfig = fs
+        .readFileSync(newStorybookPreview)
+        .toString()
+
+      const newDecoratorsName = insideNewStorybookConfig.match(
+        /export const decorators = \[(.*?)\]/
+      )[1]
+
+      const currentDecoratorsName = insideCurrentStorybookConfig.match(
+        /export const decorators = \[(.*?)\]/
+      )[1]
+
+      const finalDecoratorsExport = `export const decorators = [${currentDecoratorsName}, ${newDecoratorsName}]`
+
+      const insideNewStorybookConfigWithoutReactAndDecoration =
+        insideNewStorybookConfig
+          .replace(/import *. as React from 'react'/, '')
+          .replace(/export const decorators = .*/, '')
+
+      let ReverseInsideCurrentStorybookConfig = insideCurrentStorybookConfig
+        .split('\n')
+        .reverse()
+
+      const indexOfLastImport = ReverseInsideCurrentStorybookConfig.findIndex(
+        (value) => /import/.test(value)
       )
-      return 'done'
+      ReverseInsideCurrentStorybookConfig.splice(
+        indexOfLastImport,
+        0,
+        insideNewStorybookConfigWithoutReactAndDecoration
+      )
+      const finalInsideCurrentStorybookConfig =
+        ReverseInsideCurrentStorybookConfig.reverse().join(`\n`) +
+        `\n` +
+        finalDecoratorsExport
+
+      finalNewStorybookPreview = finalInsideCurrentStorybookConfig
     }
+  } else {
+    finalNewStorybookPreview = newStorybookPreview
   }
 
-  return 'todo'
-}
-
-/**
- * Configure Storybook for the given template by creating a custom preview config
- */
-export function configureStorybook(storybookPreview) {
-  const { storybookPreviewConfig } = getPaths().web
-
-  fs.outputFileSync(storybookPreviewConfig, storybookPreview)
+  fs.outputFileSync(storybookPreviewConfig, finalNewStorybookPreview)
 }
