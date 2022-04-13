af6c98031307289f039b6def141c9e0e28db4557
A. David Thyresson <dthyresson@gmail.com>
2022-04-13 09:58:06 -0400

    WIP

--- packages/api/src/auth/decoders/__tests__/auth0.test.ts
+++ packages/api/src/auth/decoders/__tests__/auth0.test.ts
@@ -1,23 +1,123 @@
 import jwt from 'jsonwebtoken'
+import nock from 'nock'
 
-import { verifyAuth0Token } from '../auth0'
+import {
+  auth0Config,
+  verifyAuth0Token,
+  getSigningKey,
+  getPublicKey,
+} from '../auth0'
 
 jest.mock('jsonwebtoken', () => ({
   verify: jest.fn(),
   decode: jest.fn(),
 }))
 
-test('verify, and not decode, should be called in production', () => {
-  const { NODE_ENV } = process.env
-  process.env.NODE_ENV = 'production'
-  process.env.AUTH0_DOMAIN = 'redwoodjs.com'
-  process.env.AUTH0_AUDIENCE = 'michael bolton'
+describe('JwksClient', () => {
+  beforeEach(() => {
+    nock.cleanAll()
+  })
 
-  // @ts-expect-error Ignore this error.
-  verifyAuth0Token({})
+  test('verify, and not decode, should be called in production', () => {
+    const { NODE_ENV } = process.env
+    process.env.NODE_ENV = 'production'
+    process.env.AUTH0_DOMAIN = 'redwoodjs.com'
+    process.env.AUTH0_AUDIENCE = 'michael bolton'
 
-  expect(jwt.decode).not.toBeCalled()
-  expect(jwt.verify).toBeCalled()
+    // @ts-expect-error Ignore this error.
+    verifyAuth0Token({})
 
-  process.env.NODE_ENV = NODE_ENV
+    expect(jwt.decode).not.toBeCalled()
+    expect(jwt.verify).toBeCalled()
+
+    process.env.NODE_ENV = NODE_ENV
+  })
+
+  test('signingKey', async () => {
+    process.env.NODE_ENV = 'production'
+    process.env.AUTH0_DOMAIN = 'redwoodjs.com'
+    process.env.AUTH0_AUDIENCE = 'michael bolton'
+
+    const keys = [
+      {
+        alg: 'RS256',
+        kty: 'RSA',
+        use: 'sig',
+        x5c: [
+          'MIIDDTCCAfWgAwIBAgIJAJVkuSv2H8mDMA0GCSqGSIb3DQEBBQUAMB0xGzAZBgNVBAMMEnNhbmRyaW5vLmF1dGgwLmNvbTAeFw0xNDA1MTQyMTIyMjZaFw0yODAxMjEyMTIyMjZaMB0xGzAZBgNVBAMMEnNhbmRyaW5vLmF1dGgwLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAL6jWASkHhXz5Ug6t5BsYBrXDIgrWu05f3oq2fE+5J5REKJiY0Ddc+Kda34ZwOptnUoef3JwKPDAckTJQDugweNNZPwOmFMRKj4xqEpxEkIX8C+zHs41Q6x54ZZy0xU+WvTGcdjzyZTZ/h0iOYisswFQT/s6750tZG0BOBtZ5qS/80tmWH7xFitgewdWteJaASE/eO1qMtdNsp9fxOtN5U/pZDUyFm3YRfOcODzVqp3wOz+dcKb7cdZN11EYGZOkjEekpcedzHCo9H4aOmdKCpytqL/9FXoihcBMg39s1OW3cfwfgf5/kvOJdcqR4PoATQTfsDVoeMWVB4XLGR6SC5kCAwEAAaNQME4wHQYDVR0OBBYEFHDYn9BQdup1CoeoFi0Rmf5xn/W9MB8GA1UdIwQYMBaAFHDYn9BQdup1CoeoFi0Rmf5xn/W9MAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADggEBAGLpQZdd2ICVnGjc6CYfT3VNoujKYWk7E0shGaCXFXptrZ8yaryfo6WAizTfgOpQNJH+Jz+QsCjvkRt6PBSYX/hb5OUDU2zNJN48/VOw57nzWdjI70H2Ar4oJLck36xkIRs/+QX+mSNCjZboRwh0LxanXeALHSbCgJkbzWbjVnfJEQUP9P/7NGf0MkO5I95C/Pz9g91y8gU+R3imGppLy9Zx+OwADFwKAEJak4JrNgcjHBQenakAXnXP6HG4hHH4MzO8LnLiKv8ZkKVL67da/80PcpO0miMNPaqBBMd2Cy6GzQYE0ag6k0nk+DMIFn7K+o21gjUuOEJqIbAvhbf2KcM=',
+        ],
+        n: 'vqNYBKQeFfPlSDq3kGxgGtcMiCta7Tl_eirZ8T7knlEQomJjQN1z4p1rfhnA6m2dSh5_cnAo8MByRMlAO6DB401k_A6YUxEqPjGoSnESQhfwL7MezjVDrHnhlnLTFT5a9MZx2PPJlNn-HSI5iKyzAVBP-zrvnS1kbQE4G1nmpL_zS2ZYfvEWK2B7B1a14loBIT947Woy102yn1_E603lT-lkNTIWbdhF85w4PNWqnfA7P51wpvtx1k3XURgZk6SMR6Slx53McKj0fho6Z0oKnK2ov_0VeiKFwEyDf2zU5bdx_B-B_n-S84l1ypHg-gBNBN-wNWh4xZUHhcsZHpILmQ',
+        e: 'AQAB',
+        kid: 'RkI5MjI5OUY5ODc1N0Q4QzM0OUYzNkVGMTJDOUEzQkFCOTU3NjE2Rg',
+        x5t: 'RkI5MjI5OUY5ODc1N0Q4QzM0OUYzNkVGMTJDOUEzQkFCOTU3NjE2Rg',
+      },
+      {
+        alg: 'RS256',
+        kty: 'RSA',
+        use: 'sig',
+        x5c: [
+          'MIIDGzCCAgOgAwIBAgIJAPQM5+PwmOcPMA0GCSqGSIb3DQEBCwUAMCQxIjAgBgNVBAMMGXNhbmRyaW5vLWRldi5ldS5hdXRoMC5jb20wHhcNMTUwMzMxMDkwNTQ3WhcNMjgxMjA3MDkwNTQ3WjAkMSIwIAYDVQQDDBlzYW5kcmluby1kZXYuZXUuYXV0aDAuY29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAv/SECtT7H4rxKtX2HpGhSyeYTe3Vet8YQpjBAr+1TnQ1fcYfvfmnVRHvhmTwABktD1erF1lxFsrRw92yBDOHlL7lj1n2fcfLftSoStgvRHVg52kR+CkBVQ6/mF1lYkefIjik6YRMf55Eu4FqDyVG2dgd5EA8kNO4J8OPc7vAtZyXrRYOZjVXbEgyjje/V+OpMQxAHP2Er11TLuzJjioP0ICVqhAZdq2sLk7agoxn64md6fqOk4N+7lJkU4+412VD0qYwKxD7nGsEclYawKoZD9/xhCk2qfQ/HptIumrdQ5ox3Sq5t2a7VKa41dBUQ1MQtXG2iY7S9RlfcMIyQwGhOQIDAQABo1AwTjAdBgNVHQ4EFgQUHpS1fvO/54G2c1VpEDNUZRSl44gwHwYDVR0jBBgwFoAUHpS1fvO/54G2c1VpEDNUZRSl44gwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQsFAAOCAQEAtm9I0nr6eXF5aq4yllfiqZcQ6mKrJLH9Rm4Jv+olniNynTcnpwprRVLToIawc8MmzIGZTtCn7u+dSxWf1UNE+SH7XgEnGtO74239vleEx1+Tf5viIdsnCxgvFiPdOqRlc9KcFSWd6a7RzcglnyU7GEx0K5GLv1wPA6qEM+3uwNwjAyVSu5dFw8kCfaSvlk5rXKRUzSoW9NVomw6+tADR8vMZS+4KThZ+4GH0rMN4KjIaRFxW8OMVYOn12uq33fLCd6MuPHW/rklxLbQBoHIU/ClNhbD0t6f00w9lHhPy4IP73rv7Oow0Ny6i70Iq0ijqj+kAtnrphlOvLFxqn6nCvQ==',
+        ],
+        n: 'v_SECtT7H4rxKtX2HpGhSyeYTe3Vet8YQpjBAr-1TnQ1fcYfvfmnVRHvhmTwABktD1erF1lxFsrRw92yBDOHlL7lj1n2fcfLftSoStgvRHVg52kR-CkBVQ6_mF1lYkefIjik6YRMf55Eu4FqDyVG2dgd5EA8kNO4J8OPc7vAtZyXrRYOZjVXbEgyjje_V-OpMQxAHP2Er11TLuzJjioP0ICVqhAZdq2sLk7agoxn64md6fqOk4N-7lJkU4-412VD0qYwKxD7nGsEclYawKoZD9_xhCk2qfQ_HptIumrdQ5ox3Sq5t2a7VKa41dBUQ1MQtXG2iY7S9RlfcMIyQwGhOQ',
+        e: 'AQAB',
+        kid: 'NkFCNEE1NDFDNTQ5RTQ5OTE1QzRBMjYyMzY0NEJCQTJBMjJBQkZCMA',
+        x5t: 'NkFCNEE1NDFDNTQ5RTQ5OTE1QzRBMjYyMzY0NEJCQTJBMjJBQkZCMA',
+      },
+    ]
+    const x = {
+      keys: keys,
+    }
+    nock(auth0Config().issuer).get('/.well-known/jwks.json').reply(200, x)
+    const kid = keys[0].kid // ?
+    // const signingKey = await getSigningKey('1')
+    const signingKey = await getSigningKey({
+      kid,
+    })
+
+    expect(signingKey.kid).toEqual(kid)
+  })
+
+  test('getPublicKey', async () => {
+    process.env.NODE_ENV = 'production'
+    process.env.AUTH0_DOMAIN = 'redwoodjs.com'
+    process.env.AUTH0_AUDIENCE = 'michael bolton'
+
+    const keys = [
+      {
+        alg: 'RS256',
+        kty: 'RSA',
+        use: 'sig',
+        x5c: [
+          'MIIDDTCCAfWgAwIBAgIJAJVkuSv2H8mDMA0GCSqGSIb3DQEBBQUAMB0xGzAZBgNVBAMMEnNhbmRyaW5vLmF1dGgwLmNvbTAeFw0xNDA1MTQyMTIyMjZaFw0yODAxMjEyMTIyMjZaMB0xGzAZBgNVBAMMEnNhbmRyaW5vLmF1dGgwLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAL6jWASkHhXz5Ug6t5BsYBrXDIgrWu05f3oq2fE+5J5REKJiY0Ddc+Kda34ZwOptnUoef3JwKPDAckTJQDugweNNZPwOmFMRKj4xqEpxEkIX8C+zHs41Q6x54ZZy0xU+WvTGcdjzyZTZ/h0iOYisswFQT/s6750tZG0BOBtZ5qS/80tmWH7xFitgewdWteJaASE/eO1qMtdNsp9fxOtN5U/pZDUyFm3YRfOcODzVqp3wOz+dcKb7cdZN11EYGZOkjEekpcedzHCo9H4aOmdKCpytqL/9FXoihcBMg39s1OW3cfwfgf5/kvOJdcqR4PoATQTfsDVoeMWVB4XLGR6SC5kCAwEAAaNQME4wHQYDVR0OBBYEFHDYn9BQdup1CoeoFi0Rmf5xn/W9MB8GA1UdIwQYMBaAFHDYn9BQdup1CoeoFi0Rmf5xn/W9MAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADggEBAGLpQZdd2ICVnGjc6CYfT3VNoujKYWk7E0shGaCXFXptrZ8yaryfo6WAizTfgOpQNJH+Jz+QsCjvkRt6PBSYX/hb5OUDU2zNJN48/VOw57nzWdjI70H2Ar4oJLck36xkIRs/+QX+mSNCjZboRwh0LxanXeALHSbCgJkbzWbjVnfJEQUP9P/7NGf0MkO5I95C/Pz9g91y8gU+R3imGppLy9Zx+OwADFwKAEJak4JrNgcjHBQenakAXnXP6HG4hHH4MzO8LnLiKv8ZkKVL67da/80PcpO0miMNPaqBBMd2Cy6GzQYE0ag6k0nk+DMIFn7K+o21gjUuOEJqIbAvhbf2KcM=',
+        ],
+        n: 'vqNYBKQeFfPlSDq3kGxgGtcMiCta7Tl_eirZ8T7knlEQomJjQN1z4p1rfhnA6m2dSh5_cnAo8MByRMlAO6DB401k_A6YUxEqPjGoSnESQhfwL7MezjVDrHnhlnLTFT5a9MZx2PPJlNn-HSI5iKyzAVBP-zrvnS1kbQE4G1nmpL_zS2ZYfvEWK2B7B1a14loBIT947Woy102yn1_E603lT-lkNTIWbdhF85w4PNWqnfA7P51wpvtx1k3XURgZk6SMR6Slx53McKj0fho6Z0oKnK2ov_0VeiKFwEyDf2zU5bdx_B-B_n-S84l1ypHg-gBNBN-wNWh4xZUHhcsZHpILmQ',
+        e: 'AQAB',
+        kid: 'RkI5MjI5OUY5ODc1N0Q4QzM0OUYzNkVGMTJDOUEzQkFCOTU3NjE2Rg',
+        x5t: 'RkI5MjI5OUY5ODc1N0Q4QzM0OUYzNkVGMTJDOUEzQkFCOTU3NjE2Rg',
+      },
+      {
+        alg: 'RS256',
+        kty: 'RSA',
+        use: 'sig',
+        x5c: [
+          'MIIDGzCCAgOgAwIBAgIJAPQM5+PwmOcPMA0GCSqGSIb3DQEBCwUAMCQxIjAgBgNVBAMMGXNhbmRyaW5vLWRldi5ldS5hdXRoMC5jb20wHhcNMTUwMzMxMDkwNTQ3WhcNMjgxMjA3MDkwNTQ3WjAkMSIwIAYDVQQDDBlzYW5kcmluby1kZXYuZXUuYXV0aDAuY29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAv/SECtT7H4rxKtX2HpGhSyeYTe3Vet8YQpjBAr+1TnQ1fcYfvfmnVRHvhmTwABktD1erF1lxFsrRw92yBDOHlL7lj1n2fcfLftSoStgvRHVg52kR+CkBVQ6/mF1lYkefIjik6YRMf55Eu4FqDyVG2dgd5EA8kNO4J8OPc7vAtZyXrRYOZjVXbEgyjje/V+OpMQxAHP2Er11TLuzJjioP0ICVqhAZdq2sLk7agoxn64md6fqOk4N+7lJkU4+412VD0qYwKxD7nGsEclYawKoZD9/xhCk2qfQ/HptIumrdQ5ox3Sq5t2a7VKa41dBUQ1MQtXG2iY7S9RlfcMIyQwGhOQIDAQABo1AwTjAdBgNVHQ4EFgQUHpS1fvO/54G2c1VpEDNUZRSl44gwHwYDVR0jBBgwFoAUHpS1fvO/54G2c1VpEDNUZRSl44gwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQsFAAOCAQEAtm9I0nr6eXF5aq4yllfiqZcQ6mKrJLH9Rm4Jv+olniNynTcnpwprRVLToIawc8MmzIGZTtCn7u+dSxWf1UNE+SH7XgEnGtO74239vleEx1+Tf5viIdsnCxgvFiPdOqRlc9KcFSWd6a7RzcglnyU7GEx0K5GLv1wPA6qEM+3uwNwjAyVSu5dFw8kCfaSvlk5rXKRUzSoW9NVomw6+tADR8vMZS+4KThZ+4GH0rMN4KjIaRFxW8OMVYOn12uq33fLCd6MuPHW/rklxLbQBoHIU/ClNhbD0t6f00w9lHhPy4IP73rv7Oow0Ny6i70Iq0ijqj+kAtnrphlOvLFxqn6nCvQ==',
+        ],
+        n: 'v_SECtT7H4rxKtX2HpGhSyeYTe3Vet8YQpjBAr-1TnQ1fcYfvfmnVRHvhmTwABktD1erF1lxFsrRw92yBDOHlL7lj1n2fcfLftSoStgvRHVg52kR-CkBVQ6_mF1lYkefIjik6YRMf55Eu4FqDyVG2dgd5EA8kNO4J8OPc7vAtZyXrRYOZjVXbEgyjje_V-OpMQxAHP2Er11TLuzJjioP0ICVqhAZdq2sLk7agoxn64md6fqOk4N-7lJkU4-412VD0qYwKxD7nGsEclYawKoZD9_xhCk2qfQ_HptIumrdQ5ox3Sq5t2a7VKa41dBUQ1MQtXG2iY7S9RlfcMIyQwGhOQ',
+        e: 'AQAB',
+        kid: 'NkFCNEE1NDFDNTQ5RTQ5OTE1QzRBMjYyMzY0NEJCQTJBMjJBQkZCMA',
+        x5t: 'NkFCNEE1NDFDNTQ5RTQ5OTE1QzRBMjYyMzY0NEJCQTJBMjJBQkZCMA',
+      },
+    ]
+    const x = {
+      keys: keys,
+    }
+    nock(auth0Config().issuer).get('/.well-known/jwks.json').reply(200, x)
+    const kid = keys[0].kid // ?
+    // const signingKey = await getSigningKey('1')
+    const publicKey = await getPublicKey({
+      kid,
+    })
+
+    expect(publicKey).toBeTruthy()
+  })
 })
--- packages/api/src/auth/decoders/auth0.ts
+++ packages/api/src/auth/decoders/auth0.ts
@@ -1,5 +1,40 @@
-import jwt from 'jsonwebtoken'
+import jwt, { JsonWebTokenError } from 'jsonwebtoken'
 import jwksClient from 'jwks-rsa'
+
+export const auth0Config = () => {
+  const { AUTH0_DOMAIN, AUTH0_AUDIENCE } = process.env
+  if (!AUTH0_DOMAIN || !AUTH0_AUDIENCE) {
+    throw new Error('`AUTH0_DOMAIN` or `AUTH0_AUDIENCE` env vars are not set.')
+  }
+
+  return {
+    issuer: `https://${AUTH0_DOMAIN}`,
+    audience: AUTH0_AUDIENCE,
+  }
+}
+
+const auth0Client = () => {
+  return jwksClient({
+    jwksUri: `${auth0Config().issuer}/.well-known/jwks.json`,
+  })
+}
+
+export const getSigningKey = async (header: any) => {
+  const client = auth0Client() // ?
+
+  const kid = header.kid as string // ?
+  const key = await client.getSigningKey(kid) //?
+  return key // ?
+}
+
+export const getPublicKey = async (header: any) => {
+  const key = await getSigningKey(header)
+  if (key) {
+    return key.getPublicKey()
+  } else {
+    throw new JsonWebTokenError('JWT Error')
+  }
+}
 
 /**
  * This takes an auth0 jwt and verifies it. It returns something like this:
@@ -16,37 +51,25 @@ import jwksClient from 'jwks-rsa'
  * ```
  *
  * You can use `sub` as a stable reference to your user, but  if you want the email
- * addres you can set a context object[^0] in rules[^1]:
+ * address you can set a context object[^0] in rules[^1]:
  *
  * ^0: https://auth0.com/docs/rules/references/context-object
  * ^1: https://manage.auth0.com/#/rules/new
  *
  */
-export const verifyAuth0Token = (
+
+export const verifyAuth0Token = async (
   bearerToken: string
 ): Promise<null | Record<string, unknown>> => {
   return new Promise((resolve, reject) => {
-    const { AUTH0_DOMAIN, AUTH0_AUDIENCE } = process.env
-    if (!AUTH0_DOMAIN || !AUTH0_AUDIENCE) {
-      throw new Error(
-        '`AUTH0_DOMAIN` or `AUTH0_AUDIENCE` env vars are not set.'
-      )
-    }
-
-    const client = jwksClient({
-      jwksUri: `https://${AUTH0_DOMAIN}/.well-known/jwks.json`,
-    })
-
     jwt.verify(
       bearerToken,
-      (header, callback) => {
-        client.getSigningKey(header.kid as string, (error, key) => {
-          callback(error, key.getPublicKey())
-        })
+      async (header) => {
+        return getPublicKey(header)
       },
       {
-        audience: AUTH0_AUDIENCE,
-        issuer: `https://${AUTH0_DOMAIN}/`,
+        audience: auth0Config().audience,
+        issuer: auth0Config().issuer,
         algorithms: ['RS256'],
       },
       (verifyError, decoded) => {
