fca212a6414966dabe55f061ef526844f0dfcbb6
Tobbe Lundberg <tobbe@tlundberg.com>
2022-04-24 16:46:27 +0200

    Fix tests

--- packages/internal/src/__tests__/graphqlSchema.test.ts
+++ packages/internal/src/__tests__/graphqlSchema.test.ts
@@ -49,17 +49,23 @@ test('Prints error message when schema loading fails', async () => {
   try {
     await generateGraphQLSchema()
 
-    const invocation1to4 = (console.error as jest.Mock).mock.calls.slice(0, 4)
-    const invocation5 = (console.error as jest.Mock).mock.calls[4]
+    const invocation1to6 = (console.error as jest.Mock).mock.calls.slice(0, 6)
+    const invocation7 = (console.error as jest.Mock).mock.calls[6]
 
-    expect(invocation1to4).toEqual([
+    expect(invocation1to6).toEqual([
       ['Schema loading failed.', 'Unknown type: "Shelf".'],
       [''],
       ['It looks like you have a Shelf model in your database schema.'],
-      ['Try running the generator you just used for that model first instead'],
+      ['Maybe you need to generate SDL or scaffolding for Shelf first.'],
+      [''],
+      [
+        'See the section on troubleshooting generators ' +
+          '(​https://redwoodjs.com/docs/schema-relations#troubleshooting-generators​) ' +
+          'in our docs for more help',
+      ],
     ])
-    expect(invocation5[0]).toMatch('Schema loading failed')
-    expect(invocation5[1].toString()).toMatch('Error: Unknown type: "Shelf".')
+    expect(invocation7[0]).toMatch('Schema loading failed')
+    expect(invocation7[1].toString()).toMatch('Error: Unknown type: "Shelf".')
   } finally {
     console.error = oldConsoleError
     delete process.env.RWJS_CWD
