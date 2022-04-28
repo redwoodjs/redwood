31f8e2d005e96901d8792847f625ec7fbf072cff
Daniel Choudhury <dannychoudhury@gmail.com>
2022-04-28 16:34:44 +0700

    Set large timeout for all platforms

--- tasks/smoke-test/playwright.config.ts
+++ tasks/smoke-test/playwright.config.ts
@@ -2,7 +2,7 @@ import type { PlaywrightTestConfig } from '@playwright/test'
 
 // See https://playwright.dev/docs/test-configuration#global-configuration
 const config: PlaywrightTestConfig = {
-  timeout: process.platform === 'win32' ? 100_000 : 60_000,
+  timeout: 120_000,
   // Leaving this here to make debugging easier, by uncommenting
   // use: {
   //   launchOptions: {
