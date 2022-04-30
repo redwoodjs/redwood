8eded09847a5c2eed00f4b037b5e732f784339e5
Tobbe Lundberg <tobbe@tlundberg.com>
2022-04-30 23:15:23 +0200

    Update FatalErrorPage codemod to work with hybrid JS/TS projects

--- packages/codemods/src/codemods/v0.50.x/updateDevFatalErrorPage/__tests__/updateDevFatalErrorPage.test.ts
+++ packages/codemods/src/codemods/v0.50.x/updateDevFatalErrorPage/__tests__/updateDevFatalErrorPage.test.ts
@@ -1,7 +1,11 @@
 import { updateDevFatalErrorPage } from '../updateDevFatalErrorPage'
 
 describe('updateDevFatalErrorPage', () => {
-  it('Replaces the FatalErrorPage with a new version that includes development info', async () => {
-    await matchFolderTransform(updateDevFatalErrorPage, 'default')
+  it('Replaces the JS FatalErrorPage with a new version that includes development info', async () => {
+    await matchFolderTransform(updateDevFatalErrorPage, 'javascript')
+  })
+
+  it('Replaces the TS FatalErrorPage with a new version that includes development info', async () => {
+    await matchFolderTransform(updateDevFatalErrorPage, 'typescript')
   })
 })
