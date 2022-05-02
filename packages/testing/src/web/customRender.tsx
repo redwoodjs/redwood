fcee9d5d39dce7435b7c253bc88f29157e13a911
Curtis Reimer <curtis.reimer@cengys.com>
2022-05-01 21:20:52 -0500

    added customRenderHook

--- packages/testing/src/web/customRender.tsx
+++ packages/testing/src/web/customRender.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 import { render } from '@testing-library/react'
 import type { RenderResult } from '@testing-library/react'
+import { renderHook } from '@testing-library/react-hooks'
+import type {
+  RenderHookOptions,
+  RenderHookResult,
+} from '@testing-library/react-hooks'
 
 import { MockProviders } from './MockProviders'
 
@@ -10,6 +15,16 @@ export const customRender = (
   options = {}
 ): RenderResult => {
   return render(ui, {
+    wrapper: (props) => <MockProviders {...props} />,
+    ...options,
+  })
+}
+
+export const customRenderHook = <Result, Props>(
+  render: (props: Props) => Result,
+  options?: RenderHookOptions<Props>
+): RenderHookResult<Result, Props> => {
+  return renderHook(render, {
     wrapper: (props) => <MockProviders {...props} />,
     ...options,
   })
--- packages/testing/src/web/index.ts
+++ packages/testing/src/web/index.ts
@@ -2,7 +2,7 @@
 import './global'
 
 export * from '@testing-library/react'
-export { customRender as render } from './customRender'
+export { customRender as render, customRenderHook as renderHook } from './customRender'
 
 export { MockProviders } from './MockProviders'
 
