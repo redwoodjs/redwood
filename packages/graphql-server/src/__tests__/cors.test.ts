374ce04ebab9ae02e12fae15c98ff0333f33cfcc
Daniel Choudhury <dannychoudhury@gmail.com>
2022-05-04 18:40:10 +0700

    Try with latest canary build

--- packages/graphql-server/src/__tests__/cors.test.ts
+++ packages/graphql-server/src/__tests__/cors.test.ts
@@ -53,14 +53,14 @@ jest.mock('../directives/makeDirectives', () => {
 })
 
 describe('CORS', () => {
-  it('does stuff', async () => {
+  it('Returns the origin correctly when configured in handler', async () => {
     const handler = createGraphQLHandler({
       loggerConfig: { logger: createLogger({}), options: {} },
       sdls: {},
       directives: {},
       services: {},
       cors: {
-        origin: 'https://redwoodjs.com',
+        origin: 'https://web.redwoodjs.com',
       },
       onException: () => {},
     })
@@ -76,9 +76,103 @@ describe('CORS', () => {
       multiValueQueryStringParameters: null,
     } as unknown as APIGatewayProxyEvent
 
+    const response = await handler(mockedEvent, {} as Context)
+
+    expect(response.statusCode).toBe(200)
+    expect(response.multiValueHeaders['access-control-allow-origin']).toEqual([
+      'https://web.redwoodjs.com',
+    ])
+  })
+
+  it('Returns requestOrigin if cors origin set to true', async () => {
+    const handler = createGraphQLHandler({
+      loggerConfig: { logger: createLogger({}), options: {} },
+      sdls: {},
+      directives: {},
+      services: {},
+      cors: {
+        origin: true,
+      },
+      onException: () => {},
+    })
+
+    // We don't need to fully mock out a lambda event for these tests
+    const mockedEvent = {
+      headers: {
+        origin: 'https://someothersite.newjsframework.com',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query: '{ me { id, name } }' }),
+      httpMethod: 'POST',
+      multiValueQueryStringParameters: null,
+    } as unknown as APIGatewayProxyEvent
+
+    const response = await handler(mockedEvent, {} as Context)
+
+    expect(response.statusCode).toBe(200)
+    expect(response.multiValueHeaders['access-control-allow-origin']).toEqual([
+      'https://someothersite.newjsframework.com',
+    ])
+  })
+
+  it('Returns the origin for OPTIONS requests', async () => {
+    const handler = createGraphQLHandler({
+      loggerConfig: { logger: createLogger({}), options: {} },
+      sdls: {},
+      directives: {},
+      services: {},
+      cors: {
+        origin: 'https://mycrossdomainsite.co.uk',
+      },
+      onException: () => {},
+    })
+
+    // We don't need to fully mock out a lambda event for these tests
+    const mockedEvent = {
+      headers: {
+        origin: 'https://someothersite.newjsframework.com',
+        'Content-Type': 'application/json',
+      },
+      httpMethod: 'OPTIONS',
+      multiValueQueryStringParameters: null,
+    } as unknown as APIGatewayProxyEvent
+
+    const response = await handler(mockedEvent, {} as Context)
+
+    expect(response.statusCode).toBe(204)
+    expect(response.multiValueHeaders['access-control-allow-origin']).toEqual([
+      'https://mycrossdomainsite.co.uk',
+    ])
+  })
+
+  it('Returns the requestOrigin when moore than one origin supplied in config', async () => {
+    const handler = createGraphQLHandler({
+      loggerConfig: { logger: createLogger({}), options: {} },
+      sdls: {},
+      directives: {},
+      services: {},
+      cors: {
+        origin: ['https://site1.one', 'https://site2.two'],
+      },
+      onException: () => {},
+    })
+
+    // We don't need to fully mock out a lambda event for these tests
+    const mockedEvent = {
+      headers: {
+        origin: 'https://site2.two',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query: '{ me { id, name } }' }),
+      httpMethod: 'POST',
+      multiValueQueryStringParameters: null,
+    } as unknown as APIGatewayProxyEvent
+
     const response = await handler(mockedEvent, {} as Context) //?
 
     expect(response.statusCode).toBe(200)
-    expect(response.multiValueHeaders['access-control-allow-origin']).toBe(null)
+    expect(response.multiValueHeaders['access-control-allow-origin']).toEqual([
+      'https://site2.two',
+    ])
   })
 })
