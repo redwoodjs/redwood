863ed26abf8bcfc76aded7d9cba4fc1fa0c17e66
Rob Cameron <rob.cameron@fastmail.com>
2022-05-06 17:36:24 +0000

    Reverse environment/server order

--- packages/cli/src/commands/deploy/baremetal.js
+++ packages/cli/src/commands/deploy/baremetal.js
@@ -132,8 +132,8 @@ const commands = (yargs, ssh) => {
   )
   let envConfig
 
-  if (deployConfig.servers[yargs.environment]) {
-    envConfig = deployConfig.servers[yargs.environment]
+  if (deployConfig[yargs.environment]) {
+    envConfig = deployConfig[yargs.environment]
   } else if (
     yargs.environment === 'production' &&
     Array.isArray(deployConfig.servers)
@@ -149,7 +149,7 @@ const commands = (yargs, ssh) => {
   let tasks = []
 
   // loop through each server in deploy.toml
-  for (const serverConfig of envConfig) {
+  for (const serverConfig of envConfig.servers) {
     const sshOptions = {
       host: serverConfig.host,
       username: serverConfig.username,
