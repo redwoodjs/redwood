86aeccebe147fc3b929334c47871b84a861c3759
Tobbe Lundberg <tobbe@tlundberg.com>
2022-05-14 15:13:07 +0200

    WIP: Cell prerendering

---
+++ packages/prerender/src/graphql/graphql.ts
@@ -0,0 +1,183 @@
+import path from 'path'
+import glob from 'glob'
+
+import {
+  createGraphQLHandler,
+  SdlGlobImports,
+  ServicesGlobImports,
+} from '@redwoodjs/graphql-server'
+
+import { getPaths } from '@redwoodjs/internal'
+
+async function getDirectives() {
+  const paths = getPaths()
+  const directivePaths = glob
+    .sync(paths.api.directives + '/**/*.{js,ts}')
+    .filter((directivePath) =>
+      // Keep only files that have just one dot (.) in the filename
+      /^[^.]+\.(js|ts)$/.test(directivePath.split(path.sep).slice(-1)[0])
+    )
+
+  const directives = await directivePaths.reduce<
+    Promise<Record<string, unknown>>
+  >(async (prev, cur) => {
+    // `name` is just the filename without path, and without extension
+    const { name } = path.parse(cur)
+
+    return {
+      ...(await prev),
+      [name + '_' + name]: await import(cur),
+    }
+  }, Promise.resolve({}))
+
+  return directives
+}
+
+async function getSdls() {
+  const paths = getPaths()
+  const sdlPaths = glob.sync(paths.api.graphql + '/**/*.sdl.{js,ts}')
+
+  const sdls = await sdlPaths.reduce<Promise<SdlGlobImports>>(
+    async (prev, cur) => {
+      // `name` is just the filename without path, and without extension
+      const { name } = path.parse(cur)
+
+      return {
+        ...(await prev),
+        [name.replace('.', '_')]: await import(cur),
+      }
+    },
+    Promise.resolve({})
+  )
+
+  return sdls
+}
+
+async function getServices() {
+  const paths = getPaths()
+  const servicesPaths = glob
+    .sync(paths.api.services + '/**/*.{js,ts}')
+    .filter((servicePath) =>
+      // Keep only files that have just one dot (.) in the filename
+      /^[^.]+\.(js|ts)$/.test(servicePath.split(path.sep).slice(-1)[0])
+    )
+
+  const services = await servicesPaths.reduce<Promise<ServicesGlobImports>>(
+    async (prev, cur) => {
+      // `name` is just the filename without path, and without extension
+      const { name } = path.parse(cur)
+
+      return {
+        ...(await prev),
+        [name + '_' + name]: await import(cur),
+      }
+    },
+    Promise.resolve({})
+  )
+
+  return services
+}
+
+export async function graphqlHandler() {
+  const directives = await getDirectives()
+  const sdls = await getSdls()
+  const services = await getServices()
+
+  const logger = {
+    level: 'debug',
+    fatal: () => {},
+    error: () => {},
+    info: () => {},
+    debug: () => {},
+    trace: () => {},
+    warn: () => {},
+    silent: () => {},
+    child: () => {
+      return logger
+    },
+  }
+
+  const handler = createGraphQLHandler({
+    getCurrentUser: () => {
+      return Promise.resolve({})
+    },
+    loggerConfig: { logger: logger as any, options: {} },
+    directives,
+    sdls,
+    services,
+    onException: () => {
+      console.log('got an exception')
+    },
+  })
+
+  return (operation: Record<string, unknown>) => {
+    handler(buildApiEvent(operation), buildContext())
+  }
+}
+
+function buildApiEvent(body: Record<string, unknown>) {
+  return {
+    body: JSON.stringify(body),
+    headers: {
+      origin: 'http://localhost:8910',
+      accept: '*/*',
+      host: 'localhost:8910',
+    },
+    multiValueHeaders: {},
+    httpMethod: 'POST',
+    isBase64Encoded: false,
+    path: '/graphql',
+    pathParameters: null,
+    queryStringParameters: {},
+    multiValueQueryStringParameters: null,
+    stageVariables: null,
+    resource: '',
+    requestContext: {
+      requestId: 'req-3',
+      identity: {
+        sourceIp: '::1',
+        accessKey: null,
+        accountId: null,
+        apiKey: null,
+        apiKeyId: null,
+        caller: null,
+        clientCert: null,
+        cognitoAuthenticationProvider: null,
+        cognitoAuthenticationType: null,
+        cognitoIdentityId: null,
+        cognitoIdentityPoolId: null,
+        principalOrgId: null,
+        user: null,
+        userAgent: null,
+        userArn: null,
+      },
+      authorizer: {},
+      protocol: 'http',
+      httpMethod: 'POST',
+      path: '/graphql',
+      stage: '',
+      requestTimeEpoch: 0,
+      resourceId: '',
+      resourcePath: '',
+      accountId: '',
+      apiId: '',
+    },
+  }
+}
+
+function buildContext() {
+  return {
+    callbackWaitsForEmptyEventLoop: false,
+    functionName: '',
+    functionVersion: '',
+    invokedFunctionArn: '',
+    memoryLimitInMB: '',
+    awsRequestId: '',
+    logGroupName: '',
+    logStreamName: '',
+    getRemainingTimeInMillis: () => 100,
+    done: () => {},
+    fail: () => {},
+    succeed: () => {},
+  }
+}
--- packages/prerender/src/runPrerender.tsx
+++ packages/prerender/src/runPrerender.tsx
@@ -4,13 +4,87 @@ import path from 'path'
 import React from 'react'
 
 import cheerio from 'cheerio'
+import { Kind } from 'graphql'
 import ReactDOMServer from 'react-dom/server'
 
 import { getPaths, registerWebSideBabelHook } from '@redwoodjs/internal'
 import { LocationProvider } from '@redwoodjs/router'
+import { getProject } from '@redwoodjs/structure'
 
 import mediaImportsPlugin from './babelPlugins/babel-plugin-redwood-prerender-media-imports'
+import { graphqlHandler } from './graphql/graphql'
 import { getRootHtmlPath, registerShims, writeToDist } from './internal'
+
+function getCellQueries() {
+  const cellQueries = getProject(
+    '/Users/tobbe/tmp/rw-pageloadingcontext/'
+  ).cells.map((cell) => {
+    const ast = cell.queryAst
+
+    let vars
+
+    if (!ast) {
+      console.log('No ast for', cell)
+    } else {
+      for (const def of ast.definitions) {
+        if (def.kind === Kind.OPERATION_DEFINITION) {
+          vars = def.variableDefinitions
+            ?.map((varDef) => {
+              if (
+                varDef.type.kind === Kind.NON_NULL_TYPE &&
+                varDef.type.type.kind === Kind.NAMED_TYPE
+              ) {
+                return {
+                  name: varDef.variable.name.value,
+                  type: varDef.type.type.name.value,
+                }
+              } else {
+                console.log('Unhandled varDef', varDef)
+              }
+
+              return undefined
+            })
+            ?.filter(Boolean)
+        }
+      }
+    }
+
+    return {
+      path: cell.filePath,
+      operationName: cell.queryOperationName,
+      query: cell.queryString,
+      vars,
+    }
+  })
+
+  return cellQueries
+}
+
+async function getCellData() {
+  const cellQueries = getCellQueries()
+
+  const gqlHandler = await graphqlHandler()
+
+  console.log('gqlHandler', gqlHandler)
+
+  const cellData = await Promise.all(
+    cellQueries.map(async (cellQuery) => {
+      const operation = {
+        operationName: cellQuery.operationName,
+        query: cellQuery.query,
+        variables: cellQuery.vars || {},
+      }
+
+      console.log('operation', operation)
+
+      const handlerResult = await gqlHandler(operation)
+
+      console.log('handlerResult', handlerResult)
+    })
+  )
+
+  return cellData
+}
 
 interface PrerenderParams {
   routerPath: string // e.g. /about, /dashboard/me
@@ -19,6 +93,9 @@ interface PrerenderParams {
 export const runPrerender = async ({
   routerPath,
 }: PrerenderParams): Promise<string | void> => {
+  const cellData = await getCellData()
+  console.log('cellData', cellData)
+
   // Prerender specific configuration
   // extends projects web/babelConfig
   registerWebSideBabelHook({
