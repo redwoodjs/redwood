29bb3dcbe242995610f564fdb65de1d39d4685ec
Daniel Choudhury <dannychoudhury@gmail.com>
2022-05-20 16:59:48 +0700

    Fix and rebuild test-project fixture

--- __fixtures__/test-project/web/src/layouts/ContactsLayout/ContactsLayout.tsx
+++ __fixtures__/test-project/web/src/layouts/ContactsLayout/ContactsLayout.tsx
@@ -11,11 +11,17 @@ const ContactsLayout = ({ children }: ContactLayoutProps) => {
       <Toaster toastOptions={{ className: 'rw-toast', duration: 6000 }} />
       <header className="rw-header">
         <h1 className="rw-heading rw-heading-primary">
-          <Link to={routes.contacts()} className="rw-link">
+          <Link
+            to={routes.contacts()}
+            className="rw-link"
+          >
             Contacts
           </Link>
         </h1>
-        <Link to={routes.newContact()} className="rw-button rw-button-green">
+        <Link
+          to={routes.newContact()}
+          className="rw-button rw-button-green"
+        >
           <div className="rw-button-icon">+</div> New Contact
         </Link>
       </header>
