344eedffb1e1c468d4fbcd61e51e639953f12f3b
Rob Cameron <cannikin@fastmail.com>
2022-06-09 15:54:00 -0700

    Create and throw named errors on web side

--- packages/auth/src/webAuthn.ts
+++ packages/auth/src/webAuthn.ts
@@ -4,7 +4,21 @@ import {
   startAuthentication,
 } from './simplewebauthn'
 
-export class WebAuthnAlreadyRegisteredError extends Error {
+export class WebAuthnRegistrationError extends Error {
+  constructor(message: string) {
+    super(message)
+    this.name = 'WebAuthnRegistrationError'
+  }
+}
+
+export class WebAuthnAuthenticationError extends Error {
+  constructor(message: string) {
+    super(message)
+    this.name = 'WebAuthnAuthenticationError'
+  }
+}
+
+export class WebAuthnAlreadyRegisteredError extends WebAuthnRegistrationError {
   constructor() {
     super('This device is already registered')
     this.name = 'WebAuthnAlreadyRegisteredError'
@@ -29,7 +43,9 @@ export const webAuthnAuthenticate = async () => {
     const options = await optionsResponse.json()
 
     if (optionsResponse.status !== 200) {
-      return { error: `Could not start authentication: ${options.error}` }
+      throw new WebAuthnAuthenticationError(
+        `Could not start authentication: ${options.error}`
+      )
     }
 
     const browserResponse = await startAuthentication(options)
@@ -42,7 +58,7 @@ export const webAuthnAuthenticate = async () => {
     })
 
     if (authResponse.status !== 200) {
-      throw new Error(
+      throw new WebAuthnAuthenticationError(
         `Could not complete authentication: ${
           (await authResponse.json()).error
         }`
@@ -51,7 +67,9 @@ export const webAuthnAuthenticate = async () => {
       return true
     }
   } catch (e: any) {
-    throw new Error(`Error while authenticating: ${e.message}`)
+    throw new WebAuthnAuthenticationError(
+      `Error while authenticating: ${e.message}`
+    )
   }
 }
 
@@ -67,9 +85,12 @@ export const webAuthnRegister = async () => {
     const options = await optionsResponse.json()
 
     if (optionsResponse.status !== 200) {
-      return { error: `Could not start registration: ${options.error}` }
+      throw new WebAuthnRegistrationError(
+        `Could not start registration: ${options.error}`
+      )
     }
 
+    console.info(options)
     const regResponse = await startRegistration(options)
 
     const verifyResponse = await fetch(global.RWJS_API_DBAUTH_URL, {
@@ -80,7 +101,9 @@ export const webAuthnRegister = async () => {
     })
 
     if (verifyResponse.status !== 200) {
-      throw new Error(`Could not complete registration: ${options.error}`)
+      throw new WebAuthnRegistrationError(
+        `Could not complete registration: ${options.error}`
+      )
     } else {
       return true
     }
@@ -88,7 +111,9 @@ export const webAuthnRegister = async () => {
     if (e.name === 'InvalidStateError') {
       throw new WebAuthnAlreadyRegisteredError()
     } else {
-      throw new Error(`Error while registering: ${e.message}`)
+      throw new WebAuthnRegistrationError(
+        `Error while registering: ${e.message}`
+      )
     }
   }
 }
