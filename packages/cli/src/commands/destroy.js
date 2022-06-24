1f6ff2807a1589873a8684e6b54a1c662ef3b0b8
Daniel Choudhury <dannychoudhury@gmail.com>
2022-06-24 17:57:50 -0400

    Ignore unnecessary loads in generate and destroy cmds

--- packages/cli/src/commands/destroy.js
+++ packages/cli/src/commands/destroy.js
@@ -5,7 +5,16 @@ import terminalLink from 'terminal-link'
 
 export const builder = (yargs) =>
   yargs
-    .commandDir('./destroy', { recurse: true })
+    .commandDir('./destroy', {
+      recurse: true,
+      /*
+      @NOTE This regex will ignore all double nested commands
+      e.g. /destroy/hi.js & destroy/hi/hi.js are picked up,
+      but destroy/hi/__tests__/whatever.test.js will be ignored
+      The [\/\\] bit is for supporting both windows and unix style paths
+      */
+      exclude: /destroy[\/\\]+.*[\/\\]+.*[\/\\]/,
+    })
     .demandCommand()
     .epilogue(
       `Also see the ${terminalLink(
