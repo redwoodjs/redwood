4c3b2483f247bb3ad54d2e112a954b154e497f40
Dominic Saadi <dominiceliassaadi@gmail.com>
2022-06-27 17:21:47 +0900

    feat: apply suggestion

--- packages/testing/src/api/directive.ts
+++ packages/testing/src/api/directive.ts
@@ -33,64 +33,40 @@ interface DirectiveMocker {
 }
 
 /**
- *
  * @description
- * Used for writing tests for asynchronous directives e.g.
- * - Transformer directives can be passed resolvedValue
+ *
+ * Used for writing both synchronous and asynchronous directive tests e.g.
+ *
+ * - Transformer directives can be passed mockedResolvedValue
  * - Validator directives should check for errors thrown in certain situations
  *
  * @example
- *  const mockExecution = mockAsyncRedwoodDirective(myTransformer, {
- *    context: currentUser,
- *    mockedResolvedValue: 'Original Value',
- *  })
  *
- *  await expect(mockExecution).resolves.not.toThrow()
- *  await expect(mockExecution()).resolves.toEqual('Transformed Value')
- */
-export const mockAsyncRedwoodDirective: DirectiveMocker = (
-  directive,
-  executionMock
-) => {
-  const { directiveArgs = {}, context, ...others } = executionMock
-
-  if (context) {
-    setContext(context || {})
-  }
-
-  return async () => {
-    if (directive.type === DirectiveType.TRANSFORMER) {
-      const { mockedResolvedValue } = others as TransformerMock
-      return await directive.onResolverCalled({
-        resolvedValue: mockedResolvedValue,
-        context: globalContext,
-        ...others,
-      } as DirectiveParams)
-    } else {
-      await directive.onResolverCalled({
-        context: globalContext,
-        directiveArgs,
-        ...others,
-      } as DirectiveParams)
-    }
-  }
-}
-
-/**
+ * Synchronous transformer directive:
  *
- * @description
- * Used for writing directive tests e.g.
- * - Transformer directives can be passed resolvedValue
- * - Validator directives should check for errors thrown in certain situtations
+ * ```ts
+ * const mockExecution = mockRedwoodDirective(myTransformer, {
+ *   context: currentUser,
+ *   mockedResolvedValue: 'Original Value',
+ * })
+ *
+ * expect(mockExecution).not.toThrow()
+ * expect(mockExecution()).toEqual('Transformed Value')
+ * ```ts
  *
  * @example
- *  const mockExecution = mockRedwoodDirective(myTransformer, {
- *    context: currentUser,
- *    resolvedValue: 'Original Value',
- *  })
  *
- *  expect(mockExecution).not.toThrow()
- *  expect(mockExecution()).toEqual('Transformed Value')
+ * Asynchronous transformer directive:
+ *
+ * ```ts
+ * const mockExecution = mockRedwoodDirective(myTransformer, {
+ *   context: currentUser,
+ *   mockedResolvedValue: 'Original Value',
+ * })
+ *
+ * await expect(mockExecution).resolves.not.toThrow()
+ * await expect(mockExecution()).resolves.toEqual('Transformed Value')
+ * ```
  */
 export const mockRedwoodDirective: DirectiveMocker = (
   directive,
@@ -100,6 +76,25 @@ export const mockRedwoodDirective: DirectiveMocker = (
 
   if (context) {
     setContext(context || {})
+  }
+
+  if (directive.onResolverCalled.constructor.name === 'AsyncFunction') {
+    return async () => {
+      if (directive.type === DirectiveType.TRANSFORMER) {
+        const { mockedResolvedValue } = others as TransformerMock
+        return directive.onResolverCalled({
+          resolvedValue: mockedResolvedValue,
+          context: globalContext,
+          ...others,
+        } as DirectiveParams)
+      } else {
+        await directive.onResolverCalled({
+          context: globalContext,
+          directiveArgs,
+          ...others,
+        } as DirectiveParams)
+      }
+    }
   }
 
   return () => {
