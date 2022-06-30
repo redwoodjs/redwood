3af656ea72b88b636b2938f871b23497ec51f2b3
Daniel Choudhury <dannychoudhury@gmail.com>
2022-06-30 11:46:36 -0400

    Remove destroy exclusion

--- packages/cli/src/commands/destroy.js
+++ packages/cli/src/commands/destroy.js
@@ -5,16 +5,7 @@ import terminalLink from 'terminal-link'
 
 export const builder = (yargs) =>
   yargs
-    .commandDir('./destroy', {
-      recurse: true,
-      /*
-      @NOTE This regex will ignore all double nested commands
-      e.g. /destroy/hi.js & destroy/hi/hi.js are picked up,
-      but destroy/hi/utils/whatever.test.js will be ignored
-      The [\/\\] bit is for supporting both windows and unix style paths
-      */
-      exclude: /destroy[\/\\]+.*[\/\\]+.*[\/\\]/,
-    })
+    .commandDir('./destroy', { recurse: true })
     .demandCommand()
     .epilogue(
       `Also see the ${terminalLink(
