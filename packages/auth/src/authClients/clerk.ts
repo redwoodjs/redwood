9f526a4b6bcf550de1a8e53d242d9274b3c1086e
Tobbe Lundberg <tobbe@tlundberg.com>
2022-07-03 13:11:09 +0200

    Debug smoke-test

--- packages/auth/src/authClients/clerk.ts
+++ packages/auth/src/authClients/clerk.ts
@@ -35,7 +35,8 @@ function clerkClient(propsClient: Clerk | null): Clerk | null {
 
 export const clerk = async (client: Clerk): Promise<AuthClientClerk> => {
   // We use the typescript dynamic import feature to pull in the react library only if clerk is needed.
-  const { useUser: useClerkUser } = await import('@clerk/clerk-react')
+  // const { useUser: useClerkUser } = await import('@clerk/clerk-react')
+  const useClerkUser = () => ({ isSignedIn: false, user: {}, isLoaded: false })
 
   return {
     type: 'clerk',
