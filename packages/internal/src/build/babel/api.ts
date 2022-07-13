8b7841e2d5804a6780b7206d985a8466c7a27201
Dominic Saadi <dominiceliassaadi@gmail.com>
2022-07-14 00:24:48 +0900

    update to 14.20

--- packages/internal/src/build/babel/api.ts
+++ packages/internal/src/build/babel/api.ts
@@ -17,7 +17,7 @@ import {
 // RedwoodJS targets Node.js 14.x because it's the default version for Netlify and Vercel's functions:
 // - https://docs.netlify.com/functions/build-with-javascript/#runtime-settings
 // - https://vercel.com/docs/runtimes#official-runtimes/node-js/node-js-version
-const TARGETS_NODE = '14.19'
+const TARGETS_NODE = '14.20'
 // Warning! Use the minor core-js version: "corejs: '3.6'", instead of "corejs: 3",
 // because we want to include the features added in the minor version.
 // https://github.com/zloirock/core-js/blob/master/README.md#babelpreset-env
