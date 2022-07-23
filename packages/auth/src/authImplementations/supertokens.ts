ee9018c316d0f09a0562605c1f96b70e9494c2a9
Tobbe Lundberg <tobbe@tlundberg.com>
2022-07-23 09:11:57 +0200

    SuperTokens authImplementation

---
+++ packages/auth/src/authImplementations/supertokens.ts
@@ -0,0 +1,99 @@
+import { CurrentUser } from 'src/AuthContext'
+import { createAuthentication } from 'src/authFactory'
+
+import { AuthImplementation } from './AuthImplementation'
+
+export interface SuperTokensUser {
+  userId: string
+  accessTokenPayload: any
+}
+
+type SessionRecipe = {
+  signOut: () => Promise<void>
+  doesSessionExist: () => Promise<boolean>
+  getAccessTokenPayloadSecurely: () => Promise<any>
+  getUserId: () => Promise<string>
+}
+
+type AuthRecipe = {
+  redirectToAuth: (input: 'signin' | 'signup') => void
+}
+
+type SuperTokensAuthImplementation = AuthImplementation<
+  SuperTokensUser,
+  never,
+  void,
+  void,
+  void,
+  never,
+  never,
+  never,
+  never
+>
+
+const superTokensCreateAuthentication = (
+  authImplementation: SuperTokensAuthImplementation,
+  customProviderHooks?: {
+    useCurrentUser?: () => Promise<Record<string, unknown>>
+    useHasRole?: (
+      currentUser: CurrentUser | null
+    ) => (rolesToCheck: string | string[]) => boolean
+  }
+) => createAuthentication(authImplementation, customProviderHooks)
+
+export function createSuperTokensAuth(
+  superTokens: {
+    authRecipe: AuthRecipe
+    sessionRecipe: SessionRecipe
+  },
+  customProviderHooks?: {
+    useCurrentUser?: () => Promise<Record<string, unknown>>
+    useHasRole?: (
+      currentUser: CurrentUser | null
+    ) => (rolesToCheck: string | string[]) => boolean
+  }
+): ReturnType<typeof superTokensCreateAuthentication> {
+  const authImplementation = createSuperTokensAuthImplementation(superTokens)
+
+  return superTokensCreateAuthentication(
+    authImplementation,
+    customProviderHooks
+  )
+}
+
+function createSuperTokensAuthImplementation(superTokens: {
+  authRecipe: AuthRecipe
+  sessionRecipe: SessionRecipe
+}): SuperTokensAuthImplementation {
+  return {
+    type: 'supertokens',
+    login: async () => superTokens.authRecipe.redirectToAuth('signin'),
+
+    signup: async () => superTokens.authRecipe.redirectToAuth('signup'),
+
+    logout: async () => superTokens.sessionRecipe.signOut(),
+
+    getToken: async (): Promise<string | null> => {
+      if (await superTokens.sessionRecipe.doesSessionExist()) {
+        const accessTokenPayload =
+          await superTokens.sessionRecipe.getAccessTokenPayloadSecurely()
+        const jwtPropertyName = accessTokenPayload['_jwtPName']
+        return accessTokenPayload[jwtPropertyName]
+      } else {
+        return null
+      }
+    },
+
+    getUserMetadata: async (): Promise<SuperTokensUser | null> => {
+      if (await superTokens.sessionRecipe.doesSessionExist()) {
+        return {
+          userId: await superTokens.sessionRecipe.getUserId(),
+          accessTokenPayload:
+            await superTokens.sessionRecipe.getAccessTokenPayloadSecurely(),
+        }
+      } else {
+        return null
+      }
+    },
+  }
+}
