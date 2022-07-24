03153427cadf721811eb334ecfb4ca2dcb4640c0
Peter Pistorius <peter.pistorius@gmail.com>
2022-07-24 08:51:48 +0200

    Attempt to directly import methods.

--- packages/cli/src/index.js
+++ packages/cli/src/index.js
@@ -9,26 +9,26 @@ import yargs from 'yargs'
 // import { telemetryMiddleware } from '@redwoodjs/telemetry'
 
 import * as buildCommand from './commands/build'
-// import * as checkCommand from './commands/check'
+import * as checkCommand from './commands/check'
 import * as consoleCommand from './commands/console'
-// import * as dataMigrateCommand from './commands/data-migrate'
-// import * as deployCommand from './commands/deploy'
-// import * as destroyCommand from './commands/destroy'
+import * as dataMigrateCommand from './commands/data-migrate'
+import * as deployCommand from './commands/deploy'
+import * as destroyCommand from './commands/destroy'
 import * as devCommand from './commands/dev'
-// import * as execCommand from './commands/exec'
-// import * as generateCommand from './commands/generate'
-// import * as infoCommand from './commands/info'
-// import * as lintCommand from './commands/lint'
-// import * as prerenderCommand from './commands/prerender'
-// import * as prismaCommand from './commands/prisma'
-// import * as recordCommand from './commands/record'
-// import * as serveCommand from './commands/serve'
-// import * as setupCommand from './commands/setup'
-// import * as storybookCommand from './commands/storybook'
-// import * as testCommand from './commands/test'
-// import * as tstojsCommand from './commands/ts-to-js'
-// import * as typeCheckCommand from './commands/type-check'
-// import * as upgradeCommand from './commands/upgrade'
+import * as execCommand from './commands/exec'
+import * as generateCommand from './commands/generate'
+import * as infoCommand from './commands/info'
+import * as lintCommand from './commands/lint'
+import * as prerenderCommand from './commands/prerender'
+import * as prismaCommand from './commands/prisma'
+import * as recordCommand from './commands/record'
+import * as serveCommand from './commands/serve'
+import * as setupCommand from './commands/setup'
+import * as storybookCommand from './commands/storybook'
+import * as testCommand from './commands/test'
+import * as tstojsCommand from './commands/ts-to-js'
+import * as typeCheckCommand from './commands/type-check'
+import * as upgradeCommand from './commands/upgrade'
 
 /**
  * The current working directory can be set via:
@@ -94,26 +94,26 @@ yargs
     describe: 'Working directory to use (where `redwood.toml` is located.)',
   })
   .command(buildCommand)
-  // .command(checkCommand)
+  .command(checkCommand)
   .command(consoleCommand)
-  // .command(dataMigrateCommand)
-  // .command(deployCommand)
-  // .command(destroyCommand)
+  .command(dataMigrateCommand)
+  .command(deployCommand)
+  .command(destroyCommand)
   .command(devCommand)
-  // .command(execCommand)
-  // .command(generateCommand)
-  // .command(infoCommand)
-  // .command(lintCommand)
-  // .command(prerenderCommand)
-  // .command(prismaCommand)
-  // .command(recordCommand)
-  // .command(serveCommand)
-  // .command(setupCommand)
-  // .command(storybookCommand)
-  // .command(testCommand)
-  // .command(tstojsCommand)
-  // .command(typeCheckCommand)
-  // .command(upgradeCommand)
+  .command(execCommand)
+  .command(generateCommand)
+  .command(infoCommand)
+  .command(lintCommand)
+  .command(prerenderCommand)
+  .command(prismaCommand)
+  .command(recordCommand)
+  .command(serveCommand)
+  .command(setupCommand)
+  .command(storybookCommand)
+  .command(testCommand)
+  .command(tstojsCommand)
+  .command(typeCheckCommand)
+  .command(upgradeCommand)
   .example(
     'yarn rw g page home /',
     "\"Create a page component named 'Home' at path '/'\""
