47001b77ff020ca01fbf0da6a53b4ff16788b598
Peter Pistorius <peter.pistorius@gmail.com>
2022-07-24 09:14:48 +0200

    Remove unused imports.

--- packages/cli/src/commands/console.js
+++ packages/cli/src/commands/console.js
@@ -1,9 +1,3 @@
-import fs from 'fs'
-import path from 'path'
-import repl from 'repl'
-
-import { registerApiSideBabelHook } from '@redwoodjs/internal'
-
 export const command = 'console'
 export const aliases = ['c']
 export const description = 'Launch an interactive Redwood shell (experimental)'
