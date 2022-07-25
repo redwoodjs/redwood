4ca4aad9382e6fe1951dc9becd40072a91877bd0
Dominic Saadi <dominiceliassaadi@gmail.com>
2022-07-25 18:29:45 +0900

    wip changes

--- tasks/e2e/cypress.config.js
+++ tasks/e2e/cypress.config.js
@@ -1,25 +1,20 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  projectId: 'ao5sqb',
+  e2e: {
+    excludeSpecPattern: ['**/codemods/*.js', '**/sharedTests.js'],
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    supportFile: false,
+  },
+  // `runMode` is for `cypress run`, `openMode` is for `cypress open`.
+  // Locally, we use open. But in CI, we use run.
   retries: {
     runMode: 5,
     openMode: 0,
   },
-  videoUploadOnPasses: false,
-  video: false,
-  execTimeout: 120000,
-  pageLoadTimeout: 120000,
-  defaultCommandTimeout: 120000,
-  taskTimeout: 120000,
-  requestTimeout: 120000,
-  e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
-    excludeSpecPattern: ['**/codemods/*.js', '**/sharedTests.js'],
-    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
-  },
+  defaultCommandTimeout: 12_0000,
+  execTimeout: 12_0000,
+  pageLoadTimeout: 12_0000,
+  requestTimeout: 12_0000,
+  taskTimeout: 12_0000,
 })
