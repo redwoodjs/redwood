49680f1f826c01bad471c74f7df75550adc46948
Daniel Choudhury <dannychoudhury@gmail.com>
2022-07-25 12:14:25 +0100

    Add mockCurrentUser to fixture

--- __fixtures__/test-project/web/src/pages/ProfilePage/ProfilePage.stories.tsx
+++ __fixtures__/test-project/web/src/pages/ProfilePage/ProfilePage.stories.tsx
@@ -1,7 +1,13 @@
 import ProfilePage from './ProfilePage'
 
-export const generated = () => {
-  return <ProfilePage />
+export const generated = (args) => {
+  mockCurrentUser({
+    email: 'ba@zinga.com',
+    id: 55,
+    roles: 'ADMIN',
+  })
+
+  return <ProfilePage {...args} />
 }
 
 export default { title: 'Pages/ProfilePage' }
