ff7032633f2e99dffb2857346b2bd4132f77a636
Rob Cameron <cannikin@fastmail.com>
2022-07-25 11:36:39 -0700

    Revert "Make base64url a dependency install only if setting up dbAuth + webAuthn"
    This reverts commit 6d81c79e8fce7ab382afe7670cfbcd62063bda6d.

--- packages/cli/src/commands/setup/auth/providers/dbAuth.webAuthn.js
+++ packages/cli/src/commands/setup/auth/providers/dbAuth.webAuthn.js
@@ -20,7 +20,7 @@ export const config = {
 
 // required packages to install
 export const webPackages = ['@simplewebauthn/browser']
-export const apiPackages = ['@simplewebauthn/server', 'base64url']
+export const apiPackages = ['@simplewebauthn/server']
 
 // any notes to print out when the job is done
 export const notes = [
