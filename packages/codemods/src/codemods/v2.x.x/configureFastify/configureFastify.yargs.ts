6a0b05a702aee31f639fadb0780de13530e5b6af
Daniel Choudhury <dannychoudhury@gmail.com>
2022-07-28 16:37:11 +0100

    Fix linting

--- packages/codemods/src/codemods/v2.x.x/configureFastify/configureFastify.yargs.ts
+++ packages/codemods/src/codemods/v2.x.x/configureFastify/configureFastify.yargs.ts
@@ -9,18 +9,15 @@ export const command = 'configure-fastify'
 export const description = '(v2.x.x->v2.x.x) Converts world to bazinga'
 
 export const handler = () => {
-  task(
-    'Configure Fastify',
-    async ({ setOutput }: task.TaskInnerApi) => {
-      await runTransform({
-        transformPath: path.join(__dirname, 'configureFastify.js'),
-        // Here we know exactly which file we need to transform, but often times you won't.
-        // If you need to transform files based on their name, location, etc, use `fast-glob`.
-        // If you need to transform files based on their contents, use `getFilesWithPattern`.
-        targetPaths: [path.join(getRWPaths().base, 'redwood.toml')],
-      })
+  task('Configure Fastify', async ({ setOutput }: task.TaskInnerApi) => {
+    await runTransform({
+      transformPath: path.join(__dirname, 'configureFastify.js'),
+      // Here we know exactly which file we need to transform, but often times you won't.
+      // If you need to transform files based on their name, location, etc, use `fast-glob`.
+      // If you need to transform files based on their contents, use `getFilesWithPattern`.
+      targetPaths: [path.join(getRWPaths().base, 'redwood.toml')],
+    })
 
-      setOutput('All done! Run `yarn rw lint --fix` to prettify your code')
-    }
-  )
+    setOutput('All done! Run `yarn rw lint --fix` to prettify your code')
+  })
 }
