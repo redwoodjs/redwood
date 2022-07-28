4dafcf02f719a856279010c73cce5102dde9b853
David Thyresson <dthyresson@gmail.com>
2022-07-28 12:37:22 -0400

    Refactor codemod to use AssignmentExpression

--- packages/codemods/src/codemods/v2.x.x/configureFastify/configureFastify.ts
+++ packages/codemods/src/codemods/v2.x.x/configureFastify/configureFastify.ts
@@ -4,25 +4,19 @@ export default function transform(file: FileInfo, api: API) {
   const j = api.jscodeshift
   const ast = j(file.source)
 
-  ast.find(j.ExpressionStatement).forEach((path) => {
-    if (path.value.expression.type === 'AssignmentExpression') {
-      const lhs = path.value.expression.left as MemberExpression
-      if (lhs.type === 'MemberExpression') {
-        const rhs = path.value.expression.right as Identifier
-        if (rhs.name === 'config') {
-          if (lhs) {
-            j(path).replaceWith(
-              j.expressionStatement(
-                j.assignmentExpression(
-                  '=',
-                  j.identifier('module.exports'),
-                  j.identifier('{ config }')
-                )
-              )
-            )
-          }
-        }
-      }
+  ast.find(j.AssignmentExpression).forEach((path) => {
+    const lhs = path.value.left
+    const rhs = path.value.right
+    if (lhs && rhs.type === 'Identifier' && rhs.name === 'config') {
+      j(path).replaceWith(
+        j.expressionStatement(
+          j.assignmentExpression(
+            '=',
+            j.identifier('module.exports'),
+            j.identifier('{ config }')
+          )
+        )
+      )
     }
   })
 
