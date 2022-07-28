b2544b6f8c036ee4e800d74eadc20eefe7ef9409
Rob Cameron <cannikin@fastmail.com>
2022-07-28 16:03:08 -0700

    Refactor for more generic client usage, no more init()

--- packages/api/src/cache/index.ts
+++ packages/api/src/cache/index.ts
@@ -6,7 +6,7 @@ export * from './clients/redis'
 export interface CacheClient {
   init(): void
   get(key: string): Promise<{ value: Buffer; flags: Buffer }>
-  set(key: string, value: string | Buffer, options: object): Promise<boolean>
+  set(key: string, value: unknown, options: object): Promise<boolean>
 }
 
 export interface CacheOptions {
@@ -21,7 +21,6 @@ export type Cacheable = () => unknown
 
 export const createCache = (cacheClient: CacheClient) => {
   const client = cacheClient
-  client.init()
 
   const cache = async (
     key: CacheKey,
@@ -31,59 +30,63 @@ export const createCache = (cacheClient: CacheClient) => {
     try {
       const result = await client.get(key)
 
-      // value will be `null` if cache MISS
-      if (result?.value) {
+      if (result) {
         console.debug(`Cache HIT ${key}`)
-        return JSON.parse(result.value.toString())
-      }
-
-      let data
-
-      try {
-        console.debug(`Cache MISS ${key}`)
-        data = await input()
-        await client.set(key, JSON.stringify(data), options || {})
-        return data
-      } catch (e) {
-        console.error('Error in cache SET', e)
-        return data || (await input())
+        return result
       }
     } catch (e: any) {
-      console.error('Error in cache GET', e.message)
+      // error occurred, just return the input function as if the cache didn't
+      // even exist
+      console.error(`Error in cache GET for key '${key}'`, e.message)
       return await input()
+    }
+
+    // data wasn't found, SET it instead
+    let data
+
+    try {
+      console.debug(`Cache MISS ${key}`)
+      data = await input()
+      await client.set(key, data, options || {})
+      return data
+    } catch (e) {
+      console.error(`Error in cache SET for key '${key}'`, e)
+      return data || (await input())
+    }
+  }
+
+  const cacheLatest = async (
+    key: string,
+    query: LatestQuery,
+    options: CacheLatestOptions
+  ) => {
+    let cacheKey = key
+    const { model, ...rest } = options
+    const queryFunction = Object.keys(query)[0]
+    const conditions = query[queryFunction] as object
+
+    // take the conditions from the query that's going to be cached, and only
+    // return the latest record (based on `updatedAt`) from that set of
+    // records and use it as the cache key
+    try {
+      const latest = await model.findFirst({
+        ...conditions,
+        orderBy: { updatedAt: 'desc' },
+        select: { id: true, updatedAt: true },
+      })
+      cacheKey = `${key}-${latest.id}-${latest.updatedAt.getTime()}`
+
+      return cache(cacheKey, () => model[queryFunction](conditions), {
+        ...rest,
+      })
+    } catch (e) {
+      console.error('Error in cacheLatest', e)
+      return model[queryFunction](conditions)
     }
   }
 
   return {
     cache,
-    cacheLatest: async (
-      key: string,
-      query: LatestQuery,
-      options: CacheLatestOptions
-    ) => {
-      let cacheKey = key
-      const { model, ...rest } = options
-      const queryFunction = Object.keys(query)[0]
-      const conditions = query[queryFunction] as object
-
-      // take the conditions from the query that's going to be cached, and only
-      // return the latest record (based on `updatedAt`) from that set of
-      // records and use it as the cache key
-      try {
-        const latest = await model.findFirst({
-          ...conditions,
-          orderBy: { updatedAt: 'desc' },
-          select: { id: true, updatedAt: true },
-        })
-        cacheKey = `${key}-${latest.id}-${latest.updatedAt.getTime()}`
-
-        return cache(cacheKey, () => model[queryFunction](conditions), {
-          ...rest,
-        })
-      } catch (e) {
-        console.error('Error in cacheLatest', e)
-        return model[queryFunction](conditions)
-      }
-    },
+    cacheLatest,
   }
 }
