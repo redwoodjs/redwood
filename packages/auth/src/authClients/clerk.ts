e40ba155b7c9a5308c61134a92db0c54218dc33a
Dominic Saadi <dominiceliassaadi@gmail.com>
2022-08-09 23:55:05 +0900

    Revert auth provider refactor (#6135)
    * remove clerk codemod
    
    see https://github.com/redwoodjs/redwood/pull/5969
    
    * revert 4880 (fix clerk)
    
    see https://github.com/redwoodjs/redwood/pull/4880
    
    * changes for okta
    
    see https://github.com/redwoodjs/redwood/pull/5088
    
    * changes for webauthn
    
    see https://github.com/redwoodjs/redwood/pull/5680

--- packages/auth/src/authClients/clerk.ts
+++ packages/auth/src/authClients/clerk.ts
@@ -1,12 +1,9 @@
-import { useEffect } from 'react'
-
+import Clerk from '@clerk/clerk-js'
 import {
   UserResource as ClerkUserResource,
   SignInProps,
   SignUpProps,
   SignOutCallback,
-  Resources,
-  Clerk,
   GetTokenOptions,
   SignOutOptions,
   SignOut,
@@ -22,21 +19,18 @@ export type { Clerk }
 
 export type ClerkUser = ClerkUserResource & { roles: string[] | null }
 
-// Because Clerk's client is nulled out while it is loading, there is a race
-// condition under normal usage on a clean load of the app. This falls back
-// to the window.Clerk property when necessary to circumvent that.
-function clerkClient(propsClient: Clerk | null): Clerk | null {
-  if (!propsClient && typeof window !== undefined) {
-    return (window as any).Clerk ?? null
+// In production, there is an issue where the AuthProvider sometimes captures
+// Clerk as null. This intercepts that
+// issue and falls back to `window.Clerk` to access the client.
+function clerkClient(propsClient: Clerk | null) {
+  if (!propsClient) {
+    return window.Clerk ?? null
   } else {
     return propsClient
   }
 }
 
-export const clerk = async (client: Clerk): Promise<AuthClientClerk> => {
-  // We use the typescript dynamic import feature to pull in the react library only if clerk is needed.
-  const { useUser: useClerkUser } = await import('@clerk/clerk-react')
-
+export const clerk = (client: Clerk): AuthClientClerk => {
   return {
     type: 'clerk',
     client,
@@ -48,43 +42,6 @@ export const clerk = async (client: Clerk): Promise<AuthClientClerk> => {
     ) => clerkClient(client)?.signOut(callbackOrOptions as any, options),
     signup: async (options?: SignUpProps) =>
       clerkClient(client)?.openSignUp(options || {}),
-    restoreAuthState: async () => {
-      const clerk = clerkClient(client)
-      if (!clerk) {
-        // If the client is null, we can't restore state or listen for it to happen.
-        // This behavior is somewhat undefined, which is why we instruct the user to wrap
-        // the auth provider in <ClerkLoaded> to prevent it. For now we'll just return.
-
-        if (process.env.NODE_ENV === 'development') {
-          console.log('Please wrap your auth provider with `<ClerkLoaded>`')
-        }
-
-        return
-      }
-
-      // NOTE: Clerk's API docs say session will be undefined if loading (null if loaded and confirmed unset).
-      if (!clerk.client || clerk.session !== undefined) {
-        return new Promise<void>((res) => {
-          clerk.addListener((msg: Resources) => {
-            if (msg.session !== undefined && msg.client) {
-              res()
-            }
-          })
-        })
-      } else {
-        // In this case, we assume everything has been restored already.
-        return
-      }
-    },
-    // Hook to inform AuthProvider of Clerk's life-cycle
-    useListenForUpdates: ({ reauthenticate }) => {
-      const { isSignedIn, user, isLoaded } = useClerkUser()
-      useEffect(() => {
-        if (isLoaded) {
-          reauthenticate()
-        }
-      }, [isSignedIn, user, reauthenticate, isLoaded])
-    },
     getToken: async (options?: GetTokenOptions) => {
       let token
 
