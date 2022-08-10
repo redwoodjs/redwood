0156be959aea9a5d3704da577a5ae89ce90cc05a
Daniel Choudhury <dannychoudhury@gmail.com>
2022-08-10 14:14:57 +0100

    fix(generators): Dont set tests=false in sdl generators by default (#6195)
    * fix(generators): Dont set tests=false in sdl generators by default
    
    * Rebuild fixture
    
    * Put back jest resolution

--- __fixtures__/test-project/api/src/services/posts/posts.scenarios.ts
+++ __fixtures__/test-project/api/src/services/posts/posts.scenarios.ts
@@ -10,7 +10,7 @@ export const standard = defineScenario<Prisma.PostCreateArgs>({
         body: 'String',
         author: {
           create: {
-            email: 'String5263094',
+            email: 'String7710942',
             hashedPassword: 'String',
             fullName: 'String',
             salt: 'String',
@@ -24,7 +24,7 @@ export const standard = defineScenario<Prisma.PostCreateArgs>({
         body: 'String',
         author: {
           create: {
-            email: 'String5687670',
+            email: 'String6732615',
             hashedPassword: 'String',
             fullName: 'String',
             salt: 'String',
