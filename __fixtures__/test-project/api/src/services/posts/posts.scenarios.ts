b1127becfb346f0a0141dbc9879543a9b2bfa2e4
Daniel Choudhury <dannychoudhury@gmail.com>
2022-08-26 19:36:37 +0100

    Regen fixture

--- __fixtures__/test-project/api/src/services/posts/posts.scenarios.ts
+++ __fixtures__/test-project/api/src/services/posts/posts.scenarios.ts
@@ -10,7 +10,7 @@ export const standard = defineScenario<Prisma.PostCreateArgs>({
         body: 'String',
         author: {
           create: {
-            email: 'String1880855',
+            email: 'String5324606',
             hashedPassword: 'String',
             fullName: 'String',
             salt: 'String',
@@ -24,7 +24,7 @@ export const standard = defineScenario<Prisma.PostCreateArgs>({
         body: 'String',
         author: {
           create: {
-            email: 'String3103243',
+            email: 'String3607246',
             hashedPassword: 'String',
             fullName: 'String',
             salt: 'String',
--- __fixtures__/test-project/api/src/services/users/users.scenarios.ts
+++ __fixtures__/test-project/api/src/services/users/users.scenarios.ts
@@ -6,7 +6,7 @@ export const standard = defineScenario<Prisma.UserCreateArgs>({
   user: {
     one: {
       data: {
-        email: 'String8646974',
+        email: 'String5576131',
         hashedPassword: 'String',
         fullName: 'String',
         salt: 'String',
@@ -14,7 +14,7 @@ export const standard = defineScenario<Prisma.UserCreateArgs>({
     },
     two: {
       data: {
-        email: 'String1817344',
+        email: 'String3328572',
         hashedPassword: 'String',
         fullName: 'String',
         salt: 'String',
