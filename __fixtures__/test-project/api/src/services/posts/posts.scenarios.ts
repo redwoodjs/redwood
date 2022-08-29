40084032f7033a4daefe6b57d577882e2c3e2423
Daniel Choudhury <dannychoudhury@gmail.com>
2022-08-29 12:20:48 +0100

    Update full fixtures

--- __fixtures__/test-project/api/src/services/posts/posts.scenarios.ts
+++ __fixtures__/test-project/api/src/services/posts/posts.scenarios.ts
@@ -10,7 +10,7 @@ export const standard = defineScenario<Prisma.PostCreateArgs>({
         body: 'String',
         author: {
           create: {
-            email: 'String1880855',
+            email: 'String5205455',
             hashedPassword: 'String',
             fullName: 'String',
             salt: 'String',
@@ -24,7 +24,7 @@ export const standard = defineScenario<Prisma.PostCreateArgs>({
         body: 'String',
         author: {
           create: {
-            email: 'String3103243',
+            email: 'String4790139',
             hashedPassword: 'String',
             fullName: 'String',
             salt: 'String',
