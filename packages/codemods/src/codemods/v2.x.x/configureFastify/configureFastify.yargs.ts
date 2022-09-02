08641785091dcf02cb19f6b7a4fbc253df39be2c
David Thyresson <dthyresson@gmail.com>
2022-09-02 18:02:25 -0400

    Fix codemod error when trying to deal w/ toml

--- packages/codemods/src/codemods/v2.x.x/configureFastify/configureFastify.yargs.ts
+++ packages/codemods/src/codemods/v2.x.x/configureFastify/configureFastify.yargs.ts
@@ -6,16 +6,14 @@ import getRWPaths from '../../../lib/getRWPaths'
 import runTransform from '../../../lib/runTransform'
 
 export const command = 'configure-fastify'
-export const description = '(v2.x.x->v2.x.x) Converts world to bazinga'
+export const description =
+  '(v2.x.x->v2.x.x) Updates api side’s server.config.js to configure Fastify'
 
 export const handler = () => {
   task('Configure Fastify', async ({ setOutput }: task.TaskInnerApi) => {
     await runTransform({
       transformPath: path.join(__dirname, 'configureFastify.js'),
-      // Here we know exactly which file we need to transform, but often times you won't.
-      // If you need to transform files based on their name, location, etc, use `fast-glob`.
-      // If you need to transform files based on their contents, use `getFilesWithPattern`.
-      targetPaths: [path.join(getRWPaths().base, 'redwood.toml')],
+      targetPaths: [__dirname],
     })
 
     setOutput('All done! Run `yarn rw lint --fix` to prettify your code')
