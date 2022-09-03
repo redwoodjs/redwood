49bb347b70a01a5cfc47576649522056c732228c
Dominic Saadi <dominiceliassaadi@gmail.com>
2022-09-03 13:37:52 +0900

    target api/server.config.{js,ts}, prettify

--- packages/codemods/src/codemods/v2.x.x/configureFastify/configureFastify.yargs.ts
+++ packages/codemods/src/codemods/v2.x.x/configureFastify/configureFastify.yargs.ts
@@ -1,8 +1,11 @@
+import fs from 'fs'
 import path from 'path'
 
+import fg from 'fast-glob'
 import task from 'tasuku'
 
 import getRWPaths from '../../../lib/getRWPaths'
+import prettify from '../../../lib/prettify'
 import runTransform from '../../../lib/runTransform'
 
 export const command = 'configure-fastify'
@@ -11,10 +14,27 @@ export const description =
 
 export const handler = () => {
   task('Configure Fastify', async ({ setOutput }: task.TaskInnerApi) => {
+    const [API_SERVER_CONFIG_PATH] = fg.sync('server.config.{js,ts}', {
+      cwd: getRWPaths().api.base,
+      absolute: true,
+    })
+
     await runTransform({
       transformPath: path.join(__dirname, 'configureFastify.js'),
-      targetPaths: [__dirname],
+      targetPaths: [API_SERVER_CONFIG_PATH],
     })
+
+    // The transform generates two extra semicolons for some reason:
+    //
+    // ```js
+    // module.exports = { config };;
+    // ```
+    //
+    // They don't show up in tets cause we run prettier. Let's do the same here.
+    fs.writeFileSync(
+      API_SERVER_CONFIG_PATH,
+      prettify(fs.readFileSync(API_SERVER_CONFIG_PATH, 'utf-8'))
+    )
 
     setOutput('All done! Run `yarn rw lint --fix` to prettify your code')
   })
