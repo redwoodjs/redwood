8e66e4d96ff86ccac95d96b1d7813284b6891e97
Daniel Choudhury <dannychoudhury@gmail.com>
2022-09-04 21:39:43 +0100

    Bit of cleanup

--- packages/codemods/src/codemods/v2.x.x/updateResolverTypes/updateResolverTypes.yargs.ts
+++ packages/codemods/src/codemods/v2.x.x/updateResolverTypes/updateResolverTypes.yargs.ts
@@ -1,7 +1,7 @@
 import path from 'path'
 
 import fg from 'fast-glob'
-import task from 'tasuku'
+import task, { TaskInnerAPI } from 'tasuku'
 
 import getRWPaths from '../../../lib/getRWPaths'
 import runTransform from '../../../lib/runTransform'
@@ -11,17 +11,13 @@ export const description =
   '(v2.x.x->v2.x.x) Wraps types for "relation" resolvers in the bottom of service files'
 
 export const handler = () => {
-  task('Update Resolver Types', async ({ setOutput }: task.TaskInnerApi) => {
+  task('Update Resolver Types', async ({ setOutput }: TaskInnerAPI) => {
     await runTransform({
       transformPath: path.join(__dirname, 'updateResolverTypes.js'),
       // Target services written in TS only
       targetPaths: fg.sync('**/*.ts', {
-        cwd: path.join(getRWPaths().api.services),
-        ignore: [
-          '**/node_modules/**',
-          '**/*.test.{js,ts}',
-          '**/*.scenarios.{js,ts}',
-        ],
+        cwd: getRWPaths().api.services,
+        ignore: ['**/node_modules/**', '**/*.test.ts', '**/*.scenarios.ts'],
         absolute: true,
       }),
     })
