4bfe420c92d9c66c0032d676a12adf236ee68e31
Tobbe Lundberg <tobbe@tlundberg.com>
2022-09-05 14:56:56 +0200

    RelationResolver types

--- packages/internal/src/generate/plugins/rw-typescript-resolvers/index.ts
+++ packages/internal/src/generate/plugins/rw-typescript-resolvers/index.ts
@@ -37,13 +37,20 @@ export const plugin: PluginFunction<
     content: string
   }
 
-  // A couple of types needed for our own RW-specific solution
+  // A few types needed for our own RW-specific solution
   prepend.push(`export type OptArgsResolverFn<TResult, TParent, TContext, TArgs> = (
       args?: TArgs,
       obj?: { root: TParent; context: TContext; info: GraphQLResolveInfo }
     ) => TResult | Promise<TResult>
 
-    export type OptArgsResolver<TResult, TParent = {}, TContext = {}, TArgs = {}> = OptArgsResolverFn<TResult, TParent, TContext, TArgs>`)
+    export type OptArgsResolver<TResult, TParent = {}, TContext = {}, TArgs = {}> = OptArgsResolverFn<TResult, TParent, TContext, TArgs>
+
+    export type RequiredResolverFn<TResult, TParent, TContext, TArgs> = (
+      args: TArgs,
+      obj: { root: TParent; context: TContext; info: GraphQLResolveInfo }
+    ) => TResult | Promise<TResult>
+
+    export type RequiredResolver<TResult, TParent = {}, TContext = {}, TArgs = {}> = RequiredResolverFn<TResult, TParent, TContext, TArgs>`)
 
   // `content` is constructed like this:
   //   content: [
--- packages/internal/src/generate/plugins/rw-typescript-resolvers/visitor.ts
+++ packages/internal/src/generate/plugins/rw-typescript-resolvers/visitor.ts
@@ -2,7 +2,15 @@ import {
   TypeScriptResolversPluginConfig,
   TypeScriptResolversVisitor,
 } from '@graphql-codegen/typescript-resolvers'
-import { FieldDefinitionNode, GraphQLSchema } from 'graphql'
+import {
+  indent,
+  DeclarationBlock,
+} from '@graphql-codegen/visitor-plugin-common'
+import {
+  FieldDefinitionNode,
+  GraphQLSchema,
+  ObjectTypeDefinitionNode,
+} from 'graphql'
 
 export class RwTypeScriptResolversVisitor extends TypeScriptResolversVisitor {
   constructor(
@@ -35,5 +43,87 @@ export class RwTypeScriptResolversVisitor extends TypeScriptResolversVisitor {
 
       return fieldDef
     }
+  }
+
+  // This is mostly copy/paste from
+  // https://github.com/dotansimha/graphql-code-generator/blob/c6c60a3078f3797af435c3852220d8898964031d/packages/plugins/other/visitor-plugin-common/src/base-resolvers-visitor.ts#L1091
+  // Just duplicating every block to get RequiredResolver types for use with
+  // our relation resolvers
+  ObjectTypeDefinition(node: ObjectTypeDefinitionNode): string {
+    const declarationKind = 'type'
+    const name = this.convertName(node, {
+      suffix: this.config.resolverTypeSuffix,
+    })
+    const typeName = node.name as any as string
+    const parentType = this.getParentTypeToUse(typeName)
+    const isRootType = [
+      this.schema.getQueryType()?.name,
+      this.schema.getMutationType()?.name,
+      this.schema.getSubscriptionType()?.name,
+    ].includes(typeName)
+
+    const fieldsContent = (node.fields || []).map((f: any) => f(node.name))
+
+    if (!isRootType) {
+      fieldsContent.push(
+        indent(
+          `${
+            this.config.internalResolversPrefix
+          }isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>${this.getPunctuation(
+            declarationKind
+          )}`
+        )
+      )
+    }
+
+    const block = new DeclarationBlock(this._declarationBlockConfig)
+      .export()
+      .asKind(declarationKind)
+      .withName(
+        name,
+        `<ContextType = ${
+          this.config.contextType.type
+        }, ${this.transformParentGenericType(parentType)}>`
+      )
+      .withBlock(fieldsContent.join('\n'))
+
+    // This is what's different compared to the implementation I copy/pasted
+    // We create a new block with a different type called
+    // "ModelRelationResolver" where "Model" is the name of a prisma model.
+    // We also replace all Resolver types with RelationResolver types where
+    // all arguments are required
+    //
+    // It can look like this:
+    //   export type PostRelationResolvers<ContextType = RedwoodGraphQLContext, ParentType extends ResolversParentTypes['Post'] = ResolversParentTypes['Post']> = {
+    //     author: RelationResolver<ResolversTypes['User'], ParentType, ContextType>;
+    //     authorId: RelationResolver<ResolversTypes['Int'], ParentType, ContextType>;
+    //     body: RelationResolver<ResolversTypes['String'], ParentType, ContextType>;
+    //     createdAt: RelationResolver<ResolversTypes['DateTime'], ParentType, ContextType>;
+    //     id: RelationResolver<ResolversTypes['Int'], ParentType, ContextType>;
+    //     title: RelationResolver<ResolversTypes['String'], ParentType, ContextType>;
+    //     __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+    //   };
+    const blockRelationsResolver = new DeclarationBlock(
+      this._declarationBlockConfig
+    )
+      .export()
+      .asKind(declarationKind)
+      .withName(
+        name.replace('Resolver', 'RelationResolver'),
+        `<ContextType = ${
+          this.config.contextType.type
+        }, ${this.transformParentGenericType(parentType)}>`
+      )
+      .withBlock(
+        fieldsContent
+          .map((content) =>
+            content.replace(/: (?:OptArgs)?Resolver/, ': RequiredResolver')
+          )
+          .join('\n')
+      )
+
+    this._collectedResolvers[node.name as any] = name + '<ContextType>'
+
+    return block.string + '\n' + blockRelationsResolver.string
   }
 }
