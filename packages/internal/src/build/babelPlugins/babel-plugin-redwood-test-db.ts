30d2ca9955a81a3411a4248b458ca1de9b2f048b
Daniel Choudhury <dannychoudhury@gmail.com>
2022-09-08 18:15:20 +0100

    Add comment about redwood-test-db Add plugin test

--- packages/internal/src/build/babelPlugins/babel-plugin-redwood-test-db.ts
+++ packages/internal/src/build/babelPlugins/babel-plugin-redwood-test-db.ts
@@ -1,6 +1,17 @@
 import type { PluginObj, types } from '@babel/core'
 import template from '@babel/template'
 
+/**
+ * This plugin just adds a global variable to src/lib/db
+ * So we can track whether the Prisma client has been importted.
+ *
+ * This allows us to skip teardown/disconnect steps in tests if the db isn't used
+ * See other files:
+ * packages/testing/config/jest/api/jest.setup.js - where we check the global we set
+ * packages/testing/config/jest/api/apiBabelConfig.js - where this is used
+ *
+ */
+
 export default function ({ types: _t }: { types: typeof types }): PluginObj {
   return {
     name: 'babel-plugin-redwood-test-db',
