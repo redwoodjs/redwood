7ca4f7875b6a14db9078c999f248bd12f817fac4
Daniel Choudhury <dannychoudhury@gmail.com>
2022-09-09 13:55:02 +0100

    Break up RouteParser, add aditional tests

--- packages/router/src/__typetests__/routeParamsTypes.test.ts
+++ packages/router/src/__typetests__/routeParamsTypes.test.ts
@@ -1,41 +1,100 @@
+// import { A } from 'ts-toolbelt'
 import { expectType } from 'tsd-lite'
 
-import { RouteParams } from '@redwoodjs/router'
+import type { RouteParams, ParamType } from '@redwoodjs/router'
 
-test('Single parameters', () => {
-  const singleParameters: RouteParams<'bazinga/{id:Int}'> = {
-    id: 1,
-  }
+describe('RouteParams<>', () => {
+  test('Single parameters', () => {
+    const singleParameters: RouteParams<'bazinga/{id:Int}'> = {
+      id: 1,
+    }
 
-  expectType<{ id: number }>(singleParameters)
+    expectType<{ id: number }>(singleParameters)
+  })
+
+  test('Route string with no types defaults to string', () => {
+    const singleParameters: RouteParams<'/blog/{year}/{month}/{day}/{slug}'> = {
+      year: '2020',
+      month: '01',
+      day: '01',
+      slug: 'hello-world',
+    }
+
+    expectType<{ year: string; month: string; day: string; slug: string }>(
+      singleParameters
+    )
+  })
+
+  test('Custom param types', () => {
+    const customParams: RouteParams<'/post/{name:slug}'> = {
+      name: 'hello-world-slug',
+    }
+
+    expectType<{ name: string }>(customParams)
+  })
+
+  // test('Glob route params', () => {
+  //   const globRoutes: RouteParams<'/from/{fromDate...}/to/{toDate...}'> = {
+  //     fromDate: '2021/11/03',
+  //     toDate: '2021/11/17',
+  //   }
+
+  //   expectType<{ fromDate: string; toDate: string }>(globRoutes)
+  // })
+
+  test('Mixed typed and untyped params', () => {
+    const untypedFirst: RouteParams<'/mixed/{b}/{c:Boolean}'> = {
+      b: 'bazinga',
+      c: true,
+    }
+
+    const typedFirst: RouteParams<'/mixed/{b:Float}/{c}'> = {
+      b: 1245,
+      c: 'stringy-string',
+    }
+
+    expectType<{ b: string; c: boolean }>(untypedFirst)
+    expectType<{ b: number; c: string }>(typedFirst)
+  })
+
+  // test('Params in the middle', () => {
+  //   const paramsInTheMiddle: RouteParams<'/posts/{authorId:string}/{id:Int}/edit'> =
+  //     {
+  //       authorId: 'id:author',
+  //       id: 10,
+  //     }
+
+  //   //     A.Compute<{ authorId: string; id: number } & Record<string | number, any>>
+
+  //   expectType<{ authorId: string; id: number }>(paramsInTheMiddle)
+  // })
 })
 
-test('Route string with no types defaults to string', () => {
-  const singleParameters: RouteParams<'/blog/{year}/{month}/{day}/{slug}'> = {
-    year: '2020',
-    month: '01',
-    day: '01',
-    slug: 'hello-world',
-  }
+describe('ParamType<>', () => {
+  test('Float', () => {
+    const float: ParamType<'Float'> = 1
 
-  expectType<{ year: string; month: string; day: string; slug: string }>(
-    singleParameters
-  )
-})
+    expectType<number>(float)
+  })
 
-test('Custom param types', () => {
-  const customParams: RouteParams<'/post/{name:slug}'> = {
-    name: 'hello-world-slug',
-  }
+  test('Boolean', () => {
+    // Use a function because assigning a boolean narrows the type automatically
+    const returnBool: (a?: any) => ParamType<'Boolean'> = (a) => {
+      return !!a
+    }
 
-  expectType<{ name: string }>(customParams)
-})
+    expectType<boolean>(returnBool())
+  })
 
-test('Glob route params', () => {
-  const globRoutes: RouteParams<'/from/{fromDate...}/to/{toDate...}'> = {
-    fromDate: '2021/11/03',
-    toDate: '2021/11/17',
-  }
+  test('Int', () => {
+    const myInt: ParamType<'Int'> = 1
 
-  expectType<{ fromDate: string; toDate: string }>(globRoutes)
+    expectType<number>(myInt)
+  })
+
+  test('String', () => {
+    const myString: ParamType<'String'> = 'bazinga'
+
+    expectType<string>(myString)
+  })
 })
--- packages/router/src/routeParserTypes.ts
+++ packages/router/src/routeParserTypes.ts
@@ -16,29 +16,79 @@ export type ParamType<match> = match extends 'Int'
   ? number
   : string
 
+// This is used for a specific case where the first param doesnt have a type, but second one does
+type AdjacentParams<
+  TParam extends string,
+  TMatch extends string,
+  TRest extends string
+> = { [ParamName in TParam]: string } & ParsedParams<`${TRest}:${TMatch}}`> &
+  ParsedParams<`${TRest}`>
+
+type TypedParamInFront<
+  TParam extends string,
+  TMatch extends string,
+  TRest extends string
+> = TParam extends `${infer Param2}}/${infer Rest2}`
+  ? // check for greedy match (basically if the param contains a slash in it)
+    // e.g. in {b}/{c:Int} it matches b}/{c as the param
+    // Rest2 = {c, Match = Int so we reconstruct the old one {c + : + Int + }
+    AdjacentParams<Param2, TMatch, Rest2>
+  : // Otherwise its a regular match
+    { [Entry in TParam]: ParamType<TMatch> } & ParsedParams<`${TRest}`>
+
+// has type, but at the end e.g. {d:Int}
+type TypedParamAtEnd<
+  TParam extends string,
+  TMatch extends string
+> = TParam extends `${infer Param2}}/${infer Rest2}`
+  ? { [ParamName in Param2]: string } & ParsedParams<`${Rest2}:${TMatch}}`>
+  : { [Entry in TParam]: ParamType<TMatch> }
+
+// no type, but has stuff after it, e.g. {c}/{d}
+type NoTypesButParams<TParam extends string, TRest extends string> = {
+  [ParamName in TParam]: string
+} & ParsedParams<`${TRest}`>
+
+type JustParamNoType<TParam extends string> = { [ParamName in TParam]: string }
+
 // Path string parser for Redwood Routes
 type ParsedParams<PartialRoute> =
   // {a:Int}/[...moar]
   PartialRoute extends `{${infer Param}:${infer Match}}/${infer Rest}`
-    ? // check for greedy match e.g. {b}/{c:Int}
-      // Param = b}/{c, Rest2 = {c, Match = Int so we reconstruct the old one {c + : + Int + }
-      Param extends `${infer Param2}}/${infer Rest2}`
-      ? { [ParamName in Param2]: string } & ParsedParams<`${Rest2}:${Match}}`> &
-          ParsedParams<`${Rest}`>
-      : { [Entry in Param]: ParamType<Match> } & ParsedParams<`${Rest}`>
+    ? TypedParamInFront<Param, Match, Rest>
     : // has type, but at the end e.g. {d:Int}
     PartialRoute extends `{${infer Param}:${infer Match}}`
     ? // Greedy match order 2
-      Param extends `${infer Param2}}/${infer Rest2}`
-      ? { [ParamName in Param2]: string } & ParsedParams<`${Rest2}:${Match}}`>
-      : { [Entry in Param]: ParamType<Match> }
+      TypedParamAtEnd<Param, Match>
     : // no type, but has stuff after it, e.g. {c}/{d}
     PartialRoute extends `{${infer Param}}/${infer Rest}`
-    ? { [ParamName in Param]: string } & ParsedParams<`${Rest}`>
-    : // last one with no type e.g. {d}
+    ? NoTypesButParams<Param, Rest>
+    : // last one with no type e.g. {d} - just a param
     PartialRoute extends `{${infer Param}}`
-    ? { [ParamName in Param]: string }
+    ? JustParamNoType<Param>
     : // if theres a non param
     PartialRoute extends `${string}/${infer Rest}`
     ? ParsedParams<`${Rest}`>
-    : Record<string | number, any>
+    : // Fallback when doesn't match any of these
+      Record<string | number, any>
+
+/**
+ * Translation in pseudocode without ternaries
+ *
+if ('{c:Int}/...rest') {
+  checkForGreedyMatch()
+} else if ('{c:Int}') {
+  typedParamAtEnd()
+} else if ('{c}/...rest') {
+  noTypesButParams()
+} else if('{d}') {
+  justParamNoType()
+} else if ('bazinga/..rest') {
+  // Call itself
+  parseParamsRecursiveCall(rest)
+}
+
+
+{...glob}/...rest // beginning
+{...glob} // end
+**/
