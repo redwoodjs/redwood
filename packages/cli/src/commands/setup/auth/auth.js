6ff10af87793b3b65c7ae7f00475fb649ed13eb0
Tobbe Lundberg <tobbe@tlundberg.com>
2022-09-09 13:42:54 +0200

    Re-add setup auth command

---
+++ packages/cli/src/commands/setup/auth/auth.js
@@ -0,0 +1,47 @@
+import terminalLink from 'terminal-link'
+
+export const command = 'auth <provider>'
+
+export const description = 'Generate an auth configuration'
+
+export async function builder(yargs) {
+  const {
+    setupAuthAuth0Command,
+    setupAuthAzureActiveDirectoryCommand,
+    setupAuthClerkCommand,
+    setupAuthCustomCommand,
+    setupAuthDbAuthCommand,
+    setupAuthEthereumCommand,
+    setupAuthFirebaseCommand,
+    setupAuthGoTrueCommand,
+    setupAuthMagicLinkCommand,
+    setupAuthNetlifyCommand,
+    setupAuthNhostCommand,
+    setupAuthOktaCommand,
+    setupAuthSupabaseCommand,
+    setupAuthSupertokensCommand,
+  } = await import('@redwoodjs/auth-providers')
+
+  yargs
+    .demandCommand()
+    .epilogue(
+      `Also see the ${terminalLink(
+        'Redwood CLI Reference',
+        'https://redwoodjs.com/docs/cli-commands#setup-auth'
+      )}`
+    )
+    .command(setupAuthAuth0Command)
+    .command(setupAuthAzureActiveDirectoryCommand)
+    .command(setupAuthClerkCommand)
+    .command(setupAuthCustomCommand)
+    .command(setupAuthDbAuthCommand)
+    .command(setupAuthEthereumCommand)
+    .command(setupAuthFirebaseCommand)
+    .command(setupAuthGoTrueCommand)
+    .command(setupAuthMagicLinkCommand)
+    .command(setupAuthNetlifyCommand)
+    .command(setupAuthNhostCommand)
+    .command(setupAuthOktaCommand)
+    .command(setupAuthSupabaseCommand)
+    .command(setupAuthSupertokensCommand)
+}
