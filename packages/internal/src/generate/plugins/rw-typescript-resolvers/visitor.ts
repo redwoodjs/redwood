5f4d49ef91d54a5ea3a1a56c362d00b9c3aa8456
Tobbe Lundberg <tobbe@tlundberg.com>
2022-09-14 00:11:39 +0200

    Update test-project fixture and add more comments to visitor

--- packages/internal/src/generate/plugins/rw-typescript-resolvers/visitor.ts
+++ packages/internal/src/generate/plugins/rw-typescript-resolvers/visitor.ts
@@ -55,7 +55,6 @@ export class RwTypeScriptResolversVisitor extends TypeScriptResolversVisitor {
     // function. We're duplicating every block to get RequiredResolver types
     // for use with what we call "relation resolvers" (The stuff at the bottom
     // of service files for models that have relations)
-    const declarationKind = 'type'
     const name = this.convertName(node, {
       suffix: this.config.resolverTypeSuffix,
     })
@@ -75,7 +74,7 @@ export class RwTypeScriptResolversVisitor extends TypeScriptResolversVisitor {
           `${
             this.config.internalResolversPrefix
           }isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>${this.getPunctuation(
-            declarationKind
+            'type'
           )}`
         )
       )
@@ -84,26 +83,49 @@ export class RwTypeScriptResolversVisitor extends TypeScriptResolversVisitor {
     // This is what's different compared to the implementation I copy/pasted
     // We create a new block with a different type called
     // "ModelRelationResolver" where "Model" is the name of a prisma model.
-    // We also replace all Resolver types with RelationResolver types where
+    // We also replace all Resolver types with RequiredResolverFn types where
     // all arguments are required
     //
-    // It can look like this:
-    //   export type PostRelationResolvers<ContextType = RedwoodGraphQLContext, ParentType extends ResolversParentTypes['Post'] = ResolversParentTypes['Post']> = {
-    //     author?: RelationResolver<ResolversTypes['User'], ParentType, ContextType>;
-    //     authorId?: RelationResolver<ResolversTypes['Int'], ParentType, ContextType>;
-    //     body?: RelationResolver<ResolversTypes['String'], ParentType, ContextType>;
-    //     createdAt?: RelationResolver<ResolversTypes['DateTime'], ParentType, ContextType>;
-    //     id?: RelationResolver<ResolversTypes['Int'], ParentType, ContextType>;
-    //     title?: RelationResolver<ResolversTypes['String'], ParentType, ContextType>;
-    //     __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
-    //   };
+    // 1. We start by creating a DeclarationBlock. So something on the form
+    //    X = {}
+    // 2. Begin by defining X. It should be an exported type.
+    // 3. `name` is our resolvers, so like 'PostResolvers', 'RedwoodResolvers',
+    //    'QueryResolvers' etc.
+    //    Replace 'Resolvers' with 'RelationResolvers' to construct our type name.
+    //    Also set the generics for the type
+    // 4. Time to generate our Block, i.e. what goes inside {} in the X = {} that
+    //    we started with.
+    // 5. `fieldsContent` is something like
+    //    [
+    //      "  contact: Resolver<Maybe<ResolversTypes['Contact']>, ParentType, ContextType, RequireFields<QuerycontactArgs, 'id'>>;",
+    //      "  contacts: OptArgsResolverFn<Array<ResolversTypes['Contact']>, ParentType, ContextType>;",
+    //      "  post: Resolver<Maybe<ResolversTypes['Post']>, ParentType, ContextType, RequireFields<QuerypostArgs, 'id'>>;",
+    //      "  posts: OptArgsResolverFn<Array<ResolversTypes['Post']>, ParentType, ContextType>;",
+    //      "  redwood: OptArgsResolverFn<Maybe<ResolversTypes['Redwood']>, ParentType, ContextType>;",
+    //      "  user: Resolver<Maybe<ResolversTypes['User']>, ParentType, ContextType, RequireFields<QueryuserArgs, 'id'>>;"
+    //    ]
+    //    Here the type name needs to be replaced by 'RequiredResolverFn'. We
+    //    do that by running a regex `replace()` on the string
+    // 6. Finally we join the array with '\n' and we'll have something that
+    //    looks like the type below that we can return together whatever the
+    //    original implementation of this function returned (this is a
+    //    different example than the one above)
+    //    export type PostRelationResolvers<ContextType = RedwoodGraphQLContext, ParentType extends ResolversParentTypes['Post'] = ResolversParentTypes['Post']> = {
+    //      author?: RequiredResolverFn<ResolversTypes['User'], ParentType, ContextType>;
+    //      authorId?: RequiredResolverFn<ResolversTypes['Int'], ParentType, ContextType>;
+    //      body?: RequiredResolverFn<ResolversTypes['String'], ParentType, ContextType>;
+    //      createdAt?: RequiredResolverFn<ResolversTypes['DateTime'], ParentType, ContextType>;
+    //      id?: RequiredResolverFn<ResolversTypes['Int'], ParentType, ContextType>;
+    //      title?: RequiredResolverFn<ResolversTypes['String'], ParentType, ContextType>;
+    //      __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+    //    };
     const blockRelationsResolver = new DeclarationBlock(
       this._declarationBlockConfig
     )
       .export()
-      .asKind(declarationKind)
+      .asKind('type')
       .withName(
-        name.replace('Resolver', 'RelationResolver'),
+        name.replace('Resolvers', 'RelationResolvers'),
         `<ContextType = ${
           this.config.contextType.type
         }, ${this.transformParentGenericType(parentType)}>`
