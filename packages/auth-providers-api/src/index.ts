7e95bd33fa59cd10c14a33e49a0f24d9262d9c0d
Tobbe Lundberg <tobbe@tlundberg.com>
2022-09-13 13:25:41 +0200

    auth-providers -> auth-providers-api

---
+++ packages/auth-providers-api/src/index.ts
@@ -0,0 +1,4 @@
+export { authDecoder as dbAuthAuthDecoder } from './dbAuth/decoder'
+export { authDecoder as netlifyAuthDecoder } from './netlify/decoder'
+export { authDecoder as supabaseAuthDecoder } from './supabase/decoder'
+export { authDecoder as supertokensAuthDecoder } from './supertokens/decoder'
--- packages/auth-providers/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { authDecoder as dbAuthAuthDecoder } from './dbAuth/api/decoder'
-export { authDecoder as netlifyAuthDecoder } from './netlify/api/decoder'
-export { authDecoder as supabaseAuthDecoder } from './supabase/api/decoder'
-export { authDecoder as supertokensAuthDecoder } from './supertokens/api/decoder'
