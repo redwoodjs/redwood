0497253cc4c5d309409290d817f446d89ea29bc8
Dominic Saadi <dominiceliassaadi@gmail.com>
2022-09-15 19:39:06 +0700

    fix bin proxy

--- packages/web/src/bins/msw.ts
+++ packages/web/src/bins/msw.ts
@@ -3,7 +3,7 @@ import { createRequire } from 'module'
 
 const requireFromMSW = createRequire(require.resolve('msw/package.json'))
 
-const bins = requireFromMSW('./package.json')['bin']
+const bins = requireFromMSW('../package.json')['bin']
 
 // Most package.json's list their bins as relative paths, but not MSW.
-requireFromMSW(`./${bins['msw']}`)
+requireFromMSW(`../${bins['msw']}`)
