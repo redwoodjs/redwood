78f491b84f8d6df773f0b63144fc50ce8976ec81
Tobbe Lundberg <tobbe@tlundberg.com>
2022-09-17 23:49:58 +0200

    Update test project fixture

--- __fixtures__/test-project/api/src/services/posts/posts.scenarios.ts
+++ __fixtures__/test-project/api/src/services/posts/posts.scenarios.ts
@@ -10,7 +10,7 @@ export const standard = defineScenario<Prisma.PostCreateArgs>({
         body: 'String',
         author: {
           create: {
-            email: 'String8893764',
+            email: 'String9519070',
             hashedPassword: 'String',
             fullName: 'String',
             salt: 'String',
@@ -24,7 +24,7 @@ export const standard = defineScenario<Prisma.PostCreateArgs>({
         body: 'String',
         author: {
           create: {
-            email: 'String3198698',
+            email: 'String6435942',
             hashedPassword: 'String',
             fullName: 'String',
             salt: 'String',
--- __fixtures__/test-project/api/src/services/users/users.scenarios.ts
+++ __fixtures__/test-project/api/src/services/users/users.scenarios.ts
@@ -6,7 +6,7 @@ export const standard = defineScenario<Prisma.UserCreateArgs>({
   user: {
     one: {
       data: {
-        email: 'String1893596',
+        email: 'String854868',
         hashedPassword: 'String',
         fullName: 'String',
         salt: 'String',
@@ -14,7 +14,7 @@ export const standard = defineScenario<Prisma.UserCreateArgs>({
     },
     two: {
       data: {
-        email: 'String6766410',
+        email: 'String1526569',
         hashedPassword: 'String',
         fullName: 'String',
         salt: 'String',
