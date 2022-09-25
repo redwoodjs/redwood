9aef28d856175744227f497f38d95849a855a52e
Josh-Walker-GM <56300765+Josh-Walker-GM@users.noreply.github.com>
2022-09-25 19:34:48 +0100

    Replaced manual mock with automatic one

--- packages/cli/src/commands/__tests__/build.test.js
+++ packages/cli/src/commands/__tests__/build.test.js
@@ -21,11 +21,7 @@ jest.mock('@redwoodjs/internal/dist/config', () => {
 })
 
 import { Listr } from 'listr2'
-jest.mock('listr2', () => {
-  return jest.fn().mockImplementation(function FakeListr() {
-    return { run: jest.fn() }
-  })
-})
+jest.mock('listr2')
 
 // Make sure prerender doesn't get triggered
 jest.mock('execa', () =>
