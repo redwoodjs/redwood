79696edd50b27568ac5564d69b3c831db84cd059
Tobbe Lundberg <tobbe@tlundberg.com>
2022-09-28 11:08:25 +0200

    auth-providers-api ethereum tests

---
+++ packages/auth-providers-api/src/ethereum/__tests__/ethereum.test.ts
@@ -0,0 +1,46 @@
+import jwt from 'jsonwebtoken'
+
+import { authDecoder } from '../decoder'
+
+jest.mock('jsonwebtoken', () => ({
+  verify: jest.fn(),
+  decode: jest.fn(),
+}))
+
+let consoleError
+
+beforeAll(() => {
+  consoleError = console.error
+  console.error = () => {}
+})
+
+afterAll(() => {
+  console.error = consoleError
+})
+
+test('returns null for unsupported type', async () => {
+  const decoded = await authDecoder('token', 'netlify', {} as any)
+
+  expect(decoded).toBe(null)
+})
+
+test('throws if ETHEREUM_JWT_SECRET env var is not set', async () => {
+  delete process.env.ETHEREUM_JWT_SECRET
+
+  await expect(authDecoder('token', 'ethereum', {} as any)).rejects.toThrow(
+    'ETHEREUM_JWT_SECRET env var is not set'
+  )
+})
+
+test('verify, and not decode, should be called in production', () => {
+  const NODE_ENV = process.env.NODE_ENV
+  process.env.NODE_ENV = 'production'
+  process.env.ETHEREUM_JWT_SECRET = 'jwt-secret'
+
+  authDecoder('token', 'ethereum', {} as any)
+
+  expect(jwt.decode).not.toBeCalled()
+  expect(jwt.verify).toBeCalled()
+
+  process.env.NODE_ENV = NODE_ENV
+})
