d4ec44ba7e4655cbf9556db83137974777a46b84
Josh-Walker-GM <56300765+Josh-Walker-GM@users.noreply.github.com>
2022-09-30 23:27:34 +0100

    Added basic starter tests

--- packages/cli/src/commands/__tests__/update.test.js
+++ packages/cli/src/commands/__tests__/update.test.js
@@ -41,11 +41,13 @@ describe('Upgrade is not available (1.0.0 -> 1.0.0)', () => {
 
   test('No update message', async () => {
     await handler({})
-    expect(console.log.mock.calls.length).toBe(0)
+    expect(
+      console.log.mock.calls[console.log.mock.calls.length - 1][0]
+    ).toEqual(expect.stringContaining('No upgrade is available'))
   })
 })
 
-describe('Upgrade is available (1.0.0 -> 2.0.0)', () => {
+describe('Upgrade is not available (1.0.0 -> 2.0.0)', () => {
   beforeEach(() => {
     latestVersion.mockImplementation(() => {
       return '2.0.0'
@@ -64,11 +66,49 @@ describe('Upgrade is available (1.0.0 -> 2.0.0)', () => {
     console.log = jest.fn()
   })
 
-  test('No update message', async () => {
+  test('Correct update message', async () => {
     await handler({})
-    expect(console.log.mock.calls.length).toBe(1)
-    expect(console.log.mock.calls[0][0]).not.toBe(
+    expect(
+      console.log.mock.calls[console.log.mock.calls.length - 2][0]
+    ).toEqual(expect.stringContaining('New upgrade is available: 2.0.0'))
+
+    expect(
+      console.log.mock.calls[console.log.mock.calls.length - 1][0]
+    ).toEqual(
+      expect.stringContaining('Redwood Upgrade Available: 1.0.0 -> 2.0.0')
+    )
+  })
+})
+
+describe('Lock already set', () => {
+  beforeEach(() => {
+    latestVersion.mockImplementation(() => {
+      return '2.0.0'
+    })
+    fs.__setMockFiles({
+      'package.json': JSON.stringify({
+        devDependencies: {
+          '@redwoodjs/core': '^1.0.0',
+        },
+      }),
+      '/tmp/.redwood/update-data.json': JSON.stringify({
+        skipVersion: '0.0.0',
+      }),
+      '/tmp/.redwood/locks/update-command': '',
+    })
+
+    console.log = jest.fn()
+  })
+
+  test('Locked message', async () => {
+    await handler({})
+    expect(console.log.mock.calls[console.log.mock.calls.length - 1][0]).toBe(
       'An update command is already running, please try again in a few seconds.'
     )
+  })
+
+  test('Silent has no output', async () => {
+    await handler({ silent: true })
+    expect(console.log.mock.calls.length).toBe(0)
   })
 })
