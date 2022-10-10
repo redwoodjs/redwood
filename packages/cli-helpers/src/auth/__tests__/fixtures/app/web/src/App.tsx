95547da11dccb3e8ef7712b6f2a1b8cb5c2f6a27
Tobbe Lundberg <tobbe@tlundberg.com>
2022-10-10 22:18:12 +0200

    @redwoodjs/cli-helpers (#6629)
    * cli-helpers for decoupling auth
    
    * Fix tests and update versions
    
    * Update README file
    
    * cli-helpers depend on listr2

---
+++ packages/cli-helpers/src/auth/__tests__/fixtures/app/web/src/App.tsx
@@ -0,0 +1,19 @@
+import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
+import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
+
+import FatalErrorPage from 'src/pages/FatalErrorPage'
+import Routes from 'src/Routes'
+
+import './index.css'
+
+const App = () => (
+  <FatalErrorBoundary page={FatalErrorPage}>
+    <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
+      <RedwoodApolloProvider>
+        <Routes />
+      </RedwoodApolloProvider>
+    </RedwoodProvider>
+  </FatalErrorBoundary>
+)
+
+export default App
