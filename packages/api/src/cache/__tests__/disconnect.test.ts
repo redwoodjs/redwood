9fbd5893dd8172192b41966018af95a87525ba69
Daniel Choudhury <dannychoudhury@gmail.com>
2022-10-13 16:52:51 -0400

    fix: reconnect -> disconnect tests

--- packages/api/src/cache/__tests__/disconnect.test.ts
+++ packages/api/src/cache/__tests__/disconnect.test.ts
@@ -2,25 +2,25 @@ import InMemoryClient from '../clients/InMemoryClient'
 import { CacheTimeoutError } from '../errors'
 import { createCache } from '../index'
 
-describe('client.reconnect', () => {
+describe('client.disconnect', () => {
   beforeEach(() => {
     jest.clearAllMocks()
   })
 
-  it('attempts to reconnect on timeout error', async () => {
+  it('attempts to disconnect on timeout error', async () => {
     const client = new InMemoryClient()
     const { cache } = createCache(client)
     const getSpy = jest.spyOn(client, 'get')
     getSpy.mockImplementation(() => {
       throw new CacheTimeoutError()
     })
-    const reconnectSpy = jest.spyOn(client, 'reconnect')
+    const disconnectSpy = jest.spyOn(client, 'disconnect')
 
     await cache('test', () => {
       return { bar: 'baz' }
     })
 
     // returns existing cached value, not the one that was just set
-    expect(reconnectSpy).toHaveBeenCalled()
+    expect(disconnectSpy).toHaveBeenCalled()
   })
 })
