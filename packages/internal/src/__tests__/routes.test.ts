ea516abb311edcf4243688f44d75472c1e444316
Josh-Walker-GM <56300765+Josh-Walker-GM@users.noreply.github.com>
2022-11-09 02:45:31 +0000

    Added routes extraction, duplicate detection and associated tests

---
+++ packages/internal/src/__tests__/routes.test.ts
@@ -0,0 +1,83 @@
+import path from 'path'
+
+import { getRoutes, getDuplicateRoutes } from '../routes'
+
+const FIXTURE_PATH = path.resolve(
+  __dirname,
+  '../../../../__fixtures__/example-todo-main'
+)
+
+const FIXTURE_WITH_ERRORS_PATH = path.resolve(
+  __dirname,
+  '../../../../__fixtures__/example-todo-main-with-errors'
+)
+
+describe('Routes within the example todo', () => {
+  beforeEach(() => {
+    process.env.RWJS_CWD = FIXTURE_PATH
+  })
+  afterAll(() => {
+    delete process.env.RWJS_CWD
+  })
+
+  it('Detects all routes', () => {
+    expect(getRoutes()).toStrictEqual([
+      { name: 'home', page: 'HomePage', path: '/' },
+      {
+        name: 'typescriptPage',
+        page: 'TypeScriptPage',
+        path: '/typescript',
+      },
+      {
+        name: 'someOtherPage',
+        page: 'EditUserPage',
+        path: '/somewhereElse',
+      },
+      { name: 'fooPage', page: 'FooPage', path: '/foo' },
+      { name: 'barPage', page: 'BarPage', path: '/bar' },
+      {
+        name: 'privatePage',
+        page: 'PrivatePage',
+        path: '/private-page',
+      },
+      {
+        name: undefined,
+        page: 'NotFoundPage',
+        path: undefined,
+      },
+    ])
+  })
+})
+
+describe('Routes within the example todo with errors', () => {
+  beforeEach(() => {
+    process.env.RWJS_CWD = FIXTURE_WITH_ERRORS_PATH
+  })
+  afterAll(() => {
+    delete process.env.RWJS_CWD
+  })
+
+  it('Detects all routes', () => {
+    expect(getRoutes()).toStrictEqual([
+      { name: 'home', page: 'HomePage', path: '/' },
+      { name: 'home', page: 'HomePage', path: '/' },
+      {
+        name: 'something-else',
+        page: 'DoesNotExist',
+        path: '/{foo}/{foo}',
+      },
+      {
+        name: undefined,
+        page: 'NotFoundPage',
+        path: undefined,
+      },
+    ])
+  })
+
+  it('Detects dupliocate root routes', () => {
+    expect(getDuplicateRoutes()).toStrictEqual([
+      { name: 'home', page: 'HomePage', path: '/' },
+      { name: 'home', page: 'HomePage', path: '/' },
+    ])
+  })
+})
---
+++ packages/internal/src/routes.ts
@@ -0,0 +1,64 @@
+//
+
+import fs from 'fs'
+
+import { getPaths } from './paths'
+
+export interface Route {
+  name?: string
+  path?: string
+  page?: string // All valid routes should have a page but it's optional here for convenience
+}
+
+/**
+ * Extracts all the routes defined within the Routes.(js|tsx) file
+ */
+export function getRoutes() {
+  const routes: Route[] = []
+
+  const routesFile = fs.readFileSync(getPaths().web.routes, {
+    encoding: 'utf8',
+    flag: 'r',
+  })
+
+  const routeRE = /<Route (.*?)\/>/g
+  const nameRE = /(name)=\s*(?:"([^"]*)"|(\S+))/g
+  const pathRE = /(path)=\s*(?:"([^"]*)"|(\S+))/g
+  const pageRE = /(page)=\s*(?:{([^}]*)}|(\S+))/g
+
+  const routesInFile = routesFile.match(routeRE)
+  if (routesInFile) {
+    for (const route of routesInFile) {
+      const name = route.match(nameRE)?.at(0)?.split('=')[1].slice(1, -1)
+      const path = route.match(pathRE)?.at(0)?.split('=')[1].slice(1, -1)
+      const page = route.match(pageRE)?.at(0)?.split('=')[1].slice(1, -1)
+      routes.push({ name, path, page })
+    }
+  }
+
+  return routes
+}
+
+/**
+ * Returns an array of routes which conflict on their defined paths
+ */
+export function getDuplicateRoutes() {
+  const duplicateRoutes: Route[] = []
+  const routes = getRoutes()
+  const uniquePaths = routes
+    .map((route) => {
+      return route.path
+    })
+    .filter((value, index, self) => {
+      return self.indexOf(value) === index
+    })
+  uniquePaths.forEach((path) => {
+    const routesWithPath = routes.filter((route) => {
+      return route.path === path
+    })
+    if (routesWithPath.length > 1) {
+      duplicateRoutes.push(...routesWithPath)
+    }
+  })
+  return duplicateRoutes
+}
