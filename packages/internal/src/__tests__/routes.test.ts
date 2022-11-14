53aaf08ee933f8367106c99fa025e0efeb3de187
Josh-Walker-GM <56300765+Josh-Walker-GM@users.noreply.github.com>
2022-11-14 19:35:32 +0000

    Updated to use @redwoodjs/structure to extract routes

--- packages/internal/src/__tests__/routes.test.ts
+++ packages/internal/src/__tests__/routes.test.ts
@@ -1,6 +1,6 @@
 import path from 'path'
 
-import { getRoutes, getDuplicateRoutes } from '../routes'
+import { getDuplicateRoutes } from '../routes'
 
 const FIXTURE_PATH_EMPTY_PROJECT = path.resolve(
   __dirname,
@@ -23,21 +23,11 @@ const FIXTURE_PATH_TEST_PROJECT = path.resolve(
 )
 
 describe('Routes within the empty project', () => {
-  beforeEach(() => {
+  beforeAll(() => {
     process.env.RWJS_CWD = FIXTURE_PATH_EMPTY_PROJECT
   })
   afterAll(() => {
     delete process.env.RWJS_CWD
-  })
-
-  it('Detects all routes', () => {
-    expect(getRoutes()).toStrictEqual([
-      {
-        name: undefined,
-        page: 'NotFoundPage',
-        path: undefined,
-      },
-    ])
   })
 
   it('Detects no duplicate routes', () => {
@@ -46,39 +36,11 @@ describe('Routes within the empty project', () => {
 })
 
 describe('Routes within the example todo project', () => {
-  beforeEach(() => {
+  beforeAll(() => {
     process.env.RWJS_CWD = FIXTURE_PATH_EXAMPLE
   })
   afterAll(() => {
     delete process.env.RWJS_CWD
-  })
-
-  it('Detects all routes', () => {
-    expect(getRoutes()).toStrictEqual([
-      { name: 'home', page: 'HomePage', path: '/' },
-      {
-        name: 'typescriptPage',
-        page: 'TypeScriptPage',
-        path: '/typescript',
-      },
-      {
-        name: 'someOtherPage',
-        page: 'EditUserPage',
-        path: '/somewhereElse',
-      },
-      { name: 'fooPage', page: 'FooPage', path: '/foo' },
-      { name: 'barPage', page: 'BarPage', path: '/bar' },
-      {
-        name: 'privatePage',
-        page: 'PrivatePage',
-        path: '/private-page',
-      },
-      {
-        name: undefined,
-        page: 'NotFoundPage',
-        path: undefined,
-      },
-    ])
   })
 
   it('Detects no duplicate routes', () => {
@@ -87,28 +49,11 @@ describe('Routes within the example todo project', () => {
 })
 
 describe('Routes within the example todo with errors project', () => {
-  beforeEach(() => {
+  beforeAll(() => {
     process.env.RWJS_CWD = FIXTURE_PATH_EXAMPLE_WITH_ERRORS
   })
   afterAll(() => {
     delete process.env.RWJS_CWD
-  })
-
-  it('Detects all routes', () => {
-    expect(getRoutes()).toStrictEqual([
-      { name: 'home', page: 'HomePage', path: '/' },
-      { name: 'home', page: 'HomePage', path: '/' },
-      {
-        name: 'something-else',
-        page: 'DoesNotExist',
-        path: '/{foo}/{foo}',
-      },
-      {
-        name: undefined,
-        page: 'NotFoundPage',
-        path: undefined,
-      },
-    ])
   })
 
   it('Detects duplicate root routes', () => {
@@ -120,63 +65,11 @@ describe('Routes within the example todo with errors project', () => {
 })
 
 describe('Routes within the test project', () => {
-  beforeEach(() => {
+  beforeAll(() => {
     process.env.RWJS_CWD = FIXTURE_PATH_TEST_PROJECT
   })
   afterAll(() => {
     delete process.env.RWJS_CWD
-  })
-
-  it('Detects all routes', () => {
-    expect(getRoutes()).toStrictEqual([
-      { name: 'login', page: 'LoginPage', path: '/login' },
-      { name: 'signup', page: 'SignupPage', path: '/signup' },
-      {
-        name: 'forgotPassword',
-        page: 'ForgotPasswordPage',
-        path: '/forgot-password',
-      },
-      {
-        name: 'resetPassword',
-        page: 'ResetPasswordPage',
-        path: '/reset-password',
-      },
-      {
-        name: 'newContact',
-        page: 'ContactNewContactPage',
-        path: '/contacts/new',
-      },
-      {
-        name: 'editContact',
-        page: 'ContactEditContactPage',
-        path: '/contacts/{id:Int}/edit',
-      },
-      {
-        name: 'contact',
-        page: 'ContactContactPage',
-        path: '/contacts/{id:Int}',
-      },
-      { name: 'contacts', page: 'ContactContactsPage', path: '/contacts' },
-      { name: 'newPost', page: 'PostNewPostPage', path: '/posts/new' },
-      {
-        name: 'editPost',
-        page: 'PostEditPostPage',
-        path: '/posts/{id:Int}/edit',
-      },
-      { name: 'post', page: 'PostPostPage', path: '/posts/{id:Int}' },
-      { name: 'posts', page: 'PostPostsPage', path: '/posts' },
-      { name: 'waterfall', page: 'WaterfallPage', path: '/waterfall/{id:Int}' },
-      { name: 'profile', page: 'ProfilePage', path: '/profile' },
-      { name: 'blogPost', page: 'BlogPostPage', path: '/blog-post/{id:Int}' },
-      { name: 'contact', page: 'ContactPage', path: '/contact' },
-      { name: 'about', page: 'AboutPage', path: '/about' },
-      { name: 'home', page: 'HomePage', path: '/' },
-      {
-        name: undefined,
-        page: 'NotFoundPage',
-        path: undefined,
-      },
-    ])
   })
 
   it('Detects no duplicate routes', () => {
--- packages/internal/src/routes.ts
+++ packages/internal/src/routes.ts
@@ -1,63 +1,22 @@
-import fs from 'fs'
-
 import chalk from 'chalk'
+
+import { getProject } from '@redwoodjs/structure/dist/index'
 
 import { getPaths } from './paths'
 
-export interface Route {
+export interface RouteInformation {
   name?: string
   path?: string
   page?: string
 }
 
 /**
- * Extracts all the routes defined within the Routes.(js|tsx) file
- */
-export function getRoutes() {
-  const routes: Route[] = []
-
-  const routesFile = fs.readFileSync(getPaths().web.routes, {
-    encoding: 'utf8',
-    flag: 'r',
-  })
-
-  // Strip out commented code
-  let routesWithoutComments = routesFile
-  let startPosition = routesWithoutComments.indexOf('{/*')
-  let endPosition = -1
-  while (startPosition !== -1) {
-    endPosition = routesWithoutComments.indexOf('*/}')
-    routesWithoutComments =
-      routesWithoutComments.substring(0, startPosition) +
-      routesWithoutComments.substring(endPosition + 3)
-    startPosition = routesWithoutComments.indexOf('{/*')
-  }
-
-  const routeRE = /<Route (.*?)\/>/g
-  const nameRE = /(name)=\s*(?:"([^"]*)"|(\S+))/g
-  const pathRE = /(path)=\s*(?:"([^"]*)"|(\S+))/g
-  const pageRE = /(page)=\s*(?:{([^}]*)}|(\S+))/g
-
-  const routesInFile = routesWithoutComments.match(routeRE)
-  if (routesInFile) {
-    for (const route of routesInFile) {
-      const name = route.match(nameRE)?.at(0)?.split('=')[1].slice(1, -1)
-      const path = route.match(pathRE)?.at(0)?.split('=')[1].slice(1, -1)
-      const page = route.match(pageRE)?.at(0)?.split('=')[1].slice(1, -1)
-      routes.push({ name, path, page })
-    }
-  }
-
-  return routes
-}
-
-/**
  * Returns an array of routes which conflict on their defined names
  */
 export function getDuplicateRoutes() {
-  const duplicateRoutes: Route[] = []
-  const routes = getRoutes()
-  const uniqueNames = routes
+  const duplicateRoutes: RouteInformation[] = []
+  const allRoutes = getProject(getPaths().base).router.routes
+  const uniqueNames = allRoutes
     .map((route) => {
       return route.name
     })
@@ -65,11 +24,19 @@ export function getDuplicateRoutes() {
       return self.indexOf(value) === index
     })
   uniqueNames.forEach((name) => {
-    const routesWithName = routes.filter((route) => {
+    const routesWithName = allRoutes.filter((route) => {
       return route.name === name
     })
     if (routesWithName.length > 1) {
-      duplicateRoutes.push(...routesWithName)
+      duplicateRoutes.push(
+        ...routesWithName.map((route) => {
+          return {
+            name: route.name,
+            page: route.page_identifier_str,
+            path: route.path,
+          }
+        })
+      )
     }
   })
   return duplicateRoutes
