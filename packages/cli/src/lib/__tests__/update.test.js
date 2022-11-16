8534241caa65b3293c65a04539a993d6232aaf32
Josh-Walker-GM <56300765+Josh-Walker-GM@users.noreply.github.com>
2022-11-16 14:43:37 +0000

    Initial middleware tests added

--- packages/cli/src/lib/__tests__/update.test.js
+++ packages/cli/src/lib/__tests__/update.test.js
@@ -26,7 +26,21 @@ import { rw } from '../../__tests__/cwd.test'
 import { setLock } from '../locking'
 import * as update from '../update'
 
+const realfs = jest.requireActual('fs')
+
 const TESTING_CURRENT_DATETIME = 1640995200000
+const TEST_PROJECT_UPDATE_DATA_PATH = path.join(
+  __dirname,
+  '..',
+  '..',
+  '..',
+  '..',
+  '..',
+  '__fixtures__',
+  'test-project',
+  '.redwood',
+  'update-data.json'
+)
 
 describe('Upgrade is not available (1.0.0 -> 1.0.0)', () => {
   beforeAll(() => {
@@ -85,26 +99,175 @@ describe('Upgrade is not available (1.0.0 -> 1.0.0)', () => {
     )
   })
 
-  it.only('A command does not show an upgrade message', async () => {
-    const { stdout, stderr } = rw([
+  it('A command does not show an upgrade message', async () => {
+    realfs.writeFileSync(
+      TEST_PROJECT_UPDATE_DATA_PATH,
+      JSON.stringify({
+        localVersion: '1.0.0',
+        remoteVersion: '1.0.0',
+        checkedAt: Date.now(),
+        shownAt: update.DEFAULT_DATETIME_MS,
+      })
+    )
+    const { stdout, _ } = rw([
       '--cwd',
       path.join('__fixtures__', 'test-project'),
       'info',
     ])
-    console.log({ stdout, stderr })
-    expect(stdout).toBe('') // TODO: Need to ensure it does not contain an upgrade message
+    expect(stdout).not.toMatch(/Redwood Upgrade Available: 1.0.0/)
+  })
+})
+
+describe('Upgrade is available (1.0.0 -> 2.0.0)', () => {
+  beforeAll(() => {
+    // Use fake datetime
+    jest.useFakeTimers()
+    jest.setSystemTime(new Date(TESTING_CURRENT_DATETIME))
   })
 
-  it.only('A command after a check does not show an upgrade message', async () => {
-    await update.check()
+  afterAll(() => {
+    jest.useRealTimers()
+  })
 
-    const { stdout, stderr } = rw([
+  beforeEach(() => {
+    // Set the fake remote version
+    latestVersion.mockImplementation(() => {
+      return '2.0.0'
+    })
+
+    fs.__setMockFiles({
+      // Users package.json containing the redwood version
+      'package.json': JSON.stringify({
+        devDependencies: {
+          '@redwoodjs/core': '^1.0.0',
+        },
+      }),
+
+      // We add in the default update-data.json file otherwise we get "undefined" as the file contents when it doesn't exist - even though we do handle this case
+      '.redwood/update-data.json': JSON.stringify({
+        localVersion: '0.0.0',
+        remoteVersion: '0.0.0',
+        checkedAt: update.DEFAULT_DATETIME_MS,
+        shownAt: update.DEFAULT_DATETIME_MS,
+      }),
+    })
+  })
+
+  afterEach(() => {
+    fs.__setMockFiles({})
+    jest.restoreAllMocks()
+  })
+
+  it('Produces the correct update-data.json file', async () => {
+    await update.check()
+    expect(update.readUpdateFile()).toStrictEqual({
+      localVersion: '1.0.0',
+      remoteVersion: '2.0.0',
+      checkedAt: TESTING_CURRENT_DATETIME,
+      shownAt: update.DEFAULT_DATETIME_MS,
+    })
+  })
+
+  it('Respects the lock', async () => {
+    setLock(update.LOCK_IDENTIFIER)
+    await expect(update.check()).rejects.toThrow(
+      `Lock "${update.LOCK_IDENTIFIER}" is already set`
+    )
+  })
+
+  it('A command does show an upgrade message', async () => {
+    realfs.writeFileSync(
+      TEST_PROJECT_UPDATE_DATA_PATH,
+      JSON.stringify({
+        localVersion: '1.0.0',
+        remoteVersion: '2.0.0',
+        checkedAt: Date.now(),
+        shownAt: update.DEFAULT_DATETIME_MS,
+      })
+    )
+    const { stdout, _ } = rw([
       '--cwd',
       path.join('__fixtures__', 'test-project'),
       'info',
     ])
+    expect(stdout).toMatch(/Redwood Upgrade Available: 1.0.0 -> 2.0.0/)
+  })
+})
 
-    console.log({ stdout, stderr })
-    expect(stdout).toBe('') // TODO: Need to ensure it does not contain an upgrade message
+describe('Upgrade is available with rc tag (1.0.0-rc.1 -> 1.0.1-rc.58)', () => {
+  beforeAll(() => {
+    // Use fake datetime
+    jest.useFakeTimers()
+    jest.setSystemTime(new Date(TESTING_CURRENT_DATETIME))
+  })
+
+  afterAll(() => {
+    jest.useRealTimers()
+  })
+
+  beforeEach(() => {
+    // Set the fake remote version
+    latestVersion.mockImplementation(() => {
+      return '1.0.1-rc.58'
+    })
+
+    fs.__setMockFiles({
+      // Users package.json containing the redwood version
+      'package.json': JSON.stringify({
+        devDependencies: {
+          '@redwoodjs/core': '^1.0.0-rc.1',
+        },
+      }),
+
+      // We add in the default update-data.json file otherwise we get "undefined" as the file contents when it doesn't exist - even though we do handle this case
+      '.redwood/update-data.json': JSON.stringify({
+        localVersion: '0.0.0',
+        remoteVersion: '0.0.0',
+        checkedAt: update.DEFAULT_DATETIME_MS,
+        shownAt: update.DEFAULT_DATETIME_MS,
+      }),
+    })
+  })
+
+  afterEach(() => {
+    fs.__setMockFiles({})
+    jest.restoreAllMocks()
+  })
+
+  it('Produces the correct update-data.json file', async () => {
+    await update.check()
+    expect(update.readUpdateFile()).toStrictEqual({
+      localVersion: '1.0.0-rc.1',
+      remoteVersion: '1.0.1-rc.58',
+      checkedAt: TESTING_CURRENT_DATETIME,
+      shownAt: update.DEFAULT_DATETIME_MS,
+    })
+  })
+
+  it('Respects the lock', async () => {
+    setLock(update.LOCK_IDENTIFIER)
+    await expect(update.check()).rejects.toThrow(
+      `Lock "${update.LOCK_IDENTIFIER}" is already set`
+    )
+  })
+
+  it('A command does show an upgrade message', async () => {
+    realfs.writeFileSync(
+      TEST_PROJECT_UPDATE_DATA_PATH,
+      JSON.stringify({
+        localVersion: '1.0.0-rc.1',
+        remoteVersion: '1.0.1-rc.58',
+        checkedAt: Date.now(),
+        shownAt: update.DEFAULT_DATETIME_MS,
+      })
+    )
+    const { stdout, _ } = rw([
+      '--cwd',
+      path.join('__fixtures__', 'test-project'),
+      'info',
+    ])
+    expect(stdout).toMatch(
+      /Redwood Upgrade Available: 1.0.0-rc.1 -> 1.0.1-rc.58/
+    )
   })
 })
