5749349acf19e50ef4a7d7334da70ecd45d9cd1b
Dominic Saadi <dominiceliassaadi@gmail.com>
2022-11-17 10:57:53 -0800

    feat: add readme, expand milestone validation

--- tasks/release/branchStrategy/validateMilestonesCommand.mjs
+++ tasks/release/branchStrategy/validateMilestonesCommand.mjs
@@ -1,118 +1,153 @@
 /* eslint-env node, es2022 */
 
 import { Octokit } from 'octokit'
-import { chalk } from 'zx'
+import { $ } from 'zx'
+import { chalk, question } from 'zx'
 
 import { isCommitInBranch, getReleaseBranch } from './branchStrategyLib.mjs'
 
 export const command = 'validate-milestones'
-export const description = 'Validate PRs with the "next-release" milestone'
+export const description =
+  "Validate PRs' milestone (i.e., that a PR milestoned v3.5.0 is in release/minor/v3.5.0)"
 
-export async function handler() {
+export function builder(yargs) {
+  yargs.option('prompt', {
+    description: 'Prompt for confirmation before fixing',
+    type: 'boolean',
+    default: false,
+  })
+}
+
+export async function handler({ prompt }) {
   const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN })
-
-  const {
-    node: {
-      pullRequests: { nodes: prs },
-    },
-  } = await octokit.graphql(getNextReleasePRs)
-
-  const branch = await getReleaseBranch()
-  console.log()
 
   let {
     repository: {
-      milestones: { nodes: milestones },
+      milestones: { nodes },
     },
-  } = await octokit.graphql(getMilestoneIds)
+  } = await octokit.graphql(getPRs)
 
-  milestones = milestones.reduce((obj, { title, id }) => {
+  nodes = nodes.filter((node) => node.title !== 'chore')
+
+  if (
+    !nodes.every((milestone) => !milestone.pullRequests.pageInfo.hasNextPage)
+  ) {
+    console.log('A milestone has a next page; this script needs to be updated')
+    process.exit(1)
+  }
+
+  const prs = nodes.flatMap((milestone) => {
+    return milestone.pullRequests.nodes.map((pr) => {
+      pr.mergeCommit.message = pr.mergeCommit.message.split('\n').shift()
+
+      return {
+        ...pr,
+        milestone: milestone.title,
+      }
+    })
+  })
+
+  const milestoneTitlesToIds = nodes.reduce((obj, { title, id }) => {
     obj[title] = id
     return obj
   }, {})
 
-  for (const pr of prs) {
-    if (await isCommitInBranch(branch, pr.mergeCommit.messageHeadline)) {
-      console.log(
-        [
-          `${chalk.red('error')}: pr #${
-            pr.number
-          } should be milestoned ${chalk.green(branch.split('/')[2])}`,
-          `${chalk.blue('fixing')}: milestoning PR #${
-            pr.number
-          } to ${chalk.green(branch.split('/')[2])}`,
-        ].join('\n')
-      )
+  async function validateMilestone(pr, milestone) {
+    const hasCorrectMilestone = pr.milestone === milestone
 
-      await octokit.graphql(milestonePullRequest, {
-        pullRequestId: pr.id,
-        milestoneId: milestones[branch.split('/')[2]],
-      })
-
-      console.log(chalk.green('done'))
-      console.log()
-
-      continue
-    }
-
-    if (await isCommitInBranch('next', pr.mergeCommit.messageHeadline)) {
-      console.log(
-        `${chalk.green('ok')}: pr #${
-          pr.number
-        } should be milestoned next release`
-      )
-      console.log(chalk.green('done'))
-      console.log()
-      continue
-    }
-
+    console.log()
     console.log(
       [
-        `${chalk.red('error')}: pr #${
-          pr.number
-        } should be milestoned ${chalk.green('v4.0.0')}`,
-        `${chalk.blue('fixing')}: milestoning PR #${pr.number} to ${chalk.green(
-          'v4.0.0'
-        )}`,
+        `  #${chalk.yellow(pr.number)} ${chalk.blue(
+          pr.title
+        )} should be milestoned ${chalk.magenta(milestone)}`,
+        `  ${
+          hasCorrectMilestone ? chalk.green('ok') : chalk.red('error')
+        }: it's currently milestoned ${chalk.magenta(pr.milestone)}`,
       ].join('\n')
     )
 
-    await octokit.graphql(milestonePullRequest, {
-      pullRequestId: pr.id,
-      milestoneId: milestones['v4.0.0'],
-    })
+    if (hasCorrectMilestone) {
+      console.log(`  ${chalk.green('done')}`)
+      return
+    }
 
-    console.log(chalk.green('done'))
-    console.log()
+    let answer = 'y'
+
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      if (prompt) {
+        answer = await question('  ok to fix? [Y/n/o(pen)] > ')
+      }
+
+      if (answer === 'o' || answer === 'open') {
+        await $`open https://github.com/redwoodjs/redwood/pull/${pr.number}`
+        continue
+      }
+
+      if (answer === 'y' || answer === 'Y' || answer === '') {
+        console.log(
+          `  ${chalk.blue('fixing')}: milestoning #${chalk.yellow(
+            pr.number
+          )} ${chalk.magenta(milestone)}`
+        )
+        await octokit.graphql(milestonePullRequest, {
+          pullRequestId: pr.id,
+          milestoneId: milestoneTitlesToIds[milestone],
+        })
+      }
+
+      console.log(`  ${chalk.green('done')}`)
+
+      break
+    }
+  }
+
+  const branch = await getReleaseBranch()
+  console.log()
+
+  for (const pr of prs) {
+    console.log(chalk.dim('-'.repeat(process.stdout.columns)))
+
+    if (await isCommitInBranch(branch, pr.mergeCommit.message)) {
+      await validateMilestone(pr, branch.split('/')[2])
+      continue
+    }
+
+    if (await isCommitInBranch('next', pr.mergeCommit.message)) {
+      await validateMilestone(pr, 'next-release')
+      continue
+    }
+
+    await validateMilestone(pr, 'v4.0.0')
   }
 }
 
-const getNextReleasePRs = `
-  query GetNextReleasePRs {
-    node(id: "MI_kwDOC2M2f84Aa82f") {
-      ... on Milestone {
-        pullRequests(first: 100) {
-          nodes {
-            id
-            number
-            title
-            mergeCommit {
-              messageHeadline
-            }
-          }
-        }
-      }
-    }
-  }
-`
-
-const getMilestoneIds = `
-  query GetMilestoneIds {
+const getPRs = `
+  query GetPRs {
     repository(owner: "redwoodjs", name: "redwood") {
       milestones(first: 10, states: OPEN) {
         nodes {
           id
           title
+
+          pullRequests(first: 100) {
+            pageInfo {
+              hasNextPage
+              endCursor
+            }
+
+            totalCount
+
+            nodes {
+              id
+              number
+              title
+              mergeCommit {
+                message
+              }
+            }
+          }
         }
       }
     }
