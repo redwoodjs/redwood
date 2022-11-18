eca145df1df4868420a88d28910b95501914b159
Daniel Choudhury <dannychoudhury@gmail.com>
2022-11-18 14:21:35 +0700

    Rebuild fixture

--- __fixtures__/test-project/api/src/services/users/users.scenarios.ts
+++ __fixtures__/test-project/api/src/services/users/users.scenarios.ts
@@ -6,7 +6,7 @@ export const standard = defineScenario<Prisma.UserCreateArgs>({
   user: {
     one: {
       data: {
-        email: 'String1171894',
+        email: 'String3034944',
         hashedPassword: 'String',
         fullName: 'String',
         salt: 'String',
@@ -14,7 +14,7 @@ export const standard = defineScenario<Prisma.UserCreateArgs>({
     },
     two: {
       data: {
-        email: 'String1703130',
+        email: 'String5417616',
         hashedPassword: 'String',
         fullName: 'String',
         salt: 'String',
