28385157e7bd36ff4f070f03e6cd0a162e2631e8
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2022-11-21 23:11:25 +0000

    Fix: Add missing `jsonDisplay` function to formatters.tsx.template (#6902)
    * Added jsonDisplay to formatters
    
    * Update test snapshots
    
    * Added jsonDisplay test to formatters.test.tsx.template
    
    * Rebuilt test-project fixture
    
    * Fix missing import in formatters test template
    
    * Update test snapshot for jsonDisplay test
    
    * update fixtures to include import fix
    
    Co-authored-by: Tobbe Lundberg <tobbe@tlundberg.com>

--- __fixtures__/test-project/api/src/services/posts/posts.scenarios.ts
+++ __fixtures__/test-project/api/src/services/posts/posts.scenarios.ts
@@ -10,7 +10,7 @@ export const standard = defineScenario<Prisma.PostCreateArgs>({
         body: 'String',
         author: {
           create: {
-            email: 'String9043010',
+            email: 'String559996',
             hashedPassword: 'String',
             fullName: 'String',
             salt: 'String',
@@ -24,7 +24,7 @@ export const standard = defineScenario<Prisma.PostCreateArgs>({
         body: 'String',
         author: {
           create: {
-            email: 'String3280784',
+            email: 'String4687427',
             hashedPassword: 'String',
             fullName: 'String',
             salt: 'String',
