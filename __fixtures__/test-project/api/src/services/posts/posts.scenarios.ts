0960a8cc6150fffb975ffb8a90bbdd3f8b86f34e
Josh-Walker-GM <56300765+Josh-Walker-GM@users.noreply.github.com>
2022-11-24 16:46:38 +0000

    Rebuilt the test fixture to check

--- __fixtures__/test-project/api/src/services/posts/posts.scenarios.ts
+++ __fixtures__/test-project/api/src/services/posts/posts.scenarios.ts
@@ -10,7 +10,7 @@ export const standard = defineScenario<Prisma.PostCreateArgs>({
         body: 'String',
         author: {
           create: {
-            email: 'String559996',
+            email: 'String1937405',
             hashedPassword: 'String',
             fullName: 'String',
             salt: 'String',
@@ -24,7 +24,7 @@ export const standard = defineScenario<Prisma.PostCreateArgs>({
         body: 'String',
         author: {
           create: {
-            email: 'String4687427',
+            email: 'String7987179',
             hashedPassword: 'String',
             fullName: 'String',
             salt: 'String',
--- __fixtures__/test-project/api/src/services/users/users.scenarios.ts
+++ __fixtures__/test-project/api/src/services/users/users.scenarios.ts
@@ -6,7 +6,7 @@ export const standard = defineScenario<Prisma.UserCreateArgs>({
   user: {
     one: {
       data: {
-        email: 'String8459362',
+        email: 'String1259578',
         hashedPassword: 'String',
         fullName: 'String',
         salt: 'String',
@@ -14,7 +14,7 @@ export const standard = defineScenario<Prisma.UserCreateArgs>({
     },
     two: {
       data: {
-        email: 'String3453414',
+        email: 'String4140619',
         hashedPassword: 'String',
         fullName: 'String',
         salt: 'String',
