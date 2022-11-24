685c0291a3c8f5c82f4ea2e9350cb6457320abcb
Tobbe Lundberg <tobbe@tlundberg.com>
2022-11-24 18:21:23 +0100

    Remove unneeded jest resolvers

--- packages/auth-providers/netlify/web/jest.config.js
+++ packages/auth-providers/netlify/web/jest.config.js
@@ -1,8 +1,5 @@
-const path = require('path')
-
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   testPathIgnorePatterns: ['fixtures', 'dist'],
-  resolver: path.resolve(__dirname, './resolver.js'),
 }
