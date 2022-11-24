540c69ed69bc5f0904ec8609ba89393b3f0e66e5
Tobbe Lundberg <tobbe@tlundberg.com>
2022-11-24 20:04:14 +0100

    pin auth0 js sdk to v1.22.5

--- packages/auth-providers/auth0/setup/src/setup.ts
+++ packages/auth-providers/auth0/setup/src/setup.ts
@@ -25,7 +25,10 @@ export const handler = async ({ rwVersion, force: forceArg }: Args) => {
     authDecoderImport:
       "import { auth0AuthDecoder as authDecoder } from '@redwoodjs/auth-providers-api'",
     apiPackages: ['@redwoodjs/auth-providers-api'],
-    webPackages: ['@auth0/auth0-spa-js', '@redwoodjs/auth-providers-web'],
+    webPackages: [
+      '@auth0/auth0-spa-js@1.22.5',
+      '@redwoodjs/auth-providers-web',
+    ],
     notes: [
       'You will need to create several environment variables with your Auth0 config options.',
       'Check out web/src/App.{js,tsx} for the variables you need to add.',
