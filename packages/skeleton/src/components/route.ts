ece707792d883f0968423809548721291d4b6347
Josh-Walker-GM <56300765+Josh-Walker-GM@users.noreply.github.com>
2022-12-06 21:05:31 +0000

    refactor the route attribute extraction

--- packages/skeleton/src/components/route.ts
+++ packages/skeleton/src/components/route.ts
@@ -3,16 +3,9 @@ import fs from 'fs'
 import type { NodePath } from '@babel/core'
 import traverse from '@babel/traverse'
 import type { JSXElement, JSXAttribute } from '@babel/types'
-import {
-  isJSXIdentifier,
-  isJSXAttribute,
-  isStringLiteral,
-  isJSXExpressionContainer,
-  isIdentifier,
-  isJSXElement,
-} from '@babel/types'
+import { isJSXIdentifier, isJSXAttribute, isJSXElement } from '@babel/types'
 
-import { getASTFromCode } from '../lib/ast'
+import { getASTFromCode, getJSXElementAttributes } from '../lib/ast'
 
 import { RedwoodSkeleton } from './base'
 import { RedwoodPage } from './page'
@@ -25,98 +18,30 @@ export class RedwoodRoute extends RedwoodSkeleton {
   errors: string[] = []
 
   readonly path: string | undefined
-  private readonly pageName: string | undefined
+  private readonly pageIdentifier: string | undefined
   readonly prerender: boolean
   readonly isNotFound: boolean
   readonly hasParameters: boolean
 
   readonly isPrivate: boolean
 
-  // TODO: Fix this overly repetitive code...
   constructor(filepath: string, routeJSXElementNodePath: NodePath<JSXElement>) {
-    // name property, must be pre-super because we want to pass name to the superclass
-    let name
-    const nameErrors: string[] = []
-    const nameAttribute =
-      routeJSXElementNodePath.node.openingElement.attributes.find(
-        (node): node is JSXAttribute => {
-          return isJSXAttribute(node) && node.name.name === 'name'
-        }
-      )
-    if (nameAttribute) {
-      // TODO: Must it be a string value?
-      if (nameAttribute != null && isStringLiteral(nameAttribute.value)) {
-        name = nameAttribute.value.value
-      } else {
-        nameErrors.push('The name property must have a string value')
-      }
-    }
+    const routeAttributes = getJSXElementAttributes(
+      routeJSXElementNodePath.node
+    )
 
-    // notfound property, must be pre-super because we want to set the name to notfound
-    const notfoundAttribute =
-      routeJSXElementNodePath.node.openingElement.attributes.find(
-        (node): node is JSXAttribute => {
-          return isJSXAttribute(node) && node.name.name === 'notfound'
-        }
-      )
-    if (notfoundAttribute !== undefined) {
-      name = 'notfound'
-    }
+    super(
+      filepath,
+      routeAttributes.has('notfound') ? 'notfound' : routeAttributes.get('name') // name
+    )
 
-    super(filepath, name)
-    this.errors.push(...nameErrors)
+    this.path = routeAttributes.get('path')
+    this.pageIdentifier = routeAttributes.get('page')
+    this.isNotFound = routeAttributes.has('notfound')
+    this.prerender = routeAttributes.has('prerender')
+    this.hasParameters = this.path?.match(/(.*\{.+\}.*)+/) != null // TODO: Should use a proper route parameter check here
 
-    // path property
-    const pathAttribute =
-      routeJSXElementNodePath.node.openingElement.attributes.find(
-        (node): node is JSXAttribute => {
-          return isJSXAttribute(node) && node.name.name === 'path'
-        }
-      )
-    if (pathAttribute) {
-      // TODO: Must it be a string value?
-      if (pathAttribute != null && isStringLiteral(pathAttribute.value)) {
-        this.path = pathAttribute.value.value
-      } else {
-        this.errors.push('The path property must have a string value')
-      }
-    }
-
-    // page property
-    const pageAttribute =
-      routeJSXElementNodePath.node.openingElement.attributes.find(
-        (node): node is JSXAttribute => {
-          return isJSXAttribute(node) && node.name.name === 'page'
-        }
-      )
-    if (pageAttribute) {
-      // TODO: What other ways can a Page be passed into the route?
-      if (
-        pageAttribute != null &&
-        isJSXExpressionContainer(pageAttribute.value) &&
-        isIdentifier(pageAttribute.value.expression)
-      ) {
-        this.pageName = pageAttribute.value.expression.name
-      } else {
-        this.errors.push('The page property could not be processed')
-      }
-    }
-
-    // notfound property
-    this.isNotFound = notfoundAttribute !== undefined
-
-    // prerender property
-    const prerenderAttribute =
-      routeJSXElementNodePath.node.openingElement.attributes.find(
-        (node): node is JSXAttribute => {
-          return isJSXAttribute(node) && node.name.name === 'prerender'
-        }
-      )
-    this.prerender = prerenderAttribute !== undefined
-
-    // TODO: Improve this detection
-    this.hasParameters = this.path?.match(/(.*\{.+\}.*)+/) != null
-
+    // TODO: Extract layouts from wrapping elements
     let parent = routeJSXElementNodePath.parentPath
     while (parent.parentPath != null) {
       if (
@@ -155,6 +80,7 @@ export class RedwoodRoute extends RedwoodSkeleton {
         this.errors.push('The notfound page cannot have a path property')
       }
     }
+    // TODO: (Check) Routes should contain a name if they aren't the notfound page?
   }
 
   getPage(): RedwoodPage | undefined {
@@ -163,7 +89,7 @@ export class RedwoodRoute extends RedwoodSkeleton {
     })
       .getPages()
       .find((page) => {
-        return page.name === this.pageName
+        return page.name === this.pageIdentifier
       })
   }
 
--- packages/skeleton/src/lib/ast.ts
+++ packages/skeleton/src/lib/ast.ts
@@ -1,6 +1,13 @@
 import fs from 'fs'
 
 import { parse } from '@babel/parser'
+import { JSXElement, assertIdentifier } from '@babel/types'
+import {
+  assertJSXOpeningElement,
+  assertJSXIdentifier,
+  isJSXAttribute,
+  isJSXSpreadAttribute,
+} from '@babel/types'
 
 export function getASTFromCode(code: string) {
   return parse(code, {
@@ -13,6 +20,49 @@ export function getASTFromCode(code: string) {
       'objectRestSpread',
     ],
   })
+}
+
+export function getJSXElementAttributes(
+  element: JSXElement
+): Map<string, string> {
+  const attributes: Map<string, string> = new Map()
+  assertJSXOpeningElement(element.openingElement)
+  if (element.openingElement.attributes.length > 0) {
+    element.openingElement.attributes.forEach((attribute) => {
+      if (isJSXAttribute(attribute)) {
+        assertJSXIdentifier(attribute.name)
+        const key = attribute.name.name
+        let value: string
+        switch (attribute.value?.type) {
+          case 'StringLiteral':
+            value = attribute.value.value
+            break
+          case 'JSXExpressionContainer':
+            assertIdentifier(attribute.value.expression)
+            value = attribute.value.expression.name
+            break
+          case undefined: // Accept this because it could be a attribute with no value, i.e. notfound
+            value = 'true'
+            break
+          default:
+            throw new Error(
+              `Uknown value type found while trying to process the ${key} property when extracting all attributes`
+            )
+            break
+        }
+        attributes.set(key, value)
+      } else if (isJSXSpreadAttribute(attribute)) {
+        throw new Error(
+          'JSXSpreadAttribute type is not yet supported when extracting all attributes'
+        )
+      } else {
+        throw new Error(
+          'Uknown attribute type found while trying to extract all attributes'
+        )
+      }
+    })
+  }
+  return attributes
 }
 
 export function getASTFromFile(filePath: string) {
