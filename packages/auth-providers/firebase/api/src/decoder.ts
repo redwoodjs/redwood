e0a8364d096a9859ff9d566359064b7415684817
Tobbe Lundberg <tobbe@tlundberg.com>
2022-12-13 00:31:36 +0100

    revert changes to the decoder

--- packages/auth-providers/firebase/api/src/decoder.ts
+++ packages/auth-providers/firebase/api/src/decoder.ts
@@ -1,17 +1,13 @@
-import type FirebaseAdmin from 'firebase-admin'
+import admin from 'firebase-admin'
 
 import { Decoder } from '@redwoodjs/api'
 
-export const authDecoder = (admin: typeof FirebaseAdmin) => {
-  const authDecoderFn: Decoder = async (token: string, type: string) => {
-    if (type !== 'firebase') {
-      return null
-    }
-
-    return admin.auth().verifyIdToken(token)
-    // Alternative third-party JWT verification process described here:
-    // https://firebase.google.com/docs/auth/admin/verify-id-tokens#verify_id_tokens_using_a_third-party_jwt_library
+export const authDecoder: Decoder = async (token: string, type: string) => {
+  if (type !== 'firebase') {
+    return null
   }
 
-  return authDecoderFn
+  return admin.auth().verifyIdToken(token)
+  // Alternative third-party JWT verification process described here:
+  // https://firebase.google.com/docs/auth/admin/verify-id-tokens#verify_id_tokens_using_a_third-party_jwt_library
 }
