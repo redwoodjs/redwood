1c157feec9a1b665a8cf1c6d7599ad6bf79ea1ca
Josh-Walker-GM <56300765+Josh-Walker-GM@users.noreply.github.com>
2022-12-16 00:36:33 +0000

    Add success message to check command output

--- packages/cli/src/commands/check.js
+++ packages/cli/src/commands/check.js
@@ -1,5 +1,4 @@
-// import { getPaths } from '../lib'
-// import c from '../lib/colors'
+import chalk from 'chalk'
 
 export const command = 'check'
 export const aliases = ['diagnostics']
@@ -7,15 +6,29 @@ export const description =
   'Get structural diagnostics for a Redwood project (experimental)'
 
 export const handler = async () => {
-  // Generate a full project and list all errors and warnings
+  // Generate a full project and list any errors and warnings
   const { RedwoodProject } = await import('@redwoodjs/skeleton')
   const project = RedwoodProject.getProject({
     full: true,
     readFromCache: false, // Don't read from the cache so we know the errors/warnings aren't stale
   })
-  project.printErrors(true)
-  project.printWarnings(true)
-  if (project.hasErrors(true)) {
+  const hasErrors = project.hasErrors(true)
+  if (hasErrors) {
+    project.printErrors(true)
+  }
+  const hasWarnings = project.hasWarnings(true)
+  if (hasWarnings) {
+    project.printWarnings(true)
+  }
+
+  if (!(hasErrors || hasWarnings)) {
+    console.log(
+      chalk.bgGreen('[Success]'),
+      'There were no warnings or errors detected'
+    )
+  }
+
+  if (hasErrors) {
     process.exit(1)
   }
 }
