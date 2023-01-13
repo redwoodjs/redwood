2a0eca1a2623e585a7c9bdf9cac7c4b8be2b8ad3
Tobbe Lundberg <tobbe@tlundberg.com>
2023-01-13 08:30:59 +0100

    Test prerendering meta tags (#7334)
    Co-authored-by: Daniel Choudhury <dannychoudhury@gmail.com>

--- __fixtures__/test-project/api/src/services/posts/posts.scenarios.ts
+++ __fixtures__/test-project/api/src/services/posts/posts.scenarios.ts
@@ -10,7 +10,7 @@ export const standard = defineScenario<Prisma.PostCreateArgs>({
         body: 'String',
         author: {
           create: {
-            email: 'String14978',
+            email: 'String626106',
             hashedPassword: 'String',
             fullName: 'String',
             salt: 'String',
@@ -24,7 +24,7 @@ export const standard = defineScenario<Prisma.PostCreateArgs>({
         body: 'String',
         author: {
           create: {
-            email: 'String9717259',
+            email: 'String4595960',
             hashedPassword: 'String',
             fullName: 'String',
             salt: 'String',
