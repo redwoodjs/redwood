295fd75cc6dd03a1d33d21a27d0230a8ff238ecd
Peter Colapietro <petercolapietro+spacehippie@gmail.com>
2023-01-17 15:09:31 -0500

    chore(test@ci): fix for failing smoke test
    ```
    ...
    src/pages/ProfilePage/ProfilePage.tsx(31,21): error TS2322: Type 'unknown' is not assignable to type 'ReactNode'.
    ...
    ```
    
    https://github.com/redwoodjs/redwood/actions/runs/3942582022/jobs/6746415523#step:24:82
    
    see: https://github.com/redwoodjs/redwood/pull/4992#issuecomment-1385955177

--- tasks/test-project/codemods/profilePage.js
+++ tasks/test-project/codemods/profilePage.js
@@ -22,7 +22,7 @@ return (
           return (
             <tr key={key}>
               <td>{key.toUpperCase()}</td>
-              <td>{currentUser[key]}</td>
+              <td>{currentUser[key] as ReactNode}</td>
             </tr>
           )
         })}
