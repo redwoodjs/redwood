eb2b72f50c051a4cef388e6d13b6672ad7674781
Peter Colapietro <petercolapietro+spacehippie@gmail.com>
2023-01-17 17:37:37 -0500

    chore(test@ci): fix for failing smoke test
    ```
    ...
    src/pages/ProfilePage/ProfilePage.tsx(31,21): error TS2322: Type 'unknown' is not assignable to type 'ReactNode'.
    ...
    ```
    
    https://github.com/redwoodjs/redwood/actions/runs/3942582022/jobs/6746415523#step:24:82
    
    see: https://github.com/redwoodjs/redwood/pull/4992#issuecomment-1385955177

--- __fixtures__/test-project/web/src/pages/ProfilePage/ProfilePage.tsx
+++ __fixtures__/test-project/web/src/pages/ProfilePage/ProfilePage.tsx
@@ -1,5 +1,6 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+import { ReactNode } from 'react'
 
 import { useAuth } from 'src/auth'
 
@@ -28,7 +29,7 @@ const ProfilePage = () => {
             return (
               <tr key={key}>
                 <td>{key.toUpperCase()}</td>
-                <td>{currentUser[key]}</td>
+                <td>{currentUser[key] as ReactNode}</td>
               </tr>
             )
           })}
