60e53aa21dd84877ea7263afd1f5c3e8f168260d
Josh-Walker-GM <56300765+Josh-Walker-GM@users.noreply.github.com>
2023-01-22 17:26:04 +0000

    Initial experiment with OTel

--- packages/cli/src/commands/info.js
+++ packages/cli/src/commands/info.js
@@ -1,7 +1,10 @@
 // inspired by gatsby/packages/gatsby-cli/src/create-cli.js and
 // and gridsome/packages/cli/lib/commands/info.js
+import opentelemetry from '@opentelemetry/api'
 import envinfo from 'envinfo'
 import terminalLink from 'terminal-link'
+
+import { tracerName } from '../telemetry/const'
 
 export const command = 'info'
 export const description = 'Print your system environment information'
@@ -14,6 +17,13 @@ export const builder = (yargs) => {
   )
 }
 export const handler = async () => {
+  const tracer = opentelemetry.trace.getTracer(tracerName)
+  const handlerSpan = tracer.startSpan(
+    'handler',
+    undefined,
+    opentelemetry.context.active()
+  )
+  handlerSpan.setAttribute('command', 'info')
   try {
     const output = await envinfo.run({
       System: ['OS', 'Shell'],
@@ -24,9 +34,12 @@ export const handler = async () => {
       Databases: ['SQLite'],
     })
     console.log(output)
+    handlerSpan.end()
   } catch (e) {
     console.log('Error: Cannot access environment info')
     console.log(e)
+    handlerSpan.recordException(e)
+    handlerSpan.end()
     process.exit(1)
   }
 }
