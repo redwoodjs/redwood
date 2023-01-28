dd8c56257a728f26389a88d12616e61ded9366c4
Tobbe Lundberg <tobbe@tlundberg.com>
2023-01-28 12:47:34 +0100

    SuperTokens setup: Add to Routes.tsx

---
+++ packages/auth-providers/supertokens/setup/src/__tests__/setupHandler.test.ts
@@ -0,0 +1,62 @@
+// mock Telemetry for CLI commands so they don't try to spawn a process
+jest.mock('@redwoodjs/telemetry', () => {
+  return {
+    errorTelemetry: () => jest.fn(),
+    timedTelemetry: () => jest.fn(),
+  }
+})
+
+jest.mock('@redwoodjs/cli-helpers', () => {
+  return {
+    getPaths: () => {
+      return {
+        web: {
+          src: '',
+          app: '',
+          routes: 'Routes.tsx',
+        },
+        base: '',
+      }
+    },
+    standardAuthHandler: () => jest.fn(),
+  }
+})
+
+// This will load packages/auth-providers/__mocks__/fs.js
+jest.mock('fs')
+
+const mockFS = fs as unknown as Omit<jest.Mocked<typeof fs>, 'readdirSync'> & {
+  __setMockFiles: (files: Record<string, string>) => void
+  __getMockFiles: () => Record<string, string>
+  readdirSync: () => string[]
+}
+
+import fs from 'fs'
+
+import { extraTask } from '../setupHandler'
+
+test('extraTask', () => {
+  mockFS.__setMockFiles({
+    'Routes.tsx':
+      "// In this file, all Page components from 'src/pages` are auto-imported.\n" +
+      `
+import { Router, Route } from '@redwoodjs/router'
+
+const Routes = () => {
+  return (
+    <Router useAuth={useAuth}>
+      <Route path="/login" page={LoginPage} name="login" />
+      <Route path="/signup" page={SignupPage} name="signup" />
+      <Route notfound page={NotFoundPage} />
+    </Router>
+  )
+}
+
+export default Routes
+`,
+  })
+
+  extraTask.task()
+
+  expect(mockFS.readFileSync('Routes.tsx')).toMatchSnapshot()
+})
--- packages/auth-providers/supertokens/setup/src/setupHandler.ts
+++ packages/auth-providers/supertokens/setup/src/setupHandler.ts
@@ -1,15 +1,38 @@
 import fs from 'fs'
 import path from 'path'
 
-import { standardAuthHandler } from '@redwoodjs/cli-helpers'
+import { getPaths, standardAuthHandler } from '@redwoodjs/cli-helpers'
 
 import { Args } from './setup'
 
-const { version } = JSON.parse(
-  fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf-8')
-)
+// exported for testing
+export const extraTask = {
+  title: `Adding SuperTokens routing component to Routes.{js,tsx}...`,
+  task: () => {
+    const webRoutesPath = getPaths().web.routes
+
+    let content = fs.readFileSync(webRoutesPath).toString()
+
+    if (!/^\sif \(SuperTokens.canHandleRoute\(\)\) \{/.test(content)) {
+      content = "import SuperTokens from 'supertokens-auth-react'\n\n" + content
+      content = content.replace(
+        /const Routes = \(\) => \{\n/,
+        'const Routes = () => {\n' +
+          '  if (SuperTokens.canHandleRoute()) {\n' +
+          '    return SuperTokens.getRoutingComponent()\n' +
+          '  }\n\n'
+      )
+
+      fs.writeFileSync(webRoutesPath, content)
+    }
+  },
+}
 
 export async function handler({ force: forceArg }: Args) {
+  const { version } = JSON.parse(
+    fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf-8')
+  )
+
   standardAuthHandler({
     basedir: __dirname,
     forceArg,
@@ -25,6 +48,7 @@ export async function handler({ force: forceArg }: Args) {
       'supertokens-auth-react@^0.30',
       'supertokens-web-js@^0.4',
     ],
+    extraTask,
     notes: [
       "We've implemented some of SuperToken's recipes, but feel free",
       'to switch to something that better fits your needs. See https://supertokens.com/docs/guides.',
