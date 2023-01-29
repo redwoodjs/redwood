c87b6b14eafbeca19a1be491e2e64d314285a1b8
Tobbe Lundberg <tobbe@tlundberg.com>
2023-01-29 20:04:12 +0100

    SuperTokens setup: Add to Routes.tsx (#7483)
    * SuperTokens setup: Add to Routes.tsx
    
    * Self-review fixes
    
    * Correct path in comment
    
    * Better placement of SuperTokens import
    
    * Remove unused functions from mockfs type
    
    * Add test for already setup and fix bug

--- packages/auth-providers/supertokens/setup/src/setupHandler.ts
+++ packages/auth-providers/supertokens/setup/src/setupHandler.ts
@@ -1,15 +1,59 @@
 import fs from 'fs'
 import path from 'path'
 
-import { standardAuthHandler } from '@redwoodjs/cli-helpers'
+import { getPaths, standardAuthHandler } from '@redwoodjs/cli-helpers'
 
 import { Args } from './setup'
 
-const { version } = JSON.parse(
-  fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf-8')
-)
+// exported for testing
+export const extraTask = {
+  title: `Adding SuperTokens routing component to Routes.{js,tsx}...`,
+  task: () => {
+    const webRoutesPath = getPaths().web.routes
+
+    let content = fs.readFileSync(webRoutesPath).toString()
+
+    if (!/\n\s*if \(SuperTokens.canHandleRoute\(\)\) \{/.test(content)) {
+      let hasImportedSuperTokens = false
+
+      content = content
+        .split('\n')
+        .reduce<string[]>((acc, line) => {
+          // Add the SuperTokens import before the first import from @redwoodjs
+          if (
+            !hasImportedSuperTokens &&
+            line.includes('import') &&
+            line.includes('@redwoodjs')
+          ) {
+            acc.push("import SuperTokens from 'supertokens-auth-react'")
+            acc.push('')
+
+            hasImportedSuperTokens = true
+          }
+
+          acc.push(line)
+
+          return acc
+        }, [])
+        .join('\n')
+        .replace(
+          /const Routes = \(\) => \{\n/,
+          'const Routes = () => {\n' +
+            '  if (SuperTokens.canHandleRoute()) {\n' +
+            '    return SuperTokens.getRoutingComponent()\n' +
+            '  }\n\n'
+        )
+
+      fs.writeFileSync(webRoutesPath, content)
+    }
+  },
+}
 
 export async function handler({ force: forceArg }: Args) {
+  const { version } = JSON.parse(
+    fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf-8')
+  )
+
   standardAuthHandler({
     basedir: __dirname,
     forceArg,
@@ -25,6 +69,7 @@ export async function handler({ force: forceArg }: Args) {
       'supertokens-auth-react@^0.30',
       'supertokens-web-js@^0.4',
     ],
+    extraTask,
     notes: [
       "We've implemented some of SuperToken's recipes, but feel free",
       'to switch to something that better fits your needs. See https://supertokens.com/docs/guides.',
