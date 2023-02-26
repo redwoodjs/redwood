cf2fe47c9077bcef40e0abe44a9882b28fe91213
Zachary Zaro <zach@withcoherence.com>
2023-02-26 02:14:12 +0000

    second pass, much better tested. should almost work, except for some cases of replacing the port in the toml

--- packages/cli/src/commands/setup/deploy/providers/coherence.js
+++ packages/cli/src/commands/setup/deploy/providers/coherence.js
@@ -9,8 +9,9 @@ import { errorTelemetry } from '@redwoodjs/telemetry'
 
 import { getPaths, writeFilesTask } from '../../../../lib'
 import c from '../../../../lib/colors'
-import { printSetupNotes } from '../helpers'
+import { printSetupNotes, addFilesTask } from '../helpers'
 import {
+  COHERENCE_HEALTH_CHECK,
   DATABASE_YAML,
   COHERENCE_YAML,
 } from '../templates/coherence'
@@ -25,17 +26,18 @@ export const getCoherenceYamlContent = async () => {
       content: COHERENCE_YAML(''),
     }
   } else {
-    const schema = await getSchema('api/db/schema.prisma')
-    const config = await getConfig({ datamodel: schema })
-    const detectedDatabase = config.datasources[0].activeProvider
+    const schema = await getSchema('api/db/schema.prisma');
+    const config = await getConfig({ datamodel: schema });
+    var detectedDatabase:string = config.datasources[0].activeProvider;
 
-    if (detectedDatabase === 'postgres') {
+    if ((detectedDatabase === 'mysql') ||  (detectedDatabase === 'postgresql')) {
+      if (detectedDatabase === 'postgresql') {detectedDatabase = 'postgres'}
       return {
         path: path.join(getPaths().base, 'coherence.yml'),
-        content: COHERENCE_YAML(DATABASE_YAML()),
+        content: COHERENCE_YAML(DATABASE_YAML(detectedDatabase)),
       }
     } else {
-      printSetupNotes('Only postgres supported at this time...')
+      printSetupNotes('Only mysql & postgresql prisma DBs are supported on Coherence at this time...')
       return {
         path: path.join(getPaths().base, 'coherence.yml'),
         content: COHERENCE_YAML(''),
@@ -47,9 +49,35 @@ export const getCoherenceYamlContent = async () => {
 // any notes to print out when the job is done
 const notes = [
   'You are ready to deploy to Coherence!\n',
-  'Go to https://app.withcoherence.com to create your account and deploy to Render',
-  'Check out the deployment docs at https://docs.withcoherence.com for detailed instructions',
+  'Go to https://app.withcoherence.com to create your account and setup your cloud/github connections.',
+  'Check out the deployment docs at https://docs.withcoherence.com for detailed instructions and more information.\n',
+  'Reach out to redwood@withcoherence.com with any questions! We are here to support you...'
 ]
+
+
+const additionalFiles = [
+  {
+    path: path.join(getPaths().base, 'api/src/functions/health.js'),
+    content: COHERENCE_HEALTH_CHECK,
+  },
+]
+
+// updates the PORTs to use an environment variable.
+const updateRedwoodTomlTask = () => {
+  return {
+    title: 'Updating redwood.toml PORTs...',
+    task: () => {
+      const configPath = path.join(getPaths().base, 'redwood.toml')
+      const content = fs.readFileSync(configPath).toString()
+
+      const newContent = content.replace(
+        /port.*?\n/m,
+        'port = "${PORT}"'
+      )
+      fs.writeFileSync(configPath, newContent)
+    },
+  }
+}
 
 export const handler = async ({ force, database }) => {
   const tasks = new Listr(
@@ -63,6 +91,12 @@ export const handler = async ({ force, database }) => {
           return writeFilesTask(files, { overwriteExisting: force })
         },
       },
+      addFilesTask({
+        title: "Adding helath check function...",
+        files: additionalFiles,
+        force,
+      }),
+      updateRedwoodTomlTask(),
       printSetupNotes(notes),
     ],
     { rendererOptions: { collapse: false } }
--- packages/cli/src/commands/setup/deploy/templates/coherence.js
+++ packages/cli/src/commands/setup/deploy/templates/coherence.js
@@ -10,15 +10,15 @@ api:
   type: backend
   url_path: "/api"
   prod:
-    command: ["yarn", "rw", "serve", "api"]
+    command: ["yarn", "rw", "build", "api", "&&", "yarn", "rw", "serve", "api"]
   dev:
-    command: ["yarn", "rw", "dev", "api"]
+    command: ["yarn", "rw", "build", "api", "&&", "yarn", "rw", "dev", "api"]
   local_packages: ["node_modules"]
 
   system:
     cpu: 2
     memory: 2G
-    health_check: "/graphql/health"
+    health_check: "/health"
 
   ${database}
 
@@ -28,7 +28,7 @@ web:
   prod:
     command: ["yarn", "rw", "serve", "web"]
   dev:
-    command: ["yarn", "rw", "dev", "web", "--fwd=\"--allowed-hosts all\""]
+    command: ["yarn", "rw", "dev", "web", "--fwd=\\"--allowed-hosts all\\""]
   build: ["yarn", "rw", "build", "web"]
   local_packages: ["node_modules"]
 
@@ -38,12 +38,12 @@ web:
 `
 }
 
-export const DATABASE_YAML = (PROJECT_NAME) => `
-resources:
-- name: ${PROJECT_NAME}-db
-  engine: postgres
-  version: 13
-  type: database
+export const DATABASE_YAML = (detectedDatabase) => `
+  resources:
+  - name: ${PROJECT_NAME}-db
+    engine: ${detectedDatabase}
+    version: 13
+    type: database
 `
 
 export const COHERENCE_HEALTH_CHECK = `// coherence-health-check
