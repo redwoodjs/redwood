d1f57f657db6630918d3494fd8a15165501fcfb6
Josh-Walker-GM <56300765+Josh-Walker-GM@users.noreply.github.com>
2023-03-01 22:58:57 +0000

    Codemod for cell parameter adjustments

---
+++ packages/codemods/src/codemods/v5.x.x/cellQueryResult/cellQueryResult.yargs.ts
@@ -0,0 +1,24 @@
+import path from 'path'
+
+import task from 'tasuku'
+
+import { findCells } from 'src/lib/cells'
+
+import runTransform from '../../../lib/runTransform'
+
+export const command = 'cell-query-result'
+export const description =
+  '(v4.x.x->v5.x.x) Updates cells to use the queryResult property'
+
+export const handler = () => {
+  task('cellQueryResult', async ({ setOutput }) => {
+    await runTransform({
+      transformPath: path.join(__dirname, 'cellQueryResult.ts'),
+      targetPaths: findCells(),
+    })
+
+    setOutput(
+      'Updates to your cells are complete! Please run `yarn rw lint --fix` to prettify your code'
+    )
+  })
+}
