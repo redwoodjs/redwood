d62202eb7b87d3750c01737a4b031211b1d6915c
Dominic Saadi <dominiceliassaadi@gmail.com>
2023-03-01 14:35:25 -0800

    add upgrade step

--- packages/codemods/src/codemods/v5.x.x/updateAuth0/updateAuth0.yargs.ts
+++ packages/codemods/src/codemods/v5.x.x/updateAuth0/updateAuth0.yargs.ts
@@ -1,5 +1,6 @@
 import path from 'path'
 
+import execa from 'execa'
 import task, { TaskInnerAPI } from 'tasuku'
 
 import getRWPaths from '../../../lib/getRWPaths'
@@ -14,7 +15,15 @@ export const handler = () => {
   task('Update Auth0', async ({ setOutput }: TaskInnerAPI) => {
     const authFile = isTSProject ? 'auth.ts' : 'auth.js'
 
-    // TODO: upgrade web-side auth0 package
+    try {
+      await execa.command('yarn up @auth0/auth0-spa-js', {
+        cwd: getRWPaths().web.base,
+      })
+    } catch {
+      console.error(
+        "Couldn't update @auth0/auth0-spa-js; you'll have to upgrade it manually to the latest v2.x.x version"
+      )
+    }
 
     await runTransform({
       transformPath: path.join(__dirname, 'updateAuth0.js'),
