e40c832fb1d9127392e4d0db015530ef52790d72
aslaker <51129804+aslaker@users.noreply.github.com>
2023-03-23 17:13:17 -0500

    Add regex parsing for directUrl env variable in schema.prisma and set it to test database

--- packages/testing/config/jest/api/globalSetup.js
+++ packages/testing/config/jest/api/globalSetup.js
@@ -1,8 +1,12 @@
+const { readFileSync } = require('fs')
 const path = require('path')
 
 const { getPaths } = require('@redwoodjs/internal/dist/paths')
 
 const rwjsPaths = getPaths()
+
+const PRISMA_DIRECT_URL_REGEX = /directUrl(\s?)=(\s?)env\(('|")(.*)('|")\)/g
+const BETWEEN_PARENTHESES_REGEX = /\(('|")([^)]+)('|")\)/
 
 module.exports = async function () {
   if (process.env.SKIP_DB_PUSH !== '1') {
@@ -10,10 +14,10 @@ module.exports = async function () {
     // Load dotenvs
     require('dotenv-defaults/config')
 
-    const cacheDirDb = `file:${path.join(__dirname, '.redwood', 'test.db')}`
+    const cacheDirDb = getDefaultDb()
     process.env.DATABASE_URL = process.env.TEST_DATABASE_URL || cacheDirDb
-    process.env.DIRECT_URL =
-      process.env.TEST_DIRECT_URL || process.env.TEST_DATABASE_URL || cacheDirDb
+
+    checkAndReplaceDirectUrl()
 
     const command =
       process.env.TEST_DATABASE_STRATEGY === 'reset'
@@ -31,4 +35,24 @@ module.exports = async function () {
       },
     })
   }
+}
+
+function checkAndReplaceDirectUrl() {
+  const prismaSchema = readFileSync(path.join(rwjsPaths.api.dbSchema), 'utf-8')
+  const regex = new RegExp(PRISMA_DIRECT_URL_REGEX)
+  const cacheDirDb = getDefaultDb()
+
+  // Check Prisma Schema for the existence of a directUrl
+  const directUrl = prismaSchema.match(regex)
+
+  if (directUrl[0]) {
+    // If it exists, set the env var to the value of the directUrl env var
+    const directUrlEnv = directUrl[0].match(BETWEEN_PARENTHESES_REGEX)[2]
+    process.env[directUrlEnv] =
+      process.env.TEST_DIRECT_URL || process.env.TEST_DATABASE_URL || cacheDirDb
+  }
+}
+
+function getDefaultDb() {
+  return `file:${path.join(__dirname, '.redwood', 'test.db')}`
 }
