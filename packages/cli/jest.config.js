333cac871900472944ca323ffd5edddca16eb302
Daniel Choudhury <dannychoudhury@gmail.com>
2023-04-03 13:30:11 +0700

    Undo jest config change

--- packages/cli/jest.config.js
+++ packages/cli/jest.config.js
@@ -1,10 +1,9 @@
-/** @type {import('jest').Config} */
 module.exports = {
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/*.test.[jt]s?(x)'],
   testPathIgnorePatterns: ['fixtures', 'dist'],
-  setupFilesAfterEnv: ['./jest.setup.js'],
   moduleNameMapper: {
     '^src/(.*)': '<rootDir>/src/$1',
   },
-  testTimeout: 60000,
+  testTimeout: 15000,
+  setupFilesAfterEnv: ['./jest.setup.js'],
 }
