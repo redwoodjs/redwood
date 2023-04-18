7f619f97729e7e259b32553c8229e1797a8af72e
Josh-Walker-GM <56300765+Josh-Walker-GM@users.noreply.github.com>
2023-04-18 21:26:49 +0100

    Automatically install studio package if not found

--- packages/cli/src/commands/experimental/studio.js
+++ packages/cli/src/commands/experimental/studio.js
@@ -1,7 +1,11 @@
+import execa from 'execa'
 import terminalLink from 'terminal-link'
+
+import { getPaths } from '../../lib'
 
 export const command = 'studio'
 export const description = 'Run the Redwood development studio'
+
 export const builder = (yargs) => {
   yargs.epilogue(
     `Also see the ${terminalLink(
@@ -10,13 +14,31 @@ export const builder = (yargs) => {
     )}`
   )
 }
+
 export const handler = async () => {
   try {
+    // Import studio and start it
     const { start } = await import('@redwoodjs/studio')
     await start()
   } catch (e) {
-    console.log('Error: Cannot start the development studio')
-    console.log(e)
-    process.exit(1)
+    console.log('Cannot start the development studio')
+
+    // If we don't have the package installed, install it for the user
+    if (e.code === 'MODULE_NOT_FOUND') {
+      console.log(
+        'Installing the studio package for you, this may take a moment...'
+      )
+      await execa('yarn', ['add', '@redwoodjs/studio', '--dev'], {
+        shell: true,
+        cwd: getPaths().base,
+      })
+      console.log(
+        'Studio package installed successfully, please execute the command again.'
+      )
+    } else {
+      // Otherwise, log the error and exit
+      console.log(e)
+      process.exit(1)
+    }
   }
 }
