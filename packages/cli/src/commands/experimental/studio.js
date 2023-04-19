090ef337e43bbeef9bed5508c030f1940f4625b9
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2023-04-19 01:45:21 +0100

    fix(cli): Install studio package if not found (#8058)
    * Automatically install studio package if not found
    
    * Use existing package check/install code and rework the check in the cli command
    
    * fix typo and comment auth reference

--- packages/cli/src/commands/experimental/studio.js
+++ packages/cli/src/commands/experimental/studio.js
@@ -1,7 +1,10 @@
 import terminalLink from 'terminal-link'
 
+import { isModuleInstalled, installRedwoodModule } from '../../lib/packages'
+
 export const command = 'studio'
 export const description = 'Run the Redwood development studio'
+
 export const builder = (yargs) => {
   yargs.epilogue(
     `Also see the ${terminalLink(
@@ -10,12 +13,23 @@ export const builder = (yargs) => {
     )}`
   )
 }
+
 export const handler = async () => {
   try {
+    // Check the module is installed
+    if (!isModuleInstalled('@redwoodjs/studio')) {
+      console.log(
+        'The studio package is not installed, installing it for you, this may take a moment...'
+      )
+      await installRedwoodModule('@redwoodjs/studio')
+      console.log('Studio package installed successfully.')
+    }
+
+    // Import studio and start it
     const { start } = await import('@redwoodjs/studio')
     await start()
   } catch (e) {
-    console.log('Error: Cannot start the development studio')
+    console.log('Cannot start the development studio')
     console.log(e)
     process.exit(1)
   }
