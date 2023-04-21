bfeba749a7edf605b223d886bd59a21a5b9c5ce0
Dominic Saadi <dominiceliassaadi@gmail.com>
2023-04-21 13:26:11 -0400

    [CRWA, Tui] Try removing Babel and bundling minimally (#8074)
    * remove babel from crwa and tui w/ minimal bundling
    
    * chore: remove test script in crwa

---
+++ packages/tui/build.mjs
@@ -0,0 +1,19 @@
+import fs from 'node:fs'
+
+import * as esbuild from 'esbuild'
+
+// Since this is a library, there's no bundling going on here by design.
+// Instead we plan for this library to be bundled by leaf packages so-to-speak like create-redwood-app.
+const result = await esbuild.build({
+  entryPoints: ['src/index.ts'],
+  format: 'cjs',
+  platform: 'node',
+  target: ['node18'],
+  outfile: 'dist/index.js',
+
+  // For visualizing the bundle.
+  // See https://esbuild.github.io/api/#metafile and https://esbuild.github.io/analyze/.
+  metafile: true,
+})
+
+fs.writeFileSync('meta.json', JSON.stringify(result.metafile))
