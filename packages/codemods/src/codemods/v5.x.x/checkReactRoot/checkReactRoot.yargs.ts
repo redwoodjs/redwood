b6e8602e9b69b0548a414e32b9bbf42d906dc84a
Dominic Saadi <dominiceliassaadi@gmail.com>
2023-04-26 11:04:03 -0700

    do react

--- packages/codemods/src/codemods/v5.x.x/checkReactRoot/checkReactRoot.yargs.ts
+++ packages/codemods/src/codemods/v5.x.x/checkReactRoot/checkReactRoot.yargs.ts
@@ -1,13 +1,19 @@
 import task, { TaskInnerAPI } from 'tasuku'
 
-import { checkReactRoot } from './checkReactRoot'
+import { checkReactRoot, updateReactDeps } from './checkReactRoot'
 
 export const command = 'check-react-root'
+
 export const description = '(v5.x.x->v5.x.x) Converts world to bazinga'
 
 export const handler = () => {
-  task('Check React Root', ({ setOutput }: TaskInnerAPI) => {
+  task('Check react root', ({ setOutput }: TaskInnerAPI) => {
     checkReactRoot()
+    setOutput('All done!')
+  })
+
+  task('Update react deps', ({ setOutput }: TaskInnerAPI) => {
+    updateReactDeps()
     setOutput('All done!')
   })
 }
