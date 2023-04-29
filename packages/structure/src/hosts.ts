3df916aac8e7322d5bebbd0c0d5e561af9fa63c8
Tobbe Lundberg <tobbe@tlundberg.com>
2023-04-29 19:48:10 +0200

    Windows paths handling needed for fast-glob too

--- packages/structure/src/hosts.ts
+++ packages/structure/src/hosts.ts
@@ -32,7 +32,7 @@ export class DefaultHost implements Host {
   }
   globSync(pattern: string) {
     // globSync only works with / as the path separator, even on Windows
-    return fg.sync(pattern) //.replaceAll('\\', '/'))
+    return fg.sync(pattern.replaceAll('\\', '/'))
   }
   writeFileSync(path: string, contents: string) {
     return fs.writeFileSync(path, contents)
