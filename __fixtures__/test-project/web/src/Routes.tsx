c4660497a3a80eb3c1a620ea0ff0fa152582d43b
Tobbe Lundberg <tobbe@tlundberg.com>
2023-04-30 23:42:51 +0200

    Update test-project fixture and test-project generation task

--- __fixtures__/test-project/web/src/Routes.tsx
+++ __fixtures__/test-project/web/src/Routes.tsx
@@ -18,11 +18,11 @@ import { useAuth } from './auth'
 const Routes = () => {
   return (
     <Router useAuth={useAuth}>
+      <Route path="/double" page={DoublePage} name="double" prerender />
       <Route path="/login" page={LoginPage} name="login" />
       <Route path="/signup" page={SignupPage} name="signup" />
       <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
       <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
-      <Route path="/double" page={DoublePage} name="double" prerender />
       <Set wrap={ScaffoldLayout} title="Contacts" titleTo="contacts" buttonLabel="New Contact" buttonTo="newContact">
         <Route path="/contacts/new" page={ContactNewContactPage} name="newContact" prerender />
         <Route path="/contacts/{id:Int}/edit" page={ContactEditContactPage} name="editContact" />
