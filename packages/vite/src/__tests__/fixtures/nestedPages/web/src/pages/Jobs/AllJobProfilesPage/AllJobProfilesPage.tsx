70a632caf9442db91dc10619e66f97785d2ab787
Tobbe Lundberg <tobbe@tlundberg.com>
2023-04-30 08:33:00 +0200

    Move vite test to vite package

---
+++ packages/vite/src/__tests__/fixtures/nestedPages/web/src/pages/Jobs/AllJobProfilesPage/AllJobProfilesPage.tsx
@@ -0,0 +1,33 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+import JobProfilesCell from 'src/components/Jobs/JobProfilesCell'
+
+const AllJobProfilesPage = () => {
+  return (
+    <>
+      <MetaTags title="AllJobProfiles" description="AllJobProfiles page" />
+
+      <section className="max-w-screen-lg mx-auto mt-36 mb-24">
+        <header className="text-center">
+          <h1 className="mt-12" className="text-orange-800">
+            RedwoodJS Developers
+          </h1>
+          <p className="mt-2 text-stone-500">
+            Experienced RedwoodJS devs looking for their next role
+          </p>
+        </header>
+
+        <div className="flex justify-end mt-4">
+          <Link to={routes.newJobProfile()} className="button-sm">
+            + Create Profile
+          </Link>
+        </div>
+        <div className="border border-orange-200 rounded-lg mt-2">
+          <JobProfilesCell limit={5} />
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default AllJobProfilesPage
