d66989886850318ebafe0de254514824291ce5a5
Dominic Saadi <dominiceliassaadi@gmail.com>
2023-05-03 15:25:19 -0700

    chore(crwa): add template test

---
+++ packages/create-redwood-app/tests/template.test.js
@@ -0,0 +1,97 @@
+import path from 'path'
+
+import klawSync from 'klaw-sync'
+
+const TEMPLATE_DIR = path.join(__dirname, '../template')
+
+describe('template', () => {
+  it('files should not have changed unintentionally', () => {
+    expect(getDirectoryStructure(TEMPLATE_DIR)).toMatchInlineSnapshot(`
+      [
+        "/.editorconfig",
+        "/.env",
+        "/.env.defaults",
+        "/.env.example",
+        "/.nvmrc",
+        "/.vscode",
+        "/.vscode/extensions.json",
+        "/.vscode/launch.json",
+        "/.vscode/settings.json",
+        "/.yarn",
+        "/.yarn/releases",
+        "/.yarn/releases/yarn-3.5.1.cjs",
+        "/.yarnrc.yml",
+        "/README.md",
+        "/api",
+        "/api/db",
+        "/api/db/schema.prisma",
+        "/api/jest.config.js",
+        "/api/package.json",
+        "/api/server.config.js",
+        "/api/src",
+        "/api/src/directives",
+        "/api/src/directives/requireAuth",
+        "/api/src/directives/requireAuth/requireAuth.test.ts",
+        "/api/src/directives/requireAuth/requireAuth.ts",
+        "/api/src/directives/skipAuth",
+        "/api/src/directives/skipAuth/skipAuth.test.ts",
+        "/api/src/directives/skipAuth/skipAuth.ts",
+        "/api/src/functions",
+        "/api/src/functions/graphql.ts",
+        "/api/src/graphql",
+        "/api/src/graphql/.keep",
+        "/api/src/lib",
+        "/api/src/lib/auth.ts",
+        "/api/src/lib/db.ts",
+        "/api/src/lib/logger.ts",
+        "/api/src/services",
+        "/api/src/services/.keep",
+        "/api/tsconfig.json",
+        "/gitignore.template",
+        "/graphql.config.js",
+        "/jest.config.js",
+        "/package.json",
+        "/prettier.config.js",
+        "/redwood.toml",
+        "/scripts",
+        "/scripts/.keep",
+        "/scripts/seed.ts",
+        "/scripts/tsconfig.json",
+        "/web",
+        "/web/jest.config.js",
+        "/web/package.json",
+        "/web/public",
+        "/web/public/README.md",
+        "/web/public/favicon.png",
+        "/web/public/robots.txt",
+        "/web/src",
+        "/web/src/App.tsx",
+        "/web/src/Routes.tsx",
+        "/web/src/components",
+        "/web/src/components/.keep",
+        "/web/src/index.css",
+        "/web/src/index.html",
+        "/web/src/layouts",
+        "/web/src/layouts/.keep",
+        "/web/src/pages",
+        "/web/src/pages/FatalErrorPage",
+        "/web/src/pages/FatalErrorPage/FatalErrorPage.tsx",
+        "/web/src/pages/NotFoundPage",
+        "/web/src/pages/NotFoundPage/NotFoundPage.tsx",
+        "/web/tsconfig.json",
+      ]
+    `)
+  })
+})
+
+/**
+ * @param {string} dir
+ * @returns string[]
+ */
+function getDirectoryStructure(dir) {
+  let fileStructure = klawSync(dir)
+
+  return (fileStructure = fileStructure.map((fileStructure) =>
+    fileStructure.path.replace(dir, '').split(path.sep).join(path.posix.sep)
+  ))
+}
