91de4c4c64f589ae7d6bc3b8bd3bfce8f9408506
Dominic Saadi <dominiceliassaadi@gmail.com>
2023-05-05 12:01:18 -0700

    fix(crwa): decouple JS preference from `yarn install` step, remove `yarn1` option (#8191)
    * wip
    
    * fix: decouple js preference from yarn install
    
    * add tests
    
    * rm ts2js export
    
    * fix lint and lint fix
    
    * fix run e2e script
    
    * test project script updates
    
    * add back ts-to-js
    
    * add .nvmrc back
    
    * deprecate ts to js
    
    * add back ts2js in internal
    
    * use babel instead of esbuild to preserve comments
    
    * lint fix
    
    * add prettier step, .nvmrc file to js template
    
    * style
    
    * fix yarn check
    
    * update crwa snapshot
    
    * ignore js template routes file in linting
    
    * fix yarn versions
    
    * fix tsconfig to jsconfig transformation
    
    * handle script files
    
    * wip update release command
    
    * log section
    
    * comment delete jsConfig allowJS line
    
    * wip
    
    * add action to catch changes to crwa ts template
    
    * style

--- packages/create-redwood-app/tests/template.test.js
+++ packages/create-redwood-app/tests/template.test.js
@@ -2,11 +2,11 @@ import path from 'path'
 
 import klawSync from 'klaw-sync'
 
-const TEMPLATE_DIR = path.join(__dirname, '../template')
+const TS_TEMPLATE_DIR = path.join(__dirname, '../templates', 'ts')
 
 describe('template', () => {
   it('files should not have changed unintentionally', () => {
-    expect(getDirectoryStructure(TEMPLATE_DIR)).toMatchInlineSnapshot(`
+    expect(getDirectoryStructure(TS_TEMPLATE_DIR)).toMatchInlineSnapshot(`
       [
         "/.editorconfig",
         "/.env",
@@ -79,6 +79,88 @@ describe('template', () => {
         "/web/src/pages/NotFoundPage",
         "/web/src/pages/NotFoundPage/NotFoundPage.tsx",
         "/web/tsconfig.json",
+      ]
+    `)
+  })
+})
+
+const JS_TEMPLATE_DIR = path.join(__dirname, '../templates', 'js')
+
+describe('JS template', () => {
+  it('files should not have changed unintentionally', () => {
+    expect(getDirectoryStructure(JS_TEMPLATE_DIR)).toMatchInlineSnapshot(`
+      [
+        "/.editorconfig",
+        "/.env",
+        "/.env.defaults",
+        "/.env.example",
+        "/.nvmrc",
+        "/.vscode",
+        "/.vscode/extensions.json",
+        "/.vscode/launch.json",
+        "/.vscode/settings.json",
+        "/.yarn",
+        "/.yarn/releases",
+        "/.yarn/releases/yarn-3.5.1.cjs",
+        "/.yarnrc.yml",
+        "/README.md",
+        "/api",
+        "/api/db",
+        "/api/db/schema.prisma",
+        "/api/jest.config.js",
+        "/api/jsconfig.json",
+        "/api/package.json",
+        "/api/server.config.js",
+        "/api/src",
+        "/api/src/directives",
+        "/api/src/directives/requireAuth",
+        "/api/src/directives/requireAuth/requireAuth.js",
+        "/api/src/directives/requireAuth/requireAuth.test.js",
+        "/api/src/directives/skipAuth",
+        "/api/src/directives/skipAuth/skipAuth.js",
+        "/api/src/directives/skipAuth/skipAuth.test.js",
+        "/api/src/functions",
+        "/api/src/functions/graphql.js",
+        "/api/src/graphql",
+        "/api/src/graphql/.keep",
+        "/api/src/lib",
+        "/api/src/lib/auth.js",
+        "/api/src/lib/db.js",
+        "/api/src/lib/logger.js",
+        "/api/src/services",
+        "/api/src/services/.keep",
+        "/gitignore.template",
+        "/graphql.config.js",
+        "/jest.config.js",
+        "/package.json",
+        "/prettier.config.js",
+        "/redwood.toml",
+        "/scripts",
+        "/scripts/.keep",
+        "/scripts/jsconfig.json",
+        "/scripts/seed.js",
+        "/web",
+        "/web/jest.config.js",
+        "/web/jsconfig.json",
+        "/web/package.json",
+        "/web/public",
+        "/web/public/README.md",
+        "/web/public/favicon.png",
+        "/web/public/robots.txt",
+        "/web/src",
+        "/web/src/App.js",
+        "/web/src/Routes.js",
+        "/web/src/components",
+        "/web/src/components/.keep",
+        "/web/src/index.css",
+        "/web/src/index.html",
+        "/web/src/layouts",
+        "/web/src/layouts/.keep",
+        "/web/src/pages",
+        "/web/src/pages/FatalErrorPage",
+        "/web/src/pages/FatalErrorPage/FatalErrorPage.js",
+        "/web/src/pages/NotFoundPage",
+        "/web/src/pages/NotFoundPage/NotFoundPage.js",
       ]
     `)
   })
