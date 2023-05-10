db0c96896c223a5fc86329722d716b4675a451d8
Dominic Saadi <dominiceliassaadi@gmail.com>
2023-05-10 15:17:32 -0700

    update build

--- packages/fastify/build.mjs
+++ packages/fastify/build.mjs
@@ -1,27 +1,20 @@
-import fs from 'node:fs'
-
 import * as esbuild from 'esbuild'
 
-// Since this is a library, there's no bundling going on here by design.
-// Instead we plan for this library to be bundled by leaf packages so-to-speak like create-redwood-app.
-const result = await esbuild.build({
+await esbuild.build({
   entryPoints: [
+    'src/api.ts',
+    'src/config.ts',
     'src/index.ts',
     'src/types.ts',
-    'src/config.ts',
     'src/web.ts',
-    'src/api.ts',
     'src/lambda/index.ts',
     'src/plugins/withApiProxy.ts',
   ],
+  outdir: 'dist',
+
   format: 'cjs',
   platform: 'node',
   target: ['node18'],
-  outdir: 'dist',
 
-  // For visualizing the bundle.
-  // See https://esbuild.github.io/api/#metafile and https://esbuild.github.io/analyze/.
-  metafile: true,
+  logLevel: 'info',
 })
-
-fs.writeFileSync('meta.json', JSON.stringify(result.metafile))
