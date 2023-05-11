6fa476c1fe55dd66e97cd3c3753808318d45427c
David Thyresson <dthyresson@gmail.com>
2023-05-11 15:40:52 -0400

    Update packages/studio/frontend/src/Pages/Overview.tsx
    Co-authored-by: Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>

--- packages/studio/frontend/src/Pages/Overview.tsx
+++ packages/studio/frontend/src/Pages/Overview.tsx
@@ -6,7 +6,7 @@ import SeriesTypeBarList from '../BarLists/SeriesTypeBarList'
 
 export default function App() {
   return (
-    <div className="bg-slate-50 p-6 sm:p-10 h-full">
+    <div className="p-6 sm:p-10 h-full">
       <Title className="mb-4">RedwoodJS Studio</Title>
       <Grid numCols={1} numColsSm={2} numColsLg={2} className="gap-2">
         <Col numColSpan={1} numColSpanLg={2}>
