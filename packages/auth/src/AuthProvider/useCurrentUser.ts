c5016029566eb167163e7831c210ab712c07ce25
cjkihl <carljohan.kihl@gmail.com>
2023-05-19 20:08:47 +0800

    Add config for Auth Provider

--- packages/auth/src/AuthProvider/useCurrentUser.ts
+++ packages/auth/src/AuthProvider/useCurrentUser.ts
@@ -2,21 +2,33 @@ import { useCallback } from 'react'
 
 import type { AuthImplementation } from '../AuthImplementation'
 
+import { AuthProviderConfig } from './AuthProvider'
 import { useToken } from './useToken'
 
-export const useCurrentUser = (authImplementation: AuthImplementation) => {
+export const useCurrentUser = (
+  authImplementation: AuthImplementation,
+  authProviderClientConfig?: AuthProviderConfig
+) => {
   const getToken = useToken(authImplementation)
 
   return useCallback(async (): Promise<Record<string, unknown>> => {
     // Always get a fresh token, rather than use the one in state
     const token = await getToken()
-    const response = await globalThis.fetch(globalThis.RWJS_API_GRAPHQL_URL, {
+
+    const fetcher =
+      authProviderClientConfig?.fetchConfig?.fetch ?? globalThis.fetch
+
+    const response = await fetcher(globalThis.RWJS_API_GRAPHQL_URL, {
+      ...(authProviderClientConfig?.fetchConfig?.fetchOptions ?? {}),
       method: 'POST',
-      credentials: 'include',
+      credentials:
+        (authProviderClientConfig?.fetchConfig
+          ?.credentials as RequestCredentials) ?? 'include',
       headers: {
         'content-type': 'application/json',
         'auth-provider': authImplementation.type,
         authorization: `Bearer ${token}`,
+        ...(authProviderClientConfig?.fetchConfig?.headers ?? {}),
       },
       body: JSON.stringify({
         query:
@@ -32,5 +44,12 @@ export const useCurrentUser = (authImplementation: AuthImplementation) => {
         `Could not fetch current user: ${response.statusText} (${response.status})`
       )
     }
-  }, [authImplementation, getToken])
+  }, [
+    authImplementation.type,
+    authProviderClientConfig?.fetchConfig?.credentials,
+    authProviderClientConfig?.fetchConfig?.fetch,
+    authProviderClientConfig?.fetchConfig?.fetchOptions,
+    authProviderClientConfig?.fetchConfig?.headers,
+    getToken,
+  ])
 }
