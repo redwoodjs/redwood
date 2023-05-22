01e96a15617675b93dc1d0628f99801735706e3a
Daniel Choudhury <dannychoudhury@gmail.com>
2023-05-22 13:45:41 +0700

    Disable replay tests in CI

--- tasks/smoke-test/playwright.config.ts
+++ tasks/smoke-test/playwright.config.ts
@@ -1,14 +1,28 @@
 import { PlaywrightTestConfig, devices } from '@playwright/test'
 import { devices as replayDevices } from '@replayio/playwright'
 
+const replayDeviceList = [
+  {
+    name: 'replay-firefox',
+    use: { ...(replayDevices['Replay Firefox'] as any) },
+  },
+  {
+    name: 'replay-chromium',
+    use: { ...(replayDevices['Replay Chromium'] as any) },
+  },
+  {
+    name: 'firefox',
+    use: { ...devices['Desktop Firefox'] },
+  },
+]
+
 // See https://playwright.dev/docs/test-configuration#global-configuration
 const config: PlaywrightTestConfig = {
   timeout: 90_000,
   expect: {
     timeout: 10 * 1000,
   },
-  workers: 1, // do not run things in parallel
-
+  workers: 1, // Avoid running tests in parallel
   // Leaving this here to make debugging easier, by uncommenting
   // use: {
   //   launchOptions: {
@@ -18,22 +32,14 @@ const config: PlaywrightTestConfig = {
   // },
   projects: [
     {
-      name: 'replay-firefox',
-      use: { ...(replayDevices['Replay Firefox'] as any) },
-    },
-    {
-      name: 'replay-chromium',
-      use: { ...(replayDevices['Replay Chromium'] as any) },
-    },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
       name: 'chromium',
       use: { ...devices['Desktop Chromium'] },
     },
   ],
+}
+
+if (process.env.CI) {
+  config.projects?.concat(replayDeviceList)
 }
 
 export default config
