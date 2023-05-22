e31e622e6606ec5432efcb2c8c7a7480057283ab
Daniel Choudhury <dannychoudhury@gmail.com>
2023-05-22 16:19:19 +0700

    Only use replay devices on CI

--- tasks/smoke-test/playwright.config.ts
+++ tasks/smoke-test/playwright.config.ts
@@ -1,28 +1,14 @@
 import { PlaywrightTestConfig, devices } from '@playwright/test'
 import { devices as replayDevices } from '@replayio/playwright'
 
-const replayDeviceList = [
-  {
-    name: 'replay-firefox',
-    use: { ...(replayDevices['Replay Firefox'] as any) },
-  },
-  {
-    name: 'replay-chromium',
-    use: { ...(replayDevices['Replay Chromium'] as any) },
-  },
-  {
-    name: 'firefox',
-    use: { ...devices['Desktop Firefox'] },
-  },
-]
-
 // See https://playwright.dev/docs/test-configuration#global-configuration
 const config: PlaywrightTestConfig = {
   timeout: 90_000,
   expect: {
     timeout: 10 * 1000,
   },
-  workers: 1, // Avoid running tests in parallel
+  workers: 1, // do not run things in parallel
+
   // Leaving this here to make debugging easier, by uncommenting
   // use: {
   //   launchOptions: {
@@ -30,16 +16,27 @@ const config: PlaywrightTestConfig = {
   //     headless: false,
   //   },
   // },
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chromium'] },
-    },
-  ],
-}
-
-if (process.env.CI) {
-  config.projects?.concat(replayDeviceList)
+  projects: process.env.CI // use replay browsers in CI
+    ? [
+        {
+          name: 'replay-firefox',
+          use: { ...(replayDevices['Replay Firefox'] as any) },
+        },
+        {
+          name: 'replay-chromium',
+          use: { ...(replayDevices['Replay Chromium'] as any) },
+        },
+      ]
+    : [
+        {
+          name: 'firefox',
+          use: { ...devices['Desktop Firefox'] },
+        },
+        {
+          name: 'chromium',
+          use: { ...devices['Desktop Chromium'] },
+        },
+      ],
 }
 
 export default config
