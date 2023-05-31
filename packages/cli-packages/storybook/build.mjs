ba427f20e9a4897170ace2cb348519d37811288c
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2023-05-31 18:41:12 +0100

    Remove bin handling from esbuild

--- packages/cli-packages/storybook/build.mjs
+++ packages/cli-packages/storybook/build.mjs
@@ -4,23 +4,7 @@ import * as esbuild from 'esbuild'
 import fg from 'fast-glob'
 
 // Get source files
-const sourceBinFiles = fg.sync(['./src/bins/**/*.ts'])
-const sourceFiles = fg
-  .sync(['./src/**/*.ts'])
-  .filter((f) => !sourceBinFiles.includes(f))
-
-// Build bin files, we need to insert the shebang in this case
-await esbuild.build({
-  entryPoints: sourceBinFiles,
-  format: 'cjs',
-  platform: 'node',
-  target: ['node18'],
-  outdir: 'dist/bins',
-  logLevel: 'info',
-  banner: {
-    js: '#!/usr/bin/env node',
-  },
-})
+const sourceFiles = fg.sync(['./src/**/*.ts'])
 
 // Build general source files
 const result = await esbuild.build({
