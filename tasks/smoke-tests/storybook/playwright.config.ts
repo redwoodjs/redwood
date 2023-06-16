07f130c57df396d6f60998dd7f76c045101793a5
Dominic Saadi <dominiceliassaadi@gmail.com>
2023-06-16 11:09:12 -0700

    chore(smoke-tests): make smoke tests easier to run locally (#8607)
    * chore(smoke-tests): make smoke tests easier to run locally
    
    * REDWOOD_TEST_PROJECT_PATH
    
    ---------
    
    Co-authored-by: Tobbe Lundberg <tobbe@tlundberg.com>

--- tasks/smoke-tests/storybook/playwright.config.ts
+++ tasks/smoke-tests/storybook/playwright.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   // Run your local dev server before starting the tests
   webServer: {
     command: 'yarn redwood storybook --ci --no-open',
-    cwd: process.env.REDWOOD_PROJECT_PATH,
+    cwd: process.env.REDWOOD_TEST_PROJECT_PATH,
     url: 'http://localhost:7910',
     reuseExistingServer: !process.env.CI,
     stdout: 'pipe',
