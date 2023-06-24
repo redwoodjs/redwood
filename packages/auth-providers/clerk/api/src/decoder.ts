5aba35c8c7e8ae83d29cadf58f08eca9cbbf4609
Tobbe Lundberg <tobbe@tlundberg.com>
2023-06-24 16:13:45 +0200

    Clerk decoder: Fix code format (#8721)

--- packages/auth-providers/clerk/api/src/decoder.ts
+++ packages/auth-providers/clerk/api/src/decoder.ts
@@ -38,7 +38,10 @@ export const authDecoder: Decoder = async (token: string, type: string) => {
   }
 }
 
-export const clerkAuthDecoder: Decoder = async (token: string, type: string) => {
+export const clerkAuthDecoder: Decoder = async (
+  token: string,
+  type: string
+) => {
   if (type !== 'clerk') {
     return null
   }
