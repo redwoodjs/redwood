03f29daa64e14169f4b3faf2eaaa1995f1337fd3
Tobbe Lundberg <tobbe@tlundberg.com>
2023-06-28 01:29:56 +0200

    Remove all core bins

--- packages/core/src/bins/eslint.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import { createRequire } from 'module'
-
-const requireFromESLint = createRequire(require.resolve('eslint/package.json'))
-
-const bins = requireFromESLint('./package.json')['bin']
-
-requireFromESLint(bins['eslint'])
--- packages/core/src/bins/jest.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import { createRequire } from 'module'
-
-const requireFromJest = createRequire(require.resolve('jest/package.json'))
-
-const bin = requireFromJest('./package.json')['bin']
-
-requireFromJest(bin)
