a4d2df488fbf7806408587b976920c0294ea66f5
Tobbe Lundberg <tobbe@tlundberg.com>
2023-06-27 13:22:55 +0200

    User yargs-parser

--- packages/vite/bins/rw-vite-build.mjs
+++ packages/vite/bins/rw-vite-build.mjs
@@ -1,9 +1,32 @@
 import fs from 'node:fs'
 
+import yargsParser from 'yargs-parser'
+
 import { buildWeb } from '@redwoodjs/internal/dist/build/web.js'
 import projectConfig from '@redwoodjs/project-config'
 
 const rwPaths = projectConfig.getPaths()
+
+const { webDir } = yargsParser(process.argv.slice(2), {
+  string: ['webDir'],
+})
+
+if (!webDir) {
+  console.error(
+    'Please pass the full path to the web side using the --webDir argument'
+  )
+  process.exit(1)
+}
+
+if (!fs.existsSync(webDir)) {
+  console.error(`Could not find web directory at ${webDir}`)
+  process.exit(1)
+}
+
+if (!fs.existsSync(`${webDir}/package.json`)) {
+  console.error(`${webDir} does not appear to be a Redwood web directory`)
+  process.exit(1)
+}
 
 /**
  * Build the web side of a Redwood project using Vite
@@ -25,33 +48,6 @@ const buildWebSide = async (webDir) => {
   buildWeb({
     verbose: true,
   })
-}
-
-const webDirIndex = process.argv.findIndex((arg) => arg.startsWith('--webDir'))
-
-if (webDirIndex === -1) {
-  console.error(
-    'Please pass the full path to the web side using the --webDir argument'
-  )
-  process.exit(1)
-}
-
-let webDir
-
-if (process.argv[webDirIndex].includes('=')) {
-  // rw-vite-build --webDir="/full/path/with/eq=sign/to/rw-app/web"
-  webDir = process.argv[webDirIndex].split('=').slice(1).join('=')
-} else {
-  // rw-vite-build --webDir /full/path/to/rw-app/web
-  webDir = process.argv[webDirIndex + 1]
-}
-
-if (!fs.existsSync(webDir)) {
-  throw new Error(`Could not find web directory at ${webDir}`)
-}
-
-if (!fs.existsSync(`${webDir}/package.json`)) {
-  throw new Error(`${webDir} does not appear to be a Redwood web directory`)
 }
 
 buildWebSide(webDir)
