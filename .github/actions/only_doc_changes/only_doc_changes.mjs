0b7c659a4d82e2e22f1b2a0d5339665ba7af891a
Dominic Saadi <dominiceliassaadi@gmail.com>
2023-07-06 16:30:19 -0700

    chore: update docs action to handle push event

--- .github/actions/only_doc_changes/only_doc_changes.mjs
+++ .github/actions/only_doc_changes/only_doc_changes.mjs
@@ -1,35 +1,45 @@
-import { exec, getExecOutput } from '@actions/exec'
 import core from '@actions/core'
+import { exec, getExecOutput } from '@actions/exec'
 
-const branch = process.env.GITHUB_BASE_REF
+async function main() {
+  const branch = process.env.GITHUB_BASE_REF
 
-await exec(`git fetch origin ${branch}`)
-
-const { stdout } = await getExecOutput(`git diff origin/${branch} --name-only`)
-
-const changedFiles = stdout.toString().trim().split('\n').filter(Boolean)
-
-for (const changedFile of changedFiles) {
-  if (changedFile.startsWith('docs')) {
-    continue
+  // If there is no branch, we're not in a pull request
+  if (!branch) {
+    core.setOutput('only-doc-changes', false)
+    return
   }
 
-  for (const fileToIgnore of [
-    'CHANGELOG.md',
-    'CODE_OF_CONDUCT.md',
-    'CONTRIBUTING.md',
-    'CONTRIBUTORS.md',
-    'LICENSE',
-    'README.md',
-    'SECURITY.md',
-  ]) {
-    if (changedFile === fileToIgnore) {
+  await exec(`git fetch origin ${branch}`)
+
+  const { stdout } = await getExecOutput(`git diff origin/${branch} --name-only`)
+
+  const changedFiles = stdout.toString().trim().split('\n').filter(Boolean)
+
+  for (const changedFile of changedFiles) {
+    if (changedFile.startsWith('docs')) {
       continue
     }
+
+    for (const fileToIgnore of [
+      'CHANGELOG.md',
+      'CODE_OF_CONDUCT.md',
+      'CONTRIBUTING.md',
+      'CONTRIBUTORS.md',
+      'LICENSE',
+      'README.md',
+      'SECURITY.md',
+    ]) {
+      if (changedFile === fileToIgnore) {
+        continue
+      }
+    }
+
+    core.setOutput('only-doc-changes', false)
+    return
   }
 
-  core.setOutput('only-doc-changes', false)
-  process.exit(0)
+  core.setOutput('only-doc-changes', true)
 }
 
-core.setOutput('only-doc-changes', true)
+main()
