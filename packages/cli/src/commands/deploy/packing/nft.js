redwoodjs/redwood

commit a1c3bbe15e38e58ae0c1a0dfb994ad5bb35cc5f0

Get rid of 'proposal-dynamic-import' (#8456)
* Get rid of 'proposal-dynamic-import'

* Work around The Guild's whatwg-node/server bug

* Import with .js

* Switch back to moduleResolution: node

* Don't touch potentially broken db imports

* try just using require for exec

* do the same for runPrerender

* use require for importing App

* use require for data migrate

* fix remaining problematic await import statements

* fix check

* Regular yargs import

---------

Co-authored-by: Dominic Saadi <dominiceliassaadi@gmail.com>

packages/cli/src/commands/deploy/serverless.js
@@ -82,7 +82,7 @@ export const buildCommands = ({ sides }) => {
       task: async () => {
         // Dynamically import this function
         // because its dependencies are only installed when `rw setup deploy serverless` is run
-        const { nftPack } = (await import('./packing/nft')).default
+        const { nftPack } = (await import('./packing/nft.js')).default
 
         await nftPack()
       },

babel.config.js
@@ -34,7 +34,10 @@ module.exports = {
           // List of supported proposals: https://github.com/zloirock/core-js/blob/master/docs/2019-03-19-core-js-3-babel-and-a-look-into-the-future.md#ecmascript-proposals
           proposals: true,
         },
-        exclude: ['es.error.cause'],
+        exclude: [
+          'es.error.cause',
+          process.env.NODE_ENV !== 'test' && 'proposal-dynamic-import',
+        ].filter(Boolean),
       },
     ],
     '@babel/preset-react',

packages/auth-providers/auth0/setup/src/setup.ts
@@ -1,4 +1,4 @@
-import yargs from 'yargs'
+import * as yargs from 'yargs'
 
 import { standardAuthBuilder } from '@redwoodjs/cli-helpers'
 

packages/auth-providers/azureActiveDirectory/setup/src/setup.ts
@@ -14,6 +14,6 @@ export interface Args {
 }
 
 export async function handler(options: Args) {
-  const { handler } = await import('./setupHandler')
+  const { handler } = await import('./setupHandler.js')
   return handler(options)
 }

packages/auth-providers/clerk/setup/src/setup.ts
@@ -14,6 +14,6 @@ export interface Args {
 }
 
 export async function handler(options: Args) {
-  const { handler } = await import('./setupHandler')
+  const { handler } = await import('./setupHandler.js')
   return handler(options)
 }

packages/auth-providers/custom/setup/src/setup.ts
@@ -14,6 +14,6 @@ export interface Args {
 }
 
 export async function handler(options: Args) {
-  const { handler } = await import('./setupHandler')
+  const { handler } = await import('./setupHandler.js')
   return handler(options)
 }

packages/auth-providers/dbAuth/setup/src/setup.ts
@@ -32,6 +32,6 @@ export interface Args {
 }
 
 export const handler = async (options: Args) => {
-  const { handler } = await import('./setupHandler')
+  const { handler } = await import('./setupHandler.js')
   return handler(options)
 }

packages/auth-providers/firebase/setup/src/setup.ts
@@ -14,6 +14,6 @@ export interface Args {
 }
 
 export async function handler(options: Args) {
-  const { handler } = await import('./setupHandler')
+  const { handler } = await import('./setupHandler.js')
   return handler(options)
 }

commit 46420463a3f54f24d542d763d6b6ecc54d90063b

fix(v6, serverless): use named exports instead of default (#8857)
The packing/nft helper kind of mixes named exports with default exports
and it ends up confusing build tools. It looks like we want named
exports, so I just refactored it to that. This was causing `yarn rw
deploy serverless --pack-only` to fail in deploy target CI (see
https://github.com/redwoodjs/deploy-target-ci/actions/runs/5482609165/jobs/9988107090).
Bug was introduced in https://github.com/redwoodjs/redwood/pull/8456.

packages/cli/src/commands/deploy/packing/nft.js
@@ -9,7 +9,7 @@ import { ensurePosixPath, getPaths } from '@redwoodjs/project-config'
 
 const ZIPBALL_DIR = './api/dist/zipball'
 
-function zipDirectory(source, out) {
+export function zipDirectory(source, out) {
   const archive = archiver('zip', { zlib: { level: 5 } })
   const stream = fse.createWriteStream(out)
 
@@ -25,7 +25,7 @@ function zipDirectory(source, out) {
 }
 
 // returns a tuple of [filePath, fileContent]
-function generateEntryFile(functionAbsolutePath, name) {
+export function generateEntryFile(functionAbsolutePath, name) {
   const relativeImport = ensurePosixPath(
     path.relative(getPaths().base, functionAbsolutePath)
   )
@@ -35,7 +35,7 @@ function generateEntryFile(functionAbsolutePath, name) {
   ]
 }
 
-async function packageSingleFunction(functionFile) {
+export async function packageSingleFunction(functionFile) {
   const { name: functionName } = path.parse(functionFile)
 
   const { fileList: functionDependencyFileList } = await nodeFileTrace([
@@ -67,18 +67,7 @@ async function packageSingleFunction(functionFile) {
   return
 }
 
-function nftPack() {
+export function nftPack() {
   const filesToBePacked = findApiDistFunctions()
   return Promise.all(filesToBePacked.map(exports.packageSingleFunction))
 }
-
-// We do this, so we can spy the functions in the test
-// It didn't make sense to separate into different files
-const exports = {
-  nftPack,
-  packageSingleFunction,
-  generateEntryFile,
-  zipDirectory,
-}
-
-export default exports

packages/cli/src/commands/deploy/serverless.js
@@ -84,7 +84,7 @@ export const buildCommands = ({ sides }) => {
       task: async () => {
         // Dynamically import this function
         // because its dependencies are only installed when `rw setup deploy serverless` is run
-        const { nftPack } = (await import('./packing/nft.js')).default
+        const { nftPack } = await import('./packing/nft.js')
 
         await nftPack()
       },

packages/cli/src/commands/deploy/__tests__/nftPack.test.js
@@ -4,7 +4,7 @@ import path from 'path'
 import { buildApi } from '@redwoodjs/internal/dist/build/api'
 import { findApiDistFunctions } from '@redwoodjs/internal/dist/files'
 
-import nftPacker from '../packing/nft'
+import * as nftPacker from '../packing/nft'
 
 const FIXTURE_PATH = path.resolve(
   __dirname,