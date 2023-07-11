82da171b621e84b2dc05491c614a17bd1e3d5fd8
David Price <thedavid@thedavidprice.com>
2023-07-10 17:48:04 -0700

    fix lint errors

--- packages/router/src/skipNav.tsx
+++ packages/router/src/skipNav.tsx
@@ -3,7 +3,7 @@
 // If resolved, should consider reverting to @reach/skip-nav
 // See: https://github.com/reach/reach-ui/issues/916
 
-import * as React from "react"
+import * as React from 'react'
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -13,37 +13,37 @@ import * as React from "react"
 type Merge<P1 = {}, P2 = {}> = Omit<P1, keyof P2> & P2
 
 type ForwardRefExoticComponent<E, OwnProps> = React.ForwardRefExoticComponent<
-	Merge<
-		E extends React.ElementType ? React.ComponentPropsWithRef<E> : never,
-		OwnProps & { as?: E }
-	>
+  Merge<
+    E extends React.ElementType ? React.ComponentPropsWithRef<E> : never,
+    OwnProps & { as?: E }
+  >
 >
 
 interface ForwardRefComponent<
-	IntrinsicElementString,
-	OwnProps = {}
-	/*
-	 * Extends original type to ensure built in React types play nice with
-	 * polymorphic components still e.g. `React.ElementRef` etc.
-	 */
+  IntrinsicElementString,
+  OwnProps = {}
+  /*
+   * Extends original type to ensure built in React types play nice with
+   * polymorphic components still e.g. `React.ElementRef` etc.
+   */
 > extends ForwardRefExoticComponent<IntrinsicElementString, OwnProps> {
-	/*
-	 * When `as` prop is passed, use this overload. Merges original own props
-	 * (without DOM props) and the inferred props from `as` element with the own
-	 * props taking precendence.
-	 *
-	 * We explicitly avoid `React.ElementType` and manually narrow the prop types
-	 * so that events are typed when using JSX.IntrinsicElements.
-	 */
-	<As = IntrinsicElementString>(
-		props: As extends ""
-			? { as: keyof JSX.IntrinsicElements }
-			: As extends React.ComponentType<infer P>
-			? Merge<P, OwnProps & { as: As }>
-			: As extends keyof JSX.IntrinsicElements
-			? Merge<JSX.IntrinsicElements[As], OwnProps & { as: As }>
-			: never
-	): React.ReactElement | null
+  /*
+   * When `as` prop is passed, use this overload. Merges original own props
+   * (without DOM props) and the inferred props from `as` element with the own
+   * props taking precendence.
+   *
+   * We explicitly avoid `React.ElementType` and manually narrow the prop types
+   * so that events are typed when using JSX.IntrinsicElements.
+   */
+  <As = IntrinsicElementString>(
+    props: As extends ''
+      ? { as: keyof JSX.IntrinsicElements }
+      : As extends React.ComponentType<infer P>
+      ? Merge<P, OwnProps & { as: As }>
+      : As extends keyof JSX.IntrinsicElements
+      ? Merge<JSX.IntrinsicElements[As], OwnProps & { as: As }>
+      : never
+  ): React.ReactElement | null
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -53,7 +53,7 @@ interface ForwardRefComponent<
 
 // The user may want to provide their own ID (maybe there are multiple nav
 // menus on a page a use might want to skip at various points in tabbing?).
-let defaultId = "reach-skip-nav"
+const defaultId = 'reach-skip-nav'
 
 /**
  * SkipNavLink
@@ -63,44 +63,44 @@ let defaultId = "reach-skip-nav"
  * @see Docs https://reach.tech/skip-nav#skipnavlink
  */
 const SkipNavLink = React.forwardRef(function SkipNavLink(
-	{ as: Comp = "a", children = "Skip to content", contentId, ...props },
-	forwardedRef
+  { as: Comp = 'a', children = 'Skip to content', contentId, ...props },
+  forwardedRef
 ) {
-	let id = contentId || defaultId
-	return (
-		<Comp
-			{...props}
-			ref={forwardedRef}
-			href={`#${id}`}
-			// TODO: Remove in 1.0 (kept for back compat)
-			data-reach-skip-link=""
-			data-reach-skip-nav-link=""
-		>
-			{children}
-		</Comp>
-	)
-}) as ForwardRefComponent<"a", SkipNavLinkProps>
+  const id = contentId || defaultId
+  return (
+    <Comp
+      {...props}
+      ref={forwardedRef}
+      href={`#${id}`}
+      // TODO: Remove in 1.0 (kept for back compat)
+      data-reach-skip-link=""
+      data-reach-skip-nav-link=""
+    >
+      {children}
+    </Comp>
+  )
+}) as ForwardRefComponent<'a', SkipNavLinkProps>
 
 /**
  * @see Docs https://reach.tech/skip-nav#skipnavlink-props
  */
 interface SkipNavLinkProps {
-	/**
-	 * Allows you to change the text for your preferred phrase or localization.
-	 *
-	 * @see Docs https://reach.tech/skip-nav#skipnavlink-children
-	 */
-	children?: React.ReactNode
-	/**
-	 * An alternative ID for `SkipNavContent`. If used, the same value must be
-	 * provided to the `id` prop in `SkipNavContent`.
-	 *
-	 * @see Docs https://reach.tech/skip-nav#skipnavlink-contentid
-	 */
-	contentId?: string
+  /**
+   * Allows you to change the text for your preferred phrase or localization.
+   *
+   * @see Docs https://reach.tech/skip-nav#skipnavlink-children
+   */
+  children?: React.ReactNode
+  /**
+   * An alternative ID for `SkipNavContent`. If used, the same value must be
+   * provided to the `id` prop in `SkipNavContent`.
+   *
+   * @see Docs https://reach.tech/skip-nav#skipnavlink-contentid
+   */
+  contentId?: string
 }
 
-SkipNavLink.displayName = "SkipNavLink"
+SkipNavLink.displayName = 'SkipNavLink'
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -112,52 +112,52 @@ SkipNavLink.displayName = "SkipNavLink"
  * @see Docs https://reach.tech/skip-nav#skipnavcontent
  */
 const SkipNavContent = React.forwardRef(function SkipNavContent(
-	{ as: Comp = "div", id: idProp, ...props },
-	forwardedRef
+  { as: Comp = 'div', id: idProp, ...props },
+  forwardedRef
 ) {
-	let id = idProp || defaultId
-	return (
-		<Comp
-			{...props}
-			ref={forwardedRef}
-			id={id}
-			data-reach-skip-nav-content=""
-		/>
-	)
-}) as ForwardRefComponent<"div", SkipNavContentProps>
+  const id = idProp || defaultId
+  return (
+    <Comp
+      {...props}
+      ref={forwardedRef}
+      id={id}
+      data-reach-skip-nav-content=""
+    />
+  )
+}) as ForwardRefComponent<'div', SkipNavContentProps>
 
 /**
  * @see Docs https://reach.tech/skip-nav#skipnavcontent-props
  */
 interface SkipNavContentProps {
-	/**
-	 * You can place the `SkipNavContent` element as a sibling to your main
-	 * content or as a wrapper.
-	 *
-	 * Keep in mind it renders a `div`, so it may mess with your CSS depending on
-	 * where it’s placed.
-	 *
-	 * @example
-	 *   <SkipNavContent />
-	 *   <YourMainContent />
-	 *   // vs.
-	 *   <SkipNavContent>
-	 *     <YourMainContent/>
-	 *   </SkipNavContent>
-	 *
-	 * @see Docs https://reach.tech/skip-nav#skipnavcontent-children
-	 */
-	children?: React.ReactNode
-	/**
-	 * An alternative ID. If used, the same value must be provided to the
-	 * `contentId` prop in `SkipNavLink`.
-	 *
-	 * @see Docs https://reach.tech/skip-nav#skipnavcontent-id
-	 */
-	id?: string
+  /**
+   * You can place the `SkipNavContent` element as a sibling to your main
+   * content or as a wrapper.
+   *
+   * Keep in mind it renders a `div`, so it may mess with your CSS depending on
+   * where it’s placed.
+   *
+   * @example
+   *   <SkipNavContent />
+   *   <YourMainContent />
+   *   // vs.
+   *   <SkipNavContent>
+   *     <YourMainContent/>
+   *   </SkipNavContent>
+   *
+   * @see Docs https://reach.tech/skip-nav#skipnavcontent-children
+   */
+  children?: React.ReactNode
+  /**
+   * An alternative ID. If used, the same value must be provided to the
+   * `contentId` prop in `SkipNavLink`.
+   *
+   * @see Docs https://reach.tech/skip-nav#skipnavcontent-id
+   */
+  id?: string
 }
 
-SkipNavContent.displayName = "SkipNavContent"
+SkipNavContent.displayName = 'SkipNavContent'
 
 ////////////////////////////////////////////////////////////////////////////////
 // Exports
