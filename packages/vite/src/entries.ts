f5fc2e2e696d791536bd14d386efff99fe121ecd
Tobbe Lundberg <tobbe@tlundberg.com>
2023-07-12 11:10:24 +0200

    RSC: Use exported defineEntries() (#8886)

--- packages/vite/src/entries.ts
+++ packages/vite/src/entries.ts
@@ -5,7 +5,8 @@ export type GetEntry = (
 ) => Promise<FunctionComponent | { default: FunctionComponent } | null>
 
 export type GetBuilder = (
-  // FIXME can we somehow avoid leaking internal implementation?
+  // FIXME (from original waku code) can we somehow avoid leaking internal
+  // implementation?
   unstable_decodeId: (encodedId: string) => [id: string, name: string]
 ) => Promise<{
   [pathStr: string]: {
@@ -16,12 +17,10 @@ export type GetBuilder = (
   }
 }>
 
-// This is for ignored dynamic imports
-// XXX Are there any better ways?
-export type unstable_GetCustomModules = () => Promise<{
-  [name: string]: string
-}>
-
+/**
+ * Used to look up the component to import when calling `serve('App')` in
+ * entry.client.tsx
+ */
 export function defineEntries(getEntry: GetEntry, getBuilder?: GetBuilder) {
   return { getEntry, getBuilder }
 }
