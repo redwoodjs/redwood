f665851c8dd105ee5714c8d1f81f27ac8b9d27f6
Dominic Saadi <dominiceliassaadi@gmail.com>
2023-07-17 17:04:58 -0700

    chore(v6): add codemod changing `global` to `globalThis`

---
+++ packages/codemods/src/codemods/v6.x.x/changeGlobalToGlobalThis/__testfixtures__/default.output.js
@@ -0,0 +1,23 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const HomePage = () => {
+  console.log(globalThis)
+
+  return (
+    <>
+      <MetaTags title="Home" description="Home page" />
+
+      <h1>HomePage</h1>
+      <p>
+        Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
+      </p>
+      <p>
+        My default route is named <code>home</code>, link to me with `
+        <Link to={routes.home()}>Home</Link>`
+      </p>
+    </>
+  )
+}
+
+export default HomePage
