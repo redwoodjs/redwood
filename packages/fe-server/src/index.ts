f3bc38528f27fa7b754b297fb23e86853a9871b1
Tobbe Lundberg <tobbe@tlundberg.com>
2023-07-19 19:41:31 +0200

    Accept bin arguments

--- packages/fe-server/src/index.ts
+++ packages/fe-server/src/index.ts
@@ -1,7 +1,14 @@
 #!/usr/bin/env node
 
+import yargsParser from 'yargs-parser'
+
 import { serve } from './server'
 
 if (require.main === module) {
-  serve()
+  const { port, socket, apiHost } = yargsParser(process.argv.slice(2), {
+    string: ['port, socket, apiHost'],
+    alias: { apiHost: ['api-host'], port: ['p'] },
+  })
+
+  serve({ port, socket, apiHost })
 }
