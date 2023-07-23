4d5974495d1ac8c75d113c25e3e0098e8e7ab609
Tobbe Lundberg <tobbe@tlundberg.com>
2023-07-24 01:59:50 +0200

    CRWA: Prompt for installation dir

--- packages/create-redwood-app/src/create-redwood-app.js
+++ packages/create-redwood-app/src/create-redwood-app.js
@@ -3,7 +3,6 @@
 import path from 'path'
 
 import { trace, SpanStatusCode } from '@opentelemetry/api'
-import chalk from 'chalk'
 import checkNodeVersionCb from 'check-node-version'
 import execa from 'execa'
 import fs from 'fs-extra'
@@ -404,6 +403,28 @@ async function initializeGit(newAppDir, commitMessage) {
   tui.stopReactive()
 }
 
+async function handleTargetDirPreference(targetDir) {
+  if (targetDir) {
+    return targetDir
+  }
+
+  // Prompt user for preference
+  try {
+    const response = await tui.prompt({
+      type: 'input',
+      name: 'targetDir',
+      message: 'Where would you like to create your Redwood app?',
+      initial: 'my-redwood-app',
+    })
+
+    return response.targetDir
+  } catch {
+    recordErrorViaTelemetry('User cancelled install at target dir prompt')
+    await shutdownTelemetry()
+    process.exit(1)
+  }
+}
+
 async function handleTypescriptPreference(typescriptFlag) {
   // Handle case where flag is set
   if (typescriptFlag !== null) {
@@ -603,34 +624,14 @@ async function createRedwoodApp() {
   trace.getActiveSpan()?.setAttribute('overwrite', overwrite)
 
   // Get the directory for installation from the args
-  const targetDir = String(args).replace(/,/g, '-')
+  let targetDir = String(args).replace(/,/g, '-')
 
-  // Throw an error if there is no target directory specified
-  if (!targetDir) {
-    tui.displayError(
-      'No target directory specified',
-      [
-        'Please specify the project directory',
-        `  ${chalk.cyan('yarn create redwood-app')} ${chalk.green(
-          '<project-directory>'
-        )}`,
-        '',
-        'For example:',
-        `  ${chalk.cyan('yarn create redwood-app')} ${chalk.green(
-          'my-redwood-app'
-        )}`,
-      ].join('\n')
-    )
-    recordErrorViaTelemetry('No target directory specified')
-    await shutdownTelemetry()
-    process.exit(1)
-  }
-
-  const newAppDir = path.resolve(process.cwd(), targetDir)
   const templatesDir = path.resolve(__dirname, '../templates')
 
   // Engine check
   await executeCompatibilityCheck(path.join(templatesDir, 'ts'))
+
+  targetDir = await handleTargetDirPreference(targetDir)
 
   // Determine ts/js preference
   const useTypescript = await handleTypescriptPreference(typescriptFlag)
@@ -653,6 +654,8 @@ async function createRedwoodApp() {
   if (!_isYarnBerryOrNewer) {
     yarnInstall = await handleYarnInstallPreference(yarnInstallFlag)
   }
+
+  const newAppDir = path.resolve(process.cwd(), targetDir)
 
   // Create project files
   await createProjectFiles(newAppDir, { templateDir, overwrite })
