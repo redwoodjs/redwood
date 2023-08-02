045e114cc4c673f7840cfb92a60748a8fd23631d
Daniel Choudhury <dannychoudhury@gmail.com>
2023-08-02 18:22:13 +0700

    Partly backwards compatible Meta tag injection

---
+++ packages/web/src/components/Head.tsx
@@ -0,0 +1,18 @@
+import { createPortal } from 'react-dom'
+
+import { useServerInsertedHTML } from './ServerInject'
+
+const Head: React.FC<React.PropsWithChildren> = ({ children }) => {
+  useServerInsertedHTML(() => {
+    return children
+  })
+
+  if (typeof window === 'undefined') {
+    // Don't do anything on the server, handled by above callback
+    return null
+  } else {
+    return createPortal(<>{children}</>, document.head)
+  }
+}
+
+export default Head
