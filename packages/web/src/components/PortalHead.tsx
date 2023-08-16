c89e1364c7e9af541438f3274ddf1f92401dc2c8
Daniel Choudhury <dannychoudhury@gmail.com>
2023-08-17 00:19:58 +0700

    exp(streaming): Allow SSR with experimental apollo client (#9038)

--- packages/web/src/components/PortalHead.tsx
+++ packages/web/src/components/PortalHead.tsx
@@ -1,19 +1,48 @@
+import React from 'react'
+
 import { createPortal } from 'react-dom'
 
 import { useServerInsertedHTML } from './ServerInject'
 
+function addDataAttributeMarker(
+  children: React.ReactNode,
+  marker = 'data-rwjs-head'
+) {
+  return React.Children.toArray(children).map((child, i) => {
+    return React.cloneElement(child as React.ReactElement, {
+      [marker]: true,
+      key: `${marker}-` + i,
+    })
+  })
+}
+
 const PortalHead: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const isServerRendering = typeof window === 'undefined'
+
   useServerInsertedHTML(() => {
-    // @TODO this component should be wrapped in: document.head.append()
-    // because its possible for meta tags to be rendered after <head> is closed
-    return children
+    // Add "data-rwjs-head" attribute to anything inside <PortalHead>,
+    // This is then later moved to the <head> in the final block of the transform stream (see streamHelpers.ts)
+    return addDataAttributeMarker(children)
   })
 
-  if (typeof window === 'undefined') {
-    // Don't do anything on the server, handled by above callback
+  // shouldPortal is always false on hard render. Because we use a ref,
+  // the value change is not detected by React and the component is not re-rendered (intentionally)
+  // On a soft render, the value is always true. This is all to prevent double rendering of the head elements.
+
+  // @TODO
+  // There is an edgecase: if you change the children inside <PortalHead> after a hard render, it will not be reflected.
+  // Workaround: don't change children, render a new portal head: x ? <PH>aaa</PH> : <PH>bbb</PH>
+  // we may want to look at using a callback ref: https://shrtm.nu/gMx
+  const shouldPortal = React.useRef(
+    isServerRendering ? false : document.readyState === 'complete'
+  )
+
+  if (isServerRendering) {
+    // Don't do anything on the server, handled by useServerInsertedHTML
     return null
   } else {
-    return createPortal(<>{children}</>, document.head)
+    // On hard render, don't do anything on the client. On soft render, portal to <head>
+    return shouldPortal.current ? createPortal(children, document.head) : null
   }
 }
 
