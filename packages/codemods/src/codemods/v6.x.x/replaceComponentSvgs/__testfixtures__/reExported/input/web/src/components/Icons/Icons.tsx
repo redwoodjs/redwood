6844bb5beecb9b1be457db9eb2f24df2d2458d9a
Daniel Choudhury <dannychoudhury@gmail.com>
2023-08-22 16:07:19 +0700

    Also check for src alias

--- packages/codemods/src/codemods/v6.x.x/replaceComponentSvgs/__testfixtures__/reExported/input/web/src/components/Icons/Icons.tsx
+++ packages/codemods/src/codemods/v6.x.x/replaceComponentSvgs/__testfixtures__/reExported/input/web/src/components/Icons/Icons.tsx
@@ -1,7 +1,7 @@
 import CurrencyYen from './currency-yen.svg'
 import CursorClick from './cursor-click.svg'
 import Database from './database.svg'
-import Desktop from './desktop-computer.svg'
+import Desktop from 'src/components/other/desktop-computer.svg'
 import DeviceMobile from './device-mobile.svg'
 
 export { CurrencyYen, CursorClick, Database as DBIcon, Desktop, DeviceMobile as Mobile }
--- packages/codemods/src/codemods/v6.x.x/replaceComponentSvgs/__testfixtures__/reExported/output/web/src/components/Icons/Icons.tsx
+++ packages/codemods/src/codemods/v6.x.x/replaceComponentSvgs/__testfixtures__/reExported/output/web/src/components/Icons/Icons.tsx
@@ -1,7 +1,7 @@
 import CurrencyYen from "./CurrencyYenSVG"
 import CursorClick from "./CursorClickSVG"
 import Database from "./DatabaseSVG"
-import Desktop from "./DesktopComputerSVG"
+import Desktop from "src/components/other/DesktopComputerSVG"
 import DeviceMobile from "./DeviceMobileSVG"
 
 export { CurrencyYen, CursorClick, Database as DBIcon, Desktop, DeviceMobile as Mobile}
