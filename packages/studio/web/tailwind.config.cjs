989055e691814baf58444fad3f524d87c2092b0c
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2023-08-25 19:59:23 +0100

    Refactor studio project structure

--- packages/studio/web/tailwind.config.cjs
+++ packages/studio/web/tailwind.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
-    './node_modules/@tremor/**/*.{js,ts,jsx,tsx}',
+    '../../../node_modules/@tremor/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: 'class',
   theme: {
