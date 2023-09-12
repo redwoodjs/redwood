7b2ff7d1dc06a6b01bb2e97abd59bc094f8714fc
David Thyresson <dthyresson@gmail.com>
2023-09-12 16:08:02 -0400

    Generate possible types as part of gql codegen

---
+++ packages/internal/src/generate/possibleTypes.ts
@@ -0,0 +1,72 @@
+import fs from 'fs'
+import path from 'path'
+
+import * as fragmentMatcher from '@graphql-codegen/fragment-matcher'
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
+import { loadDocuments, loadSchemaSync } from '@graphql-tools/load'
+
+import { getPaths } from '@redwoodjs/project-config'
+
+import { getLoadDocumentsOptions } from './graphqlCodeGen'
+
+type PossibleTypesResult = {
+  possibleTypesFiles: string[]
+  errors: { message: string; error: unknown }[]
+}
+
+export const generatePossibleTypes = async (): Promise<PossibleTypesResult> => {
+  // generate possible types from fragments and union types
+  const filename = path.join(getPaths().web.types, 'possible-types.ts')
+  const options = getLoadDocumentsOptions(filename)
+  const documentsGlob = './web/src/**/!(*.d).{ts,tsx,js,jsx}'
+
+  let documents
+
+  try {
+    documents = await loadDocuments([documentsGlob], options)
+  } catch {
+    // No GraphQL documents present, no need to try to generate possibleTypes
+    return {
+      possibleTypesFiles: [],
+      errors: [],
+    }
+  }
+
+  const errors: { message: string; error: unknown }[] = []
+
+  try {
+    const files = []
+    const pluginConfig = {}
+    const info = {
+      outputFile: filename,
+    }
+    const schema = loadSchemaSync(getPaths().generated.schema, {
+      loaders: [new GraphQLFileLoader()],
+      sort: true,
+    })
+
+    const possibleTypes = await fragmentMatcher.plugin(
+      schema,
+      documents,
+      pluginConfig,
+      info
+    )
+
+    files.push(filename)
+
+    fs.mkdirSync(path.dirname(filename), { recursive: true })
+    fs.writeFileSync(filename, possibleTypes.toString())
+
+    return { possibleTypesFiles: [filename], errors }
+  } catch (e) {
+    errors.push({
+      message: 'Error: Could not generate GraphQL possible types (web)',
+      error: e,
+    })
+
+    return {
+      possibleTypesFiles: [],
+      errors,
+    }
+  }
+}
