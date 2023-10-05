d8eec693f8364270dccacf902cd1872fc58be60c
Dominic Saadi <dominiceliassaadi@gmail.com>
2023-10-04 18:02:08 -0700

    fix(api-sever): fix infinite loop on large projects

--- packages/api-server/src/watch.ts
+++ packages/api-server/src/watch.ts
@@ -187,6 +187,13 @@ chokidar
     await validate()
   })
   .on('all', async (eventName, filePath) => {
+    // On sufficiently large projects (500+ files, or >= 2000 ms build times) on older machines, esbuild opening the api directory
+    // makes chokidar emit an `addDir` event. This starts an infinite loop where the api starts building itself as soon as its finished building.
+    // This could probably be fixed with build caching, or by digging into chokidar/esbuild.
+    if (eventName === 'addDir' && filePath === rwjsPaths.api.base) {
+      return
+    }
+
     // We validate here, so that developers will see the error
     // As they're running the dev server
     if (filePath.includes('.sdl')) {
