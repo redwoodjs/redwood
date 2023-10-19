dcb6ff125eb59e96aadff9e52076785833638a37
Rob Cameron <cannikin@fastmail.com>
2023-10-19 11:56:21 -0700

    Remove unused imports

--- packages/web/src/components/MetaTags.test.tsx
+++ packages/web/src/components/MetaTags.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from '@testing-library/react'
+import { render } from '@testing-library/react'
 
 import '@testing-library/jest-dom'
 import { MetaTags } from './MetaTags'
