3ebd55b3cc37ac769fb08f60fd81630bf6dbafe0
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2023-10-20 18:51:25 +0100

    update template files

--- packages/create-redwood-app/templates/js/api/src/lib/mailer.js
+++ packages/create-redwood-app/templates/js/api/src/lib/mailer.js
@@ -1,17 +1,12 @@
 import { Mailer } from '@redwoodjs/mailer-core'
-import { InMemoryMailHandler } from '@redwoodjs/mailer-handler-in-memory'
 import { NodemailerMailHandler } from '@redwoodjs/mailer-handler-nodemailer'
-import { StudioMailHandler } from '@redwoodjs/mailer-handler-studio'
 import { ReactEmailRenderer } from '@redwoodjs/mailer-renderer-react-email'
 
 import { logger } from './logger'
 
 export const mailer = new Mailer({
-  // Handlers to send our emails out to the world
   handling: {
     handlers: {
-      inMemory: new InMemoryMailHandler(),
-      studio: new StudioMailHandler(),
       // TODO: Update this handler config or switch it out for a different handler completely
       nodemailer: new NodemailerMailHandler({
         transport: {
@@ -21,11 +16,9 @@ export const mailer = new Mailer({
         },
       }),
     },
-    // This is our default handler when in production only
     default: 'nodemailer',
   },
 
-  // Renderers to convert our templates into HTML/text
   rendering: {
     renderers: {
       reactEmail: new ReactEmailRenderer(),
@@ -33,19 +26,5 @@ export const mailer = new Mailer({
     default: 'reactEmail',
   },
 
-  // Specific handlers for testing and development time
-  test: {
-    handler: 'inMemory',
-    // when: process.env.NODE_ENV === 'test',
-  },
-  development: {
-    handler: 'studio',
-    // when: process.env.NODE_ENV !== 'production',
-  },
-
-  // Default send options
-  defaults: {
-    // replyTo: 'noreply@example.com',
-  },
   logger,
 })
