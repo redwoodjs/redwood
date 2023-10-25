c8c8a3b1e3c599a610b3856b71078357e70e44be
Tobbe Lundberg <tobbe@tlundberg.com>
2023-10-25 16:05:39 +0200

    RSC: vite externalConditions (#9333)

---
+++ __fixtures__/test-project-rsc-external-packages/prettier.config.js
@@ -0,0 +1,18 @@
+// https://prettier.io/docs/en/options.html
+/** @type {import('prettier').RequiredOptions} */
+module.exports = {
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  arrowParens: 'always',
+  overrides: [
+    {
+      files: 'Routes.*',
+      options: {
+        printWidth: 999,
+      },
+    },
+  ],
+}
