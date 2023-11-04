6882948ce11fbf15403ab6e0edb07c27362dd4e9
Rob Cameron <cannikin@fastmail.com>
2023-11-03 21:04:06 -0700

    Update crypto library, CryptoJS CVE & deprecation (#9350)
    So CryptoJS just dropped a bomb: everything they do by default is not as
    strong as it could be. Oh and by the way, the entire library is now
    deprecated. :(
    https://github.com/brix/crypto-js/security/advisories/GHSA-xwcq-pm8m-c4vf
    Unfortunately we can't just upgrade to the latest release 4.2.0 because
    the hashing algorithm has changed, and a user would no longer be able to
    login: the default hash generated by CryptoJS 4.2.0 won't match the hash
    generated by CryptoJS 4.1.0.
    
    Note that this is only an issue if someone got the contents of your
    database and wanted to figure out user passwords (but it still cost
    [$45,000 per password](https://eprint.iacr.org/2020/014.pdf)
    apparently?).
    
    In the wake of this CVE we're going to convert dbAuth to use the
    built-in `node:crypto` library instead, with more sensible default
    configuration.
    
    There are two areas where we use the crypto libs:
    
    1. Hashing the user's password to store in the DB and compare on login
    2. Encrypting/decrypting the session data in a cookie
    
    We're going to do this in a non-breaking way by supporting *both* the
    original CryptoJS-derived values, and the new `node:crypto` ones. The
    alternative would be to require every user to change their password,
    which seems like a non-starter.
    
    1. On signup, store the hashedPassword using the new `node:crypto`
    algorithm
    2. On login, compare the user's hashedPassword using the `node:crypto`
    algorithm:
      * If a match is found, user is logged in
    * If a match fails, fall back to the original CryptoJS algorithm (but
    using the `node:crypto` implementation)
    * If a match is found, update the `hashedPassword` in the database to
    the new algorithm, user is logged in
    * If a match is still not found, the user entered the wrong password.
    
    Likewise for cookies and login:
    
    1. When encrypting the user's session, always use the new `node:crypto`
    algorithm
    2. When decrypting the user's session, first try with `node:crypto`
      * If decrypting works, user is logged in
    * If decrypting fails, try the older CryptoJS algorithm ([I haven't
    figured how](https://github.com/brix/crypto-js/issues/468) to use
    `node:crypto` to decrypt something that was encrypted with CryptoJS yet,
    so we'll need to keep the dependency on CryptoJS around for now)
    * If decrypting works, re-encrypt the cookie using the new `node:crypto`
    algorithm, user is logged in
    * If decrypting still fails, the session is invalid (someone tampered
    with the cookie) so log them out
    
    We could announce in the Release Notes that if a platform wants the
    absolute safest route, they should change their `SESSION_SECRET` *and*
    have users change their password if, for example, they suspect that
    their database may have been compromised before our release.
    
    The next most secure thing would be to just change `SESSION_SECRET`
    which would log everyone out, and on next login their password will get
    re-hashed with the new algorithm.
    
    But the default for most people will probably be to just go about
    business as usual, and as time goes by more and more users' passwords
    will be re-hashed on login.
    
    Related to #9337 #9338 #9339 #9340
    
    ---------
    
    Co-authored-by: Dominic Saadi <dominiceliassaadi@gmail.com>

--- packages/auth-providers/dbAuth/api/src/DbAuthHandler.ts
+++ packages/auth-providers/dbAuth/api/src/DbAuthHandler.ts
@@ -13,7 +13,6 @@ import type {
 } from '@simplewebauthn/typescript-types'
 import type { APIGatewayProxyEvent, Context as LambdaContext } from 'aws-lambda'
 import base64url from 'base64url'
-import CryptoJS from 'crypto-js'
 import md5 from 'md5'
 import { v4 as uuidv4 } from 'uuid'
 
@@ -23,11 +22,15 @@ import { createCorsContext, normalizeRequest } from '@redwoodjs/api'
 import * as DbAuthError from './errors'
 import {
   decryptSession,
+  encryptSession,
   extractCookie,
   getSession,
   hashPassword,
+  legacyHashPassword,
+  isLegacySession,
   hashToken,
   webAuthnSession,
+  extractHashingOptions,
 } from './shared'
 
 type SetCookieHeader = { 'set-cookie': string }
@@ -545,11 +548,15 @@ export class DbAuthHandler<
   async getToken() {
     try {
       const user = await this._getCurrentUser()
+      let headers = {}
 
-      // need to return *something* for our existing Authorization header stuff
-      // to work, so return the user's ID in case we can use it for something
-      // in the future
-      return [user[this.options.authFields.id]]
+      // if the session was encrypted with the old algorithm, re-encrypt it
+      // with the new one
+      if (isLegacySession(this.cookie)) {
+        headers = this._loginResponse(user)[1]
+      }
+
+      return [user[this.options.authFields.id], headers]
     } catch (e: any) {
       if (e instanceof DbAuthError.NotLoggedInError) {
         return this._logoutResponse()
@@ -612,12 +619,16 @@ export class DbAuthHandler<
     }
 
     let user = await this._findUserByToken(resetToken as string)
-    const [hashedPassword] = hashPassword(password, user.salt)
+    const [hashedPassword] = hashPassword(password, {
+      salt: user.salt,
+    })
+    const [legacyHashedPassword] = legacyHashPassword(password, user.salt)
 
     if (
-      !(this.options.resetPassword as ResetPasswordFlowOptions)
+      (!(this.options.resetPassword as ResetPasswordFlowOptions)
         .allowReusedPassword &&
-      user.hashedPassword === hashedPassword
+        user.hashedPassword === hashedPassword) ||
+      user.hashedPassword === legacyHashedPassword
     ) {
       throw new DbAuthError.ReusedPasswordError(
         (
@@ -1120,11 +1131,6 @@ export class DbAuthHandler<
     return meta
   }
 
-  // encrypts a string with the SESSION_SECRET
-  _encrypt(data: string) {
-    return CryptoJS.AES.encrypt(data, process.env.SESSION_SECRET as string)
-  }
-
   // returns the set-cookie header to be returned in the request (effectively
   // creates the session)
   _createSessionHeader<TIdType = any>(
@@ -1132,9 +1138,9 @@ export class DbAuthHandler<
     csrfToken: string
   ): SetCookieHeader {
     const session = JSON.stringify(data) + ';' + csrfToken
-    const encrypted = this._encrypt(session)
+    const encrypted = encryptSession(session)
     const cookie = [
-      `session=${encrypted.toString()}`,
+      `session=${encrypted}`,
       ...this._cookieAttributes({ expires: this.sessionExpiresDate }),
     ].join(';')
 
@@ -1245,19 +1251,56 @@ export class DbAuthHandler<
       )
     }
 
-    // is password correct?
-    const [hashedPassword, _salt] = hashPassword(
-      password,
-      user[this.options.authFields.salt]
+    await this._verifyPassword(user, password)
+    return user
+  }
+
+  // extracts scrypt strength options from hashed password (if present) and
+  // compares the hashed plain text password just submitted using those options
+  // with the one in the database. Falls back to the legacy CryptoJS algorihtm
+  // if no options are present.
+  async _verifyPassword(user: Record<string, unknown>, password: string) {
+    const options = extractHashingOptions(
+      user[this.options.authFields.hashedPassword] as string
     )
-    if (hashedPassword === user[this.options.authFields.hashedPassword]) {
-      return user
+
+    if (Object.keys(options).length) {
+      // hashed using the node:crypto algorithm
+      const [hashedPassword] = hashPassword(password, {
+        salt: user[this.options.authFields.salt] as string,
+        options,
+      })
+
+      if (hashedPassword === user[this.options.authFields.hashedPassword]) {
+        return user
+      }
     } else {
-      throw new DbAuthError.IncorrectPasswordError(
-        username,
-        (this.options.login as LoginFlowOptions)?.errors?.incorrectPassword
+      // fallback to old CryptoJS hashing
+      const [legacyHashedPassword] = legacyHashPassword(
+        password,
+        user[this.options.authFields.salt] as string
       )
+
+      if (
+        legacyHashedPassword === user[this.options.authFields.hashedPassword]
+      ) {
+        const [newHashedPassword] = hashPassword(password, {
+          salt: user[this.options.authFields.salt] as string,
+        })
+
+        // update user's hash to the new algorithm
+        await this.dbAccessor.update({
+          where: { id: user.id },
+          data: { [this.options.authFields.hashedPassword]: newHashedPassword },
+        })
+        return user
+      }
     }
+
+    throw new DbAuthError.IncorrectPasswordError(
+      user[this.options.authFields.username] as string,
+      (this.options.login as LoginFlowOptions)?.errors?.incorrectPassword
+    )
   }
 
   // gets the user from the database and returns only its ID
@@ -1316,8 +1359,7 @@ export class DbAuthHandler<
         )
       }
 
-      // if we get here everything is good, call the app's signup handler and let
-      // them worry about scrubbing data and saving to the DB
+      // if we get here everything is good, call the app's signup handler
       const [hashedPassword, salt] = hashPassword(password)
       const newUser = await (this.options.signup as SignupFlowOptions).handler({
         username,
@@ -1371,9 +1413,7 @@ export class DbAuthHandler<
   ] {
     const sessionData = { id: user[this.options.authFields.id] }
 
-    // TODO: this needs to go into graphql somewhere so that each request makes
-    // a new CSRF token and sets it in both the encrypted session and the
-    // csrf-token header
+    // TODO: this needs to go into graphql somewhere so that each request makes a new CSRF token and sets it in both the encrypted session and the csrf-token header
     const csrfToken = DbAuthHandler.CSRF_TOKEN
 
     return [
