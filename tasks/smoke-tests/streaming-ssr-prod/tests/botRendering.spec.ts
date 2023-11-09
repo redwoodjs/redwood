95edd84f90f9575a38c5f8ec0c49ad53526ad24a
Daniel Choudhury <dannychoudhury@gmail.com>
2023-11-09 15:37:59 +0700

    Update bot rendering spec to completely block JS for cell rendering test

--- tasks/smoke-tests/streaming-ssr-prod/tests/botRendering.spec.ts
+++ tasks/smoke-tests/streaming-ssr-prod/tests/botRendering.spec.ts
@@ -6,33 +6,47 @@ import { checkHomePageCellRender } from '../../shared/homePage'
 
 let botPageNoJs: Page
 
-test.beforeAll(async ({ browser }) => {
-  // UA taken from https://developers.google.com/search/docs/crawling-indexing/overview-google-crawlers
+// UA taken from https://developers.google.com/search/docs/crawling-indexing/overview-google-crawlers
+const BOT_USERAGENT =
+  'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Chrome/W.X.Y.Z Safari/537.36'
+
+test('Check that homepage has content fully rendered from the server, without JS', async ({
+  browser,
+}) => {
   const botContext = await browser.newContext({
-    userAgent:
-      'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Chrome/W.X.Y.Z Safari/537.36',
+    userAgent: BOT_USERAGENT,
     // Even without JS, this should be a fully rendered page
     javaScriptEnabled: false,
   })
 
-  botPageNoJs = await botContext.newPage()
-})
+  const botPageNoJs = await botContext.newPage()
 
-test.afterAll(() => {
-  botPageNoJs.close()
-})
-
-test('Check that homepage has content rendered from the server', async () => {
   await botPageNoJs.goto('/')
 
   // Appears when Cell is successfully rendered
   await botPageNoJs.waitForSelector('article')
 
   await checkHomePageCellRender(botPageNoJs)
+
+  await botPageNoJs.close()
 })
 
-test('Check delayed page is NOT progressively rendered', async () => {
-  await checkDelayedPageRendering(botPageNoJs, {
+test('Check delayed page is NOT progressively rendered', async ({
+  browser,
+}) => {
+  // For this test we need to enable JS, but block all client side scripts
+  // So that we can check that expected delay is 0
+  const botContext = await browser.newContext({
+    userAgent: BOT_USERAGENT,
+  })
+
+  const botPageNoBundle = await botContext.newPage()
+
+  await botPageNoBundle.route('**/*.*.{js,tsx,ts,jsx}', (route) =>
+    route.abort()
+  )
+
+  await checkDelayedPageRendering(botPageNoBundle, {
     expectedDelay: 0,
   })
 })
