e7fe5c1b9ae98dd173bf77e76e13ceaac5338135
Tom Mrazauskas <tom@mrazauskas.de>
2023-11-13 11:24:01 +0200

    simplify Jest configs

--- packages/router/jest.config.js
+++ packages/router/jest.config.js
@@ -1,10 +1,6 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
-  projects: [
-    {
-      setupFilesAfterEnv: ['./jest.setup.js'],
-      testEnvironment: 'jest-environment-jsdom',
-      testMatch: ['**/*.test.+(ts|tsx|js|jsx)', '!**/__typetests__/*.ts'],
-    },
-  ],
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  testMatch: ['**/*.test.+(ts|tsx|js|jsx)', '!**/__typetests__/*.ts'],
 }
--- packages/web/jest.config.js
+++ packages/web/jest.config.js
@@ -1,16 +1,12 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
-  projects: [
-    {
-      testEnvironment: 'jest-environment-jsdom',
-      testMatch: [
-        '**/*.test.+(ts|tsx|js|jsx)',
-        '!**/__typetests__/*.+(ts|tsx|js|jsx)',
-      ],
-      globals: {
-        // Required for code that use experimental flags
-        RWJS_ENV: {},
-      },
-    },
+  testEnvironment: 'jest-environment-jsdom',
+  testMatch: [
+    '**/*.test.+(ts|tsx|js|jsx)',
+    '!**/__typetests__/*.+(ts|tsx|js|jsx)',
   ],
+  globals: {
+    // Required for code that use experimental flags
+    RWJS_ENV: {},
+  },
 }
