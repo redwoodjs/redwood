d55eff57647e0cdb54cdeca3c140368b0aaf536e
Tobbe Lundberg <tobbe@tlundberg.com>
2023-11-19 18:27:15 +0100

    RSC: Combine server with StreamingSSR server

--- packages/vite/src/buildFeServer.ts
+++ packages/vite/src/buildFeServer.ts
@@ -136,7 +136,10 @@ export const buildFeServer = async ({ verbose, webDir }: BuildOptions = {}) => {
   //  * With `assert` and `@babel/plugin-syntax-import-assertions` the
   //    code compiled and ran properly, but Jest tests failed, complaining
   //    about the syntax.
-  const manifestPath = path.join(getPaths().web.dist, 'build-manifest.json')
+  const manifestPath = path.join(
+    getPaths().web.dist,
+    'client-build-manifest.json'
+  )
   const buildManifestStr = await fs.readFile(manifestPath, 'utf-8')
   const clientBuildManifest: ViteBuildManifest = JSON.parse(buildManifestStr)
 
