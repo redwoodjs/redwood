5ca49c581d05aaf5467bb27b479ba35428e2bdb5
David Thyresson <dthyresson@gmail.com>
2023-11-28 15:51:59 -0500

    client present silences output

--- packages/internal/src/generate/clientPreset.ts
+++ packages/internal/src/generate/clientPreset.ts
@@ -31,6 +31,7 @@ export const generateClientPreset = async () => {
   const config: CodegenConfig = {
     schema: getPaths().generated.schema,
     documents: documentsGlob,
+    silent: true, // Plays nicely with cli task output
     generates: {
       // should be graphql.d.ts
       [`${getPaths().web.base}/types/types.d.ts`]: {
