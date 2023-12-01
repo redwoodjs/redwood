51ab9e8aeec5173ef224bbf9d277b31e4ef4fae1
Tobbe Lundberg <tobbe@tlundberg.com>
2023-12-01 13:25:05 +0100

    RSC: Update external modules test fixture

--- __fixtures__/test-project-rsc-external-packages/web/src/AboutPage.tsx
+++ __fixtures__/test-project-rsc-external-packages/web/src/AboutPage.tsx
@@ -1,29 +1,26 @@
 import { Assets } from '@redwoodjs/vite/assets'
 import { ProdRwRscServerGlobal } from '@redwoodjs/vite/rwRscGlobal'
 
-import { onSend } from './chat'
-import { Form } from './Form'
-// @ts-expect-error no types
-import styles from './HomePage.module.css'
+import { Counter } from './Counter'
 
-import './HomePage.css'
+import './AboutPage.css'
 
 // TODO (RSC) Something like this will probably be needed
 // const RwRscGlobal = import.meta.env.PROD ? ProdRwRscServerGlobal : DevRwRscServerGlobal;
 
 globalThis.rwRscGlobal = new ProdRwRscServerGlobal()
 
-const HomePage = ({ name = 'Anonymous' }) => {
+const AboutPage = () => {
   return (
-    <div className="home-page">
+    <div className="about-page">
       {/* TODO (RSC) <Assets /> should be part of the router later */}
       <Assets />
       <div style={{ border: '3px red dashed', margin: '1em', padding: '1em' }}>
-        <h1 className={styles.title}>Hello {name}!!</h1>
-        <Form onSend={onSend} />
+        <h1>About Redwood</h1>
+        <Counter />
       </div>
     </div>
   )
 }
 
-export default HomePage
+export default AboutPage
