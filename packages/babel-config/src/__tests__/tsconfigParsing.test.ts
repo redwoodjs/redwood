a3bebf998e72f6164c8cb5b16d6253e856535a28
Daniel Choudhury <dannychoudhury@gmail.com>
2023-12-01 17:11:06 +0700

    Trying mocking path.resolve

--- packages/babel-config/src/__tests__/tsconfigParsing.test.ts
+++ packages/babel-config/src/__tests__/tsconfigParsing.test.ts
@@ -16,9 +16,6 @@ afterEach(() => {
   vol.reset()
 })
 
-const FAKE_API_ROOT = '/redwood-app/api'
-const FAKE_WEB_ROOT = '/redwood-app/web'
-
 describe('TypeScript config file parsing', () => {
   it("returns `null` if it can't find TypeScript config files", () => {
     vol.fromNestedJSON(
@@ -79,137 +76,6 @@ describe('TypeScript config file parsing', () => {
     expect(typeScriptConfig.web).toMatchObject(JSON.parse(webJSConfig))
   })
 
-  describe('getPathsFromTypeScriptConfig', () => {
-    it("returns an empty object if there's no TypeScript config files", () => {
-      vol.fromNestedJSON(
-        {
-          'redwood.toml': '',
-          api: {},
-          web: {},
-        },
-        redwoodProjectPath
-      )
-
-      const typeScriptConfig = parseTypeScriptConfigFiles()
-
-      const apiPaths = getPathsFromTypeScriptConfig(
-        typeScriptConfig.api,
-        FAKE_API_ROOT
-      )
-      expect(apiPaths).toMatchObject({})
-
-      const webPaths = getPathsFromTypeScriptConfig(
-        typeScriptConfig.web,
-        FAKE_WEB_ROOT
-      )
-      expect(webPaths).toMatchObject({})
-    })
-
-    it("returns an empty object if there's no compilerOptions, baseUrl, or paths", () => {
-      const apiTSConfig = '{}'
-      const webTSConfig = '{"compilerOptions":{"allowJs": true}}'
-
-      vol.fromNestedJSON(
-        {
-          'redwood.toml': '',
-          api: {
-            'tsconfig.json': apiTSConfig,
-          },
-          web: {
-            'tsconfig.json': webTSConfig,
-          },
-        },
-        redwoodProjectPath
-      )
-
-      const typeScriptConfig = parseTypeScriptConfigFiles()
-
-      const apiPaths = getPathsFromTypeScriptConfig(
-        typeScriptConfig.api,
-        FAKE_API_ROOT
-      )
-      expect(apiPaths).toMatchInlineSnapshot(`{}`)
-
-      const webPaths = getPathsFromTypeScriptConfig(
-        typeScriptConfig.web,
-        FAKE_WEB_ROOT
-      )
-      expect(webPaths).toMatchInlineSnapshot(`{}`)
-    })
-
-    it('excludes "src/*", "$api/*", "types/*", and "@redwoodjs/testing"', () => {
-      const apiTSConfig =
-        '{"compilerOptions":{"baseUrl":"./","paths":{"src/*":["./src/*","../.redwood/types/mirror/api/src/*"],"types/*":["./types/*","../types/*"],"@redwoodjs/testing":["../node_modules/@redwoodjs/testing/api"]}}}'
-      const webTSConfig =
-        '{"compilerOptions":{"baseUrl":"./","paths":{"src/*":["./src/*","../.redwood/types/mirror/web/src/*"],"$api/*":[ "../api/*" ],"types/*":["./types/*", "../types/*"],"@redwoodjs/testing":["../node_modules/@redwoodjs/testing/web"]}}}'
-
-      vol.fromNestedJSON(
-        {
-          'redwood.toml': '',
-          api: {
-            'tsconfig.json': apiTSConfig,
-          },
-          web: {
-            'tsconfig.json': webTSConfig,
-          },
-        },
-        redwoodProjectPath
-      )
-
-      const typeScriptConfig = parseTypeScriptConfigFiles()
-
-      const apiPaths = getPathsFromTypeScriptConfig(
-        typeScriptConfig.api,
-        FAKE_API_ROOT
-      )
-      expect(apiPaths).toMatchInlineSnapshot(`{}`)
-
-      const webPaths = getPathsFromTypeScriptConfig(
-        typeScriptConfig.web,
-        FAKE_WEB_ROOT
-      )
-      expect(webPaths).toMatchInlineSnapshot(`{}`)
-    })
-
-    it('gets and formats paths', () => {
-      const apiTSConfig =
-        '{"compilerOptions":{"baseUrl":"./","paths":{"@services/*":["./src/services/*"]}}}'
-      const webTSConfig =
-        '{"compilerOptions":{"baseUrl":"./","paths":{"@ui/*":["./src/ui/*"]}}}'
-
-      vol.fromNestedJSON(
-        {
-          'redwood.toml': '',
-          api: {
-            'tsconfig.json': apiTSConfig,
-          },
-          web: {
-            'tsconfig.json': webTSConfig,
-          },
-        },
-        redwoodProjectPath
-      )
-
-      const typeScriptConfig = parseTypeScriptConfigFiles()
-
-      const apiPaths = getPathsFromTypeScriptConfig(
-        typeScriptConfig.api,
-        FAKE_API_ROOT
-      )
-      expect(ensurePosixPath(apiPaths['@services'])).toEqual(
-        ensurePosixPath('/redwood-app/api/src/services')
-      )
-
-      const webPaths = getPathsFromTypeScriptConfig(
-        typeScriptConfig.web,
-        FAKE_WEB_ROOT
-      )
-      expect(ensurePosixPath(webPaths['@ui'])).toEqual(
-        ensurePosixPath('/redwood-app/web/src/ui')
-      )
-    })
-  })
-
   it('handles invalid JSON', () => {
     const apiTSConfig =
       '{"compilerOptions": {"noEmit": true,"allowJs": true,"esModuleInterop": true,"target": "esnext","module": "esnext","moduleResolution": "node","baseUrl": "./","rootDirs": ["./src","../.redwood/types/mirror/api/src"],"paths": {"src/*": ["./src/*","../.redwood/types/mirror/api/src/*"],"types/*": ["./types/*", "../types/*"],"@redwoodjs/testing": ["../node_modules/@redwoodjs/testing/api"]},"typeRoots": ["../node_modules/@types","./node_modules/@types"],"types": ["jest"],},"include": ["src","../.redwood/types/includes/all-*","../.redwood/types/includes/api-*","../types"]}'
@@ -230,5 +96,150 @@ describe('TypeScript config file parsing', () => {
     )
 
     expect(parseTypeScriptConfigFiles).not.toThrow()
+  })
+})
+
+describe('getPathsFromTypeScriptConfig', () => {
+  const FAKE_API_ROOT = '/redwood-app/api'
+  const FAKE_WEB_ROOT = '/redwood-app/web'
+
+  jest.mock('path', () => {
+    return {
+      ...jest.requireActual('path'),
+      // Mocking resolve here, so that Windows doesn't return d:/
+      resolve: (path) => {
+        return path
+      },
+    }
+  })
+
+  it("returns an empty object if there's no TypeScript config files", () => {
+    vol.fromNestedJSON(
+      {
+        'redwood.toml': '',
+        api: {},
+        web: {},
+      },
+      redwoodProjectPath
+    )
+
+    const typeScriptConfig = parseTypeScriptConfigFiles()
+
+    const apiPaths = getPathsFromTypeScriptConfig(
+      typeScriptConfig.api,
+      FAKE_API_ROOT
+    )
+    expect(apiPaths).toMatchObject({})
+
+    const webPaths = getPathsFromTypeScriptConfig(
+      typeScriptConfig.web,
+      FAKE_WEB_ROOT
+    )
+    expect(webPaths).toMatchObject({})
+  })
+
+  it("returns an empty object if there's no compilerOptions, baseUrl, or paths", () => {
+    const apiTSConfig = '{}'
+    const webTSConfig = '{"compilerOptions":{"allowJs": true}}'
+
+    vol.fromNestedJSON(
+      {
+        'redwood.toml': '',
+        api: {
+          'tsconfig.json': apiTSConfig,
+        },
+        web: {
+          'tsconfig.json': webTSConfig,
+        },
+      },
+      redwoodProjectPath
+    )
+
+    const typeScriptConfig = parseTypeScriptConfigFiles()
+
+    const apiPaths = getPathsFromTypeScriptConfig(
+      typeScriptConfig.api,
+      FAKE_API_ROOT
+    )
+    expect(apiPaths).toMatchInlineSnapshot(`{}`)
+
+    const webPaths = getPathsFromTypeScriptConfig(
+      typeScriptConfig.web,
+      FAKE_WEB_ROOT
+    )
+    expect(webPaths).toMatchInlineSnapshot(`{}`)
+  })
+
+  it('excludes "src/*", "$api/*", "types/*", and "@redwoodjs/testing"', () => {
+    const apiTSConfig =
+      '{"compilerOptions":{"baseUrl":"./","paths":{"src/*":["./src/*","../.redwood/types/mirror/api/src/*"],"types/*":["./types/*","../types/*"],"@redwoodjs/testing":["../node_modules/@redwoodjs/testing/api"]}}}'
+    const webTSConfig =
+      '{"compilerOptions":{"baseUrl":"./","paths":{"src/*":["./src/*","../.redwood/types/mirror/web/src/*"],"$api/*":[ "../api/*" ],"types/*":["./types/*", "../types/*"],"@redwoodjs/testing":["../node_modules/@redwoodjs/testing/web"]}}}'
+
+    vol.fromNestedJSON(
+      {
+        'redwood.toml': '',
+        api: {
+          'tsconfig.json': apiTSConfig,
+        },
+        web: {
+          'tsconfig.json': webTSConfig,
+        },
+      },
+      redwoodProjectPath
+    )
+
+    const typeScriptConfig = parseTypeScriptConfigFiles()
+
+    const apiPaths = getPathsFromTypeScriptConfig(
+      typeScriptConfig.api,
+      FAKE_API_ROOT
+    )
+    expect(apiPaths).toMatchInlineSnapshot(`{}`)
+
+    const webPaths = getPathsFromTypeScriptConfig(
+      typeScriptConfig.web,
+      FAKE_WEB_ROOT
+    )
+    expect(webPaths).toMatchInlineSnapshot(`{}`)
+  })
+
+  it('gets and formats paths', () => {
+    const apiTSConfig =
+      '{"compilerOptions":{"baseUrl":"./","paths":{"@services/*":["./src/services/*"]}}}'
+    const webTSConfig =
+      '{"compilerOptions":{"baseUrl":"./","paths":{"@ui/*":["./src/ui/*"]}}}'
+
+    vol.fromNestedJSON(
+      {
+        'redwood.toml': '',
+        api: {
+          'tsconfig.json': apiTSConfig,
+        },
+        web: {
+          'tsconfig.json': webTSConfig,
+        },
+      },
+      redwoodProjectPath
+    )
+
+    const typeScriptConfig = parseTypeScriptConfigFiles()
+
+    const apiPaths = getPathsFromTypeScriptConfig(
+      typeScriptConfig.api,
+      FAKE_API_ROOT
+    )
+
+    expect(ensurePosixPath(apiPaths['@services'])).toEqual(
+      ensurePosixPath('/redwood-app/api/src/services')
+    )
+
+    const webPaths = getPathsFromTypeScriptConfig(
+      typeScriptConfig.web,
+      FAKE_WEB_ROOT
+    )
+    expect(ensurePosixPath(webPaths['@ui'])).toEqual(
+      ensurePosixPath('/redwood-app/web/src/ui')
+    )
   })
 })
