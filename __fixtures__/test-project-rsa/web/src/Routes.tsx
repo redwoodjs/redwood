8f985e945d50b272d5cb409dd9796c37669082cd
Tobbe Lundberg <tobbe@tlundberg.com>
2023-12-01 13:17:17 +0100

    RSC: Update RSA test fixture with routing

--- __fixtures__/test-project-rsa/web/src/Routes.tsx
+++ __fixtures__/test-project-rsa/web/src/Routes.tsx
@@ -12,6 +12,8 @@ import { Router, Route } from '@redwoodjs/router'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/about" page={AboutPage} name="about" />
+      <Route path="/" page={HomePage} name="home" />
       <Route notfound page={NotFoundPage} />
     </Router>
   )
