953653b6bf9db92fff60cdcf25bec8ab5eec4d4a
Dominic Saadi <dominiceliassaadi@gmail.com>
2023-12-13 21:04:42 -0800

    rename extend-expect to jest-globals

--- packages/router/src/__tests__/location.test.tsx
+++ packages/router/src/__tests__/location.test.tsx
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
+import '@testing-library/jest-dom/jest-globals'
 
 import { LocationProvider, useLocation } from '../location'
 
