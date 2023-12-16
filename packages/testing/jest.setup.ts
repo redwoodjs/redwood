5f89c0176f517b894cb2c3f1ab9cee4c7c207393
Tobbe Lundberg <tobbe@tlundberg.com>
2023-12-16 14:43:35 +0100

    Revert "Revert `@testing-library/jest-dom` v6 upgrade (#9713)" (#9719)

--- packages/testing/jest.setup.ts
+++ packages/testing/jest.setup.ts
@@ -1,4 +1,4 @@
-import '@testing-library/jest-dom/extend-expect'
+import '@testing-library/jest-dom/jest-globals'
 import '@testing-library/jest-dom'
 
 import { startMSW } from './src/web/mockRequests'
--- packages/web/src/components/cell/createCell.test.tsx
+++ packages/web/src/components/cell/createCell.test.tsx
@@ -3,7 +3,7 @@
  */
 
 import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
+import '@testing-library/jest-dom/jest-globals'
 
 import { GraphQLHooksProvider } from '../GraphQLHooksProvider'
 
