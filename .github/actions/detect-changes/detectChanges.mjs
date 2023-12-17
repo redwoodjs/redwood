de5fae562aca65cead0d60ab7a174ce63b8f4c7b
Tobbe Lundberg <tobbe@tlundberg.com>
2023-12-17 17:37:29 +0100

    Parse PR number from GITHUB_REF or read GITHUB_EVENT_PATH

--- .github/actions/detect-changes/detectChanges.mjs
+++ .github/actions/detect-changes/detectChanges.mjs
@@ -1,40 +1,46 @@
 import fs from 'node:fs'
 
 import core from '@actions/core'
-import * as github from '@actions/github'
 import { exec, getExecOutput } from '@actions/exec'
 import { onlyDocsChanged } from './cases/onlydocs.mjs'
 import { rscChanged } from './cases/rsc.mjs'
 import { ssrChanged } from './cases/ssr.mjs'
 
+const getPrNumber = (githubRef) => {
+  // Example GITHUB_REF refs/pull/9544/merge
+  const result = /refs\/pull\/(\d+)\/merge/g.exec(process.env.GITHUB_REF)
+
+  let prNumber = result?.[1]
+
+  if (!prNumber) {
+    try {
+      // Example GITHUB_EVENT_PATH
+      // /home/runner/work/_temp/_github_workflow/event.json
+      const ev = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8'))
+      prNumber = ev.pull_request?.number
+    } catch {
+      // fall through
+    }
+  }
+
+  if (!prNumber) {
+    throw new Error('Could not find the PR number')
+  }
+
+  return prNumber
+}
+
 async function getChangedFiles(page = 1) {
+  const prNumber = getPrNumber()
+
   console.log(
     `Getting changed files (${page}) for PR ${process.env.GITHUB_BASE_REF}`
   )
   let changedFiles = []
 
-  const issueNumber = github.context.issue.number
-
-  const ev = JSON.parse(
-    fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8')
-  )
-
-  // TODO: Remove this:
-  console.log('---')
-  console.log('BRANCH', process.env.BRANCH)
-  console.log('HEAD', process.env.HEAD)
-  console.log('COMMIT_REF', process.env.COMMIT_REF)
-  console.log('CACHED_COMMIT_REF', process.env.CACHED_COMMIT_REF)
-  console.log('GITHUB_BASE_REF', process.env.GITHUB_BASE_REF)
-  console.log('GITHUB_EVENT_PATH', process.env.GITHUB_EVENT_PATH)
-  console.log('GITHUB_REF', process.env.GITHUB_REF)
-  console.log('issueNumber', issueNumber)
-  console.log('ev.pull_request.number', ev.pull_request.number)
-  console.log('---')
-
   // Query the GitHub API to get the changed files in the PR
   const githubToken = process.env.GITHUB_TOKEN
-  const url = `https://api.github.com/repos/redwoodjs/redwood/pulls/${process.env.GITHUB_BASE_REF}/files?per_page=1&page=${page}`
+  const url = `https://api.github.com/repos/redwoodjs/redwood/pulls/${prNumber}/files?per_page=1&page=${page}`
   const resp = await fetch(url, {
     headers: {
       Authorization: githubToken ? `Bearer ${githubToken}` : undefined,
@@ -42,6 +48,7 @@ async function getChangedFiles(page = 1) {
       Accept: 'application/vnd.github+json',
     },
   })
+
   console.log({ resp })
   const json = await resp.json()
   const files = json.map((file) => file.filename) || []
