fe0cd3f00ee012e73934f311cd07307a9a929450
Tobbe Lundberg <tobbe@tlundberg.com>
2023-12-19 11:32:33 +0100

    Update templates, and manually tweak test-project

--- __fixtures__/test-project/api/src/functions/auth.ts
+++ __fixtures__/test-project/api/src/functions/auth.ts
@@ -115,7 +115,12 @@ export const handler = async (
     //
     // If this returns anything else, it will be returned by the
     // `signUp()` function in the form of: `{ message: 'String here' }`.
-    handler: ({ username, hashedPassword, salt, userAttributes }) => {
+    handler: ({
+      username,
+      hashedPassword,
+      salt,
+      userAttributes
+    }) => {
       return db.user.create({
         data: {
           email: username,
