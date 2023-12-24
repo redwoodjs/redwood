b74534e369836aad1be97dd5374f7df38e179904
Raphael Hetherington <30865601+raph90@users.noreply.github.com>
2023-12-24 19:46:57 +0000

    #9620: Update studio to support variable components (Mailer) (#9639)
    Reference to #9620. 
    
    Currently Studio supports `export function Welcome()` syntax for emails.
    This PR adds support for `export const Welcome = () => ...`
    
    Also added optional dependencies for SWC to fix an error that _I think_
    is architecture related.
    
    ---------
    
    Co-authored-by: Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>

--- packages/studio/api/services/mail.ts
+++ packages/studio/api/services/mail.ts
@@ -105,9 +105,14 @@ export async function getRenderedMail(
       `.studio_${Date.now()}.js`
     )
     fs.copyFileSync(templateComponentDistPath, templateImportPath)
-    const templateComponent = await import(templateImportPath)
+    const templateComponent = (await import(`file://${templateImportPath}`))
+      .default
     fs.removeSync(templateImportPath)
-    const Component = templateComponent[component.name]
+
+    const Component =
+      component.name.indexOf('default') !== -1
+        ? templateComponent.default
+        : templateComponent[component.name]
 
     // Import the mailer
     const mailerFilePath = path.join(getPaths().api.dist, 'lib', 'mailer.js')
@@ -116,7 +121,7 @@ export async function getRenderedMail(
       `.studio_${Date.now()}.js`
     )
     fs.copyFileSync(mailerFilePath, mailerImportPath)
-    const mailer = (await import(mailerImportPath)).mailer
+    const mailer = (await import(`file://${mailerImportPath}`)).mailer
     fs.removeSync(mailerImportPath)
 
     // Render the component
