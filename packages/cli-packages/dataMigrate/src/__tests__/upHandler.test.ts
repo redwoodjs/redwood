70302b3df8e32d3acce2a022511a9dac47e05a4c
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2024-01-03 12:30:13 +0000

    chore(data-migration): Fix test exit code (#9795)
    Co-authored-by: Tobbe Lundberg <tobbe@tlundberg.com>

--- packages/cli-packages/dataMigrate/src/__tests__/upHandler.test.ts
+++ packages/cli-packages/dataMigrate/src/__tests__/upHandler.test.ts
@@ -249,6 +249,10 @@ describe('upHandler', () => {
       distPath: getPaths().api.dist,
     })
 
+    // The handler will error and set the exit code to 1, we must revert that
+    // or test suite itself will fail.
+    process.exitCode = 0
+
     expect(console.info.mock.calls[0][0]).toMatch(
       '1 data migration(s) completed successfully.'
     )
