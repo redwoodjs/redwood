0e2e7d800a6f420892ee06021003df319c627aae
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2024-01-03 11:32:15 +0000

    reset exit code

--- packages/cli-packages/dataMigrate/src/__tests__/upHandler.test.ts
+++ packages/cli-packages/dataMigrate/src/__tests__/upHandler.test.ts
@@ -248,6 +248,8 @@ describe('upHandler', () => {
       importDbClientFromDist: true,
       distPath: getPaths().api.dist,
     })
+    // The handler will error and set the exit code to 1, we must revert that or test suite itself will fail.
+    process.exitCode = 0
 
     expect(console.info.mock.calls[0][0]).toMatch(
       '1 data migration(s) completed successfully.'
