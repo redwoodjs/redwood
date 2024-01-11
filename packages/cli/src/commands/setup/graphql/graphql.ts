74db5b5cd2d70443495d877a7d756ab7be7e2b5b
Tobbe Lundberg <tobbe@tlundberg.com>
2024-01-11 12:45:17 +0100

    yarn rw setup graphql fragments (#9811)

---
+++ packages/cli/src/commands/setup/graphql/graphql.ts
@@ -0,0 +1,17 @@
+import terminalLink from 'terminal-link'
+import type { Argv } from 'yargs'
+
+import * as fragmentsCommand from './features/fragments/fragments'
+
+export const command = 'graphql <feature>'
+export const description = 'Set up GraphQL feature support'
+export function builder(yargs: Argv) {
+  return yargs
+    .command(fragmentsCommand)
+    .epilogue(
+      `Also see the ${terminalLink(
+        'Redwood CLI Reference',
+        'https://redwoodjs.com/docs/cli-commands#setup-graphql'
+      )}`
+    )
+}
