cb448990732c536370f7ab8ce2168f14906c9c7f
Tobbe Lundberg <tobbe@tlundberg.com>
2024-01-12 09:51:46 +0100

    fragments setup: newline fix + refactor->rename (#9821)

--- packages/cli/src/commands/setup/graphql/features/fragments/fragmentsHandler.ts
+++ packages/cli/src/commands/setup/graphql/features/fragments/fragmentsHandler.ts
@@ -54,7 +54,8 @@ export async function handler({ force }: Args) {
           const hasExistingGraphqlSection = !!redwoodTomlObject?.graphql
 
           let newTomlContent =
-            originalTomlContent + '\n\n[graphql]\n  fragments = true'
+            originalTomlContent.replace(/\n$/, '') +
+            '\n\n[graphql]\n  fragments = true'
 
           if (hasExistingGraphqlSection) {
             const existingGraphqlSetting = Object.keys(
@@ -121,13 +122,13 @@ export async function handler({ force }: Args) {
       {
         title: 'Add possibleTypes to the GraphQL cache config',
         task: async () => {
-          const result = await runTransform({
+          const transformResult = await runTransform({
             transformPath: path.join(__dirname, 'appGqlConfigTransform.js'),
             targetPaths: [getPaths().web.app],
           })
 
-          if (result.error) {
-            throw new Error(result.error)
+          if (transformResult.error) {
+            throw new Error(transformResult.error)
           }
 
           const appPath = getPaths().web.app
