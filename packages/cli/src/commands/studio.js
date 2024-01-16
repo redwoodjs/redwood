1e6a12852f5ff31f9c6a2371addf983fc21ed359
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2024-01-16 17:03:02 +0000

    feat(studio): Switch to newer version of studio (#9799)
    We have rewritten studio and it now lives in its own repository. This PR
    switches out the current implementation to use this new one.

--- packages/cli/src/commands/studio.js
+++ packages/cli/src/commands/studio.js
@@ -1,24 +1,18 @@
 import { recordTelemetryAttributes } from '@redwoodjs/cli-helpers'
 
-import { getEpilogue } from './util'
-
 export const command = 'studio'
 export const description = 'Run the Redwood development studio'
 
-export const EXPERIMENTAL_TOPIC_ID = 4771
-
 export function builder(yargs) {
-  yargs
-    .option('open', {
-      default: true,
-      description: 'Open the studio in your browser',
-    })
-    .epilogue(getEpilogue(command, description, EXPERIMENTAL_TOPIC_ID, true))
+  yargs.option('open', {
+    default: true,
+    description: 'Open the studio in your browser',
+  })
 }
 
 export async function handler(options) {
   recordTelemetryAttributes({
-    command: 'experimental studio',
+    command: 'studio',
     open: options.open,
   })
   const { handler } = await import('./studioHandler.js')
