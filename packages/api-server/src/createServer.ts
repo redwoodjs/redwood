cde8091fabecd22f840eb4663264f3d495fa5731
Dominic Saadi <dominiceliassaadi@gmail.com>
2024-01-20 11:53:16 +0000

    comment graphql options

--- packages/api-server/src/createServer.ts
+++ packages/api-server/src/createServer.ts
@@ -143,6 +143,7 @@ export async function createServer(options: CreateServerOptions = {}) {
 
   if (graphqlFunctionPath) {
     const { redwoodFastifyGraphQLServer } = require('./plugins/graphql')
+    // This comes from a babel plugin that's applied to api/dist/functions/graphql.{ts,js} in user projects
     const { __rw_graphqlOptions } = require(graphqlFunctionPath)
 
     await server.register(redwoodFastifyGraphQLServer, {
