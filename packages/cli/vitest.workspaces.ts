c8277a247d70fcb7715bcce58c0ec11314e054d6
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2024-01-21 20:52:44 +0000

    uncomments

--- packages/cli/vitest.workspaces.ts
+++ packages/cli/vitest.workspaces.ts
@@ -1,31 +1,27 @@
 import { defineWorkspace, configDefaults } from 'vitest/config'
 
 export default defineWorkspace([
-  // {
-  //   extends: './vitest.config.mts',
-  //   test: {
-  //     name: 'root',
-  //     include: ['**/__tests__/**/*.[jt]s?(x)', '**/*.test.[jt]s?(x)'],
-  //     exclude: [
-  //       ...configDefaults.exclude,
-  //       '__fixtures__',
-  //       '__testfixtures__',
-  //       '**/__codemod_tests__',
-  //       '__tests__/utils/*',
-  //       '**/__tests__/fixtures/**/*',
-  //       '.d.ts',
-  //       'dist',
-  //     ],
-  //     // moduleNameMapper: {
-  //     //   '^src/(.*)': '<rootDir>/src/$1',
-  //     // },
-  //     alias: {
-  //       '^src/(.*)': '<rootDir>/src/$1',
-  //     },
-  //     // setupFilesAfterEnv: ['./jest.setup.js'],
-  //     setupFiles: ['./vitest.setup.mts'],
-  //   },
-  // },
+  {
+    extends: './vitest.config.mts',
+    test: {
+      name: 'root',
+      include: ['**/__tests__/**/*.[jt]s?(x)', '**/*.test.[jt]s?(x)'],
+      exclude: [
+        ...configDefaults.exclude,
+        '__fixtures__',
+        '__testfixtures__',
+        '**/__codemod_tests__',
+        '__tests__/utils/*',
+        '**/__tests__/fixtures/**/*',
+        '.d.ts',
+        'dist',
+      ],
+      alias: {
+        '^src/(.*)': '<rootDir>/src/$1',
+      },
+      setupFiles: ['./vitest.setup.mts'],
+    },
+  },
   {
     extends: './vitest.config.mts',
     test: {
