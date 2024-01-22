25595abf635c136e90831bee9875826cc0008401
Dominic Saadi <dominiceliassaadi@gmail.com>
2024-01-22 00:02:46 +0000

    feat: convert to esm

--- .eslintrc.js
+++ .eslintrc.js
@@ -196,7 +196,7 @@ module.exports = {
         'import/no-extraneous-dependencies': [
           'error',
           {
-            devDependencies: false,
+            devDependencies: true,
             optionalDependencies: false,
             peerDependencies: true,
           },
---
+++ packages/project-config/build.js
@@ -0,0 +1,26 @@
+import * as esbuild from 'esbuild'
+
+const optiions = {
+  entryPoints: ['./src/index.ts'],
+  outdir: 'dist',
+
+  platform: 'node',
+  target: ['node20'],
+  bundle: true,
+  packages: 'external',
+
+  logLevel: 'info',
+  metafile: true,
+}
+
+await esbuild.build({
+  ...optiions,
+  format: 'esm',
+  outExtension: { '.js': '.mjs' },
+})
+
+await esbuild.build({
+  ...optiions,
+  format: 'cjs',
+  outExtension: { '.js': '.cjs' },
+})
