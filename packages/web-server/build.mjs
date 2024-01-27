9cb42cc158c249e4fd29fd65cd09d7a484fe4518
Dominic Saadi <dominiceliassaadi@gmail.com>
2024-01-27 10:29:12 +0000

    check for bin entry

--- packages/web-server/build.mjs
+++ packages/web-server/build.mjs
@@ -1,8 +1,27 @@
+import fs from 'fs-extra'
+
 import {
   build,
   defaultBuildOptions,
   defaultIgnorePatterns,
 } from '../../buildDefaults.mjs'
+
+const { bin } = await fs.readJSON('./package.json')
+
+// This package uses the name of the bin as `scriptName` for Yargs to keep things in sync.
+// There should only be one bin entry for this to work.
+// Otherwise we have to rethink the code.
+const bins = Object.keys(bin).length
+
+if (bins !== 1) {
+  console.error(
+    [
+      `Error: Expected exactly one bin entry; found ${bins}`,
+      './packages/web-server/src/bin.ts uses the bin entry as its scriptName',
+    ].join('\n')
+  )
+  process.exit(1)
+}
 
 // Build the package
 await build({
--- packages/web-server/src/bin.ts
+++ packages/web-server/src/bin.ts
@@ -6,8 +6,6 @@ import { bin } from '../package.json'
 import { description, builder } from './cliConfig'
 import { handler } from './cliConfigHandler'
 
-// Use the name of the bin as `scriptName` for Yargs to keep things in sync.
-// Obviously the way it's written it depends on there being only one bin
 const [scriptName] = Object.keys(bin)
 
 yargs(hideBin(process.argv))
