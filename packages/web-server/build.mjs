f36455f49170f7a81eef1520c427c8369e45f9ce
Dominic Saadi <dominiceliassaadi@gmail.com>
2024-01-27 00:24:53 +0000

    build bin separately

--- packages/web-server/build.mjs
+++ packages/web-server/build.mjs
@@ -1,3 +1,24 @@
-import { build } from '../../buildDefaults.mjs'
+import {
+  build,
+  defaultBuildOptions,
+  defaultIgnorePatterns,
+} from '../../buildDefaults.mjs'
 
-await build()
+await build({
+  entryPointOptions: {
+    ignore: [...defaultIgnorePatterns, './src/bin.ts'],
+  },
+})
+
+await build({
+  buildOptions: {
+    ...defaultBuildOptions,
+    banner: {
+      js: '#!/usr/bin/env node',
+    },
+    bundle: true,
+    entryPoints: ['./src/bin.ts'],
+    packages: 'external',
+  },
+  metafileName: 'meta.bin.json',
+})
