b3219ba5d60885a1736bcd885f60d60ad2affa49
Tobbe Lundberg <tobbe@tlundberg.com>
2024-01-27 18:41:33 +0100

    fix(gql): Add back gql global type (#9888)

--- packages/web/src/global.web-auto-imports.ts
+++ packages/web/src/global.web-auto-imports.ts
@@ -1,13 +1,21 @@
 import type _React from 'react'
 
-import type _gql from 'graphql-tag'
+import type { DocumentNode } from 'graphql'
 
 // These are the global types exposed to a user's project
 // For "internal" global types see ambient.d.ts
 
 declare global {
-  const React: typeof _React
-  // const gql: typeof _gql
+  // This type is used for both regular RW projects and projects that have
+  // enabled Trusted Documents. For regular RW projects, this could have been
+  // typed just by importing gql from `graphql-tag`. But for Trusted Documents
+  // the type should be imported from `web/src/graphql/gql` in the user's
+  // project. The type here is generic enough to cover both cases.
+  const gql: (source: string | TemplateStringsArray) => DocumentNode
+
+  // Having this as a type instead of a const allows us to augment/override it
+  // in other packages
+  type React = typeof _React
 
   interface Window {
     /** URL or absolute path to the GraphQL serverless function */
