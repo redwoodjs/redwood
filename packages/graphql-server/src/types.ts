bbc9bd2f6033b65e8ab6c350710fe6ddfa4dc00f
Daniel Choudhury <dannychoudhury@gmail.com>
2024-02-01 03:29:21 +0700

    chore(ts): Remove src alias and baseUrl from all tsconfigs (#9944)

--- packages/graphql-server/src/types.ts
+++ packages/graphql-server/src/types.ts
@@ -13,8 +13,7 @@ import type { AuthContextPayload, Decoder } from '@redwoodjs/api'
 import type { CorsConfig } from '@redwoodjs/api'
 import type { RedwoodRealtimeOptions } from '@redwoodjs/realtime'
 
-import type { DirectiveGlobImports } from 'src/directives/makeDirectives'
-
+import type { DirectiveGlobImports } from './directives/makeDirectives'
 import type {
   useRedwoodDirectiveReturn,
   DirectivePluginOptions,
