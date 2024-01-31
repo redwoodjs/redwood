dde957f8e1726f4a071e8f35c682db68ec4d58cc
Tobbe Lundberg <tobbe@tlundberg.com>
2024-01-31 17:10:56 +0100

    trim build script

--- packages/framework-tools/build.ts
+++ packages/framework-tools/build.ts
@@ -1,16 +1,11 @@
 import { build, defaultBuildOptions } from './src/buildDefaults'
 
-const options = {
-  ...defaultBuildOptions,
-  bundle: true,
-  entryPoints: ['./src/buildDefaults.ts'],
-  packages: 'external',
-}
-
 // ESM build.
 await build({
   buildOptions: {
-    ...options,
+    ...defaultBuildOptions,
+    entryPoints: ['./src/buildDefaults.ts'],
+    packages: 'external',
     format: 'esm',
     outExtension: { '.js': '.mjs' },
   },
