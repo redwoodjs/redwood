bfa27fc82db916826b8f2f75da68c86a899a0594
Tobbe Lundberg <tobbe@tlundberg.com>
2024-02-02 15:21:35 +0100

    Update url

--- packages/codemods/src/codemods/v7.x.x/updateGraphQLConfig/updateGraphqlConfig.ts
+++ packages/codemods/src/codemods/v7.x.x/updateGraphQLConfig/updateGraphqlConfig.ts
@@ -7,7 +7,10 @@ import { getPaths } from '@redwoodjs/project-config'
 
 export const updateGraphqlConfig = async () => {
   const res = await fetch(
-    'https://raw.githubusercontent.com/redwoodjs/redwood/release/major/v7.0.0/packages/create-redwood-app/templates/ts/graphql.config.js'
+    // TODO: Have to come back here to update the URL when we have a more
+    // stable location than main
+    // 'https://raw.githubusercontent.com/redwoodjs/redwood/release/major/v7.0.0/packages/create-redwood-app/templates/ts/graphql.config.js'
+    'https://raw.githubusercontent.com/redwoodjs/redwood/main/packages/create-redwood-app/templates/ts/graphql.config.js'
   )
   const text = await res.text()
   fs.writeFileSync(path.join(getPaths().base, 'graphql.config.js'), text)
