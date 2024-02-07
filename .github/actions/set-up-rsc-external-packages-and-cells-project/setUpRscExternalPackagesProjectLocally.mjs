e316136d75bc695b691dce5219443361697ca0d9
Tobbe Lundberg <tobbe@tlundberg.com>
2024-02-07 15:09:59 +0100

    Fix some names

--- .github/actions/set-up-rsc-external-packages-and-cells-project/setUpRscExternalPackagesProjectLocally.mjs
+++ .github/actions/set-up-rsc-external-packages-and-cells-project/setUpRscExternalPackagesProjectLocally.mjs
@@ -71,7 +71,7 @@ function getExecaOptions(cwd, env = {}) {
   }
 }
 
-const testProjectAndFixtureName = 'test-project-rsc-external-packages'
+const testProjectAndFixtureName = 'test-project-rsc-external-packages-and-cells'
 
 const testProjectPath = path.join(
   os.tmpdir(),
