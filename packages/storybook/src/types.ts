cf893bff1270ef206cd306b1b7c77f7837195fdc
Ari Mendelow <ari@mendelow.me>
2024-02-24 14:04:40 -0800

    Fix code style, mostly

--- packages/storybook/src/types.ts
+++ packages/storybook/src/types.ts
@@ -1,16 +1,19 @@
+import type docgenTypescript from '@joshwooding/vite-plugin-react-docgen-typescript'
+import type {
+  StorybookConfigVite,
+  BuilderOptions,
+} from '@storybook/builder-vite'
 import type {
   StorybookConfig as StorybookConfigBase,
   TypescriptOptions as TypescriptOptionsBase,
-} from '@storybook/types';
-import type { StorybookConfigVite, BuilderOptions } from '@storybook/builder-vite';
-import type docgenTypescript from '@joshwooding/vite-plugin-react-docgen-typescript';
+} from '@storybook/types'
 
-type FrameworkName = '@storybook/react-vite';
-type BuilderName = '@storybook/builder-vite';
+type FrameworkName = '@storybook/react-vite'
+type BuilderName = '@storybook/builder-vite'
 
 export type FrameworkOptions = {
-  builder?: BuilderOptions;
-  strictMode?: boolean;
+  builder?: BuilderOptions
+  strictMode?: boolean
   /**
    * Use React's legacy root API to mount components
    * @description
@@ -18,25 +21,25 @@ export type FrameworkOptions = {
    * If this flag is true, the legacy Root API is used to mount components to make it easier to migrate step by step to React 18.
    * @default false
    */
-  legacyRootApi?: boolean;
-};
+  legacyRootApi?: boolean
+}
 
 type StorybookConfigFramework = {
   framework:
     | FrameworkName
     | {
-        name: FrameworkName;
-        options: FrameworkOptions;
-      };
+        name: FrameworkName
+        options: FrameworkOptions
+      }
   core?: StorybookConfigBase['core'] & {
     builder?:
       | BuilderName
       | {
-          name: BuilderName;
-          options: BuilderOptions;
-        };
-  };
-};
+          name: BuilderName
+          options: BuilderOptions
+        }
+  }
+}
 
 type TypescriptOptions = TypescriptOptionsBase & {
   /**
@@ -44,12 +47,12 @@ type TypescriptOptions = TypescriptOptionsBase & {
    *
    * @default `'react-docgen'`
    */
-  reactDocgen: 'react-docgen-typescript' | 'react-docgen' | false;
+  reactDocgen: 'react-docgen-typescript' | 'react-docgen' | false
   /**
    * Configures `@joshwooding/vite-plugin-react-docgen-typescript`
    */
-  reactDocgenTypescriptOptions: Parameters<typeof docgenTypescript>[0];
-};
+  reactDocgenTypescriptOptions: Parameters<typeof docgenTypescript>[0]
+}
 
 /**
  * The interface for Storybook configuration in `main.ts` files.
@@ -60,5 +63,5 @@ export type StorybookConfig = Omit<
 > &
   StorybookConfigVite &
   StorybookConfigFramework & {
-    typescript?: Partial<TypescriptOptions>;
-  };
+    typescript?: Partial<TypescriptOptions>
+  }
