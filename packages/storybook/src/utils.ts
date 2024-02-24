4d4e469204ba5a36261f6f301c9f92624a8a9c98
Ari Mendelow <ari@mendelow.me>
2024-02-24 15:20:13 -0800

    Format code

--- packages/storybook/src/utils.ts
+++ packages/storybook/src/utils.ts
@@ -1,12 +1,12 @@
-import path from 'path';
-import fs from 'fs';
+import fs from 'fs'
+import path from 'path'
 
 export function readPackageJson(): Record<string, any> | false {
-  const packageJsonPath = path.resolve('package.json');
+  const packageJsonPath = path.resolve('package.json')
   if (!fs.existsSync(packageJsonPath)) {
-    return false;
+    return false
   }
 
-  const jsonContent = fs.readFileSync(packageJsonPath, 'utf8');
-  return JSON.parse(jsonContent);
+  const jsonContent = fs.readFileSync(packageJsonPath, 'utf8')
+  return JSON.parse(jsonContent)
 }
