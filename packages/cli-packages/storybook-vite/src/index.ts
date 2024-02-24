5c45541e501a180ab223891202b07a40b57a9093
Ari Mendelow <ari@mendelow.me>
2024-02-24 11:14:40 -0800

    Copy storybook cli package

---
+++ packages/cli-packages/storybook-vite/src/index.ts
@@ -0,0 +1,17 @@
+import {
+  command,
+  aliases,
+  description,
+  builder,
+  handler,
+} from './commands/storybook'
+
+export const commands = [
+  {
+    command,
+    aliases,
+    description,
+    builder,
+    handler,
+  },
+]
