03ade584add1a13713f543ec878aa0ac093914fa
Ari Mendelow <ari@mendelow.me>
2024-02-24 19:46:26 -0800

    Copy in all mocks

---
+++ packages/storybook/src/__mocks__/MockProviders.tsx
@@ -0,0 +1,35 @@
+/**
+ * NOTE: This module should not contain any nodejs functionality,
+ * because it's also used by Storybook in the browser.
+ */
+import React from 'react'
+
+// @ts-expect-error - We inject useAuth when testing, so it will be available
+import { useAuth } from '@redwoodjs/auth'
+import { LocationProvider } from '@redwoodjs/router'
+import { RedwoodProvider } from '@redwoodjs/web'
+import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
+
+import { MockParamsProvider } from './MockParamsProvider'
+
+// Import the user's Router from `./web/src/Router.{tsx,jsx}`,
+// we pass the `children` from the user's Router to `./MockRouter.Router`
+// so that we can populate the `routes object` in Storybook and tests.
+// @ts-expect-error - this comes from a Vite alias in main.ts
+import UserRouterWithRoutes from '~__REDWOOD__USER_ROUTES_FOR_MOCK'
+
+// TODO(pc): see if there are props we want to allow to be passed into our mock provider (e.g. AuthProviderProps)
+export const MockProviders: React.FunctionComponent<{
+  children: React.ReactNode
+}> = ({ children }) => {
+  return (
+    <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
+      <RedwoodApolloProvider useAuth={useAuth}>
+        <UserRouterWithRoutes />
+        <LocationProvider>
+          <MockParamsProvider>{children}</MockParamsProvider>
+        </LocationProvider>
+      </RedwoodApolloProvider>
+    </RedwoodProvider>
+  )
+}
---
+++ packages/storybook/src/__mocks__/MockRouter.tsx
@@ -0,0 +1,71 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import type React from 'react'
+
+// Bypass the `main` field in `package.json` because we alias `@redwoodjs/router`
+// for jest and Storybook. Not doing so would cause an infinite loop.
+// See: ./packages/testing/config/jest/web/jest-preset.js
+// @ts-ignore
+import type { RouterProps } from '@redwoodjs/router/dist/router'
+import { isRoute } from '@redwoodjs/router/dist/router'
+import { flattenAll, replaceParams } from '@redwoodjs/router/dist/util'
+
+// the compiler didn't like the `import * from '...'` syntax so we have to import each thing individually
+export {
+  navigate,
+  back,
+  NavLink,
+  Link,
+  useLocation,
+  LocationProvider,
+  Redirect,
+  usePageLoadingContext,
+  PageLoadingContextProvider,
+  useParams,
+  ParamsProvider,
+  ParamsContext,
+  Route,
+  WrapperType,
+  Set,
+  Private,
+  PrivateSet,
+  isSetNode,
+  isPrivateSetNode,
+  isPrivateNode,
+  RouteAnnouncement,
+  RouteAnnouncementProps,
+  RouteFocus,
+  RouteFocusProps,
+  useRouteName,
+  useRoutePaths,
+  useRoutePath,
+  useMatch,
+  parseSearch,
+  getRouteRegexAndParams,
+  matchPath,
+  SkipNavLink,
+  SkipNavContent,
+} from '@redwoodjs/router/dist/index'
+
+export const routes: { [routeName: string]: () => string } = {}
+
+/**
+ * We overwrite the default `Router` export (see jest-preset). So every import
+ * of @redwoodjs/router will import this Router instead
+ *
+ * This router populates the `routes.<pageName>()` utility object.
+ */
+export const Router: React.FC<RouterProps> = ({ children }) => {
+  const flatChildArray = flattenAll(children)
+
+  flatChildArray.forEach((child) => {
+    if (isRoute(child)) {
+      const { name, path } = child.props
+
+      if (name && path) {
+        routes[name] = (args = {}) => replaceParams(path, args)
+      }
+    }
+  })
+
+  return null
+}
