e126c75d36064d6d62b2b24684145a4a460972c6
Tobbe Lundberg <tobbe@tlundberg.com>
2024-03-04 11:53:47 +0100

    chore(comment): cli index FIXME comment about ugly big red box

--- packages/cli/src/index.js
+++ packages/cli/src/index.js
@@ -128,6 +128,8 @@ async function main() {
       recordTelemetryAttributes({ command: '--help' })
     }
 
+    // FIXME: There's currently a BIG RED BOX on exiting feServer
+    // Is yargs or the RW cli not passing SigInt on to the child process?
     try {
       // Run the command via yargs
       await runYargs()
