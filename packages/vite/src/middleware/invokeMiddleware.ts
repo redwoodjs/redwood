05e33d2664dee9e415afa1defeaf29c1d7e095b1
Daniel Choudhury <dannychoudhury@gmail.com>
2024-03-06 14:09:18 +0700

    Update invokeMiddleware comments

--- packages/vite/src/middleware/invokeMiddleware.ts
+++ packages/vite/src/middleware/invokeMiddleware.ts
@@ -35,14 +35,18 @@ export const invoke = async (
       (await middleware(mwReq, MiddlewareResponse.next(), options)) ||
       MiddlewareResponse.next()
 
+    // Error out early, incase user returns something else from the middleware
+    // returning nothing is still fine!
     if (output instanceof MiddlewareResponse) {
       mwRes = output
     } else if (typeof output === 'object' && output instanceof Response) {
       // If it was a WebAPI Response
       mwRes = MiddlewareResponse.fromResponse(output)
     } else {
+      console.error('Return from middleware >> ', output)
+      console.error('\n----\n')
       throw new Error(
-        'Middleware must return a MiddlewareResponse or a Response'
+        'Invalid return type from middleware. You must return a MiddlewareResponse or a Response object, or nothing at all'
       )
     }
   } catch (e) {
