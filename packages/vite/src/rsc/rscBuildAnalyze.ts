80065ef00b31c2aa261d559bcfb2ba62c75d4ef5
Tobbe Lundberg <tobbe@tlundberg.com>
2024-03-09 13:57:24 +0100

    RSC: Move vite plugins into their own files

--- packages/vite/src/rsc/rscBuildAnalyze.ts
+++ packages/vite/src/rsc/rscBuildAnalyze.ts
@@ -4,7 +4,7 @@ import { getPaths } from '@redwoodjs/project-config'
 
 import { onWarn } from '../lib/onWarn.js'
 
-import { rscAnalyzePlugin } from './rscVitePlugins.js'
+import { rscAnalyzePlugin } from '../plugins/vite-plugin-rsc-analyze.js'
 
 /**
  * RSC build. Step 1.
