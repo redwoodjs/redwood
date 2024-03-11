dece4d9606deb28631737fe0c2f290ee9f731ccb
Dominic Saadi <dominiceliassaadi@gmail.com>
2024-03-11 10:53:16 +0100

    chore(rsc): add smoke test for rsc dev

---
+++ tasks/smoke-tests/rsc-dev/tests/rsc.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test'
+
+test('Setting up RSC should give you a test project with a client side counter component', async ({
+  page,
+}) => {
+  await page.goto('/')
+
+  const h3 = await page.locator('h3').first().innerHTML()
+  expect(h3).toMatch(/This is a server component/)
+  await page.locator('p').filter({ hasText: 'Count: 0' }).first().isVisible()
+
+  await page.locator('button').filter({ hasText: 'Increment' }).click()
+
+  const count = await page.locator('p').first().innerText()
+  expect(count).toMatch('Count: 1')
+
+  page.close()
+})
+
+test('RWJS_* env vars', async ({ page }) => {
+  await page.goto('/about')
+
+  await expect(page.getByText('RSC on client: enabled')).toBeVisible()
+  await expect(page.getByText('RSC on server: disabled')).toBeVisible()
+
+  page.close()
+})
