e01c63ee9b403bba5e186eebc7cbc89e14895597
Rob Cameron <cannikin@fastmail.com>
2024-03-12 01:08:17 -0700

    Adds an existential check in case no query vars are present in dbAuth invocation (#10204)

--- packages/auth-providers/dbAuth/api/src/DbAuthHandler.ts
+++ packages/auth-providers/dbAuth/api/src/DbAuthHandler.ts
@@ -1455,7 +1455,7 @@ export class DbAuthHandler<
   // figure out which auth method we're trying to call
   async _getAuthMethod() {
     // try getting it from the query string, /.redwood/functions/auth?method=[methodName]
-    let methodName = this.normalizedRequest.query.method as AuthMethodNames
+    let methodName = this.normalizedRequest.query?.method as AuthMethodNames
 
     if (
       !DbAuthHandler.METHODS.includes(methodName) &&
