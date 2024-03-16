3f05e4f51c258a33e0f6adb7d05abd357b82ff63
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2024-03-16 12:50:35 +0100

    reenable font style test

--- tasks/smoke-tests/rsc-external-packages-and-cells/tests/rsc-external-packages-and-cells.spec.ts
+++ tasks/smoke-tests/rsc-external-packages-and-cells/tests/rsc-external-packages-and-cells.spec.ts
@@ -27,12 +27,11 @@ test('CSS has been loaded', async ({ page }) => {
   // rgb(255, 165, 0) is orange
   expect(clientH3Color).toBe('rgb(255, 165, 0)')
 
-  // TODO: Reenable this test once we can easily debug windows behavior
   // Check font style of client component h3
-  // const clientH3Font = await clientH3.evaluate((el) => {
-  //   return window.getComputedStyle(el).getPropertyValue('font-style')
-  // })
-  // expect(clientH3Font).toBe('italic')
+  const clientH3Font = await clientH3.evaluate((el) => {
+    return window.getComputedStyle(el).getPropertyValue('font-style')
+  })
+  expect(clientH3Font).toBe('italic')
 
   page.close()
 })
