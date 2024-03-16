151cce85716f9753a7b59b347ba6aea1c48255f7
Tobbe Lundberg <tobbe@tlundberg.com>
2024-03-16 21:13:11 +0100

    more shim stuff

--- packages/vite/src/rsc/rscWebpackShims.ts
+++ packages/vite/src/rsc/rscWebpackShims.ts
@@ -1,11 +1,11 @@
-export const rscWebpackShims = `globalThis.__rw_module_cache__ = new Map();
+export const rscWebpackShims = `globalThis.__rw_module_cache__ ||= new Map();
 
-globalThis.__webpack_chunk_load__ = (id) => {
+globalThis.__webpack_chunk_load__ ||= (id) => {
   console.log('rscWebpackShims chunk load id', id)
   return import(id).then((m) => globalThis.__rw_module_cache__.set(id, m))
 };
 
-globalThis.__webpack_require__ = (id) => {
+globalThis.__webpack_require__ ||= (id) => {
   console.log('rscWebpackShims require id', id)
   return globalThis.__rw_module_cache__.get(id)
 };\n`
