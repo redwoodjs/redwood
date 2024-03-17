a30af7896c55df9aadfcc8ae6a79f97d51e65175
Tobbe Lundberg <tobbe@tlundberg.com>
2024-03-17 10:42:45 +0100

    buildForStreamingServer: inline plugins

--- packages/vite/src/streaming/buildForStreamingServer.ts
+++ packages/vite/src/streaming/buildForStreamingServer.ts
@@ -1,12 +1,9 @@
 import react from '@vitejs/plugin-react'
-import type { PluginOption } from 'vite'
 import { build as viteBuild } from 'vite'
 import { cjsInterop } from 'vite-plugin-cjs-interop'
 
 import { RedwoodRoutesAutoLoaderRscServerPlugin } from '@redwoodjs/babel-config'
 import { getPaths } from '@redwoodjs/project-config'
-
-import { rscTransformEntryPlugin } from '../plugins/vite-plugin-rsc-transform-entry'
 
 export async function buildForStreamingServer({
   verbose = false,
@@ -22,30 +19,22 @@ export async function buildForStreamingServer({
     throw new Error('Vite config not found')
   }
 
-  const plugins: PluginOption[] = [
-    cjsInterop({
-      dependencies: ['@redwoodjs/**'],
-    }),
-  ]
-
-  // Add additional plugins when this is for an RSC build
-  if (rscEnabled) {
-    plugins.push(rscTransformEntryPlugin())
-    plugins.push(
-      react({
-        babel: {
-          only: [/Routes.(js|tsx|jsx)$/],
-          plugins: [[RedwoodRoutesAutoLoaderRscServerPlugin, {}]],
-          babelrc: false,
-          ignore: ['node_modules'],
-        },
-      }),
-    )
-  }
-
   await viteBuild({
     configFile: rwPaths.web.viteConfig,
-    plugins,
+    plugins: [
+      cjsInterop({
+        dependencies: ['@redwoodjs/**'],
+      }),
+      rscEnabled &&
+        react({
+          babel: {
+            only: [/Routes.(js|tsx|jsx)$/],
+            plugins: [[RedwoodRoutesAutoLoaderRscServerPlugin, {}]],
+            babelrc: false,
+            ignore: ['node_modules'],
+          },
+        }),
+    ],
     build: {
       // TODO (RSC): Remove `minify: false` when we don't need to debug as often
       minify: false,
