c1f6ec1da4ed2e3e7fedb0466b97ed8a6c9b4cab
Tobbe Lundberg <tobbe@tlundberg.com>
2024-03-17 09:35:26 +0100

    Silence vite warning about import

--- packages/vite/src/plugins/vite-plugin-rsc-transform-entry.ts
+++ packages/vite/src/plugins/vite-plugin-rsc-transform-entry.ts
@@ -11,7 +11,7 @@ globalThis.__rw_module_cache__ ||= new Map();
 
 globalThis.__webpack_chunk_load__ ||= (id) => {
   console.log('rscWebpackShims chunk load id', id)
-  return import(id).then((m) => globalThis.__rw_module_cache__.set(id, m))
+  return import(/* @vite-ignore */ id).then((m) => globalThis.__rw_module_cache__.set(id, m))
 };
 
 globalThis.__webpack_require__ ||= (id) => {
