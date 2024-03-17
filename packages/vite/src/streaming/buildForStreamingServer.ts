fd245b375bc155208e03ad9b06b5007e4c7d7f37
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2024-03-17 13:50:34 +0100

    fixes

--- packages/vite/src/streaming/buildForStreamingServer.ts
+++ packages/vite/src/streaming/buildForStreamingServer.ts
@@ -7,8 +7,10 @@ import { getPaths } from '@redwoodjs/project-config'
 
 export async function buildForStreamingServer({
   verbose = false,
+  rscEnabled = false,
 }: {
   verbose?: boolean
+  rscEnabled?: boolean
 }) {
   console.log('Starting streaming server build...\n')
   const rwPaths = getPaths()
@@ -23,14 +25,15 @@ export async function buildForStreamingServer({
       cjsInterop({
         dependencies: ['@redwoodjs/**'],
       }),
-      react({
-        babel: {
-          only: [/Routes.(js|tsx|jsx)$/],
-          plugins: [[RedwoodRoutesAutoLoaderRscServerPlugin, {}]],
-          babelrc: false,
-          ignore: ['node_modules'],
-        },
-      }),
+      rscEnabled &&
+        react({
+          babel: {
+            only: [/Routes.(js|tsx|jsx)$/],
+            plugins: [[RedwoodRoutesAutoLoaderRscServerPlugin, {}]],
+            babelrc: false,
+            ignore: ['node_modules'],
+          },
+        }),
     ],
     build: {
       // TODO (RSC): Remove `minify: false` when we don't need to debug as often
