d25a9828d83a563bf537147a729459bbbee8935f
Dominic Saadi <dominiceliassaadi@gmail.com>
2024-03-17 16:08:21 +0100

    refactor out file

--- packages/vite/src/plugins/vite-plugin-rsc-transform-entry.ts
+++ packages/vite/src/plugins/vite-plugin-rsc-transform-entry.ts
@@ -2,7 +2,19 @@ import { normalizePath, type Plugin } from 'vite'
 
 import { getPaths } from '@redwoodjs/project-config'
 
-import { rscWebpackShims } from '../lib/rscWebpackShims.js'
+const rscWebpackShims = `\
+globalThis.__rw_module_cache__ ||= new Map();
+
+globalThis.__webpack_chunk_load__ ||= (id) => {
+  console.log('rscWebpackShims chunk load id', id)
+  return import(id).then((m) => globalThis.__rw_module_cache__.set(id, m))
+};
+
+globalThis.__webpack_require__ ||= (id) => {
+  console.log('rscWebpackShims require id', id)
+  return globalThis.__rw_module_cache__.get(id)
+};
+`
 
 export function rscTransformEntryPlugin(): Plugin {
   const entryServerPath = normalizePath(getPaths().web.entryServer || '')
