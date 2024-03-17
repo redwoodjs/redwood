137007efbd186bb8171c3331bfd5872bbb0a4cc0
Dominic Saadi <dominiceliassaadi@gmail.com>
2024-03-17 14:01:34 +0100

    temp: get rsc dev passing

--- packages/vite/src/devFeServer.ts
+++ packages/vite/src/devFeServer.ts
@@ -19,6 +19,20 @@ import { ensureProcessDirWeb } from './utils.js'
 // TODO (STREAMING) Just so it doesn't error out. Not sure how to handle this.
 globalThis.__REDWOOD__PRERENDER_PAGES = {}
 
+const rscWebpackShims = `\
+globalThis.__rw_module_cache__ ||= new Map();
+
+globalThis.__webpack_chunk_load__ ||= (id) => {
+  console.log('rscWebpackShims chunk load id', id)
+  return import(id).then((m) => globalThis.__rw_module_cache__.set(id, m))
+};
+
+globalThis.__webpack_require__ ||= (id) => {
+  console.log('rscWebpackShims require id', id)
+  return globalThis.__rw_module_cache__.get(id)
+};
+`
+
 async function createServer() {
   ensureProcessDirWeb()
 
@@ -60,6 +74,16 @@ async function createServer() {
     logLevel: 'info',
     clearScreen: false,
     appType: 'custom',
+    esbuild: {
+      banner: rscWebpackShims,
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        banner: {
+          js: rscWebpackShims,
+        },
+      },
+    },
   })
 
   // use vite's connect instance as middleware
