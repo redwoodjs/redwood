73e2df0e6372623735cf7e12fb47cb83b30dbe5b
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2024-03-19 12:32:41 +0100

    bump

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -2,7 +2,8 @@ import { processEnvComputedRule } from './process-env-computed.js'
 import { serviceTypeAnnotations } from './service-type-annotations.js'
 import { unsupportedRouteComponents } from './unsupported-route-components.js'
 
-export const rules = {
+// TODO: Use a better type than 'any' for the value of this record
+export const rules: Record<string, any> = {
   'process-env-computed': processEnvComputedRule,
   'service-type-annotations': serviceTypeAnnotations,
   'unsupported-route-components': unsupportedRouteComponents,
