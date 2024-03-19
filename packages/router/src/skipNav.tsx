a9769a0e09031cb9f99f8102a107f0fd49949ca2
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2024-03-19 12:33:09 +0100

    use 'object' over '{}'

--- packages/router/src/skipNav.tsx
+++ packages/router/src/skipNav.tsx
@@ -9,8 +9,9 @@ import * as React from 'react'
 
 // Original Code Source @reach/polymorphic
 // https://github.com/reach/reach-ui/blob/dev/packages/polymorphic/src/reach-polymorphic.ts
+// We updated three instances of type "{}" to "object" to avoid linting errors
 
-type Merge<P1 = {}, P2 = {}> = Omit<P1, keyof P2> & P2
+type Merge<P1 = object, P2 = object> = Omit<P1, keyof P2> & P2
 
 type ForwardRefExoticComponent<E, OwnProps> = React.ForwardRefExoticComponent<
   Merge<
@@ -21,7 +22,7 @@ type ForwardRefExoticComponent<E, OwnProps> = React.ForwardRefExoticComponent<
 
 interface ForwardRefComponent<
   IntrinsicElementString,
-  OwnProps = {},
+  OwnProps = object,
   /*
    * Extends original type to ensure built in React types play nice with
    * polymorphic components still e.g. `React.ElementRef` etc.
