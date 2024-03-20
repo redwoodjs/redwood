b78c24eb619b0a9ec1c7bce9090723049f956572
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2024-03-20 20:30:21 +0000

    use a better type than any

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -1,9 +1,13 @@
+import type { ESLintUtils } from '@typescript-eslint/utils'
+
 import { processEnvComputedRule } from './process-env-computed.js'
 import { serviceTypeAnnotations } from './service-type-annotations.js'
 import { unsupportedRouteComponents } from './unsupported-route-components.js'
 
-// TODO: Use a better type than 'any' for the value of this record
-export const rules: Record<string, any> = {
+export const rules: Record<
+  string,
+  ESLintUtils.RuleModule<string, never[], ESLintUtils.RuleListener>
+> = {
   'process-env-computed': processEnvComputedRule,
   'service-type-annotations': serviceTypeAnnotations,
   'unsupported-route-components': unsupportedRouteComponents,
