82916ef6a79015a14b359ab927e33e8ff277e5db
Tobbe Lundberg <tobbe@tlundberg.com>
2024-03-24 10:16:21 +0100

    Infer error type

--- packages/vite/src/streaming/createReactStreamingHandler.ts
+++ packages/vite/src/streaming/createReactStreamingHandler.ts
@@ -145,7 +145,7 @@ export const createReactStreamingHandler = async (
       },
       {
         waitForAllReady: isSeoCrawler,
-        onError: (err: Error) => {
+        onError: (err) => {
           if (!isProd && viteDevServer) {
             viteDevServer.ssrFixStacktrace(err)
           }
