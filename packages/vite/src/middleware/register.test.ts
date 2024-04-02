79266a226685abbbe80e76e552302e31849bc1eb
Daniel Choudhury <dannychoudhury@gmail.com>
2024-04-02 15:18:08 +0700

    Add tests for chaining and using find-my-way

--- packages/vite/src/middleware/register.test.ts
+++ packages/vite/src/middleware/register.test.ts
@@ -1,12 +1,25 @@
+import fmw from 'find-my-way'
 import { describe, expect, it, vitest } from 'vitest'
 
 import { MiddlewareRequest } from './MiddlewareRequest'
 import { MiddlewareResponse } from './MiddlewareResponse'
 import type { MiddlewareReg } from './register'
-import { groupByRoutePatterns } from './register'
+import { addMiddlewareHandlers, chain, groupByRoutePatterns } from './register'
 import type { Middleware, MiddlewareClass } from './types'
 
 const fakeMiddleware: Middleware = vitest.fn()
+
+class FakeClassMw implements MiddlewareClass {
+  value: number
+  constructor(value: number) {
+    this.value = value
+  }
+  async invoke(req, res) {
+    res.body = 'MW initialized with ' + this.value
+    res.headers.set('class-mw-value', this.value.toString())
+    return res
+  }
+}
 
 describe('groupByRoutePatterns', () => {
   it('should group middleware by default *', () => {
@@ -51,19 +64,9 @@ describe('groupByRoutePatterns', () => {
   })
 
   it('Handles class based middleware', async () => {
-    class FakeMiddleware implements MiddlewareClass {
-      value: number
-      constructor(value: number) {
-        this.value = value
-      }
-      async invoke() {
-        return new MiddlewareResponse('MW initialized with ' + this.value)
-      }
-    }
-
     const classBased: MiddlewareReg = [
-      new FakeMiddleware(1),
-      [new FakeMiddleware(2), '/second-path'],
+      new FakeClassMw(1),
+      [new FakeClassMw(2), '/second-path'],
     ]
 
     const output = groupByRoutePatterns(classBased)
@@ -72,10 +75,70 @@ describe('groupByRoutePatterns', () => {
       new Request('https://example.com'),
     )
 
-    const firstOutput = await output['*'][0]?.(exampleRequest)
+    const firstOutput = await output['*'][0]?.(
+      exampleRequest,
+      new MiddlewareResponse(),
+    )
     expect(firstOutput?.body).toBe('MW initialized with 1')
 
-    const secondOutput = await output['/second-path'][0]?.(exampleRequest)
+    const secondOutput = await output['/second-path'][0]?.(
+      exampleRequest,
+      new MiddlewareResponse(),
+    )
     expect(secondOutput?.body).toBe('MW initialized with 2')
+  })
+})
+
+describe('chain', () => {
+  const addHeaderMw: Middleware = (req, res) => {
+    res.headers.append('add-header-mw', 'added')
+    return res
+  }
+
+  const addCookieMw: Middleware = (req, res) => {
+    res.cookies.set('add-cookie-mw', 'added')
+    return res
+  }
+
+  const registerList: MiddlewareReg = [
+    // Note the order here, fake middleware first, which sets body and headers
+    new FakeClassMw(5),
+    [addHeaderMw, '*'],
+    [addCookieMw, '/bazinga'],
+    [new FakeClassMw(999), '/bazinga'],
+  ]
+
+  const exampleRequest = new MiddlewareRequest(
+    new Request('https://example.com/bazinga'),
+  )
+
+  it('should chain middleware together', async () => {
+    const grouped = groupByRoutePatterns(registerList)
+
+    const allRoutes = chain(grouped['*'])
+
+    const output = await allRoutes(exampleRequest, new MiddlewareResponse())
+
+    expect(output.body).toBe('MW initialized with 5')
+    expect(output.headers.get('class-mw-value')).toBe('5')
+    expect(output.headers.get('add-header-mw')).toBe('added')
+  })
+
+  it('Routing with find-my-way', async () => {
+    const mwRouter = addMiddlewareHandlers(registerList)
+    const match = mwRouter.find('GET', '/bazinga')
+    // @ts-expect-error No way of customising find-my-way route type
+    const output = await match?.handler(
+      exampleRequest,
+      new MiddlewareResponse(),
+    )
+
+    // The more specific one gets used
+    expect(output.body).toBe('MW initialized with 999')
+    expect(output.headers.get('class-mw-value')).toBe('999')
+
+    // Because /bazinga is more specific, the '*' handlers won't be executed
+    expect(output.headers.get('add-header-mw')).toBeFalsy()
+    expect(output.cookies.get('add-header-mw')).toBeFalsy()
   })
 })
