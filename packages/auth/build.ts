9bf13551fde6aedf8949c70e0bc5273fb645dc52
Tobbe Lundberg <tobbe@tlundberg.com>
2024-04-07 11:39:02 +0200

    Explain post-build steps

--- packages/auth/build.ts
+++ packages/auth/build.ts
@@ -22,6 +22,15 @@ await build({
   },
 })
 
+// Because the package.json files has `type: module` the CJS entry file can't
+// be named `index.js` because in that case it would be treated as an ESM file.
+// By changing it to .cjs it will be treated as a CommonJS file.
 renameSync('dist/index.js', 'dist/index.cjs')
+
+// Place a package.json file with `type: commonjs` in the dist folder so that
+// all .js files are treated as CommonJS files.
 writeFileSync('dist/package.json', JSON.stringify({ type: 'commonjs' }))
+
+// Place a package.json file with `type: module` in the dist/esm folder so that
+// all .js files are treated as ES Module files.
 writeFileSync('dist/esm/package.json', JSON.stringify({ type: 'module' }))
