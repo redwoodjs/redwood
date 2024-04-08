5c66dcbc8550c54f4006c123dc6e602b2438b9fd
Dominic Saadi <dominiceliassaadi@gmail.com>
2024-04-08 14:30:15 -0700

    Revert "fix(rw-eslint): Implement more specific checking on Routes (#10404)"
    This reverts commit 30056a503fae4276b1179235de44957f61fbe8c6.

--- packages/eslint-plugin/src/unsupported-route-components.ts
+++ packages/eslint-plugin/src/unsupported-route-components.ts
@@ -1,35 +1,10 @@
-import type { TSESTree } from '@typescript-eslint/utils'
 import { ESLintUtils } from '@typescript-eslint/utils'
-import type { RuleContext } from '@typescript-eslint/utils/ts-eslint'
 
 const createRule = ESLintUtils.RuleCreator.withoutDocs
 
 function isAllowedElement(name: string) {
   const allowedElements = ['Router', 'Route', 'Set', 'PrivateSet', 'Private']
   return allowedElements.includes(name)
-}
-
-function checkNodes(
-  nodesToCheck: TSESTree.JSXElement | TSESTree.JSXChild,
-  context: RuleContext<'unexpected', []>,
-) {
-  if (nodesToCheck.type === 'JSXElement') {
-    const name =
-      nodesToCheck.openingElement.name.type === 'JSXIdentifier'
-        ? nodesToCheck.openingElement.name.name
-        : null
-    if (name && !isAllowedElement(name)) {
-      context.report({
-        node: nodesToCheck,
-        messageId: 'unexpected',
-        data: { name },
-      })
-    }
-
-    if (nodesToCheck.children) {
-      nodesToCheck.children.forEach((node) => checkNodes(node, context))
-    }
-  }
 }
 
 export const unsupportedRouteComponents = createRule({
@@ -41,44 +16,30 @@ export const unsupportedRouteComponents = createRule({
     },
     messages: {
       unexpected:
-        'Unexpected JSX element <{{name}}>. Only <Router>, <Route>, <Set>, <PrivateSet> and <Private> are allowed in the Routes component.',
+        'Unexpected JSX element <{{name}}>. Only <Router>, <Route>, <Set>, <PrivateSet> and <Private> are allowed in the Routes file.',
     },
-    schema: [],
+    schema: [], // No additional configuration needed
   },
   defaultOptions: [],
   create(context) {
     return {
-      VariableDeclaration(node) {
-        if (isRoutesRenderBlock(node.declarations[0])) {
-          const routesDeclaration = node.declarations[0].init
+      JSXOpeningElement: function (node) {
+        let name = ''
 
-          if (routesDeclaration?.type === 'ArrowFunctionExpression') {
-            if (routesDeclaration.body.type === 'JSXElement') {
-              // Routes = () => <Router>...</Router>
-              checkNodes(routesDeclaration.body, context)
-            } else if (routesDeclaration.body.type === 'BlockStatement') {
-              // For when  Routes = () => { return (<Router>...</Router>) }
-              if (
-                routesDeclaration.body.body[0].type === 'ReturnStatement' &&
-                routesDeclaration.body.body[0].argument?.type === 'JSXElement'
-              ) {
-                const routesReturnStatement =
-                  routesDeclaration.body.body[0].argument
+        if (node.name.type === 'JSXIdentifier') {
+          name = node.name.name
+        } else {
+          return
+        }
 
-                checkNodes(routesReturnStatement, context)
-              }
-            }
-          }
+        if (!isAllowedElement(name)) {
+          context.report({
+            node,
+            messageId: 'unexpected',
+            data: { name },
+          })
         }
       },
     }
   },
 })
-
-function isRoutesRenderBlock(node?: TSESTree.VariableDeclarator) {
-  return (
-    node?.type === 'VariableDeclarator' &&
-    node?.id.type === 'Identifier' &&
-    node?.id.name === 'Routes'
-  )
-}
