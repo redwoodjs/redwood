f6192658c4eb6883800723e0eeb27a401e5ecc08
David Thyresson <dthyresson@gmail.com>
2024-04-15 16:04:20 -0400

    Adds login test

--- packages/auth-providers/dbAuth/middleware/modules.d.ts
+++ packages/auth-providers/dbAuth/middleware/modules.d.ts
@@ -1,4 +1,7 @@
 declare module '@redwoodjs/vite/middleware' {
   export type { MiddlewareRequest } from '@redwoodjs/vite/middleware'
-  export { MiddlewareResponse } from '@redwoodjs/vite/middleware'
+  export {
+    MiddlewareRequest,
+    MiddlewareResponse,
+  } from '@redwoodjs/vite/middleware'
 }
--- packages/auth-providers/dbAuth/middleware/src/__tests__/createDbAuthMiddleware.test.ts
+++ packages/auth-providers/dbAuth/middleware/src/__tests__/createDbAuthMiddleware.test.ts
@@ -2,6 +2,9 @@ import path from 'node:path'
 
 import { describe, it, expect, beforeAll, afterAll } from 'vitest'
 
+import type { MiddlewareRequest } from '@redwoodjs/vite/middleware'
+import { MiddlewareRequest as MWRequest } from '@redwoodjs/vite/middleware'
+
 import { createDbAuthMiddleware } from '../index'
 import type { DbAuthMiddlewareOptions } from '../index'
 
@@ -19,16 +22,6 @@ afterAll(() => {
   delete process.env.RWJS_CWD
 })
 
-// login
-// logout
-// signup
-// currentUser
-// POST that isn't one of the above
-// GET request with correct cookies
-// GET request with no cookies
-// GET request with incorrect cookies (bad decrypt)
-// GET request with no cookies and currentUser
-// GET request with expired cookies
 describe('createDbAuthMiddleware()', () => {
   it('creates middleware for dbAuth cookie auth', async () => {
     const options: DbAuthMiddlewareOptions = {
@@ -50,7 +43,7 @@ describe('createDbAuthMiddleware()', () => {
       method: 'GET',
       headers: new Headers(),
       url: 'http://localhost:8911',
-    }
+    } as MiddlewareRequest
 
     const res = await middleware(req)
 
@@ -58,4 +51,51 @@ describe('createDbAuthMiddleware()', () => {
     expect(res).toHaveProperty('body', undefined)
     expect(res).toHaveProperty('status', 200)
   })
+  it('handles a login request', async () => {
+    const request = new Request(
+      'http://localhost:8911/middleware/dbauth/login',
+      {
+        method: 'POST',
+        body: JSON.stringify({ username: ' ' }),
+      },
+    )
+
+    const req = new MWRequest(request)
+
+    const options: DbAuthMiddlewareOptions = {
+      cookieName: 'session_8911',
+      getCurrentUser: async () => {
+        return {}
+      },
+      dbAuthHandler: async () => {
+        return {
+          body: JSON.stringify({ id: 1, email: 'user-1@example.com' }),
+          headers: {
+            Cookie:
+              'session=cookie-value; Path=/; HttpOnly; SameSite=Lax; Secure',
+          },
+          statusCode: 200,
+        }
+      },
+    }
+    const middleware = createDbAuthMiddleware(options)
+
+    const res = await middleware(req)
+
+    expect(res).toBeDefined()
+    expect(res).toHaveProperty(
+      'body',
+      JSON.stringify({ id: 1, email: 'user-1@example.com' }),
+    )
+    expect(res).toHaveProperty('status', 200)
+  })
+  it('handles a logout request', async () => {})
+  it('handles a signup request', async () => {})
+  it('handles a currentUser request', async () => {})
+  it('handles a POST that is not one of the above', async () => {})
+  it('handles a GET request with correct cookies', async () => {})
+  it('handles a GET request with no cookies', async () => {})
+  it('handles a GET request with incorrect cookies (bad decrypt)', async () => {})
+  it('handles a GET request with no cookies and currentUser', async () => {})
+  it('handles a GET request with expired cookies', async () => {})
 })
