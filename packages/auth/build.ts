4ce0d19de6ab92bd2e0ca4332a1e1a13918f98bf
Tobbe Lundberg <tobbe@tlundberg.com>
2024-04-15 11:36:01 +0200

    chore(auth): Build: Put ESM at the root, and CJS in /cjs

--- packages/auth/build.ts
+++ packages/auth/build.ts
@@ -1,4 +1,4 @@
-import { renameSync, writeFileSync } from 'node:fs'
+import { writeFileSync } from 'node:fs'
 
 import { build, defaultBuildOptions } from '@redwoodjs/framework-tools'
 
@@ -8,7 +8,6 @@ await build({
     ...defaultBuildOptions,
     tsconfig: 'tsconfig.build-esm.json',
     format: 'esm',
-    outdir: 'dist/esm',
     packages: 'external',
   },
 })
@@ -18,19 +17,15 @@ await build({
   buildOptions: {
     ...defaultBuildOptions,
     tsconfig: 'tsconfig.build.json',
+    outdir: 'dist/cjs',
     packages: 'external',
   },
 })
 
-// Because the package.json files has `type: module` the CJS entry file can't
-// be named `index.js` because in that case it would be treated as an ESM file.
-// By changing it to .cjs it will be treated as a CommonJS file.
-renameSync('dist/index.js', 'dist/index.cjs')
-
 // Place a package.json file with `type: commonjs` in the dist folder so that
 // all .js files are treated as CommonJS files.
-writeFileSync('dist/package.json', JSON.stringify({ type: 'commonjs' }))
+writeFileSync('dist/cjs/package.json', JSON.stringify({ type: 'commonjs' }))
 
 // Place a package.json file with `type: module` in the dist/esm folder so that
 // all .js files are treated as ES Module files.
-writeFileSync('dist/esm/package.json', JSON.stringify({ type: 'module' }))
+writeFileSync('dist/package.json', JSON.stringify({ type: 'module' }))
