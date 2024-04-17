7a3e09ce2ec724f77037f061e705d8e03f8412ac
Daniel Choudhury <dannychoudhury@gmail.com>
2024-04-17 18:48:42 +0700

    Update ts templates and fixtures

--- __fixtures__/test-project/web/src/App.tsx
+++ __fixtures__/test-project/web/src/App.tsx
@@ -4,11 +4,9 @@ import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
 import FatalErrorPage from 'src/pages/FatalErrorPage'
-import Routes from 'src/Routes'
 
 import { AuthProvider, useAuth } from './auth'
 
-import './scaffold.css'
 import './index.css'
 interface AppProps {
   children?: ReactNode
@@ -19,7 +17,7 @@ const App = ({ children }: AppProps) => (
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
       <AuthProvider>
         <RedwoodApolloProvider useAuth={useAuth}>
-          {children ? children : <Routes />}
+          {children}
         </RedwoodApolloProvider>
       </AuthProvider>
     </RedwoodProvider>
--- packages/create-redwood-app/templates/ts/web/src/App.tsx
+++ packages/create-redwood-app/templates/ts/web/src/App.tsx
@@ -4,7 +4,6 @@ import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
 import FatalErrorPage from 'src/pages/FatalErrorPage'
-import Routes from 'src/Routes'
 
 import './index.css'
 interface AppProps {
@@ -15,7 +14,7 @@ const App = ({ children }: AppProps) => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
       <RedwoodApolloProvider>
-        {children ? children : <Routes />}
+        {children}
       </RedwoodApolloProvider>
     </RedwoodProvider>
   </FatalErrorBoundary>
