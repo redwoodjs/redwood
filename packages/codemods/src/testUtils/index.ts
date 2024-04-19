c236924532a878430b7725cd46afdba985169945
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2024-04-19 01:11:55 +0100

    test utils and test snapshot update

--- packages/codemods/src/testUtils/index.ts
+++ packages/codemods/src/testUtils/index.ts
@@ -5,7 +5,7 @@ import { format } from 'prettier'
 import parserBabel from 'prettier/parser-babel'
 import tempy from 'tempy'
 
-export const formatCode = (code: string) => {
+export const formatCode = async (code: string) => {
   return format(code, {
     parser: 'babel-ts',
     plugins: [parserBabel],
