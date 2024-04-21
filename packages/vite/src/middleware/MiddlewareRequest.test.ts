b1c4bb7f8575701eeea7d0b60f925ca93deaf4e6
Daniel Choudhury <dannychoudhury@gmail.com>
2024-04-21 16:31:57 +0700

    feat(cookieJar): Change cookie.get to directly return value

--- packages/vite/src/middleware/MiddlewareRequest.test.ts
+++ packages/vite/src/middleware/MiddlewareRequest.test.ts
@@ -18,7 +18,7 @@ describe('MiddlewareRequest', () => {
     })
     const mReq = createMiddlewareRequest(req)
 
-    expect(mReq.cookies.get('foo')).toStrictEqual({ value: 'bar' })
+    expect(mReq.cookies.get('foo')).toStrictEqual('bar')
     expect(mReq.method).toStrictEqual('POST')
     expect(mReq.headers.get('Content-Type')).toStrictEqual('application/json')
 
@@ -43,7 +43,7 @@ describe('MiddlewareRequest', () => {
 
     const mReq = createMiddlewareRequest(whatWgRequest)
 
-    expect(mReq.cookies.get('errybody')).toStrictEqual({ value: 'lets-funk' })
+    expect(mReq.cookies.get('errybody')).toStrictEqual('lets-funk')
     expect(mReq.method).toStrictEqual('PUT')
 
     expect(mReq.headers.get('x-custom-header')).toStrictEqual('beatdrop')
