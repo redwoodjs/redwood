17744aadfc1b7651e2c2164a42d856ca7f2f6668
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2024-05-06 23:33:41 +0100

    fix var name

--- packages/cli/src/commands/setup/middleware/middleware.ts
+++ packages/cli/src/commands/setup/middleware/middleware.ts
@@ -1,13 +1,13 @@
 import terminalLink from 'terminal-link'
 import type { Argv } from 'yargs'
 
-import * as fragmentsCommand from './ogImage/ogImage'
+import * as ogImageCommand from './ogImage/ogImage'
 
 export const command = 'middleware <type>'
 export const description = 'Set up a middleware'
 export function builder(yargs: Argv) {
   return yargs
-    .command(fragmentsCommand)
+    .command(ogImageCommand)
     .epilogue(
       `Also see the ${terminalLink(
         'Redwood CLI Reference',
