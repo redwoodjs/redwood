4a86946264720c5c60af10fe49263bc9aac5f208
Tobbe Lundberg <tobbe@tlundberg.com>
2024-05-22 00:06:41 +0200

    RSC: Source format of EmptyUsersCell in kitchen-sink (#10658)

--- __fixtures__/test-project-rsc-kitchen-sink/web/src/components/EmptyUser/EmptyUsersCell/EmptyUsersCell.tsx
+++ __fixtures__/test-project-rsc-kitchen-sink/web/src/components/EmptyUser/EmptyUsersCell/EmptyUsersCell.tsx
@@ -11,27 +11,25 @@ import type {
 
 import EmptyUsers from 'src/components/EmptyUser/EmptyUsers'
 
-export const QUERY: TypedDocumentNode<
-  FindEmptyUsers,
-  FindEmptyUsersVariables
-> = gql`
-  query FindEmptyUsers {
-    emptyUsers {
-      id
-      email
-      name
+export const QUERY: TypedDocumentNode<FindEmptyUsers, FindEmptyUsersVariables> =
+  gql`
+    query FindEmptyUsers {
+      emptyUsers {
+        id
+        email
+        name
+      }
     }
-  }
-`
+  `
 
 export const Loading = () => <div>Loading...</div>
 
 export const Empty = () => {
   return (
     <div className="rw-text-center">
-      {'No emptyUsers yet. '}
+      No emptyUsers yet.{' '}
       <Link to={routes.newEmptyUser()} className="rw-link">
-        {'Create one?'}
+        Create one?
       </Link>
     </div>
   )
