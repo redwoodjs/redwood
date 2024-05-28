6954028f86fd2158b15cfc8e325616ba95317f6e
Daniel Choudhury <dannychoudhury@gmail.com>
2024-05-28 15:08:22 +0700

    Setup react server export for ogMiddleware

---
+++ packages/ogimage-gen/empty.js
@@ -0,0 +1,9 @@
+// This file is intentionally left empty, because we do not want to bundle middleware
+
+export default function empty() {
+  console.log(`
+  _______ _     _ _______ _______ _______ _______ _______ _______
+ |______ |_____| |______ |______ |______ |______ |______ |______
+ ______| |     | |______ ______| ______| ______| ______| ______|
+`)
+}
