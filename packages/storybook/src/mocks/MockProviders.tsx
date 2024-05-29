a8ece018518302f0902aca51cd111d7c502d7867
Ari Mendelow <ari@mendelow.me>
2024-05-29 14:48:41 -0700

    Update packages/storybook/src/mocks/MockProviders.tsx
    Co-authored-by: Tobbe Lundberg <tobbe@tlundberg.com>

--- packages/storybook/src/mocks/MockProviders.tsx
+++ packages/storybook/src/mocks/MockProviders.tsx
@@ -12,7 +12,7 @@ import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 import { MockParamsProvider } from './MockParamsProvider'
 
 // Import the user's Routes from `./web/src/Routes.{tsx,jsx}`,
-// we pass the `children` from the user's Router to `./MockRouter.Router`
+// we pass the `children` from the user's Routes to `./MockRouter.Router`
 // so that we can populate the `routes object` in Storybook and tests.
 let UserRouterWithRoutes: React.FC
 
