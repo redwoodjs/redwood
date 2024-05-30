f73ff4189eb0c4338402c886c32cfb9b16609e05
Tobbe Lundberg <tobbe@tlundberg.com>
2024-05-30 09:25:51 +0200

    chore(testing): Remove unused member in Props interface

--- packages/testing/src/web/MockParamsProvider.tsx
+++ packages/testing/src/web/MockParamsProvider.tsx
@@ -3,23 +3,15 @@ import React from 'react'
 import { useLocation, ParamsContext, parseSearch } from '@redwoodjs/router'
 
 interface Props {
-  path?: string
   children?: React.ReactNode
 }
 
 export const MockParamsProvider: React.FC<Props> = ({ children }) => {
   const location = useLocation()
-
   const searchParams = parseSearch(location.search)
 
   return (
-    <ParamsContext.Provider
-      value={{
-        params: {
-          ...searchParams,
-        },
-      }}
-    >
+    <ParamsContext.Provider value={{ params: { ...searchParams } }}>
       {children}
     </ParamsContext.Provider>
   )
