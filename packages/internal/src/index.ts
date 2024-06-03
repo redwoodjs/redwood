352e52fe178fc8d3aac481cdb13be81d7d6ddde1
Tobbe Lundberg <tobbe@tlundberg.com>
2024-06-03 22:33:26 +0200

    packages/internal: Try to fix CI

--- packages/internal/src/index.ts
+++ packages/internal/src/index.ts
@@ -1,7 +1,7 @@
 export * from '@redwoodjs/project-config'
 
 export * from './ts2js'
-export * from './dev'
+export { shutdownPort } from './dev'
 export * from './routes'
 
 export * from './files'
