683beb15b50f191c61a2f23fa1314bdf2fdd902f
Rob Cameron <cannikin@fastmail.com>
2024-06-05 15:32:51 -0700

    Removes hardcoded model creation from seed, include minimal usage example, link to docs for more info

--- packages/create-redwood-app/templates/ts/scripts/seed.ts
+++ packages/create-redwood-app/templates/ts/scripts/seed.ts
@@ -1,67 +1,25 @@
 import type { Prisma } from '@prisma/client'
 import { db } from 'api/src/lib/db'
 
+// Manually apply seeds via the `yarn rw prisma db seed` command.
+// Seeds automatically run with `yarn rw prisma migrate dev` and
+// `yarn rw prisma migrate reset` commands.
+//
+// See https://redwoodjs.com/docs/seeds for more info
+
 export default async () => {
   try {
-    //
-    // Manually seed via `yarn rw prisma db seed`
-    // Seeds automatically with `yarn rw prisma migrate dev` and `yarn rw prisma migrate reset`
-    //
-    // Update "const data = []" to match your data model and seeding needs
-    //
-    const data: Prisma.UserExampleCreateArgs['data'][] = [
-      // To try this example data with the UserExample model in schema.prisma,
-      // uncomment the lines below and run 'yarn rw prisma migrate dev'
-      //
-      // { name: 'alice', email: 'alice@example.com' },
-      // { name: 'mark', email: 'mark@example.com' },
-      // { name: 'jackie', email: 'jackie@example.com' },
-      // { name: 'bob', email: 'bob@example.com' },
-    ]
-    console.log(
-      "\nUsing the default './scripts/seed.ts' template\nEdit the file to add seed data\n"
-    )
+    const data = []
 
-    if ((await db.userExample.count()) === 0) {
-      // Note: if using PostgreSQL, using `createMany` to insert multiple records is much faster
-      // @see: https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#createmany
-      await Promise.all(
-        //
-        // Change to match your data model and seeding needs
-        //
-        data.map(async (data: Prisma.UserExampleCreateArgs['data']) => {
-          const record = await db.userExample.create({ data })
-          console.log(record)
-        })
-      )
-    } else {
-      console.log('Users already seeded')
+    for (const item of data) {
+      // Create a database record for each item in `data`
+      // ie: `await db.user.create({ data: item })`
     }
 
-    // If using dbAuth and seeding users, you'll need to add a `hashedPassword`
-    // and associated `salt` to their record. Here's how to create them using
-    // the same algorithm that dbAuth uses internally:
-    //
-    //   import { hashPassword } from '@redwoodjs/auth-dbauth-api'
-    //
-    //   const users = [
-    //     { name: 'john', email: 'john@example.com', password: 'secret1' },
-    //     { name: 'jane', email: 'jane@example.com', password: 'secret2' }
-    //   ]
-    //
-    //   for (const user of users) {
-    //     const [hashedPassword, salt] = hashPassword(user.password)
-    //     await db.user.create({
-    //       data: {
-    //         name: user.name,
-    //         email: user.email,
-    //         hashedPassword,
-    //         salt
-    //       }
-    //     })
-    //   }
+    console.info(
+      'No seed data, skipping. See scripts/seed.ts to start seeding your database!'
+    )
   } catch (error) {
-    console.warn('Please define your seed data.')
     console.error(error)
   }
 }
