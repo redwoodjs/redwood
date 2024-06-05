ee0a4233d59a704847f33987e311c95c67cd2e3c
Tobbe Lundberg <tobbe@tlundberg.com>
2024-06-05 20:00:06 +0200

    RSC: Add middleware to kitchen-sink (#10735)

--- __fixtures__/test-project-rsc-kitchen-sink/web/src/entry.server.tsx
+++ __fixtures__/test-project-rsc-kitchen-sink/web/src/entry.server.tsx
@@ -20,3 +20,9 @@ export const ServerEntry: React.FC<Props> = ({ css, meta, location }) => {
     </Document>
   )
 }
+
+export async function registerMiddleware() {
+  const { middleware: selfMtsMw } = await import('./middleware/self.mjs')
+
+  return [selfMtsMw]
+}
