4bf71d9d26cc0a01ca1c1bee0fe71c236d56ebe9
Rob Cameron <cannikin@fastmail.com>
2024-06-05 16:32:36 -0700

    Update docs link

--- packages/create-redwood-app/templates/js/scripts/seed.js
+++ packages/create-redwood-app/templates/js/scripts/seed.js
@@ -5,7 +5,7 @@ import { db } from 'api/src/lib/db'
 // Seeds automatically run the first time you run the `yarn rw prisma migrate dev`
 // command and every time you run the `yarn rw prisma migrate reset` command.
 //
-// See https://redwoodjs.com/docs/seeds for more info
+// See https://redwoodjs.com/docs/database-seeds for more info
 
 export default async () => {
   try {
