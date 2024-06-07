c57f90c789a544537ad92a8e50e993a3f71d4308
Rob Cameron <cannikin@fastmail.com>
2024-06-07 10:33:17 -0700

    Update fixture

--- __fixtures__/test-project/scripts/seed.ts
+++ __fixtures__/test-project/scripts/seed.ts
@@ -1,6 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import type { Prisma } from '@prisma/client'
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { db } from 'api/src/lib/db'
 
 // Manually apply seeds via the `yarn rw prisma db seed` command.
