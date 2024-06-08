ff8987728727d03271f75ea514ba8516e94c0938
Tobbe Lundberg <tobbe@tlundberg.com>
2024-06-08 23:15:19 +0200

    externalize cookie-jar too

--- packages/vite/src/rsc/rscBuildForServer.ts
+++ packages/vite/src/rsc/rscBuildForServer.ts
@@ -53,7 +53,12 @@ export async function rscBuildForServer(
       // Can't inline prisma client (db calls fail at runtime) or react-dom
       // (css pre-init failure)
       // Server store has to be externalized, because it's a singleton (shared between FW and App)
-      external: ['@prisma/client', 'react-dom', '@redwoodjs/server-store'],
+      external: [
+        '@prisma/client',
+        'react-dom',
+        '@redwoodjs/cookie-jar',
+        '@redwoodjs/server-store',
+      ],
       resolve: {
         // These conditions are used in the plugin pipeline, and only affect non-externalized
         // dependencies during the SSR build. Which because of `noExternal: true` means all
