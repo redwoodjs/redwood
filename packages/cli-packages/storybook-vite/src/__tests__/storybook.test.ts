08ec4a981d42ac147e71fc3e7dd55e5c59d37d34
ari <ari@mendelow.me>
2024-06-11 21:04:15 -0700

    More work on testing, including temp unblocking by adding the experimental flag to the script

--- packages/cli-packages/storybook-vite/src/__tests__/storybook.test.ts
+++ packages/cli-packages/storybook-vite/src/__tests__/storybook.test.ts
@@ -1,3 +1,4 @@
+import terminalLink from 'terminal-link'
 import type yargs from 'yargs'
 
 import * as storybookCommand from '../commands/storybook'
@@ -26,34 +27,31 @@ describe('storybook', () => {
   })
 
   it("`command`, `aliases`, and `description` haven't unintentionally changed", () => {
-    expect(storybookCommand.command).toMatchInlineSnapshot(`"storybook-vite"`)
-    expect(storybookCommand.aliases).toMatchInlineSnapshot(`
-      Array [
-        "sbv",
-      ]
-    `)
-    expect(storybookCommand.description).toMatchInlineSnapshot(`
-      "Launch Storybook: a tool for building UI components and pages in isolation (now, with Vite)!"
-    `)
+    expect(storybookCommand.command).toEqual('storybook-vite')
+    expect(storybookCommand.aliases).toEqual(['sbv'])
+    expect(storybookCommand.description).toEqual(
+      'Launch Storybook: a tool for building UI components and pages in isolation (now, with Vite)!',
+    )
   })
 
   it("`defaultOptions` haven't unintentionally changed", () => {
-    expect(storybookCommand.defaultOptions).toMatchInlineSnapshot(`
-      Object {
-        "open": true,
-        "build": false,
-        "ci": false,
-        "port": 7910,
-        "buildDirectory": 'public/storybook',
-        "smokeTest": false,
-      }
-    `)
+    expect(storybookCommand.defaultOptions).toEqual({
+      open: true,
+      build: false,
+      ci: false,
+      port: 7910,
+      buildDirectory: 'public/storybook',
+      smokeTest: false,
+    })
   })
 
   it('`builder` has an epilogue', () => {
     // The typecasting here is to make TS happy when calling `builder(yargs)`
     // further down. We know that only `epilogue` will be called.
-    const yargs = { epilogue: jest.fn() } as unknown as yargs.Argv
+    const yargs = {
+      option: jest.fn().mockReturnThis(),
+      epilogue: jest.fn().mockReturnThis(),
+    } as unknown as yargs.Argv
     storybookCommand.builder(yargs)
     // The epilogue is a string that contains a link to the docs. The string
     // contains special control characters when rendered in a terminal that
@@ -61,12 +59,10 @@ describe('storybook', () => {
     // to avoid having to match control characters that might not even always
     // be there
     expect(yargs.epilogue).toHaveBeenCalledWith(
-      expect.stringMatching(/Also see the .*Redwood CLI Reference.*/),
-    )
-    expect(yargs.epilogue).toHaveBeenCalledWith(
-      expect.stringMatching(
-        /https:\/\/redwoodjs\.com\/docs\/cli-commands#datamigrate-install/,
-      ),
+      `Also see the ${terminalLink(
+        'Redwood CLI Reference',
+        'https://redwoodjs.com/docs/cli-commands#storybook',
+      )}`,
     )
   })
 
