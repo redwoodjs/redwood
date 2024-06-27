54002a906edfcd07f64a6d88d6d5829bda3354ac
Rob Cameron <cannikin@fastmail.com>
2024-06-27 00:00:54 -0700

    Add paths for jobs/logger

--- packages/project-config/src/paths.ts
+++ packages/project-config/src/paths.ts
@@ -199,7 +199,9 @@ export const getPaths = (BASE_DIR: string = getBaseDir()): Paths => {
       dbSchema: path.join(BASE_DIR, schemaPath),
       functions: path.join(BASE_DIR, PATH_API_DIR_FUNCTIONS),
       graphql: path.join(BASE_DIR, PATH_API_DIR_GRAPHQL),
+      jobs: resolveFile(path.join(BASE_DIR, PATH_API_DIR_LIB, 'jobs')),
       lib: path.join(BASE_DIR, PATH_API_DIR_LIB),
+      logger: resolveFile(path.join(BASE_DIR, PATH_API_DIR_LIB, 'logger')),
       generators: path.join(BASE_DIR, PATH_API_DIR_GENERATORS),
       config: path.join(BASE_DIR, PATH_API_DIR_CONFIG),
       services: path.join(BASE_DIR, PATH_API_DIR_SERVICES),
@@ -207,6 +209,7 @@ export const getPaths = (BASE_DIR: string = getBaseDir()): Paths => {
       subscriptions: path.join(BASE_DIR, PATH_API_DIR_SUBSCRIPTIONS),
       src: path.join(BASE_DIR, PATH_API_DIR_SRC),
       dist: path.join(BASE_DIR, 'api/dist'),
+      // TODO Add dist paths for logger and jobs
       types: path.join(BASE_DIR, 'api/types'),
       models: path.join(BASE_DIR, PATH_API_DIR_MODELS),
       mail: path.join(BASE_DIR, PATH_API_DIR_SRC, 'mail'),
