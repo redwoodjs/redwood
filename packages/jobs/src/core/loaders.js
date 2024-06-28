d07ed753f184a8de6cc15c5ce3736bb3db5d2356
Rob Cameron <cannikin@fastmail.com>
2024-06-27 20:11:55 -0700

    Move bins/shared to core/loaders

--- packages/jobs/src/core/loaders.js
+++ packages/jobs/src/core/loaders.js
@@ -1,19 +1,21 @@
 import console from 'node:console'
 import path from 'node:path'
 
-import fg from 'fast-glob'
-
 import { registerApiSideBabelHook } from '@redwoodjs/babel-config'
-import { getPaths } from '@redwoodjs/project-config'
+import { getPaths, resolveFile } from '@redwoodjs/project-config'
 
-import { AdapterNotFoundError, JobsLibNotFoundError } from '../core/errors'
+import {
+  AdapterNotFoundError,
+  JobsLibNotFoundError,
+  JobNotFoundError,
+} from './errors'
 
 // TODO Don't use this in production, import from dist directly
 registerApiSideBabelHook()
 
 export const loadAdapter = async () => {
   if (getPaths().api.jobs) {
-    const { default: jobsModule } = await import(getPaths().api.jobs)
+    const { default: jobsModule } = await import(getPaths().api.jobsConfig)
     if (jobsModule.adapter) {
       return jobsModule.adapter
     } else {
@@ -29,7 +31,20 @@ export const loadLogger = async () => {
     const { default: loggerModule } = await import(getPaths().api.logger)
     return loggerModule.logger
   } catch (e) {
-    // import didn't work for whatever reason, fall back to console
     return console
+  }
+}
+
+export const loadJob = async (name) => {
+  try {
+    const filename = resolveFile(path.join(getPaths().api.jobs, name))
+    const { default: jobModule } = await import(filename)
+    return jobModule
+  } catch (e) {
+    if (e.code === 'ERR_MODULE_NOT_FOUND') {
+      throw new JobNotFoundError(name)
+    } else {
+      throw e
+    }
   }
 }
