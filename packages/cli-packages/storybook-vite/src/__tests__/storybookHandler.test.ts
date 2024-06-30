196cab59a5f384ea8bf0cbcb1f441efac8a61cbd
ari <ari@mendelow.me>
2024-06-30 11:19:15 -0700

    Attempt to add test for checking that config files were created

--- packages/cli-packages/storybook-vite/src/__tests__/storybookHandler.test.ts
+++ packages/cli-packages/storybook-vite/src/__tests__/storybookHandler.test.ts
@@ -1,5 +1,55 @@
+import fs from 'fs'
+
+import { vol } from 'memfs'
+
+import { getPaths } from '@redwoodjs/project-config'
+
+import { handler } from '../commands/storybookHandler'
+
+jest.mock('fs', () => require('memfs').fs)
+
+jest.mock('execa', () => {
+  return {
+    command: jest.fn(() => {
+      return {
+        stdout: 42,
+      }
+    }),
+  }
+})
+
 describe('storybookHandler', () => {
-  it('should work', () => {
-    expect(true).toBe(true)
+  it('adds the config files', async () => {
+    const redwoodProjectPath = '/redwood-app'
+    process.env.RWJS_CWD = redwoodProjectPath
+
+    vol.fromNestedJSON(
+      {
+        'redwood.toml': '',
+        web: {},
+      },
+      redwoodProjectPath,
+    )
+
+    const options = {
+      open: true,
+      build: false,
+      ci: false,
+      port: 7910,
+      buildDirectory: 'public/storybook',
+      smokeTest: false,
+    }
+    const webPath = getPaths().web.base
+    console.log('web dir', fs.readdirSync(webPath))
+
+    await handler(options)
+
+    const storybookConfigPath = getPaths().web.storybook
+
+    expect(fs.readdirSync(storybookConfigPath)).toEqual([
+      'main.ts',
+      'preview-body.html',
+    ])
+    // TODO add tests to verify content...
   })
 })
