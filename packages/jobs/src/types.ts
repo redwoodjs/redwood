b8247b816a8dcf1f99efa57da7f87163d03014b1
Rob Cameron <cannikin@fastmail.com>
2024-07-03 16:27:25 -0700

    Comments on types

--- packages/jobs/src/types.ts
+++ packages/jobs/src/types.ts
@@ -1,3 +1,7 @@
+// Defines the basic shape of a logger that RedwoodJob will invoke to print
+// debug messages. Both Redwood's Logger and the standard console object
+// conform to this shape. RedwoodJob will fallback to use `console` if no
+// logger is passed in to RedwoodJob or any adapter.
 export interface BasicLogger {
   debug: (message?: any, ...optionalParams: any[]) => void
   info: (message?: any, ...optionalParams: any[]) => void
@@ -14,7 +18,10 @@ export interface SchedulePayload {
   priority: number
 }
 
-// Arguments returned from an adapter when a job is found
+// Arguments returned from an adapter when a job is found. This is the absolute
+// minimum interface that's needed for the Executor to invoke the job, but any
+// adapter will likely return more info, like the number of previous tries, so
+// that it can reschedule the job to run in the future.
 export interface BaseJob {
   handler: string
   args: any
