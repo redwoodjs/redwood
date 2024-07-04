7cf621b62d6a430d779f67549cf88f4097cb58f3
Tobbe Lundberg <tobbe@tlundberg.com>
2024-07-04 14:53:03 +0200

    RSC: Add auth support to ClientRouter

--- packages/vite/src/ClientRouter.tsx
+++ packages/vite/src/ClientRouter.tsx
@@ -3,9 +3,11 @@
 
 import React, { useMemo } from 'react'
 
+import type { GeneratedRoutesMap } from '@redwoodjs/router/dist/analyzeRoutes'
 import { analyzeRoutes } from '@redwoodjs/router/dist/analyzeRoutes'
 import { LocationProvider, useLocation } from '@redwoodjs/router/dist/location'
 import { namedRoutes } from '@redwoodjs/router/dist/namedRoutes'
+import { Redirect } from '@redwoodjs/router/dist/redirect'
 import type { RouterProps } from '@redwoodjs/router/dist/router'
 
 import { rscFetch } from './rsc/rscFetchForClientRouter'
@@ -24,7 +26,7 @@ export const Router = ({ paramTypes, children }: RouterProps) => {
 const LocationAwareRouter = ({ paramTypes, children }: RouterProps) => {
   const { pathname, search } = useLocation()
 
-  const { namedRoutesMap } = useMemo(() => {
+  const { namedRoutesMap, pathRouteMap } = useMemo(() => {
     return analyzeRoutes(children, {
       currentPathName: pathname,
       userParamTypes: paramTypes,
@@ -34,6 +36,57 @@ const LocationAwareRouter = ({ paramTypes, children }: RouterProps) => {
   // Assign namedRoutes so it can be imported like import {routes} from 'rwjs/router'
   // Note that the value changes at runtime
   Object.assign(namedRoutes, namedRoutesMap)
+
+  // Use results from `analyzeRoutes` to determine if the user has access to
+  // the requested route
+  const requestedRoute = pathRouteMap[pathname]
+
+  const privateSet = requestedRoute.sets.find((set) => set.isPrivate)
+
+  if (privateSet) {
+    const redirectTarget = privateSet.props.unauthenticated
+
+    if (!redirectTarget || typeof redirectTarget !== 'string') {
+      throw new Error(
+        `Route ${pathname} is private and no unauthenticated redirect target was provided`,
+      )
+    }
+
+    // We type cast like this, because AvailableRoutes is generated in the
+    // user's project
+    const generatedRoutesMap = namedRoutes as GeneratedRoutesMap
+
+    if (!generatedRoutesMap[redirectTarget]) {
+      throw new Error(`We could not find a route named ${redirectTarget}`)
+    }
+
+    const currentLocation =
+      globalThis.location.pathname +
+      encodeURIComponent(globalThis.location.search)
+
+    let unauthenticatedPath
+
+    try {
+      unauthenticatedPath = generatedRoutesMap[redirectTarget]()
+    } catch (e) {
+      if (
+        e instanceof Error &&
+        /Missing parameter .* for route/.test(e.message)
+      ) {
+        throw new Error(
+          `Redirecting to route "${redirectTarget}" would require route ` +
+            'parameters, which currently is not supported. Please choose ' +
+            'a different route',
+        )
+      }
+
+      throw new Error(`Could not redirect to the route named ${redirectTarget}`)
+    }
+
+    return (
+      <Redirect to={`${unauthenticatedPath}?redirectTo=${currentLocation}`} />
+    )
+  }
 
   return rscFetch('__rwjs__Routes', { location: { pathname, search } })
 }
