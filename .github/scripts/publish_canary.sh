91f78faede36c8e513b163d6e94aa61490b35355
Tobbe Lundberg <tobbe@tlundberg.com>
2024-07-05 09:44:57 +0200

    chore(ci): Use more supported syntax in publish_canary script (#10915)

--- .github/scripts/publish_canary.sh
+++ .github/scripts/publish_canary.sh
@@ -26,14 +26,14 @@ args+=(
 
 # `echo 'n'` to answer "no" to the "Are you sure you want to publish these
 #   packages?" prompt.
-# `|&` to pipe both stdout and stderr to grep. Mostly do this keep the github
+# `2>&1` to pipe both stdout and stderr to grep. Mostly do this keep the github
 #   action output clean.
 # At the end we use awk to increase the commit count by 1, because we'll commit
 #   updated package.jsons in the next step, which will increase increase the
 #   final number that lerna will use when publishing the canary packages.
 echo 'n' \
-  | yarn lerna publish "${args[@]}" \
-  |& grep '\-canary\.' \
+  | yarn lerna publish "${args[@]}" 2>&1 \
+  | grep '\-canary\.' \
   | tail -n 1 \
   | sed 's/.*=> //' \
   | sed 's/\+.*//' \
