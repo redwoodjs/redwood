df241b3010e7613e98ee89a4d9594122f5bc4041
Rob Cameron <cannikin@fastmail.com>
2024-07-12 10:22:18 -0700

    Adds `yarn rw g job` generator

---
+++ packages/cli/src/commands/generate/job/__tests__/job.test.ts
@@ -0,0 +1,83 @@
+globalThis.__dirname = __dirname
+// Load shared mocks
+import '../../../../lib/test'
+
+import path from 'path'
+
+import { describe, it, expect } from 'vitest'
+
+import * as jobGenerator from '../job'
+
+// Should be refactored as it's repeated
+type WordFilesType = { [key: string]: string }
+
+describe('Single word default files', async () => {
+  const files: WordFilesType = await jobGenerator.files({
+    name: 'Sample',
+    tests: true,
+    typescript: true,
+  })
+
+  it('creates a single word function file', () => {
+    expect(
+      files[
+        path.normalize('/path/to/project/api/src/jobs/SampleJob/SampleJob.ts')
+      ],
+    ).toMatchSnapshot()
+
+    expect(
+      files[
+        path.normalize(
+          '/path/to/project/api/src/jobs/SampleJob/SampleJob.test.ts',
+        )
+      ],
+    ).toMatchSnapshot('Test snapshot')
+
+    expect(
+      files[
+        path.normalize(
+          '/path/to/project/api/src/jobs/SampleJob/SampleJob.scenarios.ts',
+        )
+      ],
+    ).toMatchSnapshot('Scenario snapshot')
+  })
+})
+
+describe('multi-word files', () => {
+  it('creates a multi word function file', async () => {
+    const multiWordDefaultFiles = await jobGenerator.files({
+      name: 'send-mail',
+      tests: false,
+      typescript: true,
+    })
+
+    expect(
+      multiWordDefaultFiles[
+        path.normalize(
+          '/path/to/project/api/src/functions/SendMailJob/SendMailJob.js',
+        )
+      ],
+    ).toMatchSnapshot()
+  })
+})
+
+describe('generation of js files', async () => {
+  const jsFiles: WordFilesType = await jobGenerator.files({
+    name: 'Sample',
+    tests: true,
+    typescript: false,
+  })
+
+  it('returns tests, scenario and job file for JS', () => {
+    const fileNames = Object.keys(jsFiles)
+    expect(fileNames.length).toEqual(3)
+
+    expect(fileNames).toEqual(
+      expect.arrayContaining([
+        expect.stringContaining('SampleJob.js'),
+        expect.stringContaining('SampleJob.test.js'),
+        expect.stringContaining('SampleJob.scenarios.js'),
+      ]),
+    )
+  })
+})
---
+++ packages/cli/src/commands/generate/job/job.js
@@ -0,0 +1,182 @@
+import path from 'path'
+
+import * as changeCase from 'change-case'
+import { Listr } from 'listr2'
+import terminalLink from 'terminal-link'
+
+import { recordTelemetryAttributes } from '@redwoodjs/cli-helpers'
+import { errorTelemetry } from '@redwoodjs/telemetry'
+
+import { getPaths, transformTSToJS, writeFilesTask } from '../../../lib'
+import c from '../../../lib/colors'
+import { isTypeScriptProject } from '../../../lib/project'
+import { prepareForRollback } from '../../../lib/rollback'
+import { yargsDefaults } from '../helpers'
+import { validateName, templateForComponentFile } from '../helpers'
+
+// Makes sure the name ends up looking like: `WelcomeNotice` even if the user
+// called it `welcome-notice` or `welcomeNoticeJob` or anything else
+const normalizeName = (name) => {
+  return changeCase.pascalCase(name).replace(/Job$/, '')
+}
+
+export const files = async ({
+  name,
+  typescript: generateTypescript,
+  tests: generateTests = true,
+  ...rest
+}) => {
+  const extension = generateTypescript ? '.ts' : '.js'
+
+  const outputFiles = []
+
+  const jobName = normalizeName(name)
+
+  const jobFiles = await templateForComponentFile({
+    name: jobName,
+    componentName: jobName,
+    extension,
+    apiPathSection: 'jobs',
+    generator: 'job',
+    templatePath: 'job.ts.template',
+    templateVars: { name: jobName, ...rest },
+    outputPath: path.join(
+      getPaths().api.jobs,
+      `${jobName}Job`,
+      `${jobName}Job${extension}`,
+    ),
+  })
+
+  outputFiles.push(jobFiles)
+
+  if (generateTests) {
+    const testFile = await templateForComponentFile({
+      name: jobName,
+      componentName: jobName,
+      extension,
+      apiPathSection: 'jobs',
+      generator: 'job',
+      templatePath: 'test.ts.template',
+      templateVars: { ...rest },
+      outputPath: path.join(
+        getPaths().api.jobs,
+        `${jobName}Job`,
+        `${jobName}Job.test${extension}`,
+      ),
+    })
+
+    const scenarioFile = await templateForComponentFile({
+      name: jobName,
+      componentName: jobName,
+      extension,
+      apiPathSection: 'jobs',
+      generator: 'job',
+      templatePath: 'scenarios.ts.template',
+      templateVars: { ...rest },
+      outputPath: path.join(
+        getPaths().api.jobs,
+        `${jobName}Job`,
+        `${jobName}Job.scenarios${extension}`,
+      ),
+    })
+
+    outputFiles.push(testFile)
+    outputFiles.push(scenarioFile)
+  }
+
+  return outputFiles.reduce(async (accP, [outputPath, content]) => {
+    const acc = await accP
+
+    const template = generateTypescript
+      ? content
+      : await transformTSToJS(outputPath, content)
+
+    return {
+      [outputPath]: template,
+      ...acc,
+    }
+  }, Promise.resolve({}))
+}
+
+export const command = 'job <name>'
+export const description = 'Generate a Background Job'
+
+// This could be built using createYargsForComponentGeneration;
+// however, functions shouldn't have a `stories` option. createYargs...
+// should be reversed to provide `yargsDefaults` as the default configuration
+// and accept a configuration such as its CURRENT default to append onto a command.
+export const builder = (yargs) => {
+  yargs
+    .positional('name', {
+      description: 'Name of the Job',
+      type: 'string',
+    })
+    .option('typescript', {
+      alias: 'ts',
+      description: 'Generate TypeScript files',
+      type: 'boolean',
+      default: isTypeScriptProject(),
+    })
+    .option('tests', {
+      description: 'Generate test files',
+      type: 'boolean',
+      default: true,
+    })
+    .option('rollback', {
+      description: 'Revert all generator actions if an error occurs',
+      type: 'boolean',
+      default: true,
+    })
+    .epilogue(
+      `Also see the ${terminalLink(
+        'Redwood CLI Reference',
+        'https://redwoodjs.com/docs/cli-commands#generate-job',
+      )}`,
+    )
+
+  // Add default options, includes '--typescript', '--javascript', '--force', ...
+  Object.entries(yargsDefaults).forEach(([option, config]) => {
+    yargs.option(option, config)
+  })
+}
+
+// This could be built using createYargsForComponentGeneration;
+// however, we need to add a message after generating the function files
+export const handler = async ({ name, force, ...rest }) => {
+  recordTelemetryAttributes({
+    command: 'generate job',
+    force,
+    rollback: rest.rollback,
+  })
+
+  validateName(name)
+
+  const tasks = new Listr(
+    [
+      {
+        title: 'Generating job files...',
+        task: async () => {
+          return writeFilesTask(await files({ name, ...rest }), {
+            overwriteExisting: force,
+          })
+        },
+      },
+      {
+        title: 'Adding to api/src/lib/jobs export...',
+        task: async () => {},
+      },
+    ],
+    { rendererOptions: { collapseSubtasks: false }, exitOnError: true },
+  )
+
+  try {
+    if (rest.rollback && !force) {
+      prepareForRollback(tasks)
+    }
+    await tasks.run()
+  } catch (e) {
+    errorTelemetry(process.argv, e.message)
+    console.error(c.error(e.message))
+    process.exit(e?.exitCode || 1)
+  }
+}
