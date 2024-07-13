d39d278dd47697f2b065888f595d9b0e459a64c7
Tobbe Lundberg <tobbe@tlundberg.com>
2024-07-13 22:37:00 +0200

    chore(auth): Fix esm cjs exports and add tooling to verify correctness (#10942)

--- packages/auth/build.ts
+++ packages/auth/build.ts
@@ -1,4 +1,7 @@
-import { writeFileSync } from 'node:fs'
+import { readFileSync, writeFileSync } from 'node:fs'
+
+import type { PackageJson } from 'type-fest'
+import { $ } from 'zx'
 
 import { build, defaultBuildOptions } from '@redwoodjs/framework-tools'
 
@@ -29,3 +32,26 @@ writeFileSync('dist/cjs/package.json', JSON.stringify({ type: 'commonjs' }))
 // Place a package.json file with `type: module` in the dist/esm folder so that
 // all .js files are treated as ES Module files.
 writeFileSync('dist/package.json', JSON.stringify({ type: 'module' }))
+
+// Finally we need to also produce CJS type definitions
+//
+// The best way[1] to do this is to (temporarily) change the "type" in
+// package.json to "commonjs" and run tsc with our tsconfig.build-cjs.json
+// config file.
+// It's possible to run TSC programmatically[2] but it's much easier to just
+// shell out to the CLI.
+//
+// [1]: https://github.com/arethetypeswrong/arethetypeswrong.github.io/issues/21#issuecomment-1494618930
+// [2]: https://github.com/Microsoft/TypeScript/wiki/Using-the-Compiler-API
+
+await $`cp package.json package.json.bak`
+
+const packageJson: PackageJson = JSON.parse(
+  readFileSync('./package.json', 'utf-8'),
+)
+packageJson.type = 'commonjs'
+writeFileSync('./package.json', JSON.stringify(packageJson, null, 2))
+
+await $`yarn build:types-cjs`
+
+await $`mv package.json.bak package.json`
