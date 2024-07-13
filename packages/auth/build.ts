d7a0a6a51abce95465c81f71550a69d91a02c76a
Tobbe Lundberg <tobbe@tlundberg.com>
2024-07-14 01:11:59 +0200

    chore(web): publint and attw (#10943)

--- packages/auth/build.ts
+++ packages/auth/build.ts
@@ -25,11 +25,11 @@ await build({
   },
 })
 
-// Place a package.json file with `type: commonjs` in the dist folder so that
-// all .js files are treated as CommonJS files.
+// Place a package.json file with `type: commonjs` in the dist/cjs folder so
+// that all .js files are treated as CommonJS files.
 writeFileSync('dist/cjs/package.json', JSON.stringify({ type: 'commonjs' }))
 
-// Place a package.json file with `type: module` in the dist/esm folder so that
+// Place a package.json file with `type: module` in the dist folder so that
 // all .js files are treated as ES Module files.
 writeFileSync('dist/package.json', JSON.stringify({ type: 'module' }))
 
