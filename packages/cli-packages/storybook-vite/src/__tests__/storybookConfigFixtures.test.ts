9e694af825a04d21952acc9b241f0b5b21f56198
ari <ari@mendelow.me>
2024-07-19 15:29:23 -0700

    Add test for config fixture content

---
+++ packages/cli-packages/storybook-vite/src/__tests__/storybookConfigFixtures.test.ts
@@ -0,0 +1,40 @@
+import fs from 'fs'
+
+import { describe, it, expect } from 'vitest'
+
+describe('storybook config file fixtures', () => {
+  it('main.js', () => {
+    const mainTemplatePath = `${__dirname}/../commands/templates/main.ts.template`
+    console.log('mainTemplatePath', mainTemplatePath)
+    const mainTemplate = fs.readFileSync(mainTemplatePath, { encoding: 'utf8' })
+    expect(mainTemplate)
+      .toEqual(`import type { StorybookConfig } from 'storybook-framework-redwoodjs-vite'
+
+import { getPaths, importStatementPath } from '@redwoodjs/project-config'
+
+const redwoodProjectPaths = getPaths()
+
+const config: StorybookConfig = {
+  framework: 'storybook-framework-redwoodjs-vite',
+
+  stories: [
+    \`\${importStatementPath(
+      redwoodProjectPaths.web.src
+    )}/**/*.stories.@(js|jsx|ts|tsx|mdx)\`,
+  ],
+
+  addons: ['@storybook/addon-essentials'],
+}
+
+export default config
+`)
+  })
+
+  it('preview-body.html', () => {
+    const previewBodyTemplatePath = `${__dirname}/../commands/templates/preview-body.html.template`
+    const previewBodyHtml = fs.readFileSync(previewBodyTemplatePath, {
+      encoding: 'utf8',
+    })
+    expect(previewBodyHtml).toEqual('<div id="redwood-app"></div>')
+  })
+})
--- packages/cli-packages/storybook-vite/src/__tests__/storybookHandler.test.ts
+++ packages/cli-packages/storybook-vite/src/__tests__/storybookHandler.test.ts
@@ -17,6 +17,9 @@ vi.mock('@redwoodjs/project-config', () => {
           base: '/redwood-app/web',
           storybook: '/redwood-app/web/src/storybook',
         },
+        api: {
+          base: '/redwood-app/api',
+        },
         generated: {
           types: {
             includes: '/redwood-app/web/src/generated',
@@ -59,11 +62,12 @@ describe('storybookHandler', () => {
     // Note: The `undefined` here is because the contents to the writeFileSync
     // are undefined during tests because we are also mocking the readFileSync
     // function and this means the templates are undefined.
+    // Instead, we test the contents of the templates in a separate test (storybookConfigFixtures.test.ts).
     const storybookConfigPath = getPaths().web.storybook
     expect(fs.writeFileSync).toHaveBeenCalledTimes(2)
     expect(fs.writeFileSync).toHaveBeenNthCalledWith(
       1,
-      `${storybookConfigPath}/main.ts`,
+      `${storybookConfigPath}/main.js`, // because we aren't scaffolding tsconfig files, it'll think it's a JS project, and write out the JS version of the file
       undefined,
     )
     expect(fs.writeFileSync).toHaveBeenNthCalledWith(
@@ -71,7 +75,5 @@ describe('storybookHandler', () => {
       `${storybookConfigPath}/preview-body.html`,
       undefined,
     )
-
-    // TODO add tests to verify content...
   })
 })
