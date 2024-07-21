2c8e4efe1feb620d88875344fdc5af90be6247ba
Tobbe Lundberg <tobbe@tlundberg.com>
2024-07-21 23:18:36 +0200

    Improve error handling

--- src/error.ts
+++ src/error.ts
@@ -6,3 +6,40 @@ export class ExitCodeError extends Error {
     this.exitCode = exitCode
   }
 }
+
+export function handleError(e: unknown) {
+  // using process.exitCode instead of `process.exit(1) to give Node a chance to properly
+  // clean up
+  // See https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/no-process-exit.md
+
+  if (e instanceof ExitCodeError) {
+    if (e.exitCode === 0) {
+      console.log('👋 Exiting')
+    } else {
+      console.log()
+      console.error('🚨 An error occurred:')
+      console.error(e.message)
+    }
+
+    process.exitCode = e.exitCode
+  } else if (typeof e === 'string' || e instanceof String) {
+    console.log()
+
+    if (e) {
+      console.error('🚨 An error occurred:')
+      console.error(e)
+    } else {
+      console.error('🚨 An error occurred')
+    }
+  } else {
+    console.log()
+    if ('message' in e) {
+      console.error('🚨 An error occurred:')
+      console.error(e)
+    } else {
+      console.error('🚨 An error occurred')
+    }
+
+    process.exitCode = 1
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 
 import { initConfig } from './config.js'
 import { downloadTemplate } from './download.js'
-import { ExitCodeError } from './error.js'
+import { handleError } from './error.js'
 import { initialCommit } from './git.js'
 import { install } from './install.js'
 import { setInstallationDir } from './installationDir.js'
@@ -25,24 +25,5 @@ try {
 
   printDone(config)
 } catch (e) {
-  // using process.exitCode instead of `process.exit(1) to give Node a chance to properly
-  // clean up
-  // See https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/no-process-exit.md
-
-  if (e instanceof ExitCodeError) {
-    if (e.exitCode === 0) {
-      console.log('👋 Exiting')
-    } else {
-      console.log()
-      console.error('🚨 An error occurred:')
-      console.error(e.message)
-    }
-
-    process.exitCode = e.exitCode
-  } else {
-    console.log()
-    console.error('🚨 An error occurred:')
-    console.error(e)
-    process.exitCode = 1
-  }
+  handleError(e)
 }
