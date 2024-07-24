44dfe2e01f03d1a0b126d23a36092b658f020bd9
Tobbe Lundberg <tobbe@tlundberg.com>
2024-07-24 19:05:48 +0200

    RscCache explainatory comments

--- packages/router/src/rsc/RscCache.ts
+++ packages/router/src/rsc/RscCache.ts
@@ -1,3 +1,10 @@
+/**
+ * This cache is used for RSC fetches, so that we don't re-fetch the same
+ * component (i.e. page) multiple times and get stuck in a loop.
+ *
+ * `key`: A stringified location-like object.
+ * `value`: A Promise that resolves to a React element.
+ */
 export class RscCache {
   private cache = new Map<string, Thenable<React.ReactElement>>()
   private socket: WebSocket
@@ -14,19 +21,63 @@ export class RscCache {
     // Event listener for incoming messages
     this.socket.addEventListener('message', (event) => {
       console.log('Incomming message', event)
+      if (event.data.startsWith('{')) {
+        const data = JSON.parse(event.data)
+
+        console.log('Incomming message id', data.id)
+        console.log('Incomming message key', data.key)
+
+        if (data.id === 'rsc-cache-delete') {
+          if (!this.cache.has(data.key)) {
+            console.error('')
+            console.error(
+              'RscCache::message::rsc-cache-delete key not found in cache',
+            )
+            console.error('')
+          }
+          this.cache.delete(data.key)
+
+          this.sendToWebSocket('update', {
+            fullCache: Object.fromEntries(
+              Array.from(this.cache.entries()).map(
+                ([location, elementThenable]) => [
+                  location,
+                  // @ts-expect-error hack to get the value of a Thenable
+                  elementThenable.value,
+                ],
+              ),
+            ),
+          })
+        }
+      }
     })
   }
 
-  get(key: string): any {
+  get(key: string): Thenable<React.ReactElement> | undefined {
     const value = this.cache.get(key)
     console.log('RscCache.get', key, value)
     return value
   }
 
-  set(key: string, value: any) {
+  set(key: string, value: Thenable<React.ReactElement>) {
     console.log('RscCache.set', key, value)
-    this.sendToWebSocket('set', { key, value })
     this.cache.set(key, value)
+
+    // There's no point in sending a Promise over the WebSocket, so we wait for
+    // it to resolve before sending the value.
+    value.then((resolvedValue) => {
+      console.log('RscCache.set key:', key)
+      console.log('RscCache.set resolved value:', resolvedValue)
+      this.sendToWebSocket('set', {
+        updatedKey: key,
+        fullCache: Object.fromEntries(
+          Array.from(this.cache.entries()).map(
+            // @ts-expect-error hack to get the value of a Thenable
+            ([location, elementThenable]) => [location, elementThenable.value],
+          ),
+        ),
+      })
+    })
   }
 
   private sendToWebSocket(action: string, payload: Record<string, any>) {
--- packages/vite/src/runFeServer.ts
+++ packages/vite/src/runFeServer.ts
@@ -210,20 +210,20 @@ export async function runFeServer() {
 }
 
 function createWebSocketServer() {
-  console.log('createWebSocketServer')
   const wsServer = new WebSocketServer({ port: 18998 })
 
   wsServer.on('connection', (ws) => {
     console.log('A new client connected.')
 
-    // Event listener for incoming messages
-    ws.on('message', (message) => {
-      console.log('Received message:', message.toString())
+    // Event listener for incoming messages. The `data` is a Buffer
+    ws.on('message', (data) => {
+      const message = data.toString()
+      console.log('Received message:', message)
 
       // Broadcast the message to all connected clients
       wsServer.clients.forEach((client) => {
         if (client.readyState === WebSocket.OPEN) {
-          client.send(message.toString())
+          client.send(message)
         }
       })
     })
