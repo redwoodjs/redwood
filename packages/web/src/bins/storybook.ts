372b39553999dd064e61c15b127506a470973a04
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2024-07-24 14:13:47 +0100

    fix bin change

--- packages/web/src/bins/storybook.ts
+++ packages/web/src/bins/storybook.ts
@@ -9,13 +9,13 @@ function isErrorWithCode(error: any): error is { code: string } {
 // installed before we try to run it.
 try {
   const requireFromStorybook = createRequire(
-    require.resolve('storybook-vite/package.json'),
+    require.resolve('storybook/package.json'),
   )
   const bins = requireFromStorybook('./package.json')['bin']
-  requireFromStorybook(bins['storybook-vite'])
+  requireFromStorybook(bins['storybook'])
 } catch (error) {
   if (isErrorWithCode(error) && error.code === 'MODULE_NOT_FOUND') {
-    console.error('Storybook Vite is not currently installed.')
+    console.error('Storybook is not currently installed.')
     process.exit(1)
   }
 
