b41ad99a819c3a9a3d9b8b714b1eb0235cc8d8d5
Rob Cameron <cannikin@fastmail.com>
2024-07-27 09:00:01 -0700

    Update packages/jobs/src/core/consts.ts
    Co-authored-by: Tobbe Lundberg <tobbe@tlundberg.com>

--- packages/jobs/src/core/consts.ts
+++ packages/jobs/src/core/consts.ts
@@ -3,7 +3,8 @@ import console from 'node:console'
 export const DEFAULT_MAX_ATTEMPTS = 24
 /** 4 hours in seconds */
 export const DEFAULT_MAX_RUNTIME = 14_400
-export const DEFAULT_SLEEP_DELAY = 5 // 5 seconds
+/** 5 seconds */
+export const DEFAULT_SLEEP_DELAY = 5
 export const DEFAULT_DELETE_FAILED_JOBS = false
 export const DEFAULT_LOGGER = console
 export const DEFAULT_QUEUE = 'default'
