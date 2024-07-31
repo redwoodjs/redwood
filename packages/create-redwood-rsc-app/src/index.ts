5bb2f5c3d61978e42a8139674cab0ff153b30bad
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2024-07-31 22:42:21 +0100

    initial basic

--- packages/create-redwood-rsc-app/src/index.ts
+++ packages/create-redwood-rsc-app/src/index.ts
@@ -11,10 +11,13 @@ import { setInstallationDir } from './installationDir.js'
 import { relaunchOnLatest, shouldRelaunch } from './latest.js'
 import { printDone, printWelcome } from './messages.js'
 import { checkNodeVersion, checkYarnInstallation } from './prerequisites.js'
+import { sendTelemetry } from './telemetry.js'
 import { upgradeToLatestCanary } from './upgradeToLatestCanary.js'
 import { unzip } from './zip.js'
 
 let config: Config | null = null
+
+const startTime = Date.now()
 
 try {
   config = initConfig()
@@ -42,3 +45,5 @@ if (shouldRelaunch(config)) {
     handleError(e)
   }
 }
+
+await sendTelemetry(config, Date.now() - startTime)
