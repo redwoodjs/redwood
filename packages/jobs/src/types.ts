9e302e4d9a05caeab179efaae2ddc61517aa1ef4
Rob Cameron <cannikin@fastmail.com>
2024-08-05 16:24:13 -0700

    Removes unused types

--- packages/jobs/src/types.ts
+++ packages/jobs/src/types.ts
@@ -1,5 +1,3 @@
-import type { RedwoodJob } from './core/RedwoodJob'
-
 // Defines the basic shape of a logger that RedwoodJob will invoke to print
 // debug messages. RedwoodJob will fallback to use `console` if no
 // logger is passed in to RedwoodJob or any adapter. Luckily both Redwood's
@@ -9,10 +7,4 @@ export interface BasicLogger {
   info: (message?: any, ...optionalParams: any[]) => void
   warn: (message?: any, ...optionalParams: any[]) => void
   error: (message?: any, ...optionalParams: any[]) => void
-}
-
-// The type of the `jobs` object that's exported from api/src/lib/jobs.ts that
-// contains an instance of all jobs that can be run
-export interface AvailableJobs {
-  [key: string]: RedwoodJob
 }
