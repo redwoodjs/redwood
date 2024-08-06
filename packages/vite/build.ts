0695012446a7342ae7bc9860c752d4b0440e82a8
Tobbe Lundberg <tobbe@tlundberg.com>
2024-08-06 14:23:07 +0200

    chore(rwjs/vite): Rename build script and format source

--- packages/vite/build.ts
+++ packages/vite/build.ts
@@ -1,13 +1,13 @@
+import { writeFileSync } from 'node:fs'
+
+import { commonjs } from '@hyrious/esbuild-plugin-commonjs'
+import * as esbuild from 'esbuild'
+
 import {
   build,
   defaultBuildOptions,
   defaultIgnorePatterns,
 } from '@redwoodjs/framework-tools'
-
-import { writeFileSync } from 'node:fs'
-import { commonjs } from '@hyrious/esbuild-plugin-commonjs'
-
-import * as esbuild from 'esbuild'
 
 // CJS Build
 await build({
@@ -43,28 +43,43 @@ await esbuild.build({
   conditions: ['react-server'],
   platform: 'node',
   target: ['node20'],
-  // ⭐ Without this plugin, we get "Error: Dynamic require of "util" is not supported"
-  // when trying to run the built files. This plugin will
-  //  "just rewrite that file to replace "require(node-module)" to a toplevel static import statement." (see issue)
+  // Without this plugin, we get "Error: Dynamic require of "util" is not
+  // supported" when trying to run the built files. This plugin will "just
+  // rewrite that file to replace "require(node-module)" to a toplevel static
+  // import statement." (see issue)
   // https://github.com/evanw/esbuild/issues/2113
   // https://github.com/evanw/esbuild/pull/2067
   plugins: [commonjs()],
   logLevel: 'info',
 })
 
-// Place a package.json file with `type: commonjs` in the dist/cjs folder so that
-// all .js files are treated as CommonJS files.
+// Place a package.json file with `type: commonjs` in the dist/cjs folder so
+// that all .js files are treated as CommonJS files.
 writeFileSync('dist/cjs/package.json', JSON.stringify({ type: 'commonjs' }))
 
 // Place a package.json file with `type: module` in the dist folder so that
 // all .js files are treated as ES Module files.
 writeFileSync('dist/package.json', JSON.stringify({ type: 'module' }))
 
-// Add CommonJS types by creating common JS types. Using a .d.ts file, because .d.cts files don't auto resolve
-// Notice I haven't specified thee types in package.json for these - it's following the naming conversion TSC wants.
+// Add CommonJS types by creating common JS types. Using a .d.ts file, because
+// .d.cts files don't auto resolve
+// Notice I haven't specified the types in package.json for these - it's
+// following the naming conversion TSC wants.
 writeFileSync('dist/cjs/index.d.ts', 'export type * from "../index.d.ts"')
-writeFileSync('dist/cjs/buildFeServer.d.ts', 'export type * from "../buildFeServer.d.ts"')
+writeFileSync(
+  'dist/cjs/buildFeServer.d.ts',
+  'export type * from "../buildFeServer.d.ts"',
+)
 writeFileSync('dist/cjs/client.d.ts', 'export type * from "../client.d.ts"')
-writeFileSync('dist/cjs/clientSsr.d.ts', 'export type * from "../clientSsr.d.ts"')
-writeFileSync('dist/cjs/ClientRouter.d.ts', 'export type * from "../ClientRouter.d.ts"')
-writeFileSync('dist/cjs/build/build.d.ts', 'export type * from "../../build/build.d.ts"')
+writeFileSync(
+  'dist/cjs/clientSsr.d.ts',
+  'export type * from "../clientSsr.d.ts"',
+)
+writeFileSync(
+  'dist/cjs/ClientRouter.d.ts',
+  'export type * from "../ClientRouter.d.ts"',
+)
+writeFileSync(
+  'dist/cjs/build/build.d.ts',
+  'export type * from "../../build/build.d.ts"',
+)
