781e1ad09bf8cf88ace38156a4cdef991a601b15
Rob Cameron <cannikin@fastmail.com>
2024-08-07 16:20:08 -0700

    Update loads for new job design

--- packages/jobs/src/loaders.ts
+++ packages/jobs/src/loaders.ts
@@ -1,11 +1,11 @@
 import path from 'node:path'
 import { pathToFileURL } from 'node:url'
 
-import fg from 'fast-glob'
-
 import { getPaths } from '@redwoodjs/project-config'
 
+import type { JobManager } from './core/JobManager'
 import { JobsLibNotFoundError, JobNotFoundError } from './errors'
+import type { Adapters, BasicLogger, Job } from './types'
 
 export function makeFilePath(path: string) {
   return pathToFileURL(path).href
@@ -13,26 +13,41 @@ export function makeFilePath(path: string) {
 
 // Loads the named export from the app's jobs config in api/src/lib/jobs.{js,ts}
 // to configure the worker, defaults to `workerConfig`
-export const loadJobsConfig = async () => {
+export const loadJobsManager = (): JobManager<
+  Adapters,
+  string[],
+  BasicLogger
+> => {
   const jobsConfigPath = getPaths().api.distJobsConfig
 
   if (jobsConfigPath) {
-    return require(jobsConfigPath)
+    return require(jobsConfigPath).jobs
   } else {
     throw new JobsLibNotFoundError()
   }
 }
 
 // Loads a job from the app's filesystem in api/src/jobs
-export const loadJob = async (name: string) => {
+export const loadJob = ({
+  name: jobName,
+  path: jobPath,
+}: {
+  name: string
+  path: string
+}): Job<string[], unknown[]> => {
   const jobsPath = getPaths().api.distJobs
 
-  // Specifying {js,ts} extensions, so we don't accidentally try to load .json
-  // files or similar
-  const files = fg.sync(`**/${name}.{js,ts}`, { cwd: jobsPath })
-  if (!files[0]) {
-    throw new JobNotFoundError(name)
+  let job
+
+  try {
+    job = require(path.join(jobsPath, jobPath))
+  } catch (e) {
+    throw new JobNotFoundError(jobName)
   }
-  const jobModule = require(path.join(jobsPath, files[0]))
-  return jobModule
+
+  if (!job[jobName]) {
+    throw new JobNotFoundError(jobName)
+  }
+
+  return job[jobName]
 }
