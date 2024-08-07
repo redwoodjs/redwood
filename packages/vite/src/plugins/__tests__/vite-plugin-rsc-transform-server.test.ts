7dd63adb65107ed77f1767a1a6706234ee3a11ca
Tobbe Lundberg <tobbe@tlundberg.com>
2024-08-07 10:13:36 +0200

    Fix TS issues with transform-server test

--- packages/vite/src/plugins/__tests__/vite-plugin-rsc-transform-server.test.ts
+++ packages/vite/src/plugins/__tests__/vite-plugin-rsc-transform-server.test.ts
@@ -1,4 +1,5 @@
 import { vol } from 'memfs'
+import type { TransformPluginContext } from 'rollup'
 import {
   afterAll,
   beforeAll,
@@ -9,26 +10,38 @@ import {
   afterEach,
 } from 'vitest'
 
-import { rscTransformUseServerPlugin } from '../vite-plugin-rsc-transform-server'
+import { rscTransformUseServerPlugin } from '../vite-plugin-rsc-transform-server.js'
 
 vi.mock('fs', async () => ({ default: (await import('memfs')).fs }))
 
 const RWJS_CWD = process.env.RWJS_CWD
 
 beforeAll(() => {
-  // Add a toml entry for getPaths et al.
   process.env.RWJS_CWD = '/Users/tobbe/rw-app/'
-  vol.fromJSON(
-    {
-      'redwood.toml': '',
-    },
-    process.env.RWJS_CWD,
-  )
+
+  // Add a toml entry for getPaths et al.
+  vol.fromJSON({ 'redwood.toml': '' }, process.env.RWJS_CWD)
 })
 
 afterAll(() => {
   process.env.RWJS_CWD = RWJS_CWD
 })
+
+function getPluginTransform() {
+  const plugin = rscTransformUseServerPlugin()
+
+  if (typeof plugin.transform !== 'function') {
+    throw new Error('Plugin does not have a transform function')
+  }
+
+  // Calling `bind` to please TS
+  // See https://stackoverflow.com/a/70463512/88106
+  // Typecasting because we're only going to call transform, and we don't need
+  // anything provided by the context.
+  return plugin.transform.bind({} as TransformPluginContext)
+}
+
+const pluginTransform = getPluginTransform()
 
 describe('rscTransformUseServerPlugin', () => {
   afterEach(() => {
@@ -36,12 +49,6 @@ describe('rscTransformUseServerPlugin', () => {
   })
 
   it('should handle module scoped "use server"', async () => {
-    const plugin = rscTransformUseServerPlugin()
-
-    if (typeof plugin.transform !== 'function') {
-      expect.fail('Expected plugin to have a transform function')
-    }
-
     const id = 'some/path/to/actions.ts'
     const input = `'use server'
 
@@ -54,7 +61,11 @@ describe('rscTransformUseServerPlugin', () => {
         )
       }`
 
-    const output = await plugin.transform.bind({})(input, id)
+    const output = await pluginTransform(input, id)
+
+    if (typeof output !== 'string') {
+      throw new Error('Expected output to be a string')
+    }
 
     expect(
       output.split('\n').some((line) => line.startsWith('"use server"')),
