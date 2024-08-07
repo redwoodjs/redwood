4acf7891c7bc4967773590cddd01e85104f55c61
Rob Cameron <cannikin@fastmail.com>
2024-08-07 16:17:57 -0700

    Remove type export in index

--- packages/jobs/src/index.ts
+++ packages/jobs/src/index.ts
@@ -7,6 +7,4 @@ export { Worker } from './core/Worker'
 export { BaseAdapter } from './adapters/BaseAdapter'
 export { PrismaAdapter } from './adapters/PrismaAdapter'
 
-export type { WorkerConfig } from './core/Worker'
-
 // TODO(jgmw): We tend to avoid wanting to barrel export everything
