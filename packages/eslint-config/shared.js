d47cad0076dc32479b5705b2693263834c11acb8
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2024-08-13 04:32:04 +0100

    disable new rules for now

--- packages/eslint-config/shared.js
+++ packages/eslint-config/shared.js
@@ -154,6 +154,8 @@ module.exports = {
         // 'recommended' rules we alter
         '@typescript-eslint/no-explicit-any': 'warn',
         '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/no-require-imports': 'off',
+        '@typescript-eslint/no-empty-object-type': 'off',
         '@typescript-eslint/no-unused-vars': [
           'error',
           { varsIgnorePattern: '^_', argsIgnorePattern: '^_' },
