594ab3eebd2edbe49a2edebb313ecf877c2a659f
Rob Cameron <cannikin@fastmail.com>
2024-08-13 13:21:31 -0700

    Remove unused babel mock

--- packages/jobs/src/bins/__tests__/rw-jobs-worker.test.js
+++ packages/jobs/src/bins/__tests__/rw-jobs-worker.test.js
@@ -2,9 +2,6 @@ import { describe, expect, vi, it } from 'vitest'
 
 // import * as worker from '../worker'
 
-// so that registerApiSideBabelHook() doesn't freak out about redwood.toml
-vi.mock('@redwoodjs/babel-config')
-
 describe('worker', () => {
   it('placeholder', () => {
     expect(true).toBeTruthy()
