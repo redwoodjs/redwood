88db895640d749695636431d73994846176e2fdf
Rob Cameron <cannikin@fastmail.com>
2024-08-13 13:18:51 -0700

    Add adapter exports

--- packages/jobs/src/index.ts
+++ packages/jobs/src/index.ts
@@ -3,3 +3,6 @@ export * from './errors'
 export { JobManager } from './core/JobManager'
 export { Executor } from './core/Executor'
 export { Worker } from './core/Worker'
+
+export { BaseAdapter } from './adapters/BaseAdapter/BaseAdapter'
+export { PrismaAdapter } from './adapters/PrismaAdapter/PrismaAdapter'
