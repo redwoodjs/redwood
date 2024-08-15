46c0cd119f18dc02f8fc6f139b01ef0030ee4497
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2024-08-15 05:21:50 +0100

    type workaround until vitest

--- packages/graphql-server/src/plugins/__tests__/useRedwoodError.test.ts
+++ packages/graphql-server/src/plugins/__tests__/useRedwoodError.test.ts
@@ -1,7 +1,10 @@
 import type { APIGatewayProxyEvent, Context } from 'aws-lambda'
 import { CurrencyDefinition, CurrencyResolver } from 'graphql-scalars'
 
-import { RedwoodError, EmailValidationError } from '@redwoodjs/api'
+import type {
+  RedwoodError as RedwoodErrorType,
+  EmailValidationError as EmailValidationErrorType,
+} from '@redwoodjs/api'
 import { createLogger } from '@redwoodjs/api/logger'
 
 import { createGraphQLHandler } from '../../functions/graphql'
@@ -15,6 +18,10 @@ jest.mock('../../makeMergedSchema', () => {
   } = require('@redwoodjs/graphql-server/dist/errors')
 
   const { CurrencyResolver } = require('graphql-scalars')
+  const { RedwoodError, EmailValidationError } = require('@redwoodjs/api') as {
+    RedwoodError: typeof RedwoodErrorType
+    EmailValidationError: typeof EmailValidationErrorType
+  }
 
   class WeatherError extends RedwoodError {
     constructor(message: string, extensions?: Record<string, any>) {
