818032751b1c6adcd1069ec14a62538b59b40cac
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2024-08-15 15:40:14 +0100

    chore(lint): enable 'typescript-eslint/prefer-includes' (#11261)
    Enables the `@typescript-eslint/prefer-includes` rule and addresses and
    errors which result.

--- .eslintrc.js
+++ .eslintrc.js
@@ -221,7 +221,6 @@ module.exports = {
         '@typescript-eslint/restrict-template-expressions': 'off',
         '@typescript-eslint/non-nullable-type-assertion-style': 'off',
         '@typescript-eslint/no-implied-eval': 'off',
-        '@typescript-eslint/prefer-includes': 'off',
         '@typescript-eslint/no-base-to-string': 'off',
         '@typescript-eslint/no-duplicate-type-constituents': 'off',
         '@typescript-eslint/unbound-method': 'off',
