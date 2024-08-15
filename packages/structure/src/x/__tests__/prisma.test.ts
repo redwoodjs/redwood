febd1bab89b554ddfad5bfd47b2bc1f715a3607b
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2024-08-15 23:18:26 +0100

    structure

--- packages/structure/src/x/__tests__/prisma.test.ts
+++ packages/structure/src/x/__tests__/prisma.test.ts
@@ -4,7 +4,7 @@ import { Range } from 'vscode-languageserver'
 import { prisma_parseEnvExpressions } from '../prisma'
 
 describe('prisma_parseEnvExpressions', () => {
-  it('can find env() expressions in a prisma schema', async () => {
+  it('can find env() expressions in a prisma schema', () => {
     const [r] = Array.from(prisma_parseEnvExpressions(`env("foo") `))
     const range = Range.create(0, 0, 0, 10)
     expect(r).toEqual({ range, key: 'foo' })
