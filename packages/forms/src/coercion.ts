bbf16c1a3c2eff5c3afae2b9093ade15afd4185f
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2024-08-16 01:21:11 +0100

    chore(lint): enable 'typescript-eslint/prefer-string-starts-ends-with' (#11280)
    Enables the `typescript-eslint/prefer-string-starts-ends-with` rule and
    fixes issues that resulted.

--- packages/forms/src/coercion.ts
+++ packages/forms/src/coercion.ts
@@ -260,6 +260,6 @@ export const setCoercion = (
     valueAs, // type
     emptyAs, // emptyAs
     validation.required !== undefined && validation.required !== false, // required
-    /Id$/.test(name || ''), // isId
+    (name || '').endsWith('Id'), // isId
   )
 }
