289cb6249ad8972807b203225c46d98b7edafd36
Silas Smith <silas.smith@gmail.com>
2024-08-17 14:19:31 -0700

    Detect/resolve ambiguous script names (#9848)
    Co-authored-by: Tobbe Lundberg <tobbe@tlundberg.com>

--- packages/cli/src/commands/__tests__/exec.test.ts
+++ packages/cli/src/commands/__tests__/exec.test.ts
@@ -9,12 +9,22 @@ import { handler } from '../execHandler'
 vi.mock('envinfo', () => ({ default: { run: () => '' } }))
 vi.mock('@redwoodjs/project-config', () => ({
   getPaths: () => ({
-    scripts: path.join(
-      __dirname,
-      '../../../../../__fixtures__/test-project/scripts',
-    ),
+    scripts: path.join('redwood-app', 'scripts'),
   }),
   resolveFile: (path: string) => path,
+}))
+vi.mock('@redwoodjs/internal/dist/files', () => ({
+  findScripts: () => {
+    const scriptsPath = path.join('redwood-app', 'scripts')
+
+    return [
+      path.join(scriptsPath, 'one', 'two', 'myNestedScript.ts'),
+      path.join(scriptsPath, 'conflicting.js'),
+      path.join(scriptsPath, 'conflicting.ts'),
+      path.join(scriptsPath, 'normalScript.ts'),
+      path.join(scriptsPath, 'secondNormalScript.ts'),
+    ]
+  },
 }))
 
 const mockRedwoodToml = {
@@ -47,6 +57,26 @@ describe('yarn rw exec --list', () => {
       .replaceAll('\\', '\\\\')
     expect(vi.mocked(console).log).toHaveBeenCalledWith(
       expect.stringMatching(new RegExp('\\b' + scriptPath + '\\b')),
+    )
+  })
+
+  it("does not include the file extension if there's no ambiguity", async () => {
+    await handler({ list: true })
+    expect(vi.mocked(console).log).toHaveBeenCalledWith(
+      expect.stringMatching(new RegExp('\\bnormalScript\\b')),
+    )
+    expect(vi.mocked(console).log).toHaveBeenCalledWith(
+      expect.stringMatching(new RegExp('\\bsecondNormalScript\\b')),
+    )
+  })
+
+  it('includes the file extension if there could be ambiguity', async () => {
+    await handler({ list: true })
+    expect(vi.mocked(console).log).toHaveBeenCalledWith(
+      expect.stringMatching(new RegExp('\\bconflicting.js\\b')),
+    )
+    expect(vi.mocked(console).log).toHaveBeenCalledWith(
+      expect.stringMatching(new RegExp('\\bconflicting.ts\\b')),
     )
   })
 })
