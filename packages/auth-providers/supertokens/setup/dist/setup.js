e47ec6858f1a0f3aea9780c9991ebd76f7b5f860
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2024-08-17 23:14:59 +0100

    supertokens

--- packages/auth-providers/supertokens/setup/dist/setup.js
+++ packages/auth-providers/supertokens/setup/dist/setup.js
@@ -1,21 +1,53 @@
 "use strict";
-
-var _Object$defineProperty = require("@babel/runtime-corejs3/core-js/object/define-property");
-_Object$defineProperty(exports, "__esModule", {
-  value: true
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  // If the importer is in node compatibility mode or this is not an ESM
+  // file that has been converted to a CommonJS file using a Babel-
+  // compatible transform (i.e. "__esModule" has not been set), then set
+  // "default" to the CommonJS "module.exports" for node compatibility.
+  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+  mod
+));
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+var setup_exports = {};
+__export(setup_exports, {
+  builder: () => builder,
+  command: () => command,
+  description: () => description,
+  handler: () => handler
 });
-exports.builder = builder;
-exports.description = exports.command = void 0;
-exports.handler = handler;
-var _cliHelpers = require("@redwoodjs/cli-helpers");
-const command = exports.command = 'supertokens';
-const description = exports.description = 'Set up auth for for SuperTokens';
+module.exports = __toCommonJS(setup_exports);
+var import_cli_helpers = require("@redwoodjs/cli-helpers");
+const command = "supertokens";
+const description = "Set up auth for for SuperTokens";
 async function builder(yargs) {
-  return (0, _cliHelpers.standardAuthBuilder)(yargs);
+  return (0, import_cli_helpers.standardAuthBuilder)(yargs);
 }
 async function handler(options) {
-  const {
-    handler
-  } = await import('./setupHandler.js');
-  return handler(options);
+  const { handler: handler2 } = await import("./setupHandler.js");
+  return handler2(options);
 }
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
+  builder,
+  command,
+  description,
+  handler
+});
--- packages/auth-providers/supertokens/setup/dist/setupHandler.js
+++ packages/auth-providers/supertokens/setup/dist/setupHandler.js
@@ -1,62 +1,101 @@
 "use strict";
-
-var _Object$defineProperty = require("@babel/runtime-corejs3/core-js/object/define-property");
-var _interopRequireDefault = require("@babel/runtime-corejs3/helpers/interopRequireDefault").default;
-_Object$defineProperty(exports, "__esModule", {
-  value: true
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  // If the importer is in node compatibility mode or this is not an ESM
+  // file that has been converted to a CommonJS file using a Babel-
+  // compatible transform (i.e. "__esModule" has not been set), then set
+  // "default" to the CommonJS "module.exports" for node compatibility.
+  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+  mod
+));
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+var setupHandler_exports = {};
+__export(setupHandler_exports, {
+  addRoutingLogic: () => addRoutingLogic,
+  handler: () => handler
 });
-exports.addRoutingLogic = void 0;
-exports.handler = handler;
-var _reduce = _interopRequireDefault(require("@babel/runtime-corejs3/core-js/instance/reduce"));
-var _includes = _interopRequireDefault(require("@babel/runtime-corejs3/core-js/instance/includes"));
-require("core-js/modules/es.array.push.js");
-require("core-js/modules/esnext.json.parse.js");
-var _fs = _interopRequireDefault(require("fs"));
-var _path = _interopRequireDefault(require("path"));
-var _cliHelpers = require("@redwoodjs/cli-helpers");
-async function handler({
-  force: forceArg
-}) {
-  const {
-    version
-  } = JSON.parse(_fs.default.readFileSync(_path.default.resolve(__dirname, '../package.json'), 'utf-8'));
-  (0, _cliHelpers.standardAuthHandler)({
+module.exports = __toCommonJS(setupHandler_exports);
+var import_fs = __toESM(require("fs"));
+var import_path = __toESM(require("path"));
+var import_cli_helpers = require("@redwoodjs/cli-helpers");
+async function handler({ force: forceArg }) {
+  const { version } = JSON.parse(
+    import_fs.default.readFileSync(import_path.default.resolve(__dirname, "../package.json"), "utf-8")
+  );
+  (0, import_cli_helpers.standardAuthHandler)({
     basedir: __dirname,
     forceArg,
-    provider: 'supertokens',
+    provider: "supertokens",
     authDecoderImport: "import { authDecoder } from '@redwoodjs/auth-supertokens-api'",
-    apiPackages: [`@redwoodjs/auth-supertokens-api@${version}`, 'supertokens-node@^15'],
-    webPackages: [`@redwoodjs/auth-supertokens-web@${version}`, 'supertokens-auth-react@~0.34.0', 'supertokens-web-js@~0.7.0'],
+    apiPackages: [
+      `@redwoodjs/auth-supertokens-api@${version}`,
+      "supertokens-node@^15"
+    ],
+    webPackages: [
+      `@redwoodjs/auth-supertokens-web@${version}`,
+      "supertokens-auth-react@~0.34.0",
+      "supertokens-web-js@~0.7.0"
+    ],
     extraTasks: [addRoutingLogic],
-    notes: ["We've implemented SuperToken's EmailPassword with Social / Enterprise (OAuth 2.0, SAML) login recipe,", 'but feel free to switch to something that better fits your needs. See https://supertokens.com/docs/guides.', '', "To get things working, you'll need to add quite a few env vars to your .env file.", 'See https://redwoodjs.com/docs/auth/supertokens for a full walkthrough.']
+    notes: [
+      "We've implemented SuperToken's EmailPassword with Social / Enterprise (OAuth 2.0, SAML) login recipe,",
+      "but feel free to switch to something that better fits your needs. See https://supertokens.com/docs/guides.",
+      "",
+      "To get things working, you'll need to add quite a few env vars to your .env file.",
+      "See https://redwoodjs.com/docs/auth/supertokens for a full walkthrough."
+    ]
   });
 }
-
-// Exported for testing.
-const addRoutingLogic = exports.addRoutingLogic = {
+const addRoutingLogic = {
   title: `Adding SuperTokens routing logic to Routes.{jsx,tsx}...`,
   task: () => {
-    const routesPath = (0, _cliHelpers.getPaths)().web.routes;
-    let content = _fs.default.readFileSync(routesPath, 'utf-8');
-
-    // Remove the old setup if it's there.
-    content = content.replace("import SuperTokens from 'supertokens-auth-react'", '').replace(/if \(SuperTokens.canHandleRoute\(\)\) {[^}]+}/, '');
+    const routesPath = (0, import_cli_helpers.getPaths)().web.routes;
+    let content = import_fs.default.readFileSync(routesPath, "utf-8");
+    content = content.replace("import SuperTokens from 'supertokens-auth-react'", "").replace(/if \(SuperTokens.canHandleRoute\(\)\) {[^}]+}/, "");
     if (!/\s*if\s*\(canHandleRoute\(PreBuiltUI\)\)\s*\{/.test(content)) {
-      var _context;
       let hasImportedSuperTokensFunctions = false;
-      content = (0, _reduce.default)(_context = content.split('\n')).call(_context, (acc, line) => {
-        // Add the SuperTokens import before the first import from a RedwoodJS package.
-        if (!hasImportedSuperTokensFunctions && (0, _includes.default)(line).call(line, 'import') && (0, _includes.default)(line).call(line, '@redwoodjs')) {
-          acc.push("import { canHandleRoute, getRoutingComponent } from 'supertokens-auth-react/ui'");
-          acc.push('');
+      content = content.split("\n").reduce((acc, line) => {
+        if (!hasImportedSuperTokensFunctions && line.includes("import") && line.includes("@redwoodjs")) {
+          acc.push(
+            "import { canHandleRoute, getRoutingComponent } from 'supertokens-auth-react/ui'"
+          );
+          acc.push("");
           hasImportedSuperTokensFunctions = true;
         }
         acc.push(line);
         return acc;
-      }, []).join('\n');
-      content = content.replace("import { useAuth } from './auth'", "import { useAuth, PreBuiltUI } from './auth'");
-      content = content.replace(/const Routes = \(\) => \{\n/, 'const Routes = () => {\n' + '  if (canHandleRoute(PreBuiltUI)) {\n' + '    return getRoutingComponent(PreBuiltUI)\n' + '  }\n\n');
-      _fs.default.writeFileSync(routesPath, content);
+      }, []).join("\n");
+      content = content.replace(
+        "import { useAuth } from './auth'",
+        "import { useAuth, PreBuiltUI } from './auth'"
+      );
+      content = content.replace(
+        /const Routes = \(\) => \{\n/,
+        "const Routes = () => {\n  if (canHandleRoute(PreBuiltUI)) {\n    return getRoutingComponent(PreBuiltUI)\n  }\n\n"
+      );
+      import_fs.default.writeFileSync(routesPath, content);
     }
   }
 };
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
+  addRoutingLogic,
+  handler
+});
