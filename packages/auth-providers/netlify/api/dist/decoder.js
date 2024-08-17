c7900e1e2e6ac28e36c58f8814a532fda022b699
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2024-08-17 19:05:37 +0100

    netlify

--- packages/auth-providers/netlify/api/dist/decoder.js
+++ packages/auth-providers/netlify/api/dist/decoder.js
@@ -1,31 +1,49 @@
 "use strict";
-
-var _Object$defineProperty = require("@babel/runtime-corejs3/core-js/object/define-property");
-var _interopRequireWildcard = require("@babel/runtime-corejs3/helpers/interopRequireWildcard").default;
-var _interopRequireDefault = require("@babel/runtime-corejs3/helpers/interopRequireDefault").default;
-_Object$defineProperty(exports, "__esModule", {
-  value: true
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  // If the importer is in node compatibility mode or this is not an ESM
+  // file that has been converted to a CommonJS file using a Babel-
+  // compatible transform (i.e. "__esModule" has not been set), then set
+  // "default" to the CommonJS "module.exports" for node compatibility.
+  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+  mod
+));
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+var decoder_exports = {};
+__export(decoder_exports, {
+  authDecoder: () => authDecoder
 });
-exports.authDecoder = void 0;
-var _now = _interopRequireDefault(require("@babel/runtime-corejs3/core-js/date/now"));
-var _jsonwebtoken = _interopRequireWildcard(require("jsonwebtoken"));
+module.exports = __toCommonJS(decoder_exports);
+var import_jsonwebtoken = __toESM(require("jsonwebtoken"));
 const authDecoder = async (token, type, req) => {
-  if (type !== 'netlify') {
+  if (type !== "netlify") {
     return null;
   }
-
-  // Netlify verifies and decodes the JWT before the request is passed to our
-  // Serverless function, so the decoded JWT is already available in production.
-  // For development and test we can't verify the token because we don't have
-  // the signing key. Just decoding the token is the best we can do to emulate
-  // the native Netlify experience
-  if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
-    // In dev, we don't have access to the JWT private key to verify
-    // So we simulate a verification
-    const decodedToken = _jsonwebtoken.default.decode(token);
-    const nowTimestamp = Math.floor((0, _now.default)() / 1000);
+  if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test") {
+    const decodedToken = import_jsonwebtoken.default.decode(token);
+    const nowTimestamp = Math.floor(Date.now() / 1e3);
     if (nowTimestamp >= decodedToken.exp) {
-      throw new _jsonwebtoken.TokenExpiredError('jwt expired', new Date(decodedToken.exp * 1000));
+      throw new import_jsonwebtoken.TokenExpiredError(
+        "jwt expired",
+        new Date(decodedToken.exp * 1e3)
+      );
     }
     return decodedToken;
   } else {
@@ -33,4 +51,7 @@ const authDecoder = async (token, type, req) => {
     return clientContext?.user || null;
   }
 };
-exports.authDecoder = authDecoder;
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
+  authDecoder
+});
