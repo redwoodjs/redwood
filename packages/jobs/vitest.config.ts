f5be93e6eea0f665734bdd9971591aa037f3ac4d
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2024-08-19 17:11:16 +0100

    remove unneeded includes

--- packages/jobs/vitest.config.ts
+++ packages/jobs/vitest.config.ts
@@ -3,7 +3,6 @@ import { defineConfig, configDefaults } from 'vitest/config'
 export default defineConfig({
   test: {
     testTimeout: 15_000,
-    include: ['**/__tests__/**/*.test.[jt]s'],
     exclude: [...configDefaults.exclude, '**/fixtures', '**/dist'],
   },
 })
