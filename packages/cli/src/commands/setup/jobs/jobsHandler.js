8faad72379f6c77af18c12b2fad2e5a369c4ae1e
Tobbe Lundberg <tobbe@tlundberg.com>
2024-08-21 18:58:05 +0200

    don't warn

--- packages/cli/src/commands/setup/jobs/jobsHandler.js
+++ packages/cli/src/commands/setup/jobs/jobsHandler.js
@@ -113,10 +113,10 @@ const tasks = async ({ force }) => {
           ${c.success('\nBackground jobs configured!\n')}
 
           ${!modelExists ? 'Migrate your database to finish setting up jobs:\n' : ''}
-          ${!modelExists ? c.warning('\n\u00A0\u00A0yarn rw prisma migrate dev\n') : ''}
+          ${!modelExists ? c.important('\n\u00A0\u00A0yarn rw prisma migrate dev\n') : ''}
 
-          Generate jobs with: ${c.warning('yarn rw g job <name>')}
-          Execute jobs with:  ${c.warning('yarn rw jobs work\n')}
+          Generate jobs with: ${c.info('yarn rw g job <name>')}
+          Execute jobs with:  ${c.info('yarn rw jobs work\n')}
 
           Check out the docs for more info:
           ${c.link('https://docs.redwoodjs.com/docs/background-jobs')}
