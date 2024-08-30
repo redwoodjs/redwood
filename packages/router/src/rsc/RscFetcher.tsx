254960c884ece1de688daf0f1e8ace75462a6850
Tobbe Lundberg <tobbe@tlundberg.com>
2024-08-30 23:37:59 +0200

    import type React

--- packages/router/src/rsc/RscFetcher.tsx
+++ packages/router/src/rsc/RscFetcher.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react'
+import type { React } from 'react'
+import { useState, useEffect } from 'react'
 
 import type { Options } from 'react-server-dom-webpack/client'
 import { createFromFetch, encodeReply } from 'react-server-dom-webpack/client'
