f795502525f3d9932196e4a8ba9e87bfccfdb290
Tobbe Lundberg <tobbe@tlundberg.com>
2024-09-03 10:56:45 +0200

    New Post, Edit Post

--- __fixtures__/test-project-rsc-kitchen-sink/web/src/layouts/BlogLayout/BlogLayout.tsx
+++ __fixtures__/test-project-rsc-kitchen-sink/web/src/layouts/BlogLayout/BlogLayout.tsx
@@ -1,3 +1,8 @@
+// import { useRouteName } from '@redwoodjs/router/dist/useRouteName'
+import { Link } from '@redwoodjs/router/Link'
+import { namedRoutes as routes } from '@redwoodjs/router/namedRoutes'
+import { getLocation } from '@redwoodjs/server-store'
+
 import BlogPostsNavCell from 'src/components/BlogPostsNavCell/BlogPostsNavCell'
 
 import './BlogLayout.css'
@@ -7,10 +12,34 @@ type BlogLayoutProps = {
 }
 
 const BlogLayout = ({ children }: BlogLayoutProps) => {
+  // I wish I could do this, but I can't because this is a server component,
+  // and server components can't use hooks
+  // const routeName = useRouteName()
+
+  const { pathname } = getLocation()
+  const blogPostPageMatch = pathname.match(/^\/blog\/([-\w]+)$/)
+  const slug = blogPostPageMatch?.[1]
+
   return (
     <div className="blog-layout">
       <nav className="blog-posts-nav">
         <BlogPostsNavCell />
+        <hr />
+        <ul>
+          <li className="new-post">
+            <Link to={routes.newBlogPost()}>New Blog Post</Link>
+          </li>
+          {slug && (
+            <>
+              <li>
+                <Link to={routes.editBlogPost({ slug })}>Edit Blog Post</Link>
+              </li>
+              <li>
+                <button type="button">Delete Blog Post</button>
+              </li>
+            </>
+          )}
+        </ul>
       </nav>
       <section>{children}</section>
     </div>
---
+++ __fixtures__/test-project-rsc-kitchen-sink/web/src/pages/EditBlogPostPage/EditBlogPostPage.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from '@redwoodjs/web/Metadata'
+
+const EditBlogPostPage = () => {
+  return (
+    <>
+      <Metadata title="EditBlogPost" description="EditBlogPost page" />
+
+      <h1>EditBlogPostPage</h1>
+    </>
+  )
+}
+
+export default EditBlogPostPage
