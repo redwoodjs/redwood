1efb918b1ac96c9cc4dc890fc2205df795dcb390
Peter Pistorius <peter.pistorius@gmail.com>
2024-09-10 10:37:11 +0200

    Enable the not found page.

--- packages/vite/src/envs/__example__/web/src/Routes.tsx
+++ packages/vite/src/envs/__example__/web/src/Routes.tsx
@@ -6,7 +6,7 @@ import { useAuth } from './auth'
 
 // NOTE(jgmw): These are typically injected by a babel plugin
 import HomePage from './pages/HomePage/HomePage.jsx'
-// import NotFoundPage from './pages/NotFoundPage/NotFoundPage.jsx'
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage.jsx'
 import Test1Page from './pages/Test1Page/Test1Page.jsx'
 import Test2Page from './pages/Test2Page/Test2Page.jsx'
 
@@ -16,7 +16,7 @@ const Routes = () => {
       <Route path="/test-2" page={Test2Page} name="test-2" />
       <Route path="/test-1" page={Test1Page} name="test-1" />
       <Route path="/" page={HomePage} name="home" />
-      {/* <Route notfound page={NotFoundPage} /> */}
+      <Route notfound page={NotFoundPage} />
     </Router>
   )
 }
