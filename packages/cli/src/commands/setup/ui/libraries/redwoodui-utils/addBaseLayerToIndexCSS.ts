b23d6691fe4eb34a964e14b5cabe96ab36aa6ef4
ari <ari@mendelow.me>
2024-09-19 20:05:34 -0700

    Better address conflicting classes and such

--- packages/cli/src/commands/setup/ui/libraries/redwoodui-utils/addBaseLayerToIndexCSS.ts
+++ packages/cli/src/commands/setup/ui/libraries/redwoodui-utils/addBaseLayerToIndexCSS.ts
@@ -25,48 +25,85 @@ const addBaseLayerToIndexCSS = (
     if (hasEmptyBaseLayer) {
       newCSSContent = projectIndexCSS.replace(
         /@layer base {[^}]*}/s,
-        `@layer base {\n  ${rwuiBaseLayer}\n}\n`,
+        `@layer base {\n  ${rwuiBaseLayer}\n}`,
       )
       task.output = c.success(
         "Added RedwoodUI's base layer to your project's index.css.",
       )
     } else {
       // If there's no base layer, add the RWUI base layer to the end of the file.
-      newCSSContent = projectIndexCSS + `\n@layer base {\n  ${rwuiBaseLayer}\n}`
+      newCSSContent = projectIndexCSS + `\n@layer base {\n  ${rwuiBaseLayer}}`
       task.output = c.success(
         "Added RedwoodUI's base layer to your project's index.css.",
       )
     }
   } else {
     // If the project does have a base layer, check whether its classes have the same name as those of the RWUI base layer.
-    const projectBaseLayerClasses: string[] =
-      projectBaseLayer.match(/[a-zA-Z0-9_-]+(?=\s*\{)/g) || []
-    const rwuiBaseLayerClasses: string[] =
+    // Here, in the base layer, these are HTML tags, not classes. Therefore, they won't start with a dot.
+    const classesToAdd: string[] =
       rwuiBaseLayer.match(/[a-zA-Z0-9_-]+(?=\s*\{)/g) || []
 
-    console.log('projectBaseLayerClasses', projectBaseLayerClasses)
-    console.log('rwuiBaseLayerClasses', rwuiBaseLayerClasses)
+    // For each class that we want to add, check if it already exists in the project's base layer.
+    // If it does, check if it's the same as the RWUI base layer class.
+    // If it is, remove it from the list of classes to add.
+    // If it's not, add it to a list of conflicting classes.
+    const conflictingClasses: string[] = []
 
-    const conflictingClasses = projectBaseLayerClasses.filter((cls) =>
-      rwuiBaseLayerClasses.includes(cls),
-    )
+    classesToAdd.forEach((className) => {
+      const classRegex = new RegExp(`(${className}\\s*{[^}]*})`, 's')
+      const rwuiClassMatch = rwuiBaseLayer.match(classRegex)
+      const projectClassMatch = projectBaseLayer.match(classRegex)
 
-    const hasConflictingClasses = conflictingClasses.length > 0
+      if (projectClassMatch) {
+        // If the class exists in the project's base layer, check if it's the same as the RWUI base layer class.
+        // TODO: This is a naive check. It doesn't account for whitespace or ordering differences.
+        if (rwuiClassMatch && rwuiClassMatch[0] === projectClassMatch[0]) {
+          // If it is the same, just remove it from the list of classes to add.
+          classesToAdd.splice(classesToAdd.indexOf(className), 1)
+        } else {
+          // If it's not the same, add it to the list of conflicting classes and remove it from the list of classes to add.
+          conflictingClasses.push(className)
+          classesToAdd.splice(classesToAdd.indexOf(className), 1)
+        }
+      }
+    })
 
-    if (hasConflictingClasses) {
-      task.output = c.warning(
-        "Conflicting classes found between your project's base layer and RedwoodUI's base layer.\nPlease review the following classes in the base layer of your index.css:\n" +
-          `${conflictingClasses.join(', ')}\n\n`,
+    // Now, if there's no conflicting classes or classes to add, we don't need to do anything more.
+    if (conflictingClasses.length === 0 && classesToAdd.length === 0) {
+      task.output = c.info(
+        "Your project's base layer already has the correct classes.",
       )
-    } else {
-      // If they don't match, just add the RWUI base layer to the end of the existing base layer.
-      // TODO: instead of at the end of the layer, is erroneously adding the new classes after the first of the existing classes
-      newCSSContent = projectIndexCSS.replace(
-        /(@layer base {[^}]*})/s,
-        `$1\n  ${rwuiBaseLayer}`,
+      return projectIndexCSS
+    } else if (classesToAdd.length > 0) {
+      // If there are classes to add, add them.
+      // Remember that right now we just have a list of class names, not the full class definition,
+      // so we need to create the list of class definitions to add.
+      const classesToAddString = classesToAdd
+        .map((className) => {
+          const classRegex = new RegExp(`(${className}\\s*{[^}]*})`, 's')
+          const rwuiClassMatch = rwuiBaseLayer.match(classRegex)
+          return rwuiClassMatch ? rwuiClassMatch[0] : ''
+        })
+        .join('\n  ')
+
+      newCSSContent = addToEndOfLayer(
+        'base',
+        classesToAddString,
+        projectIndexCSS,
       )
       task.output = c.success(
-        "Added RedwoodUI's base layer classes to your project's existing base layer in index.css.",
+        "Added the following new classes to your project's base layer in index.css:\n" +
+          `${classesToAdd.join(', ')}`,
+      )
+      task.output += c.warning(
+        "Some classes in RedwoodUI's base layer were not added to your project's base layer because they conflict with existing classes.\nPlease review the following classes in the base layer of your index.css:\n" +
+          `${conflictingClasses.join(', ')}`,
+      )
+    } else {
+      // If there are no classes to add, but there are conflicting classes, throw an error.
+      throw new Error(
+        "Added no new classes to your project's base layer, because they all conflicted with your existing classes.\nPlease review the following classes in the base layer of your index.css:\n" +
+          `${conflictingClasses.join(', ')}`,
       )
     }
   }
@@ -75,3 +112,14 @@ const addBaseLayerToIndexCSS = (
 }
 
 export default addBaseLayerToIndexCSS
+
+const addToEndOfLayer = (
+  layerName: string,
+  layerContent: string,
+  cssFileContent: string,
+): string => {
+  return cssFileContent.replace(
+    new RegExp(`(@layer ${layerName}\\s*{)([^{}]*(?:{[^{}]*}[^{}]*)*)}`, 's'),
+    `$1$2  ${layerContent}\n}`,
+  )
+}
