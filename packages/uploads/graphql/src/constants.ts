250ac95e56c2b39f4a09d7ff117e249399c3e350
David Thyresson <dthyresson@gmail.com>
2024-10-11 18:01:16 -0400

    template updates

--- packages/uploads/graphql/src/constants.ts
+++ packages/uploads/graphql/src/constants.ts
@@ -8,7 +8,12 @@ export const DEFAULT_UPLOAD_TARGET = 'RedwoodUpload'
 //signify that it's for the upload functionality of your app,
 // such as 'uploads'.
 // export const UPLOAD_TARGET = 'uploads'
-export const IMAGE_CONTENT_TYPES = ['image/jpeg', 'image/png', 'image/gif']
+export const IMAGE_CONTENT_TYPES = [
+  'image/jpeg',
+  'image/png',
+  'image/gif',
+  'image/webp',
+]
 export const PDF_CONTENT_TYPE = 'application/pdf'
 export const MAX_FILE_SIZE = 5 * 1024 * 1024
 export const MAX_FILES = 20
