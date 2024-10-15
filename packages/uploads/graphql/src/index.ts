02151e1952bd46110fa96010bd9b5e77b29d8391
David Thyresson <dthyresson@gmail.com>
2024-10-15 18:16:29 -0400

    export types

--- packages/uploads/graphql/src/index.ts
+++ packages/uploads/graphql/src/index.ts
@@ -3,4 +3,10 @@ export { createUploadToken } from './lib/createUploadToken'
 export { validateUploadToken, validateFiles } from './lib/validateUploadToken'
 export type { RedwoodUploadContext } from './lib/validateUploadToken'
 export * from './constants'
-export * from './types'
+export type {
+  UploadTokenPayload,
+  UploadsConfig,
+  UploadErrorMessage,
+  UploadErrorMessages,
+  RedwoodUploadsOptions,
+} from './types'
