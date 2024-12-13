7fc3aca0ddbc277f18840f6db404ae3a9715efe9
Tobbe Lundberg <tobbe@tlundberg.com>
2024-12-13 09:00:52 +0100

    Separate PrivateSetProps

--- packages/router/src/Set.tsx
+++ packages/router/src/Set.tsx
@@ -58,9 +58,38 @@ export function Set<WrapperProps>(props: SetProps<WrapperProps>) {
   return <>{props.children}</>
 }
 
-type PrivateSetProps<P> = Omit<SetProps<P>, 'private' | 'unauthenticated'> & {
+type PrivateSetProps<P> = (P extends React.FC<any>
+  ? React.ComponentProps<P>
+  : P extends React.FC<any>[]
+    ? React.ComponentProps<P[0]> &
+        React.ComponentProps<P[1]> &
+        React.ComponentProps<P[2]> &
+        React.ComponentProps<P[3]> &
+        React.ComponentProps<P[4]> &
+        React.ComponentProps<P[5]> &
+        React.ComponentProps<P[6]> &
+        React.ComponentProps<P[7]> &
+        React.ComponentProps<P[8]> &
+        React.ComponentProps<P[9]>
+    : unknown) & {
+  /**
+   * A React component, or an array of React components, that the children of
+   * the Set will be wrapped in (typically a Layout component and/or a context)
+   */
+  wrap?: P
   /** The page name where a user will be redirected when not authenticated */
-  unauthenticated: keyof AvailableRoutes
+  unauthenticated?: keyof AvailableRoutes
+  /**
+   * Route is permitted when authenticated and user has any of the provided
+   * roles such as "admin" or ["admin", "editor"]
+   */
+  roles?: string | string[]
+  /** Prerender all pages in the set */
+  prerender?: boolean
+  children: ReactNode
+  /** Loading state for auth to distinguish with whileLoading */
+  whileLoadingAuth?: () => ReactElement | null
+  whileLoadingPage?: () => ReactElement | null
 }
 
 /** @deprecated Please use `<PrivateSet>` instead */
