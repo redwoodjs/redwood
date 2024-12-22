redwoodjs/redwood

commit f2051e79c4228a7ac7d82e6088ac0d055def296f

Fix AuthProviderProps export missing from `@redwood/auth` (#11766)
May resolve #11765 

In that case patchreleaseworthy. :wink:

---------

Co-authored-by: Tobbe Lundberg <tobbe@tlundberg.com>

packages/auth/src/index.ts
@@ -1,4 +1,5 @@
 export type { AuthContextInterface, CurrentUser } from './AuthContext.js'
+export type { AuthProviderProps } from './AuthProvider/AuthProvider.js';
 export { useNoAuth } from './useAuth.js'
 export type { UseAuth } from './useAuth.js'
 export { createAuthentication } from './authFactory.js'

commit 77c124b51ef519144fd9b47584516986b0da9625

chore(formatting): Fix formatting in auth/src/index.ts (#11792)
Very minor issue introduced by
https://github.com/redwoodjs/redwood/pull/11766

The reason it went unnoticed is code formatting with prettier was
broken, so I had gotten used to just ignoring the format check failures.
This is being worked around by
https://github.com/redwoodjs/redwood/pull/11791

packages/auth/src/index.ts
@@ -1,5 +1,5 @@
 export type { AuthContextInterface, CurrentUser } from './AuthContext.js'
-export type { AuthProviderProps } from './AuthProvider/AuthProvider.js';
+export type { AuthProviderProps } from './AuthProvider/AuthProvider.js'
 export { useNoAuth } from './useAuth.js'
 export type { UseAuth } from './useAuth.js'
 export { createAuthentication } from './authFactory.js'