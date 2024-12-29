0d9c115425bf3ba73141d8f7fbd9abde88f6d4ad
Philzen <philzen@riseup.net>
2024-12-30 00:37:26 +0100

    [refactor paths tests] Add central DEFAULT_PATHS object

--- packages/project-config/src/__tests__/paths.test.ts
+++ packages/project-config/src/__tests__/paths.test.ts
@@ -14,6 +14,81 @@ import {
 
 const RWJS_CWD = process.env.RWJS_CWD
 
+/**
+ * All paths relevant to the redwood stack as defined in
+ * {@link ../paths | paths.ts}, relative from project root
+ */
+const DEFAULT_PATHS = {
+  base: [],
+  generated: {
+    base: ['.redwood'],
+    schema: ['.redwood', 'schema.graphql'],
+    types: {
+      includes: ['.redwood', 'types', 'includes'],
+      mirror: ['.redwood', 'types', 'mirror'],
+    },
+    prebuild: ['.redwood', 'prebuild'],
+  },
+  scripts: ['scripts'],
+  api: {
+    base: ['api'],
+    dataMigrations: ['api', 'db', 'dataMigrations'],
+    db: ['api', 'db'],
+    dbSchema: ['api', 'db', 'schema.prisma'],
+    functions: ['api', 'src', 'functions'],
+    graphql: ['api', 'src', 'graphql'],
+    lib: ['api', 'src', 'lib'],
+    generators: ['api', 'generators'],
+    config: ['api', 'src', 'config'],
+    services: ['api', 'src', 'services'],
+    directives: ['api', 'src', 'directives'],
+    subscriptions: ['api', 'src', 'subscriptions'],
+    src: ['api', 'src'],
+    dist: ['api', 'dist'],
+    types: ['api', 'types'],
+    models: ['api', 'src', 'models'],
+    mail: ['api', 'src', 'mail'],
+    jobs: ['api', 'src', 'jobs'],
+    distJobs: ['api', 'dist', 'jobs'],
+    jobsConfig: ['api', 'src', 'lib', 'jobs'],
+    distJobsConfig: ['api', 'dist', 'lib', 'jobs'],
+    logger: ['api', 'src', 'lib', 'logger.ts'],
+  },
+
+  web: {
+    routes: ['web', 'src', 'Routes.tsx'],
+    base: ['web'],
+    pages: ['web', 'src', 'pages/'],
+    components: ['web', 'src', 'components'],
+    layouts: ['web', 'src', 'layouts/'],
+    src: ['web', 'src'],
+    storybook: ['web', '.storybook'],
+    generators: ['web', 'generators'],
+    app: ['web', 'src', 'App.tsx'],
+    document: ['web', 'src', 'Document'],
+    html: ['web', 'src', 'index.html'],
+    config: ['web', 'config'],
+    viteConfig: ['web', 'vite.config.ts'],
+    postcss: ['web', 'config', 'postcss.config.js'],
+    storybookConfig: ['web', '.storybook', 'main.js'],
+    storybookPreviewConfig: ['web', '.storybook', 'preview.js'],
+    storybookManagerConfig: ['web', '.storybook', 'manager.js'],
+    dist: ['web', 'dist'],
+    distBrowser: ['web', 'dist', 'browser'],
+    distRsc: ['web', 'dist', 'rsc'],
+    distSsr: ['web', 'dist', 'ssr'],
+    distSsrDocument: ['web', 'dist', 'ssr', 'Document.mjs'],
+    distSsrEntryServer: ['web', 'dist', 'ssr', 'entry.server.mjs'],
+    distRouteHooks: ['web', 'dist', 'ssr', 'routeHooks'],
+    distRscEntries: ['web', 'dist', 'rsc', 'entries.mjs'],
+    routeManifest: ['web', 'dist', 'ssr', 'route-manifest.json'],
+    types: ['web', 'types'],
+    entryClient: ['web', 'src', 'entry.client.tsx'], // new vite/stream entry point for client
+    entryServer: ['web', 'src', 'entry.server'],
+    graphql: ['web', 'src', 'graphql'],
+  },
+}
+
 describe('paths', () => {
   describe('within empty project', () => {
     const FIXTURE_BASEDIR = path.join(
@@ -323,6 +398,7 @@ describe('paths', () => {
     })
 
     it('gets the correct paths', () => {
+      // todo api.routes tsx → '.js', web.app tsx  → js
       const expectedPaths = {
         base: FIXTURE_BASEDIR,
         generated: {
@@ -643,6 +719,7 @@ describe('paths', () => {
     })
 
     it('gets the correct paths', () => {
+      // todo api.routes .tsx → .js
       const expectedPaths = {
         base: FIXTURE_BASEDIR,
         generated: {
