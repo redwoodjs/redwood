20381a77c98fd5eb0066c0ebac28924e65b6fb4d
Philzen <Philzen@users.noreply.github.com>
2025-01-08 04:33:23 +0100

    [chore] Re-use loadEnvFiles from cli-helpers lib, elimininating code duplicatation (#11885)

--- packages/cli-helpers/src/index.ts
+++ packages/cli-helpers/src/index.ts
@@ -3,6 +3,12 @@
 
 export * from './lib/index.js'
 export * from './lib/colors.js'
+export { loadEnvFiles } from './lib/loadEnvFiles.js'
+export {
+  loadDefaultEnvFiles,
+  loadNodeEnvDerivedEnvFile,
+  loadUserSpecifiedEnvFiles,
+} from './lib/loadEnvFiles.js'
 export * from './lib/paths.js'
 export * from './lib/project.js'
 export * from './lib/version.js'
