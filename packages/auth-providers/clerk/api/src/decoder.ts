df74040fcd35fad74bf4f8bdb1c02bfd7fce0af1
wobsoriano <sorianorobertc@gmail.com>
2025-02-16 17:33:06 -0800

    chore(clerk): Migrate to @clerk/backend SDK

--- packages/auth-providers/clerk/api/src/decoder.ts
+++ packages/auth-providers/clerk/api/src/decoder.ts
@@ -8,17 +8,14 @@ export const authDecoder: Decoder = async (token: string, type: string) => {
     return null
   }
 
-  const { users, verifyToken } = await import('@clerk/clerk-sdk-node')
+  const { createClerkClient, verifyToken } = await import('@clerk/backend')
+
+  const clerk = createClerkClient({
+    jwtKey: process.env.CLERK_JWT_KEY,
+  })
 
   try {
-    const issuer = (iss: string) =>
-      iss.startsWith('https://clerk.') || iss.includes('.clerk.accounts')
-
     const jwtPayload = await verifyToken(token, {
-      issuer,
-      apiUrl: process.env.CLERK_API_URL || 'https://api.clerk.dev',
-      jwtKey: process.env.CLERK_JWT_KEY,
-      apiKey: process.env.CLERK_API_KEY,
       secretKey: process.env.CLERK_SECRET_KEY,
     })
 
@@ -26,7 +23,7 @@ export const authDecoder: Decoder = async (token: string, type: string) => {
       return Promise.reject(new Error('Session invalid'))
     }
 
-    const user = await users.getUser(jwtPayload.sub)
+    const user = await clerk.users.getUser(jwtPayload.sub)
 
     return {
       ...user,
@@ -46,18 +43,11 @@ export const clerkAuthDecoder: Decoder = async (
     return null
   }
 
-  const { verifyToken } = await import('@clerk/clerk-sdk-node')
+  const { verifyToken } = await import('@clerk/backend')
 
   try {
-    const issuer = (iss: string) =>
-      iss.startsWith('https://clerk.') || iss.includes('.clerk.accounts')
-
     const jwtPayload = await verifyToken(token, {
-      issuer,
-      apiUrl: process.env.CLERK_API_URL || 'https://api.clerk.dev',
       jwtKey: process.env.CLERK_JWT_KEY,
-      apiKey: process.env.CLERK_API_KEY,
-      secretKey: process.env.CLERK_SECRET_KEY,
     })
 
     if (!jwtPayload.sub) {
